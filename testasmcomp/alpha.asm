7d761dd6dfe2f119131592deba0d9f9b30231de4
Xavier Leroy <xavier.leroy@inria.fr>
1995-07-07 12:08:38 +0000

    MAJ des conventions d'appel.
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@66 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- testasmcomp/alpha.asm
+++ testasmcomp/alpha.asm
@@ -1,16 +1,6 @@
         .globl  call_gen_code
         .ent    call_gen_code
 	
-/****
-               C                               Caml Light
-
- Args:         $16 $17 $18 $19	                $9 $10 $11 $12
-               $f16 $f17 $f18 $f19             $f2 $f3 $f4 $f5
-
- Results:      $0                              $f0
-               $0                              $f0
-****/
-
 call_gen_code:
         lda     $sp, -80($sp)
         stq     $26, 0($sp)
@@ -22,16 +12,12 @@ call_gen_code:
         stt     $f3, 48($sp)
         stt     $f4, 56($sp)
         stt     $f5, 64($sp)
-        mov     $17, $9
-        mov     $18, $10
-        mov     $19, $11
-        mov     $20, $12
-        fmov    $f16, $f2
-        fmov    $f17, $f3
-        fmov    $f18, $f4
-        fmov    $f19, $f5
         mov     $16, $27
-        jsr     ($16)
+        mov     $17, $16
+        mov     $18, $17
+        mov     $19, $18
+        mov     $20, $19
+        jsr     ($27)
         ldq     $26, 0($sp)
         ldq     $9, 8($sp)
         ldq     $10, 16($sp)
