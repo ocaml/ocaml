f2237ca6ba6c31a5722333c503bc0eda1fc51786
Xavier Leroy <xavier.leroy@inria.fr>
1995-07-11 18:09:15 +0000

    Eclatement de meta.c en obj.c et meta.c
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@86 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

---
+++ byterun/obj.c
@@ -0,0 +1,58 @@
+/* Operations on objects */
+
+#include "alloc.h"
+#include "major_gc.h"
+#include "memory.h"
+#include "minor_gc.h"
+#include "misc.h"
+#include "mlvalues.h"
+#include "prims.h"
+
+value static_alloc(size)        /* ML */
+     value size;
+{
+  return (value) stat_alloc((asize_t) Long_val(size));
+}
+
+value static_free(blk)          /* ML */
+     value blk;
+{
+  stat_free((char *) blk);
+  return Val_unit;
+}
+
+value static_resize(blk, new_size) /* ML */
+     value blk, new_size;
+{
+  return (value) stat_resize((char *) blk, (asize_t) Long_val(new_size));
+}
+
+value obj_is_block(arg)             /* ML */
+     value arg;
+{
+  return Val_bool(Is_block(arg));
+}
+
+value obj_tag(arg)                 /* ML */
+     value arg;
+{
+  return Val_int(Tag_val(arg));
+}
+
+value obj_block(tag, size) /* ML */
+     value tag, size;
+{
+  value res;
+  mlsize_t sz, i;
+  tag_t tg;
+
+  sz = Long_val(size);
+  tg = Long_val(tag);
+  if (sz == 0) return Atom(tg);
+  res = alloc(sz, tg);
+  for (i = 0; i < sz; i++)
+    Field(res, i) = Val_long(0);
+
+  return res;
+}
+
