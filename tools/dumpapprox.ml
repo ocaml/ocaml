4cac7cd8a6bb28872e37eba29adc366aaea75dac
Xavier Leroy <xavier.leroy@inria.fr>
1995-07-13 09:02:55 +0000

    Affichage des CRC.
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@102 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- tools/dumpapprox.ml
+++ tools/dumpapprox.ml
@@ -28,19 +28,20 @@ let rec print_approx = function
   | Value_unknown ->
       print_string "_"
 
-let print_infos ui =
+let print_name_crc (name, crc) =
+  print_space(); print_string name;
+  print_string " ("; print_int crc; print_string ")"
+
+let print_infos (ui, crc) =
   print_string "Name: "; print_string ui.ui_name; print_newline();
+  print_string "CRC: "; print_int crc; print_newline();
   open_hovbox 2;
   print_string "Interfaces imported:";
-  List.iter
-    (fun (name, crc) -> print_space(); print_string name)
-    ui.ui_interfaces;
+  List.iter print_name_crc ui.ui_interfaces;
   close_box(); print_newline();
   open_hovbox 2;
   print_string "Implementations imported:";
-  List.iter
-    (fun (name, crc) -> print_space(); print_string name)
-    ui.ui_interfaces;
+  List.iter print_name_crc ui.ui_imports;
   close_box(); print_newline();
   open_hovbox 2;
   print_string "Approximation:"; print_space(); print_approx ui.ui_approx;
@@ -71,7 +72,7 @@ let read_unit_info filename =
     let ui = (input_value ic : unit_infos) in
     let crc = input_binary_int ic in
     close_in ic;
-    ui
+    (ui, crc)
   with End_of_file | Failure _ ->
     close_in ic;
     prerr_endline "Error reading file";
