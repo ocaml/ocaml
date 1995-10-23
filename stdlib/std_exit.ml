862813719d571b907ff3cb3ffddf7ead6a7138c1
Xavier Leroy <xavier.leroy@inria.fr>
1995-10-23 17:00:07 +0000

    Ajout de std_exit
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@352 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

---
+++ stdlib/std_exit.ml
@@ -0,0 +1,16 @@
+(***********************************************************************)
+(*                                                                     *)
+(*                         Caml Special Light                          *)
+(*                                                                     *)
+(*            Xavier Leroy, projet Cristal, INRIA Rocquencourt         *)
+(*                                                                     *)
+(*  Copyright 1995 Institut National de Recherche en Informatique et   *)
+(*  Automatique.  Distributed only by permission.                      *)
+(*                                                                     *)
+(***********************************************************************)
+
+(* $Id$ *)
+
+(* Ensure that [exit] is called at the end of every program *)
+
+let _ = exit 0
