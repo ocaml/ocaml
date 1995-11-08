bf969d0aa8be613080b33e993e7bf058017754f3
No author <no_author@ocaml.org>
1995-11-08 10:36:54 +0000

    This commit was manufactured by cvs2svn to create tag 'rel110'.
    git-svn-id: http://caml.inria.fr/svn/ocaml/release/csl-1.10@421 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- bytecomp/codegen.mli
+++
@@ -1,8 +0,0 @@
-(* Generation of bytecode from lambda terms *)
-
-open Lambda
-open Instruct
-
-val compile_implementation: lambda -> instruction list
-val compile_phrase: lambda -> instruction list * instruction list
-
