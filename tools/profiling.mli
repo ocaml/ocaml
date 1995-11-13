78a3daa3649c6705ca6b1c9096210c2c634780ca
Xavier Leroy <xavier.leroy@inria.fr>
1995-11-13 14:25:17 +0000

    Le module Profiling a ete sorti de la lib standard et mis ici. Raison: qd on linke en -linkall (e.g. pour le toplevel), on ne veut pas linker Profiling systematiquement.
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@442 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

---
+++ tools/profiling.mli
@@ -0,0 +1,17 @@
+(***********************************************************************)
+(*                                                                     *)
+(*                         Caml Special Light                          *)
+(*                                                                     *)
+(*      Damien Doligez and Francois Rouaix, INRIA Rocquencourt         *)
+(*   Ported to Caml Special Light by John Malecki and Xavier Leroy     *)
+(*                                                                     *)
+(*  Copyright 1995 Institut National de Recherche en Informatique et   *)
+(*  Automatique.  Distributed only by permission.                      *)
+(*                                                                     *)
+(***********************************************************************)
+
+(* $Id$ *)
+
+(* Run-time library for profiled programs *)
+
+val counters: (string * (string * int array)) list ref
