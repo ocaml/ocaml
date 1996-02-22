c8137bb64e17c9c1fe01bbe1c946282ed3240b72
Xavier Leroy <xavier.leroy@inria.fr>
1996-02-22 12:53:13 +0000

    Ajout d'un header special pour NT (utilise spawn au lieu de exec).
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@656 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

---
+++ stdlib/headernt.c
@@ -0,0 +1,33 @@
+/***********************************************************************/
+/*                                                                     */
+/*                         Caml Special Light                          */
+/*                                                                     */
+/*            Xavier Leroy, projet Cristal, INRIA Rocquencourt         */
+/*                                                                     */
+/*  Copyright 1995 Institut National de Recherche en Informatique et   */
+/*  Automatique.  Distributed only by permission.                      */
+/*                                                                     */
+/***********************************************************************/
+
+/* $Id$ */
+
+#include <process.h>
+
+char * runtime_name = "cslrun.exe";
+char * errmsg = "Cannot find cslrun.exe\n";
+
+int main(argc, argv)
+     int argc;
+     char ** argv;
+{
+  int retcode;
+  retcode = spawnvp(P_WAIT, runtime_name, argv);
+  /* We use P_WAIT instead of P_OVERLAY here because under NT,
+     P_OVERLAY returns to the command interpreter, displaying the prompt
+     before executing the command. */
+  if (retcode == -1) {
+    write(2, errmsg, strlen(errmsg));
+    return 2;
+  }
+  return retcode;
+}
