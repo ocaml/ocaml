83139b71063ac4c1193006c7d9334b55ff028a34
Xavier Leroy <xavier.leroy@inria.fr>
1996-04-02 08:41:11 +0000

    Adaptation aux vrais threads POSIX (avec code de compatibilite pour OSF1).
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@726 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- otherlibs/threads/threadstubs.c
+++ otherlibs/threads/threadstubs.c
@@ -20,6 +20,11 @@
 #include "roots.h"
 #include "signals.h"
 #include "stacks.h"
+#include "sys.h"
+
+#ifdef HAS_THR_YIELD
+#include <thread.h>
+#endif
 
 /* Max computation time before rescheduling, in microseconds (50ms) */
 #define Thread_timeout 50000
@@ -29,7 +34,7 @@
 
 /* The thread descriptors */
 
-struct thread_struct {
+struct csl_thread_struct {
   pthread_t pthread;            /* The corresponding Posix thread */
   value ident;                  /* Unique id */
   value * stack_low;            /* The execution stack for this thread */
@@ -37,11 +42,11 @@ struct thread_struct {
   value * stack_threshold;
   value * sp;                   /* Saved value of extern_sp for this thread */
   value * trapsp;               /* Saved value of trapsp for this thread */
-  struct thread_struct * next;  /* Double linking of threads */
-  struct thread_struct * prev;
+  struct csl_thread_struct * next;  /* Double linking of threads */
+  struct csl_thread_struct * prev;
 };
 
-typedef struct thread_struct * thread_t;
+typedef struct csl_thread_struct * csl_thread_t;
 
 #define Assign(dst,src) modify((value *)&(dst), (value)(src))
 
@@ -50,7 +55,7 @@ typedef struct thread_struct * thread_t;
 pthread_mutex_t csl_mutex;
 
 /* Head of the list of thread descriptors */
-thread_t thread_list = NULL;
+csl_thread_t thread_list = NULL;
 
 /* The key used for storing the thread descriptor in the specific data
    of the corresponding Posix thread. */
@@ -59,6 +64,24 @@ pthread_key_t thread_descriptor_key;
 /* Identifier for next thread creation */
 static value thread_next_ident = 0;
 
+/* Compatibility code for DEC OSF1 */
+
+#ifdef __osf__
+#define Attr_default pthread_attr_default                    
+#define Mutexattr_default pthread_mutexattr_default
+#define Condattr_default pthread_condattr_default
+#define Getspecific(res,key) pthread_getspecific(key, (void **) &(res))
+#define Pthread_key_create pthread_keycreate
+#define Pthread_detach(th) pthread_detach(&(th))
+#else
+#define Attr_default NULL
+#define Mutexattr_default NULL
+#define Condattr_default NULL
+#define Getspecific(res,key) (res) = pthread_getspecific(key)
+#define Pthread_key_create pthread_key_create
+#define Pthread_detach pthread_detach
+#endif
+
 /* Hook for scanning the stacks of the other threads */
 
 static void (*prev_scan_roots_hook) P((scanning_action));
@@ -66,7 +89,7 @@ static void (*prev_scan_roots_hook) P((scanning_action));
 static void csl_thread_scan_roots(action)
      scanning_action action;
 {
-  thread_t th;
+  csl_thread_t th;
   register value * sp;
   /* Scan all thread descriptors */
   (*action)((value) thread_list, (value *) &thread_list);
@@ -90,12 +113,12 @@ static void (*prev_leave_blocking_section_hook) ();
 
 static void csl_thread_enter_blocking_section()
 {
-  thread_t curr_thread;
+  csl_thread_t curr_thread;
   if (prev_enter_blocking_section_hook != NULL)
     (*prev_enter_blocking_section_hook)();
   /* Save the stack-related global variables in the thread descriptor
      of the current thread */
-  pthread_getspecific(thread_descriptor_key, (void **) &curr_thread);
+  Getspecific(curr_thread, thread_descriptor_key);
   curr_thread->stack_low = stack_low;
   curr_thread->stack_high = stack_high;
   curr_thread->stack_threshold = stack_threshold;
@@ -105,25 +128,13 @@ static void csl_thread_enter_blocking_section()
   pthread_mutex_unlock(&csl_mutex);
 }
 
-/*********** DEBUG **************/
-
-#ifdef DEBUG
-
-static void print_self_thread()
-{
-  pthread_t me = pthread_self();
-  printf("Thread %x/%x/%x: ", me.field1, me.field2, me.field3);
-}
-
-#endif
-
 static void csl_thread_leave_blocking_section()
 {
-  thread_t curr_thread;
+  csl_thread_t curr_thread;
   /* Re-acquire the global mutex */
   pthread_mutex_lock(&csl_mutex);
   /* Restore the stack-related global variables */
-  pthread_getspecific(thread_descriptor_key, (void **) &curr_thread);
+  Getspecific(curr_thread, thread_descriptor_key);
   stack_low = curr_thread->stack_low;
   stack_high = curr_thread->stack_high;
   stack_threshold = curr_thread->stack_threshold;
@@ -135,7 +146,7 @@ static void csl_thread_leave_blocking_section()
 
 /* The "tick" thread fakes a SIGVTALRM signal at regular intervals. */
 
-static void csl_thread_tick()
+static void * csl_thread_tick()
 {
   struct timeval timeout;
   while(1) {
@@ -152,12 +163,8 @@ static void csl_thread_tick()
    free the stack space and the descriptor itself. */
 
 static void csl_thread_cleanup(th)
-     thread_t th;
+     csl_thread_t th;
 {
-#ifdef DEBUG
-  print_self_thread();
-  printf("cleaning up, data %lx\n", th);
-#endif
   /* Remove th from the doubly-linked list of threads */
   if (th == thread_list) {
     thread_list = th->next;
@@ -181,14 +188,14 @@ value csl_thread_initialize(unit)   /* ML */
 {
   pthread_t tick_pthread;
   /* Initialize the mutex */
-  if (pthread_mutex_init(&csl_mutex, pthread_mutexattr_default) == -1)
+  if (pthread_mutex_init(&csl_mutex, Mutexattr_default) == -1)
     sys_error("Thread.init");
   pthread_mutex_lock(&csl_mutex);
   /* Initialize the key */
-  pthread_keycreate(&thread_descriptor_key, NULL);
+  Pthread_key_create(&thread_descriptor_key, NULL);
   /* Create a descriptor for the current thread */
-  thread_list =
-    (thread_t) alloc_shr(sizeof(struct thread_struct) / sizeof(value), 0);
+  thread_list = (csl_thread_t)
+    alloc_shr(sizeof(struct csl_thread_struct) / sizeof(value), 0);
   thread_list->pthread = pthread_self();
   thread_list->ident = Val_long(thread_next_ident);
   thread_next_ident++;
@@ -198,10 +205,6 @@ value csl_thread_initialize(unit)   /* ML */
   thread_list->prev = NULL;
   /* Associate the thread descriptor with the thread */
   pthread_setspecific(thread_descriptor_key, (void *) thread_list);
-#ifdef DEBUG
-  print_self_thread();
-  printf("main thread, data %lx\n", (long) thread_list);
-#endif
   /* Set up the hooks */
   prev_scan_roots_hook = scan_roots_hook;
   scan_roots_hook = csl_thread_scan_roots;
@@ -210,25 +213,20 @@ value csl_thread_initialize(unit)   /* ML */
   prev_leave_blocking_section_hook = leave_blocking_section_hook;
   leave_blocking_section_hook = csl_thread_leave_blocking_section;
   /* Fork the tick thread */
-  if (pthread_create(&tick_pthread, pthread_attr_default,
-                     csl_thread_tick, 0) == -1)
+  if (pthread_create(&tick_pthread, Attr_default, csl_thread_tick, 0) == -1)
     sys_error("Thread.init");
-  pthread_detach(&tick_pthread);
+  Pthread_detach(tick_pthread);
   return Val_unit;
 }
 
 /* Create a thread */
 
 static void * csl_thread_start(th)
-     thread_t th;
+     csl_thread_t th;
 {
   value clos;
   /* Associate the thread descriptor with the thread */
   pthread_setspecific(thread_descriptor_key, (void *) th);
-#ifdef DEBUG
-  print_self_thread();
-  printf("started, data %lx\n", (long) th);
-#endif
   /* Set up termination routine */
   pthread_cleanup_push(csl_thread_cleanup, (void *) th);
   /* Acquire the global mutex before running the thread */
@@ -244,10 +242,6 @@ static void * csl_thread_start(th)
   callback(clos, Val_unit);
   /* Cleanup: free the thread resources */
   pthread_cleanup_pop(1);
-#ifdef DEBUG
-  print_self_thread();
-  printf("terminated");
-#endif
   /* Release the mutex and die quietly */
   pthread_mutex_unlock(&csl_mutex);
   return 0;
@@ -256,9 +250,10 @@ static void * csl_thread_start(th)
 value csl_thread_new(clos)          /* ML */
      value clos;
 {
-  thread_t th;
+  csl_thread_t th;
   /* Allocate the thread and its stack */
-  th = (thread_t) alloc_shr(sizeof(struct thread_struct) / sizeof(value), 0);
+  th = (csl_thread_t)
+    alloc_shr(sizeof(struct csl_thread_struct) / sizeof(value), 0);
   th->ident = Val_long(thread_next_ident);
   thread_next_ident++;
   th->stack_low = (value *) stat_alloc(Thread_stack_size);
@@ -275,7 +270,7 @@ value csl_thread_new(clos)          /* ML */
      preserved by garbage collection */
   *--(th->sp) = clos;
   /* Fork the new thread */
-  if (pthread_create(&(th->pthread), pthread_attr_default, csl_thread_start,
+  if (pthread_create(&(th->pthread), Attr_default, csl_thread_start,
                      (void *) th) == -1)
     sys_error("Thread.new");
   return (value) th;
@@ -286,15 +281,15 @@ value csl_thread_new(clos)          /* ML */
 value csl_thread_self(unit)         /* ML */
      value unit;
 {
-  thread_t curr_thread;
-  pthread_getspecific(thread_descriptor_key, (void **) &curr_thread);
+  csl_thread_t curr_thread;
+  Getspecific(curr_thread, thread_descriptor_key);
   return (value) curr_thread;
 }
 
 /* Return the identifier of a thread */
 
 value csl_thread_id(th)          /* ML */
-     thread_t th;
+     csl_thread_t th;
 {
   return th->ident;
 }
@@ -302,9 +297,14 @@ value csl_thread_id(th)          /* ML */
 /* Allow re-scheduling */
 
 value csl_thread_yield(unit)        /* ML */
+     value unit;
 {
   enter_blocking_section();
+#if defined(HAS_PTHREAD_YIELD)
   pthread_yield();
+#elif defined(HAS_THR_YIELD)
+  thr_yield();
+#endif
   leave_blocking_section();
   return Val_unit;
 }
@@ -312,16 +312,16 @@ value csl_thread_yield(unit)        /* ML */
 /* Detach a thread */
 
 value csl_thread_detach(th)         /* ML */
-     thread_t th;
+     csl_thread_t th;
 {
-  if (pthread_detach(&(th->pthread)) == -1) sys_error("Thread.detach");
+  if (Pthread_detach(th->pthread) == -1) sys_error("Thread.detach");
   return Val_unit;
 }
 
 /* Suspend the current thread until another thread terminates */
 
 value csl_thread_join(th)          /* ML */
-     thread_t th;
+     csl_thread_t th;
 {
   void * status;
   int retcode;
@@ -353,19 +353,13 @@ static void csl_mutex_finalize(mut)
   pthread_mutex_destroy(&Mutex_val(mut));
 }
 
-#ifdef __osf__
-#define Mutexattr pthread_mutexattr_default
-#else
-#define Mutexattr NULL
-#endif
-
 value csl_mutex_new(unit)        /* ML */
      value unit;
 {
   value mut;
   mut = alloc_final(1 + sizeof(pthread_mutex_t) / sizeof(value),
                     csl_mutex_finalize, 1, Max_mutex_number);
-  if (pthread_mutex_init(&Mutex_val(mut), Mutexattr) == -1)
+  if (pthread_mutex_init(&Mutex_val(mut), Mutexattr_default) == -1)
     sys_error("Mutex.new");
   return mut;
 }
@@ -412,19 +406,13 @@ static void csl_condition_finalize(cond)
   pthread_cond_destroy(&Condition_val(cond));
 }
 
-#ifdef __osf__
-#define Condattr pthread_condattr_default
-#else
-#define Condattr NULL
-#endif
-
 value csl_condition_new(unit)        /* ML */
      value unit;
 {
   value cond;
   cond = alloc_final(1 + sizeof(pthread_cond_t) / sizeof(value),
                      csl_condition_finalize, 1, Max_condition_number);
-  if (pthread_cond_init(&Condition_val(cond), Condattr) == -1)
+  if (pthread_cond_init(&Condition_val(cond), Condattr_default) == -1)
     sys_error("Condition.new");
   return cond;
 }
