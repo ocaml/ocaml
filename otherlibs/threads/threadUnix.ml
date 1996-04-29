b13bc56289f598f9477f769635a10f042d5efba9
Xavier Leroy <xavier.leroy@inria.fr>
1996-04-29 13:25:10 +0000

    Thread-compatible syscalls.
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@773 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

---
+++ otherlibs/threads/threadUnix.ml
@@ -0,0 +1,158 @@
+(***********************************************************************)
+(*                                                                     *)
+(*                         Caml Special Light                          *)
+(*                                                                     *)
+(*            Xavier Leroy, projet Cristal, INRIA Rocquencourt         *)
+(*                                                                     *)
+(*  Copyright 1995 Institut National de Recherche en Informatique et   *)
+(*  Automatique.  Distributed only by permission.                      *)
+(*                                                                     *)
+(***********************************************************************)
+
+(* $Id$ *)
+
+(* Module [ThreadUnix]: thread-compatible system calls *)
+
+open Unix
+
+(*** Process handling *)
+
+(* Disable the timer interrupt before doing exec, because some OS
+   keep sending timer interrupts to the exec'ed code.
+   Make sure we're not preempted just after disabling the timer... *)
+let execv proc args =
+  Thread.critical_section := true;
+  Unix.setitimer ITIMER_VIRTUAL {it_interval = 0.0; it_value = 0.0};
+  Unix.execv proc args
+
+let execve proc args env =
+  Thread.critical_section := true;
+  Unix.setitimer ITIMER_VIRTUAL {it_interval = 0.0; it_value = 0.0};
+  Unix.execv proc args
+
+let execvp proc args =
+  Thread.critical_section := true;
+  Unix.setitimer ITIMER_VIRTUAL {it_interval = 0.0; it_value = 0.0};
+  Unix.execv proc args
+
+let wait () =
+  Thread.wait_pid (-1)
+  
+let waitpid flags pid =
+  if List.mem WNOHANG flags
+  then Unix.waitpid flags pid
+  else Thread.wait_pid pid
+
+let system cmd =
+  match fork() with
+     0 -> execv "/bin/sh" [| "/bin/sh"; "-c"; cmd |]; exit 127
+  | id -> snd(waitpid [] id)
+
+(*** File I/O *)
+
+let read fd buff ofs len =
+  Thread.wait_read fd;
+  Unix.read fd buff ofs len
+
+let write fd buff ofs len =
+  Thread.wait_write fd;
+  Unix.write fd buff ofs len
+
+let timed_read fd buff ofs len timeout =
+  if Thread.wait_timed_read fd timeout
+  then Unix.read fd buff ofs len
+  else raise (Unix_error(ETIMEDOUT, "timed_read", ""))
+
+let timed_write fd buff ofs len timeout =
+  if Thread.wait_timed_write fd timeout
+  then Unix.write fd buff ofs len
+  else raise (Unix_error(ETIMEDOUT, "timed_write", ""))
+
+(*** Pipes *)
+
+let pipe() =
+  let (out_fd, in_fd as fd_pair) = Unix.pipe() in
+  Unix.set_nonblock in_fd;
+  fd_pair
+
+let open_process_out cmd =
+  let oc = Unix.open_process_out cmd in
+  Unix.set_nonblock(Unix.descr_of_out_channel oc);
+  oc
+
+let open_process cmd =
+  let (ic, oc as channels) = Unix.open_process cmd in
+  Unix.set_nonblock(Unix.descr_of_out_channel oc);
+  channels
+
+(*** Time *)
+
+let sleep secs =
+  Thread.delay (float secs)
+
+(*** Sockets *)
+
+let socket dom typ proto =
+  let s = Unix.socket dom typ proto in
+  Unix.set_nonblock s;
+  s
+
+let socketpair dom typ proto =
+  let (s1, s2 as spair) = Unix.socketpair dom typ proto in
+  Unix.set_nonblock s1; Unix.set_nonblock s2;
+  spair
+
+let accept req =
+  Thread.wait_read req;
+  let (s, caller as result) = Unix.accept req in
+  Unix.set_nonblock s;
+  result
+
+let connect s addr =
+  try
+    Unix.connect s addr
+  with Unix_error((EINPROGRESS | EWOULDBLOCK | EAGAIN), _, _) ->
+    Thread.wait_write s;
+    (* Check if it really worked *)
+    Unix.getpeername s;
+    ()
+
+let recv fd buf ofs len flags =
+  Thread.wait_read fd; Unix.recv fd buf ofs len flags
+let recvfrom fd buf ofs len flags =
+  Thread.wait_read fd; Unix.recvfrom fd buf ofs len flags
+let send fd buf ofs len flags =
+  Thread.wait_write fd; Unix.send fd buf ofs len flags
+let sendto fd buf ofs len flags addr =
+  Thread.wait_write fd; Unix.sendto fd buf ofs len flags addr
+
+let open_connection sockaddr =
+  let domain =
+    match sockaddr with ADDR_UNIX _ -> PF_UNIX | ADDR_INET(_,_) -> PF_INET in
+  let sock =
+    socket domain SOCK_STREAM 0 in
+  connect sock sockaddr;
+  (in_channel_of_descr sock, out_channel_of_descr sock)
+
+let establish_server server_fun sockaddr =
+  let domain =
+    match sockaddr with ADDR_UNIX _ -> PF_UNIX | ADDR_INET(_,_) -> PF_INET in
+  let sock =
+    socket domain SOCK_STREAM 0 in
+  bind sock sockaddr;
+  listen sock 3;
+  while true do
+    let (s, caller) = accept sock in
+    (* The "double fork" trick, the process which calls server_fun will not
+       leave a zombie process *)
+    match fork() with
+       0 -> if fork() <> 0 then exit 0; (* The son exits, the grandson works *)
+            let inchan = in_channel_of_descr s in
+            let outchan = out_channel_of_descr s in
+            server_fun inchan outchan;
+            close_in inchan;
+            close_out outchan;
+            exit 0
+    | id -> close s; waitpid [] id (* Reclaim the son *); ()
+  done
+
---
+++ otherlibs/threads/threadUnix.mli
@@ -0,0 +1,72 @@
+(***********************************************************************)
+(*                                                                     *)
+(*                         Caml Special Light                          *)
+(*                                                                     *)
+(*            Xavier Leroy, projet Cristal, INRIA Rocquencourt         *)
+(*                                                                     *)
+(*  Copyright 1995 Institut National de Recherche en Informatique et   *)
+(*  Automatique.  Distributed only by permission.                      *)
+(*                                                                     *)
+(***********************************************************************)
+
+(* $Id$ *)
+
+(* Module [ThreadUnix]: thread-compatible system calls *)
+
+(* This module reimplements some of the functions from [Unix]
+   so that they only block the calling thread, not all threads
+   in the program, if they cannot complete immediately.
+   See the documentation of the [Unix] module for more
+   precise descriptions of the functions below. *)
+
+(*** Process handling *)
+
+val execv : string -> string array -> unit
+val execve : string -> string array -> string array -> unit
+val execvp : string -> string array -> unit
+val wait : unit -> int * Unix.process_status
+val waitpid : Unix.wait_flag list -> int -> int * Unix.process_status
+val system : string -> Unix.process_status
+
+(*** Basic input/output *)
+
+val read : Unix.file_descr -> string -> int -> int -> int
+val write : Unix.file_descr -> string -> int -> int -> int
+
+(*** Input/output with timeout *)
+
+val timed_read : Unix.file_descr -> string -> int -> int -> float -> int
+val timed_write : Unix.file_descr -> string -> int -> int -> float -> int
+      (* Behave as [read] and [write], except that 
+         [Unix_error(ETIMEDOUT,_,_)] is raised if no data is
+         available for reading or ready for writing after [d] seconds.
+         The delay [d] is given in the fifth argument, in seconds. *)
+
+(*** Pipes and redirections *)
+
+val pipe : unit -> Unix.file_descr * Unix.file_descr
+val open_process_out: string -> out_channel
+val open_process: string -> in_channel * out_channel
+
+(*** Time *)
+
+val sleep : int -> unit
+
+(*** Sockets *)
+
+val socket : Unix.socket_domain -> Unix.socket_type -> int -> Unix.file_descr
+val socketpair : Unix.socket_domain -> Unix.socket_type -> int ->
+                 Unix.file_descr * Unix.file_descr
+val accept : Unix.file_descr -> Unix.file_descr * Unix.sockaddr
+val connect : Unix.file_descr -> Unix.sockaddr -> unit
+val recv : Unix.file_descr -> string -> int -> int -> Unix.msg_flag list -> int
+val recvfrom : Unix.file_descr -> string -> int -> int ->
+               Unix.msg_flag list -> int * Unix.sockaddr
+val send : Unix.file_descr -> string -> int -> int ->
+           Unix.msg_flag list -> int
+val sendto : Unix.file_descr -> string -> int -> int ->
+             Unix.msg_flag list -> Unix.sockaddr -> int
+val open_connection : Unix.sockaddr -> in_channel * out_channel
+val establish_server :
+      (in_channel -> out_channel -> 'a) -> Unix.sockaddr -> unit
+
