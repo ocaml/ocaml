8ee3109c42f1f297c86bcc6ad9b5c85f65c95c15
Damien Doligez <damien.doligez-inria.fr>
1996-11-02 18:04:50 +0000

    Portage Mac/MPW
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@1140 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

---
+++ config/sm-Mac.h
@@ -0,0 +1,33 @@
+/***********************************************************************/
+/*                                                                     */
+/*                           Objective Caml                            */
+/*                                                                     */
+/*             Damien Doligez, projet Para, INRIA Rocquencourt         */
+/*                                                                     */
+/*  Copyright 1996 Institut National de Recherche en Informatique et   */
+/*  Automatique.  Distributed only by permission.                      */
+/*                                                                     */
+/***********************************************************************/
+
+/* $Id$ */
+
+/* Processor dependencies */
+
+#define ARCH_BIG_ENDIAN
+
+/* Operating system and standard library dependencies. */
+
+#define OCAML_OS_TYPE "MacOS"
+#define HAS_MEMMOVE
+#define HAS_BCOPY
+#define HAS_STRERROR
+#define HAS_GETCWD
+#define HAS_UI
+
+#if !powerc
+#define THREADED_CODE
+#endif
+
+#if sizeof(int) != 4
+   *** Error : an int must be 32 bits; change your compiler options ***
+#endif
