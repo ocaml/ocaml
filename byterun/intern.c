3812c712d30304e7f02540a8b429c74f685dfe0d
Xavier Leroy <xavier.leroy@inria.fr>
1997-02-16 13:35:09 +0000

    Faire une erreur si on relit un tableau de flottants natifs dans ocamlrun
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@1269 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- byterun/intern.c
+++ byterun/intern.c
@@ -184,6 +184,7 @@ static void intern_rec(dest)
       case CODE_DOUBLE_ARRAY8_BIG:
         len = read8u();
       read_double_array:
+#ifdef NATIVE_CODE
         if (sizeof(double) != 8) {
           intern_cleanup();
           invalid_argument("input_value: non-standard floats");
@@ -199,6 +200,10 @@ static void intern_rec(dest)
           mlsize_t i;
           for (i = 0; i < len; i++) Reverse_double((value)((double *)v + i));
         }
+#else
+        intern_cleanup();
+        failwith("input_value: cannot read float array");
+#endif
         break;
       case CODE_DOUBLE_ARRAY32_LITTLE:
       case CODE_DOUBLE_ARRAY32_BIG:
