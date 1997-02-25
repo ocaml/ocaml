c53ca1c57997dba5b510647cff18e2798c537b67
Xavier Leroy <xavier.leroy@inria.fr>
1997-02-25 14:39:02 +0000

    Notices de copyright
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@1295 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- byterun/weak.h
+++ byterun/weak.h
@@ -2,9 +2,9 @@
 /*                                                                     */
 /*                           Objective Caml                            */
 /*                                                                     */
-/*          Damien Doligez, projet Cristal, INRIA Rocquencourt         */
+/*            Damien Doligez, projet Para, INRIA Rocquencourt          */
 /*                                                                     */
-/*  Copyright 1996 Institut National de Recherche en Informatique et   */
+/*  Copyright 1997 Institut National de Recherche en Informatique et   */
 /*  Automatique.  Distributed only by permission.                      */
 /*                                                                     */
 /***********************************************************************/
