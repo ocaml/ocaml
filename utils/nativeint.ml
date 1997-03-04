b289a5cdade8928096122aa7fbd8d6ca56d5b8c3
Xavier Leroy <xavier.leroy@inria.fr>
1997-03-04 15:08:43 +0000

    Arithmetique sur entiers 32/64 bits.
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@1317 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

---
+++ utils/nativeint.ml
@@ -0,0 +1,54 @@
+(***********************************************************************)
+(*                                                                     *)
+(*                           Objective Caml                            *)
+(*                                                                     *)
+(*            Xavier Leroy, projet Cristal, INRIA Rocquencourt         *)
+(*                                                                     *)
+(*  Copyright 1996 Institut National de Recherche en Informatique et   *)
+(*  Automatique.  Distributed only by permission.                      *)
+(*                                                                     *)
+(***********************************************************************)
+
+(* $Id$ *)
+
+(* lsb is the least significant bit of the native int (0 <= lsb <= 1)
+   msw is the remaining 31 or 63 most significant bits. *)
+type t = { msw: int; lsb: int }
+
+let from i = { msw = i asr 1; lsb = i land 1 }
+
+let to_int n = (n.msw lsl 1) lor n.lsb
+
+let add n1 n2 =
+  let s = n1.lsb + n2.lsb in
+  { msw = n1.msw + n2.msw + s asr 1; lsb = s land 1 }
+
+let sub n1 n2 =
+  let d = n1.lsb - n2.lsb in
+  { msw = n1.msw - n2.msw + d asr 1; lsb = d land 1 }
+
+let shift n s =
+  if s = 0 then n else
+  if s > 0 then
+    { msw = (n.msw lsl s) lor (n.lsb lsl (s-1)); lsb = 0 }
+  else
+    { msw = n.msw asr (-s); lsb = (n.msw asr (1-s)) land 1 }
+
+let sign n =
+  if n.msw < 0 then -1 else
+  if n.msw = 0 && n.lsb = 0 then 0 else 1
+
+let compare n1 n2 = sign(sub n1 n2)
+
+let cmp n1 i2 = compare n1 (from i2)
+
+let to_string n =
+  let a = if n.msw >= 0 then n else sub (from 0) n in
+  let (q, r) =
+    (* Watch out for the case n.msw = min_int, in which case a.msw < 0 *)
+    if a.msw <> min_int
+    then (a.msw / 5, a.msw mod 5)
+    else (max_int / 5, max_int mod 5 + 1) in
+  (if n.msw >= 0 then "" else "-") ^
+  (if q > 0 then string_of_int q else "") ^
+  string_of_int (r * 2 + a.lsb)
---
+++ utils/nativeint.mli
@@ -0,0 +1,40 @@
+(***********************************************************************)
+(*                                                                     *)
+(*                           Objective Caml                            *)
+(*                                                                     *)
+(*            Xavier Leroy, projet Cristal, INRIA Rocquencourt         *)
+(*                                                                     *)
+(*  Copyright 1996 Institut National de Recherche en Informatique et   *)
+(*  Automatique.  Distributed only by permission.                      *)
+(*                                                                     *)
+(***********************************************************************)
+
+(* $Id$ *)
+
+(* Operations on native integers (32 or 64 bits). *)
+
+type t
+
+val from: int -> t
+        (* Turn an integer into a native integer *)
+val to_int : t -> int
+        (* Return the integer value of a native integer, dropping the
+           most significant bit *)
+val add: t -> t -> t
+val sub: t -> t -> t
+        (* Addition, subtraction. *)
+val shift: t -> int -> t
+        (* [shift n s] shifts [n] left by [s] bits if [s] > 0
+           and right by [-s] bits if [s] < 0. *)
+val sign: t -> int
+        (* [sign n1] returns [0] if [n1] is zero, a positive
+           integer if [n] is positive, and a negative integer if
+           [n] is negative. *)
+val compare: t -> t -> int
+        (* [compare n1 n2] returns [0] if [n1 = n2], a positive
+           integer if [n1] > [n2], and a negative integer if
+           [n1] < [n2]. *)
+val cmp: t -> int -> int
+        (* [cmp n1 i2] is [compare n1 (from i2)]. *)
+val to_string: t -> string
+        (* Return the signed decimal representation of a native integer. *)
