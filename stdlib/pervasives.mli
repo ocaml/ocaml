4e9a819a2237dcbdb1c05b6716177aaee8e86a8d
Jérôme Vouillon <Jerome.Vouillon@pps.jussieu.fr>
1997-05-11 22:44:09 +0000

    Les objets peuvent etre compares avec les fonctions generiques de comparaisons, et etre appliques a la primitive de hachage.
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@1526 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- stdlib/pervasives.mli
+++ stdlib/pervasives.mli
@@ -96,8 +96,7 @@ external (=) : 'a -> 'a -> bool = "%equal"
            if and only if their current contents are structurally equal,
            even if the two mutable objects are not the same physical object.
            Equality between functional values raises [Invalid_argument].
-           Equality between cyclic data structures may not terminate.
-           Equality should not be used between object values. *)
+           Equality between cyclic data structures may not terminate. *)
 external (<>) : 'a -> 'a -> bool = "%notequal"
         (* Negation of [(=)]. *)
 external (<) : 'a -> 'a -> bool = "%lessthan"
