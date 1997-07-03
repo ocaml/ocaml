30374ece5cba6ebc3a56ace439912b96eea28e63
Xavier Leroy <xavier.leroy@inria.fr>
1997-07-03 13:49:11 +0000

    Pour utiliser les fonctions MD5 depuis C
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@1634 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

---
+++ byterun/md5.h
@@ -0,0 +1,26 @@
+/* MD5 message digest */
+
+#ifndef _md5
+#define _md5
+
+
+#include "mlvalues.h"
+#include "io.h"
+
+value md5_string P((value str, value ofs, value len));
+value md5_chan P((struct channel * chan, value len));
+
+struct MD5Context {
+        uint32 buf[4];
+        uint32 bits[2];
+        unsigned char in[64];
+};
+
+void MD5Init P((struct MD5Context *context));
+void MD5Update P((struct MD5Context *context, unsigned char *buf,
+               unsigned len));
+void MD5Final P((unsigned char digest[16], struct MD5Context *ctx));
+void MD5Transform P((uint32 buf[4], uint32 in[16]));
+
+
+#endif
