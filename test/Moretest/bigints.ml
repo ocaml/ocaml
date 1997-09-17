ff20f13c36a66fb35aace04167fc10b10dd9b2c0
Xavier Leroy <xavier.leroy@inria.fr>
1997-09-17 11:57:19 +0000

    Tester les grandes constantes entieres pour l'Alpha
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@1711 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

---
+++ test/Moretest/bigints.ml
@@ -0,0 +1,12 @@
+let _ =
+  print_int 1000000000; print_newline();
+  print_int 10000000000; print_newline();
+  print_int 100000000000; print_newline();
+  print_int 1000000000000; print_newline();
+  print_int 10000000000000; print_newline();
+  print_int 100000000000000; print_newline();
+  print_int 1000000000000000; print_newline();
+  print_int 10000000000000000; print_newline();
+  print_int 100000000000000000; print_newline();
+  print_int 1000000000000000000; print_newline()
+
