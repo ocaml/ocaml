3d39038914aec646a8802bf3955bd6a61a25325f
Damien Doligez <damien.doligez-inria.fr>
1997-10-14 15:50:22 +0000

    cosmetique
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@1720 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- stdlib/gc.mli
+++ stdlib/gc.mli
@@ -86,7 +86,7 @@ type control = {
              of free memory is more than [max_overhead] percent of the amount
              of live data.  If [max_overhead] is set to 0, heap
              compaction is triggered at the end of each major GC cycle
-             (this last setting is intended for testing purposes only).
+             (this setting is intended for testing purposes only).
 	     If [max_overhead >= 1000000], compaction is never triggered.
              Default: 1000000.
 -     [verbose]  This flag controls the GC messages on standard error output.
