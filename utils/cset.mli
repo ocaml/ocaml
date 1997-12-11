c03ef6d46cb85b29d7c88089f8be578b1ce55162
No author <no_author@ocaml.org>
1997-12-11 20:53:08 +0000

    This commit was manufactured by cvs2svn to create tag 'ocaml107'.
    git-svn-id: http://caml.inria.fr/svn/ocaml/release/1.07@1859 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- utils/cset.mli
+++
@@ -1,9 +0,0 @@
-(* Sets over types ordered with the default ordering *)
-
-type 'a t
-
-val empty: 'a t
-val mem: 'a -> 'a t -> bool
-val add: 'a -> 'a t -> 'a t
-val remove: 'a -> 'a t -> 'a t
-val elements: 'a t -> 'a list
