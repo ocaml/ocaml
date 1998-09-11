ecf542ed93b43ab718481bc0c33d92efae9d4198
Xavier Leroy <xavier.leroy@inria.fr>
1998-09-11 17:38:44 +0000

    Ajout de invoke_traced_function
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@2096 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- bytecomp/meta.mli
+++ bytecomp/meta.mli
@@ -21,3 +21,5 @@ external static_resize : string -> int -> string = "static_resize"
 type closure = unit -> Obj.t
 external reify_bytecode : string -> int -> closure = "reify_bytecode"
 external available_primitives : unit -> string array = "available_primitives"
+external invoke_traced_function : Obj.t -> Obj.t -> Obj.t -> Obj.t
+                                = "invoke_traced_function"
