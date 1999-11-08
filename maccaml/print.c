94b3b43078a9ac4444f922c7ae6fdc4e239b13be
Damien Doligez <damien.doligez-inria.fr>
1999-11-08 15:10:24 +0000

    *** empty log message ***
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@2472 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

---
+++ maccaml/print.c
@@ -0,0 +1,24 @@
+/***********************************************************************/
+/*                                                                     */
+/*                           Objective Caml                            */
+/*                                                                     */
+/*             Damien Doligez, projet Para, INRIA Rocquencourt         */
+/*                                                                     */
+/*  Copyright 1999 Institut National de Recherche en Informatique et   */
+/*  en Automatique.  Distributed only by permission                    */
+/*                                                                     */
+/***********************************************************************/
+
+/* $Id$ */
+
+#include "main.h"
+
+void FilePageSetup (void)
+{
+  XXX ();
+}
+
+void FilePrint (void)
+{
+  XXX ();
+}
