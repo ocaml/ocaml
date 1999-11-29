9f82177a69368b07c46e08394c0e1e5e806250de
Damien Doligez <damien.doligez-inria.fr>
1999-11-29 19:04:45 +0000

    ajout options -noassert -w -dparsetree
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@2635 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- toplevel/topmain.ml
+++ toplevel/topmain.ml
@@ -23,8 +23,23 @@ let main () =
   Arg.parse [
      "-I", Arg.String(fun dir -> include_dirs := dir :: !include_dirs),
            "<dir>  Add <dir> to the list of include directories";
+     "-noassert", Arg.Set noassert, " Do not compile assertion checks";
      "-rectypes", Arg.Set recursive_types, " Allow arbitrary recursive types";
      "-unsafe", Arg.Set fast, " No bound checking on array and string access";
+     "-w", Arg.String Warnings.parse_options,
+           "<flags>  Enable or disable warnings according to <flags>:\n\
+       \032    A/a enable/disable all warnings\n\
+       \032    C/c enable/disable suspicious comment\n\
+       \032    F/f enable/disable partially applied function\n\
+       \032    M/m enable/disable overriden method\n\
+       \032    P/p enable/disable partial match\n\
+       \032    S/s enable/disable non-unit statement\n\
+       \032    U/u enable/disable unused match case\n\
+       \032    V/v enable/disable hidden instance variable\n\
+       \032    X/x enable/disable all other warnings\n\
+       \032    default setting is A (all warnings enabled)";
+
+     "-dparsetree", Arg.Set dump_parsetree, " (undocumented)";
      "-drawlambda", Arg.Set dump_rawlambda, " (undocumented)";
      "-dlambda", Arg.Set dump_lambda, " (undocumented)";
      "-dinstr", Arg.Set dump_instr, " (undocumented)"
