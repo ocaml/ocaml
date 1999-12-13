8a1bcb4550edf478209af630306e3e67ba639136
Fabrice Le Fessant <Fabrice.Le_fessant@inria.fr>
1999-12-13 12:45:56 +0000

    Ajout Group-iconification
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@2689 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- otherlibs/labltk/browser/jg_toplevel.ml
+++ otherlibs/labltk/browser/jg_toplevel.ml
@@ -7,4 +7,5 @@ let titled ?:iconname title  =
   let tl = Toplevel.create Widget.default_toplevel in
   Wm.title_set tl :title;
   Wm.iconname_set tl name:iconname;
+  Wm.group_set tl leader: Widget.default_toplevel;
   tl
