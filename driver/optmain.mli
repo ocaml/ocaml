96ddeabcb78cbce02561d9ae66891d7e56a8fc68
Damien Doligez <damien.doligez-inria.fr>
2000-01-07 16:03:04 +0000

    optimisation des racines
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@2740 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

---
+++ driver/optmain.mli
@@ -0,0 +1,17 @@
+(***********************************************************************)
+(*                                                                     *)
+(*                           Objective Caml                            *)
+(*                                                                     *)
+(*          Damien Doligez, projet Moscova, INRIA Rocquencourt         *)
+(*                                                                     *)
+(*  Copyright 2000 Institut National de Recherche en Informatique et   *)
+(*  en Automatique.  All rights reserved.  This file is distributed    *)
+(*  under the terms of the Q Public License version 1.0.               *)
+(*                                                                     *)
+(***********************************************************************)
+
+(* $Id$ *)
+
+(*
+  this "empty" file is here to speed up garbage collection in ocamlopt.opt
+*)
