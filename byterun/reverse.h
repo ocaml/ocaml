7175ab048dcaaa39649ebc386ae37750baaf27e1
Xavier Leroy <xavier.leroy@inria.fr>
2000-02-10 14:03:09 +0000

    Generaliser les operations Reverse
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@2803 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- byterun/reverse.h
+++ byterun/reverse.h
@@ -12,39 +12,52 @@
 
 /* $Id$ */
 
-/* Swap byte-order in 32-bit integers and in 64-bit floats */
+/* Swap byte-order in 16, 32, and 64-bit integers or floats */
 
 #ifndef _reverse_
 #define _reverse_
 
-#define Reverse_int32(w) {                                                    \
-  char * _p;                                                                  \
-  int _a;                                                                     \
-  _p = (char *) (w);                                                          \
-  _a = _p[0];                                                                 \
-  _p[0] = _p[3];                                                              \
-  _p[3] = _a;                                                                 \
-  _a = _p[1];                                                                 \
-  _p[1] = _p[2];                                                              \
-  _p[2] = _a;                                                                 \
+#define Reverse_16(dst,src) {                                               \
+  char * _p, * _q;                                                          \
+  char _a;                                                                  \
+  _p = (char *) (src);                                                      \
+  _q = (char *) (dst);                                                      \
+  _a = _p[0];                                                               \
+  _q[0] = _p[1];                                                            \
+  _q[1] = _a;                                                               \
 }
 
-#define Reverse_double(d) {                                                   \
-  char * _p;                                                                  \
-  int _a;                                                                     \
-  _p = (char *) (d);                                                          \
-  _a = _p[0];                                                                 \
-  _p[0] = _p[7];                                                              \
-  _p[7] = _a;                                                                 \
-  _a = _p[1];                                                                 \
-  _p[1] = _p[6];                                                              \
-  _p[6] = _a;                                                                 \
-  _a = _p[2];                                                                 \
-  _p[2] = _p[5];                                                              \
-  _p[5] = _a;                                                                 \
-  _a = _p[3];                                                                 \
-  _p[3] = _p[4];                                                              \
-  _p[4] = _a;                                                                 \
+#define Reverse_32(dst,src) {                                               \
+  char * _p, * _q;                                                          \
+  char _a, _b;                                                              \
+  _p = (char *) (src);                                                      \
+  _q = (char *) (dst);                                                      \
+  _a = _p[0];                                                               \
+  _b = _p[1];                                                               \
+  _q[0] = _p[3];                                                            \
+  _q[1] = _p[2];                                                            \
+  _q[3] = _a;                                                               \
+  _q[2] = _b;                                                               \
 }
+
+#define Reverse_64(dst,src) {                                               \
+  char * _p, * _q;                                                          \
+  char _a, _b;                                                              \
+  _p = (char *) (src);                                                      \
+  _q = (char *) (dst);                                                      \
+  _a = _p[0];                                                               \
+  _b = _p[1];                                                               \
+  _q[0] = _p[7];                                                            \
+  _q[1] = _p[6];                                                            \
+  _q[7] = _a;                                                               \
+  _q[6] = _b;                                                               \
+  _a = _p[2];                                                               \
+  _b = _p[3];                                                               \
+  _q[2] = _p[5];                                                            \
+  _q[3] = _p[4];                                                            \
+  _q[5] = _a;                                                               \
+  _q[4] = _b;                                                               \
+}
+
 
 #endif /* _reverse_ */
