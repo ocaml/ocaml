6adae769af9f331dda3508430c2a8b2fb949a2f7
Xavier Leroy <xavier.leroy@inria.fr>
2000-03-10 15:07:20 +0000

    Exiger gas (GNU as) comme assembleur
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@2939 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- asmcomp/hppa/proc.ml
+++ asmcomp/hppa/proc.ml
@@ -217,5 +217,5 @@ let contains_calls = ref false
 (* Calling the assembler *)
 
 let assemble_file infile outfile =
-  Ccomp.command ("as -o " ^ outfile ^ " " ^ infile)  
+  Ccomp.command ("gas -o " ^ outfile ^ " " ^ infile)  
 
