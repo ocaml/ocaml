790770e1cace566ab377e4371ce8d813dd37879c
Xavier Leroy <xavier.leroy@inria.fr>
2000-04-17 08:52:45 +0000

    Erreurs de syntaxe.  Ce fichier sert-il a quelque chose?
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@3097 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- otherlibs/labltk/support/slave.ml
+++ otherlibs/labltk/support/slave.ml
@@ -28,13 +28,14 @@ let dynload args =
 
 (* Default modules include everything from 
 let default_modules = []
+*)
 
 (* [caml::run foo.cmo .. bar.cmo] is now available from Tcl *)
 let init () =
   Dynlink.init();
   (* Make it unsafe by default, with everything available *)
   Dynlink.allow_unsafe_modules true;
-  Dynlink.add_interfaces [] []
+  Dynlink.add_interfaces [] [];
   let s = register_callback Widget.dummy dynload in
   tcl_command (sprintf "proc caml::run {l} {camlcb %s l}" s)
 
