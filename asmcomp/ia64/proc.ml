b00caa17907ff31b65875509da76d1b9192064c7
Xavier Leroy <xavier.leroy@inria.fr>
2000-07-07 14:09:06 +0000

    Suite portage IA64
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@3228 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- asmcomp/ia64/proc.ml
+++ asmcomp/ia64/proc.ml
@@ -210,4 +210,4 @@ let contains_calls = ref false
 (* Calling the assembler *)
 
 let assemble_file infile outfile =
-  Ccomp.command ("as -xauto -o " ^ outfile ^ " " ^ infile)
+  Ccomp.command ("as -xexplicit -o " ^ outfile ^ " " ^ infile)
