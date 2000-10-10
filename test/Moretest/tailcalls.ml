430070c090c2292083007173e25e04e412059da6
Xavier Leroy <xavier.leroy@inria.fr>
2000-10-10 12:39:37 +0000

    Tests de tail recursion avec bcp d'arguments
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@3308 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

---
+++ test/Moretest/tailcalls.ml
@@ -0,0 +1,28 @@
+let rec tailcall4 a b c d =
+  if a < 0
+  then b
+  else tailcall4 (a-1) (b+1) (c+2) (d+3)
+
+let rec tailcall8 a b c d e f g h =
+  if a < 0
+  then b
+  else tailcall8 (a-1) (b+1) (c+2) (d+3) (e+4) (f+5) (g+6) (h+7)
+
+let rec tailcall16 a b c d e f g h i j k l m n o p =
+  if a < 0
+  then b
+  else tailcall16 (a-1) (b+1) (c+2) (d+3) (e+4) (f+5) (g+6) (h+7)
+                  (i+8) (j+9) (k+10) (l+11) (m+12) (n+13) (o+14) (p+15)
+
+let indtailcall8 fn a b c d e f g h =
+  fn a b c d e f g h
+
+let indtailcall16 fn a b c d e f g h i j k l m n o p =
+  fn a b c d e f g h i j k l m n o p 
+
+let _ =
+  print_int (tailcall4 10000000 0 0 0); print_newline();
+  print_int (tailcall8 10000000 0 0 0 0 0 0 0); print_newline();
+  print_int (tailcall16 10000000 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0); print_newline();
+  print_int (indtailcall8 tailcall8 10 0 0 0 0 0 0 0); print_newline();
+  print_int (indtailcall16 tailcall16 10 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0); print_newline()
