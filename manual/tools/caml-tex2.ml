8c4ec75d149b49390f1cb972c2adf092842adb82
Jacques Garrigue <garrigue at math.nagoya-u.ac.jp>
2001-09-26 08:21:11 +0000

    strict labels
    git-svn-id: http://caml.inria.fr/svn/ocamldoc/trunk@9919 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- manual/tools/caml-tex2.ml
+++ manual/tools/caml-tex2.ml
@@ -1,5 +1,6 @@
 (* $Id$ *)
 
+open StdLabels
 open Printf
 open Str
 
@@ -10,7 +11,7 @@ let camlout = "\\\\:\\1"
 let camlbunderline = "\\<"
 let camleunderline = "\\>"
 
-let camllight = ref "TERM=dumb ocaml"
+let camllight = ref "TERM=character ocaml"
 let linelen = ref 72
 let outfile = ref ""
 let cut_at_blanks = ref false
@@ -43,9 +44,9 @@ let () =
 
 let read_output () =
   let input = ref (input_line caml_input) in
-  input := replace_first ~pat:~!"^# *" ~templ:"" !input;
+  input := replace_first ~!"^# *" "" !input;
   let underline =
-    if string_match ~pat:~!"Characters *\\([0-9]+\\)-\\([0-9]+\\):$" !input ~pos:0
+    if string_match ~!"Characters *\\([0-9]+\\)-\\([0-9]+\\):$" !input 0
     then
       let b = int_of_string (matched_group 1 !input)
       and e = int_of_string (matched_group 2 !input) in
@@ -54,7 +55,7 @@ let read_output () =
     else 0, 0
   in
   let output = Buffer.create 256 in
-  while not (string_match ~pat:~!".*\"end_of_input\"$" !input ~pos:0) do
+  while not (string_match ~!".*\"end_of_input\"$" !input 0) do
     prerr_endline !input;
     Buffer.add_string output !input;
     Buffer.add_char output '\n';
@@ -62,7 +63,7 @@ let read_output () =
   done;
   Buffer.contents output, underline
 
-let escape_backslash = global_replace ~pat:~!"\\\\" ~templ:"\\\\\\\\"
+let escape_backslash = global_replace ~!"\\\\" "\\\\\\\\"
 
 let process_file file =
   prerr_endline ("Processing " ^ file);
@@ -71,15 +72,15 @@ let process_file file =
     try if !outfile = "-" then
       stdout
     else if !outfile = "" then
-      open_out (replace_first ~pat:~!"\\.tex$" ~templ:"" file ^ ".ml.tex")
+      open_out (replace_first ~!"\\.tex$" "" file ^ ".ml.tex")
     else
-      open_out_gen ~mode:[Open_wronly; Open_creat; Open_append; Open_text]
-        ~perm:0x666 !outfile
+      open_out_gen [Open_wronly; Open_creat; Open_append; Open_text]
+        0x666 !outfile
     with _ -> failwith "Cannot open output file" in
   try while true do
     let input = ref (input_line ic) in
-    if string_match ~pat:~!"\\\\begin{caml_example\\(\\*?\\)}[ \t]*$"
-        !input ~pos:0
+    if string_match ~!"\\\\begin{caml_example\\(\\*?\\)}[ \t]*$"
+        !input 0
     then begin
       let omit_answer = matched_group 1 !input = "*" in
       output_string oc camlbegin;
@@ -88,12 +89,12 @@ let process_file file =
         let phrase = Buffer.create 256 in
         while
           let input = input_line ic in
-          if string_match ~pat:~!"\\\\end{caml_example\\*?}[ \t]*$"
-              input ~pos:0
+          if string_match ~!"\\\\end{caml_example\\*?}[ \t]*$"
+              input 0
           then raise End_of_file;
           if Buffer.length phrase > 0 then Buffer.add_char phrase '\n';
           Buffer.add_string phrase input;
-          not (string_match ~pat:~!".*;;[ \t]*$" input ~pos:0)
+          not (string_match ~!".*;;[ \t]*$" input 0)
         do
           ()
         done;
@@ -119,8 +120,8 @@ let process_file file =
                escape_backslash rest]
           end else
             escape_backslash phrase in
-        let phrase = global_replace ~pat:~!"^\(.\)" ~templ:camlin phrase
-        and output = global_replace ~pat:~!"^\(.\)" ~templ:camlout output in
+        let phrase = global_replace ~!"^\(.\)" camlin phrase
+        and output = global_replace ~!"^\(.\)" camlout output in
         if not !first then output_string oc "\\;\n";
         fprintf oc "%s\n" phrase;
         if not omit_answer then fprintf oc "%s" output;
@@ -129,14 +130,13 @@ let process_file file =
       done
       with End_of_file -> output_string oc camlend
     end
-    else if string_match ~pat:~!"\\\\begin{caml_eval}[ \t]*$" !input ~pos:0
+    else if string_match ~!"\\\\begin{caml_eval}[ \t]*$" !input 0
     then begin
       while input := input_line ic; 
-        not (string_match ~pat:~!"\\\\end{caml_eval}[ \t]*$"
-               !input ~pos:0)
+        not (string_match ~!"\\\\end{caml_eval}[ \t]*$" !input 0)
       do
         fprintf caml_output "%s\n" !input;
-        if string_match ~pat:~!".*;;[ \t]*$" !input ~pos:0 then begin
+        if string_match ~!".*;;[ \t]*$" !input 0 then begin
           flush caml_output;
           output_string caml_output "\"end_of_input\";;\n";
           flush caml_output;
