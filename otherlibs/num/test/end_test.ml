34bfa7dda06a65e886208b41b0fecfa00b7ec43b
Pierre Weis <Pierre.Weis@inria.fr>
2001-12-04 14:24:43 +0000

    Added new tests to big_int.
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@4093 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- otherlibs/num/test/end_test.ml
+++ otherlibs/num/test/end_test.ml
@@ -1,8 +1,1 @@
-open Test
-
-let _ =
-  print_newline();
-  if !error_occurred then begin
-    prerr_endline "************* TEST FAILED ****************"; exit 2
-  end else
-    exit 0
+Test.end_tests ();;
