6b3edba61823c8d0874b4569f4b17c64570696aa
Xavier Leroy <xavier.leroy@inria.fr>
2001-12-04 14:42:42 +0000

    Bug dans l'elevation a la puissance (PR#451)
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@4094 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- otherlibs/num/big_int.ml
+++ otherlibs/num/big_int.ml
@@ -370,7 +370,7 @@ let power_base_nat base nat off len =
   if n = 0 then copy_nat power_base (pred rem) 1 else
    begin
     let res = make_nat n
-    and res2 = make_nat n
+    and res2 = make_nat (succ n)
     and l = num_bits_int n - 2 in
     let p = ref (1 lsl l) in
       blit_nat res 0 power_base pmax 1;
@@ -391,7 +391,7 @@ let power_base_nat base nat off len =
           p := !p lsr 1
       done;
     if rem > 0
-     then (mult_digit_nat res2 0 n 
+     then (mult_digit_nat res2 0 (succ n)
                           res 0 n power_base (pred rem);
            res2)
      else res
@@ -470,7 +470,7 @@ let power_big_int_positive_big_int bi1 bi2 =
          let p = ref (1 lsl l) in
            blit_nat res 0 (bi1.abs_value) 0 bi1_len;
            for i = l downto 0 do
-             let nat = bi2.abs_value in
+             let nat = copy_nat bi2.abs_value 0 len_bi2 in
              let len = num_digits_nat res 0 res_len in
              let len2 = min res_len (2 * len) in
              let succ_len2 = succ len2 in
