46f244a514b87e27cefc2b1dacd75397a3564e5f
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2001-12-14 21:19:35 +0000

    -
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@4170 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

---
+++ camlp4/camlp4/ast2pt.mli
@@ -0,0 +1,22 @@
+(* camlp4r *)
+(***********************************************************************)
+(*                                                                     *)
+(*                             Camlp4                                  *)
+(*                                                                     *)
+(*        Daniel de Rauglaudre, projet Cristal, INRIA Rocquencourt     *)
+(*                                                                     *)
+(*  Copyright 2001 Institut National de Recherche en Informatique et   *)
+(*  Automatique.  Distributed only by permission.                      *)
+(*                                                                     *)
+(***********************************************************************)
+
+(* $Id$ *)
+
+value fast : ref bool;
+value no_constructors_arity : ref bool;
+value mkloc : (int * int) -> Location.t;
+value long_id_of_string_list : (int * int) -> list string -> Longident.t;
+
+value str_item : MLast.str_item -> Parsetree.structure -> Parsetree.structure;
+value interf : list MLast.sig_item -> Parsetree.signature;
+value implem : list MLast.str_item -> Parsetree.structure;
