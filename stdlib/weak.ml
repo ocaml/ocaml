0032c482a2024b7b3ceb593a494b1a8b10794f2c
Damien Doligez <damien.doligez-inria.fr>
2002-01-23 17:54:11 +0000

    quelques bugs sur les weak hash tables
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@4306 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- stdlib/weak.ml
+++ stdlib/weak.ml
@@ -13,7 +13,8 @@
 
 (* $Id$ *)
 
-(* Weak array operations *)
+
+(** Weak array operations *)
 
 type 'a t;;
 
@@ -22,11 +23,8 @@ external create: int -> 'a t = "weak_create";;
 let length x = Obj.size(Obj.repr x) - 1;;
 
 external set : 'a t -> int -> 'a option -> unit = "weak_set";;
-
 external get: 'a t -> int -> 'a option = "weak_get";;
-
 external get_copy: 'a t -> int -> 'a option = "weak_get_copy";;
-
 external check: 'a t -> int -> bool = "weak_check";;
 
 let fill ar ofs len x =
@@ -54,3 +52,184 @@ let blit ar1 of1 ar2 of2 len =
     end
   end
 ;;
+
+
+(** Weak hash tables *)
+
+module type S = sig
+  type data
+  type t
+  val create : int -> t
+  val clear : t -> unit
+  val merge : t -> data -> data
+  val add : t -> data -> unit
+  val remove : t -> data -> unit
+  val find : t -> data -> data
+  val find_all : t -> data -> data list
+  val mem : t -> data -> bool
+  val iter : (data -> unit) -> t -> unit
+  val fold : (data -> 'a -> 'a) -> t -> 'a -> 'a
+  val count : t -> int
+  val stats : t -> int * int * int * int * int * int
+end;;
+
+module Make (H : Hashtbl.HashedType) : (S with type data = H.t) = struct
+
+  type 'a weak_t = 'a t;;
+  let weak_create = create;;
+  let emptybucket = weak_create 0;;
+
+  type data = H.t;;
+
+  type t = {
+    mutable table : data weak_t array;
+    mutable totsize : int;             (* sum of the bucket sizes *)
+    mutable limit : int;               (* max ratio totsize/table length *)
+  };;
+
+  let get_index t d = (H.hash d land max_int) mod (Array.length t.table);;
+
+  let create sz =
+    let sz = if sz < 7 then 7 else sz in
+    let sz = if sz > Sys.max_array_length then Sys.max_array_length else sz in
+    {
+      table = Array.create sz emptybucket;
+      totsize = 0;
+      limit = 3;
+    };;
+
+  let clear t =
+    for i = 0 to Array.length t.table - 1 do
+      t.table.(i) <- emptybucket;
+    done;
+    t.totsize <- 0;
+    t.limit <- 3;
+  ;;
+
+  let fold f t init =
+    let rec fold_bucket i b accu =
+      if i >= length b then accu else
+      match get b i with
+      | Some v -> fold_bucket (i+1) b (f v accu)
+      | None -> fold_bucket (i+1) b accu
+    in
+    Array.fold_right (fold_bucket 0) t.table init
+  ;;
+
+  let iter f t = fold (fun d () -> ()) t ();;
+
+  let count t =
+    let rec count_bucket i b accu =
+      if i >= length b then accu else
+      count_bucket (i+1) b (accu + (if check b i then 1 else 0))
+    in
+    Array.fold_right (count_bucket 0) t.table 0
+  ;;
+
+  let next_sz n = min (3*n/2 + 3) (Sys.max_array_length - 1);;
+
+  let rec resize t =
+    let oldlen = Array.length t.table in
+    let newlen = next_sz oldlen in
+    if newlen > oldlen then begin
+      let newt = create newlen in
+      newt.limit <- t.limit + 100;          (* prevent resizing of newt *)
+      fold (fun d () -> add newt d) t ();
+   (* assert Array.length newt.table = newlen; *)
+      t.table <- newt.table;
+      t.limit <- t.limit + 2;
+    end
+
+  and add_aux t d index =
+    let bucket = t.table.(index) in
+    let sz = length bucket in
+    let rec loop i =
+      if i >= sz then begin
+        let newsz = min (sz + 3) (Sys.max_array_length - 1) in
+        if newsz <= sz then failwith "Weak.Make : hash bucket cannot grow more";
+        let newbucket = weak_create newsz in
+        blit bucket 0 newbucket 0 sz;
+        set newbucket i (Some d);
+        t.table.(index) <- newbucket;
+        t.totsize <- t.totsize + (newsz - sz);
+        if t.totsize > t.limit * Array.length t.table then resize t;
+      end else begin
+        if check bucket i
+        then loop (i+1)
+        else set bucket i (Some d)
+      end
+    in
+    loop 0;
+
+  and add t d = add_aux t d (get_index t d)
+  ;;
+
+  let find_or t d ifnotfound =
+    let index = get_index t d in
+    let bucket = t.table.(index) in
+    let sz = length bucket in
+    let rec loop i =
+      if i >= sz then ifnotfound index
+      else begin
+        match get_copy bucket i with
+        | Some v when H.equal v d
+           -> begin match get bucket i with
+              | Some v -> v
+              | None -> loop (i+1)
+              end
+        | _ -> loop (i+1)
+      end
+    in
+    loop 0
+  ;;
+
+  let merge t d = find_or t d (fun index -> add_aux t d index; d);;
+
+  let find t d = find_or t d (fun index -> raise Not_found);;
+
+  let find_shadow t d iffound ifnotfound =
+    let index = get_index t d in
+    let bucket = t.table.(index) in
+    let sz = length bucket in
+    let rec loop i =
+      if i >= sz then ifnotfound else begin
+        match get_copy bucket i with
+        | Some v when H.equal v d -> iffound bucket i
+        | _ -> loop (i+1)
+      end
+    in
+    loop 0
+  ;;
+
+  let remove t d = find_shadow t d (fun w i -> set w i None) ();;
+
+  let mem t d = find_shadow t d (fun w i -> true) false;;
+
+  let find_all t d =
+    let index = get_index t d in
+    let bucket = t.table.(index) in
+    let sz = length bucket in
+    let rec loop i accu =
+      if i >= sz then accu
+      else begin
+        match get_copy bucket i with
+        | Some v when H.equal v d
+           -> begin match get bucket i with
+              | Some v -> loop (i+1) (v::accu)
+              | None -> loop (i+1) accu
+              end
+        | _ -> loop (i+1) accu
+      end
+    in
+    loop 0 []
+  ;;
+
+  let stats t =
+    let len = Array.length t.table in
+    let lens = Array.map length t.table in
+    Array.sort compare lens;
+    let totlen = Array.fold_left ( + ) 0 lens in
+    (len, count t, totlen, lens.(0), lens.(len/2), lens.(len-1))
+  ;;
+
+end;;
--- stdlib/weak.mli
+++ stdlib/weak.mli
@@ -13,7 +13,10 @@
 
 (* $Id$ *)
 
-(** Arrays of weak pointers. *)
+(** Arrays of weak pointers and hash tables of weak pointers. *)
+
+
+(** {6 Low-level functions} *)
 
 type 'a t
 (** The type of arrays of weak pointers (weak arrays).  A weak
@@ -75,3 +78,70 @@ val blit : 'a t -> int -> 'a t -> int -> int -> unit
    not designate a valid subarray of [ar1], or if [off2] and [len]
    do not designate a valid subarray of [ar2].*)
 
+
+(** {6 Weak hash tables} *)
+
+(** A weak hash table is a hashed set of values.  Each value may
+    magically disappear from the set when it is not used by the
+    rest of the program any more.  This is normally used to share
+    data structures without inducing memory leaks.
+    Weak hash tables are defined on values from a {!Hashtbl.HashedType}
+    module; the [equal] relation and [hash] function are taken from that
+    module.  We will say that [v] is an instance of [x] if [equal x v]
+    is [true].
+
+    The [equal] relation must be able to work on a shallow copy of
+    the values and give the same result as with the value itself.
+    *)
+
+module type S = sig
+  type data
+    (** The type of the elements stored in the table. *)
+  type t
+    (** The type of tables that contain elements of type [data]. *)
+  val create : int -> t
+    (** [create n] creates a new empty weak hash table, of initial
+        size [n].  The table will grow as needed. *)
+  val clear : t -> unit
+    (** Remove all elements from the table. *)
+  val merge : t -> data -> data
+    (** [merge t x] returns an instance of [x] found in [t] if any,
+        or else adds [x] to [t] and return [x]. *)
+  val add : t -> data -> unit
+    (** [add t x] adds [x] to [t].  If there is already an instance
+        of [x] in [t], it is unspecified which one will be
+        returned by subsequent calls to [find] and [merge]. *)
+  val remove : t -> data -> unit
+    (** [remove t x] removes from [t] one instance of [x].  Does
+        nothing if there is no instance of [x] in [t]. *)
+  val find : t -> data -> data
+    (** [find t x] returns an instance of [x] found in [t].
+        Raise [Not_found] if there is no such element. *)
+  val find_all : t -> data -> data list
+    (** [find_all t x] returns a list of all the instances of [x]
+        found in [t]. *)
+  val mem : t -> data -> bool
+    (** [mem t x] returns [true] if there is at least one instance
+        of [x] in [t], false otherwise. *)
+  val iter : (data -> unit) -> t -> unit
+    (** [iter f t] calls [f] on each element of [t], in some unspecified
+        order.  It is not specified what happens if [f] tries to change
+        [t] itself. *)
+  val fold : (data -> 'a -> 'a) -> t -> 'a -> 'a
+    (** [fold f t init] computes [(f d1 (... (f dN init)))] where
+        [d1 ... dN] are the elements of [t] in some unspecified order.
+        It is not specified what happens if [f] tries to change [t]
+        itself. *)
+  val count : t -> int
+    (** Count the number of elements in the table.  [count t] gives the
+        same result as [fold (fun _ n -> n+1) t 0] but does not delay the
+        deallocation of the dead elements. *)
+  val stats : t -> int * int * int * int * int * int
+    (** Return statistics on the table.  The numbers are, in order:
+        table length, number of entries, sum of bucket lengths,
+        smallest bucket length, median bucket length, biggest bucket length. *)
+end;;
+(** The output signature of the functor {!Weak.Make}. *)
+
+module Make (H : Hashtbl.HashedType) : S with type data = H.t;;
+(** Functor building an implementation of the weak hash table structure. *)
