cd40f800ea077020d428ec0e1f72795568f600e4
Xavier Leroy <xavier.leroy@inria.fr>
2002-03-06 16:52:12 +0000

    Eviter duplication du code de stat_aux (a tester)
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@4482 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- otherlibs/unix/stat.c
+++ otherlibs/unix/stat.c
@@ -44,7 +44,7 @@ static int file_kind_table[] = {
   S_IFREG, S_IFDIR, S_IFCHR, S_IFBLK, S_IFLNK, S_IFIFO, S_IFSOCK
 };
 
-static value stat_aux(struct stat *buf)
+static value stat_aux(int use_64, struct stat *buf)
 {
   value v;
   value atime = Val_unit, mtime = Val_unit, ctime = Val_unit;
@@ -63,7 +63,8 @@ static value stat_aux(struct stat *buf)
     Field (v, 5) = Val_int (buf->st_uid);
     Field (v, 6) = Val_int (buf->st_gid);
     Field (v, 7) = Val_int (buf->st_rdev);
-    Field (v, 8) = Val_int (buf->st_size);
+    Field (v, 8) = 
+      use_64 ? Val_file_offset(buf->st_size) : Val_int (buf->st_size);
     Field (v, 9) = atime;
     Field (v, 10) = mtime;
     Field (v, 11) = ctime;
@@ -78,7 +79,7 @@ CAMLprim value unix_stat(value path)
   ret = stat(String_val(path), &buf);
   if (ret == -1) uerror("stat", path);
   if (buf.st_size > Max_long) unix_error(EOVERFLOW, "stat", path);
-  return stat_aux(&buf);
+  return stat_aux(0, &buf);
 }
 
 CAMLprim value unix_lstat(value path)
@@ -92,7 +93,7 @@ CAMLprim value unix_lstat(value path)
 #endif
   if (ret == -1) uerror("lstat", path);
   if (buf.st_size > Max_long) unix_error(EOVERFLOW, "lstat", path);
-  return stat_aux(&buf);
+  return stat_aux(0, &buf);
 }
 
 CAMLprim value unix_fstat(value fd)
@@ -102,34 +103,7 @@ CAMLprim value unix_fstat(value fd)
   ret = fstat(Int_val(fd), &buf);
   if (ret == -1) uerror("fstat", Nothing);
   if (buf.st_size > Max_long) unix_error(EOVERFLOW, "fstat", Nothing);
-  return stat_aux(&buf);
-}
-
-static value stat_aux_64(struct stat *buf)
-{
-  value v;
-  value atime = Val_unit, mtime = Val_unit, ctime = Val_unit;
-
-  Begin_roots3(atime,mtime,ctime)
-    atime = copy_double((double) buf->st_atime);
-    mtime = copy_double((double) buf->st_mtime);
-    ctime = copy_double((double) buf->st_ctime);
-    v = alloc_small(12, 0);
-    Field (v, 0) = Val_int (buf->st_dev);
-    Field (v, 1) = Val_int (buf->st_ino);
-    Field (v, 2) = cst_to_constr(buf->st_mode & S_IFMT, file_kind_table,
-                                 sizeof(file_kind_table) / sizeof(int), 0);
-    Field (v, 3) = Val_int(buf->st_mode & 07777);
-    Field (v, 4) = Val_int (buf->st_nlink);
-    Field (v, 5) = Val_int (buf->st_uid);
-    Field (v, 6) = Val_int (buf->st_gid);
-    Field (v, 7) = Val_int (buf->st_rdev);
-    Field (v, 8) = Val_file_offset (buf->st_size);
-    Field (v, 9) = atime;
-    Field (v, 10) = mtime;
-    Field (v, 11) = ctime;
-  End_roots();
-  return v;
+  return stat_aux(0, &buf);
 }
 
 CAMLprim value unix_stat_64(value path)
@@ -138,7 +112,7 @@ CAMLprim value unix_stat_64(value path)
   struct stat buf;
   ret = stat(String_val(path), &buf);
   if (ret == -1) uerror("stat", path);
-  return stat_aux_64(&buf);
+  return stat_aux(1, &buf);
 }
 
 CAMLprim value unix_lstat_64(value path)
@@ -151,7 +125,7 @@ CAMLprim value unix_lstat_64(value path)
   ret = stat(String_val(path), &buf);
 #endif
   if (ret == -1) uerror("lstat", path);
-  return stat_aux_64(&buf);
+  return stat_aux(1, &buf);
 }
 
 CAMLprim value unix_fstat_64(value fd)
@@ -160,6 +134,6 @@ CAMLprim value unix_fstat_64(value fd)
   struct stat buf;
   ret = fstat(Int_val(fd), &buf);
   if (ret == -1) uerror("fstat", Nothing);
-  return stat_aux_64(&buf);
+  return stat_aux(1, &buf);
 }
 
