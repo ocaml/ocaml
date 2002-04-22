53914595b1779b002f65fc63713d03e98115dda1
Jun FURUSE / 古瀬 淳 <jun.furuse@gmail.com>
2002-04-22 15:21:57 +0000

    removing a warining
    git-svn-id: http://caml.inria.fr/svn/ocaml/branches/mltk@4718 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- otherlibs/labltk/support/cltkMisc.c
+++ otherlibs/labltk/support/cltkMisc.c
@@ -33,7 +33,7 @@ CAMLprim value camltk_splitlist (value v)
 
   CheckInit();
 
-  utf = caml_string_to_tcl(String_val(v)); 
+  utf = caml_string_to_tcl(v); 
   /* argv is allocated by Tcl, to be freed by us */
   result = Tcl_SplitList(cltclinterp,utf,&argc,&argv);
   switch(result) {
