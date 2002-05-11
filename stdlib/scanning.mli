bea7012c7f3320f50e72b18f07062855854b524e
Pierre Weis <Pierre.Weis@inria.fr>
2002-05-11 12:03:42 +0000

    Documentation.
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@4801 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- stdlib/scanning.mli
+++ stdlib/scanning.mli
@@ -15,7 +15,7 @@
 
 (** The run-time library for scanners. *)
 
-(** {7 Scanning buffers} *)
+(** {6 Scanning buffers} *)
 
 type scanbuf;;
 (** The type of scanning buffers. A scanning buffer is the argument passed
@@ -32,9 +32,10 @@ val peek_char : scanbuf -> char;;
 (** [Scanning.peek_char scanbuf] returns the current char available in
     the input. *)
 
-val end_of_input : scanbuf -> bool;;
-(** [Scanning.end_of_input scanbuf] tests the end of input condition
-    of the given buffer. *)
+val store_char : scanbuf -> char -> int -> int;;
+(** [Scanning.store_char scanbuf c lim] adds [c] to the token buffer
+    of the scanning buffer. It returns [lim - 1], indicating that there
+    is one less character to read. *)
 
 val char_count : scanbuf -> int;;
 (** [Scanning.char_count scanbuf] returns the number of characters read
@@ -45,14 +46,17 @@ val token : scanbuf -> string;;
     buffer of the scanning buffer: it returns the token matched by the
     format. *)
 
-val store_char : scanbuf -> char -> int -> int;;
-(** [Scanning.store_char scanbuf c lim] adds [c] to the token buffer
-    of the scanning buffer. It returns [lim - 1], indicating that there
-    is one less character to read. *)
-
 val reset_token : scanbuf -> unit;;
 (** [Scanning.reset_token scanbuf] rests to zero the token buffer of
     the given scanning buffer. *)
+
+val token_count : scanbuf -> int;;
+(** [Scanning.token_count scanbuf] returns the number of tokens read
+    so far from [scanbuf]. *)
+
+val end_of_input : scanbuf -> bool;;
+(** [Scanning.end_of_input scanbuf] tests the end of input condition
+    of the given buffer. *)
 
 val from_string : string -> scanbuf;;
 (** Create a scanning buffer which reads from the given string.
@@ -66,4 +70,6 @@ val from_channel : in_channel -> scanbuf;;
 
 val from_function : (unit -> char) -> scanbuf;;
 (** Create a scanning buffer with the given function as its reading method.
-    When the scanning needs one more character, it calls the given function. *)
+    When the scanning needs one more character, it calls the given function.
+    When the function has no more character to provide, it must set
+    an end of input condition by raising the exception [End_of_file]. *)
