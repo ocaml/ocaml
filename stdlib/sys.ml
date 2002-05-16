3340979102de6840704f814e741f79103501ec6e
Damien Doligez <damien.doligez-inria.fr>
2002-05-16 16:23:01 +0000

    version
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@4823 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- stdlib/sys.ml
+++ stdlib/sys.ml
@@ -78,4 +78,4 @@ let catch_break on =
 (* OCaml version string, moved from utils/config.mlp.
    Must be in the format described in sys.mli. *)
 
-let ocaml_version = "3.04+10 (2002-04-18)"
+let ocaml_version = "3.04+11 (2002-05-16)"
