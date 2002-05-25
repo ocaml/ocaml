f8e9e422141804815652da99874ed639fe5d774d
Xavier Leroy <xavier.leroy@inria.fr>
2002-05-25 08:33:26 +0000

    Revu detection int64.  Nettoyages divers.
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@4833 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- config/auto-aux/longlong.c
+++ config/auto-aux/longlong.c
@@ -18,20 +18,26 @@
 
 /* Check for the availability of "long long" type as per ISO C9X */
 
+/* Meaning of return code:
+     0   long long OK, printf with %ll
+     1   long long OK, printf with %q
+     2   long long OK, no printf
+     3   long long not suitable */
+
 int main(int argc, char **argv)
 {
   long long l;
   unsigned long long u;
   char buffer[64];
 
-  if (sizeof(long long) != 8) return 1;
+  if (sizeof(long long) != 8) return 3;
   l = 123456789123456789LL;
-  buffer[0] = 0;
+  buffer[0] = '\0';
   sprintf(buffer, "%lld", l);
   if (strcmp(buffer, "123456789123456789") == 0) return 0;
   /* the MacOS X library uses qd to format long longs */
   buffer[0] = '\0';
   sprintf (buffer, "%qd", l);
   if (strcmp (buffer, "123456789123456789") == 0) return 1;
-  return 100;
+  return 2;
 }
