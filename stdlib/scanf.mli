60ed746eef7dcef6ccb3c54f85dec4ddd1ba8620
Pierre Weis <Pierre.Weis@inria.fr>
2002-05-27 07:05:36 +0000

    Documentation.
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@4840 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- stdlib/scanf.mli
+++ stdlib/scanf.mli
@@ -21,10 +21,19 @@ val fscanf : in_channel -> ('a, Scanning.scanbuf, 'b) format -> 'a -> 'b;;
    applies these values to the function [f].
    The result of this application of [f] is the result of the whole construct.
 
-   The format is a character string which contains two types of
-   objects: plain characters, which are simply matched with the
-   input channel, and conversion specifications, each of which
-   causes conversion and reading of one argument for [f].
+   The format is a character string which contains three types of
+   objects:
+   - plain characters, which are simply matched with the
+   input channel,
+   - conversion specifications, each of which causes reading and
+   conversion of one argument for [f],
+   - scanning indications to specify boundaries of tokens and the
+   amount of space to skip between tokens.
+
+   Among plain characters the white space character (ASCII code 32) has a
+   special meaning: it maches a ``space'', that is any number of tab,
+   white space, newline and return. Hence, a space in the format
+   matches any number of white spaces in the input.
 
    Conversion specifications consist in the [%] character, followed
    by optional field width, followed by one or two conversion
@@ -37,7 +46,7 @@ val fscanf : in_channel -> ('a, Scanning.scanbuf, 'b) format -> 'a -> 'b;;
    - [x]: reads an unsigned hexadecimal integer with lowercase letters.
    - [X]: reads an unsigned hexadecimal integer with uppercase letters.
    - [o]: reads an unsigned octal integer.
-   - [s]: reads a string argument (string ends with a space).
+   - [s]: reads a string argument (by default strings end with a space).
    - [S]: reads a delimited string argument (delimitors and special
      escaped characters follow the lexical conventions of Objective Caml).
    - [c]: reads a single character.
@@ -58,21 +67,23 @@ val fscanf : in_channel -> ('a, Scanning.scanbuf, 'b) format -> 'a -> 'b;;
      the format specified by the second letter.
    - [\[ range \]]: reads characters that maches one of the characters
      mentionned in the range of characters [range] (or not mentionned in
-     it, if the range starts by [^]).
+     it, if the range starts by [^]). Returns a [string] that can be
+     empty, if no character in the input matches the range.
    - [N]: applies [f] to the number of characters read so far.
    - [%]: matches one [%] character in the input.
 
    The field widths are composed of an optional integer literal
-   indicating the maximal width of the token to read, possibly followed by
-   a dot [.] and another integer literal indicating how many digits
-   follow the decimal point in the [%f], [%e], and [%E] conversions.
+   indicating the maximal width of the token to read.
    For instance, [%6d] reads an integer, having at most 6 decimal digits;
    and [%4f] reads a float with 4 characters.
 
-   Each conversion starts by skiping space (except the [c]
-   conversion). Space is any number of tab, white space, newline and
-   return. Hence, a space in the format matches any number of white
-   spaces in the input.
+   The scanning indication are introduced by a [@] character, followed
+   by any character [c], that matches a plain [c] character in the
+   input. If a scanning indication immediately follows a [s]
+   conversion specification, it specifies the boundary of the token
+   (that is the character immediately after the end of the token). For
+   instance, ["%s@\t"] reads the string preceding the next tabulation
+   character.
 
    Note: the [scanf] facility is not intended for heavy duty
    lexing and parsing; if you need efficient language syntactic analysis,
