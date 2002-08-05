c5e13cd23f5e62732cd0a04952fab1024a7c76a3
Xavier Leroy <xavier.leroy@inria.fr>
2002-08-05 14:16:12 +0000

    MAJ numero de version
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@5078 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- stdlib/sys.ml
+++ stdlib/sys.ml
@@ -78,4 +78,4 @@ let catch_break on =
 (* OCaml version string, moved from utils/config.mlp.
    Must be in the format described in sys.mli. *)
 
-let ocaml_version = "3.05"
+let ocaml_version = "3.06"
