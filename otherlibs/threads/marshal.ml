c9276f64c55e7a1dc08feebc966755ca558926b8
Xavier Leroy <xavier.leroy@inria.fr>
2002-10-26 12:44:09 +0000

    Erreur dans from_channel (le header n'etait pas copie correctement)
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@5190 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- otherlibs/threads/marshal.ml
+++ otherlibs/threads/marshal.ml
@@ -58,6 +58,7 @@ let from_channel ic =
   let header = String.create header_size in
   really_input ic header 0 header_size;
   let buff_size = data_size header 0 in
-  let buff = String.create buff_size in
-  really_input ic buff 0 buff_size;
+  let buff = String.create (header_size + buff_size) in
+  String.blit header 0 buff 0 header_size;
+  really_input ic buff 20 buff_size;
   from_string_unsafe buff 0
