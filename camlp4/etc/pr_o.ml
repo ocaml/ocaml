a43e7cbb96a4fdc9a1658c06935bff08b69208f3
Damien Doligez <damien.doligez-inria.fr>
2003-04-22 14:11:20 +0000

    cosmetique
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@5503 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- camlp4/etc/pr_o.ml
+++ camlp4/etc/pr_o.ml
@@ -1496,14 +1496,6 @@ pr_patt.pr_levels :=
                            (S RO ",") al "" [: `S RO ")"; k :] :] :]
             | _ -> [: curr x "" [: :]; `next y "" k :] ]
       | p -> fun curr next dg k -> [: `next p "" k :] ]};
-(*
-   {pr_label = ""; pr_box _ x = HOVbox x;
-    pr_rules =
-      extfun Extfun.empty with
-      [ <:patt< $x$ . $y$ >> ->
-          fun curr next dg k -> [: curr x "" [: :]; `S NO "."; `next y "" k :]
-      | p -> fun curr next dg k -> [: `next p "" k :] ]};
-*)
    {pr_label = "simple";
     pr_box p x = LocInfo (MLast.loc_of_patt p) (HOVbox x);
     pr_rules =
