961dc4bda4ccf0f440e532eed4fbc16c27db5d23
Pierre Weis <Pierre.Weis@inria.fr>
2003-04-28 09:47:04 +0000

    Introducing new constants deserve a new sub-version increment!
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@5518 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- stdlib/sys.ml
+++ stdlib/sys.ml
@@ -78,4 +78,4 @@ let catch_break on =
 
 (* OCaml version string, must be in the format described in sys.mli. *)
 
-let ocaml_version = "3.06+27 (2003-03-31)";;
+let ocaml_version = "3.06+28 (2003-04-28)";;
