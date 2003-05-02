b05ec8f2fa3d982a2d54b70b7339e24ad7146dd1
Pierre Weis <Pierre.Weis@inria.fr>
2003-05-02 08:47:34 +0000

    Addition of the Fragile_pat warning.
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@5526 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- stdlib/sys.ml
+++ stdlib/sys.ml
@@ -78,4 +78,4 @@ let catch_break on =
 
 (* OCaml version string, must be in the format described in sys.mli. *)
 
-let ocaml_version = "3.06+30 (2003-05-02)";;
+let ocaml_version = "3.06+31 (2003-05-02)";;
