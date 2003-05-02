fcebde56f9b9206d39aaf0ff8184500e26e12cc6
Pierre Weis <Pierre.Weis@inria.fr>
2003-05-02 08:48:31 +0000

    Addition of the new Fragile_pat warning.
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@5527 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- toplevel/topmain.ml
+++ toplevel/topmain.ml
@@ -53,6 +53,7 @@ let main () =
        \032    A/a enable/disable all warnings\n\
        \032    C/c enable/disable suspicious comment\n\
        \032    D/d enable/disable deprecated features\n\
+       \032    E/e enable/disable fragile match\n\
        \032    F/f enable/disable partially applied function\n\
        \032    M/m enable/disable overriden method\n\
        \032    P/p enable/disable partial match\n\
