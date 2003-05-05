0264e45236b65ee84000d1a9cbc3dc370c2dd4d7
Xavier Leroy <xavier.leroy@inria.fr>
2003-05-05 14:20:58 +0000

    Erreur de GC dans stat_aux (PR#1665)
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@5540 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- otherlibs/unix/stat.c
+++ otherlibs/unix/stat.c
@@ -46,30 +46,28 @@ static int file_kind_table[] = {
 
 static value stat_aux(int use_64, struct stat *buf)
 {
-  value v;
-  value atime = Val_unit, mtime = Val_unit, ctime = Val_unit;
+  CAMLparam0();
+  CAMLlocal5(atime, mtime, ctime, offset, v);
 
-  Begin_roots3(atime,mtime,ctime)
-    atime = copy_double((double) buf->st_atime);
-    mtime = copy_double((double) buf->st_mtime);
-    ctime = copy_double((double) buf->st_ctime);
-    v = alloc_small(12, 0);
-    Field (v, 0) = Val_int (buf->st_dev);
-    Field (v, 1) = Val_int (buf->st_ino);
-    Field (v, 2) = cst_to_constr(buf->st_mode & S_IFMT, file_kind_table,
-                                 sizeof(file_kind_table) / sizeof(int), 0);
-    Field (v, 3) = Val_int(buf->st_mode & 07777);
-    Field (v, 4) = Val_int (buf->st_nlink);
-    Field (v, 5) = Val_int (buf->st_uid);
-    Field (v, 6) = Val_int (buf->st_gid);
-    Field (v, 7) = Val_int (buf->st_rdev);
-    Field (v, 8) = 
-      use_64 ? Val_file_offset(buf->st_size) : Val_int (buf->st_size);
-    Field (v, 9) = atime;
-    Field (v, 10) = mtime;
-    Field (v, 11) = ctime;
-  End_roots();
-  return v;
+  atime = copy_double((double) buf->st_atime);
+  mtime = copy_double((double) buf->st_mtime);
+  ctime = copy_double((double) buf->st_ctime);
+  offset = use_64 ? Val_file_offset(buf->st_size) : Val_int (buf->st_size);
+  v = alloc_small(12, 0);
+  Field (v, 0) = Val_int (buf->st_dev);
+  Field (v, 1) = Val_int (buf->st_ino);
+  Field (v, 2) = cst_to_constr(buf->st_mode & S_IFMT, file_kind_table,
+                               sizeof(file_kind_table) / sizeof(int), 0);
+  Field (v, 3) = Val_int (buf->st_mode & 07777);
+  Field (v, 4) = Val_int (buf->st_nlink);
+  Field (v, 5) = Val_int (buf->st_uid);
+  Field (v, 6) = Val_int (buf->st_gid);
+  Field (v, 7) = Val_int (buf->st_rdev);
+  Field (v, 8) = offset;
+  Field (v, 9) = atime;
+  Field (v, 10) = mtime;
+  Field (v, 11) = ctime;
+  CAMLreturn(v);
 }
 
 CAMLprim value unix_stat(value path)
