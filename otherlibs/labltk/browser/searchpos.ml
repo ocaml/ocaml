b3ccea2d6de1bc35385cda52405333d9aeeaa06e
Jacques Garrigue <garrigue at math.nagoya-u.ac.jp>
2003-05-09 10:01:52 +0000

    oubli info debug
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@5544 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- otherlibs/labltk/browser/searchpos.ml
+++ otherlibs/labltk/browser/searchpos.ml
@@ -343,9 +343,7 @@ let rec view_signature ?title ?path ?(env = !start_env) ?(detach=false) sign =
             view_module path ~env;
             find_shown_module path
         in
-        prerr_endline (Widget.name mw.mw_frame);
         (try !set_path path ~sign with _ -> ());
-        prerr_endline "Here";
         begin match mw.mw_title with None -> ()
         | Some label ->
             Label.configure label ~text:title;
