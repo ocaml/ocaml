1007ca72262831c9c438435af94ab10c2553155f
Xavier Leroy <xavier.leroy@inria.fr>
2003-07-29 09:09:33 +0000

    PR#1781 + Retour a %.12g dans string_of_float (PR#1778)
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@5756 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- otherlibs/threads/pervasives.ml
+++ otherlibs/threads/pervasives.ml
@@ -198,13 +198,14 @@ let valid_float_lexem s =
   let l = string_length s in
   let rec loop i =
     if i >= l then s ^ "." else
-    if s.[i] = '.' || s.[i] = 'e' then s
-    else loop (i+1)
+    match s.[i] with
+    | '0' .. '9' | '-' -> loop (i+1)
+    | _ -> s
   in
   loop 0
 ;;
 
-let string_of_float f = valid_float_lexem (format_float "%.17g" f);;
+let string_of_float f = valid_float_lexem (format_float "%.12g" f);;
 
 external float_of_string : string -> float = "float_of_string"
 
