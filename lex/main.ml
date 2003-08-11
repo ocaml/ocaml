7d9a7ba149f9f631fdedbedd37197cbdc7ae1215
Luc Maranget <luc.maranget@inria.fr>
2003-08-11 11:56:16 +0000

    broutilles dans ocamllex, echec sur zero argument
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@5770 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- lex/main.ml
+++ lex/main.ml
@@ -18,45 +18,45 @@ open Syntax
 open Lexgen
 
 let ml_automata = ref false
-let source_name = ref ""
-let output_name = ref "";;
+let source_name = ref None
+let output_name = ref None
 
 let usage = "usage: ocamlex [options] sourcefile"
 
+let specs =
+  ["-ml", Arg.Set ml_automata,
+    " Output code that does not use the Lexing module built-in automata interpreter";
+    "-o", Arg.String (fun x -> source_name := Some x),
+    " <file>  Set output file name to <file>";
+  ] 
+
 let _ =
   Arg.parse
-    ["-ml", Arg.Set ml_automata,
-           " Output code that does not use the Lexing module built-in automata interpreter";
-     "-o", Arg.String (fun x -> source_name := x),
-           " <file>  Set output file name to <file>";
-    ] 
-    (fun name -> source_name := name)
+    specs
+    (fun name -> source_name := Some name)
     usage
 
   
 let main () =
-  let source_name = !source_name in
-  let dest_name =
-    if !output_name <> "" then
-      !output_name
-    else if Filename.check_suffix source_name ".mll" then
-      Filename.chop_suffix source_name ".mll" ^ ".ml"
-    else
-      source_name ^ ".ml"
-  in
+
+  let source_name = match !source_name with
+  | None -> Arg.usage specs usage ; exit 2 
+  | Some name -> name in
+  let dest_name = match !output_name with
+  | Some name -> name
+  | None ->
+      if Filename.check_suffix source_name ".mll" then
+        Filename.chop_suffix source_name ".mll" ^ ".ml"
+      else
+        source_name ^ ".ml" in
+
   let ic = open_in_bin source_name in
   let oc = open_out dest_name in
   let tr = Common.open_tracker dest_name oc in
   let lexbuf = Lexing.from_channel ic in
   try
     let def = Parser.lexer_definition Lexer.main lexbuf in
-    let (entries, transitions) = Lexgen.make_dfa  def.entrypoints in
-(*
-    let (entries, transitions) = Lexmin.zyva (entries,transitions) in
-    let tables = Compact.compact_tables transitions in
-    Output.output_lexdef source_name ic oc
-                         def.header tables entries def.trailer;
-*)
+    let (entries, transitions) = Lexgen.make_dfa def.entrypoints in
     if !ml_automata then begin
       Outputbis.output_lexdef
         source_name ic oc tr
