477619882d6f8388894d22ebede07d7e1b649381
Xavier Leroy <xavier.leroy@inria.fr>
2003-08-29 12:15:15 +0000

    Revu compilation et installation des fichiers 'opt' pour que ca marche aussi en Win32
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@5808 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- camlp4/compile/compile.sh
+++ camlp4/compile/compile.sh
@@ -22,5 +22,6 @@ $OTOP/boot/ocamlrun$EXE $OTOP/boot/ocamlc -I $OTOP/boot -c tmp.mli
 $OTOP/boot/ocamlrun$EXE ../meta/camlp4r$EXE -I ../meta pa_extend.cmo q_MLast.cmo -meta_action tmp.ml -o tmp.ppo
 $OTOP/boot/ocamlrun$EXE $OTOP/boot/ocamlc -I $OTOP/boot -I ../lib -I ../camlp4 -c -impl tmp.ppo
 rm tmp.ppo
-$OTOP/boot/ocamlrun$EXE ../camlp4/camlp4$EXE ./compile.cmo ./tmp.cmo ../etc/pr_r.cmo ../etc/pr_rp.cmo $ARGS -sep "\n\n" -impl /dev/null
+> tmp.null
+$OTOP/boot/ocamlrun$EXE ../camlp4/camlp4$EXE ./compile.cmo ./tmp.cmo ../etc/pr_r.cmo ../etc/pr_rp.cmo $ARGS -sep "\n\n" -impl tmp.null
 rm tmp.*
