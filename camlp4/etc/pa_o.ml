760157aac7b2517e2bb44516b48dd270b890e182
Damien Doligez <damien.doligez-inria.fr>
2003-09-14 16:25:14 +0000

    PR#1825
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@5834 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- camlp4/etc/pa_o.ml
+++ camlp4/etc/pa_o.ml
@@ -975,8 +975,12 @@ EXTEND
       | "["; tpl = LIST1 type_parameter SEP ","; "]" -> (loc, tpl) ] ]
   ;
   class_fun_def:
-    [ [ p = labeled_patt; "->"; ce = class_expr ->
+    [ [ p = patt LEVEL "simple"; "->"; ce = class_expr ->
           <:class_expr< fun $p$ -> $ce$ >>
+      | p = labeled_patt; "->"; ce = class_expr ->
+          <:class_expr< fun $p$ -> $ce$ >>
+      | p = patt LEVEL "simple"; cfd = SELF ->
+          <:class_expr< fun $p$ -> $cfd$ >>
       | p = labeled_patt; cfd = SELF ->
           <:class_expr< fun $p$ -> $cfd$ >> ] ]
   ;
