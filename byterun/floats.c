d956eeca5b7d804dcd9e5bb4cf908e74bab506f2
Xavier Leroy <xavier.leroy@inria.fr>
2003-11-21 15:56:56 +0000

    Revu semantique de compare sur les flottants
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@5955 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- byterun/floats.c
+++ byterun/floats.c
@@ -327,7 +327,14 @@ CAMLprim value float_compare(value vf, value vg)
 {
   double f = Double_val(vf);
   double g = Double_val(vg);
-  return f < g ? Val_int(-1) : f > g ? Val_int(1) : Val_int(0);
+  if (f == g) return Val_int(0);
+  if (f < g) return Val_int(-1);
+  if (f > g) return Val_int(1);
+  /* One or both of f and g is NaN.  Order according to the
+     convention NaN = NaN and NaN < x for all other floats x. */
+  if (f == f) return Val_int(1);  /* f is not NaN, g is NaN */
+  if (g == g) return Val_int(-1); /* g is not NaN, f is NaN */
+  return Val_int(0);              /* both f and g are NaN */
 }
 
 CAMLprim value float_of_bytes(value s)
