73242ee60a78fcb22c3fc34a6e31896272aac6c3
Maxence Guesdon <maxence.guesdon@inria.fr>
2004-02-20 16:02:03 +0000

    Begin a test suite for ocamldoc
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@6126 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

---
+++ test/ocamldoc/t1.ml
@@ -0,0 +1,19 @@
+(** Testing display of types.
+
+   @test_types_display
+ *)
+
+let x = 1
+
+
+module M = struct
+  let y = 2 
+
+end
+
+module type MT = sig
+  type t = string -> int -> string -> (string * string * string) ->
+    (string * string * string) ->
+      (string * string * string) -> unit
+  val y : int
+end
