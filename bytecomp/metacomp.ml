727db4f0f5e9668d3db5d51449e354756b84a017
Jun FURUSE / 古瀬 淳 <jun.furuse@gmail.com>
2004-03-13 14:42:05 +0000

    bug fix for the case of Pmakeblock
    git-svn-id: http://caml.inria.fr/svn/ocaml/branches/gcaml@6150 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- bytecomp/metacomp.ml
+++ bytecomp/metacomp.ml
@@ -4,6 +4,12 @@ open Obj
 
 exception Unsupported
 
+exception Not_constant
+let extract_constant = function
+    Lconst sc -> sc
+  | _ -> raise Not_constant
+
+(*
 let rec transl_constant v =
   if Obj.is_int v then
     Const_base (Const_int (Obj.obj v))
@@ -23,5 +29,35 @@ let rec transl_constant v =
 	  transl_constant (Obj.field v pos) :: transl_args (pos+1)
       in
       Const_block (tag, transl_args 0)
+  else raise Unsupported
+*)
+
+(* Only for imuutable constant without functions *)
+let rec transl_constant overrides v =
+  if Obj.is_int v then
+    Lconst (Const_base (Const_int (Obj.obj v)))
+  else if Obj.is_block v then 
+    try List.assq v overrides with
+    | Not_found ->
+	let tag = Obj.tag v in
+	if tag >= no_scan_tag then 
+	  if tag = string_tag then 
+	    Lconst (Const_base (Const_string (Obj.obj v)))
+	  else if tag = double_tag then
+	    Lconst (Const_base (Const_float (Obj.obj v)))
+	  else raise Unsupported
+	else
+	  let len = Obj.size v in
+	  let rec transl_args pos =
+	    if pos = len then []
+	    else 
+	      transl_constant overrides (Obj.field v pos) :: transl_args (pos+1)
+	  in
+	  let args = transl_args 0 in
+	  try
+	    Lconst (Const_block (tag, List.map extract_constant args))
+	  with
+	  | Not_constant ->
+	      Lprim(Pmakeblock(tag, Immutable), args)
   else raise Unsupported
 
