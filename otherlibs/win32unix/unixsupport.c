dadb9b9be06aafd1dd667da72c8d497391a9acd4
Xavier Leroy <xavier.leroy@inria.fr>
2004-04-01 13:12:36 +0000

    PR#2334
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@6182 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- otherlibs/win32unix/unixsupport.c
+++ otherlibs/win32unix/unixsupport.c
@@ -61,6 +61,7 @@ value win_alloc_socket(SOCKET s)
   value res = alloc_custom(&win_handle_ops, sizeof(struct filedescr), 0, 1);
   Socket_val(res) = s;
   Descr_kind_val(res) = KIND_SOCKET;
+  CRT_fd_val(res) = NO_CRT_FD;
   return res;
 }
 
