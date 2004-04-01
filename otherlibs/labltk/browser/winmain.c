0100a08d104f6e1b0b8477b51072e09b0b456d52
Xavier Leroy <xavier.leroy@inria.fr>
2004-04-01 13:09:21 +0000

    Depollution
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@6179 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- otherlibs/labltk/browser/winmain.c
+++ otherlibs/labltk/browser/winmain.c
@@ -5,13 +5,13 @@
 
 extern int __argc;
 extern char **__argv;
-extern void expand_command_line(int * argcp, char *** argvp);
+extern void caml_expand_command_line(int * argcp, char *** argvp);
 extern void caml_main (char **);
 
 int WINAPI WinMain(HINSTANCE h, HINSTANCE HPrevInstance,
                    LPSTR lpCmdLine, int nCmdShow)
 {
-  expand_command_line(&__argc, &__argv);
+  caml_expand_command_line(&__argc, &__argv);
   caml_main(__argv);
   sys_exit(Val_int(0));
   return 0;
