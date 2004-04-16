2cb1a50144f60432b809310086dff513511bbb3f
Basile Starynkevitch <basile.starynkevitch@inria.fr>
2004-04-16 13:08:05 +0000

    added caml_clear_bytecode
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@6220 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- byterun/interp.h
+++ byterun/interp.h
@@ -18,11 +18,15 @@
 #ifndef CAML_INTERP_H
 #define CAML_INTERP_H
 
+/* cvs $Id$ */
 
 #include "misc.h"
 #include "mlvalues.h"
 
+/* interpret a bytecode */
 value caml_interprete (code_t prog, asize_t prog_size);
 
+/* tell the runtime that a bytecode program is no more needed */
+void caml_clear_bytecode(code_t prog, asize_t prog_size);
 
 #endif /* CAML_INTERP_H */
