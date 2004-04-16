70781e166bc3d82d801792e37972f01071e36ca1
Jun FURUSE / 古瀬 淳 <jun.furuse@gmail.com>
2004-04-16 03:12:52 +0000

    added gprint
    git-svn-id: http://caml.inria.fr/svn/ocaml/branches/gcaml@6218 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- gcamllib/gprint.ml
+++ gcamllib/gprint.ml
@@ -1,4 +1,5 @@
 open Format
+open Rtype
 
 let rec print_list sep f ppf = function
   | [] -> ()
@@ -9,11 +10,42 @@ let rec print_list sep f ppf = function
 	sep ()
 	(print_list sep f) xs
 
-let print_tuple f ppf = print_list (fun ppf () -> fprintf ppf "@,*@,") f ppf
+let print_tuple f ppf v = 
+  print_list (fun ppf () -> fprintf ppf ",@ ") f ppf v
+
+let printers =
+  [ Builtintypes.int, (fun ppf v -> fprintf ppf "%i" (Obj.obj v));
+    Builtintypes.char, (fun ppf v -> fprintf ppf "%C" (Obj.obj v));
+    Builtintypes.string, (fun ppf v -> fprintf ppf "%S" (Obj.obj v));
+    Builtintypes.float, (fun ppf v -> fprintf ppf "%F" (Obj.obj v));
+    Builtintypes.bool, (fun ppf v -> fprintf ppf "%B" (Obj.obj v));
+    Builtintypes.unit, (fun ppf v -> fprintf ppf "()");
+    Builtintypes.exn, (fun ppf v -> fprintf ppf "<exn>");
+    Builtintypes.nativeint, (fun ppf v -> fprintf ppf "%nd" (Obj.obj v));
+    Builtintypes.int32, (fun ppf v -> fprintf ppf "%ld" (Obj.obj v));
+    Builtintypes.int64, (fun ppf v -> fprintf ppf "%Ld" (Obj.obj v));
+  ]
 
 generic val print : {'a} => formatter -> 'a -> unit =
-  fun ty ppf v ->
-    match ty.desc with
-    | Tvar -> fprintf ppf "<poly>"
-    | Tarrow (_,_,_) -> fprintf ppf "<fun">
-    | Ttuple ts -> print_tuple print ppf v
+  let rec print =
+    fun ty ppf v ->
+      Rtype.print ppf ty;
+      match ty.desc with
+      | Tvar -> fprintf ppf "<poly>"
+      | Tarrow (_,_,_) -> fprintf ppf "<fun>"
+      | Ttuple ts -> 
+  	(* bind types and values *)
+  	let rec bind pos = function
+  	  | [] -> []
+  	  | t::ts -> (t, Obj.field v pos) :: bind (pos+1) ts
+  	in
+  	fprintf ppf "(@[%a@])" 
+  	  (print_tuple (fun ppf (t,v) -> print t ppf v)) (bind 0 ts)
+      | Tconstr ((path, decl), args) -> 
+	  begin try
+	    List.assq decl printers ppf v
+	  with
+	  | Not_found -> fprintf ppf "<???>"
+	  end
+  in
+  print
