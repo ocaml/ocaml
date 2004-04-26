e95d355d4bdd7e9cb1c125b506b76a827f454cd3
Basile Starynkevitch <basile.starynkevitch@inria.fr>
2004-04-26 14:09:01 +0000

    added call to caml_prepare_bytecode
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@6258 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- byterun/meta.c
+++ byterun/meta.c
@@ -55,6 +55,7 @@ CAMLprim value caml_reify_bytecode(value prog, value len)
 #ifdef THREADED_CODE
   caml_thread_code((code_t) prog, (asize_t) Long_val(len));
 #endif
+  caml_prepare_bytecode((code_t) prog, (asize_t) Long_val(len));
   clos = caml_alloc_small (1, Closure_tag);
   Code_val(clos) = (code_t) prog;
   return clos;
