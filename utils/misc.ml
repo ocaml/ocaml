6f34fbb120fd7c31df2e8acc97c3d0f0f5dcc3e1
Xavier Leroy <xavier.leroy@inria.fr>
2004-06-13 12:48:25 +0000

    Revu chop_extension_if_any
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@6399 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- utils/misc.ml
+++ utils/misc.ml
@@ -154,10 +154,7 @@ let no_overflow_lsl a = min_int asr 1 <= a && a <= max_int asr 1
 (* String operations *)
 
 let chop_extension_if_any fname =
-  try
-    ignore(String.index (Filename.basename fname) '.');
-    Filename.chop_extension fname
-  with Not_found -> fname
+  try Filename.chop_extension fname with Invalid_argument _ -> fname
 
 let search_substring pat str start =
   let rec search i j =
