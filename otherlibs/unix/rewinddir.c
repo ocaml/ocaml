31577069f820a9a324db354a9b54e5e523ae3a59
Xavier Leroy <xavier.leroy@inria.fr>
2004-06-19 15:38:31 +0000

    MAJ suite changement representation dir_handle (PR#2732)
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@6421 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- otherlibs/unix/rewinddir.c
+++ otherlibs/unix/rewinddir.c
@@ -15,6 +15,7 @@
 
 #include <mlvalues.h>
 #include "unixsupport.h"
+#include <errno.h>
 #include <sys/types.h>
 #ifdef HAS_DIRENT
 #include <dirent.h>
@@ -24,9 +25,11 @@
 
 #ifdef HAS_REWINDDIR
 
-CAMLprim value unix_rewinddir(value d)
+CAMLprim value unix_rewinddir(value vd)
 {
-  rewinddir((DIR *) d);
+  DIR * d = DIR_Val(vd);
+  if (d == (DIR *) NULL) unix_error(EBADF, "rewinddir", Nothing);
+  rewinddir(d);
   return Val_unit;
 }
 
