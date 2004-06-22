76ed3359d4ab0292a2f7e69baaa8574d42836b98
Xavier Leroy <xavier.leroy@inria.fr>
2004-06-22 12:13:46 +0000

    Rendre une copie de la chaine dans string_of_format (PR#2166)
    git-svn-id: http://caml.inria.fr/svn/ocaml/version/3.08@6440 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- stdlib/pervasives.mli
+++ stdlib/pervasives.mli
@@ -819,9 +819,9 @@ type ('a, 'b, 'c) format = ('a, 'b, 'c, 'c) format4
     and ['b] is the type of the first argument given to
     [%a] and [%t] printing functions. *)
 
-external string_of_format :
-  ('a, 'b, 'c, 'd) format4 -> string = "%identity"
+val string_of_format : ('a, 'b, 'c, 'd) format4 -> string
 (** Converts a format string into a string. *)
+
 external format_of_string :
   ('a, 'b, 'c, 'd) format4 -> ('a, 'b, 'c, 'd) format4 = "%identity"
 (** [format_of_string s] returns a format string read from the string
