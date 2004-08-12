d704771b0cbef55c64e296518fbf361e23fb9460
Xavier Leroy <xavier.leroy@inria.fr>
2004-08-12 13:32:11 +0000

    Nettoyage de la partie system-dependent de la gestion des signaux (a tester)
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@6590 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- asmrun/signals.c
+++ asmrun/signals.c
@@ -15,9 +15,6 @@
 
 #include <signal.h>
 #include <stdio.h>
-#if defined(TARGET_sparc) && defined(SYS_solaris)
-#include <ucontext.h>
-#endif
 #include "alloc.h"
 #include "callback.h"
 #include "memory.h"
@@ -33,6 +30,8 @@
 #include <sys/resource.h>
 #endif
 
+#include "signals_osdep.h"
+
 extern char * caml_code_area_start, * caml_code_area_end;
 
 #define In_code_area(pc) \
@@ -42,71 +41,6 @@ extern char * caml_code_area_start, * caml_code_area_end;
 typedef void (*sighandler)(int sig);
 extern sighandler caml_win32_signal(int sig, sighandler action);
 #define signal(sig,act) caml_win32_signal(sig,act)
-#endif
-
-#if defined(TARGET_power) && defined(SYS_rhapsody)
-
-  #include <sys/utsname.h>
-
-  #define STRUCT_SIGCONTEXT void
-  #define CONTEXT_GPR(ctx, regno) (*context_gpr_p ((ctx), (regno)))
-  #define CONTEXT_PC(ctx) CONTEXT_GPR ((ctx), -2)
-  static int ctx_version = 0;
-  static void init_ctx (void)
-  {
-    struct utsname name;
-    if (uname (&name) == 0){
-      if (name.release[1] == '.' && name.release[0] <= '5'){
-        ctx_version = 1;
-      }else{
-        ctx_version = 2;
-      }
-    }else{
-      caml_fatal_error ("cannot determine SIGCONTEXT format");
-    }
-  }
-
-  #ifdef DARWIN_VERSION_6
-    #include <sys/ucontext.h>
-    static unsigned long *context_gpr_p (void *ctx, int regno)
-    {
-      unsigned long *regs;
-      if (ctx_version == 0) init_ctx ();
-      if (ctx_version == 1){
-        /* old-style context (10.0 and 10.1) */
-        regs = (unsigned long *)(((struct sigcontext *)ctx)->sc_regs);
-      }else{
-        Assert (ctx_version == 2);
-        /* new-style context (10.2) */
-        regs = (unsigned long *)&(((struct ucontext *)ctx)->uc_mcontext->ss);
-      }
-      return &(regs[2 + regno]);
-    }
-  #else
-    #define SA_SIGINFO 0x0040
-    struct ucontext {
-      int       uc_onstack;
-      sigset_t  uc_sigmask;
-      struct sigaltstack uc_stack;
-      struct ucontext   *uc_link;
-      size_t    uc_mcsize;
-      unsigned long     *uc_mcontext;
-    };
-    static unsigned long *context_gpr_p (void *ctx, int regno)
-    {
-      unsigned long *regs;
-      if (ctx_version == 0) init_ctx ();
-      if (ctx_version == 1){
-        /* old-style context (10.0 and 10.1) */
-        regs = (unsigned long *)(((struct sigcontext *)ctx)->sc_regs);
-      }else{
-        Assert (ctx_version == 2);
-        /* new-style context (10.2) */
-        regs = (unsigned long *)((struct ucontext *)ctx)->uc_mcontext + 8;
-      }
-      return &(regs[2 + regno]);
-    }
-  #endif
 #endif
 
 volatile int caml_async_signal_mode = 0;
@@ -123,17 +57,14 @@ static int rev_convert_signal_number(int signo);
 void caml_execute_signal(int signal_number, int in_signal_handler)
 {
   value res;
-#ifdef POSIX_SIGNALS
   sigset_t sigs;
   /* Block the signal before executing the handler, and record in sigs
      the original signal mask */
   sigemptyset(&sigs);
   sigaddset(&sigs, signal_number);
   sigprocmask(SIG_BLOCK, &sigs, &sigs);
-#endif
   res = caml_callback_exn(Field(caml_signal_handlers, signal_number),
                           Val_int(rev_convert_signal_number(signal_number)));
-#ifdef POSIX_SIGNALS
   if (! in_signal_handler) {
     /* Restore the original signal mask */
     sigprocmask(SIG_SETMASK, &sigs, NULL);
@@ -142,7 +73,6 @@ void caml_execute_signal(int signal_number, int in_signal_handler)
     sigdelset(&sigs, signal_number);
     sigprocmask(SIG_SETMASK, &sigs, NULL);
   }
-#endif
   if (Is_exception_result(res)) caml_raise(Extract_exception(res));
 }
 
@@ -212,23 +142,8 @@ void caml_leave_blocking_section(void)
   caml_async_signal_mode = 0;
 }
 
-#if defined(TARGET_alpha) || defined(TARGET_mips)
-static void handle_signal(int sig, int code, struct sigcontext * context)
-#elif defined(TARGET_power) && defined(SYS_elf)
-static void handle_signal(int sig, struct sigcontext * context)
-#elif defined(TARGET_power) && defined(SYS_rhapsody)
-static void handle_signal(int sig, int code, STRUCT_SIGCONTEXT * context)
-#elif defined(TARGET_power) && defined(SYS_bsd)
-static void handle_signal(int sig, int code, struct sigcontext * context)
-#elif defined(TARGET_sparc) && defined(SYS_solaris)
-static void handle_signal(int sig, int code, void * context)
-#else
-static void handle_signal(int sig)
-#endif
+DECLARE_SIGNAL_HANDLER(handle_signal)
 {
-#if !defined(POSIX_SIGNALS) && !defined(BSD_SIGNALS)
-  signal(sig, handle_signal);
-#endif
   if (caml_async_signal_mode) {
     /* We are interrupting a C function blocked on I/O.
        Callback the Caml code immediately. */
@@ -244,44 +159,9 @@ static void handle_signal(int sig)
     /* Some ports cache [caml_young_limit] in a register.
        Use the signal context to modify that register too, but only if
        we are inside Caml code (not inside C code). */
-#if defined(TARGET_alpha)
-    if (In_code_area(context->sc_pc)) {
-      /* Cached in register $14 */
-      context->sc_regs[14] = (long) caml_young_limit;
-    }
-#endif
-#if defined(TARGET_mips)
-    if (In_code_area(context->sc_pc)) {
-      /* Cached in register $23 */
-      context->sc_regs[23] = (int) caml_young_limit;
-    }
-#endif
-#if defined(TARGET_power) && defined(SYS_elf)
-    if (caml_last_return_address == 0) {
-      /* Cached in register 30 */
-      context->regs->gpr[30] = (unsigned long) caml_young_limit;
-    }
-#endif
-#if defined(TARGET_power) && defined(SYS_rhapsody)
-    if (In_code_area(CONTEXT_PC(context))) {
-      /* Cached in register 30 */
-      CONTEXT_GPR(context, 30) = (unsigned long) caml_young_limit;
-    }
-#endif
-#if defined(TARGET_power) && defined(SYS_bsd)
-    if (caml_last_return_address == 0) {
-      /* Cached in register 30 */
-      context->sc_frame.fixreg[30] = (unsigned long) caml_young_limit;
-    }
-#endif
-#if defined(TARGET_sparc) && defined(SYS_solaris)
-    { greg_t * gregs = ((ucontext_t *)context)->uc_mcontext.gregs;
-      if (In_code_area(gregs[REG_PC])) {
-      /* Cached in register l7, which is saved on the stack 7 words
-	 after the stack pointer.  */
-        ((long *)(gregs[REG_SP]))[7] = (long) caml_young_limit;
-      }
-    }
+#if defined(CONTEXT_PC) && defined(CONTEXT_YOUNG_LIMIT)
+    if (In_code_area(CONTEXT_PC))
+      CONTEXT_YOUNG_LIMIT = (context_reg) caml_young_limit;
 #endif
   }
 }
@@ -380,10 +260,7 @@ value caml_install_signal_handler(value signal_number, value action) /* ML */
 {
   CAMLparam2 (signal_number, action);
   int sig;
-  void (*act)(int signo), (*oldact)(int signo);
-#ifdef POSIX_SIGNALS
   struct sigaction sigact, oldsigact;
-#endif
   CAMLlocal1 (res);
 
   sig = caml_convert_signal_number(Int_val(signal_number));
@@ -391,34 +268,24 @@ value caml_install_signal_handler(value signal_number, value action) /* ML */
     caml_invalid_argument("Sys.signal: unavailable signal");
   switch(action) {
   case Val_int(0):              /* Signal_default */
-    act = SIG_DFL;
+    sigact.sa_handler = SIG_DFL;
+    sigact.sa_flags = 0;
     break;
   case Val_int(1):              /* Signal_ignore */
-    act = SIG_IGN;
+    sigact.sa_handler = SIG_IGN;
+    sigact.sa_flags = 0;
     break;
   default:                      /* Signal_handle */
-    act = (void (*)(int)) handle_signal;
+    SET_SIGACT(sigact, handle_signal);
     break;
   }
-#ifdef POSIX_SIGNALS
-  sigact.sa_handler = act;
   sigemptyset(&sigact.sa_mask);
-#if defined(SYS_solaris) || defined(SYS_rhapsody)
-  sigact.sa_flags = SA_SIGINFO;
-#else
-  sigact.sa_flags = 0;
-#endif
   if (sigaction(sig, &sigact, &oldsigact) == -1) caml_sys_error(NO_ARG);
-  oldact = oldsigact.sa_handler;
-#else
-  oldact = signal(sig, act);
-  if (oldact == SIG_ERR) caml_sys_error(NO_ARG);
-#endif
-  if (oldact == (void (*)(int)) handle_signal) {
+  if (oldsigact.sa_handler == (void (*)(int)) handle_signal) {
     res = caml_alloc_small(1, 0);          /* Signal_handle */
     Field(res, 0) = Field(caml_signal_handlers, sig);
   }
-  else if (oldact == SIG_IGN)
+  else if (oldsigact.sa_handler == SIG_IGN)
     res = Val_int(1);           /* Signal_ignore */
   else
     res = Val_int(0);           /* Signal_default */
@@ -434,94 +301,37 @@ value caml_install_signal_handler(value signal_number, value action) /* ML */
 
 /* Machine- and OS-dependent handling of bound check trap */
 
-#if defined(TARGET_sparc) && defined(SYS_sunos)
-static void trap_handler(int sig, int code, 
-                         struct sigcontext * context, char * address)
+#if defined(TARGET_sparc) || defined(TARGET_power)
+DECLARE_SIGNAL_HANDLER(trap_handler)
 {
-  int * sp;
-  /* Unblock SIGILL */
-  sigset_t mask;
-  sigemptyset(&mask);
-  sigaddset(&mask, SIGILL);
-  sigprocmask(SIG_UNBLOCK, &mask, NULL);
-  if (code != ILL_TRAP_FAULT(5)) {
-    fprintf(stderr, "Fatal error: illegal instruction, code 0x%x\n", code);
-    exit(100);
-  }
-  /* Recover [caml_young_ptr] and [caml_exception_pointer]
-     from the %l5 and %l6 regs */
-  sp = (int *) context->sc_sp;
-  caml_exception_pointer = (char *) sp[5];
-  caml_young_ptr = (char *) sp[6];
-  caml_array_bound_error();
-}
-#endif
-
-#if defined(TARGET_sparc) && defined(SYS_solaris)
-static void trap_handler(int sig, siginfo_t * info, void * context)
-{
-  long * sp;
-
+#if defined(SYS_solaris)
   if (info->si_code != ILL_ILLTRP) {
-    fprintf(stderr, "Fatal error: illegal instruction, code 0x%x\n",
-            info->si_code);
-    exit(100);
+    /* Deactivate our exception handler and return. */
+    struct sigaction act;
+    act.sa_handler = SIG_DFL;
+    act.sa_flags = 0;
+    sigemptyset(&act.sa_mask);
+    sigaction(sig, &act, NULL);
+    return;
   }
-  /* Recover [caml_young_ptr] and [caml_exception_pointer]
-     from the %l5 and %l6 regs */
-  sp = (long *) (((ucontext_t *)context)->uc_mcontext.gregs[REG_SP]);
-  caml_exception_pointer = (char *) sp[5];
-  caml_young_ptr = (char *) sp[6];
-  caml_array_bound_error();
-}
 #endif
-
-#if defined(TARGET_sparc) && (defined(SYS_bsd) || defined(SYS_linux))
-static void trap_handler(int sig)
-{
-  /* TODO: recover registers from context and call [caml_array_bound_error] */
-  caml_fatal_error("Fatal error: out-of-bound access in array or string\n");
-}
-#endif
-
-#if defined(TARGET_power) && defined(SYS_elf)
-static void trap_handler(int sig, struct sigcontext * context)
-{
-  /* Recover [caml_young_ptr] and [caml_exception_pointer]
-     from registers 31 and 29 */
-  caml_exception_pointer = (char *) context->regs->gpr[29];
-  caml_young_ptr = (char *) context->regs->gpr[31];
-  caml_array_bound_error();
-}
-#endif
-
-#if defined(TARGET_power) && defined(SYS_rhapsody)
-static void trap_handler(int sig, int code, STRUCT_SIGCONTEXT * context)
-{
+#if defined(SYS_rhapsody)
   /* Unblock SIGTRAP */
-  sigset_t mask;
-  sigemptyset(&mask);
-  sigaddset(&mask, SIGTRAP);
-  sigprocmask(SIG_UNBLOCK, &mask, NULL);
-  /* Recover [caml_young_ptr] and [caml_exception_pointer]
-     from registers 31 and 29 */
-  caml_exception_pointer = (char *) CONTEXT_GPR(context, 29);
-  caml_young_ptr = (char *) CONTEXT_GPR(context, 31);
+  { sigset_t mask;
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGTRAP);
+    sigprocmask(SIG_UNBLOCK, &mask, NULL);
+  }
+#endif
+#if defined(CONTEXT_EXCEPTION_POINTER) && defined(CONTEXT_YOUNG_PTR)
+  caml_exception_pointer = (char *) CONTEXT_EXCEPTION_POINTER;
+  caml_young_ptr = (char *) CONTEXT_YOUNG_PTR;
   caml_array_bound_error();
+#else
+  caml_fatal_error("Fatal error: out-of-bound access in array or string\n");
+#endif
 }
 #endif
-
-#if defined(TARGET_power) && defined(SYS_bsd)
-static void trap_handler(int sig, int code, struct sigcontext * context)
-{
-  /* Recover [caml_young_ptr] and [caml_exception_pointer]
-     from registers 31 and 29 */
-  caml_exception_pointer = (char *) context->sc_frame.fixreg[29];
-  caml_young_ptr = (char *) context->sc_frame.fixreg[31];
-  caml_array_bound_error();
-}
-#endif
-
 
 /* Machine- and OS-dependent handling of stack overflow */
 
@@ -555,21 +365,11 @@ static int is_stack_overflow(char * fault_addr)
   }
 }
 
-#if defined(TARGET_i386) && defined(SYS_linux_elf)
-static void segv_handler(int signo, struct sigcontext sc)
+DECLARE_SIGNAL_HANDLER(segv_handler)
 {
-  if (is_stack_overflow((char *) sc.cr2))
+  if (is_stack_overflow(CONTEXT_FAULTING_ADDRESS))
     caml_raise_stack_overflow();
 }
-#endif
-
-#if defined(TARGET_i386) && !defined(SYS_linux_elf)
-static void segv_handler(int signo, siginfo_t * info, void * arg)
-{
-  if (is_stack_overflow((char *) info->si_addr))
-    caml_raise_stack_overflow();
-}
-#endif
 
 #endif
 
@@ -578,38 +378,26 @@ static void segv_handler(int signo, siginfo_t * info, void * arg)
 void caml_init_signals(void)
 {
   /* Bound-check trap handling */
-#if defined(TARGET_sparc) && \
-      (defined(SYS_sunos) || defined(SYS_bsd) || defined(SYS_linux))
-  {
-    struct sigaction act;
-    act.sa_handler = (void (*)(int)) trap_handler;
+#if defined(TARGET_sparc)
+  { struct sigaction act;
     sigemptyset(&act.sa_mask);
-    act.sa_flags = 0;
+    SET_SIGACT(act, trap_handler);
+    act.sa_flags |= SA_NODEFER;
     sigaction(SIGILL, &act, NULL);
   }
 #endif
-#if defined(TARGET_sparc) && defined(SYS_solaris)
-  {
-    struct sigaction act;
-    act.sa_sigaction = trap_handler;
-    sigemptyset(&act.sa_mask);
-    act.sa_flags = SA_SIGINFO | SA_NODEFER;
-    sigaction(SIGILL, &act, NULL);
-  }
-#endif
+
 #if defined(TARGET_power)
-  {
-    struct sigaction act;
-    act.sa_handler = (void (*)(int)) trap_handler;
+  { struct sigaction act;
     sigemptyset(&act.sa_mask);
-#if defined (SYS_rhapsody)
-    act.sa_flags = SA_SIGINFO;
-#else
-    act.sa_flags = SA_NODEFER;
+    SET_SIGACT(act, trap_handler);
+#if !defined(SYS_rhapsody)
+    act.sa_flags |= SA_NODEFER;
 #endif
     sigaction(SIGTRAP, &act, NULL);
   }
 #endif
+
   /* Stack overflow handling */
 #ifdef HAS_STACK_OVERFLOW_DETECTION
   {
@@ -618,13 +406,8 @@ void caml_init_signals(void)
     stk.ss_sp = sig_alt_stack;
     stk.ss_size = SIGSTKSZ;
     stk.ss_flags = 0;
-#if defined(TARGET_i386) && defined(SYS_linux_elf)
-    act.sa_handler = (void (*)(int)) segv_handler;
-    act.sa_flags = SA_ONSTACK | SA_NODEFER;
-#else
-    act.sa_sigaction = segv_handler;
-    act.sa_flags = SA_SIGINFO | SA_ONSTACK | SA_NODEFER;
-#endif
+    SET_SIGACT(act, segv_handler);
+    act.sa_flags |= SA_ONSTACK | SA_NODEFER;
     sigemptyset(&act.sa_mask);
     system_stack_top = (char *) &act;
     if (sigaltstack(&stk, NULL) == 0) { sigaction(SIGSEGV, &act, NULL); }
---
+++ asmrun/signals_osdep.h
@@ -0,0 +1,237 @@
+/***********************************************************************/
+/*                                                                     */
+/*                           Objective Caml                            */
+/*                                                                     */
+/*            Xavier Leroy, projet Cristal, INRIA Rocquencourt         */
+/*                                                                     */
+/*  Copyright 2004 Institut National de Recherche en Informatique et   */
+/*  en Automatique.  All rights reserved.  This file is distributed    */
+/*  under the terms of the GNU Library General Public License, with    */
+/*  the special exception on linking described in file ../LICENSE.     */
+/*                                                                     */
+/***********************************************************************/
+
+/* $Id$ */
+
+/* Processor- and OS-dependent signal interface */
+
+/****************** Alpha, all OS */
+
+#if defined(TARGET_alpha)
+
+  #define DECLARE_SIGNAL_HANDLER(name) \
+    static void name(int sig, int code, struct sigcontext * context)
+
+  #define SET_SIGACT(sigact,name) \
+     sigact.sa_handler = (void (*)(int)) (name); \
+     sigact.sa_flags = 0
+
+  typedef long context_reg;
+  #define CONTEXT_PC (context->sc_pc)
+  #define CONTEXT_EXCEPTION_POINTER (context->sc_regs[15])
+  #define CONTEXT_YOUNG_LIMIT (context->sc_regs[13]
+  #define CONTEXT_YOUNG_PTR (context->sc_regs[14])
+
+/****************** AMD64, Linux */
+
+#elif defined(TARGET_amd64) && defined (SYS_linux_elf)
+
+  #define DECLARE_SIGNAL_HANDLER(name) \
+    static void name(int sig, siginfo_t * info, ucontext_t * context)
+
+  #define SET_SIGACT(sigact,name) \
+     sigact.sa_sigaction = (void (*)(int,siginfo_t *,void *)) (name); \
+     sigact.sa_flags = SA_SIGINFO
+
+  typedef unsigned long context_reg;
+  #define CONTEXT_PC (context->uc_mcontext.rip)
+  #define CONTEXT_EXCEPTION_POINTER (context->uc_mcontext.r14)
+  #define CONTEXT_YOUNG_PTR (context->uc_mcontext.r15)
+  #define CONTEXT_FAULTING_ADDRESS ((char *) context->uc_mcontext.cr2)
+
+/****************** I386, Linux */
+
+#elif defined(TARGET_i386) && defined(SYS_linux_elf)
+
+  #define DECLARE_SIGNAL_HANDLER(name) \
+    static void name(int sig, struct sigcontext context)
+
+  #define SET_SIGACT(sigact,name) \
+     sigact.sa_handler = (void (*)(int)) (name); \
+     sigact.sa_flags = 0
+
+  #define CONTEXT_FAULTING_ADDRESS ((char *) context.cr2)
+
+/****************** I386, BSD */
+
+#elif defined(TARGET_i386) && defined(SYS_bsd)
+
+  #define DECLARE_SIGNAL_HANDLER(name) \
+    static void name(int sig, siginfo_t * info, void * context)
+
+  #define SET_SIGACT(sigact,name) \
+     sigact.sa_sigaction = (name);
+     sigact.sa_flags = SA_SIGINFO
+
+  #define CONTEXT_FAULTING_ADDRESS ((char *) info->si_addr)
+
+/****************** MIPS, all OS */
+
+#elif defined(TARGET_mips)
+
+  #define DECLARE_SIGNAL_HANDLER(name) \
+    static void name(int sig, int code, struct sigcontext * context)
+
+  #define SET_SIGACT(sigact,name) \
+     sigact.sa_handler = (void (*)(int)) (name); \
+     sigact.sa_flags = 0
+
+  typedef int context_reg;
+  #define CONTEXT_PC (context->sc_pc)
+  #define CONTEXT_EXCEPTION_POINTER (context->sc_regs[30])
+  #define CONTEXT_YOUNG_LIMIT (context->sc_regs[22]
+  #define CONTEXT_YOUNG_PTR (context->sc_regs[23])
+
+/****************** PowerPC, MacOS X */
+
+#elif defined(TARGET_power) && defined(SYS_rhapsody)
+
+  #include <sys/utsname.h>
+
+  #define DECLARE_SIGNAL_HANDLER(name) \
+     static void name(int sig, int code, void * context)
+
+  #define SET_SIGACT(sigact,name) \
+     sigact.sa_handler = (void (*)(int)) (name); \
+     sigact.sa_flags = SA_SIGINFO
+
+  typedef unsigned long context_reg;
+  #define CONTEXT_PC (*context_gpr_p(context, -2))
+  #define CONTEXT_EXCEPTION_POINTER (*context_gpr_p(context, 29))
+  #define CONTEXT_YOUNG_LIMIT (*context_gpr_p(context, 30))
+  #define CONTEXT_YOUNG_PTR (*context_gpr_p(context, 31))
+
+  static int ctx_version = 0;
+  static void init_ctx (void)
+  {
+    struct utsname name;
+    if (uname (&name) == 0){
+      if (name.release[1] == '.' && name.release[0] <= '5'){
+        ctx_version = 1;
+      }else{
+        ctx_version = 2;
+      }
+    }else{
+      caml_fatal_error ("cannot determine SIGCONTEXT format");
+    }
+  }
+
+  #ifdef DARWIN_VERSION_6
+    #include <sys/ucontext.h>
+    static unsigned long *context_gpr_p (void *ctx, int regno)
+    {
+      unsigned long *regs;
+      if (ctx_version == 0) init_ctx ();
+      if (ctx_version == 1){
+        /* old-style context (10.0 and 10.1) */
+        regs = (unsigned long *)(((struct sigcontext *)ctx)->sc_regs);
+      }else{
+        Assert (ctx_version == 2);
+        /* new-style context (10.2) */
+        regs = (unsigned long *)&(((struct ucontext *)ctx)->uc_mcontext->ss);
+      }
+      return &(regs[2 + regno]);
+    }
+  #else
+    #define SA_SIGINFO 0x0040
+    struct ucontext {
+      int       uc_onstack;
+      sigset_t  uc_sigmask;
+      struct sigaltstack uc_stack;
+      struct ucontext   *uc_link;
+      size_t    uc_mcsize;
+      unsigned long     *uc_mcontext;
+    };
+    static unsigned long *context_gpr_p (void *ctx, int regno)
+    {
+      unsigned long *regs;
+      if (ctx_version == 0) init_ctx ();
+      if (ctx_version == 1){
+        /* old-style context (10.0 and 10.1) */
+        regs = (unsigned long *)(((struct sigcontext *)ctx)->sc_regs);
+      }else{
+        Assert (ctx_version == 2);
+        /* new-style context (10.2) */
+        regs = (unsigned long *)((struct ucontext *)ctx)->uc_mcontext + 8;
+      }
+      return &(regs[2 + regno]);
+    }
+  #endif
+
+/****************** PowerPC, ELF (Linux) */
+
+#elif defined(TARGET_power) && defined(SYS_elf)
+
+  #define DECLARE_SIGNAL_HANDLER(name) \
+    static void name(int sig, struct sigcontext * context)
+
+  #define SET_SIGACT(sigact,name) \
+     sigact.sa_handler = (void (*)(int)) (name); \
+     sigact.sa_flags = 0
+
+  typedef unsigned long context_reg;
+  #define CONTEXT_PC (context->regs->nip)
+  #define CONTEXT_EXCEPTION_POINTER (context->regs->gpr[29])
+  #define CONTEXT_YOUNG_LIMIT (context->regs->gpr[30])
+  #define CONTEXT_YOUNG_PTR (context->regs->gpr[31])
+
+/****************** PowerPC, BSD */
+
+#elif defined(TARGET_power) && defined(SYS_bsd)
+
+  #define DECLARE_SIGNAL_HANDLER(name) \
+    static void name(int sig, int code, struct sigcontext * context)
+
+  #define SET_SIGACT(sigact,name) \
+     sigact.sa_handler = (void (*)(int)) (name); \
+     sigact.sa_flags = 0
+
+  typedef unsigned long context_reg;
+  #define CONTEXT_EXCEPTION_POINTER (context->sc_frame.fixreg[29])
+  #define CONTEXT_YOUNG_LIMIT (context->sc_frame.fixreg[30])
+  #define CONTEXT_YOUNG_PTR (context->sc_frame.fixreg[31])
+
+/****************** SPARC, Solaris */
+
+#elif defined(TARGET_sparc) && defined(SYS_solaris)
+
+  #include <ucontext.h>
+
+  #define DECLARE_SIGNAL_HANDLER(name) \
+    static void name(int sig, siginfo_t * info, ucontext_t * context)
+
+  #define SET_SIGACT(sigact,name) \
+     sigact.sa_sigaction = (void (*)(int,siginfo_t *,void *)) (name); \
+     sigact.sa_flags = SA_SIGINFO
+
+  typedef long context_reg;
+  #define CONTEXT_PC (context->uc_mcontext.gregs[REG_PC])
+    /* Local register number N is saved on the stack N words
+       after the stack pointer */
+  #define SPARC_L_REG(n) ((long *)(context->uc_mcontext.gregs[REG_SP]))[n]
+  #define CONTEXT_EXCEPTION_POINTER (SPARC_L_REG(5))
+  #define CONTEXT_YOUNG_LIMIT (SPARC_L_REG(7))
+  #define CONTEXT_YOUNG_PTR (SPARC_L_REG(6))
+
+/******************** Default */
+
+#else
+
+  #define DECLARE_SIGNAL_HANDLER(name) \
+    static void name(int sig)
+
+  #define SET_SIGACT(sigact,name) \
+     sigact.sa_handler = (name); \
+     sigact.sa_flags = 0
+
+#endif
