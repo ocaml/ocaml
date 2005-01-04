51ac0f737e4041972d8ae6f53d3ed1f271d02a0d
Damien Doligez <damien.doligez-inria.fr>
2005-01-04 15:39:44 +0000

    PR#3406 CAMLreturn manquant
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@6741 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- stdlib/sys.ml
+++ stdlib/sys.ml
@@ -78,4 +78,4 @@ let catch_break on =
 
 (* OCaml version string, must be in the format described in sys.mli. *)
 
-let ocaml_version = "3.09+dev12 (2004-12-09)";;
+let ocaml_version = "3.09+dev13 (2005-01-04)";;
