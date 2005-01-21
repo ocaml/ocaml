0d18ab145e114557db34e7e471809cb0e6613c40
Damien Doligez <damien.doligez-inria.fr>
2005-01-21 18:30:20 +0000

    ajout support ocamlopt -pack pour Mac OS X
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@6753 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- stdlib/sys.ml
+++ stdlib/sys.ml
@@ -78,4 +78,4 @@ let catch_break on =
 
 (* OCaml version string, must be in the format described in sys.mli. *)
 
-let ocaml_version = "3.09+dev13 (2005-01-04)";;
+let ocaml_version = "3.09+dev14 (2005-01-21)";;
