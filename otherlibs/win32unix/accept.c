e495eab8720ba73b60d47019b26195b6c660415a
Xavier Leroy <xavier.leroy@inria.fr>
2005-02-02 15:40:14 +0000

    MAJ suite changement prototype alloc_sockaddr
    git-svn-id: http://caml.inria.fr/svn/ocaml/version/3.08@6769 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- otherlibs/win32unix/accept.c
+++ otherlibs/win32unix/accept.c
@@ -57,7 +57,7 @@ CAMLprim value unix_accept(sock)
   }
   Begin_roots2 (fd, adr)
     fd = win_alloc_socket(snew);
-    adr = alloc_sockaddr(&addr, addr_len);
+    adr = alloc_sockaddr(&addr, addr_len, snew);
     res = alloc_small(2, 0);
     Field(res, 0) = fd;
     Field(res, 1) = adr;
