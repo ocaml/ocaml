34121ffea0147fdc17eaee3c5be95c7618135a58
Pierre Weis <Pierre.Weis@inria.fr>
2005-03-04 14:55:21 +0000

    Bumping the sub version.
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@6803 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- stdlib/sys.ml
+++ stdlib/sys.ml
@@ -78,4 +78,4 @@ let catch_break on =
 
 (* OCaml version string, must be in the format described in sys.mli. *)
 
-let ocaml_version = "3.09+dev16 (2005-02-16)";;
+let ocaml_version = "3.09+dev17 (2005-03-04)";;
