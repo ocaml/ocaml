f269a3028146b5ef36e533bf0ad5d831b58bd086
Pierre Weis <Pierre.Weis@inria.fr>
2005-07-01 12:39:43 +0000

    Adding a new test for scanf: two concurrent processes communicating via stdin/stdout (this example tests proper treatment of end of file and \n at end of string; more precisely, this test finely tests char lookaheads and char lookahead attempts, since a spurious lookahead attempt may lead to a deadlock between the reader and the writer (in the example the slave and the master)).
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@6945 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

---
+++ test/Moretest/tscanf2_master.ml
@@ -0,0 +1,40 @@
+(* A very simple master:
+   - first launch a slave process,
+   - then repeat a random number of times:
+     + print the string " Ping" on stderr,
+     + send it to the slave,
+     + and wait for its answer "-pong",
+   - finally send the string "stop" to the slave
+     and wait for its answer "OK, bye!"
+     and die.
+   
+   Use the communication module Test_scanf_io.
+   
+   Usage: test_master <slave_name> *)
+
+open Tscanf2_io;;
+
+let slave = Sys.argv.(1);;
+let ic, oc = Unix.open_process slave;;
+let ib = Scanf.Scanning.from_channel ic;;
+let ob = Buffer.create 1024;;
+
+let send_string_ping ob = send_string ob oc " Ping";;
+let send_string_stop ob = send_string ob oc "stop";;
+
+let interact i =
+  Printf.eprintf " Ping"; flush stderr;
+  send_string_ping ob;
+  let s = receive_string ib in
+  if s <> "-pong" then failwith ("Master: unbound string " ^ s);;
+  
+Random.self_init ();
+let n = max (Random.int 8) 1 in
+let rec loop i =
+  if i > 0 then (interact i; loop (i - 1)) in
+loop n;;
+
+send_string_stop ob;
+let ack = receive_string ib in
+if ack = "OK, bye!" then (prerr_endline "Test OK."; exit 0) else
+(prerr_endline "Test Failed!"; exit 2);;
