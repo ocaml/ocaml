047a26f2aad4129e471886beb769bd96869e4077
Luc Maranget <luc.maranget@inria.fr>
2005-09-23 16:38:58 +0000

    oubli
    git-svn-id: http://caml.inria.fr/svn/ocaml/branches/jocamltrunk@7068 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

---
+++ otherlibs/threads/join_hash.ml
@@ -0,0 +1,42 @@
+(***********************************************************************)
+(*                                                                     *)
+(*                           Objective Caml                            *)
+(*                                                                     *)
+(*            Luc Maranget, projet Moscova, INRIA Rocquencourt         *)
+(*                                                                     *)
+(*  Copyright 2005 Institut National de Recherche en Informatique et   *)
+(*  en Automatique.  All rights reserved.  This file is distributed    *)
+(*  under the terms of the Q Public License version 1.0.               *)
+(*                                                                     *)
+(***********************************************************************)
+
+(* $Id$ *)
+
+open Join_misc
+
+type ('a,'b) t =
+  {
+    add : 'a -> 'b -> unit ;
+    find : 'a -> 'b ;
+    get : ('a -> 'b) -> 'a -> 'b ;
+  } 
+
+let create () =
+  let t =  Hashtbl.create 17
+  and c = controler_create () in
+  {
+    add = protect_write c (fun key v -> Hashtbl.add t key v) ;
+    find = protect_read c (fun key -> Hashtbl.find t key) ;
+    get = protect_write c
+      (fun d key ->
+        try Hashtbl.find t key
+        with Not_found ->
+          let r = d key in
+          Hashtbl.add t key r ;
+          r) ;
+  } 
+
+
+let add t key v = t.add key v
+and find t key = t.find key
+and get t d key = t.get d key
---
+++ otherlibs/threads/join_hash.mli
@@ -0,0 +1,23 @@
+(***********************************************************************)
+(*                                                                     *)
+(*                           Objective Caml                            *)
+(*                                                                     *)
+(*            Luc Maranget, projet Moscova, INRIA Rocquencourt         *)
+(*                                                                     *)
+(*  Copyright 2005 Institut National de Recherche en Informatique et   *)
+(*  en Automatique.  All rights reserved.  This file is distributed    *)
+(*  under the terms of the Q Public License version 1.0.               *)
+(*                                                                     *)
+(***********************************************************************)
+
+(* $Id$ *)
+
+type ('a,'b) t
+
+val create : unit -> ('a,'b) t
+
+val add :  ('a,'b) t -> 'a -> 'b -> unit
+
+val find : ('a,'b) t -> 'a -> 'b
+
+val get :  ('a,'b) t -> ('a -> 'b) -> 'a -> 'b
