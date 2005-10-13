58a48bd806051bc7a2e686c99f2798e77547116c
Xavier Leroy <xavier.leroy@inria.fr>
2005-10-13 13:32:06 +0000

    Retour en arriere sur precedente modif (PR#3556), problematique quand toutes les primitives C sont exportees avec 'external'
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@7124 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- bytecomp/bytelink.ml
+++ bytecomp/bytelink.ml
@@ -133,6 +133,7 @@ let scan_file obj_name tolink =
       seek_in ic pos_toc;
       let toc = (input_value ic : library) in
       close_in ic;
+      add_ccobjs toc;
       let required =
         List.fold_right
           (fun compunit reqd ->
@@ -146,7 +147,6 @@ let scan_file obj_name tolink =
             end else
               reqd)
           toc.lib_units [] in
-      if required <> [] || !Clflags.link_everything then add_ccobjs toc;
       Link_archive(file_name, required) :: tolink
     end
     else raise(Error(Not_an_object_file file_name))
