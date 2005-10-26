7ca09b85f07684084129804369689893963b84c9
Damien Doligez <damien.doligez-inria.fr>
2005-10-26 16:40:46 +0000

    demarrage de la version 3.10
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@7179 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- stdlib/sys.ml
+++ stdlib/sys.ml
@@ -78,4 +78,4 @@ let catch_break on =
 
 (* OCaml version string, must be in the format described in sys.mli. *)
 
-let ocaml_version = "3.09+dev36 (2005-10-25)";;
+let ocaml_version = "3.10+dev1 (2005-10-26)";;
