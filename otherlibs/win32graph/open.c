72ae682d4830f5f392dae6657034eb410ac5dc63
Luc Maranget <luc.maranget@inria.fr>
2005-10-27 08:28:52 +0000

    win32graph309
    git-svn-id: http://caml.inria.fr/svn/ocaml/branches/jocamltrunk@7185 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- otherlibs/win32graph/open.c
+++ otherlibs/win32graph/open.c
@@ -301,6 +301,15 @@ CAMLprim value caml_gr_size_y(void)
         return Val_int(grwindow.height);
 }
 
+CAMLprim value caml_gr_resize_window (value vx, value vy)
+{
+  caml_gr_check_open ();
+
+  /* FIXME TODO implement this function... */
+
+  return Val_unit;
+}
+
 CAMLprim value caml_gr_synchronize(void)
 {
         gr_check_open();
@@ -353,4 +362,3 @@ void gr_check_open(void)
 {
   if (!gr_initialized) gr_fail("graphic screen not opened", NULL);
 }
-
