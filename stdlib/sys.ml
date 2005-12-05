6f619adab46e59ce39c76fbb650713843ba67cc8
Damien Doligez <damien.doligez-inria.fr>
2005-12-05 13:37:43 +0000

    bug subtil de Weak.get_copy
    git-svn-id: http://caml.inria.fr/svn/ocaml/version/3.09@7249 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- stdlib/sys.ml
+++ stdlib/sys.ml
@@ -78,4 +78,4 @@ let catch_break on =
 
 (* OCaml version string, must be in the format described in sys.mli. *)
 
-let ocaml_version = "3.09.1+dev4 (2005-12-02)";;
+let ocaml_version = "3.09.1+dev5 (2005-12-05)";;
