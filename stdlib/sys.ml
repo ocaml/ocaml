9d1901feea29c53f74b3fa5b7af950059767e5ab
Damien Doligez <damien.doligez-inria.fr>
2006-03-22 12:41:17 +0000

    test lock
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@7361 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- stdlib/sys.ml
+++ stdlib/sys.ml
@@ -78,4 +78,4 @@ let catch_break on =
 
 (* OCaml version string, must be in the format described in sys.mli. *)
 
-let ocaml_version = "3.10+dev3 (2006-01-24)";;
+let ocaml_version = "3.10+dev4 (2006-03-22)";;
