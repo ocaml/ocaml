c026c688244f6c868c4144b03bb1be3765d6b5d7
Nicolas Pouillard <np@nicolaspouillard.fr>
2006-07-17 22:32:39 +0000

    More tests for constructors in patterns
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@7518 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- camlp4/test/fixtures/bug-camlp4o-constr-arity.ml
+++ camlp4/test/fixtures/bug-camlp4o-constr-arity.ml
@@ -7,4 +7,26 @@ fun None None None -> ();;
 
 fun (Some None) None None -> ();;
 
-fun Some None None None -> ();;
+let Some a = Some 42;;
+let Some a :: y = [Some 42];;
+let Some a, b = Some 42, 43;;
+let (Some a), b = Some 42, 43;;
+let Some a as b = let _ = b = 42 in Some 42;;
+(* let Some (a as b) = let _ = b = None in Some 42;; *)
+(* let Some (a as b) = let _ = b = 42 in Some 42;; *)
+(* let (Some a) as b = let _ = b = 42 in Some 42;; *)
+(* let (Some a) as b = let _ = b = None in Some 42;; *)
+let Some a | Some a = Some 42;;
+(* let Some 'a' .. 'b' = Some 'b';; *)
+
+let rec f x y = ();;
+
+fun x y -> ();;
+
+fun x, y -> ()
+
+function x, y -> ();;
+
+let rec next line pos0 = () in ();;
+
+(* fun Some None None None -> ();; *)
