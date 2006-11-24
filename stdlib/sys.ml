f4702f8208352a150221352f1c2b87a44e9e6fb1
Damien Doligez <damien.doligez-inria.fr>
2006-11-24 14:40:11 +0000

    PR#4167 ajout de const dans fail.h
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@7758 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- stdlib/sys.ml
+++ stdlib/sys.ml
@@ -78,4 +78,4 @@ let catch_break on =
 
 (* OCaml version string, must be in the format described in sys.mli. *)
 
-let ocaml_version = "3.10+dev18 (2006-11-17)";;
+let ocaml_version = "3.10+dev19 (2006-11-24)";;
