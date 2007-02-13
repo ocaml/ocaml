b16f2d0c692eafee774531344c3aff57b3e770f8
Jacques Garrigue <garrigue at math.nagoya-u.ac.jp>
2007-02-13 01:11:10 +0000

    build with bourne shell
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@7863 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- build/partial-boot.sh
+++ build/partial-boot.sh
@@ -4,9 +4,9 @@ cd `dirname $0`/..
 export OCAMLBUILD_PARTIAL="true"
 mkdir -p _build
 cp -rf boot _build/
-cp parsing/location.ml{,i} camlp4/build
-cp parsing/linenum.ml{l,i} camlp4/build
-cp utils/terminfo.ml{,i} camlp4/build
+cp parsing/location.ml parsing/location.mli camlp4/build
+cp parsing/linenum.mll parsing/linenum.mli camlp4/build
+cp utils/terminfo.ml utils/terminfo.mli camlp4/build
 ./build/mkconfig.sh
 ./build/mkmyocamlbuild_config.sh
 ./build/boot.sh
