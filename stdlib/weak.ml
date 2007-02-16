62fc7043b61cdaac208de799f257ca886d264e4d
Damien Doligez <damien.doligez-inria.fr>
2007-02-16 16:05:36 +0000

    bug de performance dans stdlib/weak.ml
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@7879 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- stdlib/weak.ml
+++ stdlib/weak.ml
@@ -145,7 +145,7 @@ module Make (H : Hashtbl.HashedType) : (S with type data = H.t) = struct
       fold (fun d () -> add newt d) t ();
    (* assert Array.length newt.table = newlen; *)
       t.table <- newt.table;
-      t.limit <- t.limit + 2;
+   (* t.limit <- t.limit + 2; -- performance bug *)
     end
 
   and add_aux t d index =
