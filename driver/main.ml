e3f20ca854921345314d79d77fd9c3d9ff26e5ce
Luc Maranget <luc.maranget@inria.fr>
2007-03-26 20:28:56 +0000

    proper -nojoin option that clear the -thread flag
    git-svn-id: http://caml.inria.fr/svn/ocaml/branches/jocamltrunk@8129 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- driver/main.ml
+++ driver/main.ml
@@ -118,7 +118,7 @@ module Options = Main_args.Make_options (struct
   let _pp s = preprocessor := Some s
   let _principal = set principal
   let _rectypes = set recursive_types
-  let _nojoin = set nojoin
+  let _nojoin () = nojoin := true ;  use_threads := false
   let _thread = set use_threads
   let _vmthread = set use_vmthreads
   let _nothread = unset use_threads
