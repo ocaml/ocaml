29fd422b6b5874ac08d54c95eaa0b78ffeb367df
Nicolas Pouillard <np@nicolaspouillard.fr>
2007-05-14 13:59:36 +0000

    [build] Ease the bootstrap, do not use the stdlib when cold booting
    git-svn-id: http://caml.inria.fr/svn/ocaml/version/3.10@8230 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- build/boot.sh
+++ build/boot.sh
@@ -2,12 +2,22 @@
 # $Id$
 cd `dirname $0`/..
 set -ex
-./boot/ocamlrun boot/myocamlbuild.boot boot/stdlib.cma boot/std_exit.cmo
-boot/ocamlrun boot/myocamlbuild.boot -log _boot_log1 \
+TAGLINE='true: -use_stdlib'
+./boot/ocamlrun boot/myocamlbuild.boot \
+  -tag-line "$TAG_LINE" \
+  boot/stdlib.cma boot/std_exit.cmo
+
+boot/ocamlrun boot/myocamlbuild.boot \
+  -tag-line "$TAG_LINE" -log _boot_log1 \
   ocamlbuild/ocamlbuildlightlib.cma ocamlbuild/ocamlbuildlight.byte
+
 rm -f _build/myocamlbuild
+
 boot/ocamlrun boot/myocamlbuild.boot \
   -just-plugin -install-lib-dir _build/ocamlbuild -byte-plugin
+
 cp _build/myocamlbuild boot/myocamlbuild
-./boot/ocamlrun boot/myocamlbuild $@ -log _boot_log2 \
-  boot/camlheader ocamlc
+
+./boot/ocamlrun boot/myocamlbuild \
+  -tag-line "$TAG_LINE" \
+  $@ -log _boot_log2 boot/camlheader ocamlc
