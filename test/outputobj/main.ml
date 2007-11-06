2cc37daa2ea24ec9d25085539d887aaabf4112ce
Alain Frisch <alain@frisch.fr>
2007-11-06 16:27:00 +0000

    Test outputobj.
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@8480 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

---
+++ test/outputobj/main.ml
@@ -0,0 +1,23 @@
+let load s =
+  Printf.printf "Loading %s\n%!" s;
+  try
+    Dynlink.loadfile s
+  with Dynlink.Error e ->
+    print_endline (Dynlink.error_message e)
+
+let () =
+  print_endline "Main is running.";
+  Dynlink.init ();
+  Dynlink.allow_unsafe_modules true;
+  let s1,s2 =
+    if Dynlink.is_native then
+      "../../otherlibs/bigarray/bigarray.cmxs",
+      "plugin.cmxs"
+    else
+      "../../otherlibs/bigarray/bigarray.cma",
+      "plugin.cmo"
+  in
+  load s1; 
+  load s2;
+  print_endline "OK."
+
