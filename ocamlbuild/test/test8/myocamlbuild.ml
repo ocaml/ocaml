85e9abd87ea224a94ee7444675878a1d6a27cfbc
Nicolas Pouillard <np@nicolaspouillard.fr>
2007-11-21 21:03:52 +0000

    [ocamlbuild] [ADDED] Test and examples of %(var:pattern).
    git-svn-id: http://caml.inria.fr/svn/ocaml/version/3.10@8576 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- ocamlbuild/test/test8/myocamlbuild.ml
+++ ocamlbuild/test/test8/myocamlbuild.ml
@@ -1,9 +1,18 @@
 open Ocamlbuild_pack;;
 open Ocamlbuild_plugin;;
 let version = "0.1";;
-file_rule "myconfig.ml"
-  ~prod:"myconfig.ml"
-  ~cache:(fun _ _ -> version)
-  begin fun _ oc ->
-    Printf.fprintf oc "let version = %S;;\n%!" version
-  end;;
+dispatch begin function
+  | After_rules ->
+      file_rule "myconfig.ml"
+        ~prod:"myconfig.ml"
+        ~cache:(fun _ _ -> version)
+        begin fun _ oc ->
+          Printf.fprintf oc "let version = %S;;\n%!" version
+        end;
+
+      copy_rule "copy byte-code executables" "%(path).byte" "%(path:not <**/*.*>)";
+      copy_rule "copy native executables" "%(path).native" "%(path:not <**/*.*>).opt";
+      copy_rule "copy binaries to bin" "%(basename).%(extension)"
+                                       "bin/%(basename).%(extension:<{byte,native}>)";
+  | _ -> ()
+end
