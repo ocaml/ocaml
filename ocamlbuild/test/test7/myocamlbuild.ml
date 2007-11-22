37ad746e3f46d24fe5ed373bdc698d9bb06a47a6
Nicolas Pouillard <np@nicolaspouillard.fr>
2007-11-22 18:51:13 +0000

    [ocamlbuild] [test] Wrap decls in a dispatch call.
    git-svn-id: http://caml.inria.fr/svn/ocaml/version/3.10@8608 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- ocamlbuild/test/test7/myocamlbuild.ml
+++ ocamlbuild/test/test7/myocamlbuild.ml
@@ -1,3 +1,7 @@
 open Ocamlbuild_plugin;;
-use_lib "main" "bbcc";;
-dep ["ocaml"; "link"; "byte"; "my_cool_plugin"] ["cool_plugin.cmo"];;
+dispatch begin function
+| After_rules ->
+    use_lib "main" "bbcc";
+    dep ["ocaml"; "link"; "byte"; "my_cool_plugin"] ["cool_plugin.cmo"];
+| _ -> ()
+end
