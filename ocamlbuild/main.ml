4be12009bfda8ec8343c974478da8a168bcbe14e
Nicolas Pouillard <np@nicolaspouillard.fr>
2007-11-22 18:31:22 +0000

    [ocamlbuild] Give Main.Exit_codes its own file.
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@8599 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- ocamlbuild/main.ml
+++ ocamlbuild/main.ml
@@ -209,25 +209,6 @@ let proceed () =
           (sbprintf "@[<2>Circular dependencies: %S already seen in@ %a@]@." p pp_l seen))
 ;;
 
-module Exit_codes =
-  struct
-    let rc_ok                  = 0
-    let rc_usage               = 1
-    let rc_failure             = 2
-    let rc_invalid_argument    = 3
-    let rc_system_error        = 4
-    let rc_hygiene             = 1
-    let rc_circularity         = 5
-    let rc_solver_failed       = 6
-    let rc_ocamldep_error      = 7
-    let rc_lexing_error        = 8
-    let rc_build_error         = 9
-    let rc_executor_reserved_1 = 10 (* Redefined in Executor *)
-    let rc_executor_reserved_2 = 11
-    let rc_executor_reserved_3 = 12
-    let rc_executor_reserved_4 = 13
-  end
-
 open Exit_codes;;
 
 let main () =
