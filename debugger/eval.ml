39469b78111b5a7db1bbecfc10f59b31048b33c3
Pierre Weis <Pierre.Weis@inria.fr>
2007-11-28 22:32:14 +0000

    Le debugger accede au type manifeste des abbreviations privees pour l'evaluation
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@8686 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- debugger/eval.ml
+++ debugger/eval.ml
@@ -101,7 +101,7 @@ let rec expression event env = function
       end
   | E_item(arg, n) ->
       let (v, ty) = expression event env arg in
-      begin match (Ctype.repr(Ctype.expand_head env ty)).desc with
+      begin match (Ctype.repr(Ctype.expand_head_opt env ty)).desc with
         Ttuple ty_list ->
           if n < 1 || n > List.length ty_list
           then raise(Error(Tuple_index(ty, List.length ty_list, n)))
@@ -131,7 +131,7 @@ let rec expression event env = function
       end
   | E_field(arg, lbl) ->
       let (v, ty) = expression event env arg in
-      begin match (Ctype.repr(Ctype.expand_head env ty)).desc with
+      begin match (Ctype.repr(Ctype.expand_head_opt env ty)).desc with
         Tconstr(path, args, _) ->
           let tydesc = Env.find_type path env in
           begin match tydesc.type_kind with
