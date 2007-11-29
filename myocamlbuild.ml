9856ad3793cf600f5c44d78c1ad0bf6034ba0b95
Nicolas Pouillard <np@nicolaspouillard.fr>
2007-11-29 09:40:38 +0000

    [build] More dependencies for camlp4 binaries rules.
    git-svn-id: http://caml.inria.fr/svn/ocaml/version/3.10@8688 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -759,14 +759,18 @@ let mk_camlp4_bin name ?unix:(link_unix=true) modules =
   let name = "camlp4"/name in
   let byte = name-.-"byte" in
   let native = name-.-"native" in
-  let unix_cma, unix_cmxa, include_unix =
-    if link_unix then A"unix.cma", A"unix.cmxa", S[A"-I"; P unix_dir] else N,N,N in
+  let unix_cma, dep_unix_byte, unix_cmxa, dep_unix_native, include_unix =
+    if link_unix
+    then A"unix.cma", [unix_dir/"unix.cma"],
+         A"unix.cmxa", [unix_dir/"unix.cmxa"; unix_dir/"unix"-.-C.a],
+         S[A"-I"; P unix_dir]
+    else N,[],N,[],N in
   let deps = special_modules @ modules @ [camlp4_bin] in
   let cmos = add_extensions ["cmo"] deps in
   let cmxs = add_extensions ["cmx"] deps in
   let objs = add_extensions [C.o] deps in
   rule byte
-    ~deps:(camlp4lib_cma::cmos)
+    ~deps:(camlp4lib_cma::cmos @ dep_unix_byte)
     ~prod:(add_exe byte)
     ~insert:(`before "ocaml: cmo* -> byte")
     begin fun _ _ ->
@@ -774,7 +778,7 @@ let mk_camlp4_bin name ?unix:(link_unix=true) modules =
             P camlp4lib_cma; A"-linkall"; atomize cmos; A"-o"; Px (add_exe byte)])
     end;
   rule native
-    ~deps:(camlp4lib_cmxa :: camlp4lib_lib :: (cmxs @ objs))
+    ~deps:(camlp4lib_cmxa :: camlp4lib_lib :: (cmxs @ objs @ dep_unix_native))
     ~prod:(add_exe native)
     ~insert:(`before "ocaml: cmx* & o* -> native")
     begin fun _ _ ->
