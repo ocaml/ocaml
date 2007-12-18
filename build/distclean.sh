249889d1ff397cdf0f0e88b814a4e30abdc4b080
Nicolas Pouillard <np@nicolaspouillard.fr>
2007-12-18 09:03:14 +0000

    build,clean: distclean.sh also clean boot/ocamlyacc and boot/*.bak.
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@8722 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- build/distclean.sh
+++ build/distclean.sh
@@ -9,6 +9,7 @@ rm -rf _build
 rm -f boot/ocamlrun boot/ocamlrun.exe boot/camlheader \
       boot/myocamlbuild boot/myocamlbuild.native boot/myocamlbuild.native.exe \
       myocamlbuild_config.ml config/config.sh config/Makefile \
+      boot/ocamlyacc tools/cvt_emit.bak tools/*.bak \
       config/s.h config/m.h boot/*.cm* _log _*_log*
 
 # from partial boot
