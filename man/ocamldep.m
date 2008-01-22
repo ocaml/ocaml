478735910c8cd247a800037a58871363fcf7f8a1
Damien Doligez <damien.doligez-inria.fr>
2008-01-22 13:05:00 +0000

    PR#1217: typo
    git-svn-id: http://caml.inria.fr/svn/ocaml/version/3.10@8785 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- man/ocamldep.m
+++ man/ocamldep.m
@@ -50,7 +50,7 @@ compilation unit Bar, a dependency on that unit's interface
 bar.cmi is generated only if the source for bar is found in the
 current directory or in one of the directories specified with 
 .BR -I .
-Otherwise, Bar is assumed to be a module form the standard library,
+Otherwise, Bar is assumed to be a module from the standard library,
 and no dependencies are generated. For programs that span multiple
 directories, it is recommended to pass 
 .BR ocamldep (1)
