50af4a8c98541bd9e87776a858dd892915dc6598
Xavier Leroy <xavier.leroy@inria.fr>
2008-07-31 12:09:18 +0000

    Render unto Caesar...
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@8961 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- otherlibs/win32unix/winlist.h
+++ otherlibs/win32unix/winlist.h
@@ -2,9 +2,9 @@
 /*                                                                     */
 /*                           Objective Caml                            */
 /*                                                                     */
-/*            Xavier Leroy, projet Cristal, INRIA Rocquencourt         */
+/*  Contributed by Sylvain Le Gall for Lexifi                          */
 /*                                                                     */
-/*  Copyright 1996 Institut National de Recherche en Informatique et   */
+/*  Copyright 2008 Institut National de Recherche en Informatique et   */
 /*  en Automatique.  All rights reserved.  This file is distributed    */
 /*  under the terms of the GNU Library General Public License, with    */
 /*  the special exception on linking described in file ../../LICENSE.  */
