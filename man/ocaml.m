7034f033115351466c0be3eef256eafd5f4a8073
Damien Doligez <damien.doligez-inria.fr>
2008-09-11 16:16:27 +0000

    PR#3915 update man pages
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@9023 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- man/ocaml.m
+++ man/ocaml.m
@@ -7,10 +7,7 @@ ocaml \- The Objective Caml interactive toplevel
 .SH SYNOPSIS
 .B ocaml
 [
-.B \-unsafe
-]
-[
-.BI \-I \ lib-dir
+.I options
 ]
 [
 .I object-files
@@ -32,7 +29,7 @@ system prints a # (sharp) prompt before reading each phrase.
 A toplevel phrase can span several lines. It is terminated by ;; (a
 double-semicolon). The syntax of toplevel phrases is as follows.
 
-The toplevel system is started by the command 
+The toplevel system is started by the command
 .BR ocaml (1).
 Phrases are read on standard input, results are printed on standard
 output, errors on standard error. End-of-file on standard input
@@ -58,34 +55,111 @@ exits after the execution of the last phrase.
 
 The following command-line options are recognized by
 .BR ocaml (1).
-
 .TP
-.BI \-I \ directory
+.BI -I \ directory
 Add the given directory to the list of directories searched for
 source and compiled files. By default, the current directory is
 searched first, then the standard library directory. Directories added
-with 
+with
 .B \-I
 are searched after the current directory, in the order in which they
 were given on the command line, but before the standard library
 directory.
-
+.IP
+If the given directory starts with
+.BR + ,
+it is taken relative to the
+standard library directory. For instance,
+.B \-I +labltk
+adds the subdirectory
+.B labltk
+of the standard library to the search path.
+.IP
+Directories can also be added to the search path once the toplevel
+is running with the
+.B #directory
+directive.
+.TP
+.BI \-init \ file
+Load the given file instead of the default initialization file.
+The default file is
+.B .ocamlinit
+in the current directory if it exists, otherwise
+.B .ocamlinit
+in the user's home directory.
+.TP
+.B \-labels
+Labels are not ignored in types, labels may be used in applications,
+and labelled parameters can be given in any order.  This is the default.
+.TP
+.B \-noassert
+Do not compile assertion checks.  Note that the special form
+.B assert false
+is always compiled because it is typed specially.
+.TP
+.B \-nolabels
+Ignore non-optional labels in types. Labels cannot be used in
+applications, and parameter order becomes strict.
+.TP
+.B \-noprompt
+Do not display any prompt when waiting for input.
+.TP
+.B \-nostdlib
+Do not include the standard library directory in the list of
+directories searched for source and compiled files.
+.TP
+.B \-principal
+Check information path during type-checking, to make sure that all
+types are derived in a principal way.  When using labelled arguments
+and/or polymorphic methods, this flag is required to ensure future
+versions of the compiler will be able to infer types correctly, even
+if internal algorithms change.
+All programs accepted in
+.B \-principal
+mode are also accepted in the
+default mode with equivalent types, but different binary signatures,
+and this may slow down type checking; yet it is a good idea to
+use it once before publishing source code.
+.TP
+.B \-rectypes
+Allow arbitrary recursive types during type-checking.  By default,
+only recursive types where the recursion goes through an object type
+are supported.
 .TP
 .B \-unsafe
-Turn bound checking off on array and string accesses (the v.(i)
-and s.[i] constructs). Programs compiled with 
+Turn bound checking off on array and string accesses (the
+.BR v.(i) and s.[i]
+constructs). Programs compiled with
 .B \-unsafe
 are therefore slightly faster, but unsafe: anything can happen if the program
 accesses an array or string outside of its bounds.
+.TP
+.B \-version
+Print version string and exit.
+.TP
+.BI \-w \ warning-list
+Enable or disable warnings according to the argument
+.IR warning-list .
+See
+.BR ocamlc (1)
+for the syntax of the argument.
+.TP
+.BI \-warn-error \ warning-list
+Treat as errors the warnings enabled by the argument
+.IR warning-list .
+See
+.BR ocamlc (1)
+for the syntax of the argument.
+.TP
+.BR \-help \ or \ \-\-help
+Display a usage summary and exit.
 
 .SH ENVIRONMENT VARIABLES
-
 .TP
 .B LC_CTYPE
 If set to iso_8859_1, accented characters (from the
 ISO Latin-1 character set) in string and character literals are
 printed as is; otherwise, they are printed as decimal escape sequences.
-
 .TP
 .B TERM
 When printing error messages, the toplevel system
@@ -94,8 +168,7 @@ consults the TERM variable to determines the type of output terminal
 and look up its capabilities in the terminal database.
 
 .SH SEE ALSO
-.BR ocamlc (1).
+.BR ocamlc (1), \ ocamlopt (1), \ ocamlrun (1).
 .br
-.I The Objective Caml user's manual,
+.IR The\ Objective\ Caml\ user's\ manual ,
 chapter "The toplevel system".
-
