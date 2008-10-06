a76ddd0f1285ed2ac7b761bb4eed17d866196d1a
Damien Doligez <damien.doligez-inria.fr>
2008-10-06 13:33:21 +0000

    doc detail
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@9073 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- stdlib/pervasives.mli
+++ stdlib/pervasives.mli
@@ -49,7 +49,7 @@ external ( = ) : 'a -> 'a -> bool = "%equal"
    if and only if their current contents are structurally equal,
    even if the two mutable objects are not the same physical object.
    Equality between functional values raises [Invalid_argument].
-   Equality between cyclic data structures does not terminate. *)
+   Equality between cyclic data structures may not terminate. *)
 
 external ( <> ) : 'a -> 'a -> bool = "%notequal"
 (** Negation of {!Pervasives.(=)}. *)
