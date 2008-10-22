f007e2b9176a6cb791f8b93d3e16f02fd78ee9c5
Nicolas Pouillard <np@nicolaspouillard.fr>
2008-10-22 11:23:57 +0000

    ocamlbuild: Add a 'annot' tag for the -annot option.
    git-svn-id: http://caml.inria.fr/svn/ocaml/version/3.11@9096 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- ocamlbuild/ocaml_specific.ml
+++ ocamlbuild/ocaml_specific.ml
@@ -404,6 +404,7 @@ flag ["ocaml"; "debug"; "pack"; "native"] (A "-g");;
 flag ["ocaml"; "link"; "native"; "output_obj"] (A"-output-obj");;
 flag ["ocaml"; "link"; "byte"; "output_obj"] (A"-output-obj");;
 flag ["ocaml"; "dtypes"; "compile"] (A "-dtypes");;
+flag ["ocaml"; "annot"; "compile"] (A "-annot");;
 flag ["ocaml"; "rectypes"; "compile"] (A "-rectypes");;
 flag ["ocaml"; "rectypes"; "infer_interface"] (A "-rectypes");;
 flag ["ocaml"; "linkall"; "link"] (A "-linkall");;
