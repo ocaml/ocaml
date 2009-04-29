94ebe9dfad8ff4eef7227ad9f7d8451db91d40b3
Pierre Weis <Pierre.Weis@inria.fr>
2009-04-29 08:31:20 +0000

    Banneer.
    git-svn-id: http://caml.inria.fr/svn/ocaml/version/3.11@9242 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- test/Moretest/tscanf.ml
+++ test/Moretest/tscanf.ml
@@ -1,8 +1,20 @@
+(*************************************************************************)
+(*                                                                       *)
+(*                            Objective Caml                             *)
+(*                                                                       *)
+(*            Pierre Weis, projet Cristal, INRIA Rocquencourt            *)
+(*                                                                       *)
+(*   Copyright 2002 Institut National de Recherche en Informatique et    *)
+(*   en Automatique.  All rights reserved.  This file is distributed     *)
+(*   under the terms of the Q Public License version 1.0.                *)
+(*                                                                       *)
+(*************************************************************************)
+
 (* $Id$
 
-A testbed file for module Scanf.
+A testbed file for the module Scanf.
 
- *)
+*)
 
 open Testing;;
 
