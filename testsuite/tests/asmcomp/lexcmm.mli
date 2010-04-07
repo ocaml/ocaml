ead8077daec521ef4d82697c10450087a1e74087
Xavier Clerc <xavier.clerc@inria.fr>
2010-04-07 10:15:30 +0000

    Tests moved to 'asmcomp'
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@10236 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

---
+++ testsuite/tests/asmcomp/lexcmm.mli
@@ -0,0 +1,24 @@
+(***********************************************************************)
+(*                                                                     *)
+(*                           Objective Caml                            *)
+(*                                                                     *)
+(*            Xavier Leroy, projet Cristal, INRIA Rocquencourt         *)
+(*                                                                     *)
+(*  Copyright 1996 Institut National de Recherche en Informatique et   *)
+(*  en Automatique.  All rights reserved.  This file is distributed    *)
+(*  under the terms of the Q Public License version 1.0.               *)
+(*                                                                     *)
+(***********************************************************************)
+
+(* $Id: lexcmm.mli 2553 1999-11-17 18:59:06Z xleroy $ *)
+
+val token: Lexing.lexbuf -> Parsecmm.token
+
+type error =
+    Illegal_character
+  | Unterminated_comment
+  | Unterminated_string
+
+exception Error of error
+
+val report_error: Lexing.lexbuf -> error -> unit
