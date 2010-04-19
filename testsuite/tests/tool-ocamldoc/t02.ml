be8304eeaa38576a3268e321e6d7578bf22450e9
Maxence Guesdon <maxence.guesdon@inria.fr>
2010-04-19 16:32:57 +0000

    complete test with 'with type ... :=' structure
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@10278 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- testsuite/tests/tool-ocamldoc/t02.ml
+++ testsuite/tests/tool-ocamldoc/t02.ml
@@ -1,2 +1,4 @@
-module Foo = struct type t = int let x = 1 end;;
+module Foo = struct type u type t = int let x = 1 end;;
 module type TFoo = module type of Foo;;
+
+module type TBar = TFoo with type u := float;;
