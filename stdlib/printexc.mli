e178c1ab624d51c52af30d474a30227f783b0585
Alain Frisch <alain@frisch.fr>
2010-04-19 12:25:46 +0000

    Fix #5003: document the fact that expressions raised by exception printers are just ignored; custom printers are tried before hard-coded ones.
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@10272 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- stdlib/printexc.mli
+++ stdlib/printexc.mli
@@ -15,11 +15,11 @@
 
 (** Facilities for printing exceptions. *)
 
-val to_string : exn -> string
+val to_string: exn -> string
 (** [Printexc.to_string e] returns a string representation of
    the exception [e]. *)
 
-val print : ('a -> 'b) -> 'a -> 'b
+val print: ('a -> 'b) -> 'a -> 'b
 (** [Printexc.print fn x] applies [fn] to [x] and returns the result.
    If the evaluation of [fn x] raises any exception, the
    name of the exception is printed on standard error output,
@@ -27,7 +27,7 @@ val print : ('a -> 'b) -> 'a -> 'b
    The typical use is to catch and report exceptions that
    escape a function application. *)
 
-val catch : ('a -> 'b) -> 'a -> 'b
+val catch: ('a -> 'b) -> 'a -> 'b
 (** [Printexc.catch fn x] is similar to {!Printexc.print}, but
    aborts the program with exit code 2 after printing the
    uncaught exception.  This function is deprecated: the runtime
@@ -58,11 +58,13 @@ val backtrace_status: unit -> bool
 (** [Printexc.backtrace_status()] returns [true] if exception
     backtraces are currently recorded, [false] if not. *)
 
-val register_printer : (exn -> string option) -> unit
-(** [Printexc.register_printer fn] registers [fn] as an exception printer.
-    The printer should return [None] if it does not know how to convert
-    the passed exception, and [Some s] with [s] the resulting string if
-    it can convert the passed exception.
+val register_printer: (exn -> string option) -> unit
+(** [Printexc.register_printer fn] registers [fn] as an exception
+    printer.  The printer should return [None] or raise an exception
+    if it does not know how to convert the passed exception, and [Some
+    s] with [s] the resulting string if it can convert the passed
+    exception. Exceptions raised by the printer are ignored.
+
     When converting an exception into a string, the printers will be invoked
     in the reverse order of their registrations, until a printer returns
     a [Some s] value (if no such printer exists, the runtime will use a
