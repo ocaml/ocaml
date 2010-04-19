0f44ce9898dd927634b90f90d0d158a44716f4ae
Maxence Guesdon <maxence.guesdon@inria.fr>
2010-04-19 16:48:25 +0000

    update test
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@10280 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- testsuite/tests/tool-ocamldoc/t02.ml
+++ testsuite/tests/tool-ocamldoc/t02.ml
@@ -2,3 +2,8 @@ module Foo = struct type u type t = int let x = 1 end;;
 module type TFoo = module type of Foo;;
 
 module type TBar = TFoo with type u := float;;
+
+module type Gee =
+  sig
+    module M : module type of Foo
+  end
