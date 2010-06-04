44209a9e66bda3b5490e9e4763a52266b789ed42
Luc Maranget <luc.maranget@inria.fr>
2010-06-04 14:36:46 +0000

    +
    git-svn-id: http://caml.inria.fr/svn/ocaml/branches/jocamltrunk@10497 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- camlp4/ocaml_src/tools/ocamlc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -e
-if test "`basename $OTOP`" != "ocaml_stuff"; then
-    COMM=$OTOP/ocamlcomp.sh
-else
-    COMM=ocamlc$OPT
-fi
-echo $COMM $*
-$COMM $*
--- camlp4/ocaml_src/tools/ocamlopt.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -e
-if test "`basename $OTOP`" != "ocaml_stuff"; then
-    COMM=$OTOP/ocamlcompopt.sh
-else
-    COMM=ocamlopt$OPT
-fi
-echo $COMM $*
-$COMM $*
