3b26499f0edd5816ceb39d0054abc40fc2840bb7
Luc Maranget <luc.maranget@inria.fr>
2010-07-26 09:02:43 +0000

    Merge 3.12@10610 -> 3.12/@10632 (rc1)
    git-svn-id: http://caml.inria.fr/svn/ocaml/branches/jo312@10634 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- typing/typecore.ml
+++ typing/typecore.ml
@@ -864,8 +864,8 @@ let rec is_nonexpansive exp =
   | Texp_sequence (e1, e2) -> is_nonexpansive e2  (* PR#4354 *)
   | Texp_new (_, cl_decl) when Ctype.class_type_arity cl_decl.cty_type > 0 ->
       true
-  | Texp_def (_,e) -> is_nonexpansive e
-  | Texp_loc (_,e) -> is_nonexpansive e
+  | Texp_def (_,e) 
+  | Texp_loc (_,e) -> false (* Frankly, I do not know *)
   (* Note: nonexpansive only means no _observable_ side effects *)
   | Texp_lazy e -> is_nonexpansive e
   | Texp_object ({cl_field=fields}, {cty_vars=vars}, _) ->
@@ -880,7 +880,35 @@ let rec is_nonexpansive exp =
       Vars.fold (fun _ (mut,_,_) b -> decr count; b && mut = Immutable)
         vars true &&
       !count = 0
+  | Texp_pack mexp ->
+      is_nonexpansive_mod mexp
   | _ -> false
+
+and is_nonexpansive_mod mexp =
+  match mexp.mod_desc with
+  | Tmod_ident _ -> true
+  | Tmod_functor _ -> true
+  | Tmod_unpack (e, _) -> is_nonexpansive e
+  | Tmod_constraint (m, _, _) -> is_nonexpansive_mod m
+  | Tmod_structure items ->
+      List.for_all
+        (function
+          | Tstr_eval _ | Tstr_primitive _ | Tstr_type _ | Tstr_modtype _
+          | Tstr_open _ | Tstr_cltype _ | Tstr_exn_rebind _ -> true
+          | Tstr_value (_, pat_exp_list) ->
+              List.for_all (fun (_, exp) -> is_nonexpansive exp) pat_exp_list
+          | Tstr_module (_, m) | Tstr_include (m, _) -> is_nonexpansive_mod m
+          | Tstr_recmodule id_mod_list ->
+              List.for_all (fun (_, m) -> is_nonexpansive_mod m) id_mod_list
+          | Tstr_exception _ -> false (* true would be unsound *)
+          | Tstr_class _ -> false (* could be more precise *)
+(*>JOCAML*)
+(* Err on the safe side... *)
+          | Tstr_exn_global (_, _)|Tstr_loc _|Tstr_def _ -> false
+(*<JOCAML*)
+        )
+        items
+  | Tmod_apply _ -> false
 
 and is_nonexpansive_opt = function
     None -> true
