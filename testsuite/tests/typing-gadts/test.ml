f9f3ae5bc749dd9e774bf2b8a5120d91f115177a
Jacques Le Normand <rathereasy@gmail.com>
2010-10-22 04:27:14 +0000

    added test file for gadts for the testsuite
    git-svn-id: http://caml.inria.fr/svn/ocaml/branches/gadts@10739 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

---
+++ testsuite/tests/typing-gadts/test.ml
@@ -0,0 +1,89 @@
+module Exp = 
+  struct
+
+    type _ t = 
+      | IntLit : int -> int t
+      | BoolLit : bool -> bool t
+      | Pair : 'a t * 'b t -> ('a * 'b) t
+      | App : ('a -> 'b) t * 'a t -> 'b t
+      | Abs : ('a -> 'b) -> ('a -> 'b) t 
+
+
+    let rec eval : type s . s t -> s = 
+      function
+	| IntLit x -> x
+	| BoolLit y -> y
+	| Pair (x,y) -> (eval x,eval y)
+	| App (f,a) ->
+	    (eval f) (eval a)
+	| Abs f -> f 
+  end
+
+module List = 
+  struct
+    type zero
+    type _ t = 
+      | Nil : zero t
+      | Cons : 'a * 'b t -> ('a * 'b) t
+    let head =
+      function
+	| Cons (a,b) -> a
+    let tail =
+      function
+	| Cons (a,b) -> b
+    let rec length : type a . a t -> int = 
+      function
+	| Nil -> 0
+	| Cons (a,b) -> length b
+  end
+
+    
+
+module Nonexhaustive = 
+  struct
+    type 'a u = 
+      | C1 : int -> int u 
+      | C2 : bool -> bool u
+	    
+    type 'a v = 
+      | C1 : int -> int v
+
+    let unexhaustive : type s . s u -> s = 
+      function
+	| C2 x -> x
+
+
+    module M : sig type t type u end = 
+      struct
+        type t = int
+        type u = bool
+      end	    
+    type 'a t = 
+      | Foo : M.t -> M.t t 
+      | Bar : M.u -> M.u t
+    let same_type : type s . s t * s t -> bool  =
+      function
+	| Foo _ , Foo _ -> true
+	| Bar _, Bar _ -> true
+  end
+
+module Exhaustive = 
+  struct
+    type t = int
+    type u = bool
+    type 'a v = 
+      | Foo : t -> t v 
+      | Bar : u -> u v
+
+    let same_type : type s . s v * s v -> bool  =
+      function
+	| Foo _ , Foo _ -> true
+	| Bar _, Bar _ -> true    
+  end
+
+module Existential_escape = 
+  struct
+    type _ t = C : int -> int t
+    type u = D : 'a t -> u
+    let eval (D x) = x
+  end
