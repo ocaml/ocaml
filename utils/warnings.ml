451b491a8a7876245fb983d23e4d89f1d89fd3a6
Damien Doligez <damien.doligez-inria.fr>
2011-01-06 14:24:26 +0000

    warning 28 activated by default
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@10923 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- utils/warnings.ml
+++ utils/warnings.ml
@@ -186,7 +186,7 @@ let parse_opt flags s =
 let parse_options errflag s = parse_opt (if errflag then error else active) s;;
 
 (* If you change these, don't forget to change them in man/ocamlc.m *)
-let defaults_w = "+a-4-6-7-9-27..29";;
+let defaults_w = "+a-4-6-7-9-27-29";;
 let defaults_warn_error = "-a";;
 
 let () = parse_options false defaults_w;;
