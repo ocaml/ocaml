26cbb7a36a01b60497b0bec3be39954dd2ef93bc
Xavier Clerc <xavier.clerc@inria.fr>
2011-02-10 15:32:56 +0000

    PR#5158: added access to current camlp4 parsers and printers
    git-svn-id: http://caml.inria.fr/svn/ocaml/version/3.12@10947 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- camlp4/Camlp4/Register.mli
+++ camlp4/Camlp4/Register.mli
@@ -40,6 +40,7 @@ type parser_fun 'a =
 value register_str_item_parser : parser_fun PreCast.Ast.str_item -> unit;
 value register_sig_item_parser : parser_fun PreCast.Ast.sig_item -> unit;
 value register_parser : parser_fun PreCast.Ast.str_item -> parser_fun PreCast.Ast.sig_item -> unit;
+value current_parser : unit -> (parser_fun PreCast.Ast.str_item * parser_fun PreCast.Ast.sig_item);
 
 module Parser
   (Id : Sig.Id) (Maker : functor (Ast : Sig.Ast) -> (Sig.Parser Ast).S) : sig end;
@@ -58,6 +59,7 @@ type printer_fun 'a =
 value register_str_item_printer : printer_fun PreCast.Ast.str_item -> unit;
 value register_sig_item_printer : printer_fun PreCast.Ast.sig_item -> unit;
 value register_printer : printer_fun PreCast.Ast.str_item -> printer_fun PreCast.Ast.sig_item -> unit;
+value current_printer : unit -> (printer_fun PreCast.Ast.str_item * printer_fun PreCast.Ast.sig_item);
 
 module Printer
   (Id : Sig.Id)
