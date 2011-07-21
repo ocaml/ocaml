920096846e70d56ca04507a9354482c8a1c254d0
Xavier Clerc <xavier.clerc@inria.fr>
2011-07-21 07:47:26 +0000

    test suite: 'els' (signatures) moved from 'testlabl'.
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@11139 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- testsuite/tests/typing-signatures/els.ml
+++ testsuite/tests/typing-signatures/els.ml
@@ -5,26 +5,26 @@ module type VALUE = sig
   type value (* a Lua value *)
   type state (* the state of a Lua interpreter *)
   type usert (* a user-defined value *)
-end
+end;;
 
 module type CORE0 = sig
   module V : VALUE
   val setglobal : V.state -> string -> V.value -> unit
   (* five more functions common to core and evaluator *)
-end
+end;;
 
 module type CORE = sig
   include CORE0
   val apply : V.value -> V.state -> V.value list -> V.value
   (* apply function f in state s to list of args *)
-end
+end;;
 
 module type AST = sig
   module Value : VALUE
   type chunk
   type program
   val get_value : chunk -> Value.value
-end
+end;;
 
 module type EVALUATOR = sig
   module Value : VALUE
@@ -34,59 +34,59 @@ module type EVALUATOR = sig
   exception Error of string
   val compile : Ast.program -> string
   include CORE0 with module V := Value
-end
+end;;
 
 module type PARSER = sig
   type chunk
   val parse : string -> chunk
-end
+end;;
 
 module type INTERP = sig
   include EVALUATOR
   module Parser : PARSER with type chunk = Ast.chunk
   val dostring : state -> string -> value list
   val mk	: unit -> state
-end
+end;;
 
 module type USERTYPE = sig
   type t
   val eq	: t -> t -> bool
   val to_string : t -> string
-end
+end;;
 
 module type TYPEVIEW = sig
   type combined
   type t
   val map : (combined -> t) * (t -> combined)
-end
+end;;
 
 module type COMBINED_COMMON = sig
   module T : sig type t end
   module TV1 : TYPEVIEW with type combined := T.t
   module TV2 : TYPEVIEW with type combined := T.t
-end
+end;;
 
 module type COMBINED_TYPE = sig
   module T : USERTYPE
   include COMBINED_COMMON with module T := T
-end
+end;;
 
 module type BARECODE = sig
   type state
   val init : state -> unit
-end
+end;;
 
 module USERCODE(X : TYPEVIEW) = struct
   module type F =
       functor (C : CORE with type V.usert = X.combined) ->
         BARECODE with type state := C.V.state
-end
+end;;
 
-module Weapon = struct type t end
+module Weapon = struct type t end;;
 
 module type WEAPON_LIB = sig
   type t = Weapon.t
   module T : USERTYPE with type t = t
   module Make :
     functor (TV : TYPEVIEW with type t = t) -> USERCODE(TV).F
-end
+end;;
