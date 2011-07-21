d5e1e67d6451de72b4e1e4cb11157deda19b5abb
Xavier Clerc <xavier.clerc@inria.fr>
2011-07-21 07:43:44 +0000

    test suite: 'sigsubst' moved from 'testlabl'.
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@11138 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- testsuite/tests/typing-sigsubst/sigsubst.ml
+++ testsuite/tests/typing-sigsubst/sigsubst.ml
@@ -1,38 +1,37 @@
 module type Printable = sig
   type t
   val print : Format.formatter -> t -> unit
-end
+end;;
 module type Comparable = sig
   type t
   val compare : t -> t -> int
-end
+end;;
 module type PrintableComparable = sig
   include Printable
   include Comparable with type t = t
-end
+end;;
 module type PrintableComparable = sig
   type t
   include Printable with type t := t
   include Comparable with type t := t
-end
+end;;
 module type PrintableComparable = sig
   include Printable
   include Comparable with type t := t
-end
-module type ComparableInt = Comparable with type t := int
+end;;
+module type ComparableInt = Comparable with type t := int;;
+module type S = sig type t val f : t -> t end;;
+module type S' = S with type t := int;;
 
-module type S = sig type t val f : t -> t end
-module type S' = S with type t := int
-
-module type S = sig type 'a t val map : ('a -> 'b) -> 'a t -> 'b t end
-module type S1 = S with type 'a t := 'a list
+module type S = sig type 'a t val map : ('a -> 'b) -> 'a t -> 'b t end;;
+module type S1 = S with type 'a t := 'a list;;
 module type S2 = sig
   type 'a dict = (string * 'a) list
   include S with type 'a t := 'a dict
-end
+end;;
 
 
 module type S =
-  sig module T : sig type exp type arg end val f : T.exp -> T.arg end
-module M = struct type exp = string type arg = int end
-module type S' = S with module T := M
+  sig module T : sig type exp type arg end val f : T.exp -> T.arg end;;
+module M = struct type exp = string type arg = int end;;
+module type S' = S with module T := M;;
