1061c39aac71ab7aaae7da059071487ddcddb5c8
Alain Frisch <alain@frisch.fr>
2011-12-21 16:35:36 +0000

    Check for unused value declaration in interface files.
    git-svn-id: http://caml.inria.fr/svn/ocaml/branches/unused_declarations@11928 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- driver/optcompile.ml
+++ driver/optcompile.ml
@@ -80,7 +80,12 @@ let interface ppf sourcefile outputprefix =
     let ast =
       Pparse.file ppf inputfile Parse.interface ast_intf_magic_number in
     if !Clflags.dump_parsetree then fprintf ppf "%a@." Printast.interface ast;
-    let sg = Typemod.transl_signature (initial_env()) ast in
+    Typecore.reset_delayed_checks ();
+    let env = initial_env () in
+    let sg = Typemod.transl_signature env ast in
+    if Warnings.is_active (Warnings.Unused_value_declaration "") then
+      ignore (Includemod.signatures env sg sg);
+    Typecore.force_delayed_checks ();
     if !Clflags.print_types then
       fprintf std_formatter "%a@." Printtyp.signature
                                    (Typemod.simplify_signature sg);
