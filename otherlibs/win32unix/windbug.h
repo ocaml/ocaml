e9123cd60fe2b62d1d3e825e4039b0234c51a58f
Xavier Leroy <xavier.leroy@inria.fr>
2012-01-14 09:40:49 +0000

    PR#5327, PR#5329: improvements in Unix.select emulation
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@12023 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- otherlibs/win32unix/windbug.h
+++ otherlibs/win32unix/windbug.h
@@ -18,13 +18,15 @@
 #include <stdio.h>
 #include <windows.h>
 
+/* According to MSDN, MSVC supports the gcc ## operator (to deal with empty argument lists)
+ */
 #define DEBUG_PRINT(fmt, ...) \
   do \
   { \
     if (debug_test()) \
     { \
-      fprintf(stderr, "DBUG (pid:%d, tid: %d): ", GetCurrentProcessId(), GetCurrentThreadId()); \
-      fprintf(stderr, fmt, __VA_ARGS__); \
+      fprintf(stderr, "DBUG (pid:%ld, tid: %ld): ", GetCurrentProcessId(), GetCurrentThreadId()); \
+      fprintf(stderr, fmt, ##__VA_ARGS__); \
       fprintf(stderr, "\n"); \
       fflush(stderr); \
     }; \
