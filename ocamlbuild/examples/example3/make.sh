3dd2fee2ee2d30c789df6e8cd751e4bfb24323c0
Damien Doligez <damien.doligez-inria.fr>
2012-08-01 14:47:00 +0000

    fix formatting of copyright headers
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@12808 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- ocamlbuild/examples/example3/make.sh
+++ ocamlbuild/examples/example3/make.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+#########################################################################
+#                                                                       #
+#                                 OCaml                                 #
+#                                                                       #
+#       Nicolas Pouillard, projet Gallium, INRIA Rocquencourt           #
+#                                                                       #
+#   Copyright 2007 Institut National de Recherche en Informatique et    #
+#   en Automatique.  All rights reserved.  This file is distributed     #
+#   under the terms of the Q Public License version 1.0.                #
+#                                                                       #
+#########################################################################
+
 set -e
 
 TARGET=epoch
