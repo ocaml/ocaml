d165ca651e5c5446598613d7f55b698d24aaeeab
Jacques Garrigue <garrigue at math.nagoya-u.ac.jp>
2012-08-14 05:23:40 +0000

    PR#5722: toplevel: print full module path only for first record field
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@12861 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- testsuite/tests/typing-misc/records.ml
+++ testsuite/tests/typing-misc/records.ml
@@ -10,3 +10,20 @@ fun {x=3;z=2} -> ();;
 type u = private {mutable u:int};;
 {u=3};;
 fun x -> x.u <- 3;;
+
+(* Punning and abbreviations *)
+module M = struct
+  type t = {x: int; y: int}
+end;;
+
+let f {M.x; y} = x+y;;
+let r = {M.x=1; y=2};;
+let z = f r;;
+
+module M = struct
+  type t = {x: int; y: int}
+  type u = {y: bool}
+end;;
+(* path abbreviation is syntactic *)
+let f {M.x; y} = x+y;; (* fails *)
+let r = {M.x=1; y=2};; (* fails *)
--- toplevel/genprintval.ml
+++ toplevel/genprintval.ml
@@ -279,8 +279,13 @@ module Make(O : OBJ)(EVP : EVALPATH with type valu = O.t) = struct
                                     ty_list
                                 with
                                   Ctype.Cannot_apply -> abstract_type in
-                              let lid = tree_of_label env path (Ident.name lbl_name) in
-                              let v =
+			      let name = Ident.name lbl_name in
+			      (* PR#5722: print full module path only
+				 for first record field *)
+                              let lid =
+				if pos = 0 then tree_of_label env path name
+				else Oide_ident name
+                              and v =
                                 tree_of_val (depth - 1) (O.field obj pos)
                                   ty_arg
                               in
