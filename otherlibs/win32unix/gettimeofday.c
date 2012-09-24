2932d1f4a01a0015584357401e040789a1b9c895
Xavier Leroy <xavier.leroy@inria.fr>
2012-09-24 11:36:47 +0000

    PR#5734: improved Win32 implementation of Unix.gettimeofday
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@12949 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- otherlibs/win32unix/gettimeofday.c
+++ otherlibs/win32unix/gettimeofday.c
@@ -19,15 +19,35 @@
 
 #include "unixsupport.h"
 
+#ifdef HAS_MKTIME
+static double initial_time = 0; /* 0 means uninitialized */
+#else
 static time_t initial_time = 0; /* 0 means uninitialized */
+#endif
 static DWORD initial_tickcount;
 
 CAMLprim value unix_gettimeofday(value unit)
 {
   DWORD tickcount = GetTickCount();
+  SYSTEMTIME st;
+  struct tm tm;
   if (initial_time == 0 || tickcount < initial_tickcount) {
     initial_tickcount = tickcount;
+#ifdef HAS_MKTIME
+    GetLocalTime(&st);
+    tm.tm_sec = st.wSecond;
+    tm.tm_min = st.wMinute;
+    tm.tm_hour = st.wHour;
+    tm.tm_mday = st.wDay;
+    tm.tm_mon = st.wMonth - 1;
+    tm.tm_year = st.wYear - 1900;
+    tm.tm_wday = 0;
+    tm.tm_yday = 0;
+    tm.tm_isdst = -1;
+    initial_time = ((double) mktime(&tm) + (double) st.wMilliseconds * 1e-3);
+#else
     initial_time = time(NULL);
+#endif
     return copy_double((double) initial_time);
   } else {
     return copy_double((double) initial_time +
