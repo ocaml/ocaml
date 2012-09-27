a98dcd4ce7cb0d6f225fa01caa26660034e0cdbc
Jacques Garrigue <garrigue at math.nagoya-u.ac.jp>
2012-09-27 13:29:37 +0000

    Fix PR#5758
    git-svn-id: http://caml.inria.fr/svn/ocaml/version/4.00@12960 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- testsuite/tests/basic-more/morematch.ml
+++ testsuite/tests/basic-more/morematch.ml
@@ -1168,3 +1168,22 @@ let () =
   test "lucexn1" lucexn  (Error "coucou") "coucou" ;
   test "lucexn2" lucexn (Found ("int: ",0)) "int: 0" ;
   ()
+
+(*
+  PR#5758: different representations of floats
+*)
+
+let pr5758 x str =
+  match (x, str) with
+  | (1. , "A") -> "Matched A"
+  | (1.0, "B") -> "Matched B"
+  | (1. , "C") -> "Matched C"
+  | result ->
+    match result with
+    | (1., "A") -> "Failed match A then later matched"
+    | _ -> "Failed twice"
+;;
+
+let () =
+  test "pr5758" (pr5758 1.) "A" "Matched A"
+;;
