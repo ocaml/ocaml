4dd1e7fe4106c43f2040213ff5d397779cd3350a
Xavier Clerc <xavier.clerc@inria.fr>
2012-10-10 13:59:47 +0000

    PR#5102: ocamlbuild fails when using an unbound variable in rule dependency
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@13002 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- ocamlbuild/resource.ml
+++ ocamlbuild/resource.ml
@@ -377,7 +377,7 @@ end = struct
       List.map begin fun x ->
         match x with
         | A atom -> atom
-        | V(var, _) -> List.assoc var env
+        | V(var, _) -> try List.assoc var env with Not_found -> (* unbound variable *) ""
       end s
     end
 end
