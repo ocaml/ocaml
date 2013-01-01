5e15de8ae11636bfc4393d1cabb7f2daf31c0709
Wojciech Meyer <wojciech.meyer@gmail.com>
2013-01-01 04:53:49 +0000

    Fix missing files to be installed for ocamlbuild
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@13192 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- build/install.sh
+++ build/install.sh
@@ -551,6 +551,8 @@ installdir \
   ocamlbuildlib.cmxa \
   ocamlbuildlib.cma \
   ocamlbuild_plugin.cmi \
+  ocamlbuild_plugin.cmo \
+  ocamlbuild_plugin.cmx \
   ocamlbuild_pack.cmi \
   ocamlbuild_unix_plugin.cmi \
   ocamlbuild_unix_plugin.cmo \
