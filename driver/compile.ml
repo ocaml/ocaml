726da0bdfc940b2355344ef7faf4819ec48df213
Damien Doligez <damien.doligez-inria.fr>
2013-03-22 18:19:54 +0000

    fix whitespace and over-long lines
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@13429 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- driver/compile.ml
+++ driver/compile.ml
@@ -88,15 +88,15 @@ let interface ppf sourcefile outputprefix =
     let sg = tsg.sig_type in
     if !Clflags.print_types then
       Printtyp.wrap_printing_env initial_env (fun () ->
-	fprintf std_formatter "%a@."
-	  Printtyp.signature (Typemod.simplify_signature sg));
+        fprintf std_formatter "%a@."
+          Printtyp.signature (Typemod.simplify_signature sg));
     ignore (Includemod.signatures initial_env sg sg);
     Typecore.force_delayed_checks ();
     Warnings.check_fatal ();
     if not !Clflags.print_types then begin
       let sg = Env.save_signature sg modulename (outputprefix ^ ".cmi") in
       Typemod.save_signature modulename tsg outputprefix sourcefile
-	initial_env sg ;
+        initial_env sg ;
     end;
     Pparse.remove_preprocessed inputfile
   with e ->
@@ -127,7 +127,7 @@ let implementation ppf sourcefile outputprefix =
       ++ print_if ppf Clflags.dump_source Pprintast.structure
       ++ Typemod.type_implementation sourcefile outputprefix modulename env
       ++ print_if ppf Clflags.dump_typedtree
-	   Printtyped.implementation_with_coercion);
+           Printtyped.implementation_with_coercion);
       Warnings.check_fatal ();
       Pparse.remove_preprocessed inputfile;
       Stypes.dump (Some (outputprefix ^ ".annot"));
@@ -143,7 +143,8 @@ let implementation ppf sourcefile outputprefix =
       ++ print_if ppf Clflags.dump_parsetree Printast.implementation
       ++ print_if ppf Clflags.dump_source Pprintast.structure
       ++ Typemod.type_implementation sourcefile outputprefix modulename env
-      ++ print_if ppf Clflags.dump_typedtree Printtyped.implementation_with_coercion
+      ++ print_if ppf Clflags.dump_typedtree
+                  Printtyped.implementation_with_coercion
       ++ Translmod.transl_implementation modulename
       ++ print_if ppf Clflags.dump_rawlambda Printlambda.lambda
       ++ Simplif.simplify_lambda
