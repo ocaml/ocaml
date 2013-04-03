ef3495086377f10b25428333fe038833948cda0b
Alain Frisch <alain@frisch.fr>
2013-04-03 16:29:03 +0000

    POC of a metaquotation expander.
    git-svn-id: http://caml.inria.fr/svn/ocaml/branches/extension_points@13480 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- experimental/frisch/genlifter.ml
+++ experimental/frisch/genlifter.ml
@@ -2,10 +2,38 @@
    This illustrates how to build fragments of Parsetree through
    Ast_helper and more local helper functions. *)
 
+module Main : sig end = struct
+
 open Location
 open Types
 open Asttypes
 open Ast_helper
+
+(* Convenience AST builders, to be moved to Ast_helper at some point *)
+
+let may_tuple tup = function
+  | [] -> None
+  | [x] -> Some x
+  | l -> Some (tup l)
+
+let lid s = mknoloc (Longident.parse s)
+let constr s args = Exp.construct (lid s) (may_tuple Exp.tuple args) true
+let pconstr s args = Pat.construct (lid s) (may_tuple Pat.tuple args) true
+let nil = constr "[]" []
+let cons hd tl = constr "::" [hd; tl]
+let list l = List.fold_right cons l nil
+let func l = Exp.function_ "" None l
+let lam pat exp = func [pat, exp]
+let str s = Exp.constant (Const_string (s, None))
+let app f l = Exp.apply f (List.map (fun a -> "", a) l)
+let pvar s = Pat.var (mknoloc s)
+let evar s = Exp.ident (lid s)
+let pair x y = Exp.tuple [x; y]
+let let_in b body = Exp.let_ Nonrecursive b body
+let selfcall ?(this = "this") m args = app (Exp.send (evar this) m) args
+
+(*************************************************************************)
+
 
 let env = Env.initial
 
@@ -21,31 +49,14 @@ let print_fun s = "lift_" ^ clean s
 let printed = Hashtbl.create 16
 let meths = ref []
 
-let lid s = mknoloc (Longident.parse s)
-let constr s x = Exp.construct (lid s) (Some x) true
-let constr0 s = Exp.construct (lid s) None true
-let pconstr s x = Pat.construct (lid s) (Some x) true
-let pconstr0 s = Pat.construct (lid s) None true
-let nil = constr0 "[]"
-let cons hd tl = constr "::" (Exp.tuple [hd; tl])
-let list l = List.fold_right cons l nil
-let lam pat exp = Exp.function_ "" None [pat, exp]
-let func l = Exp.function_ "" None l
-let str s = Exp.constant (Const_string (s, None))
-let app f l = Exp.apply f (List.map (fun a -> "", a) l)
-let pvar s = Pat.var (mknoloc s)
-let evar s = Exp.ident (lid s)
-let pair x y = Exp.tuple [x; y]
-let let_in b body = Exp.let_ Nonrecursive b body
-let selfcall m args = app (Exp.send (evar "this") m) args
-
 let rec gen ty =
   if Hashtbl.mem printed ty then ()
   else let tylid = Longident.parse ty in
   let (_, td) =
     try Env.lookup_type tylid env
     with Not_found ->
-      failwith (Printf.sprintf "Cannot resolve type %s" ty)
+      Format.eprintf "** Cannot resolve type %s" ty;
+      exit 2
   in
   let prefix =
     let open Longident in
@@ -87,18 +98,8 @@ let rec gen ty =
       let case (c, tyl, _) =
         let c = Ident.name c in
         let qc = prefix ^ c in
-        let pat, args =
-          match tyl with
-          | [] ->
-              pconstr0 qc, []
-          | [t] ->
-              pconstr qc (pvar "x"),
-              [ tyexpr env t (evar "x") ]
-          | l ->
-              let p, e = tuple env l in
-              pconstr qc p, e
-        in
-        pat, selfcall "constr" [str ty; pair (str c) (list args)]
+        let p, args = tuple env tyl in
+        pconstr qc p, selfcall "constr" [str ty; pair (str c) (list args)]
       in
       concrete (func (List.map case l))
   | Type_abstract, Some t ->
@@ -112,9 +113,7 @@ and tuple env tl =
     let x = Printf.sprintf "x%i" i in
     pvar x, tyexpr env t (evar x)
   in
-  let l = List.mapi arg tl in
-  Pat.tuple (List.map fst l), List.map snd l
-
+  List.split (List.mapi arg tl)
 
 and tyexpr env ty x =
   match ty.desc with
@@ -126,7 +125,7 @@ and tyexpr env ty x =
       app f [x]
   | Ttuple tl ->
       let p, e = tuple env tl in
-      let_in [p, x] (selfcall "tuple" [list e])
+      let_in [Pat.tuple p, x] (selfcall "tuple" [list e])
   | Tconstr (path, [t], _) when Path.same path Predef.path_list ->
       selfcall "list" [app (evar "List.map") [tyexpr_fun env t; x]]
   | Tconstr (path, [t], _) when Path.same path Predef.path_array ->
@@ -148,7 +147,7 @@ and tyexpr env ty x =
       gen ty;
       selfcall (print_fun ty) (List.map (tyexpr_fun env) tl @ [x])
   | _ ->
-      Format.eprintf "Cannot deal with type %a@." Printtyp.type_expr ty;
+      Format.eprintf "** Cannot deal with type %a@." Printtyp.type_expr ty;
       exit 2
 
 and tyexpr_fun env ty =
@@ -162,9 +161,14 @@ let simplify =
       let open Longident in
       let open Parsetree in
       match e.pexp_desc with
-      | Pexp_function ("", None, [{ppat_desc = Ppat_var{txt=id;_};_},
-                                  {pexp_desc = Pexp_apply(f,
-                                                          ["",{pexp_desc=Pexp_ident{txt=Lident id2;_};_}]);_}]) when id = id2 -> f
+      | Pexp_function
+          ("", None,
+           [{ppat_desc = Ppat_var{txt=id;_};_},
+            {pexp_desc =
+             Pexp_apply
+               (f,
+                ["",{pexp_desc=
+                     Pexp_ident{txt=Lident id2;_};_}]);_}]) when id = id2 -> f
       | _ -> e
   end
 
@@ -186,3 +190,5 @@ let () =
   let cl = Ci.mk ~virt:Virtual ~params (mknoloc "lifter") (Cl.structure cl) in
   let s = [Str.class_ [cl]] in
   Format.printf "%a@." Pprintast.structure (simplify # structure s)
+
+end
