200f97fef60e6053716fdbd84f276f0d1f682242
Damien Doligez <damien.doligez-inria.fr>
2013-05-13 15:11:24 +0000

    trying to figure out what goes wrong on the Windows test machines
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@13665 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- testsuite/tests/lib-threads/close.ml
+++ testsuite/tests/lib-threads/close.ml
@@ -10,18 +10,32 @@
 (*                                                                     *)
 (***********************************************************************)
 
+let debug = Printf.eprintf;;
+
 let main () =
+  debug "starting\n%!";
   let (rd, wr) = Unix.pipe() in
+  debug "after pipe\n%!";
   let _ = Thread.create
     (fun () ->
+      debug "sub: starting\n%!";
       ignore (Unix.write wr "0123456789" 0 10);
+      debug "sub: after write\n%!";
       Thread.delay 3.0;
+      debug "sub: after delay\n%!";
       print_endline "closing fd...";
-      Unix.close rd)
+      Unix.close rd;
+      debug "sub: after close\n%!";
+    )
     () in
+  debug "after thread.create\n%!";
   let buf = String.create 10 in
+  debug "after string.create\n%!";
   print_endline "reading...";
   ignore (Unix.read rd buf 0 10);
+  debug "after read\n%!";
   print_endline "read returned"
 
 let _ = Unix.handle_unix_error main ()
+
+;;debug "that's all folks\n%!"
