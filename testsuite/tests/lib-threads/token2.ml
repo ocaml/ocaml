b2cf57f35f30e506bf25be8006f57f2e79cbd3ad
Damien Doligez <damien.doligez-inria.fr>
2013-05-15 09:32:29 +0000

    testsuite/lib-threads: stop all threads before exiting
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@13676 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- testsuite/tests/lib-threads/token2.ml
+++ testsuite/tests/lib-threads/token2.ml
@@ -20,29 +20,38 @@ let token = ref 0
 
 let process (n, ins, outs, nprocs) =
   let buf = String.create 1 in
-  while true do
+  try while true do
     Unix.read ins.(n) buf 0 1;
     (* Printf.printf "Thread %d got the token\n" n; *)
     if n = 0 then begin
       decr niter;
-      if !niter <= 0 then exit 0
+      if !niter <= 0 then begin
+        for i = 0 to nprocs - 1 do
+          Unix.close ins.(i); Unix.close outs.(i)
+        done;
+        Thread.exit ()
+      end
     end;
     let next = if n + 1 >= nprocs then 0 else n + 1 in
     (* Printf.printf "Thread %d sending token to thread %d\n" n next; *)
     Unix.write outs.(next) buf 0 1
   done
+  with Unix.Unix_error _ -> ()
 
 let main() =
   let nprocs = try int_of_string Sys.argv.(1) with _ -> 100 in
   let iter = try int_of_string Sys.argv.(2) with _ -> 1000 in
   let ins = Array.create nprocs Unix.stdin in
   let outs = Array.create nprocs Unix.stdout in
+  let threads = Array.create nprocs (Thread.self ()) in
   for n = 0 to nprocs - 1 do
     let (i, o) = Unix.pipe() in ins.(n) <- i; outs.(n) <- o
   done;
   niter := iter;
-  for i = 0 to nprocs - 1 do Thread.create process (i, ins, outs, nprocs) done;
+  for i = 0 to nprocs - 1 do
+    threads.(i) <- Thread.create process (i, ins, outs, nprocs)
+  done;
   Unix.write outs.(0) "X" 0 1;
-  Thread.delay 3600.
+  for i = 0 to nprocs - 1 do Thread.join threads.(i) done
 
 let _ = main()
