e7a503dc2a836e5d1ec6495d56e48255c9cbb71f
Gabriel Scherer <gabriel.scherer@gmail.com>
2013-06-17 13:12:40 +0000

    PR#4502: ocamlbuild now reliably excludes the build-dir from hygiene check
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@13794 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- ocamlbuild/testsuite/level0.ml
+++ ocamlbuild/testsuite/level0.ml
@@ -132,10 +132,42 @@ test "SyntaxFlag"
   ~targets:("dummy.native",[]) ();;
 
 test "NativeMliCmi"
-  ~description:"check that ocamlopt is used for .mli->.cmi when tag 'native' is set"
+  ~description:"check that ocamlopt is used for .mli->.cmi when tag 'native' is set \
+                (part of PR#4613)"
   ~tree:[T.f "foo.mli" ~content:"val bar : int"]
   ~options:[`ocamlc "toto";(*using ocamlc would fail*)  `tags["native"]]
   ~matching:[M.f "_build/foo.cmi"]
   ~targets:("foo.cmi",[]) ();;
+
+test "NoIncludeNoHygiene1"
+  ~description:"check that hygiene checks are only done in traversed directories\
+                (PR#4502)"
+  ~tree:[T.d "must_ignore" [ T.f "dirty.mli" ~content:"val bug : int"];
+         T.f "hello.ml" ~content:"print_endline \"Hello, World!\"";
+         T.f "_tags" ~content:"<must_ignore>: -traverse"]
+  ~pre_cmd:"ocamlc -c must_ignore/dirty.mli"
+            (* will make hygiene fail if must_ignore/ is checked *)
+  ~targets:("hello.byte",[]) ();;
+
+test "NoIncludeNoHygiene2"
+  ~description:"check that hygiene checks are not done on the -build-dir \
+                (PR#4502)"
+  ~tree:[T.d "must_ignore" [ T.f "dirty.mli" ~content:"val bug : int"];
+         T.f "hello.ml" ~content:"print_endline \"Hello, World!\"";
+         T.f "_tags" ~content:""]
+  ~options:[`build_dir "must_ignore"]
+  ~pre_cmd:"ocamlc -c must_ignore/dirty.mli"
+            (* will make hygiene fail if must_ignore/ is checked *)
+  ~targets:("hello.byte",[]) ();;
+
+test "NoIncludeNoHygiene3"
+  ~description:"check that hygiene checks are not done on excluded dirs (PR#4502)"
+  ~tree:[T.d "must_ignore" [ T.f "dirty.mli" ~content:"val bug : int"];
+         T.f "hello.ml" ~content:"print_endline \"Hello, World!\"";
+         T.f "_tags" ~content:""]
+  ~options:[`X "must_ignore"]
+  ~pre_cmd:"ocamlc -c must_ignore/dirty.mli"
+            (* will make hygiene fail if must_ignore/ is checked *)
+  ~targets:("hello.byte",[]) ();;
 
 run ~root:"_test";;
