e8bef60f8ded5ddefb5c24c95c8e27c607b09c80
Gabriel Scherer <gabriel.scherer@gmail.com>
2013-08-19 07:42:39 +0000

    ocamlbuild testsuite: test modular plugin compilation
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@14016 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- ocamlbuild/testsuite/level0.ml
+++ ocamlbuild/testsuite/level0.ml
@@ -210,4 +210,39 @@ test "PrincipalFlag"
 Warning 18: this type-based field disambiguation is not principal."
   ~targets:("hello.byte",["hello.native"]) ();;
 
+test "ModularPlugin1"
+  ~description:"test a plugin with dependency on external libraries"
+  ~tree:[T.f "main.ml" ~content:"let x = 1";
+         T.f "_tags" ~content:"\"myocamlbuild.ml\": use_str";
+         T.f "myocamlbuild.ml" ~content:"ignore (Str.quote \"\");;"]
+  ~matching:[M.f "main.byte"]
+  ~targets:("main.byte",[]) ();;
+
+test "ModularPlugin2"
+  ~description:"check that parametrized tags defined by the plugin
+                do not warn at plugin-compilation time"
+  ~options:[`quiet]
+  ~tree:[T.f "main.ml" ~content:"let x = 1";
+         T.f "_tags" ~content:"<main.*>: toto(-g)";
+         T.f "myocamlbuild.ml"
+           ~content:"open Ocamlbuild_plugin;;
+                     pflag [\"link\"] \"toto\" (fun arg -> A arg);;"]
+  ~failing_msg:""
+  ~matching:[M.f "main.byte"]
+  ~targets:("main.byte",[]) ();;
+
+test "ModularPlugin3"
+  ~description:"check that unknown parametrized tags encountered
+                during plugin compilation still warn"
+  ~options:[`quiet]
+  ~tree:[T.f "main.ml" ~content:"let x = 1";
+         T.f "_tags" ~content:"\"myocamlbuild.ml\": toto(-g)";
+         T.f "myocamlbuild.ml"
+           ~content:"open Ocamlbuild_plugin;;
+                     pflag [\"link\"] \"toto\" (fun arg -> A arg);;"]
+  ~failing_msg:"Warning: tag \"toto\" does not expect a parameter, \
+                but is used with parameter \"-g\""
+  ~matching:[M.f "main.byte"]
+  ~targets:("main.byte",[]) ();;
+
 run ~root:"_test";;
