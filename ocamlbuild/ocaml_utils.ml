75beffa385abcefc79d0229c5f538b29bc657952
Gabriel Scherer <gabriel.scherer@gmail.com>
2013-09-15 11:36:45 +0000

    ocamlbuild: mark ocaml_lib's tag as used
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@14145 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- ocamlbuild/ocaml_utils.ml
+++ ocamlbuild/ocaml_utils.ml
@@ -118,6 +118,10 @@ let ocaml_lib ?(extern=false) ?(byte=true) ?(native=true) ?dir ?tag_name libpath
     if not extern then dep tags [lib] (* cannot happen? *)
   in
   Hashtbl.replace info_libraries tag_name (libpath, extern);
+  (* adding [tag_name] to [info_libraries] will make this tag
+     affect include-dir lookups, so it is used even if not
+     mentioned explicitly in any rule. *)
+  Flags.mark_as_used tag_name;
   if extern then begin
     if byte then
       flag_and_dep ["ocaml"; tag_name; "link"; "byte"] (libpath^".cma");
