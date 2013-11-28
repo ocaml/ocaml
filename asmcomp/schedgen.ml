140f62041327a4c3aebf159b7595a537bf1ca4a3
Xavier Leroy <xavier.leroy@inria.fr>
2013-11-28 14:42:34 +0000

    Debug info was lost when rescheduling "checkbound" instructions.
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@14323 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- asmcomp/schedgen.ml
+++ asmcomp/schedgen.ml
@@ -336,8 +336,8 @@ method private reschedule ready_queue date cont =
             if son.emitted_ancestors = son.ancestors then
               new_queue := son :: !new_queue)
           node.sons;
-        instr_cons node.instr.desc node.instr.arg node.instr.res
-          (self#reschedule !new_queue (date + issue_cycles) cont)
+        { node.instr with next =
+            self#reschedule !new_queue (date + issue_cycles) cont }
   end
 
 (* Entry point *)
