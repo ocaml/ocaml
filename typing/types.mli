7b514efefbc755de0f00fd7eb8fc5c452e2160aa
Alain Frisch <alain@frisch.fr>
2013-12-11 14:56:57 +0000

    Typo.
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@14346 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- typing/types.mli
+++ typing/types.mli
@@ -272,7 +272,7 @@ and module_declaration =
 
 and modtype_declaration =
   {
-    mtd_type: module_type option;  (* Nonte: abstract *)
+    mtd_type: module_type option;  (* None: abstract *)
     mtd_attributes: Parsetree.attributes;
   }
 
