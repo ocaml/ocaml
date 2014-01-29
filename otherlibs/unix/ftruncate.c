08986a33151df0543dbaab49c3d898868ce2f039
Jérémie Dimino <jeremie@dimino.org>
2014-01-29 09:35:31 +0000

    release the lock in ftruncate, fchown, and fchmod
    git-svn-id: http://caml.inria.fr/svn/ocaml/version/4.01@14426 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- otherlibs/unix/ftruncate.c
+++ otherlibs/unix/ftruncate.c
@@ -24,15 +24,22 @@
 
 CAMLprim value unix_ftruncate(value fd, value len)
 {
-  if (ftruncate(Int_val(fd), Long_val(len)) == -1)
-    uerror("ftruncate", Nothing);
+  int result;
+  caml_enter_blocking_section();
+  result = ftruncate(Int_val(fd), Long_val(len));
+  caml_leave_blocking_section();
+  if (result == -1) uerror("ftruncate", Nothing);
   return Val_unit;
 }
 
 CAMLprim value unix_ftruncate_64(value fd, value len)
 {
-  if (ftruncate(Int_val(fd), File_offset_val(len)) == -1)
-    uerror("ftruncate", Nothing);
+  int result;
+  file_offset ofs = File_offset_val(len);
+  caml_enter_blocking_section();
+  result = ftruncate(Int_val(fd), ofs);
+  caml_leave_blocking_section();
+  if (result == -1) uerror("ftruncate", Nothing);
   return Val_unit;
 }
 
