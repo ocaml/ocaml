7578b585a18d135b8787eb3ba9ea3c3b582b8029
Alain Frisch <alain@frisch.fr>
2014-03-20 13:27:50 +0000

    Also check sharing of immutable constants.
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@14470 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- testsuite/tests/asmcomp/staticalloc.ml
+++ testsuite/tests/asmcomp/staticalloc.ml
@@ -11,7 +11,9 @@ let () =
   let a = pair 1 2 in
   let b = pair a ["x";"y"] in
   let g () = (a, fst b) in
-  assert (g () == g ());
+  assert (g () == ((1,2), (1,2)));
+  assert (fst (pair a a) == (1, 2));
+  assert (snd b != ["x"; "y"]);  (* mutable "constant", cannot be shared *)
   let x2 = Gc.allocated_bytes () in
   assert(x1 -. x0 = x2 -. x1)
      (* check that we did not allocated anything between x1 and x2 *)
