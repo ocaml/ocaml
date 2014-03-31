29141f0f060ea4221ca954abf23c7cb8795f5b78
Alain Frisch <alain@frisch.fr>
2014-03-31 15:27:07 +0000

    Adapt ocamlopt.
    git-svn-id: http://caml.inria.fr/svn/ocaml/branches/constructors_with_record2@14511 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- asmcomp/cmmgen.ml
+++ asmcomp/cmmgen.ml
@@ -633,7 +633,7 @@ let rec expr_size env = function
       RHS_block (List.length args)
   | Uprim(Pmakearray(Pfloatarray), args, _) ->
       RHS_floatblock (List.length args)
-  | Uprim (Pduprecord (Record_regular, sz), _, _) ->
+  | Uprim (Pduprecord (Record_regular _, sz), _, _) ->
       RHS_block sz
   | Uprim (Pduprecord (Record_float, sz), _, _) ->
       RHS_floatblock sz
