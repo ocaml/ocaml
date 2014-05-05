f8efbbfc0d09d099d6d86f67d72d4d9f88cd8053
Alain Frisch <alain@frisch.fr>
2014-05-05 13:04:01 +0000

    Whitespace.
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@14747 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- parsing/ast_mapper.ml
+++ parsing/ast_mapper.ml
@@ -61,7 +61,7 @@ type mapper = {
   structure_item: mapper -> structure_item -> structure_item;
   typ: mapper -> core_type -> core_type;
   type_declaration: mapper -> type_declaration -> type_declaration;
-  type_extension : mapper -> type_extension -> type_extension;
+  type_extension: mapper -> type_extension -> type_extension;
   type_kind: mapper -> type_kind -> type_kind;
   value_binding: mapper -> value_binding -> value_binding;
   value_description: mapper -> value_description -> value_description;
