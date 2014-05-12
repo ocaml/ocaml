c2a837bcfb2cd131ebc2187362a5dde55129cd9c
Stephen Dolan <mu@netsoc.tcd.ie>
2014-05-12 10:55:21 +0100

    Squash a mutable global in sys.c

--- byterun/sys.c
+++ byterun/sys.c
@@ -114,7 +114,7 @@ CAMLprim value caml_sys_exit(value retcode)
 #endif
 #endif
 
-static int sys_open_flags[] = {
+const static int sys_open_flags[] = {
   O_RDONLY, O_WRONLY, O_APPEND | O_WRONLY, O_CREAT, O_TRUNC, O_EXCL,
   O_BINARY, O_TEXT, O_NONBLOCK
 };
