2248d13d4912118a50e893eb1e262314e227ffd0
Gabriel Scherer <gabriel.scherer@gmail.com>
2014-08-28 16:24:52 +0000

    New -strict-formats flag to detect and fix invalid formats
    (Request by Mark Shinwell, ack'ed by Damien Doligez)
    
    git-svn-id: http://caml.inria.fr/svn/ocaml/version/4.02@15138 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- tools/ocamlcp.ml
+++ tools/ocamlcp.ml
@@ -83,6 +83,7 @@ module Options = Main_args.Make_bytecomp_options (struct
   let _safe_string = option "-safe-string"
   let _short_paths = option "-short-paths"
   let _strict_sequence = option "-strict-sequence"
+  let _strict_formats = option "-strict-formats"
   let _thread () = option "-thread" ()
   let _vmthread () = option "-vmthread" ()
   let _unsafe = option "-unsafe"
