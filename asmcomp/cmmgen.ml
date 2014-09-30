50fae59bc663e8e5a706375aebb0b4344e67ea14
Alain Frisch <alain@frisch.fr>
2014-09-30 13:10:10 +0000

    Fix.
    git-svn-id: http://caml.inria.fr/svn/ocaml/branches/constructors_with_record4@15383 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- asmcomp/cmmgen.ml
+++ asmcomp/cmmgen.ml
@@ -643,7 +643,7 @@ let rec expr_size env = function
       RHS_floatblock (List.length args)
   | Uprim (Pduprecord ((Record_regular | Record_inlined _), sz), _, _) ->
       RHS_block sz
-  | Uprim (Pduprecord (Record_extension _, sz), _, _) ->
+  | Uprim (Pduprecord (Record_extension, sz), _, _) ->
       RHS_block (sz + 1)
   | Uprim (Pduprecord (Record_float, sz), _, _) ->
       RHS_floatblock sz
