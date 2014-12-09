588af24a1873393787bd66b349b08cdda18839e0
Damien Doligez <damien.doligez-inria.fr>
2014-12-09 21:45:41 +0000

    fix a bug where some GC counters are not updated when the GC is called explicity from OCaml
    git-svn-id: http://caml.inria.fr/svn/ocaml/version/4.02@15643 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- byterun/minor_gc.c
+++ byterun/minor_gc.c
@@ -226,8 +226,10 @@ void caml_oldify_mopup (void)
 void caml_empty_minor_heap (void)
 {
   value **r;
+  uintnat prev_alloc_words;
 
   if (caml_young_ptr != caml_young_end){
+    prev_alloc_words = caml_allocated_words;
     caml_in_minor_collection = 1;
     caml_gc_message (0x02, "<", 0);
     caml_oldify_local_roots();
@@ -252,6 +254,8 @@ void caml_empty_minor_heap (void)
     clear_table (&caml_weak_ref_table);
     caml_gc_message (0x02, ">", 0);
     caml_in_minor_collection = 0;
+    caml_stat_promoted_words += caml_allocated_words - prev_alloc_words;
+    ++ caml_stat_minor_collections;
   }
   caml_final_empty_young ();
 #ifdef DEBUG
@@ -271,12 +275,8 @@ void caml_empty_minor_heap (void)
 */
 CAMLexport void caml_minor_collection (void)
 {
-  intnat prev_alloc_words = caml_allocated_words;
-
   caml_empty_minor_heap ();
 
-  caml_stat_promoted_words += caml_allocated_words - prev_alloc_words;
-  ++ caml_stat_minor_collections;
   caml_major_collection_slice (0);
   caml_force_major_slice = 0;
 
