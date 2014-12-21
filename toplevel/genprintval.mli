d67a128b130d18a5e06b3f53165ecc64b1aebc69
Gabriel Scherer <gabriel.scherer@gmail.com>
2014-12-21 08:25:41 +0000

    toplevel: allows the extension of 'genprintval.ml' with parameterised printer.
    git-svn-id: http://caml.inria.fr/svn/ocaml/version/4.02@15699 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- toplevel/genprintval.mli
+++ toplevel/genprintval.mli
@@ -33,11 +33,25 @@ module type EVALPATH =
     val same_value: valu -> valu -> bool
   end
 
+type ('a, 'b) gen_printer =
+  | Zero of 'b
+  | Succ of ('a -> ('a, 'b) gen_printer)
+
 module type S =
   sig
     type t
     val install_printer :
           Path.t -> Types.type_expr -> (formatter -> t -> unit) -> unit
+    val install_generic_printer :
+          Path.t -> Path.t ->
+          (int -> (int -> t -> Outcometree.out_value,
+                   t -> Outcometree.out_value) gen_printer) ->
+          unit
+    val install_generic_printer' :
+           Path.t -> Path.t ->
+           (formatter -> t -> unit,
+            formatter -> t -> unit) gen_printer ->
+           unit
     val remove_printer : Path.t -> unit
     val outval_of_untyped_exception : t -> Outcometree.out_value
     val outval_of_value :
