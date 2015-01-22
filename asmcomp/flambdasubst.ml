c2d7291a1724530957b731cefc2e9dc16cd50674
Pierre Chambart <pierre.chambart@ocamlpro.com>
2015-01-22 18:38:56 +0100

    Enforce the substitution claim
      A substitution is empty if not active

--- asmcomp/flambdasubst.ml
+++ asmcomp/flambdasubst.ml
@@ -14,9 +14,7 @@ open Symbol
 open Abstract_identifiers
 open Flambda
 
-type t = {
-  (* Claim: the substitution is empty if [active] is [false]. *)
-  active : bool;
+type tbl = {
   sb_var : Variable.t Variable.Map.t;
   sb_sym : Variable.t SymbolMap.t;
   sb_exn : Static_exception.t Static_exception.Map.t;
@@ -26,10 +24,10 @@ type t = {
      the substitution recursively because there can be name
      clash *)
 }
+type t = tbl option
 type subst = t
 
-let empty = {
-  active = false;
+let empty_tbl = {
   sb_var = Variable.Map.empty;
   sb_sym = SymbolMap.empty;
   sb_exn = Static_exception.Map.empty;
@@ -37,9 +35,15 @@ let empty = {
   back_sym = Variable.Map.empty;
 }
 
-let new_substitution t = { empty with active = t.active }
+let empty = None
 
-let activate t = { t with active = true; }
+let new_substitution = function
+  | None -> None
+  | Some t -> Some empty_tbl
+
+let activate = function
+  | None -> Some empty_tbl
+  | Some _ as t -> t
 
 let add_sb_sym sb sym id' =
   let back_sym =
@@ -63,28 +67,40 @@ let rec add_sb_var sb id id' =
     Variable.Map.add id' (id :: l) sb.back_var in
   { sb with back_var }
 
-let add_sb_exn t i i' =
-  { t with sb_exn = Static_exception.Map.add i i' t.sb_exn; }
-
 let sb_exn t i =
-  try Static_exception.Map.find i t.sb_exn with Not_found -> i
+  match t with
+  | None ->
+     i
+  | Some t ->
+     try Static_exception.Map.find i t.sb_exn with Not_found -> i
 
 let new_subst_exn t i =
-  if t.active then
-    let i' = Static_exception.create () in
-    let t = add_sb_exn t i i' in
-    i', t
-  else i, t
+  match t with
+  | None -> i, t
+  | Some t ->
+     let i' = Static_exception.create () in
+     let sb_exn = Static_exception.Map.add i i' t.sb_exn in
+     i', Some { t with sb_exn; }
 
 let freshen_var var =
   Variable.rename ~current_compilation_unit:(Compilenv.current_unit ()) var
 
+let active_new_subst_id t id =
+  let id' = freshen_var id in
+  let t = add_sb_var t id id' in
+  id', t
+
 let new_subst_id t id =
-  if t.active then
-    let id' = freshen_var id in
-    let t = add_sb_var t id id' in
-    id', t
-  else id, t
+  match t with
+  | None -> id, t
+  | Some t ->
+     let id', t = active_new_subst_id t id in
+     id', Some t
+
+let active_new_subst_ids' t ids =
+  List.fold_right (fun id (ids, t) ->
+      let id', t = active_new_subst_id t id in
+      id' :: ids, t) ids ([], t)
 
 let new_subst_ids t defs =
   List.fold_right (fun (id, lam) (defs, t) ->
@@ -96,37 +112,26 @@ let new_subst_ids' t ids =
       let id', t = new_subst_id t id in
       id' :: ids, t) ids ([], t)
 
-let find_var_exn t id =
+let active_find_var_exn t id =
   try Variable.Map.find id t.sb_var with
   | Not_found ->
-    Misc.fatal_error (Format.asprintf "find_var: can't find %a@."
-        Variable.print id)
+      Misc.fatal_error (Format.asprintf "find_var: can't find %a@."
+          Variable.print id)
 
 let subst_var t var =
-  try Variable.Map.find var t.sb_var with
-  | Not_found -> var
+  match t with
+  | None -> var
+  | Some t ->
+     try Variable.Map.find var t.sb_var with
+     | Not_found -> var
 
 let find_symbol_exn t sym =
-  SymbolMap.find sym t.sb_sym
+  match t with
+  | None -> raise Not_found
+  | Some t ->
+     SymbolMap.find sym t.sb_sym
 
 module Alpha_renaming_map_for_ids_and_bound_vars_of_closures = struct
-  (* Tables used for identifiers substitution in
-     Fclosure ("ids of closures") and Fvariable_in_closure ("bound vars
-     of closures") constructions.
-     This information is propagated bottom up. This is
-     populated when inlining a function containing a closure
-     declaration.
-
-     For instance,
-       [let f x =
-          let g y = ... x ... in
-          ... g.x ...           (Fvariable_in_closure x)
-          ... g 1 ...           (FApply (Fclosure g ...))
-          ]
-     if f is inlined g is renamed. The approximation of g will
-     cary this table such that later the access to the field x
-     of g and selection of g in the closure can be substituted.
-   *)
 
   type t =
     { ffs_fv : Var_within_closure.t Var_within_closure.Map.t;
@@ -136,26 +141,23 @@ module Alpha_renaming_map_for_ids_and_bound_vars_of_closures = struct
     { ffs_fv = Var_within_closure.Map.empty; ffs_fun = Closure_id.Map.empty }
 
   let new_subst_fv t id subst =
-    if subst.active
-    then
-      let id' = freshen_var id in
-      let subst = add_sb_var subst id id' in
-      let off = Var_within_closure.wrap id in
-      let off' = Var_within_closure.wrap id' in
-      let off_sb = Var_within_closure.Map.add off off' t.ffs_fv in
-      id', subst, { t with ffs_fv = off_sb; }
-    else id, subst, t
+    match subst with
+    | None -> id, subst, t
+    | Some subst ->
+        let id' = freshen_var id in
+        let subst = add_sb_var subst id id' in
+        let off = Var_within_closure.wrap id in
+        let off' = Var_within_closure.wrap id' in
+        let off_sb = Var_within_closure.Map.add off off' t.ffs_fv in
+        id', Some subst, { t with ffs_fv = off_sb; }
 
   let new_subst_fun t id subst =
-    if subst.active
-    then
-      let id' = freshen_var id in
-      let subst = add_sb_var subst id id' in
-      let off = Closure_id.wrap id in
-      let off' = Closure_id.wrap id' in
-      let off_sb = Closure_id.Map.add off off' t.ffs_fun in
-      id', subst, { t with ffs_fun = off_sb; }
-    else id, subst, t
+    let id' = freshen_var id in
+    let subst = add_sb_var subst id id' in
+    let off = Closure_id.wrap id in
+    let off' = Closure_id.wrap id' in
+    let off_sb = Closure_id.Map.add off off' t.ffs_fun in
+    id', subst, { t with ffs_fun = off_sb; }
 
   (** Returns :
       * The map of new_identifiers -> expression
@@ -176,13 +178,14 @@ module Alpha_renaming_map_for_ids_and_bound_vars_of_closures = struct
       subst_free_vars must have been used to build off_sb
    *)
   let ffuns_subst t subst ffuns =
-    if subst.active
-    then
+    match subst with
+    | None -> ffuns, subst, t
+    | Some subst ->
       let subst_ffunction fun_id ffun subst =
-        let params, subst = new_subst_ids' subst ffun.params in
+        let params, subst = active_new_subst_ids' subst ffun.params in
         let free_variables =
           Variable.Set.fold (fun id set ->
-              Variable.Set.add (find_var_exn subst id) set)
+              Variable.Set.add (active_find_var_exn subst id) set)
             ffun.free_variables Variable.Set.empty in
         (* It is not a problem to share the substitution of parameter
            names between function: There should be no clash *)
@@ -201,14 +204,13 @@ module Alpha_renaming_map_for_ids_and_bound_vars_of_closures = struct
       let funs, subst =
         Variable.Map.fold (fun orig_id ffun (funs, subst) ->
             let ffun, subst = subst_ffunction orig_id ffun subst in
-            let id = find_var_exn subst orig_id in
+            let id = active_find_var_exn subst orig_id in
             let funs = Variable.Map.add id ffun funs in
             funs, subst)
           ffuns.funs (Variable.Map.empty, subst) in
       { ident = Set_of_closures_id.create (Compilenv.current_unit ());
         compilation_unit = Compilenv.current_unit ();
-        funs }, subst, t
-    else ffuns, subst, t
+        funs }, Some subst, t
 
   let subst_function_declarations_and_free_variables subst fv ffuns =
     let fv, subst, t = subst_free_vars fv subst in
--- asmcomp/flambdasubst.mli
+++ asmcomp/flambdasubst.mli
@@ -24,6 +24,15 @@ val new_subst_exn :
   Static_exception.Map.key ->
   Static_exception.Map.key * t
 val new_subst_id : t -> Variable.t -> Variable.t * t
+(** [new_subst_id subst var]
+    If [subst] is an active substitution:
+      It retuns a fresh variable [new_var] and
+      adds [var] -> [new_var] to the substitution.
+      If a substitution [other_var] -> [var] or [symbol] -> [var] is present
+      in [subst], it will also add [other_var] -> [new_var] and
+      [symbo] -> [new_var].
+    If [subst] is inactive, this is the identity.
+*)
 val new_subst_ids :
   t ->
   (Variable.t * 'a) list ->
@@ -43,6 +52,24 @@ val freshen_var : Variable.t -> Variable.t
 val subst_var : t -> Variable.t -> Variable.t
 
 module Alpha_renaming_map_for_ids_and_bound_vars_of_closures : sig
+  (* Tables used for identifiers substitution in
+     Fclosure ("ids of closures") and Fvariable_in_closure ("bound vars
+     of closures") constructions.
+     This information is propagated bottom up. This is
+     populated when inlining a function containing a closure
+     declaration.
+
+     For instance,
+       [let f x =
+          let g y = ... x ... in
+          ... g.x ...           (Fvariable_in_closure x)
+          ... g 1 ...           (FApply (Fclosure g ...))
+          ]
+     if f is inlined g is renamed. The approximation of g will
+     cary this table such that later the access to the field x
+     of g and selection of g in the closure can be substituted.
+   *)
+
   type t
   val empty : t
 
