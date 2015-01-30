c17d3068cd2fc994db930ee026b595b9139a5f05
Jérémie Dimino <jeremie@dimino.org>
2015-01-30 11:44:41 +0000

    try to unbreak travis
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@15800 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -3,11 +3,13 @@ i386)
   ./configure
   make world.opt
   sudo make install
-  cd testsuite && make all
+  cd testsuite && make all && cd ..
+  mkdir external-packages
+  cd external-packages
   git clone git://github.com/ocaml/camlp4
-  cd camlp4 && ./configure && make && sudo make install
+  cd camlp4 && ./configure && make && sudo make install && cd ..
   git clone git://github.com/ocaml/opam
-  cd opam && ./configure && make lib-ext && make && sudo make install
+  cd opam && ./configure && make lib-ext && make && sudo make install && cd ..
   git config --global user.email "some@name.com"
   git config --global user.name "Some Name"
   opam init -y -a git://github.com/ocaml/opam-repository
