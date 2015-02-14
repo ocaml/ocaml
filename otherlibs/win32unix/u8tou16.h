35184c1792f6cdb3e104245fbe09577bf2ff2c0a
ygrek <ygrek@autistici.org>
2015-02-14 12:26:28 +0800

    remove duplicate copy of u8tou16.c

--- otherlibs/win32unix/u8tou16.h
+++
@@ -1,8 +0,0 @@
-#ifndef WCHAR
-typedef unsigned short WCHAR;
-#endif
-
-int is_valid_utf8(const char *s);
-unsigned char * ansi_to_utf16(const char * str);
-unsigned char * utf8_to_utf16(const char * str);
-unsigned char * utf16_to_utf8(const unsigned char * str);
