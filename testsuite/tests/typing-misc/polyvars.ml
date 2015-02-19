982b174cb4ec5b013d647e485c98dee1c7ccfef0
Jacques Garrigue <garrigue at math.nagoya-u.ac.jp>
2015-02-19 10:03:46 +0000

    Fix PR#6787
    git-svn-id: http://caml.inria.fr/svn/ocaml/version/4.02@15852 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- testsuite/tests/typing-misc/polyvars.ml
+++ testsuite/tests/typing-misc/polyvars.ml
@@ -5,3 +5,11 @@ let f x = ignore (match x with `A|`B -> 1); ignore (x : [`A]);;
 
 let f (x : [< `A | `B]) = match x with `A | `B | `C -> 0;; (* warn *)
 let f (x : [`A | `B]) = match x with `A | `B | `C -> 0;; (* fail *)
+
+(* PR#6787 *)
+let revapply x f = f x;;
+
+let f x (g : [< `Foo]) =
+  let y = `Bar x, g in
+  revapply y (fun ((`Bar i), _) -> i);;
+(* f : 'a -> [< `Foo ] -> 'a *)
