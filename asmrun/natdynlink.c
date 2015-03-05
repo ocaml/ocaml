1551578437b5d6b39de2180e7119f5c2c1bf7ec5
Stephen Dolan <stephen.dolan@cl.cam.ac.uk>
2015-03-05 12:36:18 +0000

    Remove naked pointers and Obj.magic from natdynlink.

--- asmrun/natdynlink.c
+++ asmrun/natdynlink.c
@@ -24,6 +24,13 @@
 #include <stdio.h>
 #include <string.h>
 
+#define Handle_val(v) (*((void **) (v)))
+static value Val_handle(void* handle) {
+  value res = caml_alloc_small(1, Abstract_tag);
+  Handle_val(res) = handle;
+  return res;
+}
+
 static void *getsym(void *handle, char *module, char *name){
   char *fullname = caml_strconcat(3, "caml", module, name);
   void *sym;
@@ -37,7 +44,7 @@ extern char caml_globals_map[];
 
 CAMLprim value caml_natdynlink_getmap(value unit)
 {
-  return (value)caml_globals_map;
+  return caml_input_value_from_malloc(caml_globals_map, 0);
 }
 
 CAMLprim value caml_natdynlink_globals_inited(value unit)
@@ -47,32 +54,36 @@ CAMLprim value caml_natdynlink_globals_inited(value unit)
 
 CAMLprim value caml_natdynlink_open(value filename, value global)
 {
-  CAMLparam1 (filename);
-  CAMLlocal1 (res);
+  CAMLparam2 (filename, global);
+  CAMLlocal3 (res, handle, header);
   void *sym;
-  void *handle;
+  void *dlhandle;
 
   /* TODO: dlclose in case of error... */
 
-  handle = caml_dlopen(String_val(filename), 1, Int_val(global));
+  dlhandle = caml_dlopen(String_val(filename), 1, Int_val(global));
 
-  if (NULL == handle)
-    CAMLreturn(caml_copy_string(caml_dlerror()));
+  if (NULL == dlhandle)
+    caml_failwith(caml_dlerror());
 
-  sym = caml_dlsym(handle, "caml_plugin_header");
+  sym = caml_dlsym(dlhandle, "caml_plugin_header");
   if (NULL == sym)
-    CAMLreturn(caml_copy_string("not an OCaml plugin"));
+    caml_failwith("not an OCaml plugin");
+
+  handle = Val_handle(dlhandle);
+  header = caml_input_value_from_malloc(sym, 0);
 
   res = caml_alloc_tuple(2);
-  Field(res, 0) = (value) handle;
-  Field(res, 1) = (value) (sym);
+  Field(res, 0) = handle;
+  Field(res, 1) = header;
   CAMLreturn(res);
 }
 
-CAMLprim value caml_natdynlink_run(void *handle, value symbol) {
-  CAMLparam1 (symbol);
+CAMLprim value caml_natdynlink_run(value handle_v, value symbol) {
+  CAMLparam2 (handle_v, symbol);
   CAMLlocal1 (result);
   void *sym,*sym2;
+  void* handle = Handle_val(handle_v);
   struct code_fragment * cf;
 
 #define optsym(n) getsym(handle,unit,n)
@@ -115,7 +126,7 @@ CAMLprim value caml_natdynlink_run(void *handle, value symbol) {
 CAMLprim value caml_natdynlink_run_toplevel(value filename, value symbol)
 {
   CAMLparam2 (filename, symbol);
-  CAMLlocal2 (res, v);
+  CAMLlocal3 (res, v, handle_v);
   void *handle;
 
   /* TODO: dlclose in case of error... */
@@ -127,8 +138,9 @@ CAMLprim value caml_natdynlink_run_toplevel(value filename, value symbol)
     v = caml_copy_string(caml_dlerror());
     Store_field(res, 0, v);
   } else {
+    handle_v = Val_handle(handle);
     res = caml_alloc(1,0);
-    v = caml_natdynlink_run(handle, symbol);
+    v = caml_natdynlink_run(handle_v, symbol);
     Store_field(res, 0, v);
   }
   CAMLreturn(res);
