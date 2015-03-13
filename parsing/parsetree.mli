77cd2c8f203fff107ddb9a1ce83d8f825a62d5d8
Jérémie Dimino <jeremie@dimino.org>
2015-03-13 11:10:12 +0000

    Add rec_flag to type items in the parsetree and typedtree
    git-svn-id: http://caml.inria.fr/svn/ocaml/version/4.02@15926 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- parsing/parsetree.mli
+++ parsing/parsetree.mli
@@ -643,7 +643,7 @@ and signature_item_desc =
           val x: T
           external x: T = "s1" ... "sn"
          *)
-  | Psig_type of type_declaration list
+  | Psig_type of rec_flag * type_declaration list
         (* type t1 = ... and ... and tn = ... *)
   | Psig_typext of type_extension
         (* type t1 += ... *)
@@ -769,7 +769,7 @@ and structure_item_desc =
          *)
   | Pstr_primitive of value_description
         (* external x: T = "s1" ... "sn" *)
-  | Pstr_type of type_declaration list
+  | Pstr_type of rec_flag * type_declaration list
         (* type t1 = ... and ... and tn = ... *)
   | Pstr_typext of type_extension
         (* type t1 += ... *)
