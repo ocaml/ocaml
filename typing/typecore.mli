4582f93e8352c527bacc1cf845f00f4ada779dcb
Jeremy Yallop <yallop@gmail.com>
2015-03-20 15:03:08 +0000

    Add Illegal_letrec_pat to Typecore.error.

--- typing/typecore.mli
+++ typing/typecore.mli
@@ -124,6 +124,7 @@ type error =
   | Not_an_extension_constructor
   | Literal_overflow of string
   | Unknown_literal of string * char
+  | Illegal_letrec_pat
 
 exception Error of Location.t * Env.t * error
 exception Error_forward of Location.error
