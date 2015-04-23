142509985ab05f47fbf3eca383ff592b251bf4bc
Pierre Chambart <pierre.chambart@ocamlpro.com>
2015-04-23 13:51:14 +0200

    Tests for warnings related to the [@inlined] attribute (47, 51, 52, 53)

--- testsuite/tests/warnings/w47_inline.ml
+++ testsuite/tests/warnings/w47_inline.ml
@@ -1,8 +1,13 @@
 
-let a = (fun x -> x) [@inline]
-let b = (fun x -> x) [@inline never]
-let c = (fun x -> x) [@inline force]
-let d = (fun x -> x) [@inline malformed attribute]
-let e = (fun x -> x) [@inline malformed_attribute]
-let f = (fun x -> x) [@inline : malformed_attribute]
-let g = (fun x -> x) [@inline ? malformed_attribute]
+let a = (fun x -> x) [@inline] (* accepted *)
+let b = (fun x -> x) [@inline never] (* accepted *)
+let c = (fun x -> x) [@inline force] (* accepted *)
+let d = (fun x -> x) [@inline malformed attribute] (* rejected *)
+let e = (fun x -> x) [@inline malformed_attribute] (* rejected *)
+let f = (fun x -> x) [@inline : malformed_attribute] (* rejected *)
+let g = (fun x -> x) [@inline ? malformed_attribute] (* rejected *)
+
+let h x = (a [@inlined]) x (* accepted *)
+let i x = (a [@inlined never]) x (* accepted *)
+let j x = (a [@inlined force]) x (* accepted *)
+let k x = (a [@inlined malformed]) x (* rejected *)
--- testsuite/tests/warnings/w52.ml
+++ testsuite/tests/warnings/w52.ml
@@ -1,3 +1,7 @@
 
 let f = (fun x -> x) [@inline] [@inline never]
 let g = (fun x -> x) [@inline] [@something_else] [@ocaml.inline]
+
+let h x = (g [@inlined] [@ocaml.inlined never]) x
+
+let v = ((fun x -> x) [@inline] [@inlined]) 1 (* accepted *)
