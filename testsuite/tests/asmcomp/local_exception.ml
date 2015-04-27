cb16d409b1031f35c5c82656bc2c8f3684fe2d1d
Pierre Chambart <pierre.chambart@ocamlpro.com>
2015-04-27 17:38:55 +0200

    Replace set_oo_id x by let v = x in (set_oo_id x; v).
    This allows to statically allocate constant exceptions

---
+++ testsuite/tests/asmcomp/local_exception.ml
@@ -0,0 +1,29 @@
+(***********************************************************************)
+(*                                                                     *)
+(*                                OCaml                                *)
+(*                                                                     *)
+(*                       Pierre Chambart, OCamlPro                     *)
+(*                                                                     *)
+(*  Copyright 2015 Institut National de Recherche en Informatique et   *)
+(*  en Automatique.  All rights reserved.  This file is distributed    *)
+(*  under the terms of the Q Public License version 1.0.               *)
+(*                                                                     *)
+(***********************************************************************)
+
+(* Ensures that global exception can be statically allocated *)
+
+exception My_exception of int
+
+let () =
+  let x0 = Gc.allocated_bytes () in
+  let x1 = Gc.allocated_bytes () in
+  for i = 1 to 1000 do
+    try
+      if i mod 2 = 0
+      then raise (My_exception 123)
+      else failwith "some text"
+    with _ -> ()
+  done;
+  let x2 = Gc.allocated_bytes () in
+  assert(x1 -. x0 = x2 -. x1)
+     (* check that we have not allocated anything between x1 and x2 *)
