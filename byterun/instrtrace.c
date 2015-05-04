0477bab3bf1d3fd8abf2f4e8ba6f79fdb5996826
Damien Doligez <damien.doligez-inria.fr>
2015-05-04 15:44:40 +0000

    deduplicate some startup code and fix PR#6808: the parsing of OCAMLRUNPARAM is too lax
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@16074 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- byterun/instrtrace.c
+++ byterun/instrtrace.c
@@ -25,14 +25,13 @@
 #include "caml/opnames.h"
 #include "caml/prims.h"
 #include "caml/stacks.h"
+#include "caml/startup_aux.h"
 
 extern code_t caml_start_code;
 
 intnat caml_icount = 0;
 
 void caml_stop_here () {}
-
-int caml_trace_flag = 0;
 
 void caml_disasm_instr(pc)
      code_t pc;
@@ -252,7 +251,7 @@ caml_trace_accu_sp_file (value accu, value * sp, code_t prog, int proglen,
   caml_trace_value_file (accu, prog, proglen, f);
   fprintf (f, "\n sp=%#" ARCH_INTNAT_PRINTF_FORMAT "x @%ld:",
            (intnat) sp, caml_stack_high - sp);
-  for (p = sp, i = 0; i < 12 + (1 << caml_trace_flag) && p < caml_stack_high;
+  for (p = sp, i = 0; i < 12 + (1 << caml_trace_level) && p < caml_stack_high;
        p++, i++) {
     fprintf (f, "\n[%ld] ", caml_stack_high - p);
     caml_trace_value_file (*p, prog, proglen, f);
