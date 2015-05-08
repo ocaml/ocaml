a755f51db2c072deabeb4fd1aa7d25473dad3743
Gerd Stolpmann <gerd@gerd-stolpmann.de>
2015-05-08 14:53:00 +0200

    doc, build driver for iOS

--- cross-ios-build.sh
+++ cross-ios-build.sh
@@ -2,25 +2,38 @@
 
 # Build for iOS
 
-platform=iPhoneOS
+if [ -z "$platform" ]; then
+    platform=iPhoneOS
+fi
 # or iPhoneSimulator
 
-arch=armv7
+if [ -z "$arch" ]; then
+    arch=armv7
+fi
 # valid for phone: armv6, armv7, armv7s, arm64
 # valid for simulator: i386, x86_64
 
-sdk=8.3
+if [ -z "$sdk" ]; then
+    sdk=8.3
+fi
 
 
 export PLT="/Applications/Xcode.app/Contents/Developer/Platforms/${platform}.platform"
 export SDK="/Developer/SDKs/${platform}${sdk}.sdk"
+
+if [ ! -d "$PLT" ]; then
+    echo "Error: not found: $PLT" >&2; exit
+fi
+if [ ! -d "$PLT$SDK" ]; then
+    echo "Error: not found: $PLT$SDK" >&2; exit
+fi
 
 ./configure \
         -cc "gcc -arch $arch -isysroot $PLT$SDK -miphoneos-version-min=$sdk" \
         -as "gcc -arch $arch -isysroot $PLT$SDK -miphoneos-version-min=$sdk -c" \
         -aspp "gcc -arch $arch -isysroot $PLT$SDK -miphoneos-version-min=$sdk -c" \
         -libs "-Wl,-syslibroot,$PLT$SDK" \
-        -no-curses -no-pthread -no-shared-libs \
+        -no-curses -no-shared-libs \
         -no-debugger -no-ocamldoc -no-ocamlbuild -no-graph \
         -target $arch-apple-darwin \
         "$@" || exit
