826afb2cb9bab17c5813a9c2188238f1f0747596
KC Sivaramakrishnan <sk826@cl.cam.ac.uk>
2015-06-05 11:13:36 +0100

    Editing travis ci scripts for multicore.

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,15 +1,26 @@
+#case $XARCH in
+#i386)
+#  ./configure
+#  make world.opt
+#  sudo make install
+#  cd testsuite && make all
+#  git clone git://github.com/ocaml/camlp4
+#  cd camlp4 && ./configure && make && sudo make install
+#  git clone git://github.com/ocaml/opam
+#  cd opam && ./configure && make lib-ext && make && sudo make install
+#  opam init -y -a git://github.com/ocaml/opam-repository
+#  opam install -y utop
+#  ;;
+#*)
+#  echo unknown arch
+#  exit 1
+#  ;;
+#esac
+
 case $XARCH in
-i386)
+x86_64)
   ./configure
-  make world.opt
-  sudo make install
-  cd testsuite && make all
-  git clone git://github.com/ocaml/camlp4
-  cd camlp4 && ./configure && make && sudo make install
-  git clone git://github.com/ocaml/opam
-  cd opam && ./configure && make lib-ext && make && sudo make install
-  opam init -y -a git://github.com/ocaml/opam-repository
-  opam install -y utop
+  make world
   ;;
 *)
   echo unknown arch
