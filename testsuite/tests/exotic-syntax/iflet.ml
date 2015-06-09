60f65f1f33cbb1048c2685aebb847eb9b46bf078
Simon Cruanes <simon.cruanes.2007@m4x.org>
2015-06-09 14:35:57 +0200

    add a few tests

--- testsuite/tests/exotic-syntax/iflet.ml
+++ testsuite/tests/exotic-syntax/iflet.ml
@@ -11,3 +11,15 @@ if let Some x = Some 42 then assert_ (x=42) else fail ();;
 if let [1;2;y] = [1;2;3] then assert_ (y=3) else fail ();;
 
 if let [x] = [1] and [y] = [2] then assert_ (x=1 && y=2) ;;
+
+type foo =
+  | A of int
+  | B of int * string
+  | C
+;;
+
+let f foo =
+  if let A x | B (x, _) = foo then ok ();;
+
+f (A 42);;
+f (B (0, "foo"));;
