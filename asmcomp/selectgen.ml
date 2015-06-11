da42a4bfc051871ffe182f1fc80cf702b93861de
Frédéric Bour <frederic.bour@lakaban.net>
2015-06-11 14:14:22 +0100

    Rename retloc to retaddr after inlining

--- asmcomp/selectgen.ml
+++ asmcomp/selectgen.ml
@@ -42,7 +42,7 @@ let oper_result_type = function
   | Cintoffloat -> typ_int
   | Craise _ -> typ_void
   | Ccheckbound _ -> typ_void
-  | Cretloc -> typ_int
+  | Cretaddr -> typ_int
 
 (* Infer the size in bytes of the result of a simple expression *)
 
@@ -288,7 +288,7 @@ method select_operation op args =
   | (Cdivf, _) -> (Idivf, args)
   | (Cfloatofint, _) -> (Ifloatofint, args)
   | (Cintoffloat, _) -> (Iintoffloat, args)
-  | (Cretloc, _) -> (Iretloc, args)
+  | (Cretaddr, _) -> (Iretaddr, args)
   | (Ccheckbound _, _) -> self#select_arith Icheckbound args
   | _ -> fatal_error "Selection.select_oper"
 
