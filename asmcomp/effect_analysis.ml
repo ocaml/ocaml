9d4f597e1f2e220c2cb215b62278ea8c847695d8
Mark Shinwell <mshinwell@janestreet.com>
2015-06-17 11:34:18 +0100

    reformatting

--- asmcomp/effect_analysis.ml
+++ asmcomp/effect_analysis.ml
@@ -36,33 +36,31 @@ let rec no_effects (flam : _ Flambda.t) =
   | Fvar _ | Fsymbol _ | Fconst _ -> true
   | Flet (_, _, def, body, _) -> no_effects def && no_effects body
   | Fletrec (defs, body, _) ->
-      no_effects body &&
-      List.for_all (fun (_, def) -> no_effects def) defs
+    no_effects body
+      && List.for_all (fun (_, def) -> no_effects def) defs
   | Fprim (p, args, _, _) ->
-      no_effects_prim p &&
-      List.for_all no_effects args
+    no_effects_prim p
+      && List.for_all no_effects args
   | Fset_of_closures ({ cl_free_var }, _) ->
-      Variable.Map.for_all (fun _id def -> no_effects def) cl_free_var
+    Variable.Map.for_all (fun _id def -> no_effects def) cl_free_var
   | Fclosure ({ fu_closure = lam }, _) -> no_effects lam
   | Fvar_within_closure ({ vc_closure }, _) -> no_effects vc_closure
   | Fifthenelse (cond, ifso, ifnot, _) ->
-      no_effects cond &&
-      no_effects ifso &&
-      no_effects ifnot
+    no_effects cond && no_effects ifso && no_effects ifnot
   | Fswitch (lam, sw, _) ->
-      let aux (_, lam) = no_effects lam in
-      no_effects lam &&
-      List.for_all aux sw.fs_blocks &&
-      List.for_all aux sw.fs_consts &&
-      Misc.may_default no_effects sw.fs_failaction true
+    let aux (_, lam) = no_effects lam in
+    no_effects lam
+      && List.for_all aux sw.fs_blocks
+      && List.for_all aux sw.fs_consts
+      && Misc.may_default no_effects sw.fs_failaction true
   | Fstringswitch (lam, sw, def, _) ->
-      no_effects lam &&
-      List.for_all (fun (_, lam) -> no_effects lam) sw &&
-      Misc.may_default no_effects def true
+    no_effects lam
+      && List.for_all (fun (_, lam) -> no_effects lam) sw
+      && Misc.may_default no_effects def true
   | Fstaticcatch (_, _, body, _, _)
   | Ftrywith (body, _, _, _) ->
-      (* the raise is effectful, no need to test the handler *)
-      no_effects body
+    (* the raise is effectful, no need to test the handler *)
+    no_effects body
   | Fsequence (l1, l2, _) -> no_effects l1 && no_effects l2
   | Fwhile _ | Ffor _ | Fapply _ | Fsend _ | Fassign _
   | Fstaticraise _ -> false
