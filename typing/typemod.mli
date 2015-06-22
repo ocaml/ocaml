48b68b51e2dbdd144c921bcef5c68312b24485fa
Thomas Refis <thomas.refis@gmail.com>
2015-06-22 14:07:47 +0100

    Cleaner fix for PR#6899: Typemod.check_nongen_schemes works on Types.signature instead of Typedtree.structure

--- typing/typemod.mli
+++ typing/typemod.mli
@@ -31,7 +31,7 @@ val type_interface:
 val transl_signature:
         Env.t -> Parsetree.signature -> Typedtree.signature
 val check_nongen_schemes:
-        Env.t -> Typedtree.structure_item list -> unit
+        Env.t -> Types.signature -> unit
 val type_open_:
         ?toplevel:bool -> Asttypes.override_flag ->
         Env.t -> Location.t -> Longident.t Asttypes.loc -> Path.t * Env.t
