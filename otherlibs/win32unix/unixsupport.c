75a7b333ed2319f51be2b46fe6b38830b10acf4e
Gabriel Scherer <gabriel.scherer@gmail.com>
2015-07-26 19:28:48 +0000

    GPR#165: Added define guards around the macros in unixsupport and for the isnan and isfinite macro.
    (Bernhard Schommer)
    
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@16261 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- otherlibs/win32unix/unixsupport.c
+++ otherlibs/win32unix/unixsupport.c
@@ -170,45 +170,118 @@ void win32_maperr(DWORD errcode)
 }
 
 /* Windows socket errors */
-
+#ifndef EWOULDBLOCK
 #define EWOULDBLOCK             -WSAEWOULDBLOCK
+#endif
+#ifndef EINPROGRESS
 #define EINPROGRESS             -WSAEINPROGRESS
+#endif
+#ifndef EALREADY
 #define EALREADY                -WSAEALREADY
+#endif
+#ifndef ENOTSOCK
 #define ENOTSOCK                -WSAENOTSOCK
+#endif
+#ifndef EDESTADDRREQ
 #define EDESTADDRREQ            -WSAEDESTADDRREQ
+#endif
+#ifndef EMSGSIZE
 #define EMSGSIZE                -WSAEMSGSIZE
+#endif
+#ifndef EPROTOTYPE
 #define EPROTOTYPE              -WSAEPROTOTYPE
+#endif
+#ifndef ENOPROTOOPT
 #define ENOPROTOOPT             -WSAENOPROTOOPT
+#endif
+#ifndef EPROTONOSUPPORT
 #define EPROTONOSUPPORT         -WSAEPROTONOSUPPORT
+#endif
+#ifndef ESOCKTNOSUPPORT
 #define ESOCKTNOSUPPORT         -WSAESOCKTNOSUPPORT
+#endif
+#ifndef EOPNOTSUPP
 #define EOPNOTSUPP              -WSAEOPNOTSUPP
+#endif
+#ifndef EPFNOSUPPORT
 #define EPFNOSUPPORT            -WSAEPFNOSUPPORT
+#endif
+#ifndef EAFNOSUPPORT
 #define EAFNOSUPPORT            -WSAEAFNOSUPPORT
+#endif
+#ifndef EADDRINUSE
 #define EADDRINUSE              -WSAEADDRINUSE
+#endif
+#ifndef EADDRNOTAVAIL
 #define EADDRNOTAVAIL           -WSAEADDRNOTAVAIL
+#endif
+#ifndef ENETDOWN
 #define ENETDOWN                -WSAENETDOWN
+#endif
+#ifndef ENETUNREACH
 #define ENETUNREACH             -WSAENETUNREACH
+#endif
+#ifndef ENETRESET
 #define ENETRESET               -WSAENETRESET
+#endif
+#ifndef ECONNABORTED
 #define ECONNABORTED            -WSAECONNABORTED
+#endif
+#ifndef ECONNRESET
 #define ECONNRESET              -WSAECONNRESET
+#endif
+#ifndef ENOBUFS
 #define ENOBUFS                 -WSAENOBUFS
+#endif
+#ifndef EISCONN
 #define EISCONN                 -WSAEISCONN
+#endif
+#ifndef ENOTCONN
 #define ENOTCONN                -WSAENOTCONN
+#endif
+#ifndef ESHUTDOWN
 #define ESHUTDOWN               -WSAESHUTDOWN
+#endif
+#ifndef ETOOMANYREFS
 #define ETOOMANYREFS            -WSAETOOMANYREFS
+#endif
+#ifndef ETIMEDOUT
 #define ETIMEDOUT               -WSAETIMEDOUT
+#endif
+#ifndef ECONNREFUSED
 #define ECONNREFUSED            -WSAECONNREFUSED
+#endif
+#ifndef ELOOP
 #define ELOOP                   -WSAELOOP
+#endif
+#ifndef EHOSTDOWN
 #define EHOSTDOWN               -WSAEHOSTDOWN
+#endif
+#ifndef EHOSTUNREACH
 #define EHOSTUNREACH            -WSAEHOSTUNREACH
+#endif
+#ifndef EPROCLIM
 #define EPROCLIM                -WSAEPROCLIM
+#endif
+#ifndef EUSERS
 #define EUSERS                  -WSAEUSERS
+#endif
+#ifndef EDQUOT
 #define EDQUOT                  -WSAEDQUOT
+#endif
+#ifndef ESTALE
 #define ESTALE                  -WSAESTALE
+#endif
+#ifndef EREMOTE
 #define EREMOTE                 -WSAEREMOTE
+#endif
 
+#ifndef EOVERFLOW
 #define EOVERFLOW -ERROR_ARITHMETIC_OVERFLOW
+#endif
+#ifndef EACCESS
 #define EACCESS EACCES
+#endif
 
 int error_table[] = {
   E2BIG, EACCESS, EAGAIN, EBADF, EBUSY, ECHILD, EDEADLK, EDOM,
