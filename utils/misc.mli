291a5bffd625c4a2738b92cdde90120d2334aa94
Gabriel Scherer <gabriel.scherer@gmail.com>
2015-08-15 15:57:49 +0000

    add Color module Misc
    (Simon Cruanes)
    
    We use bold magenta for warnings, because it works on terminals with
    white background.
    
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@16347 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- utils/misc.mli
+++ utils/misc.mli
@@ -165,3 +165,56 @@ val cut_at : string -> char -> string * string
    Raise [Not_found] if the character does not appear in the string
    @since 4.01
 *)
+
+
+
+
+
+(* Color handling *)
+module Color : sig
+  type color =
+    | Black
+    | Red
+    | Green
+    | Yellow
+    | Blue
+    | Magenta
+    | Cyan
+    | White
+  ;;
+
+  type style =
+    | FG of color (* foreground *)
+    | BG of color (* background *)
+    | Bold
+    | Reset
+
+  val ansi_of_style_l : style list -> string
+  (* ANSI escape sequence for the given style *)
+
+  type styles = {
+    error: style list;
+    warning: style list;
+    loc: style list;
+  }
+
+  val default_styles: styles
+  val get_styles: unit -> styles
+  val set_styles: styles -> unit
+
+  type options = On | Off | Auto
+  (* setup for colors:
+     - on means colors are printed unconditionally
+     - off means colors are not printed
+     - auto means colors are printed on channels that "look like" they support colors
+  *)
+
+  val setup : options -> unit
+  (* [setup o] will enable or disable color handling on standard formatters
+     according to the value of [o].
+     Only the first call to this function has an effect. *)
+
+  val set_color_tag_handling : Format.formatter -> unit
+  (* adds functions to support color tags to the given formatter. *)
+end
+
