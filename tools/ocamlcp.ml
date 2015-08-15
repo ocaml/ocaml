cb3bb152ab337b895a645c05c0b15d715e78e90b
Gabriel Scherer <gabriel.scherer@gmail.com>
2015-08-15 15:57:51 +0000

    add option handling for colors in compiler, OCAMLPARAM and ocamlbuild
    (Simon Cruanes and Gabriel Scherer)
    
    Use one of
      -color auto
      -color always
      -color never
    
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@16348 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- tools/ocamlcp.ml
+++ tools/ocamlcp.ml
@@ -100,6 +100,7 @@ module Options = Main_args.Make_bytecomp_options (struct
   let _w = option_with_arg "-w"
   let _warn_error = option_with_arg "-warn-error"
   let _warn_help = option "-warn-help"
+  let _color s = option_with_arg "-color" s
   let _where = option "-where"
   let _nopervasives = option "-nopervasives"
   let _dsource = option "-dsource"
--- utils/clflags.ml
+++ utils/clflags.ml
@@ -117,3 +117,11 @@ let runtime_variant = ref "";;      (* -runtime-variant *)
 let keep_docs = ref false              (* -keep-docs *)
 let keep_locs = ref false              (* -keep-locs *)
 let unsafe_string = ref true;;         (* -safe-string / -unsafe-string *)
+
+type color_setting = Auto | Always | Never
+let parse_color_setting = function
+  | "auto" -> Some Auto
+  | "always" -> Some Always
+  | "never" -> Some Never
+  | _ -> None
+let color = ref Auto ;; (* -color *)
