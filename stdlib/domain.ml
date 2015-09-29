ebec5184eb13db00a05a3523f0e56f953fbd60b7
Stephen Dolan <stephen.dolan@cl.cam.ac.uk>
2015-09-29 11:28:59 +0100

    Remove Domain.mutex

--- stdlib/domain.ml
+++ stdlib/domain.ml
@@ -1,11 +1,3 @@
 external spawn : (unit -> unit) -> unit = "caml_domain_spawn"
 
 external self : unit -> int = "caml_ml_domain_id"
-
-type mutex
-
-external mutex : unit -> mutex = "caml_domain_mutex"
-
-external lock : mutex -> unit = "caml_domain_lock"
-
-external unlock : mutex -> unit = "caml_domain_unlock"
--- stdlib/domain.mli
+++ stdlib/domain.mli
@@ -1,11 +1,3 @@
 val spawn : (unit -> unit) -> unit
 
 val self : unit -> int
-
-type mutex
-
-val mutex : unit -> mutex
-
-val lock : mutex -> unit
-
-val unlock : mutex -> unit
