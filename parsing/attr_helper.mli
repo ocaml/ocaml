f78f46bcce38fc54c665ce2efda15c2ba4b4eb2d
Jérémie Dimino <jeremie@dimino.org>
2015-10-06 10:58:17 +0000

    Add parsing/Attr_helper
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@16451 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

---
+++ parsing/attr_helper.mli
@@ -0,0 +1,26 @@
+(***********************************************************************)
+(*                                                                     *)
+(*                                OCaml                                *)
+(*                                                                     *)
+(*                 Jeremie Dimino, Jane Street Europe                  *)
+(*                                                                     *)
+(*  Copyright 2015 Institut National de Recherche en Informatique et   *)
+(*  en Automatique.  All rights reserved.  This file is distributed    *)
+(*  under the terms of the Q Public License version 1.0.               *)
+(*                                                                     *)
+(***********************************************************************)
+
+(** Helpers for attributes *)
+
+open Asttypes
+open Parsetree
+
+type error =
+  | Multiple_attributes of string
+  | No_payload_expected of string
+
+val get_no_payload_attribute : string -> attributes -> string loc option
+
+exception Error of Location.t * error
+
+val report_error: Format.formatter -> error -> unit
