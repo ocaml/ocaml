c4fdfde2bf9390e67176215bba517b008742bf55
alainfrisch <alain@frisch.fr>
2015-10-16 10:46:27 +0200

    Extend test to check improved unboxing scheme.

--- testsuite/tests/float-unboxing/float_subst_boxed_number.ml
+++ testsuite/tests/float-unboxing/float_subst_boxed_number.ml
@@ -43,7 +43,9 @@ module GPR_109 = struct
   let f () =
     let r = ref 0. in
     for i = 1 to 1000 do
-       r := !r +. float i
+      let x = float i in
+      let y = if i mod 2 = 0 then x else x +. 1. in
+      r := !r +. y
     done;
     !r
 
