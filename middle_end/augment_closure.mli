2636202f4f54065e110a02584fdbbf7a9bbf253e
Mark Shinwell <mshinwell@gmail.com>
2015-10-29 08:23:32 +0000

    headers

--- middle_end/augment_closure.mli
+++ middle_end/augment_closure.mli
@@ -1,3 +1,15 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                OCaml                                   *)
+(*                                                                        *)
+(*                       Pierre Chambart, OCamlPro                        *)
+(*                  Mark Shinwell, Jane Street Europe                     *)
+(*                                                                        *)
+(*   Copyright 2015 Institut National de Recherche en Informatique et     *)
+(*   en Automatique.  All rights reserved.  This file is distributed      *)
+(*   under the terms of the Q Public License version 1.0.                 *)
+(*                                                                        *)
+(**************************************************************************)
 
 val run :
   env:Inline_and_simplify_aux.Env.t ->
