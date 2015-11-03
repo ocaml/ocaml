c77f38eb1bc417be8e0826dafef7c99354a07655
Mark Shinwell <mshinwell@janestreet.com>
2015-11-03 15:45:37 +0000

    comments

--- middle_end/flambda.mli
+++ middle_end/flambda.mli
@@ -11,78 +11,15 @@
 (*                                                                        *)
 (**************************************************************************)
 
-(* CR mshinwell for mshinwell: this comment needs updating to reflect
-   name changes (e.g. "closure -> set_of_closures"). *)
-(** Intermediate language used to perform closure conversion and inlining.
+(** Intermediate language used for tree-based analysis and optimization. *)
 
-    Closure conversion starts with [Lambda] code.  The conversion transforms
-    function declarations into "sets of closures" ([Set_of_closures]
-    constructor) in the [Flambda] language.
-
-    The usual case of a function declared on its own will produce a
-    [Set_of_closures] containing a single closure.  The closure itself may
-    be accessed using [Project_closure] and specifies which variables (by name, not
-    by any numeric offset) are free in the corresponding function definition.
-    Occurrences of these free variables in the body appear as the usual
-    [Var] expressions.
-
-    For the case of multiple functions defined together, possibly mutually
-    recursive, a [Set_of_closures] value will be generated containing
-    one closure per function.  Each closure may be accessed again using
-    [Project_closure], specifying which function's closure is desired.
-
-    As an example, the flambda representation of:
-
-      {[let rec f x = ...
-        and g x = ... ]}
-
-    might be (for identifiers [f] and [g] corresponding to the variables of
-    the same name in the source text):
-
-      {[Let( closure, Set_of_closures { id_f -> ...; id_g -> ... },
-              Let(f, Project_closure { closure = closure; closure_id = id_f },
-              Let(g, Project_closure { closure = closure; closure_id = id_g },
-              ...)))]}
-
-    One can also use [Project_closure] to move between closures in the same set of
-    closures.  For example for [f] and [g] as above, represented together as a
-    set of closures, we might apply [Project_closure] to extract the closure for [g],
-    and later decide to use [Project_closure] again on this value (not on the set of
-    closures) to access the closure for [f].  This is used when inlining
-    mutually-recursive functions as a means of avoiding having to keep around
-    a value corresponding to the whole set of closures.  For example,
-    continuing from the example above:
-
-      {[ Project_closure { closure = Var g; closure_id = id_f;
-                    relative_to = Some id_g } ]}
-
-    After closure conversion an inlining pass is performed.  This may
-    introduce [Project_var] expressions to represent accesses (from
-    the body of inlined functions) to variables bound by closures.  Some of
-    these [Project_var] expressions may survive in the tree after
-    inlining has finished.
-
-    Other features of this intermediate language are:
-
-    - Access to constants across modules are performed with respect to
-      named symbols in the object file (of type [Symbol.t]).
-
-    - Direct calls are distinguished from indirect calls (as in [Clambda])
-      using values of type [call_kind].
-
-    - "Structured constants" built from the constructors in type [const]
-      are not explicitly represented.  Instead, they are converted into
-      expressions such as: [Prim (Pmakeblock(...), ...)].
-*)
-
-(* CR mshinwell: add comment explaining that there must be no
-   shadowing (and that the invariant checks enforce this) or other
-   reuse of names. *)
-
+(** Whether the callee in a function application is known at compile time. *)
 type call_kind =
   | Indirect
   | Direct of Closure_id.t
 
+(** Simple constants.  ("Structured constants" are rewritten to invocations
+    of [Pmakeblock] so that they easily take part in optimizations.) *)
 type const =
   | Int of int
   | Char of char
@@ -91,6 +28,7 @@ type const =
      constructors). *)
   | Const_pointer of int
 
+(** The application of a function to a list of arguments. *)
 type apply = {
   (* CR mshinwell: rename func -> callee, and lhs_of_application -> callee *)
   func : Variable.t;
@@ -98,13 +36,18 @@ type apply = {
   kind : call_kind;
   dbg : Debuginfo.t;
   inline : Lambda.inline_attribute;
+  (** Instructions from the source code as to whether the callee should
+      be inlined. *)
 }
 
+(** The update of a mutable variable.  Mutable variables are distinct from
+    immutable variables in Flambda. *)
 type assign = {
   being_assigned : Mutable_variable.t;
   new_value : Variable.t;
 }
 
+(** The invocation of a method. *)
 type send = {
   kind : Lambda.meth_kind;
   meth : Variable.t;
@@ -113,23 +56,37 @@ type send = {
   dbg : Debuginfo.t;
 }
 
+(** The selection of one closure given a set of closures, required before
+    a function defined by said set of closures can be applied.  See more
+    detailed documentation below on [set_of_closures]. *)
 type project_closure = {
   set_of_closures : Variable.t; (** must yield a set of closures *)
   closure_id : Closure_id.t;
 }
 
+(** The selection of one closure given another closure in the same set of
+    closures.  See more detailed documentation below on [set_of_closures]. *)
 type move_within_set_of_closures = {
   closure : Variable.t;  (** must yield a closure *)
   start_from : Closure_id.t;
   move_to : Closure_id.t;
 }
 
+(** The selection from a closure of a variable bound by said closure.
+    In other words, access to a function's environment.  Also see more
+    detailed documentation below on [set_of_closures]. *)
 type project_var = {
   closure : Variable.t;  (** must yield a closure *)
   closure_id : Closure_id.t;
   var : Var_within_closure.t;
 }
 
+(** Flambda terms are partitioned in a pseudo-ANF manner; many terms are
+    required to be [let]-bound.  This in particular ensures there is always
+    a variable name for an expression that may be lifted out (for example
+    if it is found to be constant).
+    Note: All bound variables in Flambda terms must be distinct.
+    [Flambda_invariants] verifies this. *)
 type t =
   | Var of Variable.t
   | Let of let_expr
@@ -140,8 +97,8 @@ type t =
   | Assign of assign
   | If_then_else of Variable.t * t * t
   | Switch of Variable.t * switch
-  (* Restrictions on [Lambda.Lstringswitch] also apply to [String_switch]. *)
   | String_switch of Variable.t * (string * t) list * t option
+  (** Restrictions on [Lambda.Lstringswitch] also apply to [String_switch]. *)
   | Static_raise of Static_exception.t * Variable.t list
   | Static_catch of Static_exception.t * Variable.t list * t * t
   | Try_with of t * Variable.t * t
@@ -149,10 +106,7 @@ type t =
   | For of for_loop
   | Proved_unreachable
 
-(** Values of type [named] will always be [let]-bound to a [Variable.t].
-    This has an important consequence: all expressions that we might deem
-    constant (and thus assign to a symbol) have an associated variable.
-*)
+(** Values of type [named] will always be [let]-bound to a [Variable.t]. *)
 and named =
   | Symbol of Symbol.t
   | Const of const
@@ -218,6 +172,23 @@ and let_expr = private {
       important optimization. *)
 }
 
+(** The representation of a set of function declarations (possibly mutually
+    recursive).  Such a set encapsulates the declarations themselves,
+    information about their defining environment, and information used
+    specifically for optimization.
+    Before a function can be applied it must be "projected" from a set of
+    closures to yield a "closure".  This is done using [Project_closure]
+    (see above).  Given a closure, not only can it be applied, but information
+    about its defining environment can be retrieved (using [Project_var],
+    see above).
+    At runtime, a [set_of_closures] corresponds to an OCaml value with tag
+    [Closure_tag] (possibly with inline [Infix_tag](s)).  As an optimization,
+    an operation ([Move_within_set_of_closures]) is provided (see above)
+    which enables one closure within a set to be located given another
+    closure in the same set.  This avoids keeping a pointer to the whole set
+    of closures alive when compiling, for example, mutually-recursive
+    functions.
+*)
 and set_of_closures = private {
   function_decls : function_declarations;
   (* CR mshinwell: consider renaming [free_vars].  Also, it's still really
@@ -229,6 +200,11 @@ and set_of_closures = private {
       in
      in [Build_export_info]. *)
   free_vars : Variable.t Variable.Map.t;
+  (** Mapping from all variables free in the body of the [function_decls] to
+      variables in scope at the definition point of the [set_of_closures].
+      The domain of this map is sometimes known as the "variables bound by
+      the closure". *)
+  specialised_args : Variable.t Variable.Map.t;
   (** Parameters known to always alias some variable in the scope of the set
       of closures declaration. For instance, supposing all call sites of f
       are represented in this example,
@@ -268,7 +244,6 @@ and set_of_closures = private {
   *)
   (* CR mshinwell for pchambart: expand upon the last sentence of the previous
      comment *)
-  specialised_args : Variable.t Variable.Map.t;
 }
 
 and function_declarations = private {
@@ -316,6 +291,7 @@ and switch = {
   failaction : t option; (** Action to take if none matched *)
 }
 
+(** Equivalent to the similar type in [Lambda]. *)
 and for_loop = {
   bound_var : Variable.t;
   from_value : Variable.t;
@@ -325,16 +301,22 @@ and for_loop = {
 }
 
 (** Like a subset of [Flambda.named], except that instead of [Variable.t]s we
-    have [Symbol.t]s, and everything is a constant.  Values of this type
-    describe constants that will be directly assigned to symbols in the
-    object file (see below). *)
+    have [Symbol.t]s, and everything is a constant (i.e. with a fixed value
+    known at compile time).  Values of this type describe constants that will
+    be directly assigned to symbols in the object file (see below). *)
 and constant_defining_value =
   | Allocated_const of Allocated_const.t
+    (** A single constant.  These are never "simple constants" (type [const])
+        but instead more complicated constructions. *)
   | Block of Tag.t * constant_defining_value_block_field list
+    (** A pre-allocated block full of constants (either simple constants
+        or more complicated constructions, see below). *)
   | Set_of_closures of set_of_closures
     (** A closed (and thus constant) set of closures.  (That is to say,
         [free_vars] must be empty.) *)
   | Project_closure of Symbol.t * Closure_id.t
+    (** Selection of one closure from a constant set of closures.
+        Analogous to the equivalent operation on expressions. *)
 
 and constant_defining_value_block_field =
   | Symbol of Symbol.t
@@ -370,7 +352,11 @@ type program =
   (** [End] accepts the root symbol: the only symbol that can never be
       eliminated. *)
 
-(** Compute the free variables of a term.  (This is O(1) for [Let]s). *)
+(** Compute the free variables of a term.  (This is O(1) for [Let]s).
+    If [ignore_uses_in_apply], all free variables inside [Apply] expressions
+    are ignored.  Likewise [ignore_uses_in_project_var] for [Project_var]
+    expressions.
+*)
 val free_variables
    : ?ignore_uses_in_apply:unit
   -> ?ignore_uses_in_project_var:unit
@@ -390,7 +376,7 @@ val used_variables
   -> t
   -> Variable.Set.t
 
-(** Compute the free variables of a named expression. *)
+(** Compute _all_ variables occurring inside a named expression. *)
 val used_variables_named
    : ?ignore_uses_in_project_var:unit
   -> named
@@ -398,34 +384,8 @@ val used_variables_named
 
 (** Used to avoid exceeding the stack limit when handling expressions with
     multiple consecutive nested [Let]-expressions.  This saves rewriting large
-    simplification functions in CPS.
-
-    If the expression [e] is:
-        Let (v1, defining_expr1,
-          ...  (* all [Let]s *)
-          Let (vN, defining_exprN, body) ... )
-    then [fold_lets e ~init ~for_defining_expr ~for_last_body] is:
-        Let (v1, accum == for_defining_expr init defining_expr1,
-          ...
-          Let (vN, accum == for_defining_expr accum defining_exprN,
-            for_last_body accum body) ... )
-    The stack usage of [fold_lets] is independent of the depth of nesting of
-    the [Let]s.
-
-    If the expression [e] is not a [Let], then [for_last_body init e] is
-    returned.
-*)
-(*
-val fold_lets
-   : t
-  -> init:'a
-  -> for_defining_expr:('a -> Variable.t -> named -> 'a * named)
-  -> for_last_body:('a -> t -> 'b * t)
-  -> 'b * t
-*)
-
-(** Like [fold_lets], but can remove and rename definition before
-    reintroduction. *)
+    simplification functions in CPS.  This function provides for the
+    rewriting or elimination of expressions during the fold. *)
 val fold_lets_option
    : t
   -> init:'a
@@ -435,7 +395,7 @@ val fold_lets_option
                            'b * Variable.t * named option)
   -> t * 'b
 
-(** Like [fold_lets], but just a map. *)
+(** Like [fold_lets_option], but just a map. *)
 val map_lets
    : t
   -> for_defining_expr:(Variable.t -> named -> named)
@@ -443,22 +403,13 @@ val map_lets
   -> after_rebuild:(t -> t)
   -> t
 
+(** Like [map_lets], but just an iterator. *)
 val iter_lets
    : t
   -> for_defining_expr:(Variable.t -> named -> unit)
   -> for_last_body:(t -> unit)
   -> for_each_let:(t -> unit)
   -> unit
-
-(*
-(** Like [fold_lets], but just iteration.  [for_last_body] is never called
-    if the input expression is not a [Let]. *)
-val iter_lets
-   : t
-  -> for_defining_expr:(Variable.t -> named -> unit)
-  -> for_last_body:(t -> free_vars_of_body:Variable.Set.t -> unit)
-  -> unit
-*)
 
 (** Creates a [Let] expression.  (This computes the free variables of the
     defining expression and the body.) *)
@@ -468,6 +419,8 @@ val create_let : Variable.t -> named -> t -> t
     [Let]-expression, returning a new [Let]. *)
 val map_defining_expr_of_let : let_expr -> f:(named -> named) -> t
 
+(** A module for the manipulation of terms where the recomputation of free
+    variable sets is to be kept to a minimum. *)
 module With_free_variables : sig
   type 'a t
 
@@ -515,10 +468,6 @@ module With_free_variables : sig
   (** O(1) time. *)
   val free_variables : _ t -> Variable.Set.t
 end
-
-(* CR mshinwell: try to move the non-recursive types out to a separate .mli *)
-(* CR mshinwell: consider moving [Flambda_utils] functions into here, now we
-   are forced to have a .ml *)
 
 val create_function_declaration
    : params:Variable.t list
