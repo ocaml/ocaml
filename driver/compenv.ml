8a22cc81a74fba14f55076eb92b7535a366cc4c5
Mark Shinwell <mshinwell@gmail.com>
2015-11-19 15:18:47 +0100

    add new options to compenv

--- driver/compenv.ml
+++ driver/compenv.ml
@@ -237,6 +237,10 @@ let read_one_param ppf position name v =
 
       | "unbox-closures" ->
           set "unbox-closures" [ unbox_closures ] v
+      | "remove-unused-arguments" ->
+          set "remove-unused-arguments" [ remove_unused_arguments ] v
+      | "no-inline-recursive-functions" ->
+          clear "no-inline-recursive-functions" [ inline_recursive_functions ] v
 
       | "inlining-stats" ->
           if !native_code then
