0c89326c7704dee652527d6ae76426666a676931
Mark Shinwell <mshinwell@janestreet.com>
2015-11-23 17:20:22 +0000

    comment and headers

--- middle_end/share_constants.mli
+++ middle_end/share_constants.mli
@@ -1,2 +1,17 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                OCaml                                   *)
+(*                                                                        *)
+(*                       Pierre Chambart, OCamlPro                        *)
+(*           Mark Shinwell and Leo White, Jane Street Europe              *)
+(*                                                                        *)
+(*   Copyright 2015 Institut National de Recherche en Informatique et     *)
+(*   en Automatique.  All rights reserved.  This file is distributed      *)
+(*   under the terms of the Q Public License version 1.0.                 *)
+(*                                                                        *)
+(**************************************************************************)
+
+(** Share lifted constants that are eligible for sharing (e.g. not strings)
+    and have equal definitions. *)
 
 val share_constants : Flambda.program -> Flambda.program
