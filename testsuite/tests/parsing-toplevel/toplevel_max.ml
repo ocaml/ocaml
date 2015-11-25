9100813bfb5e1d199ff399144d97341ec846eeff
Drup <drupyog@zoho.com>
2015-11-26 00:56:23 +0100

    Add a test for toplevel #use parsing.

---
+++ testsuite/tests/parsing-toplevel/toplevel_max.ml
@@ -0,0 +1,14 @@
+(* This file tests the maximum amount of SEMISEMI so that #use works. *)
+
+#cd "." ;;
+#print_depth 20 ;;
+
+;;
+
+let x = 3 ;;
+type t = int ;;
+
+#show_val x ;; ;;
+#show_type t ;;
+
+;;
---
+++ testsuite/tests/parsing-toplevel/toplevel_min.ml
@@ -0,0 +1,10 @@
+(* This file tests the minimum amount of SEMISEMI so that #use works. *)
+
+#cd "."
+#print_depth 20
+
+let x = 3
+type t = int ;;
+
+#show_val x
+#show_type t
