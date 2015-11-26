8afcaeeac05e1c87be73594832b78b32f30f95f6
Pierre Chambart <pierre.chambart@ocamlpro.com>
2015-11-26 17:34:47 +0100

    Fix testsuite

--- testsuite/tests/asmcomp/main.ml
+++ testsuite/tests/asmcomp/main.ml
@@ -14,7 +14,7 @@ open Clflags
 
 let compile_file filename =
   Clflags.dlcode := false;
-  Compilenv.reset "test";
+  Compilenv.reset ~sourcefile:(Timings.File filename) "test";
   Emit.begin_assembly();
   let ic = open_in filename in
   let lb = Lexing.from_channel ic in
