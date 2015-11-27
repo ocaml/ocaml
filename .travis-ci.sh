5cdb5a7b4e957de15d8fccc297c062f6860e1543
Gabriel Scherer <gabriel.scherer@gmail.com>
2015-11-27 21:05:06 +0100

    document the travis tests

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -15,6 +15,19 @@ PREFIX=~/local
 BuildAndTest () {
   case $XARCH in
   i386)
+  echo<<EOF
+------------------------------------------------------------------------
+This test builds the OCaml compiler distribution with your pull request,
+runs its testsuite, and then tries to install some important OCaml softare
+(currently camlp4, opam and oasis) on top of it.
+
+Failing to build the compiler distribution, or testsuite failures are
+critical errors that must be understood and fixed before your pull
+request can be merged. The later installation attempts try to run
+bleeding-edge software, and failures can sometimes be out of your
+control.
+------------------------------------------------------------------------
+EOF
     mkdir -p $PREFIX
     ./configure --prefix $PREFIX
     export PATH=$PREFIX/bin:$PATH
@@ -45,13 +58,43 @@ BuildAndTest () {
 }
 
 CheckChangesModified () {
-  # check that Changes has been modified by the proposed change
+  echo<<EOF
+------------------------------------------------------------------------
+This test checks that the Changes file has been modified by the pull
+request. Most contributions should come with a message in the Changes
+file, as described in our contributor documentation:
+
+  https://github.com/ocaml/ocaml/blob/trunk/CONTRIBUTING.md#changelog
+
+Some very minor changes (typo fixes for example) may not need
+a Changes entry, in which case it is acceptable for this test to fail.
+------------------------------------------------------------------------
+EOF
+  # check that Changes has been modified
   git diff $TRAVIS_COMMIT_RANGE --name-only --exit-code Changes > /dev/null \
   && exit 1 || echo pass
 }
 
 CheckTestsuiteModified () {
-  # check that testsuite/ has been modified by the proposed change
+  echo<<EOF
+------------------------------------------------------------------------
+This test checks that the OCaml testsuite has been modified by the
+pull request. Any new feature should come with tests, bugs should come
+with regression tests, and generally any change in behavior that can
+be exercized by a test should come with a test or modify and existing
+test. See our contributor documentation:
+
+  https://github.com/ocaml/ocaml/blob/trunk/CONTRIBUTING.md#test-you-must
+
+Modifications that result in no change in observable behavior
+(documentation contributions for example) can hardly be tested, in
+which case it is acceptable for this test to fail.
+
+Note: the heuristic used by this test is extremely fragile; passing it
+does *not* imply that your change is appropriately tested.
+------------------------------------------------------------------------
+EOF
+  # check that at least a file in testsuite/ has been modified
   git diff $TRAVIS_COMMIT_RANGE --name-only --exit-code testsuite > /dev/null \
   && exit 1 || echo pass
 }
