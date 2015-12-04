7ce026e8153c77d21bc20241215b36395c4ac5ab
alainfrisch <alain@frisch.fr>
2015-12-04 13:55:35 +0100

    Disable a test that does not work on 32-bit.

--- testsuite/tests/basic/min_int.ml
+++ testsuite/tests/basic/min_int.ml
@@ -1,6 +1,9 @@
+(* TEST DISABLED FOR NOW: it cannot be compiled on 32-bit platforms. *)
+(*
 let min_int = -4611686018427387904
 let () = match min_int with
   | -4611686018427387904 as i ->
     assert (string_of_int i = "-4611686018427387904");
     print_endline "OK"
   | _ -> assert false
+*)
