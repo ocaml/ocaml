dc31fdbe0e78c64889b8f180945504092d59ee34
Mark Shinwell <mark@three-tuns.net>
2015-12-10 12:42:00 +0000

    revert c64c213f6ef0ff48f32dc3bfe956eb3e2f8fbe2a for the moment until missing Lazy.force is found

---
+++ middle_end/find_recursive_functions.ml
@@ -0,0 +1,26 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                OCaml                                   *)
+(*                                                                        *)
+(*                       Pierre Chambart, OCamlPro                        *)
+(*                  Mark Shinwell, Jane Street Europe                     *)
+(*                                                                        *)
+(*   Copyright 2015 Institut National de Recherche en Informatique et     *)
+(*   en Automatique.  All rights reserved.  This file is distributed      *)
+(*   under the terms of the Q Public License version 1.0.                 *)
+(*                                                                        *)
+(**************************************************************************)
+
+let in_function_declarations (function_decls : Flambda.function_declarations)
+      ~backend =
+  let module VCC = Sort_connected_components.Make (Variable) in
+  let directed_graph =
+    Flambda_utils.fun_vars_referenced_in_decls function_decls ~backend
+  in
+  let connected_components =
+    VCC.connected_components_sorted_from_roots_to_leaf directed_graph
+  in
+  Array.fold_left (fun rec_fun -> function
+      | VCC.No_loop _ -> rec_fun
+      | VCC.Has_loop elts -> List.fold_right Variable.Set.add elts rec_fun)
+    Variable.Set.empty connected_components
