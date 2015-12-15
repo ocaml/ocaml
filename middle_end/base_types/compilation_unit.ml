53416a2666318dd24469b0516fd7a2f8289bd233
Pierre Chambart <pierre.chambart@ocamlpro.com>
2015-12-15 23:03:25 +0000

    Use hash to speed comparison of compilation unit

--- middle_end/base_types/compilation_unit.ml
+++ middle_end/base_types/compilation_unit.ml
@@ -15,6 +15,7 @@ module T = struct
   type t = {
     id : Ident.t;
     linkage_name : Linkage_name.t;
+    hash : int;
   }
 
   (* Multiple units can have the same [id] if they come from different packs.
@@ -23,11 +24,15 @@ module T = struct
   let compare v1 v2 =
     if v1 == v2 then 0
     else
-      let v1_id = Ident.name v1.id in
-      let v2_id = Ident.name v2.id in
-      let c = String.compare v1_id v2_id in
-      if c = 0
-      then Linkage_name.compare v1.linkage_name v2.linkage_name
+      let c = compare v1.hash v2.hash in
+      if c = 0 then
+        let v1_id = Ident.name v1.id in
+        let v2_id = Ident.name v2.id in
+        let c = String.compare v1_id v2_id in
+        if c = 0 then
+          Linkage_name.compare v1.linkage_name v2.linkage_name
+        else
+          c
       else c
 
   let equal x y =
@@ -38,17 +43,17 @@ module T = struct
   let print ppf t = Format.pp_print_string ppf (string_for_printing t)
 
   let output oc x = output_string oc (Ident.name x.id)
-  let hash x = Hashtbl.hash (Ident.name x.id)
+  let hash x = x.hash
 end
 
 include T
 include Ext_types.Identifiable.Make (T)
 
-let create id linkage_name =
+let create (id:Ident.t) linkage_name =
   if not (Ident.persistent id) then begin
     Misc.fatal_error "Compilation_unit.create with non-persistent Ident.t"
   end;
-  { id; linkage_name }
+  { id; linkage_name; hash = Hashtbl.hash id.name }
 
 let get_persistent_ident cu = cu.id
 let get_linkage_name cu = cu.linkage_name
