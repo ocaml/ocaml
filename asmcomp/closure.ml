27920e7ab5ada7bf327de6df50025ee8c766f20b
Pierre Chambart <pierre.chambart@ocamlpro.com>
2015-12-18 10:08:30 +0000

    Fix closure for -inline being a float

--- asmcomp/closure.ml
+++ asmcomp/closure.ml
@@ -1217,9 +1217,10 @@ and close_functions fenv cenv fun_defs =
       match inline_attribute with
       | Default_inline ->
           let inline_threshold =
-            Clflags.Int_arg_helper.get ~key:0 !Clflags.inline_threshold
+            Clflags.Float_arg_helper.get ~key:0 !Clflags.inline_threshold
           in
-          inline_threshold + n
+          let magic_scale_constant = 8. in
+          int_of_float (inline_threshold *. magic_scale_constant) + n
       | Always_inline -> max_int
       | Never_inline -> min_int
     in
