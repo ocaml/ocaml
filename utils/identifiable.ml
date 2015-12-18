f394564620cf54056712ef66483db41e6cbcb226
Mark Shinwell <mshinwell@janestreet.com>
2015-12-18 17:02:39 +0000

    Fix header

--- utils/identifiable.ml
+++ utils/identifiable.ml
@@ -3,7 +3,7 @@
 (*                                OCaml                                   *)
 (*                                                                        *)
 (*                       Pierre Chambart, OCamlPro                        *)
-(*                  Tark Shinwell, Jane Street Europe                     *)
+(*                  Mark Shinwell, Jane Street Europe                     *)
 (*                                                                        *)
 (*   Copyright 2015 Institut National de Recherche en Informatique et     *)
 (*   en Automatique.  All rights reserved.  This file is distributed      *)
