db5bf5f3136fe18eb60f13bca5a4a5b5e58ae839
Thomas Leonard <talex5@gmail.com>
2015-12-19 20:57:21 +0000

    Add BigArray.Genarray.free
    Free the memory used by a bigarray, setting all dimensions to zero to
    prevent further use.
    
    This is useful to ensure that memory-mapped files are closed promptly,
    without waiting for the garbage collector. Otherwise, mapping a large
    number of files may hit operating system limits on the number of open
    files.
    
    If e.g. [sub_left] is used to create multiple bigarrays backed by the
    same memory block, [free] must be called on all of them before the
    memory is actually released.
    
    Test program:
    
    (* ocamlc unix.cma bigarray.cma test.ml -o test && ./test *)
    
    open Bigarray
    
    let maps = ref []
    
    let open_and_close () =
      let fd = Unix.(openfile "/etc/passwd" [O_RDONLY; O_NONBLOCK] 0o644) in
      let mapped = Array1.map_file fd char c_layout false 100 in
      (* Make bug more obvious by ensuring no GC *)
      maps := mapped :: !maps;
      (* Array1.free mapped; *)
      Unix.close fd
    
    let () =
      let i = ref 0 in
      while true do
        incr i;
        open_and_close ();
        Printf.printf "Mapped %d files\n%!" !i
      done
    
    ...
    Mapped 35589 files
    Mapped 35590 files
    Fatal error: exception Unix.Unix_error(Unix.ENFILE, "open", "/etc/passwd")

--- otherlibs/bigarray/bigarray.ml
+++ otherlibs/bigarray/bigarray.ml
@@ -112,6 +112,7 @@ module Genarray = struct
      = "caml_ba_map_file_bytecode" "caml_ba_map_file"
   let map_file fd ?(pos = 0L) kind layout shared dims =
     map_internal fd kind layout shared dims pos
+  external free: ('a, 'b, 'c) t -> unit = "caml_ba_free"
 end
 
 module Array1 = struct
@@ -140,6 +141,7 @@ module Array1 = struct
     ba
   let map_file fd ?pos kind layout shared dim =
     Genarray.map_file fd ?pos kind layout shared [|dim|]
+  let free = Genarray.free
 end
 
 module Array2 = struct
@@ -185,6 +187,7 @@ module Array2 = struct
     ba
   let map_file fd ?pos kind layout shared dim1 dim2 =
     Genarray.map_file fd ?pos kind layout shared [|dim1;dim2|]
+  let free = Genarray.free
 end
 
 module Array3 = struct
@@ -240,6 +243,7 @@ module Array3 = struct
     ba
   let map_file fd ?pos kind layout shared dim1 dim2 dim3 =
     Genarray.map_file fd ?pos kind layout shared [|dim1;dim2;dim3|]
+  let free = Genarray.free
 end
 
 external genarray_of_array1: ('a, 'b, 'c) Array1.t -> ('a, 'b, 'c) Genarray.t
