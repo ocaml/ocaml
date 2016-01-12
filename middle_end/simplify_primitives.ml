47f94141fc4b6c872af889d0e0a4c17cb5384b96
Mark Shinwell <mshinwell@gmail.com>
2016-01-12 13:35:06 +0100

    Import licence renaming patch

--- middle_end/simplify_primitives.ml
+++ middle_end/simplify_primitives.ml
@@ -9,7 +9,7 @@
 (*   Copyright 2014--2016 Jane Street Group LLC                           *)
 (*                                                                        *)
 (*   All rights reserved.  This file is distributed under the terms of    *)
-(*   the GNU Library General Public License version 2.1, with the         *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
 (*   special exception on linking described in the file ../LICENSE.       *)
 (*                                                                        *)
 (**************************************************************************)
