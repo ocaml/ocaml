d32ebf6405d17e8c676e0f21f1956f8e7bdc4e8a
Mark Shinwell <mshinwell@janestreet.com>
2016-03-15 08:39:36 +0000

    Correct copyright header

--- asmrun/clambda_checks.c
+++ asmrun/clambda_checks.c
@@ -5,8 +5,8 @@
 /*                       Pierre Chambart, OCamlPro                        */
 /*                   Mark Shinwell, Jane Street Europe                    */
 /*                                                                        */
-/*   Copyright 2015 Institut National de Recherche en Informatique et     */
-/*     en Automatique.                                                    */
+/*   Copyright 2013--2016 OCamlPro SAS                                    */
+/*   Copyright 2014--2016 Jane Street Group LLC                           */
 /*                                                                        */
 /*   All rights reserved.  This file is distributed under the terms of    */
 /*   the GNU Lesser General Public License version 2.1, with the          */
