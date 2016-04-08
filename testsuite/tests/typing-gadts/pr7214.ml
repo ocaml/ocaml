de724f051cdbf0870b07cdc195c690a5058f93e6
Jacques Garrigue <garrigue@math.nagoya-u.ac.jp>
2016-04-08 17:18:17 +0900

    Extra example of potential unsoundness for PR#7214

--- testsuite/tests/typing-gadts/pr7214.ml
+++ testsuite/tests/typing-gadts/pr7214.ml
@@ -6,3 +6,18 @@ let f (type a) (x : a t) =
     let x = (I : a t)
   end in
   () ;;
+
+(* extra example by Stephen Dolan, using recursive modules *)
+(* Should not be allowed! *)
+type (_,_) eq = Refl : ('a, 'a) eq;;
+
+let bad (type a) =
+ let module N = struct
+   module rec M : sig
+     val e : (int, a) eq
+   end = struct
+     let (Refl : (int, a) eq) = M.e  (* must fail for soundness *)
+     let e : (int, a) eq = Refl
+   end
+ end in N.M.e
+;;
