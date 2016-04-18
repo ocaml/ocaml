af46c1644c728fa3b8311402dcbd88ca93bf96bc
Damien Doligez <damien.doligez@inria.fr>
2016-04-18 11:34:16 +0200

    add explanation for tests/lib-random/rand.ml

--- testsuite/tests/lib-random/rand.ml
+++ testsuite/tests/lib-random/rand.ml
@@ -1,3 +1,9 @@
+(* Test that two Random.self_init() in close succession will not result
+   in the same PRNG state.
+   Note that even when the code is correct this test is expected to fail
+   once in 10000 runs.
+*)
+
 let () =
   Random.self_init ();
   let x = Random.int 10000 in
