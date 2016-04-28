dedb0bdbb2e81014fb1fede341cb8ede3cf785a4
Stephen Dolan <stephen.dolan@cl.cam.ac.uk>
2016-04-28 15:35:59 +0100

    Handle mark stack overflow by postponing marking of some pools.

--- byterun/caml/shared_heap.h
+++ byterun/caml/shared_heap.h
@@ -1,8 +1,10 @@
 #ifndef CAML_SHARED_HEAP_H
 #define CAML_SHARED_HEAP_H
 
-struct caml_heap_state;
+#include "roots.h"
 
+struct caml_heap_state;
+struct pool;
 
 struct caml_heap_state* caml_init_shared_heap();
 
@@ -10,11 +12,14 @@ value* caml_shared_try_alloc(struct caml_heap_state*, mlsize_t wosize, tag_t tag
 
 uintnat caml_heap_size(struct caml_heap_state*);
 
+struct pool* caml_pool_of_shared_block(value v);
 struct domain* caml_owner_of_shared_block(value v);
 
 void caml_shared_unpin(value v);
 
 int caml_mark_object(value);
+
+void caml_redarken_pool(struct pool*, scanning_action);
 
 intnat caml_sweep(struct caml_heap_state*, intnat);
 
