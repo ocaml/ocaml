ff352595353f24c7371f37b063d35911ddc714bc
Nicolas Ojeda Bar <nicolas.ojeda.bar@lexifi.com>
2016-05-01 22:47:15 +0200

    Fix pruning

--- tools/cmt_lookup.ml
+++ tools/cmt_lookup.ml
@@ -19,6 +19,20 @@ open Cmt_format
 open Lexing
 open Asttypes
 
+let debug = ref false
+
+let with_timer name f =
+  match !debug with
+  | true ->
+      let t0 = Sys.time () in
+      let x = f () in
+      let t1 = Sys.time () in
+      let dt = t1 -. t0 in
+      Printf.eprintf "%s: %f s\n%!" name dt;
+      x
+  | false ->
+      f ()
+
 type pos =
   {
     line: int;
@@ -31,6 +45,16 @@ type span =
     finish: pos;
   }
 
+type definition =
+  | External
+  | Internal of Location.t
+  | Global_variable of Location.t
+  | Local_variable of Location.t
+
+type info = Location.t * (string * (string * definition) option)
+
+exception Found of info
+
 let after {line; column} pos =
   pos.pos_lnum < line || (pos.pos_lnum = line && pos.pos_cnum - pos.pos_bol <= column)
 
@@ -40,41 +64,13 @@ let before {line; column} pos =
 let inside {start; finish} loc =
   after start loc.loc_start && before finish loc.loc_end
 
-let (<|>) o1 o2 span =
-  match o1 span with
-  | None -> o2 span
-  | Some _ as r -> r
-
-let none _ = None
-
-let option f o span =
+let option f o =
   match o with
-  | None -> None
-  | Some x -> f x span
+  | None -> ()
+  | Some x -> f x
 
-let list f l =
-  let rec loop = function
-    | [] ->
-        none
-    | x :: l ->
-        f x <|> loop l
-  in
-  loop l
-
-let if_inside loc f (span : span) =
-  if inside span loc then
-    f span
-  else
-    None
-
-let some loc x _ =
-  Some (loc, x)
-
-type definition =
-  | External
-  | Internal of Location.t
-  | Global_variable of Location.t
-  | Local_variable of Location.t
+let found loc x =
+  raise (Found (loc, x))
 
 let let_scope s scope =
   if scope <> Location.none then
@@ -97,7 +93,7 @@ let print_modtype env modtyp =
 let print_class_type env cltype =
   Printtyp.wrap_printing_env env (fun () -> Format.asprintf "%a" Printtyp.class_type cltype)
 
-let rec expression_desc loc env = function
+let rec expression_desc span loc env = function
   | Texp_ident (path, _, {Types.val_type; val_loc; _}) ->
       begin try
         let full_name = Path.name ~paren:Oprint.parenthesized_ident path in
@@ -119,204 +115,253 @@ let rec expression_desc loc env = function
           with Not_found ->
             External
         in
-        some loc (print_type_scheme env val_type, Some (full_name, annot))
+        found loc (print_type_scheme env val_type, Some (full_name, annot))
       with _ ->
-        none
+        ()
       end
   | Texp_let (Recursive, vbs, e) ->
-      list (value_binding loc) vbs <|> expression e
+      List.iter (value_binding span loc) vbs;
+      expression span e
   | Texp_let (Nonrecursive, vbs, e) ->
-      list (value_binding e.exp_loc) vbs <|> expression e
+      List.iter (value_binding span e.exp_loc) vbs;
+      expression span e
   | Texp_function (_, cases, _) ->
-      list case cases
+      List.iter (case span) cases
   | Texp_apply (e, args) ->
-      expression e <|> list (fun (_, e) -> option expression e) args
+      expression span e;
+      List.iter (fun (_, e) -> option (expression span) e) args
   | Texp_match (e, cases, cases', _) ->
-      expression e <|> list case cases <|> list case cases'
+      expression span e;
+      List.iter (case span) cases;
+      List.iter (case span) cases'
   | Texp_try (e, cases) ->
-      expression e <|> list case cases
+      expression span e;
+      List.iter (case span) cases
   | Texp_tuple el
   | Texp_construct (_, _, el) ->
-      list expression el
+      List.iter (expression span) el
   | Texp_variant (_, e) ->
-      option expression e
+      option (expression span) e
   | Texp_record (fields, e) ->
-      list (fun (_, _, e) -> expression e) fields <|> option expression e
+      List.iter (fun (_, _, e) -> expression span e) fields;
+      option (expression span) e
   | Texp_field (e, _, _) ->
-      expression e
+      expression span e
   | Texp_setfield (e1, _, _, e2) ->
-      expression e1 <|> expression e2
+      expression span e1;
+      expression span e2
   | Texp_array el ->
-      list expression el
+      List.iter (expression span) el
   | Texp_ifthenelse (e1, e2, e3) ->
-      expression e1 <|> expression e2 <|> option expression e3
+      expression span e1;
+      expression span e2;
+      option (expression span) e3
   | Texp_sequence (e1, e2)
   | Texp_while (e1, e2) ->
-      expression e1 <|> expression e2
+      expression span e1;
+      expression span e2
   | Texp_for (_, _, e1, e2, _, e3) ->
-      expression e1 <|> expression e2 <|> expression e3
+      expression span e1;
+      expression span e2;
+      expression span e3
   | Texp_send (e1, _, e2) ->
-      expression e1 <|> option expression e2
+      expression span e1;
+      option (expression span) e2
   | Texp_letmodule (_, _, me, e) ->
-      module_expr me <|> expression e
+      module_expr span me;
+      expression span e
   | Texp_assert e
   | Texp_lazy e ->
-      expression e
+      expression span e
   | Texp_object (ce, _) ->
-      class_structure ce
+      class_structure span ce
   | Texp_pack me ->
-      module_expr me
+      module_expr span me
   | _ ->
-      none
+      ()
 
-and expression {exp_loc; exp_env; exp_type; exp_desc; _} =
-  let default = some exp_loc (print_type_scheme exp_env exp_type, None) in
-  if_inside exp_loc (expression_desc exp_loc exp_env exp_desc <|> default)
+and expression span {exp_loc; exp_env; exp_type; exp_desc; _} =
+  if inside span exp_loc then begin
+    expression_desc span exp_loc exp_env exp_desc;
+    found exp_loc (print_type_scheme exp_env exp_type, None)
+  end
 
-and value_binding scope {vb_pat; vb_expr; vb_loc; _} =
-  if_inside vb_loc (pattern scope vb_pat <|> expression vb_expr)
+and value_binding span scope {vb_pat; vb_expr; vb_loc; _} =
+  if inside span vb_loc then begin
+    pattern span scope vb_pat;
+    expression span vb_expr
+  end
 
-and class_expr {cl_desc; cl_loc; cl_env; cl_type; _} =
-  let default = some cl_loc (print_class_type cl_env cl_type, None) in
-  if_inside cl_loc (class_expr_desc cl_desc <|> default)
+and class_expr span {cl_desc; cl_loc; cl_env; cl_type; _} =
+  if inside span cl_loc then begin
+    class_expr_desc span cl_desc;
+    found cl_loc (print_class_type cl_env cl_type, None)
+  end
 
-and class_expr_desc = function
+and class_expr_desc span = function
   | Tcl_ident _ ->
-      none
+      ()
   | Tcl_structure cs ->
-      class_structure cs
+      class_structure span cs
   | Tcl_fun (_, p, args, ce, _) ->
-      pattern ce.cl_loc p <|> list (fun (_, _, e) -> expression e) args <|> class_expr ce
+      pattern span ce.cl_loc p;
+      List.iter (fun (_, _, e) -> expression span e) args;
+      class_expr span ce
   | Tcl_apply (ce, args) ->
-      class_expr ce <|> list (fun (_, e) -> option expression e) args
+      class_expr span ce;
+      List.iter (fun (_, e) -> option (expression span) e) args
   | Tcl_let (_, vbs, args, ce) ->
-      list (value_binding ce.cl_loc) vbs <|> list (fun (_, _, e) -> expression e) args <|> class_expr ce
+      List.iter (value_binding span ce.cl_loc) vbs;
+      List.iter (fun (_, _, e) -> expression span e) args;
+      class_expr span ce
   | Tcl_constraint (ce, _, _, _, _) ->
-      class_expr ce
+      class_expr span ce
 
-and class_field {cf_desc; cf_loc; _} =
-  if_inside cf_loc (class_field_desc cf_desc)
+and class_field span {cf_desc; cf_loc; _} =
+  if inside span cf_loc then
+    class_field_desc span cf_desc
 
-and class_field_desc = function
+and class_field_desc span = function
   | Tcf_inherit (_, ce, _, _, _) ->
-      class_expr ce
+      class_expr span ce
   | Tcf_val (_, _, _, Tcfk_concrete (_, e), _)
   | Tcf_method (_, _, Tcfk_concrete (_, e))
   | Tcf_initializer e ->
-      expression e
+      expression span e
   | _ ->
-      none
+      ()
 
-and class_structure {cstr_self; cstr_fields; _} =
-  pattern Location.none cstr_self <|> list class_field cstr_fields (* Fix scope *)
+and class_structure span {cstr_self; cstr_fields; _} =
+  pattern span Location.none cstr_self;
+  List.iter (class_field span) cstr_fields (* Fix scope *)
 
-and module_expr {mod_desc; mod_loc; mod_env; mod_type; _} =
-  let default = some mod_loc (print_modtype mod_env mod_type, None) in
-  if_inside mod_loc (module_expr_desc mod_loc mod_desc <|> default)
+and module_expr span {mod_desc; mod_loc; mod_env; mod_type; _} =
+  if inside span mod_loc then begin
+    module_expr_desc span mod_loc mod_desc;
+    found mod_loc (print_modtype mod_env mod_type, None)
+  end
 
-and module_expr_desc scope = function
+and module_expr_desc span scope = function
   | Tmod_ident _ ->
-      none
+      ()
   | Tmod_structure str ->
-      structure scope str
+      structure span scope str
   | Tmod_functor (_, _, _, me) ->
-      module_expr me
+      module_expr span me
   | Tmod_apply (me1, me2, _) ->
-      module_expr me1 <|> module_expr me2
+      module_expr span me1;
+      module_expr span me2
   | Tmod_constraint (me, _, _, _) ->
-      module_expr me
+      module_expr span me
   | Tmod_unpack (e, _) ->
-      expression e
+      expression span e
 
-and case {c_lhs; c_guard; c_rhs} =
+and case span {c_lhs; c_guard; c_rhs} =
   let scope =
     match c_guard with
     | None -> c_rhs.exp_loc
     | Some {exp_loc = {loc_start; _}; _} -> {c_rhs.exp_loc with loc_start}
   in
-  pattern scope c_lhs <|> option expression c_guard <|> expression c_rhs
+  pattern span scope c_lhs;
+  option (expression span) c_guard ;
+  expression span c_rhs
 
-and pattern_desc scope loc env typ = function
+and pattern_desc span scope loc env typ = function
   | Tpat_var (_, s) ->
-      some loc (print_type_scheme env typ, let_scope s scope)
+      found loc (print_type_scheme env typ, let_scope s scope)
   | Tpat_alias (p, _, s) ->
-      pattern scope p <|> if_inside s.loc (some loc (print_type_scheme env typ, let_scope s scope))
+      pattern span scope p;
+      if inside span s.loc then
+        found loc (print_type_scheme env typ, let_scope s scope)
   | Tpat_tuple pl
   | Tpat_construct (_, _, pl) ->
-      list (pattern scope) pl
+      List.iter (pattern span scope) pl
   | Tpat_variant (_, Some p, _) ->
-      pattern scope p
+      pattern span scope p
   | Tpat_record (fields, _) ->
-      list (fun (_, _, p) -> pattern scope p) fields
+      List.iter (fun (_, _, p) -> pattern span scope p) fields
   | Tpat_array pl ->
-      list (pattern scope) pl
+      List.iter (pattern span scope) pl
   | Tpat_or (p1, p2, _) ->
-      pattern scope p1 <|> pattern scope p2
+      pattern span scope p1;
+      pattern span scope p2
   | Tpat_lazy p ->
-      pattern scope p
+      pattern span scope p
   | _ ->
-      none
+      ()
 
-and pattern scope {pat_loc; pat_env; pat_type; pat_desc; _} =
-  let default = some pat_loc (print_type_scheme pat_env pat_type, None) in
-  if_inside pat_loc (pattern_desc scope pat_loc pat_env pat_type pat_desc <|> default)
+and pattern span scope {pat_loc; pat_env; pat_type; pat_desc; _} =
+  if inside span pat_loc then begin
+    pattern_desc span scope pat_loc pat_env pat_type pat_desc;
+    found pat_loc (print_type_scheme pat_env pat_type, None)
+  end
 
-and structure_item_desc scope loc = function
+and structure_item_desc span scope loc = function
   | Tstr_eval (e, _) ->
-      expression e
+      expression span e
   | Tstr_value (Recursive, vbs) ->
-      list (value_binding {scope with loc_start = loc.loc_start}) vbs
+      List.iter (value_binding span {scope with loc_start = loc.loc_start}) vbs
   | Tstr_value (Nonrecursive, vbs) ->
-      list (value_binding scope) vbs
+      List.iter (value_binding span scope) vbs
   | Tstr_module mb ->
-      module_binding mb
+      module_binding span mb
   | Tstr_recmodule mbs ->
-      list module_binding mbs
+      List.iter (module_binding span) mbs
   | Tstr_class args ->
-      list (fun ({ci_expr; _}, _) -> class_expr ci_expr) args
+      List.iter (fun ({ci_expr; _}, _) -> class_expr span ci_expr) args
   | Tstr_include {incl_mod; _} ->
-      module_expr incl_mod
+      module_expr span incl_mod
   | _ ->
-      none
+      ()
 
-and structure_item scope {str_desc; str_loc; _} =
-  if_inside str_loc (structure_item_desc scope str_loc str_desc)
+and structure_item span scope {str_desc; str_loc; _} =
+  if inside span str_loc then
+    structure_item_desc span scope str_loc str_desc
 
-and module_binding {mb_expr; mb_loc; _} =
-  if_inside mb_loc (module_expr mb_expr)
+and module_binding span {mb_expr; mb_loc; _} =
+  if inside span mb_loc then
+    module_expr span mb_expr
 
-and structure scope {str_items; _} =
+and structure span scope {str_items; _} =
   let rec loop scope = function
     | si :: ({str_loc = {loc_start; _}; _} :: _ as rest) ->
-        structure_item {scope with loc_start} si <|> loop scope rest
+        structure_item span {scope with loc_start} si;
+        loop scope rest
     | si :: [] ->
-        structure_item scope si
+        structure_item span scope si
     | [] ->
-        none
+        ()
   in
   loop scope str_items
 
-let binary_part = function
+let binary_part span part =
+  match part with
   | Partial_structure str ->
-      structure Location.none str
+      structure span Location.none str
   | Partial_structure_item si ->
-      structure_item Location.none si
+      structure_item span Location.none si
   | Partial_expression e ->
-      expression e
+      expression span e
   | Partial_pattern p ->
-      pattern Location.none p
+      pattern span Location.none p
   | Partial_class_expr ce ->
-      class_expr ce
+      class_expr span ce
   | _ ->
-      none
+      ()
 
-let binary_annots = function
+let binary_annots span annot =
+  match annot with
   | Implementation typedtree ->
-      structure Location.none typedtree
+      structure span Location.none typedtree
   | Partial_implementation binary_parts ->
-      Array.fold_right (fun part rest -> binary_part part <|> rest) binary_parts none
+      Array.iter (binary_part span) binary_parts
   | _ ->
-      none
+      ()
+
+let binary_annots span annot =
+  match binary_annots span annot with
+  | () -> None
+  | exception (Found info) -> Some info
 
 let to_span loc =
   let line = loc.loc_start.pos_lnum in
@@ -339,6 +384,7 @@ module Driver = struct
   let spec =
     Arg.align
       [
+        "-v", Arg.Set debug, " Print timings";
         "-type", Arg.Unit (set_mode Get_type), " Query type";
         "-ident", Arg.Unit (set_mode Get_ident), " Query ident";
       ]
@@ -369,11 +415,15 @@ module Driver = struct
           Arg.usage spec usage_msg;
           exit 2
     in
-    let {cmt_loadpath; cmt_annots; cmt_use_summaries; _} = read_cmt filename in
+    let {cmt_loadpath; cmt_annots; cmt_use_summaries; _} =
+      with_timer "read_cmt" (fun () -> read_cmt filename)
+    in
     Envaux.reset_cache ();
     Config.load_path := cmt_loadpath;
     rebuild_env := cmt_use_summaries;
-    let info = binary_annots cmt_annots span in
+    let info =
+      with_timer "binary_annots" (fun () -> binary_annots span cmt_annots)
+    in
     match info, !mode with
     | None, _
     | Some (_, (_, None)), Get_ident ->
