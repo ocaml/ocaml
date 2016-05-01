670021ff8d68965e3d59e1b6bf0bcaada77747cf
Jeremy Yallop <yallop@gmail.com>
2016-05-01 10:54:33 +0100

    let-rec check: distinguish generic array construction

--- typing/typecore.ml
+++ typing/typecore.ml
@@ -1685,6 +1685,7 @@ and is_nonexpansive_opt = function
     None -> true
   | Some e -> is_nonexpansive e
 
+module Env' = Env
 module Rec_context =
 struct
   type access =
@@ -1852,6 +1853,60 @@ struct
           true
     | _ -> false
 
+  let scrape env ty =
+    (Ctype.repr (Ctype.expand_head_opt env (Ctype.correct_levels ty))).desc
+
+let array_element_kind env ty =
+  match scrape env ty with
+  | Tvar _ | Tunivar _ ->
+      `Pgenarray
+  | Tconstr(p, _, _) ->
+      if Path.same p Predef.path_int || Path.same p Predef.path_char then
+        `Pintarray
+      else if Path.same p Predef.path_float then
+        `Pfloatarray
+      else if Path.same p Predef.path_string
+           || Path.same p Predef.path_array
+           || Path.same p Predef.path_nativeint
+           || Path.same p Predef.path_int32
+           || Path.same p Predef.path_int64 then
+        `Paddrarray
+      else begin
+        try
+          match Env'.find_type p env with
+            {type_kind = Type_abstract} ->
+              `Pgenarray
+          | {type_kind = Type_variant cstrs}
+            when List.for_all (fun c -> c.Types.cd_args = Types.Cstr_tuple [])
+                cstrs ->
+              `Pintarray
+          | {type_kind = _} ->
+              `Paddrarray
+        with Not_found ->
+          (* This can happen due to e.g. missing -I options,
+             causing some .cmi files to be unavailable.
+             Maybe we should emit a warning. *)
+          `Pgenarray
+      end
+  | _ ->
+      `Paddrarray
+
+
+  let array_type_kind env ty =
+    match scrape env ty with
+    | Tconstr(p, [elt_ty], _) | Tpoly({desc = Tconstr(p, [elt_ty], _)}, _)
+      when Path.same p Predef.path_array ->
+        array_element_kind env elt_ty
+    | _ ->
+        (* This can happen with e.g. Obj.field *)
+        `Pgenarray
+
+  let array_kind exp = array_type_kind exp.exp_env exp.exp_type
+
+  let has_concrete_element_type : Typedtree.expression -> bool =
+    fun e -> array_kind e <> `Pgenarray
+
+    
 
   let rec expression : Env.env -> Typedtree.expression -> Use.t =
     fun env exp -> match exp.exp_desc with
@@ -1897,8 +1952,10 @@ struct
                 (inspect (list arg env args)))
       | Texp_tuple exprs ->
         Use.guard (list expression env exprs)
+      | Texp_array exprs when has_concrete_element_type exp ->
+        Use.guard (list expression env exprs)
       | Texp_array exprs ->
-        (* This is counted as a use, because the constructing an array
+        (* This is counted as a use, because constructing a generic array
            involves inspecting the elements (PR#6939). *)
         Use.inspect (list expression env exprs)
       | Texp_construct (_, _, exprs) ->
