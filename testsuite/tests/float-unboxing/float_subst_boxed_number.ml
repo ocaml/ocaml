2eb9a1296ffc0d8fa1de1d959bf183962d70a471
Pierre Chambart <pierre.chambart@ocamlpro.com>
2016-05-12 11:11:48 +0200

    Test the optimization of immutable float records

--- testsuite/tests/float-unboxing/float_subst_boxed_number.ml
+++ testsuite/tests/float-unboxing/float_subst_boxed_number.ml
@@ -132,6 +132,15 @@ let unbox_only_if_useful () =
   done;
   ignore (Sys.opaque_identity !r)
 
+let eliminate_intermediate_float_record () =
+  let r = ref 0. in
+  for n = 1 to 1000 do
+    let open Complex in
+    let c = { re = float n; im = 0. } in
+    r := !r +. (norm [@inlined]) ((add [@inlined]) c i);
+  done;
+  ignore (Sys.opaque_identity !r)
+
 let () =
   let flambda =
     match Sys.getenv "FLAMBDA" with
@@ -149,6 +158,8 @@ let () =
 
   if flambda then begin
     check_noalloc "float and int32 record" unbox_record;
+    check_noalloc "eliminate intermediate immutable float record"
+      eliminate_intermediate_float_record;
   end;
 
   ()
