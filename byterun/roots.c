a9530d203ea938c0f89a7c11622a9d8dcc373431
KC Sivaramakrishnan <kc@kcsrk.info>
2016-05-17 14:48:36 +0100

    Move globals scanning to caml_do_local_roots
    This is still not ideal. Globals should not need to be scanned for every
    minor collection.

--- byterun/roots.c
+++ byterun/roots.c
@@ -26,6 +26,20 @@
 #include "shared_heap.h"
 #include "fiber.h"
 
+#ifdef NATIVE_CODE
+#include "stack.h"
+/* Communication with [caml_start_program] and [caml_call_gc]. */
+
+/* The global roots.
+   FIXME: These should be promoted, and not scanned here.
+   FIXME: caml_globals_inited makes assumptions about store ordering.
+   XXX KC : What to do here?
+*/
+
+intnat caml_globals_inited = 0;
+static intnat caml_globals_scanned = 0;
+#endif
+
 CAMLexport __thread struct caml__roots_block *caml_local_roots = NULL;
 
 CAMLexport void caml_do_local_roots (scanning_action f, struct domain* domain)
@@ -33,6 +47,20 @@ CAMLexport void caml_do_local_roots (scanning_action f, struct domain* domain)
   struct caml__roots_block *lr;
   int i, j;
   value* sp;
+
+#ifdef NATIVE_CODE
+  /* The global roots.
+     FIXME: These should be promoted, and not scanned here.
+     FIXME: caml_globals_inited makes assumptions about store ordering.
+  */
+  value glob;
+  for (i = 0; i <= caml_globals_inited && caml_globals[i] != 0; i++) {
+    glob = caml_globals[i];
+    for (j = 0; j < Wosize_val(glob); j++){
+      f(Op_val(glob)[j], &Op_val(glob)[j]);
+    }
+  }
+#endif
 
   f(domain->state->current_stack, &(domain->state->current_stack));
   for (lr = *(domain->local_roots); lr != NULL; lr = lr->next) {
