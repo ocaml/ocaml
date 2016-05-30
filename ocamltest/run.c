0fa571cefbc1dd169c795946afc16e6bf38c7fac
Sébastien Hinderer <Sebastien.Hinderer@inria.fr>
2016-05-30 17:50:28 +0200

    Let fatal_error and fatal_perror accept a variable number of arguments.

--- ocamltest/run.c
+++ ocamltest/run.c
@@ -24,6 +24,7 @@
 #include <fcntl.h>
 #include <string.h>
 #include <errno.h>
+#include <stdarg.h>
 #include <signal.h>
 
 #include "run.h"
@@ -40,32 +41,66 @@ static inline int is_defined(const char *str)
 }
 
 __attribute__((__noreturn__)) /* TODO: make sure compiler supports this */
-void fatal_error_with_location(const char *file, int line, const char *msg)
+void fatal_error_with_location(const char *file, int line, const char *msg, ...)
 {
-  fprintf(stderr, "%s:%d: %s\n", file, line, msg);
+  va_list ap;
+  va_start(ap, msg);
+  fprintf(stderr, "%s:%d: ", file, line);
+  vfprintf(stderr, msg, ap);
+  va_end(ap);
   exit(EXIT_FAILURE);
 }
 
-#define fatal_error(msg) fatal_error_with_location(__FILE__, __LINE__, msg)
+#define fatal_error(msg, ...) \
+fatal_error_with_location(__FILE__, __LINE__, msg, ## __VA_ARGS__)
+
+/*
+  Note: the ## __VA_ARGS__ construct is gcc specific.
+  For a more portable (but also more complex) solution, see
+  http://stackoverflow.com/questions/20818800/variadic-macro-and-trailing-comma
+*/
 
 __attribute__((__noreturn__)) /* TODO: make sure compiler supports this */
-void fatal_perror_with_location(const char *file, int line, const char *msg)
+void fatal_perror_with_location(const char *file, int line, const char *msg, ...)
 {
+  va_list ap;
+  va_start(ap, msg);
   fprintf(stderr, "%s:%d: ", file, line);
+  vfprintf(stderr, msg, ap);
+  va_end(ap);
+  fprintf(stderr, ": ");
   perror(msg);
   exit(EXIT_FAILURE);
 }
 
-#define fatal_perror(msg) fatal_perror_with_location(__FILE__, __LINE__, msg)
+#define fatal_perror(msg, ...) \
+fatal_perror_with_location(__FILE__, __LINE__, msg, ## __VA_ARGS__)
+
+/* Same remark as for the fatal_error macro. */
+
+__attribute__((__noreturn__)) /* TODO: make sure compiler supports this */
+void open_error_with_location(const char *file, int line, const char *msg)
+{
+  fatal_perror_with_location(file, line, "Can not open %s", msg);
+}
+
+#define open_error(filename) \
+open_error_with_location(__FILE__, __LINE__, filename)
+
+__attribute__((__noreturn__)) /* TODO: make sure compiler supports this */
+void realpath_error_with_location(const char *file, int line, const char *msg)
+{
+  fatal_perror_with_location(file, line, "realpath(\"%s\") failed", msg);
+}
+
+#define realpath_error(filename) \
+realpath_error_with_location(__FILE__, __LINE__, filename)
 
 void handle_alarm(int sig)
 {
   if (sig!=SIGALRM)
-  {
-    char message[256];
-    sprintf(message, "Received unexpected signal %s", strsignal(sig));
-    fatal_error(message);
-  }
+    fatal_error("Received unexpected signal %s", strsignal(sig));
+
   timeout_expired = 1;
 }
 
@@ -81,8 +116,10 @@ int run_command_child(const command_settings *settings)
   if (is_defined(settings->stdout_filename))
   {
     stdout_fd = open(settings->stdout_filename, flags, mode);
-    if (stdout_fd < 0) fatal_perror("open");
-    if ( dup2(stdout_fd, STDOUT_FILENO) == -1 ) fatal_perror("dup2");
+    if (stdout_fd < 0)
+      open_error(settings->stdout_filename);
+    if ( dup2(stdout_fd, STDOUT_FILENO) == -1 )
+      fatal_perror("dup2 for stdout");
   }
 
   if (is_defined(settings->stderr_filename))
@@ -92,19 +129,20 @@ int run_command_child(const command_settings *settings)
 #ifdef __GLIBC__
       char *stdout_realpath, *stderr_realpath;
       stdout_realpath = realpath(settings->stdout_filename, NULL);
-      if (stdout_realpath == NULL) fatal_perror("realpath");
+      if (stdout_realpath == NULL)
+        realpath_error(settings->stdout_filename);
       stderr_realpath = realpath(settings->stderr_filename, NULL);
       if ( (stderr_realpath == NULL)  && (errno != ENOENT) )
       {
         free(stdout_realpath);
-        fatal_perror("realpath");
+        realpath_error(settings->stderr_filename);
       }
 #else
       char stdout_realpath[PATH_MAX], stderr_realpath[PATH_MAX];
       if (realpath(settings->stdout_filename, stdout_realpath) == NULL)
-        fatal_perror("realpath");
+        realpath_error(settings->stdout_filename);
       if ((realpath(settings->stderr_filename, stderr_realpath) == NULL) && (errno != ENOENT))
-        fatal_perror("realpath");
+        realpath_error(settings->stderr_filename);
 #endif /* __GLIBC__ */
       if (strcmp(stdout_realpath, stderr_realpath) == 0)
         stderr_fd = stdout_fd;
@@ -116,9 +154,10 @@ int run_command_child(const command_settings *settings)
     if (stderr_fd == -1)
     {
       stderr_fd = open(settings->stderr_filename, flags, mode);
-      if (stderr_fd == -1) fatal_perror("open");
+      if (stderr_fd == -1) open_error(settings->stderr_filename);
     }
-    if ( dup2(stderr_fd, STDERR_FILENO) == -1 ) fatal_perror("dup2");
+    if ( dup2(stderr_fd, STDERR_FILENO) == -1 )
+      fatal_perror("dup2 for stderr");
   }
 
   res = execve(settings->program, *settings->argv, *settings->envp);
@@ -143,11 +182,7 @@ int handle_process_termination(pid_t pid, int status, const char *corefilename_p
   if (WIFEXITED(status)) return WEXITSTATUS(status);
 
   if ( !WIFSIGNALED(status) )
-  {
-    char msg[256];
-    sprintf(msg, "Process with pid %d neither terminated normally nor received a signal!?", pid);
-    fatal_error(msg);
-  }
+    fatal_error("Process with pid %d neither terminated normally nor received a signal!?", pid);
 
   /* From here we know that the process terminated due to a signal */
   signal = WTERMSIG(status);
@@ -159,19 +194,21 @@ int handle_process_termination(pid_t pid, int status, const char *corefilename_p
     "Process %d got signal %d(%s), %score dumped\n",
     pid, signal, strsignal(signal), corestr
   );
+
   if (core)
   {
     if ( access(COREFILENAME, F_OK) == -1)
       fprintf(stderr, "Could not find core file.\n");
     else {
       char corefile[strlen(corefilename_prefix) + 128];
-      sprintf(corefile, "%s.%d.core", corefilename_prefix, pid);
+      snprintf(corefile, sizeof(corefile), "%s.%d.core", corefilename_prefix, pid);
       if ( rename(COREFILENAME, corefile) == -1)
         fprintf(stderr, "Tge core file exists but could not be renamed.\n");
       else
         fprintf(stderr,"The core file has been renamed to %s\n", corefile);
     }
   }
+
   return -signal;
 }
 
