1ec886baab7d212fde3e69530007d4b3db16baed
Sébastien Hinderer <Sebastien.Hinderer@inria.fr>
2016-06-14 16:51:17 +0200

    Make sure the run library never calls exit(2).
    Also introduce run_main.c (not yet usable) to make it possible to test
    the library from the command-line.

--- ocamltest/run.c
+++ ocamltest/run.c
@@ -47,8 +47,7 @@ void defaultLogger(void *where, const char *format, ...)
   vfprintf(stderr, format, ap);
 }
 
-__attribute__((__noreturn__)) /* TODO: make sure compiler supports this */
-void fatal_error_with_location(
+void error_with_location(
   const char *file, int line,
   const command_settings *settings,
   const char *msg, ...)
@@ -60,11 +59,10 @@ void fatal_error_with_location(
   logger(settings->loggerData, "%s:%d: ", file, line);
   logger(settings->loggerData, msg, ap);
   va_end(ap);
-  exit(EXIT_FAILURE);
 }
 
-#define fatal_error(msg, ...) \
-fatal_error_with_location(__FILE__, __LINE__, settings, msg, ## __VA_ARGS__)
+#define error(msg, ...) \
+error_with_location(__FILE__, __LINE__, settings, msg, ## __VA_ARGS__)
 
 /*
   Note: the ## __VA_ARGS__ construct is gcc specific.
@@ -72,8 +70,7 @@ fatal_error_with_location(__FILE__, __LINE__, settings, msg, ## __VA_ARGS__)
   http://stackoverflow.com/questions/20818800/variadic-macro-and-trailing-comma
 */
 
-__attribute__((__noreturn__)) /* TODO: make sure compiler supports this */
-void fatal_perror_with_location(
+void myperror_with_location(
   const char *file, int line,
   const command_settings *settings,
   const char *msg, ...)
@@ -86,33 +83,30 @@ void fatal_perror_with_location(
   logger(settings->loggerData, msg, ap);
   va_end(ap);
   logger(settings->loggerData, ": %s\n", strerror(errno));
-  exit(EXIT_FAILURE);
 }
 
-#define fatal_perror(msg, ...) \
-fatal_perror_with_location(__FILE__, __LINE__, settings, msg, ## __VA_ARGS__)
+#define myperror(msg, ...) \
+myperror_with_location(__FILE__, __LINE__, settings, msg, ## __VA_ARGS__)
 
-/* Same remark as for the fatal_error macro. */
+/* Same remark as for the error macro. */
 
-__attribute__((__noreturn__)) /* TODO: make sure compiler supports this */
 void open_error_with_location(
   const char *file, int line,
   const command_settings *settings,
   const char *msg)
 {
-  fatal_perror_with_location(file, line, settings, "Can not open %s", msg);
+  myperror_with_location(file, line, settings, "Can not open %s", msg);
 }
 
 #define open_error(filename) \
 open_error_with_location(__FILE__, __LINE__, settings, filename)
 
-__attribute__((__noreturn__)) /* TODO: make sure compiler supports this */
 void realpath_error_with_location(
   const char *file, int line,
   const command_settings *settings,
   const char *msg)
 {
-  fatal_perror_with_location(file, line, settings, "realpath(\"%s\") failed", msg);
+  myperror_with_location(file, line, settings, "realpath(\"%s\") failed", msg);
 }
 
 #define realpath_error(filename) \
@@ -130,7 +124,7 @@ int run_command_child(const command_settings *settings)
   int flags = O_WRONLY | (settings->append ? O_APPEND : O_CREAT | O_TRUNC);
   int mode = 0666;
 
-  if (setpgid(0, 0) == -1) fatal_perror("setpgid");
+  if (setpgid(0, 0) == -1) myperror("setpgid");
 
   if (is_defined(settings->stdout_filename))
   {
@@ -138,7 +132,7 @@ int run_command_child(const command_settings *settings)
     if (stdout_fd < 0)
       open_error(settings->stdout_filename);
     if ( dup2(stdout_fd, STDOUT_FILENO) == -1 )
-      fatal_perror("dup2 for stdout");
+      myperror("dup2 for stdout");
   }
 
   if (is_defined(settings->stderr_filename))
@@ -176,12 +170,12 @@ int run_command_child(const command_settings *settings)
       if (stderr_fd == -1) open_error(settings->stderr_filename);
     }
     if ( dup2(stderr_fd, STDERR_FILENO) == -1 )
-      fatal_perror("dup2 for stderr");
+      myperror("dup2 for stderr");
   }
 
   res = execv(settings->program, *settings->argv); /* , *settings->envp); */
 
-  fatal_perror("Cannot execute %s", settings->program);
+  myperror("Cannot execute %s", settings->program);
   return res;
 }
 
@@ -203,7 +197,7 @@ int handle_process_termination(
   if (WIFEXITED(status)) return WEXITSTATUS(status);
 
   if ( !WIFSIGNALED(status) )
-    fatal_error("Process with pid %d neither terminated normally nor received a signal!?", pid);
+    error("Process with pid %d neither terminated normally nor received a signal!?", pid);
 
   /* From here we know that the process terminated due to a signal */
   signal = WTERMSIG(status);
@@ -244,8 +238,8 @@ int run_command_parent(const command_settings *settings, pid_t child_pid)
     action.sa_handler = handle_alarm;
     sigemptyset(&action.sa_mask);
     action.sa_flags = SA_RESETHAND;
-    if (sigaction(SIGALRM, &action, NULL) == -1) fatal_perror("sigaction");
-    if (alarm(settings->timeout) == -1) fatal_perror("alarm");
+    if (sigaction(SIGALRM, &action, NULL) == -1) myperror("sigaction");
+    if (alarm(settings->timeout) == -1) myperror("alarm");
   }
 
   while (waiting)
@@ -260,14 +254,14 @@ int run_command_parent(const command_settings *settings, pid_t child_pid)
           {
             timeout_expired = 0;
             fprintf(stderr, "Timeout expired, killing all child processes");
-            if (kill(-child_pid, SIGKILL) == -1) fatal_perror("kill");
+            if (kill(-child_pid, SIGKILL) == -1) myperror("kill");
           };
           break;
         case ECHILD:
           waiting = 0;
           break;
         default:
-          fatal_perror("wait");
+          myperror("wait");
       }
     } else { /* Got a pid */
       code = handle_process_termination(settings, pid, status, settings->program);
@@ -281,46 +275,7 @@ int run_command_parent(const command_settings *settings, pid_t child_pid)
 int run_command(const command_settings *settings)
 {
   pid_t child_pid = fork();
-  if (child_pid == -1) fatal_perror("fork");
+  if (child_pid == -1) myperror("fork");
   if (child_pid == 0) return run_command_child(settings);
   else return run_command_parent(settings, child_pid);
 }
-
-#if 0
-int main(int argc, char *argv[])
-{
-  int result;
-  if (argc<2) fatal_error("Specify which program to execute");
-  char *program = argv[1];
-  char *args[] = { program, NULL };
-  char *env[] = { NULL };
-  command_settings settings;
-  settings.program = program;
-  /* Case 1:
-    settings.stdout_filename = NULL;
-    settings.stderr_filename = NULL;
-  */
-  /* Case 2:
-    settings.stdout_filename = "/tmp/output";
-    settings.stderr_filename = NULL;
-  */
-  /* Case 3:
-    settings.stdout_filename = NULL;
-    settings.stderr_filename = "/tmp/error";
-  */
-  /* Case 4:
-    settings.stdout_filename = "/tmp/output";
-    settings.stderr_filename = "/tmp/error";
-  */
-  /* Case 5:
-    settings.stdout_filename = "/tmp/log";
-    settings.stderr_filename = "/tmp/log";
-  */
-  settings.stdout_filename = settings.stderr_filename = NULL;
-  settings.argv = &args;
-  settings.envp = &env;
-  settings.timeout = 2;
-  result = run_command(&settings);
-  fprintf(stderr, "run_command returned %d\n", result);
-}
-#endif /* 0 */
