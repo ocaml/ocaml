df057587c8bc0f0497bce24972b1024f110d31d4
Mark Shinwell <mshinwell@janestreet.com>
2016-07-06 08:35:38 +0100

    reordering

--- asmcomp/mach.ml
+++ asmcomp/mach.ml
@@ -15,6 +15,8 @@
 
 (* Representation of machine code by sequences of pseudoinstructions *)
 
+type label = Cmm.label
+
 type integer_comparison =
     Isigned of Cmm.comparison
   | Iunsigned of Cmm.comparison
@@ -23,7 +25,7 @@ type integer_operation =
     Iadd | Isub | Imul | Imulh | Idiv | Imod
   | Iand | Ior | Ixor | Ilsl | Ilsr | Iasr
   | Icomp of integer_comparison
-  | Icheckbound of { label_after_error : Cmm.label option; }
+  | Icheckbound of { label_after_error : label option; }
 
 type test =
     Itruetest
@@ -33,8 +35,6 @@ type test =
   | Ifloattest of Cmm.comparison * bool
   | Ioddtest
   | Ieventest
-
-type label = Cmm.label
 
 type operation =
     Imove
@@ -52,7 +52,7 @@ type operation =
   | Istackoffset of int
   | Iload of Cmm.memory_chunk * Arch.addressing_mode
   | Istore of Cmm.memory_chunk * Arch.addressing_mode * bool
-  | Ialloc of { words : int; label_after_call_gc : Cmm.label option; }
+  | Ialloc of { words : int; label_after_call_gc : label option; }
   | Iintop of integer_operation
   | Iintop_imm of integer_operation * int
   | Inegf | Iabsf | Iaddf | Isubf | Imulf | Idivf
