5d18984236c5ac79d25ec9e94eb0a49edddb741e
Fabrice Le Fessant <fabrice.le_fessant@ocamlpro.com>
2016-08-11 18:06:21 +0200

    Defer -cclib and -dllib like anonymous arguments

--- driver/main.ml
+++ driver/main.ml
@@ -89,13 +89,13 @@ module Options = Main_args.Make_bytecomp_options (struct
   let _binannot = set binary_annotations
   let _c = set compile_only
   let _cc s = c_compiler := Some s
-  let _cclib s = ccobjs := Misc.rev_split_words s @ !ccobjs
+  let _cclib s = schedule (fun () -> ccobjs := Misc.rev_split_words s @ !ccobjs)
   let _ccopt s = first_ccopts := s :: !first_ccopts
   let _compat_32 = set bytecode_compatible_32
   let _config = show_config
   let _custom = set custom_runtime
   let _no_check_prims = set no_check_prims
-  let _dllib s = dllibs := Misc.rev_split_words s @ !dllibs
+  let _dllib s = schedule (fun () -> dllibs := Misc.rev_split_words s @ !dllibs)
   let _dllpath s = dllpaths := !dllpaths @ [s]
   let _for_pack s = for_package := Some s
   let _g = set debug
