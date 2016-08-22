3757542f5286b1b0a78cf4a8112328f120178bdc
Jacques Garrigue <garrigue@math.nagoya-u.ac.jp>
2016-08-22 11:01:49 +0900

    Fix PR#7325

--- typing/typedecl.ml
+++ typing/typedecl.ml
@@ -115,20 +115,21 @@ let rec get_unboxed_type_representation env ty fuel =
   let ty = Ctype.repr (Ctype.expand_head_opt env ty) in
   match ty.desc with
   | Tconstr (p, args, _) ->
-    let tydecl = Env.find_type p env in
-    if tydecl.type_unboxed.unboxed then begin
-      match tydecl.type_kind with
-      | Type_record ([{ld_type = ty2; _}], _)
-      | Type_variant [{cd_args = Cstr_tuple [ty2]; _}]
-      | Type_variant [{cd_args = Cstr_record [{ld_type = ty2; _}]; _}]
-      -> get_unboxed_type_representation env
-           (Ctype.apply env tydecl.type_params ty2 args) (fuel - 1)
-      | Type_abstract -> None
+    begin match Env.find_type p env with
+    | exception Not_found -> Some ty
+    | {type_unboxed = {unboxed = false}} -> Some ty
+    | {type_params; type_kind =
+         Type_record ([{ld_type = ty2; _}], _)
+       | Type_variant [{cd_args = Cstr_tuple [ty2]; _}]
+       | Type_variant [{cd_args = Cstr_record [{ld_type = ty2; _}]; _}]}
+
+         -> get_unboxed_type_representation env
+             (Ctype.apply env type_params ty2 args) (fuel - 1)
+    | {type_kind=Type_abstract} -> None
           (* This case can occur when checking a recursive unboxed type
              declaration. *)
-      | _ -> assert false (* only the above can be unboxed *)
-    end else
-      Some ty
+    | _ -> assert false (* only the above can be unboxed *)
+    end
   | _ -> Some ty
 
 let get_unboxed_type_representation env ty =
@@ -1626,13 +1627,14 @@ let rec parse_native_repr_attributes env core_type ty ~global_repr =
 
 let check_unboxable env loc ty =
   let ty = Ctype.repr (Ctype.expand_head_opt env ty) in
-  match ty.desc with
+  try match ty.desc with
   | Tconstr (p, _, _) ->
     let tydecl = Env.find_type p env in
     if tydecl.type_unboxed.unboxed then
       Location.prerr_warning loc
         (Warnings.Unboxable_type_in_prim_decl (Path.name p))
   | _ -> ()
+  with Not_found -> ()
 
 (* Translate a value declaration *)
 let transl_value_decl env loc valdecl =
