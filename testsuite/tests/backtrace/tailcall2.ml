574699f320908c9e0061f63fc0f2d2078f4ce229
Frederic Bour <frederic.bour@lakaban.net>
2016-08-24 18:27:37 +0100

    split backtrace/tailcall into never and always inlined

--- testsuite/tests/backtrace/tailcall2.ml
+++ testsuite/tests/backtrace/tailcall2.ml
@@ -1,9 +1,10 @@
-let g () = raise Not_found
+let g () = raise Not_found [@@inline]
 
 let h () =
   (* "g ()" is inlined by native compiler, but the location of the call should
      be dropped from the backtrace as it is in tail position. *)
   g ()
+[@@inline]
 
 let () =
   Printexc.record_backtrace true;
