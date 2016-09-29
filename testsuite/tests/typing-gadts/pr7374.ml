fe9b172982bbb66135a2bfddb863ffe3d4f3c47b
Jacques Garrigue <garrigue@math.nagoya-u.ac.jp>
2016-09-29 18:02:39 +0900

    Addendum to PR#7374: ensure that paths hidden by definitions are checked too in local_non_recursive_abbrev

--- testsuite/tests/typing-gadts/pr7374.ml
+++ testsuite/tests/typing-gadts/pr7374.ml
@@ -33,3 +33,17 @@ module Bad = Fix(Id)
 let segfault () =
   print_endline (cast (trans (Bad.uniq Refl) (Bad.uniq Refl)) 0)
 *)
+
+(* addendum: ensure that hidden paths are checked too *)
+module F (X : sig type 'a t end) = struct
+  open X
+  let f : type a b. (a, b t) eq -> (b, a t) eq -> (a, a t t) eq =
+    fun Refl Refl -> Refl;;
+end;; (* should fail *)
+[%%expect{|
+Line _, characters 21-25:
+Error: This expression has type (a, a) eq
+       but an expression was expected of type (a, a X.t X.t) eq
+       Type a = b X.t is not compatible with type a X.t X.t
+       Type b is not compatible with type a X.t
+|}]
