c545e04ee8cdd4635a43ad7757f9e0f12286f087
Jacques Garrigue <garrigue@math.nagoya-u.ac.jp>
2016-10-13 21:05:01 +0900

    PR#6608: remove extended_expression field if all fields are explicitly given

--- testsuite/tests/typing-misc/records.ml
+++ testsuite/tests/typing-misc/records.ml
@@ -93,3 +93,20 @@ let f (x : Complex.t) = x.Complex.z;;
 Line _, characters 26-35:
 Error: Unbound record field Complex.z
 |}];;
+
+
+(* PR#6608 *)
+{ "reference" with contents = 0 }
+[%%expect{|
+Line _, characters 0-33:
+Warning 23: all the fields are explicitly listed in this record:
+the 'with' clause is useless.
+- : int ref = {contents = 0}
+|}];;
+{ true with contents = 0 }
+[%%expect{|
+Line _, characters 0-26:
+Warning 23: all the fields are explicitly listed in this record:
+the 'with' clause is useless.
+- : int ref = {contents = 0}
+|}];;
