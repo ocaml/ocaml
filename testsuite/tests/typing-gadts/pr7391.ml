c5db9fc85a0a302183d1dc02e75bc8cefa28ac59
Jacques Garrigue <garrigue@math.nagoya-u.ac.jp>
2016-10-20 23:57:53 +0900

    Fix PR#7391

---
+++ testsuite/tests/typing-gadts/pr7391.ml
@@ -0,0 +1,28 @@
+class virtual child1 parent =
+  object
+    method private parent = parent
+  end
+
+class virtual child2 =
+  object(_ : 'self)
+    constraint 'parent = < previous: 'self option; .. >
+    method private virtual parent: 'parent
+  end
+
+let _ =
+  object(self)
+    method previous = None
+    method child =
+      object
+        inherit child1 self
+        inherit child2
+      end
+  end;;
+[%%expect{|
+class virtual child1 : 'a -> object method private parent : 'a end
+class virtual child2 :
+  object ('a)
+    method private virtual parent : < previous : 'a option; .. >
+  end
+- : < child : child2; previous : child2 option > = <obj>
+|}]
