1f9608a3aa24300094852983a525e271efef2796
David Allsopp <david.allsopp@metastack.com>
2016-11-13 15:08:31 -0500

    Fix segfault in Unix.create_process on Windows (#912)

--- otherlibs/win32unix/createprocess.c
+++ otherlibs/win32unix/createprocess.c
@@ -13,6 +13,8 @@
 /*                                                                        */
 /**************************************************************************/
 
+#define CAML_INTERNALS
+
 #include <caml/mlvalues.h>
 #include "unixsupport.h"
 #include <windows.h>
