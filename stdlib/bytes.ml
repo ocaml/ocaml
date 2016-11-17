b77126639483510c6a543f138ff07e246a12a1b3
Pieter Goetschalckx <3.14.e.ter@gmail.com>
2016-11-18 00:24:45 +0100

    Correct behaviour of index_from and rindex_from

--- stdlib/bytes.ml
+++ stdlib/bytes.ml
@@ -234,12 +234,12 @@ let index_opt s c = index_rec_opt s (length s) 0 c
 
 let index_from s i c =
   let l = length s in
-  if i < 0 || i > l then invalid_arg "String.index_from / Bytes.index_from" else
+  if i < 0 || i >= l then invalid_arg "String.index_from / Bytes.index_from" else
   index_rec s l i c
 
 let index_from_opt s i c =
   let l = length s in
-  if i < 0 || i > l then invalid_arg "String.index_from_opt / Bytes.index_from_opt" else
+  if i < 0 || i >= l then invalid_arg "String.index_from_opt / Bytes.index_from_opt" else
   index_rec_opt s l i c
 
 let rec rindex_rec s i c =
@@ -249,7 +249,7 @@ let rec rindex_rec s i c =
 let rindex s c = rindex_rec s (length s - 1) c
 
 let rindex_from s i c =
-  if i < -1 || i >= length s then
+  if i < 0 || i >= length s then
     invalid_arg "String.rindex_from / Bytes.rindex_from"
   else
     rindex_rec s i c
@@ -261,7 +261,7 @@ let rec rindex_rec_opt s i c =
 let rindex_opt s c = rindex_rec_opt s (length s - 1) c
 
 let rindex_from_opt s i c =
-  if i < -1 || i >= length s then
+  if i < 0 || i >= length s then
     invalid_arg "String.rindex_from_opt / Bytes.rindex_from_opt"
   else
     rindex_rec_opt s i c
