ocaml/ocaml

commit 1fff9a8daeaadf0813aec4e0b6e779b8a755ce15

Remove naked pointers and Obj.magic from natdynlink.

asmrun/natdynlink.c
@@ -24,6 +24,13 @@
 #include <stdio.h>
 #include <string.h>
 
+#define Handle_val(v) (*((void **) (v)))
+static value Val_handle(void* handle) {
+  value res = caml_alloc_small(1, Abstract_tag);
+  Handle_val(res) = handle;
+  return res;
+}
+
 static void *getsym(void *handle, char *module, char *name){
   char *fullname = caml_strconcat(3, "caml", module, name);
   void *sym;
@@ -37,7 +44,7 @@ extern char caml_globals_map[];
 
 CAMLprim value caml_natdynlink_getmap(value unit)
 {
-  return (value)caml_globals_map;
+  return caml_input_value_from_malloc(caml_globals_map, 0);
 }
 
 CAMLprim value caml_natdynlink_globals_inited(value unit)
@@ -47,32 +54,36 @@ CAMLprim value caml_natdynlink_globals_inited(value unit)
 
 CAMLprim value caml_natdynlink_open(value filename, value global)
 {
-  CAMLparam1 (filename);
-  CAMLlocal1 (res);
+  CAMLparam2 (filename, global);
+  CAMLlocal3 (res, handle, header);
   void *sym;
-  void *handle;
+  void *dlhandle;
 
   /* TODO: dlclose in case of error... */
 
-  handle = caml_dlopen(String_val(filename), 1, Int_val(global));
+  dlhandle = caml_dlopen(String_val(filename), 1, Int_val(global));
 
-  if (NULL == handle)
-    CAMLreturn(caml_copy_string(caml_dlerror()));
+  if (NULL == dlhandle)
+    caml_failwith(caml_dlerror());
 
-  sym = caml_dlsym(handle, "caml_plugin_header");
+  sym = caml_dlsym(dlhandle, "caml_plugin_header");
   if (NULL == sym)
-    CAMLreturn(caml_copy_string("not an OCaml plugin"));
+    caml_failwith("not an OCaml plugin");
+
+  handle = Val_handle(dlhandle);
+  header = caml_input_value_from_malloc(sym, 0);
 
   res = caml_alloc_tuple(2);
-  Field(res, 0) = (value) handle;
-  Field(res, 1) = (value) (sym);
+  Field(res, 0) = handle;
+  Field(res, 1) = header;
   CAMLreturn(res);
 }
 
-CAMLprim value caml_natdynlink_run(void *handle, value symbol) {
-  CAMLparam1 (symbol);
+CAMLprim value caml_natdynlink_run(value handle_v, value symbol) {
+  CAMLparam2 (handle_v, symbol);
   CAMLlocal1 (result);
   void *sym,*sym2;
+  void* handle = Handle_val(handle_v);
   struct code_fragment * cf;
 
 #define optsym(n) getsym(handle,unit,n)
@@ -115,7 +126,7 @@ CAMLprim value caml_natdynlink_run(void *handle, value symbol) {
 CAMLprim value caml_natdynlink_run_toplevel(value filename, value symbol)
 {
   CAMLparam2 (filename, symbol);
-  CAMLlocal2 (res, v);
+  CAMLlocal3 (res, v, handle_v);
   void *handle;
 
   /* TODO: dlclose in case of error... */
@@ -127,8 +138,9 @@ CAMLprim value caml_natdynlink_run_toplevel(value filename, value symbol)
     v = caml_copy_string(caml_dlerror());
     Store_field(res, 0, v);
   } else {
+    handle_v = Val_handle(handle);
     res = caml_alloc(1,0);
-    v = caml_natdynlink_run(handle, symbol);
+    v = caml_natdynlink_run(handle_v, symbol);
     Store_field(res, 0, v);
   }
   CAMLreturn(res);

otherlibs/dynlink/natdynlink.ml
@@ -13,11 +13,15 @@
 
 (* Dynamic loading of .cmx files *)
 
+open Cmx_format
+
 type handle
 
-external ndl_open: string -> bool -> handle * bytes = "caml_natdynlink_open"
+type globals_map = (string * Digest.t * Digest.t * string list) list
+
+external ndl_open: string -> bool -> handle * dynheader = "caml_natdynlink_open"
 external ndl_run: handle -> string -> unit = "caml_natdynlink_run"
-external ndl_getmap: unit -> bytes = "caml_natdynlink_getmap"
+external ndl_getmap: unit -> globals_map = "caml_natdynlink_getmap"
 external ndl_globals_inited: unit -> int = "caml_natdynlink_globals_inited"
 
 type linking_error =
@@ -38,8 +42,6 @@ type error =
 
 exception Error of error
 
-open Cmx_format
-
 (* Copied from config.ml to avoid dependencies *)
 let cmxs_magic_number = "Caml2007D002"
 
@@ -51,11 +53,10 @@ let read_file filename priv =
   let dll = dll_filename filename in
   if not (Sys.file_exists dll) then raise (Error (File_not_found dll));
 
-  let (handle,data) as res = ndl_open dll (not priv) in
-  if Obj.tag (Obj.repr res) = Obj.string_tag
-  then raise (Error (Cannot_open_dll (Obj.magic res)));
+  let (handle,header) = try
+      ndl_open dll (not priv)
+    with Failure s -> raise (Error (Cannot_open_dll s)) in
 
-  let header : dynheader = Marshal.from_bytes data 0 in
   if header.dynu_magic <> cmxs_magic_number
   then raise(Error(Not_a_bytecode_file dll));
   (dll, handle, header.dynu_units)
@@ -87,8 +88,7 @@ let allow_extension = ref true
 let inited = ref false
 
 let default_available_units () =
-  let map : (string*Digest.t*Digest.t*string list) list =
-    Marshal.from_bytes (ndl_getmap ()) 0 in
+  let map  = ndl_getmap () in
   let exe = Sys.executable_name in
   let rank = ref 0 in
   global_state :=

commit 7a0fe4a6e75cb161bef0544c41b2429e79202ac3

Fix natdynlink bug introduced by 1fff9a8

asmrun/natdynlink.c
@@ -25,6 +25,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <limits.h>
 
 #define Handle_val(v) (*((void **) (v)))
 static value Val_handle(void* handle) {
@@ -46,7 +47,7 @@ extern char caml_globals_map[];
 
 CAMLprim value caml_natdynlink_getmap(value unit)
 {
-  return caml_input_value_from_malloc(caml_globals_map, 0);
+  return caml_input_value_from_block(caml_globals_map, INT_MAX);
 }
 
 CAMLprim value caml_natdynlink_globals_inited(value unit)
@@ -78,7 +79,7 @@ CAMLprim value caml_natdynlink_open(value filename, value global)
     caml_failwith("not an OCaml plugin");
 
   handle = Val_handle(dlhandle);
-  header = caml_input_value_from_malloc(sym, 0);
+  header = caml_input_value_from_block(sym, INT_MAX);
 
   res = caml_alloc_tuple(2);
   Init_field(res, 0, handle);