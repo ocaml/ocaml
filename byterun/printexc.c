ocaml/ocaml

commit 96d1eec91135303a011b7b4d3a1c09f6bd94b1e5

Remove Init_field.
Mostly s/Init_field/caml_initialize_field/g, with
some additions of caml_alloc_N where needed.

byterun/terminfo.c
@@ -42,7 +42,8 @@ static char *standend = NULL;
 
 CAMLprim value caml_terminfo_setup (value vchan)
 {
-  value result;
+  CAMLparam1(vchan);
+  CAMLlocal1(result);
   static char buffer[1024];
   char *term;
 
@@ -67,8 +68,8 @@ CAMLprim value caml_terminfo_setup (value vchan)
     return Bad_term;
   }
   result = caml_alloc_small (1, Good_term_tag);
-  Init_field (result, 0, Val_int (num_lines));
-  return result;
+  caml_initialize_field (result, 0, Val_int (num_lines));
+  CAMLreturn (result);
 }
 
 static int terminfo_putc (int c)

asmrun/backtrace.c
@@ -312,14 +312,14 @@ CAMLprim value caml_convert_raw_backtrace_slot(value backtrace_slot) {
   if (li.loc_valid) {
     fname = caml_copy_string(li.loc_filename);
     p = caml_alloc_small(5, 0);
-    Init_field(p, 0, Val_bool(li.loc_is_raise));
-    Init_field(p, 1, fname);
-    Init_field(p, 2, Val_int(li.loc_lnum));
-    Init_field(p, 3, Val_int(li.loc_startchr));
-    Init_field(p, 4, Val_int(li.loc_endchr));
+    caml_initialize_field(p, 0, Val_bool(li.loc_is_raise));
+    caml_initialize_field(p, 1, fname);
+    caml_initialize_field(p, 2, Val_int(li.loc_lnum));
+    caml_initialize_field(p, 3, Val_int(li.loc_startchr));
+    caml_initialize_field(p, 4, Val_int(li.loc_endchr));
   } else {
     p = caml_alloc_small(1, 1);
-    Init_field(p, 0, Val_bool(li.loc_is_raise));
+    caml_initialize_field(p, 0, Val_bool(li.loc_is_raise));
   }
 
   CAMLreturn(p);
@@ -358,7 +358,7 @@ CAMLprim value caml_get_exception_raw_backtrace(value unit)
     res = caml_alloc(saved_backtrace_pos, tag);
     for (i = 0; i < saved_backtrace_pos; i++) {
       /* [Val_Descrptr] always returns an immediate. */
-      Init_field(res, i, Val_Descrptr(saved_caml_backtrace_buffer[i]));
+      caml_initialize_field(res, i, Val_Descrptr(saved_caml_backtrace_buffer[i]));
     }
   }
 
@@ -388,6 +388,6 @@ CAMLprim value caml_get_exception_backtrace(value unit)
     Store_field(arr, i, caml_convert_raw_backtrace_slot(Field(backtrace, i)));
   }
 
-  res = caml_alloc_small(1, 0); Init_field(res, 0, arr); /* Some */
+  res = caml_alloc_small(1, 0); caml_initialize_field(res, 0, arr); /* Some */
   CAMLreturn(res);
 }

asmrun/fail.c
@@ -89,8 +89,8 @@ void caml_raise_with_arg(value tag, value arg)
   CAMLlocal1 (bucket);
 
   bucket = caml_alloc_small (2, 0);
-  Init_field(bucket, 0, tag);
-  Init_field(bucket, 1, arg);
+  caml_initialize_field(bucket, 0, tag);
+  caml_initialize_field(bucket, 1, arg);
   caml_raise(bucket);
   CAMLnoreturn;
 }
@@ -99,13 +99,13 @@ void caml_raise_with_args(value tag, int nargs, value args[])
 {
   CAMLparam1 (tag);
   CAMLxparamN (args, nargs);
-  value bucket;
+  CAMLlocal1 (bucket);
   int i;
 
   Assert(1 + nargs <= Max_young_wosize);
   bucket = caml_alloc_small (1 + nargs, 0);
-  Init_field(bucket, 0, tag);
-  for (i = 0; i < nargs; i++) Init_field(bucket, 1 + i, args[i]);
+  caml_initialize_field(bucket, 0, tag);
+  for (i = 0; i < nargs; i++) caml_initialize_field(bucket, 1 + i, args[i]);
   caml_raise(bucket);
   CAMLnoreturn;
 }

asmrun/natdynlink.c
@@ -82,8 +82,8 @@ CAMLprim value caml_natdynlink_open(value filename, value global)
   header = caml_input_value_from_block(sym, INT_MAX);
 
   res = caml_alloc_tuple(2);
-  Init_field(res, 0, handle);
-  Init_field(res, 1, header);
+  caml_initialize_field(res, 0, handle);
+  caml_initialize_field(res, 1, header);
   CAMLreturn(res);
 }
 

byterun/alloc.c
@@ -43,7 +43,7 @@ CAMLexport value caml_alloc (mlsize_t wosize, tag_t tag)
         #ifdef DEBUG
           init_val = Debug_uninit_minor;
         #endif
-        Init_field (result, i, init_val);
+        Op_val(result)[i] = init_val;
       }
     }
   }else{

byterun/array.c
@@ -168,7 +168,7 @@ CAMLprim value caml_make_vect(value len, value init)
     if (size > Max_wosize) caml_invalid_argument("Array.make");
     if (size < Max_young_wosize) {
       res = caml_alloc_small(size, 0);
-      for (i = 0; i < size; i++) Init_field(res, i, init);
+      for (i = 0; i < size; i++) caml_initialize_field(res, i, init);
     }
     else {
       /* make sure init is not young, to avoid creating

byterun/backtrace.c
@@ -438,14 +438,14 @@ CAMLprim value caml_convert_raw_backtrace_slot(value backtrace_slot) {
   if (li.loc_valid) {
     fname = caml_copy_string(li.loc_filename);
     p = caml_alloc_small(5, 0);
-    Init_field(p, 0, Val_bool(li.loc_is_raise));
-    Init_field(p, 1, fname);
-    Init_field(p, 2, Val_int(li.loc_lnum));
-    Init_field(p, 3, Val_int(li.loc_startchr));
-    Init_field(p, 4, Val_int(li.loc_endchr));
+    caml_initialize_field(p, 0, Val_bool(li.loc_is_raise));
+    caml_initialize_field(p, 1, fname);
+    caml_initialize_field(p, 2, Val_int(li.loc_lnum));
+    caml_initialize_field(p, 3, Val_int(li.loc_startchr));
+    caml_initialize_field(p, 4, Val_int(li.loc_endchr));
   } else {
     p = caml_alloc_small(1, 1);
-    Init_field(p, 0, Val_bool(li.loc_is_raise));
+    caml_initialize_field(p, 0, Val_bool(li.loc_is_raise));
   }
   CAMLreturn(p);
 }
@@ -461,7 +461,7 @@ CAMLprim value caml_get_exception_raw_backtrace(value unit)
   if(caml_backtrace_buffer != NULL) {
     intnat i;
     for(i = 0; i < caml_domain_state->backtrace_pos; i++)
-      Init_field(res, i, Val_Codet(caml_backtrace_buffer[i]));
+      caml_initialize_field(res, i, Val_Codet(caml_backtrace_buffer[i]));
   }
 
   CAMLreturn(res);
@@ -499,7 +499,7 @@ CAMLprim value caml_get_exception_backtrace(value unit)
               caml_modify_field(arr, i, slot);
           }
       }
-      res = caml_alloc_small(1, 0); Init_field(res, 0, arr); /* Some */
+      res = caml_alloc_small(1, 0); caml_initialize_field(res, 0, arr); /* Some */
   }
   CAMLreturn(res);
 }

byterun/caml/mlvalues.h
@@ -210,23 +210,17 @@ static inline value Field(value x, int i) {
   return Is_foreign(v) ? caml_read_barrier(x, i) : v;
 }
 
-/*
-static inline value FieldImm(value x, int i) {
-  Assert (!Is_foreign(x));
+static inline void caml_read_field(value x, int i, value* ret) {
   Assert (Hd_val(x));
-  value v = (((value*)x))[i];
-  Assert (v != Debug_free_major);
-  Assert (v != Debug_free_minor);
+  value v = Op_val(x)[i];
   //if (Is_young(v)) Assert(young_ptr < (char*)v);
-  return v;
-} */
+  *ret = Is_foreign(v) ? caml_read_barrier(x, i) : v;
+}
 
-#define FieldImm(x, i) (((value *)(x)) [i])
-//#define Field(x, i) (((value *)(x)) [i] + 0)
+#define Field_imm(x, i) (Op_val(x)[i] + 0)
 
-/* initialise a field of an object just allocated on the minor heap */
-#define Init_field(block, offset, val) (Op_val(block)[offset] = val)
-
+#define Int_field(x, i) Int_val(Op_val(x)[i])
+#define Long_field(x, i) Long_val(Op_val(x)[i])
 
 typedef int32 opcode_t;
 typedef opcode_t * code_t;

commit 841483625a0570d8abbc2f8fd3790ad6b8bd14dd

Fix bug introduced by 96d1eec9 (lack of CAMLreturn)
A good way to find such bugs is "make CC='clang --analyze'"

byterun/printexc.c
@@ -105,7 +105,7 @@ CAMLexport char * caml_format_exception(value exn)
   *buf.ptr = 0;              /* Terminate string */
   i = buf.ptr - buf.data + 1;
   res = malloc(i);
-  if (res == NULL) return NULL;
+  if (res == NULL) CAMLreturnT (char*, NULL);
   memmove(res, buf.data, i);
   CAMLreturnT (char*, res);
 }

byterun/terminfo.c
@@ -50,8 +50,10 @@ CAMLprim value caml_terminfo_setup (value vchan)
   chan = Channel (vchan);
 
   term = getenv ("TERM");
-  if (term == NULL) return Bad_term;
-  if (tgetent(buffer, term) != 1) return Bad_term;
+  if (term == NULL)
+    CAMLreturn (Bad_term);
+  if (tgetent(buffer, term) != 1)
+    CAMLreturn (Bad_term);
 
   num_lines = tgetnum ("li");
   up = tgetstr ("up", &area_p);
@@ -65,7 +67,7 @@ CAMLprim value caml_terminfo_setup (value vchan)
   Assert (area_p <= area + 1024);
   if (num_lines == -1 || up == NULL || down == NULL
       || standout == NULL || standend == NULL){
-    return Bad_term;
+    CAMLreturn (Bad_term);
   }
   result = caml_alloc_small (1, Good_term_tag);
   caml_initialize_field (result, 0, Val_int (num_lines));