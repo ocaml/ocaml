52d44f1748ec5e6f7c32190ff8c15a39a4994640
Xavier Leroy <xavier.leroy@inria.fr>
2017-01-13 16:30:58 +0100

    Make -linkall applicable to single compilation units
    With this commit, "ocamlc -c -linkall" or "ocamlopt -c -linkall" produce object files that have the "force link" flag set.  Once put in library files (.cma/.cmxa), these object files will always be linked when the library is linked, even if no definition from the object file is referenced.  However, other object files in the library can still be removed if none of their definitions are referenced.
    
    In this respect, we get finer-grained control on which object files should always be linked, typically because they contain important initialization code.

--- asmcomp/compilenv.ml
+++ asmcomp/compilenv.ml
@@ -129,7 +129,7 @@ let reset ?packname ~source_provenance:file name =
   current_unit.ui_curry_fun <- [];
   current_unit.ui_apply_fun <- [];
   current_unit.ui_send_fun <- [];
-  current_unit.ui_force_link <- false;
+  current_unit.ui_force_link <- !Clflags.link_everything;
   Hashtbl.clear exported_constants;
   structured_constants := structured_constants_empty;
   current_unit.ui_export_info <- default_ui_export_info;
