9b2b2b64f853b0a325f0352c3e08009c72536001
Bernhard Schommer <bernhardschommer@gmail.com>
2017-01-13 20:18:26 +0100

    PR#7460, GPR#1011: Fix uncaught Arg.Bad exception in compenv.ml
    This was a regression in 4.04.0, where passing files with unknown
    extension (foo.xwds) to the compiler raises an uncaught Arg.Bad
    exception.

--- driver/optmain.ml
+++ driver/optmain.ml
@@ -236,15 +236,24 @@ let main () =
   let ppf = Format.err_formatter in
   try
     readenv ppf Before_args;
-    Arg.parse_expand (Arch.command_line_options @ Options.list) anonymous usage;
+    let spec = Arch.command_line_options @ Options.list in
+    Arg.parse_expand spec anonymous usage;
     if !gprofile && not Config.profiling then
       fatal "Profiling with \"gprof\" is not supported on this platform.";
-    Compenv.process_deferred_actions
-      (ppf,
-       Optcompile.implementation ~backend,
-       Optcompile.interface,
-       ".cmx",
-       ".cmxa");
+    begin try
+      Compenv.process_deferred_actions
+        (ppf,
+         Optcompile.implementation ~backend,
+         Optcompile.interface,
+         ".cmx",
+         ".cmxa");
+    with Arg.Bad msg ->
+      begin
+        prerr_endline msg;
+        Arg.usage spec usage;
+        exit 2
+      end
+    end;
     readenv ppf Before_link;
     if
       List.length (List.filter (fun x -> !x)
