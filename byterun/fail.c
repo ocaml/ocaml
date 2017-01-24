be0ed7ed98ebb6d8a5f4bb8bcef67a0c6135cbe7
KC Sivaramakrishnan <sk826@cl.cam.ac.uk>
2017-01-24 17:02:02 +0000

    More cleanup

--- byterun/fail.c
+++ byterun/fail.c
@@ -28,19 +28,20 @@
 
 CAMLexport void caml_raise(value v)
 {
-  CAML_DOMAIN_STATE->exn_bucket = v;
-  if (CAML_DOMAIN_STATE->external_raise == NULL) caml_fatal_uncaught_exception(v);
-  while (CAML_LOCAL_ROOTS != CAML_DOMAIN_STATE->external_raise->local_roots) {
-    Assert(CAML_LOCAL_ROOTS != NULL);
-    struct caml__mutex_unwind* m = CAML_LOCAL_ROOTS->mutexes;
+  struct caml_domain_state* caml_domain_state = CAML_DOMAIN_STATE;
+  caml_domain_state->exn_bucket = v;
+  if (caml_domain_state->external_raise == NULL) caml_fatal_uncaught_exception(v);
+  while (caml_domain_state->local_roots != CAML_DOMAIN_STATE->external_raise->local_roots) {
+    Assert(caml_domain_state->local_roots != NULL);
+    struct caml__mutex_unwind* m = caml_domain_state->local_roots->mutexes;
     while (m) {
       /* unlocked in reverse order of locking */
       caml_plat_unlock(m->mutex);
       m = m->next;
     }
-    CAML_LOCAL_ROOTS = CAML_LOCAL_ROOTS->next;
+    caml_domain_state->local_roots = caml_domain_state->local_roots->next;
   }
-  siglongjmp(CAML_DOMAIN_STATE->external_raise->jmp->buf, 1);
+  siglongjmp(caml_domain_state->external_raise->jmp->buf, 1);
 }
 
 CAMLexport void caml_raise_constant(value tag)
