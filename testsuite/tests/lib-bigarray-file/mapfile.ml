5839c9827d9fdd55e5a9eff6bbd6173370c30bbb
Xavier Leroy <xavier.leroy@inria.fr>
2017-02-14 13:59:18 +0100

    Reorganize some of the Unix and Bigarray tests
    tests/lib-unix  contains Unix-only tests
    tests/lib-bigarray contains Bigarray-only tests
    tests/lib-bigarray-file contains test that need both Unix and Bigarray
    
    The purpose is to avoid doing Unix.fork() in tests linked with bigarray.cma, because this currently causes failures in bytecode under Cygwin.

--- testsuite/tests/lib-bigarray-file/mapfile.ml
+++ testsuite/tests/lib-bigarray-file/mapfile.ml
@@ -26,40 +26,6 @@ let test test_number answer correct_answer =
 (* Tests *)
 
 let tests () =
-  let drain pipe =
-    let max = 2048 in
-    let buf = Buffer.create 2048 in
-    let tmp = Bytes.create max in
-    while begin
-      try
-        let len = Unix.read pipe tmp 0 max in
-        Buffer.add_subbytes buf tmp 0 len;
-        len > 0
-      with Unix.Unix_error (Unix.EPIPE, _, _) when false ->
-        false
-    end do () done;
-    Buffer.contents buf
-  in
-
-  let run exe args =
-    let out_in, out_out = Unix.pipe () in
-    let err_in, err_out = Unix.pipe () in
-    let args = Array.append [| exe |] args in
-    let pid = Unix.create_process exe args Unix.stdin out_out err_out in
-    Unix.close out_out;
-    Unix.close err_out;
-    let output = drain out_in in
-    let error = drain err_in in
-    Unix.close out_in;
-    Unix.close err_in;
-    let _pid, status = Unix.waitpid [ ] pid in
-    status, output, error
-  in
-
-  testing_function "create_process";
-  ignore (run "cp" [||]);
-  test 1 () ();
-
   testing_function "map_file";
   let mapped_file = Filename.temp_file "bigarray" ".data" in
   begin
