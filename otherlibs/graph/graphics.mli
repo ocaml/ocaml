153c2f70e312d8efc23aef3b5db61d09b258b2ff
Xavier Leroy <xavier.leroy@inria.fr>
2017-02-17 16:11:23 +0100

    PR#7174: document issue with coordinates not representable in 16 bits
    This is a X Windows limitation.

--- otherlibs/graph/graphics.mli
+++ otherlibs/graph/graphics.mli
@@ -48,7 +48,10 @@ external size_y : unit -> int = "caml_gr_size_y"
 (** Return the size of the graphics window. Coordinates of the screen
    pixels range over [0 .. size_x()-1] and [0 .. size_y()-1].
    Drawings outside of this rectangle are clipped, without causing
-   an error. The origin (0,0) is at the lower left corner. *)
+   an error. The origin (0,0) is at the lower left corner. 
+   Some implementation (e.g. X Windows) represent coordinates by
+   16-bit integers, hence wrong clipping may occur with coordinates
+   below [-32768] or above [32676]. *)
 
 (** {6 Colors} *)
 
