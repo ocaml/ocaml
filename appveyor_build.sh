4c883a9f3bd6fdce2e09ab8e0505ab0ba6756b02
David Allsopp <david.allsopp@metastack.com>
2017-02-20 00:39:07 +0100

    Test mingw32 as well as msvc64 on AppVeyor

--- appveyor_build.sh
+++ appveyor_build.sh
@@ -29,6 +29,14 @@ function run {
 
 cd $APPVEYOR_BUILD_FOLDER
 
+git worktree add ../build-mingw32 -b appveyor-build-mingw32
+
+cd ../build-mingw32
+
+git submodule update --init flexdll
+
+cd $APPVEYOR_BUILD_FOLDER
+
 cp config/m-nt.h config/m.h
 cp config/s-nt.h config/s.h
 cp config/Makefile.msvc64 config/Makefile
@@ -42,3 +50,17 @@ run "make world" make world
 run "make bootstrap" make bootstrap
 run "make opt" make opt
 run "make opt.opt" make opt.opt
+
+cd ../build-mingw32
+
+cp config/m-nt.h config/m.h
+cp config/s-nt.h config/s.h
+cp config/Makefile.mingw config/Makefile
+
+PREFIX="C:/Program Files/OCaml-mingw32"
+echo "Edit config/Makefile to set PREFIX=$PREFIX"
+sed -i -e "s|PREFIX=.*|PREFIX=$PREFIX|" config/Makefile
+#run "Content of config/Makefile" cat config/Makefile
+
+run "make flexdll" make flexdll
+run "make world.opt" make world.opt
