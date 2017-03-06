bd0c2ec7a04113b2e5b9ccfe3ee2f1a941838036
octachron <octa@polychoron.fr>
2017-03-06 11:07:24 +0100

    ocamldoc: avoid self-alias induced infinite loop

--- ocamldoc/odoc_module.ml
+++ ocamldoc/odoc_module.ml
@@ -219,37 +219,47 @@ let included_modules l =
     []
     l
 
+module S = Misc.StringSet
+
 (** Returns the list of elements of a module.
-   @param trans indicates if, for aliased modules, we must perform a transitive search.*)
-let rec module_elements ?(trans=true) m =
+   @param trans indicates if, for aliased modules, we must perform a transitive search.
+   @param visited is used to guard against aliases loop
+     (e.g [module rec M:sig end=M] induced loop.
+
+**)
+let rec module_elements ?(visited=S.empty) ?(trans=true) m =
   let rec iter_kind = function
       Module_struct l ->
-        print_DEBUG "Odoc_module.module_element: Module_struct";
+        print_DEBUG "Odoc_module.module_elements: Module_struct";
         l
     | Module_alias ma ->
-        print_DEBUG "Odoc_module.module_element: Module_alias";
+        print_DEBUG "Odoc_module.module_elements: Module_alias";
         if trans then
           match ma.ma_module with
             None -> []
-          | Some (Mod m) -> module_elements m
+          | Some (Mod m') ->
+              if m'.m_name = m.m_name || S.mem m'.m_name visited then
+                []
+              else
+                module_elements ~visited:(S.add m'.m_name visited) m'
           | Some (Modtype mt) -> module_type_elements mt
         else
           []
     | Module_functor (_, k)
     | Module_apply (k, _) ->
-        print_DEBUG "Odoc_module.module_element: Module_functor ou Module_apply";
+        print_DEBUG "Odoc_module.module_elements: Module_functor ou Module_apply";
         iter_kind k
     | Module_with (tk,_) ->
-        print_DEBUG "Odoc_module.module_element: Module_with";
+        print_DEBUG "Odoc_module.module_elements: Module_with";
         module_type_elements ~trans: trans
           { mt_name = "" ; mt_info = None ; mt_type = None ;
             mt_is_interface = false ; mt_file = "" ; mt_kind = Some tk ;
             mt_loc = Odoc_types.dummy_loc ;
           }
     | Module_constraint (k, _tk) ->
-        print_DEBUG "Odoc_module.module_element: Module_constraint";
+        print_DEBUG "Odoc_module.module_elements: Module_constraint";
       (* FIXME : use k or tk ? *)
-        module_elements ~trans: trans
+        module_elements ~visited ~trans: trans
           { m_name = "" ;
             m_info = None ;
             m_type = Types.Mty_signature [] ;
@@ -293,6 +303,11 @@ and module_type_elements ?(trans=true) mt =
   | Some (Module_type_typeof _) -> []
   in
   iter_kind mt.mt_kind
+
+(** Returns the list of elements of a module.
+   @param trans indicates if, for aliased modules, we must perform a transitive search.
+*)
+let module_elements ?(trans=true) m =  module_elements ~trans m
 
 (** Returns the list of values of a module.
   @param trans indicates if, for aliased modules, we must perform a transitive search.*)
@@ -459,20 +474,22 @@ let rec module_type_is_functor mt =
 
 (** The module is a functor if is defined as a functor or if it is an alias for a functor. *)
 let module_is_functor m =
-  let rec iter = function
+  let rec iter visited = function
       Module_functor _ -> true
     | Module_alias ma ->
         (
-         match ma.ma_module with
-           None -> false
-         | Some (Mod mo) -> iter mo.m_kind
-         | Some (Modtype mt) -> module_type_is_functor mt
+          not (ma.ma_name = m.m_name || S.mem ma.ma_name visited)
+          &&
+          match ma.ma_module with
+            None -> false
+          | Some (Mod mo) -> iter (S.add ma.ma_name visited) mo.m_kind
+          | Some (Modtype mt) -> module_type_is_functor mt
         )
     | Module_constraint (k, _) ->
-        iter k
+        iter visited k
     | _ -> false
   in
-  iter m.m_kind
+  iter S.empty m.m_kind
 
 (** Returns the list of values of a module type.
   @param trans indicates if, for aliased modules, we must perform a transitive search.*)
