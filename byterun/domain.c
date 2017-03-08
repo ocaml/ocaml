633bbd3484027a2c0fd74e7ad052b17e09036b8c
Stephen Dolan <stephen.dolan@cl.cam.ac.uk>
2017-03-08 12:01:16 +0000

    domain_state cleanup

--- byterun/domain.c
+++ byterun/domain.c
@@ -69,16 +69,14 @@ static __thread dom_internal* domain_self;
 #endif
 
 /* Statically assert that each field of domain_state is one word long and at the right index */
-/* Dummy domain_state for testing */
-static struct caml_domain_state* dummy_domain_state;
 #define DOMAIN_STATE(idx, type, name) \
-    CAML_STATIC_ASSERT(sizeof(dummy_domain_state->name) == sizeof(void*) && \
+    CAML_STATIC_ASSERT(sizeof(((struct caml_domain_state*)0)->name) == sizeof(void*) && \
                      offsetof(struct caml_domain_state, name) == idx * sizeof(void*));
 #include "caml/domain_state.tbl"
 #undef DOMAIN_STATE
 #ifndef NATIVE_CODE
   #define BYTE_DOMAIN_STATE(type, name) \
-      CAML_STATIC_ASSERT(sizeof(dummy_domain_state->name) == sizeof(void*));
+      CAML_STATIC_ASSERT(sizeof(((struct caml_domain_state*)0)->name) == sizeof(void*));
   #include "caml/byte_domain_state.tbl"
   #undef BYTE_DOMAIN_STATE
 #endif // BYTECODE
