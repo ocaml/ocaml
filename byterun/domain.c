902dc882df5b206ba76a67756bd89ce9e7d6a0e3
Stephen Dolan <stephen.dolan@cl.cam.ac.uk>
2017-03-08 15:24:30 +0000

    Use new CAMLalign(n) macro for domain state

--- byterun/domain.c
+++ byterun/domain.c
@@ -68,19 +68,6 @@ static __thread dom_internal* domain_self;
   CAMLexport __thread struct caml_domain_state* caml_domain_state;
 #endif
 
-/* Statically assert that each field of domain_state is one word long and at the right index */
-#define DOMAIN_STATE(idx, type, name) \
-    CAML_STATIC_ASSERT(sizeof(((struct caml_domain_state*)0)->name) == sizeof(void*) && \
-                     offsetof(struct caml_domain_state, name) == idx * sizeof(void*));
-#include "caml/domain_state.tbl"
-#undef DOMAIN_STATE
-#ifndef NATIVE_CODE
-  #define BYTE_DOMAIN_STATE(type, name) \
-      CAML_STATIC_ASSERT(sizeof(((struct caml_domain_state*)0)->name) == sizeof(void*));
-  #include "caml/byte_domain_state.tbl"
-  #undef BYTE_DOMAIN_STATE
-#endif // BYTECODE
-
 static __thread char domains_locked[Max_domains];
 
 /* If you lock a domain, you are responsible for handling
