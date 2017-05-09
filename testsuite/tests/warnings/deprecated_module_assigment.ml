da0b8297b2b9db879de023735001242b5a85ab8b
alainfrisch <alain@frisch.fr>
2017-05-09 12:58:36 +0200

    Check that extra deprecated attributes can be added during module coercion.

--- testsuite/tests/warnings/deprecated_module_assigment.ml
+++ testsuite/tests/warnings/deprecated_module_assigment.ml
@@ -11,3 +11,8 @@ module Z : sig val x : int [@@deprecated "..."] end = X
 module F(A : sig val x : int end) = struct end
 
 module B = F(X)
+
+
+
+module XX = struct let x = 7 end
+module YY : sig val x : int [@@deprecated "..."] end = XX
