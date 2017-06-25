91615fd05e458ad5e338ee5b57513f8f6dc792f8
octachron <octa@polychoron.fr>
2017-06-25 14:12:18 +0200

    PR7565: test case with nested object types abbrevs

--- testsuite/tests/typing-misc/pr7565.ml
+++ testsuite/tests/typing-misc/pr7565.ml
@@ -58,3 +58,33 @@ Error: This expression has type 'b t = < f : 'b -> int >
        but an expression was expected of type t_a = < f : 'a. 'a -> int >
        The universal variable 'a would escape its scope
 |}]
+;;
+
+type y= <n:unit;e:unit;s:unit;t:unit;e':unit;d:unit>
+type x = <i:y;n:y;n':y;e:y;r:y>
+type h = <f:x;a:x;r:x;t:x;o:x;o':x;m:x;a':x;n:x;y:x;f':x;i:x;e:x;l:x;d:x;s:x>
+type t_a = <f: 'a. 'a -> h ; g:h -> unit >
+let f (o:t_a) = o # f 0
+
+let () =
+  let o =
+    object
+      method f _ = assert false
+      method g  _ = ()
+    end
+  in
+  f o;;
+[%%expect{|
+type y = < d : unit; e : unit; e' : unit; n : unit; s : unit; t : unit >
+type x = < e : y; i : y; n : y; n' : y; r : y >
+type h =
+    < a : x; a' : x; d : x; e : x; f : x; f' : x; i : x; l : x; m : x; n :
+      x; o : x; o' : x; r : x; s : x; t : x; y : x >
+type t_a = < f : 'a. 'a -> h; g : h -> unit >
+val f : t_a -> h = <fun>
+Line _, characters 4-5:
+Error: This expression has type < f : 'b -> 'c; g : 'd -> unit >
+       but an expression was expected of type
+         t_a = < f : 'a. 'a -> h; g : h -> unit >
+       The universal variable 'a would escape its scope
+|}];;
