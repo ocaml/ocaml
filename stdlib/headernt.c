8bb01480cc5ee5ff6955f25d6fd7ec2fdcf4c16a
Nicolas Ojeda Bar <n.oje.bar@gmail.com>
2017-06-26 09:46:06 +0200

    Adapt headernt.c

--- stdlib/headernt.c
+++ stdlib/headernt.c
@@ -32,7 +32,7 @@
 #endif
 #endif
 
-char * default_runtime_name = RUNTIME_NAME;
+wchar_t * default_runtime_name = RUNTIME_NAME;
 
 static
 #if _MSC_VER >= 1200
@@ -47,10 +47,10 @@ unsigned long read_size(const char * const ptr)
          ((unsigned long) p[2] << 8) | p[3];
 }
 
-static __inline char * read_runtime_path(HANDLE h)
+static __inline wchar_t * read_runtime_path(HANDLE h)
 {
   char buffer[TRAILER_SIZE];
-  static char runtime_path[MAX_PATH];
+  static wchar_t runtime_path[MAX_PATH];
   DWORD nread;
   int num_sections, path_size, i;
   long ofs;
@@ -90,19 +90,21 @@ static BOOL WINAPI ctrl_handler(DWORD event)
 
 #define msg_and_length(msg) msg , (sizeof(msg) - 1)
 
-static __inline void __declspec(noreturn) run_runtime(char * runtime,
-         char * const cmdline)
+static __inline void __declspec(noreturn) run_runtime(wchar_t * runtime,
+         wchar_t * const cmdline)
 {
-  char path[MAX_PATH];
-  STARTUPINFOA stinfo;
+  wchar_t path[MAX_PATH];
+  STARTUPINFO stinfo;
   PROCESS_INFORMATION procinfo;
   DWORD retcode;
-  if (SearchPathA(NULL, runtime, ".exe", MAX_PATH, path, &runtime) == 0) {
+  if (SearchPath(NULL, runtime, L".exe", sizeof(path)/sizeof(wchar_t), path, &runtime) == 0) {
     HANDLE errh;
+    char runtime_cp[MAX_PATH];
     DWORD numwritten;
     errh = GetStdHandle(STD_ERROR_HANDLE);
+    WideCharToMultiByte(CP_UTF8, 0, runtime, -1, runtime_cp, sizeof(runtime_cp), NULL, NULL);
     WriteFile(errh, msg_and_length("Cannot exec "), &numwritten, NULL);
-    WriteFile(errh, runtime, strlen(runtime), &numwritten, NULL);
+    WriteFile(errh, msg_and_length(runtime_cp), &numwritten, NULL);
     WriteFile(errh, msg_and_length("\r\n"), &numwritten, NULL);
     ExitProcess(2);
 #if _MSC_VER >= 1200
@@ -120,13 +122,14 @@ static __inline void __declspec(noreturn) run_runtime(char * runtime,
   stinfo.dwFlags = 0;
   stinfo.cbReserved2 = 0;
   stinfo.lpReserved2 = NULL;
-  if (!CreateProcessA(path, cmdline, NULL, NULL, TRUE, 0, NULL, NULL,
-                      &stinfo, &procinfo)) {
+  if (!CreateProcess(path, cmdline, NULL, NULL, TRUE, 0, NULL, NULL,
+                     &stinfo, &procinfo)) {
     HANDLE errh;
+    char runtime_cp[MAX_PATH];
     DWORD numwritten;
     errh = GetStdHandle(STD_ERROR_HANDLE);
     WriteFile(errh, msg_and_length("Cannot exec "), &numwritten, NULL);
-    WriteFile(errh, runtime, strlen(runtime), &numwritten, NULL);
+    WriteFile(errh, runtime_cp, strlen(runtime_cp), &numwritten, NULL);
     WriteFile(errh, msg_and_length("\r\n"), &numwritten, NULL);
     ExitProcess(2);
 #if _MSC_VER >= 1200
@@ -149,20 +152,22 @@ int main()
 void __declspec(noreturn) __cdecl headerentry()
 #endif
 {
-  char truename[MAX_PATH];
-  char * cmdline = GetCommandLineA();
-  char * runtime_path;
+  wchar_t truename[MAX_PATH];
+  wchar_t * cmdline = GetCommandLine();
+  wchar_t * runtime_path;
   HANDLE h;
 
-  GetModuleFileNameA(NULL, truename, sizeof(truename));
-  h = CreateFileA(truename, GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE,
-                  NULL, OPEN_EXISTING, 0, NULL);
+  GetModuleFileName(NULL, truename, sizeof(truename)/sizeof(wchar_t));
+  h = CreateFile(truename, GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE,
+                 NULL, OPEN_EXISTING, 0, NULL);
   if (h == INVALID_HANDLE_VALUE ||
       (runtime_path = read_runtime_path(h)) == NULL) {
     HANDLE errh;
+    char truename_cp[MAX_PATH];
     DWORD numwritten;
     errh = GetStdHandle(STD_ERROR_HANDLE);
-    WriteFile(errh, truename, strlen(truename), &numwritten, NULL);
+    WideCharToMultiByte(CP_UTF8, 0, truename, -1, truename_cp, sizeof(truename_cp), NULL, NULL);
+    WriteFile(errh, msg_and_length(truename_cp), &numwritten, NULL);
     WriteFile(errh, msg_and_length(" not found or is not a bytecode"
                                    " executable file\r\n"),
               &numwritten, NULL);
