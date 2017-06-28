e78faef5250ad1c41878c6415ccfa62aeb286be4
Nicolas Ojeda Bar <n.oje.bar@gmail.com>
2017-06-28 18:14:17 +0200

    Re-implement Unix.environment using GetEnvironmentStrings()

--- otherlibs/win32unix/envir.c
+++ otherlibs/win32unix/envir.c
@@ -15,14 +15,27 @@
 
 #include <caml/mlvalues.h>
 #include <caml/alloc.h>
+#include <caml/memory.h>
 #include <caml/osdeps.h>
+
+#include <Windows.h>
 
 CAMLprim value unix_environment(value unit)
 {
+  CAMLparam0();
+  CAMLlocal2(v, result);
+  wchar_t * envp, * p;
+  int size, i;
+
   /* Win32 doesn't have a notion of setuid bit, so accessing environ is safe. */
-  if (_wenviron != NULL) {
-    return caml_alloc_array((void*)caml_copy_string_of_utf16, (const char**)_wenviron);
-  } else {
-    return Atom(0);
+  envp = GetEnvironmentStrings();
+  for (p = envp, size = 0; *p; p += wcslen(p) + 1) size++;
+  result = caml_alloc(size, 0);
+  for (p = envp, i = 0; *p; p += wcslen(p) + 1) {
+    v = caml_copy_string_of_utf16(p);
+    Store_field(result, i ++, v);
   }
+  FreeEnvironmentStrings(envp);
+
+  CAMLreturn(result);
 }
