670da762cf5a907a5cddbf60c69c7be054fd4b49
Jeremy Yallop <yallop@gmail.com>
2017-07-03 16:53:37 +0100

    Use a more robust search for no-change-needed lines based on merge-base.
    (No change needed.)

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -17,6 +17,13 @@ PREFIX=~/local
 
 MAKE=make SHELL=dash
 
+# TRAVIS_COMMIT_RANGE has the form   <commit1>...<commit2>
+# TRAVIS_CUR_HEAD is <commit1>
+# TRAVIS_PR_HEAD is <commit2>
+TRAVIS_CUR_HEAD=$(echo $TRAVIS_COMMIT_RANGE | sed 's!\.\.\..*!!')
+TRAVIS_PR_HEAD=$(echo $TRAVIS_COMMIT_RANGE | sed 's![^.]*\.\.\.!!')
+TRAVIS_MERGE_BASE=$(git merge-base $TRAVIS_CUR_HEAD $TRAVIS_PR_HEAD)
+
 BuildAndTest () {
   case $XARCH in
   i386)
@@ -98,12 +105,12 @@ on the github pull request.
 ------------------------------------------------------------------------
 EOF
   # check that Changes has been modified
-  git diff $TRAVIS_COMMIT_RANGE --name-only --exit-code Changes > /dev/null \
+  git diff $TRAVIS_MERGE_BASE..$TRAVIS_PR_HEAD --name-only --exit-code Changes > /dev/null \
   && CheckNoChangesMessage || echo pass
 }
 
 CheckNoChangesMessage () {
-  if test -n "$(git log --grep="[Nn]o [Cc]hange.* needed" --max-count=1 $TRAVIS_COMMIT_RANGE)"
+  if test -n "$(git log --grep="[Nn]o [Cc]hange.* needed" --max-count=1 ${TRAVIS_CUR_HEAD}..${TRAVIS_PR_HEAD})"
   then echo pass
   elif test -n "$(curl https://api.github.com/repos/$TRAVIS_REPO_SLUG/issues/$TRAVIS_PULL_REQUEST/labels \
        | grep 'no-change-entry-needed')"
