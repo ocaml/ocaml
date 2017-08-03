08ba63d51165b71d35fa8bfe75527f42ae270c62
Stephen Dolan <stephen.dolan@cl.cam.ac.uk>
2017-08-03 12:39:25 +0100

    More testing of Domain.join.

--- testsuite/tests/parallel/join.ml
+++ testsuite/tests/parallel/join.ml
@@ -1,9 +1,10 @@
 let main_join n =
   let a = Array.init n (fun _ -> false) in
   Array.init n (fun i -> Domain.spawn (fun () ->
-    a.(i) <- true
+    a.(i) <- true;
+    Some i
   )) |>
-  Array.iter Domain.join;
+  Array.iteri (fun i d -> let v = Domain.join d in assert (v = Some i));
   let ok = Array.to_list a |> List.for_all (fun x -> x) in
   assert ok
 
@@ -13,6 +14,19 @@ let rec other_join cnt n domain =
     |> other_join cnt (n-1)
   else
     Domain.join domain
+
+let join_exn () =
+  match Domain.(join (spawn (fun () -> failwith (String.make 5 '!')))) with
+  | _ -> assert false
+  | exception (Failure "!!!!!") -> ()
+
+let join_slow () =
+  let rec burn l =
+    if List.hd l > 14 then ()
+  else
+    burn (l @ l |> List.map (fun x -> x + 1)) in
+  assert (Domain.(join (spawn (fun () -> burn [0]; 42))) = 42)
+
 
 let join2 () =
   let r = ref false in
@@ -31,5 +45,7 @@ let () =
   other_join cnt 100 (Domain.spawn ignore);
   assert (!cnt = 100);
   join2 ();
+  join_exn ();
+  join_slow ();
   Gc.full_major ();
   Gc.full_major ()
