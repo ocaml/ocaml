8c3c0d93c36e466db4e15fa3801dbb8dda9eee06
Stephen Dolan <stephen.dolan@cl.cam.ac.uk>
2017-08-08 11:54:34 +0100

    Add test for deep callback chains

---
+++ testsuite/tests/callback/stack_overflow.ml
@@ -0,0 +1,14 @@
+external caml_to_c : (unit -> 'a) -> 'a = "caml_to_c"
+
+let rec deep = function
+  | 0 ->
+     ref 42
+  | n ->
+     caml_to_c (fun () -> deep (n-1))
+
+effect E : unit
+
+let () =
+  Printf.printf "%d\n%d\n%!"
+    (!(deep 1000))
+    (match deep 1000 with x -> !x | effect E k -> assert false)
