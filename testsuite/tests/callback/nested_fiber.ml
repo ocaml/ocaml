946fd14bd4ccbb29a63a8b4e4137df5e6366bc05
Stephen Dolan <stephen.dolan@cl.cam.ac.uk>
2017-08-08 12:23:27 +0100

    Add a test for callbacks inside fibers doing GC

---
+++ testsuite/tests/callback/nested_fiber.ml
@@ -0,0 +1,20 @@
+external caml_to_c : (unit -> 'a) -> 'a = "caml_to_c"
+
+effect E : unit
+
+let g () =
+  caml_to_c (fun () ->
+      Gc.full_major ();
+      Printf.printf "%d\n%!" 1)
+
+let f () =
+  let x = ref 2 in
+  match g () with
+  | effect E k -> assert false
+  | () ->
+     Printf.printf "%d\n" !x
+
+let () =
+  match f () with
+  | effect E k -> assert false
+  | () -> ()
