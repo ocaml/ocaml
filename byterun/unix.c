ocaml/ocaml

commit 65c932253b96651a6d13550a48dd386a89244ed0

String_val: return 'const char *' when -safe-string is globally enabled (#1274)

byterun/unix.c
@@ -131,9 +131,10 @@ caml_stat_string caml_decompose_path(struct ext_table * tbl, char * path)
   return p;
 }
 
-caml_stat_string caml_search_in_path(struct ext_table * path, char * name)
+caml_stat_string caml_search_in_path(struct ext_table * path, const char * name)
 {
-  char * p, * dir, * fullname;
+  const char * p;
+  char * dir, * fullname;
   int i;
   struct stat st;
 
@@ -195,7 +196,7 @@ static caml_stat_string cygwin_search_exe_in_path(struct ext_table * path, char 
 
 #endif
 
-caml_stat_string caml_search_exe_in_path(char * name)
+caml_stat_string caml_search_exe_in_path(const char * name)
 {
   struct ext_table path;
   char * tofree;
@@ -213,7 +214,7 @@ caml_stat_string caml_search_exe_in_path(char * name)
   return res;
 }
 
-caml_stat_string caml_search_dll_in_path(struct ext_table * path, char * name)
+caml_stat_string caml_search_dll_in_path(struct ext_table * path, const char * name)
 {
   caml_stat_string dllname;
   caml_stat_string res;
@@ -240,7 +241,7 @@ void caml_dlclose(void * handle)
   flexdll_dlclose(handle);
 }
 
-void * caml_dlsym(void * handle, char * name)
+void * caml_dlsym(void * handle, const char * name)
 {
   return flexdll_dlsym(handle, name);
 }
@@ -276,7 +277,7 @@ void caml_dlclose(void * handle)
   dlclose(handle);
 }
 
-void * caml_dlsym(void * handle, char * name)
+void * caml_dlsym(void * handle, const char * name)
 {
 #ifdef DL_NEEDS_UNDERSCORE
   char _name[1000] = "_";
@@ -286,7 +287,7 @@ void * caml_dlsym(void * handle, char * name)
   return dlsym(handle, name);
 }
 
-void * caml_globalsym(char * name)
+void * caml_globalsym(const char * name)
 {
 #ifdef RTLD_DEFAULT
   return caml_dlsym(RTLD_DEFAULT, name);

asmrun/natdynlink.c
@@ -31,7 +31,7 @@
 
 #include "caml/hooks.h"
 
-CAMLexport void (*caml_natdynlink_hook)(void* handle, char* unit) = NULL;
+CAMLexport void (*caml_natdynlink_hook)(void* handle, const char* unit) = NULL;
 
 #include <stdio.h>
 #include <string.h>
@@ -44,7 +44,7 @@ static value Val_handle(void* handle) {
   return res;
 }
 
-static void *getsym(void *handle, char *module, char *name){
+static void *getsym(void *handle, const char *module, const char *name){
   char *fullname = caml_stat_strconcat(3, "caml", module, name);
   void *sym;
   sym = caml_dlsym (handle, fullname);
@@ -103,7 +103,7 @@ CAMLprim value caml_natdynlink_run(value handle_v, value symbol) {
   struct code_fragment * cf;
 
 #define optsym(n) getsym(handle,unit,n)
-  char *unit;
+  const char *unit;
   void (*entrypoint)(void);
 
   unit = String_val(symbol);

byterun/alloc.c
@@ -110,6 +110,14 @@ CAMLexport value caml_alloc_string (mlsize_t len)
   return result;
 }
 
+/* [len] is a number of bytes (chars) */
+CAMLexport value caml_alloc_initialized_string (mlsize_t len, const char *p)
+{
+  value result = caml_alloc_string (len);
+  memcpy((char *)String_val(result), p, len);
+  return result;
+}
+
 /* [len] is a number of words.
    [mem] and [max] are relative (without unit).
 */
@@ -126,8 +134,7 @@ CAMLexport value caml_copy_string(char const *s)
   value res;
 
   len = strlen(s);
-  res = caml_alloc_string(len);
-  memmove(String_val(res), s, len);
+  res = caml_alloc_initialized_string(len, s);
   return res;
 }
 

byterun/callback.c
@@ -219,7 +219,7 @@ static unsigned int hash_value_name(char const *name)
 CAMLprim value caml_register_named_value(value vname, value val)
 {
   struct named_value * nv;
-  char * name = String_val(vname);
+  const char * name = String_val(vname);
   size_t namelen = strlen(name);
   unsigned int h = hash_value_name(name);
 

byterun/caml/alloc.h
@@ -32,6 +32,7 @@ CAMLextern value caml_alloc_small (mlsize_t wosize, tag_t);
 CAMLextern value caml_alloc_tuple (mlsize_t wosize);
 CAMLextern value caml_alloc_float_array (mlsize_t len);
 CAMLextern value caml_alloc_string (mlsize_t len);  /* len in bytes (chars) */
+CAMLextern value caml_alloc_initialized_string (mlsize_t len, const char *);
 CAMLextern value caml_copy_string (char const *);
 CAMLextern value caml_copy_string_array (char const **);
 CAMLextern value caml_copy_double (double);

byterun/caml/hooks.h
@@ -29,7 +29,7 @@ extern "C" {
 
 /* executed just before calling the entry point of a dynamically
    loaded native code module. */
-CAMLextern void (*caml_natdynlink_hook)(void* handle, char* unit);
+CAMLextern void (*caml_natdynlink_hook)(void* handle, const char* unit);
 
 #endif /* NATIVE_CODE */
 

byterun/caml/mlvalues.h
@@ -252,7 +252,12 @@ CAMLextern value caml_hash_variant(char const * tag);
 
 /* Strings. */
 #define String_tag 252
+#ifdef CAML_SAFE_STRING
+#define String_val(x) ((const char *) Bp_val(x))
+#else
 #define String_val(x) ((char *) Bp_val(x))
+#endif
+#define Bytes_val(x) ((unsigned char *) Bp_val(x))
 CAMLextern mlsize_t caml_string_length (value);   /* size in bytes */
 CAMLextern int caml_string_is_c_safe (value);
   /* true if string contains no '\0' null characters */

byterun/caml/osdeps.h
@@ -45,13 +45,13 @@ extern caml_stat_string caml_decompose_path(struct ext_table * tbl, char * path)
 
 /* Search the given file in the given list of directories.
    If not found, return a copy of [name]. */
-extern caml_stat_string caml_search_in_path(struct ext_table * path, char * name);
+extern caml_stat_string caml_search_in_path(struct ext_table * path, const char * name);
 
 /* Same, but search an executable name in the system path for executables. */
-CAMLextern caml_stat_string caml_search_exe_in_path(char * name);
+CAMLextern caml_stat_string caml_search_exe_in_path(const char * name);
 
 /* Same, but search a shared library in the given path. */
-extern caml_stat_string caml_search_dll_in_path(struct ext_table * path, char * name);
+extern caml_stat_string caml_search_dll_in_path(struct ext_table * path, const char * name);
 
 /* Open a shared library and return a handle on it.
    If [for_execution] is true, perform full symbol resolution and
@@ -69,9 +69,9 @@ extern void caml_dlclose(void * handle);
 
 /* Look up the given symbol in the given shared library.
    Return [NULL] if not found, or symbol value if found. */
-extern void * caml_dlsym(void * handle, char * name);
+extern void * caml_dlsym(void * handle, const char * name);
 
-extern void * caml_globalsym(char * name);
+extern void * caml_globalsym(const char * name);
 
 /* Return an error message describing the most recent dynlink failure. */
 extern char * caml_dlerror(void);

commit 56cd582505dab91b401b03ba1343e470265d75dc

Fix Cygwin breakage caused by #1274

byterun/unix.c
@@ -158,7 +158,7 @@ caml_stat_string caml_search_in_path(struct ext_table * path, const char * name)
 /* Cygwin needs special treatment because of the implicit ".exe" at the
    end of executable file names */
 
-static int cygwin_file_exists(char * name)
+static int cygwin_file_exists(const char * name)
 {
   int fd;
   /* Cannot use stat() here because it adds ".exe" implicitly */
@@ -168,9 +168,10 @@ static int cygwin_file_exists(char * name)
   return 1;
 }
 
-static caml_stat_string cygwin_search_exe_in_path(struct ext_table * path, char * name)
+static caml_stat_string cygwin_search_exe_in_path(struct ext_table * path, const char * name)
 {
-  char * p, * dir, * fullname;
+  const char * p;
+  char * dir, * fullname;
   int i;
 
   for (p = name; *p != 0; p++) {
@@ -246,7 +247,7 @@ void * caml_dlsym(void * handle, const char * name)
   return flexdll_dlsym(handle, name);
 }
 
-void * caml_globalsym(char * name)
+void * caml_globalsym(const char * name)
 {
   return flexdll_dlsym(flexdll_dlopen(NULL,0), name);
 }