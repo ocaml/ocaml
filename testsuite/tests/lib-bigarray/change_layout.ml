700942064325b5793b50e47fd754b6e83548ccd6
David Allsopp <david.allsopp@metastack.com>
2017-08-12 21:21:48 +0100

    Remove non-ASCII characters

--- testsuite/tests/lib-bigarray/change_layout.ml
+++ testsuite/tests/lib-bigarray/change_layout.ml
@@ -4,7 +4,7 @@ open Bigarray
 
 let pp_sep ppf () = Format.fprintf ppf ";@ "
  let print_array pp ppf a =
- Format.fprintf ppf "@[<hov>⟦%a⟧@]"
+ Format.fprintf ppf "@[<hov>[|%a|]@]"
     Format.(pp_print_list ~pp_sep pp) (Array.to_list a)
 
 let print_index = print_array Format.pp_print_int
