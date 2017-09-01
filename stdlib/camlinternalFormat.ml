1b7abeddf1780598010e0bb2b298e87a049f9ab4
Xavier Clerc <xclerc@janestreet.com>
2017-09-01 13:22:00 +0100

    Formats: use the optional width when formatting a boolean.

--- stdlib/camlinternalFormat.ml
+++ stdlib/camlinternalFormat.ml
@@ -96,8 +96,8 @@ fun ign fmt -> match ign with
   | Ignored_float (pad_opt, prec_opt) ->
     Param_format_EBB
       (Float (Float_f, pad_of_pad_opt pad_opt, prec_of_prec_opt prec_opt, fmt))
-  | Ignored_bool ->
-    Param_format_EBB (Bool fmt)
+  | Ignored_bool pad_opt ->
+    Param_format_EBB (Bool (pad_of_pad_opt pad_opt, fmt))
   | Ignored_format_arg (pad_opt, fmtty) ->
     Param_format_EBB (Format_arg (pad_opt, fmtty, fmt))
   | Ignored_format_subst (pad_opt, fmtty) ->
@@ -564,9 +564,10 @@ let bprint_fmt buf fmt =
     | Caml_char rest ->
       buffer_add_char buf '%'; bprint_ignored_flag buf ign_flag;
       buffer_add_char buf 'C'; fmtiter rest false;
-    | Bool rest ->
+    | Bool (pad, rest) ->
       buffer_add_char buf '%'; bprint_ignored_flag buf ign_flag;
-      buffer_add_char buf 'B'; fmtiter rest false;
+      bprint_padding buf pad; buffer_add_char buf 'B';
+      fmtiter rest false;
     | Alpha rest ->
       buffer_add_char buf '%'; bprint_ignored_flag buf ign_flag;
       buffer_add_char buf 'a'; fmtiter rest false;
@@ -884,7 +885,7 @@ fun fmtty -> match fmtty with
 
   | Char rest                  -> Char_ty (fmtty_of_fmt rest)
   | Caml_char rest             -> Char_ty (fmtty_of_fmt rest)
-  | Bool rest                  -> Bool_ty (fmtty_of_fmt rest)
+  | Bool (pad, rest)           -> fmtty_of_padding_fmtty pad (Bool_ty (fmtty_of_fmt rest))
   | Alpha rest                 -> Alpha_ty (fmtty_of_fmt rest)
   | Theta rest                 -> Theta_ty (fmtty_of_fmt rest)
   | Custom (arity, _, rest)    -> fmtty_of_custom arity (fmtty_of_fmt rest)
@@ -932,7 +933,7 @@ fun ign fmt -> match ign with
   | Ignored_nativeint (_, _)        -> fmtty_of_fmt fmt
   | Ignored_int64 (_, _)            -> fmtty_of_fmt fmt
   | Ignored_float (_, _)            -> fmtty_of_fmt fmt
-  | Ignored_bool                    -> fmtty_of_fmt fmt
+  | Ignored_bool _                  -> fmtty_of_fmt fmt
   | Ignored_format_arg _            -> fmtty_of_fmt fmt
   | Ignored_format_subst (_, fmtty) -> concat_fmtty fmtty (fmtty_of_fmt fmt)
   | Ignored_reader                  -> Ignored_reader_ty (fmtty_of_fmt fmt)
@@ -1065,9 +1066,13 @@ and type_format_gen :
       Fmt_fmtty_EBB (Float (fconv, pad, prec, fmt'), fmtty')
     | Padprec_fmtty_EBB (_, _, _) -> raise Type_mismatch
   )
-  | Bool fmt_rest, Bool_ty fmtty_rest ->
-    let Fmt_fmtty_EBB (fmt', fmtty') = type_format_gen fmt_rest fmtty_rest in
-    Fmt_fmtty_EBB (Bool fmt', fmtty')
+  | Bool (pad, fmt_rest), _ -> (
+    match type_padding pad fmtty with
+    | Padding_fmtty_EBB (pad, Bool_ty fmtty_rest) ->
+      let Fmt_fmtty_EBB (fmt', fmtty') = type_format_gen fmt_rest fmtty_rest in
+      Fmt_fmtty_EBB (Bool (pad, fmt'), fmtty')
+    | Padding_fmtty_EBB (_, _) -> raise Type_mismatch
+  )
   | Flush fmt_rest, fmtty_rest ->
     let Fmt_fmtty_EBB (fmt', fmtty') = type_format_gen fmt_rest fmtty_rest in
     Fmt_fmtty_EBB (Flush fmt', fmtty')
@@ -1155,7 +1160,7 @@ fun ign fmt fmtty -> match ign with
   | Ignored_nativeint _        as ign' -> type_ignored_param_one ign' fmt fmtty
   | Ignored_int64 _            as ign' -> type_ignored_param_one ign' fmt fmtty
   | Ignored_float _            as ign' -> type_ignored_param_one ign' fmt fmtty
-  | Ignored_bool               as ign' -> type_ignored_param_one ign' fmt fmtty
+  | Ignored_bool _             as ign' -> type_ignored_param_one ign' fmt fmtty
   | Ignored_scan_char_set _    as ign' -> type_ignored_param_one ign' fmt fmtty
   | Ignored_scan_get_counter _ as ign' -> type_ignored_param_one ign' fmt fmtty
   | Ignored_scan_next_char     as ign' -> type_ignored_param_one ign' fmt fmtty
@@ -1500,8 +1505,8 @@ fun k o acc fmt -> match fmt with
     make_int_padding_precision k o acc rest pad prec convert_int64 iconv
   | Float (fconv, pad, prec, rest) ->
     make_float_padding_precision k o acc rest pad prec fconv
-  | Bool rest ->
-    fun b -> make_printf k o (Acc_data_string (acc, string_of_bool b)) rest
+  | Bool (pad, rest) ->
+    make_string_padding k o acc rest pad string_of_bool
   | Alpha rest ->
     fun f x -> make_printf k o (Acc_delay (acc, fun o -> f o x)) rest
   | Theta rest ->
@@ -1582,7 +1587,7 @@ fun k o acc ign fmt -> match ign with
   | Ignored_nativeint (_, _)        -> make_invalid_arg k o acc fmt
   | Ignored_int64 (_, _)            -> make_invalid_arg k o acc fmt
   | Ignored_float (_, _)            -> make_invalid_arg k o acc fmt
-  | Ignored_bool                    -> make_invalid_arg k o acc fmt
+  | Ignored_bool _                  -> make_invalid_arg k o acc fmt
   | Ignored_format_arg _            -> make_invalid_arg k o acc fmt
   | Ignored_format_subst (_, fmtty) -> make_from_fmtty k o acc fmtty fmt
   | Ignored_reader                  -> assert false
@@ -1774,8 +1779,12 @@ let rec make_iprintf : type a b c d e f.
         fn_of_padding_precision k o rest pad prec
     | Float (_, pad, prec, rest) ->
         fn_of_padding_precision k o rest pad prec
-    | Bool rest ->
+    | Bool (No_padding, rest) ->
         const (make_iprintf k o rest)
+    | Bool (Lit_padding _, rest) ->
+        const (make_iprintf k o rest)
+    | Bool (Arg_padding _, rest) ->
+        const (const (make_iprintf k o rest))
     | Alpha rest ->
         const (const (make_iprintf k o rest))
     | Theta rest ->
@@ -2448,9 +2457,15 @@ let fmt_ebb_of_string ?legacy_behavior str =
           make_padprec_fmt_ebb (get_pad ()) (get_prec ()) fmt_rest in
         Fmt_EBB (Float (fconv, pad', prec', fmt_rest'))
     | 'b' | 'B' ->
+      let pad = check_no_0 symb (get_padprec ()) in
       let Fmt_EBB fmt_rest = parse str_ind end_ind in
-      if get_ign () then Fmt_EBB (Ignored_param (Ignored_bool, fmt_rest))
-      else Fmt_EBB (Bool fmt_rest)
+      if get_ign () then
+        let ignored = Ignored_bool (get_padprec_opt '_') in
+        Fmt_EBB (Ignored_param (ignored, fmt_rest))
+      else
+        let Padding_fmt_EBB (pad', fmt_rest') =
+          make_padding_fmt_ebb pad fmt_rest in
+        Fmt_EBB (Bool (pad', fmt_rest'))
     | 'a' ->
       let Fmt_EBB fmt_rest = parse str_ind end_ind in
       Fmt_EBB (Alpha fmt_rest)
