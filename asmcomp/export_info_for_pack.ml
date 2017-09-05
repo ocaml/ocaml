b1137283b242a06eba750910091f42c559c3ed70
Fu Yong Quah <quah.fy95@gmail.com>
2017-09-05 12:20:14 +0100

    Flambda: Keep one delcarations table in export info pack.
    Remove function declarations table for [Flambda.function_declarations],
    retaining only the one for [A.function_declarations] as that the one which
    is exposed in cmx files.

--- asmcomp/export_info_for_pack.ml
+++ asmcomp/export_info_for_pack.ml
@@ -20,10 +20,7 @@ module A = Simple_value_approx
 
 let rename_id_state = Export_id.Tbl.create 100
 let rename_set_of_closures_id_state = Set_of_closures_id.Tbl.create 10
-let imported_flambda_function_declarations_table =
-  (Set_of_closures_id.Tbl.create 10
-   : Flambda.function_declarations Set_of_closures_id.Tbl.t)
-let imported_a_function_declarations_table =
+let imported_function_declarations_table =
   (Set_of_closures_id.Tbl.create 10
    : A.function_declarations Set_of_closures_id.Tbl.t)
 
@@ -131,7 +128,7 @@ let rec import_code_for_pack units pack expr =
             ~specialised_args:set_of_closures.specialised_args
             ~direct_call_surrogates:set_of_closures.direct_call_surrogates
             ~function_decls:
-              (import_function_declarations_for_pack units pack
+              (import_function_declarations_for_pack_aux units pack
                  set_of_closures.function_decls)
         in
         Set_of_closures set_of_closures
@@ -156,22 +153,6 @@ and import_function_declarations_for_pack_aux units pack
     (import_set_of_closures_id_for_pack units pack)
     (import_set_of_closures_origin_for_pack units pack)
 
-and import_function_declarations_for_pack units pack
-    (function_decls:Flambda.function_declarations) =
-  let original_set_of_closures_id = function_decls.set_of_closures_id in
-  try
-    Set_of_closures_id.Tbl.find imported_flambda_function_declarations_table
-      original_set_of_closures_id
-  with Not_found ->
-    let function_decls =
-      import_function_declarations_for_pack_aux units pack function_decls
-    in
-    Set_of_closures_id.Tbl.add
-      imported_flambda_function_declarations_table
-      original_set_of_closures_id
-      function_decls;
-    function_decls
-
 let import_function_declarations_for_pack_aux units pack
       (function_decls : A.function_declarations) : A.function_declarations =
   let funs =
@@ -190,14 +171,14 @@ let import_function_declarations_for_pack units pack
       (function_decls: A.function_declarations) =
   let original_set_of_closures_id = function_decls.set_of_closures_id in
   try
-    Set_of_closures_id.Tbl.find imported_a_function_declarations_table
+    Set_of_closures_id.Tbl.find imported_function_declarations_table
       original_set_of_closures_id
   with Not_found ->
     let function_decls =
       import_function_declarations_for_pack_aux units pack function_decls
     in
     Set_of_closures_id.Tbl.add
-      imported_a_function_declarations_table
+      imported_function_declarations_table
       original_set_of_closures_id
       function_decls;
     function_decls
@@ -244,7 +225,6 @@ let import_for_pack ~pack_units ~pack (exp : Export_info.t) =
          exp.invariant_params)
 
 let clear_import_state () =
-  Set_of_closures_id.Tbl.clear imported_flambda_function_declarations_table;
-  Set_of_closures_id.Tbl.clear imported_a_function_declarations_table;
+  Set_of_closures_id.Tbl.clear imported_function_declarations_table;
   Set_of_closures_id.Tbl.clear rename_set_of_closures_id_state;
   Export_id.Tbl.clear rename_id_state
