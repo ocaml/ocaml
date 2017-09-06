f6981d952e6cae54d4aa6df82ad93ff27ad01943
David Allsopp <david.allsopp@metastack.com>
2017-09-06 19:49:42 +0100

    Correct implementation of Unix.isatty on Windows
    The null device is a character device, but is not a console -
    Unix.isatty would incorrect return true for standard handles redirected
    to null.
    
    The new implementation returns true if and only if the fd points to a
    console handle.

---
+++ otherlibs/win32unix/isatty.c
@@ -0,0 +1,22 @@
+/**************************************************************************/
+/*                                                                        */
+/*                                 OCaml                                  */
+/*                                                                        */
+/*                 David Allsopp, OCaml Labs, Cambridge.                  */
+/*                                                                        */
+/*   Copyright 2017 MetaStack Solutions Ltd.                              */
+/*                                                                        */
+/*   All rights reserved.  This file is distributed under the terms of    */
+/*   the GNU Lesser General Public License version 2.1, with the          */
+/*   special exception on linking described in the file LICENSE.          */
+/*                                                                        */
+/**************************************************************************/
+
+#include <caml/mlvalues.h>
+#include "unixsupport.h"
+
+CAMLprim value unix_isatty(value fd)
+{
+  DWORD lpMode;
+  return (Val_bool(GetConsoleMode(Handle_val(fd), &lpMode)));
+}
