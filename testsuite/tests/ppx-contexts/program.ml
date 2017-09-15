559838d8f12aebda59c4d38cf0a4b02f942d5ca9
Mark Shinwell <mshinwell@gmail.com>
2017-09-15 18:10:09 +0100

    Remove trailing whitespace

--- testsuite/tests/ppx-contexts/program.ml
+++ testsuite/tests/ppx-contexts/program.ml
@@ -2,8 +2,8 @@
 
 open Ast_mapper
 
-let () = 
-  register "test" (fun _ -> 
+let () =
+  register "test" (fun _ ->
       Printf.eprintf "use_threads=%b\n" !Clflags.use_threads;
       Printf.eprintf "use_vmthreads=%b\n" !Clflags.use_vmthreads;
       default_mapper);
