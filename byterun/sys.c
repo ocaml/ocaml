c17f4dcabefb790efa1a22297ae81d79d8f46150
David Allsopp <david.allsopp@metastack.com>
2017-10-06 11:39:00 +0100

    Determine Windows version on startup
    Determine the running version of Windows and load the 4 components into
    caml_win32_major, caml_win32_minor, caml_win32_build and
    caml_win32_revision.
    
    Starting from Windows 8.1, Microsoft made the entirely flawed decision
    to start lying to processes about the version of Windows on which
    they're running which means that the classic GetVersion and GetVersionEx
    calls can no longer be used.
    
    The code here determines the Windows version by reading the version
    information block from kernel32.dll.

--- byterun/sys.c
+++ byterun/sys.c
@@ -368,6 +368,11 @@ CAMLprim value caml_sys_get_argv(value unit)
 
 void caml_sys_init(char_os * exe_name, char_os **argv)
 {
+#ifdef _WIN32
+  /* Initialises the caml_win32_* globals on Windows with the version of
+     Windows which is running */
+  caml_probe_win32_version();
+#endif
 #ifdef CAML_WITH_CPLUGINS
   caml_cplugins_init(exe_name, argv);
 #endif
