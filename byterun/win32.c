5783424131367ad8dfcca2549349a56f24a1aec6
Nicolas Ojeda Bar <n.oje.bar@gmail.com>
2017-10-10 11:27:39 +0200

    {Sys,Unix}.getenv: use GetEnvironmentVariable on Windows

--- byterun/win32.c
+++ byterun/win32.c
@@ -736,7 +736,43 @@ int caml_snprintf(char * buf, size_t size, const char * format, ...)
 wchar_t *caml_secure_getenv (wchar_t const *var)
 {
   /* Win32 doesn't have a notion of setuid bit, so getenv is safe. */
-  return CAML_SYS_GETENV (var);
+  return _wgetenv(var);
+}
+
+CAMLexport wchar_t *caml_win32_getenv(wchar_t const *lpName)
+{
+  wchar_t * lpBuffer;
+  DWORD nSize = 256, res;
+
+  lpBuffer = caml_stat_alloc_noexc(nSize * sizeof(wchar_t));
+
+  if (lpBuffer == NULL)
+    return NULL;
+
+  res = GetEnvironmentVariable(lpName, lpBuffer, nSize);
+
+  if (res == 0) {
+    caml_stat_free(lpBuffer);
+    return NULL;
+  }
+
+  if (res < nSize)
+    return lpBuffer;
+
+  nSize = res;
+  lpBuffer = caml_stat_resize_noexc(lpBuffer, nSize * sizeof(wchar_t));
+
+  if (lpBuffer == NULL)
+    return NULL;
+
+  res = GetEnvironmentVariable(lpName, lpBuffer, nSize);
+
+  if (res == 0 || res >= nSize) {
+    caml_stat_free(lpBuffer);
+    return NULL;
+  }
+
+  return lpBuffer;
 }
 
 /* The rename() implementation in MSVC's CRT is based on MoveFile()
