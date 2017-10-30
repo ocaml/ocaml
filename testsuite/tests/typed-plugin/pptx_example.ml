68f09442e502062f2c0c42b5b3ca61c133ca93ff
kakadu <kakadu.hafanana@gmail.com>
2017-10-30 20:41:50 +0300

    WIP on test

--- testsuite/tests/typed-plugin/pptx_example.ml
+++ testsuite/tests/typed-plugin/pptx_example.ml
@@ -20,13 +20,25 @@ let tast_mapper =
       {vb with vb_pat = { vb.vb_pat with pat_desc = new_desc } })
  }
 
+let print_if ppf flag printer arg =
+  if !flag then Format.fprintf ppf "%a@." printer arg;
+  arg
+
 let () =
   Typemod.ImplementationHooks.add_hook "pptx_example"
     (fun
       (hook_info : Misc.hook_info)
-      ((ast, coercion) : Typedtree.structure * Typedtree.module_coercion) ->
+      ((ast, _, coercion) : Typedtree.structure * _ * Typedtree.module_coercion) ->
+        let () = print_endline "plugin is called" in
         let ast = tast_mapper.structure tast_mapper ast in
-        (ast, coercion)
+        let ast = 
+          Untypeast.untype_structure ast |>
+          print_if Format.std_formatter Clflags.dump_source Pprintast.structure
+        in
+        
+        let (ast, sign, _env) = Typemod.type_structure (Compmisc.initial_env ()) ast Location.none in
+        let sign = print_if Format.std_formatter (ref true) Printtyp.signature sign in
+        (ast, sign, coercion)
     );
 (*
   Typemod.InterfaceHooks.add_hook "pptx_example"
