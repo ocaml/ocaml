20e94eb96d7d65ef499e62093abd7d6ae9f2f196
Runhang Li <marklrh@gmail.com>
2017-11-23 14:34:42 -0800

    Improve tests.

--- testsuite/tests/typing-modules/open-struct.ml
+++ testsuite/tests/typing-modules/open-struct.ml
@@ -1,9 +1,15 @@
 type t = A
+[%%expect{|
+type t = A
+|}]
 
 module M = struct
     open struct type t' = t end
     type t = B of t * t' | C
 end
+[%%expect{|
+module M : sig type t = B of t * t | C end
+|}]
 
 (* test *)
 include struct
@@ -11,10 +17,8 @@ include struct
   let test = B (B (C, A), A)
 end
 [%%expect{|
-type t = A
-module M : sig type t = B of t * t | C end
 val test : M.t = M.B (M.B (M.C, A), A)
-|}];;
+|}]
 
 include struct
   open struct let aux x y = x / y end
@@ -45,12 +49,14 @@ module type S = sig
     type t = t' -> int end
   val x : t
 end
+[%%expect{|
+module type S = sig val x : char -> int end
+|}];;
 
 module M : S = struct
   let x = Char.code
 end
 [%%expect{|
-module type S = sig val x : char -> int end
 module M : S
 |}];;
 
@@ -63,7 +69,7 @@ end
 
 let () =
   inc(); inc(); dec ();
-  assert (current () = 2)
+  assert (current () = 1)
 [%%expect{|
 |}];;
 
@@ -132,3 +138,68 @@ module type T = sig type s = int end
 module F : functor (X : S) -> T
 module G : functor (X : T) -> S
 |}]
+
+module Counter : sig val inc : unit -> unit val current : unit -> int val z : int val zz : int end = struct
+  open struct let counter = ref 0 end
+  let x = 1
+  let y = 2
+  let dec () = decr counter
+
+  open struct
+    module A : sig val z : int end = struct
+      open struct
+        let n = 3
+        module A = struct
+          open struct
+            let x = 1
+          end
+          let y = x
+        end
+        let h = A.y
+        let g = A.y + n
+      end
+      let z = h + g
+    end
+
+    let z = 12
+
+    module B : sig val z : int end = struct
+      open struct
+        module A = struct
+          open struct let x = 1 end
+          let y = x
+          open struct let x = 1 end
+          let z = y + x
+        end
+        let h = A.y
+        let g = A.z + 1
+      end
+      let z = h + g
+    end
+
+    let h = A.z + B.z
+  end
+
+  let z = z + h
+  let g = 1
+  let ggg = 2
+  let inc () = incr counter
+  let zz = 5
+  let current () = !counter
+end
+[%%expect{|
+module Counter :
+  sig
+    val inc : unit -> unit
+    val current : unit -> int
+    val z : int
+    val zz : int
+  end
+|}]
+
+let () = begin
+  assert (Counter.z = 21)
+end
+[%%expect{|
+|}]
+
