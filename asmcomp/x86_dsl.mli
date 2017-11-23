8e24b4372670a35e1f336df02f1c000559b823da
Xavier Clerc <xclerc@janestreet.com>
2017-11-23 13:23:56 +0000

    Add support for CDQE instruction.

--- asmcomp/x86_dsl.mli
+++ asmcomp/x86_dsl.mli
@@ -106,6 +106,7 @@ module I : sig
   val bswap: arg -> unit
   val call: arg -> unit
   val cdq: unit -> unit
+  val cdqe: unit -> unit
   val cmp: arg -> arg -> unit
   val comisd: arg -> arg -> unit
   val cqo: unit -> unit
