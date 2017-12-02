303f89b3a527c75f16efcd8c50d082c4baed5ab9
Nicolas Ojeda Bar <n.oje.bar@gmail.com>
2017-12-03 00:19:58 +0100

    Migrate letrec tests to ocamltest

--- testsuite/tests/letrec/allowed.ml
+++ testsuite/tests/letrec/allowed.ml
@@ -1,3 +1,6 @@
+(* TEST
+*)
+
 let rec x = (x; ());;
 
 let rec x = let x = () in x;;
@@ -45,7 +48,7 @@ and y = fun () -> ignore x
 
 let rec x = { contents = y }
 and y = fun () -> ignore x;;
-               
+
 let r = ref (fun () -> ())
 let rec x = fun () -> r := x;;
 
@@ -73,6 +76,6 @@ let rec deep_cycle : [`Tuple of [`Shared of 'a] array] as 'a
   = `Tuple [| `Shared deep_cycle |];;
 
 (* Constructing float arrays was disallowed altogether at one point
-   by an overzealous check.  Constructing float arrays in recursive 
+   by an overzealous check.  Constructing float arrays in recursive
    bindings is fine when they don't partake in the recursion. *)
 let rec _x = let _ = [| 1.0 |] in 1. in ();;
--- testsuite/tests/letrec/ref.ml
+++ testsuite/tests/letrec/ref.ml
@@ -1,3 +1,5 @@
+(* TEST *)
+
 (* Test construction of cyclic values where the cycles pass through references *)
 
 type t = { mutable next : t; mutable inst : n ref }
