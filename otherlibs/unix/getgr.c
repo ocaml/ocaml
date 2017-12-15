5ec02189fdd3c4278534519cd667f46cd08c892c
aalekseyev <aalekseyev@janestreet.com>
2017-12-15 10:46:39 +0000

    getpw* and getgr* functions raise Unix_error when interrupted  (#1451)
    Currently getpw* and getgr* functions raise Not_found when there's a Unix error.
    This is appropriate behavior for some Unix errors e.g. ENOENT, but other Unix errors
    should be reported as a Unix_error exception.  
    
    Following discussion in GPR#1451 this commit raises Unix_error on EINTR and keeps
    raising Not_found on other errors.

--- otherlibs/unix/getgr.c
+++ otherlibs/unix/getgr.c
@@ -18,6 +18,7 @@
 #include <caml/alloc.h>
 #include <caml/memory.h>
 #include "unixsupport.h"
+#include <errno.h>
 #include <stdio.h>
 #include <grp.h>
 
@@ -44,15 +45,29 @@ CAMLprim value unix_getgrnam(value name)
 {
   struct group * entry;
   if (! caml_string_is_c_safe(name)) caml_raise_not_found();
+  errno = 0;
   entry = getgrnam(String_val(name));
-  if (entry == NULL) caml_raise_not_found();
+  if (entry == NULL) {
+    if (errno == EINTR) {
+      uerror("getgrnam", Nothing);
+    } else {
+      caml_raise_not_found();
+    }
+  }
   return alloc_group_entry(entry);
 }
 
 CAMLprim value unix_getgrgid(value gid)
 {
   struct group * entry;
+  errno = 0;
   entry = getgrgid(Int_val(gid));
-  if (entry == NULL) caml_raise_not_found();
+  if (entry == NULL) {
+    if (errno == EINTR) {
+      uerror("getgrgid", Nothing);
+    } else {
+      caml_raise_not_found();
+    }
+  }
   return alloc_group_entry(entry);
 }
