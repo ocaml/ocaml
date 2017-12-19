2cc67bb4e0995f3ce2f322f581a14593289b7983
Olivier Andrieu <oandrieu@gmail.com>
2017-12-19 15:16:20 +0100

    MPR#7390: caml_float_of_hex function, exponent overflow behavior (#1528)
    * MPR#7390: caml_float_of_hex function behaves incorrectly when the exponent overflows or underflows.  
    
    - make sure the adjusted exponent does not overflow
    - return 0/inf instead of raising an exception for exponent values
      ≤INT_MIN or ≥INT_MAX.
    
    * Add some comment to point out that the inclusive test for INT_MIN/INT_MAX is
    intentional because of Win64.

--- testsuite/tests/basic-float/tfloat_hex.ml
+++ testsuite/tests/basic-float/tfloat_hex.ml
@@ -15,3 +15,8 @@ let () =
   try_float_of_string "0x.";
   try_float_of_string "0xp0";
   try_float_of_string "0x.p0";
+
+  (* MPR#7690 *)
+  try_float_of_string "0x1.0p-2147483648";
+  try_float_of_string "0x123456789ABCDEF0p2147483647";
+  try_float_of_string "0x1p2147483648";
