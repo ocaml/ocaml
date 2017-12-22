c7b788ddd1bda528c59450565729bdcd95709aa8
Xavier Leroy <xavier.leroy@inria.fr>
2017-12-22 16:02:27 +0100

    Avoid deadlock in signal.ml test
    The original signal.ml test runs into the deadlock described in MPR#7503:
    the Caml signal handler is called while a thread holds the I/O lock
    on the stdout channel, then blocks trying to acquire this lock before
    printing a message.
    
    This commit reorganizes the test so that the signal handler performs no I/O
    and just changes a global reference.  This is a temporary fix until
    signal handling is revised along the lines of GPR#1128.

--- testsuite/tests/lib-threads/signal.ml
+++ testsuite/tests/lib-threads/signal.ml
@@ -1,13 +1,22 @@
+let signaled = ref false
+
 let sighandler _ =
-  print_string "Got ctrl-C, exiting..."; print_newline();
-  exit 0
+  signaled := true
 
 let print_message delay c =
-  for i = 1 to 20 do
+  while not !signaled do
     print_char c; flush stdout; Thread.delay delay
   done
 
 let _ =
   ignore (Sys.signal Sys.sigint (Sys.Signal_handle sighandler));
-  ignore (Thread.create (print_message 0.6666666666) 'a');
-  print_message 1.0 'b'
+  let th1 = Thread.create (print_message 0.6666666666) 'a' in
+  print_message 1.0 'b';
+  Thread.join th1;
+  if !signaled then begin
+    print_string "Got ctrl-C, exiting"; print_newline();
+    exit 0
+  end else begin
+    print_string "not signaled???"; print_newline();
+    exit 2
+  end
