09e9037cb12e86a989344513b3f59fc1e0a2e76b
Jeremy Yallop <yallop@gmail.com>
2018-02-01 00:25:06 +0000

    Tests for PR7717 (Don't treat unboxed constructors as statically sized)

--- testsuite/tests/letrec-disallowed/unboxed.ml
+++ testsuite/tests/letrec-disallowed/unboxed.ml
@@ -8,3 +8,22 @@ let rec x = {x = y} and y = 3L;;
 type r = A of r [@@unboxed];;
 let rec y = A y;;
               
+type a = {a: b }[@@unboxed]
+and b = X of a | Y
+
+let rec a =
+  {a=
+    (if Sys.opaque_identity true then
+       X a
+     else
+       Y)};;
+
+type d = D of e [@@unboxed]
+and e = V of d | W;;
+
+let rec d =
+  D
+    (if Sys.opaque_identity true then
+       V d
+     else
+       W);;
