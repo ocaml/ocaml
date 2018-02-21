96c5e1919e508398516cf57aa7cf861be6cfef02
Sébastien Hinderer <Sebastien.Hinderer@inria.fr>
2018-02-21 08:19:57 +0100

    Migrate the backtrace tests to ocamltest

--- testsuite/tests/backtrace/backtrace3.ml
+++ testsuite/tests/backtrace/backtrace3.ml
@@ -1,3 +1,12 @@
+(* TEST
+   flags = "-g"
+   ocamlrunparam += ",b=1"
+   * bytecode
+     reference = "${test_source_directory}/backtrace3.byte.reference"
+   * native
+     reference = "${test_source_directory}/backtrace3.opt.reference"
+     compare_programs = "false"
+*)
 
 (* A test for stack backtraces *)
 
