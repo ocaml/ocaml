02fc27a2ae6337a870ade601a053f41d46cfafe7
Jacques Garrigue <garrigue@math.nagoya-u.ac.jp>
2018-03-13 15:50:01 +0900

    run the test

--- testsuite/tests/tool-toplevel/pr7751.ml
+++ testsuite/tests/tool-toplevel/pr7751.ml
@@ -2,9 +2,10 @@
    * toplevel
 *)
 
-#directory"../../../../../../utils";;
-#directory"../../../../../../parsing";;
-#directory"../../../../../../compilerlibs";;
+(* Horrible hack; there should be a way to do that... *)
+#directory"../../../../../../../../utils";;
+#directory"../../../../../../../../parsing";;
+#directory"../../../../../../../../compilerlibs";;
 #load"ocamlcommon.cma";;
 
 Parse.expression (Lexing.from_string "1");;
