76bb01ae441c69a14e68277d8036c322c8377b63
Sébastien Hinderer <Sebastien.Hinderer@inria.fr>
2018-03-18 19:13:11 +0100

    Migrate the typing-private-bugs tests to ocamltest

--- testsuite/tests/typing-private-bugs/pr5469_ok.ml
+++ testsuite/tests/typing-private-bugs/pr5469_ok.ml
@@ -1,3 +1,10 @@
+(* TEST
+flags = " -w a "
+* setup-ocamlc.byte-build-env
+** ocamlc.byte
+*** check-ocamlc.byte-output
+*)
+
 module M (T:sig type t end)
  = struct type t = private { t : T.t } end
 module P
