36e611e7a81185356d4a50846882e30bb84594b0
Sébastien Hinderer <Sebastien.Hinderer@inria.fr>
2018-03-18 19:22:45 +0100

    Migrate the typing-recmod tests to ocamltest

--- testsuite/tests/typing-recmod/t08bad.ml
+++ testsuite/tests/typing-recmod/t08bad.ml
@@ -1,3 +1,11 @@
+(* TEST
+flags = " -w a "
+ocamlc_byte_exit_status = "2"
+* setup-ocamlc.byte-build-env
+** ocamlc.byte
+*** check-ocamlc.byte-output
+*)
+
 (* Bad (not regular) *)
 module rec A : sig type 'a t = <m: 'a list B.t; n: 'a array B.t> end
              = struct type 'a t = <m: 'a list B.t; n: 'a array B.t> end
--- testsuite/tests/typing-recmod/t13ok.ml
+++ testsuite/tests/typing-recmod/t13ok.ml
@@ -1,3 +1,10 @@
+(* TEST
+flags = " -w a "
+* setup-ocamlc.byte-build-env
+** ocamlc.byte
+*** check-ocamlc.byte-output
+*)
+
 (* OK *)
 class type [ 'node ] extension = object method node : 'node end
 class type [ 'ext ] node = object constraint 'ext = 'ext node #extension end
