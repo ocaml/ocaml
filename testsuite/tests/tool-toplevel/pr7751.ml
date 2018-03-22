4d05b656e7b0c25c878d3178e84582558dc1432f
Jacques Garrigue <garrigue@math.nagoya-u.ac.jp>
2018-03-22 20:21:00 +0900

    correct use of ocamltest

--- testsuite/tests/tool-toplevel/pr7751.ml
+++ testsuite/tests/tool-toplevel/pr7751.ml
@@ -1,11 +1,6 @@
 (* TEST
+   include ocamlcommon
    * toplevel
 *)
-
-(* Horrible hack; there should be a way to do that... *)
-#directory"../../../../../../../../utils";;
-#directory"../../../../../../../../parsing";;
-#directory"../../../../../../../../compilerlibs";;
-#load"ocamlcommon.cma";;
 
 Parse.expression (Lexing.from_string "1");;
