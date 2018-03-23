29226d27fb64996265b6f9c7ab0f01c3656a0d6c
Jacques Garrigue <garrigue@math.nagoya-u.ac.jp>
2018-03-23 09:04:17 +0900

    add broken test for the record

--- testsuite/tests/typing-modules/pr7726_bad.ml
+++ testsuite/tests/typing-modules/pr7726_bad.ml
@@ -21,3 +21,14 @@ Error: The type abbreviation Fixed.t is cyclic
 module T = Fix(functor (X:sig type t end) -> struct type t = X.t option end)
 
 module T = Fix(functor (X:sig type t end) -> struct type t = X.t end) *)
+
+module M = struct
+  module F (X : T) : T = X
+  module rec Fixed : sig type t = F(Fixed).t end = Fixed
+end;;
+
+module type S = module type of M;;
+
+module Id (X : T) = X;;
+
+module type Bad = S with module F = Id;;
