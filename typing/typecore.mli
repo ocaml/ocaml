41d5658983d87dba453bba4cd623185866e02c30
Armaël Guéneau <armael.gueneau@ens-lyon.fr>
2018-04-11 19:45:21 +0200

    Re-implement the hint indicating missing "rec" keywords
    This reverts commit 90fbe53ee5399301f3bd0bef9d35dda7a7af4294 except for the
    Changes entry.

--- typing/typecore.mli
+++ typing/typecore.mli
@@ -169,6 +169,7 @@ type error =
   | Illegal_letrec_pat
   | Illegal_letrec_expr
   | Illegal_class_expr
+  | Unbound_value_missing_rec of Longident.t * Location.t
   | Empty_pattern
 
 exception Error of Location.t * Env.t * error
