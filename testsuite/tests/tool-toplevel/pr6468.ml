fdaf9297d363696c313269fedd4b4c0fe58a4c1a
Thomas Refis <thomas.refis@gmail.com>
2018-05-04 09:56:38 -0400

    fix test checking backtrace capture in the toplevel

--- testsuite/tests/tool-toplevel/pr6468.ml
+++ testsuite/tests/tool-toplevel/pr6468.ml
@@ -2,6 +2,9 @@
    * toplevel
 *)
 
+(* Make the test reproducible regardless of whether OCAMLRUNPARAM=b or not *)
+Printexc.record_backtrace true;;
+
 let f () = raise Not_found;;
 let g () = f (); 1;;
 g ();;
