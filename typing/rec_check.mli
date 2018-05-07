e63e8421a1bc1f7b2e9ae1eeacc6d163b153c2f8
Thomas Refis <thomas.refis@gmail.com>
2018-05-07 18:05:54 -0400

    move Rec_check out of typecore

---
+++ typing/rec_check.mli
@@ -0,0 +1,20 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*             Xavier Leroy, projet Cristal, INRIA Rocquencourt           *)
+(*                                                                        *)
+(*   Copyright 1996 Institut National de Recherche en Informatique et     *)
+(*     en Automatique.                                                    *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
+exception Illegal_expr
+
+val check_recursive_expression : Ident.t list -> Typedtree.expression -> unit
+
+val check_class_expr : Ident.t list -> Typedtree.class_expr -> unit
