34504e849d165694cc51bc2430ef0e28bb910f95
Thomas Refis <thomas.refis@gmail.com>
2018-05-25 18:15:05 +0100

    testsuite: fix tests/basic/patmatch_incoherence.ml
    Make this brittle test more robust by being less precise.

--- testsuite/tests/basic/patmatch_incoherence.ml
+++ testsuite/tests/basic/patmatch_incoherence.ml
@@ -1,11 +1,9 @@
 (* TEST
-   flags = "-drawlambda -dno-unique-ids"
    * expect
 *)
 
 type tlist = { x: 'a. 'a list };;
 [%%expect{|
-0a
 type tlist = { x : 'a. 'a list; }
 |}];;
 
@@ -15,22 +13,12 @@ match { x = [] } with
 | { x = "" :: _ } -> ()
 ;;
 [%%expect{|
-(let (*match* = [0: 0a] *match* =a (field 0 *match*))
-  (if *match*
-    (let (*match* =a (field 0 *match*))
-      (catch
-        (if (!= *match* 3) (exit 1) (let (*match* =a (field 1 *match*)) 0a))
-       with (1)
-        (stringswitch *match*
-         case "": (let (*match* =a (field 1 *match*)) 0a))))
-    0a))
 - : unit = ()
 |}];;
 
 
 type t = { x: 'a. 'a };;
 [%%expect{|
-0a
 type t = { x : 'a. 'a; }
 |}];;
 
@@ -39,13 +27,6 @@ match { x = assert false } with
 | { x = "" } -> ()
 ;;
 [%%expect{|
-(let
-  (*match* =
-     (makeblock 0
-       (raise (makeblock 0 (global Assert_failureg) [0: "" 1 12])))
-   *match* =a (field 0 *match*))
-  (catch (if (!= *match* 3) (exit 2) 0a) with (2)
-    (stringswitch *match* case "": 0a)))
 Exception: Assert_failure ("", 1, 12).
 |}];;
 
@@ -61,14 +42,6 @@ Line _, characters 0-70:
 Warning 8: this pattern-matching is not exhaustive.
 Here is an example of a case that is not matched:
 {x=Some _}
-(catch
-  (let
-    (*match* =
-       (makeblock 0
-         (raise (makeblock 0 (global Assert_failureg) [0: "" 1 12])))
-     *match* =a (field 0 *match*))
-    (catch (if (!= *match* 3) (exit 4) 0a) with (4) (if *match* (exit 3) 0a)))
- with (3) (raise (makeblock 0 (global Match_failureg) [0: "" 1 0])))
 Exception: Assert_failure ("", 1, 12).
 |}];;
 
@@ -84,14 +57,6 @@ Line _, characters 0-71:
 Warning 8: this pattern-matching is not exhaustive.
 Here is an example of a case that is not matched:
 {x="*"}
-(catch
-  (let
-    (*match* =
-       (makeblock 0
-         (raise (makeblock 0 (global Assert_failureg) [0: "" 1 12])))
-     *match* =a (field 0 *match*))
-    (if *match* (exit 5) 0a))
- with (5) (raise (makeblock 0 (global Match_failureg) [0: "" 1 0])))
 Exception: Assert_failure ("", 1, 12).
 |}];;
 
@@ -107,14 +72,6 @@ Line _, characters 0-71:
 Warning 8: this pattern-matching is not exhaustive.
 Here is an example of a case that is not matched:
 {x=`AnyOtherTag}
-(catch
-  (let
-    (*match* =
-       (makeblock 0
-         (raise (makeblock 0 (global Assert_failureg) [0: "" 1 12])))
-     *match* =a (field 0 *match*))
-    (if *match* (exit 7) 0a))
- with (7) (raise (makeblock 0 (global Match_failureg) [0: "" 1 0])))
 Exception: Assert_failure ("", 1, 12).
 |}];;
 
@@ -130,16 +87,6 @@ Line _, characters 0-70:
 Warning 8: this pattern-matching is not exhaustive.
 Here is an example of a case that is not matched:
 {x=0}
-(catch
-  (let
-    (*match* =
-       (makeblock 0
-         (raise (makeblock 0 (global Assert_failureg) [0: "" 1 12])))
-     *match* =a (field 0 *match*))
-    (catch
-      (let (len =a (array.length[gen] *match*)) (if (!= len 0) (exit 10) 0a))
-     with (10) (if (!= *match* 3) (exit 9) 0a)))
- with (9) (raise (makeblock 0 (global Match_failureg) [0: "" 1 0])))
 Exception: Assert_failure ("", 1, 12).
 |}];;
 
@@ -155,15 +102,6 @@ Line _, characters 0-68:
 Warning 8: this pattern-matching is not exhaustive.
 Here is an example of a case that is not matched:
 {x=0}
-(catch
-  (let
-    (*match* =
-       (makeblock 0
-         (raise (makeblock 0 (global Assert_failureg) [0: "" 1 12])))
-     *match* =a (field 0 *match*))
-    (catch (if (!= *match* 88) (exit 12) 0a) with (12)
-      (if (!= *match* 3) (exit 11) 0a)))
- with (11) (raise (makeblock 0 (global Match_failureg) [0: "" 1 0])))
 Exception: Assert_failure ("", 1, 12).
 |}];;
 
@@ -179,21 +117,6 @@ Line _, characters 0-74:
 Warning 8: this pattern-matching is not exhaustive.
 Here is an example of a case that is not matched:
 {x=0}
-(catch
-  (let
-    (*match* =
-       (makeblock 0
-         (raise (makeblock 0 (global Assert_failureg) [0: "" 1 12])))
-     *match* =a (field 0 *match*))
-    (catch
-      (if (isint *match*) (exit 14)
-        (let (variant =a (field 0 *match*))
-          (if (!= variant 88) (exit 14)
-            (let (*match* =a (field 1 *match*))
-              (stringswitch *match* case "lol": 0a
-                                    default: (exit 13))))))
-     with (14) (if (!= *match* 3) (exit 13) 0a)))
- with (13) (raise (makeblock 0 (global Match_failureg) [0: "" 1 0])))
 Exception: Assert_failure ("", 1, 12).
 |}];;
 
@@ -211,17 +134,5 @@ Line _, characters 0-95:
 Warning 8: this pattern-matching is not exhaustive.
 Here is an example of a case that is not matched:
 {x=0}
-(catch
-  (let
-    (*match* =
-       (makeblock 0
-         (raise (makeblock 0 (global Assert_failureg) [0: "" 1 12])))
-     *match* =a (field 0 *match*)
-     *match* =a (field 0 *match*))
-    (if (!=. *match* 2.) (exit 15)
-      (let (*match* =a (field 1 *match*))
-        (stringswitch *match* case "": 0a
-                              default: (exit 15)))))
- with (15) (raise (makeblock 0 (global Match_failureg) [0: "" 1 0])))
 Exception: Assert_failure ("", 1, 12).
 |}];;
