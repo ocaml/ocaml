f80ee0f8d66b607a24a8626bdc77ee4667554eec
KC Sivaramakrishnan <kc@kcsrk.info>
2018-06-01 13:31:20 +0100

    More fixes

--- byterun/caml/shared_heap.h
+++ byterun/caml/shared_heap.h
@@ -48,11 +48,13 @@ static inline int is_garbage(value v) {
   return Has_status_hd(Hd_val(v), global.GARBAGE);
 }
 
-/* Only reliable after all marking including ephemerons is done */
 static inline int is_unmarked(value v) {
   return Has_status_hd(Hd_val(v), global.UNMARKED);
 }
 
+static inline int is_marked(value v) {
+  return Has_status_hd(Hd_val(v), global.MARKED);
+}
 
 void caml_redarken_pool(struct pool*, scanning_action, void*);
 
