73e761e8575cf3b2f882b1f258f77deff8767719
David Allsopp <david.allsopp@metastack.com>
2018-06-07 10:04:48 +0100

    Add missing copyright headers
    Build_path_prefix_map module missing headers from GPR#1515.

--- utils/build_path_prefix_map.ml
+++ utils/build_path_prefix_map.ml
@@ -1,3 +1,18 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*            Gabriel Scherer, projet Parsifal, INRIA Saclay              *)
+(*                                                                        *)
+(*   Copyright 2017 Institut National de Recherche en Informatique et     *)
+(*     en Automatique.                                                    *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
 type path = string
 type path_prefix = string
 type error_message = string
