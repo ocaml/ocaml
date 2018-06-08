f4e76a6934c5a476bd586034c0067f6fefac8fc8
Thomas Refis <thomas.refis@gmail.com>
2018-06-08 16:03:26 +0100

    nondep_type_decl: add a test showing private abbrevs are ignored

---
+++ testsuite/tests/typing-modules/nondep_private_abbrev.ml
@@ -0,0 +1,29 @@
+(* TEST
+   * expect
+*)
+
+module F(_ : sig end) : sig
+  type t = private int
+end = struct
+  type t = int
+end;;
+[%%expect{|
+module F : sig  end -> sig type t = private int end
+|}]
+
+module Direct = F(struct end);;
+[%%expect{|
+module Direct : sig type t = private int end
+|}]
+
+module G(X : sig end) : sig
+  type t = F(X).t
+end = F(X);;
+[%%expect{|
+module G : functor (X : sig  end) -> sig type t = F(X).t end
+|}]
+
+module Indirect = G(struct end);;
+[%%expect{|
+module Indirect : sig type t end
+|}]
