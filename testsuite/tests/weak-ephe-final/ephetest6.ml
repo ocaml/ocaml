d2c99fde160c7c52721439ae851a371a50417d3f
KC Sivaramakrishnan <kc@kcsrk.info>
2018-06-09 15:38:16 +0100

    Make ephetest6 more stable

--- testsuite/tests/weak-ephe-final/ephetest6.ml
+++ testsuite/tests/weak-ephe-final/ephetest6.ml
@@ -3,13 +3,13 @@ module E = Ephemeron.Kn
 let test1 =
   let e1 = E.create 16 in
   for i = 0 to 15 do
-    E.set_key e1 i (ref i)
+    E.set_key e1 i i
   done;
   let d = Domain.spawn (fun () ->
     let e2 = E.create 16 in
     E.blit_key e1 0 e2 0 16;
     match E.get_key e2 1 with
-    | Some r -> assert (!r = 1)
+    | Some i -> assert (i = 1)
     | None -> assert false)
   in
   Domain.join d;
@@ -22,11 +22,11 @@ let test2 =
   let d = Domain.spawn (fun () ->
     let e2 = E.create 16 in
     for i = 0 to 15 do
-      E.set_key e2 i (ref i)
+      E.set_key e2 i i
     done;
     E.blit_key e2 0 e1 0 16;
     match E.get_key e1 1 with
-    | Some r -> assert (!r = 1)
+    | Some i -> assert (i = 1)
     | None -> assert false)
   in
   Domain.join d;
@@ -45,3 +45,18 @@ let test3 =
   in
   Domain.join d1;
   print_endline "test3: ok"
+
+let test4 =
+  let e1 = E.create 16 in
+  for i = 0 to 15 do
+    E.set_key e1 i i
+  done;
+  let e2 = E.create 16 in
+  let d = Domain.spawn (fun () ->
+    E.blit_key e1 0 e2 0 16;
+    match E.get_key e2 1 with
+    | Some i -> assert (i = 1)
+    | None -> assert false)
+  in
+  Domain.join d;
+  print_endline "test4: ok"
