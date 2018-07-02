a000769c8d813f7228211bd2cfb10467b564f86b
Pierre Weis <Pierre.Weis@inria.fr>
2018-07-02 10:30:57 +0200

    Fix MPR#7235 (#1870)

--- stdlib/format.ml
+++ stdlib/format.ml
@@ -1367,9 +1367,13 @@ let kasprintf k (Format (fmt, _)) =
 
 let asprintf fmt = kasprintf (fun s -> s) fmt
 
-(* Output everything left in the pretty printer queue at end of execution. *)
-let () = at_exit print_flush
+(* Flushing standard formatters at end of execution. *)
 
+let flush_standard_formatters () =
+  pp_print_flush std_formatter ();
+  pp_print_flush err_formatter ()
+
+let () = at_exit flush_standard_formatters
 
 (*
 
