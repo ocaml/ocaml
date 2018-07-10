b6fb92ea9cc8b038d6a6630076632d082aa957f2
Stephen Dolan <stephen.dolan@cl.cam.ac.uk>
2018-07-10 16:22:01 +0100

    Darken during promotion.
    Partial revert of 08dcece5b71ff28756d0ee7ac0447e36ad284d62

--- byterun/minor_gc.c
+++ byterun/minor_gc.c
@@ -410,7 +410,7 @@ CAMLexport value caml_promote(struct domain* domain, value root)
   oldify_mopup (&st);
 
   CAMLassert (!Is_minor(root));
-  CAMLassert (Has_status_hd(Hd_val(root), global.MARKED));
+  caml_darken(0, root, 0);
 
   if (tag == Stack_tag) {
     /* Since we've promoted the objects on the stack, the stack is now clean. */
