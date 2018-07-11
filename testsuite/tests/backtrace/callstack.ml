70e7ee7f25d70c086cca73ffe1317bf71107d9d2
Valentin Gatien-Baron <vgatien-baron@janestreet.com>
2018-07-11 10:12:06 -0400

    in native code, make Printexc.get_callstack work in new threads

---
+++ testsuite/tests/backtrace/callstack.ml
@@ -0,0 +1,18 @@
+(* TEST
+   flags = "-g"
+   include systhreads
+   compare_programs = "false"
+   * no-flambda
+   reference = "${test_source_directory}/callstack.reference"
+   ** native
+   ** bytecode
+*)
+let[@inline never] f0 () =
+  Printexc.print_raw_backtrace stdout (Printexc.get_callstack 100); ()
+let[@inline never] f1 () = f0 (); ()
+let[@inline never] f2 () = f1 (); ()
+let[@inline never] f3 () = f2 (); ()
+let () = Printf.printf "main thread:\n"
+let () = f3 ()
+let () = Printf.printf "new thread:\n"
+let () = Thread.join (Thread.create f3 ())
