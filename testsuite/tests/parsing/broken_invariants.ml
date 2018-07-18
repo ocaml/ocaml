5cfcbd38561bd3370eebd249eac300721ecb5065
Florian Angeletti <octa@polychoron.fr>
2018-07-18 19:30:23 +0200

    test broken invariants in parsetree due to ppxs

---
+++ testsuite/tests/parsing/broken_invariants.ml
@@ -0,0 +1,52 @@
+(* TEST
+  ppxs="illegal_ppx.ml"
+  * expect
+*)
+
+let empty_tuple = [%tuple];;
+
+[%%expect{|
+Line _:
+Error: File "", line 6, characters 20-25:
+Error: broken invariant in parsetree: Tuples must have at least 2 components.
+|}]
+
+
+let empty_record = [%record];;
+
+[%%expect{|
+Line _:
+Error: File "", line 15, characters 21-27:
+Error: broken invariant in parsetree: Records cannot be empty.
+|}]
+
+let empty_apply = [%no_args f];;
+
+[%%expect{|
+Line _:
+Error: File "", line 23, characters 20-27:
+Error: broken invariant in parsetree: Function application with no argument.
+|}]
+
+let f = function [%record_with_functor_fields] -> ();;
+[%%expect_test {||}];;
+
+[%%expect{|
+Line _:
+Error: File "", line 31, characters 19-45:
+Error: broken invariant in parsetree: Functor application not allowed here.
+|}]
+
+[%%empty_let];;
+[%%expect {|
+Line _:
+Error: File "", line 40, characters 3-12:
+Error: broken invariant in parsetree: Let with no bindings.
+|}]
+
+[%%empty_type];;
+[%%expect {|
+Line _:
+Error: File "", line 47, characters 3-13:
+Error: broken invariant in parsetree: Type declarations cannot be empty.
+|}]
---
+++ testsuite/tests/parsing/illegal_ppx.ml
@@ -0,0 +1,38 @@
+module H = Ast_helper
+module M = Ast_mapper
+open Parsetree
+let empty_tuple loc = H.Exp.tuple ~loc []
+let empty_record loc = H.Exp.record ~loc [] None
+let empty_apply loc f =
+  H.Exp.apply ~loc f []
+
+let empty_let loc = H.Str.value ~loc Asttypes.Nonrecursive []
+let empty_type loc = H.Str.type_ ~loc Asttypes.Nonrecursive []
+let functor_id loc = Location.mkloc
+    (Longident.( Lapply (Lident "F", Lident "X"))) loc
+let complex_record loc =
+  H.Pat.record ~loc [functor_id loc, H.Pat.any ~loc () ] Asttypes.Closed
+
+let super = M.default_mapper
+let expr mapper e =
+  match e.pexp_desc with
+  | Pexp_extension ({txt="tuple";loc},_) -> empty_tuple loc
+  | Pexp_extension({txt="record";loc},_) -> empty_record loc
+  | Pexp_extension({txt="no_args";loc},PStr[{pstr_desc= Pstr_eval (e,_);_}])
+    -> empty_apply loc e
+  | _ -> super.M.expr mapper e
+
+let pat mapper p =
+  match p.ppat_desc with
+  | Ppat_extension ({txt="record_with_functor_fields";loc},_) ->
+      complex_record loc
+  | _ -> super.M.pat mapper p
+
+let structure_item mapper stri = match stri.pstr_desc with
+  | Pstr_extension (({Location.txt="empty_let";loc},_),_) -> empty_let loc
+  | Pstr_extension (({Location.txt="empty_type";loc},_),_) -> empty_type loc
+  | _ -> super.structure_item mapper stri
+
+let () = M.register "illegal ppx" (fun _ ->
+    { super with expr; pat; structure_item }
+  )
