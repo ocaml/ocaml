9867b3831c8d70e0e537cb0a556232179b2c6984
Thomas Refis <refis.thomas@gmail.com>
2018-07-19 14:00:42 +0100

    Merge pull request #1914 from trefis/subtype_row
    MPR#7824

---
+++ testsuite/tests/typing-polyvariants-bugs/pr7824.ml
@@ -0,0 +1,78 @@
+(* TEST
+   * expect
+*)
+
+module Element : sig
+  type +'a t
+
+  val from_a  : [`A] t -> unit
+  val from_ab : [< `A | `B] t -> unit
+
+  val to_a  : unit -> [`A] t
+  val to_ab : unit -> [< `A | `B] t
+end = struct
+  type +'a t
+
+  let from_a x = assert false
+  let from_ab x = assert false
+
+  let to_a x = assert false
+  let to_ab x = assert false
+end ;;
+[%%expect{|
+module Element :
+  sig
+    type +'a t
+    val from_a : [ `A ] t -> unit
+    val from_ab : [< `A | `B ] t -> unit
+    val to_a : unit -> [ `A ] t
+    val to_ab : unit -> [< `A | `B ] t
+  end
+|}];;
+
+let f x =
+  Element.from_a x;
+  Element.from_ab x;
+  match [] with
+  | _::_ -> (x :> [`A | `C] Element.t)
+;;
+[%%expect{|
+Line _, characters 2-54:
+  ..match [] with
+    | _::_ -> (x :> [`A | `C] Element.t)
+Warning 8: this pattern-matching is not exhaustive.
+Here is an example of a case that is not matched:
+[]
+val f : [ `A ] Element.t -> [ `A | `C ] Element.t = <fun>
+|}];;
+
+type _ t = T : 'a -> 'a t
+
+let f x =
+  Element.from_a x;
+  Element.from_ab x;
+  match T () with
+  | T _ -> (x :> [`A | `C] Element.t)
+;;
+[%%expect{|
+type _ t = T : 'a -> 'a t
+val f : [ `A ] Element.t -> [ `A | `C ] Element.t = <fun>
+|}];;
+
+let f () =
+  let open Element in
+  let x = if true then to_ab () else to_a () in
+  (x :> [ `A | `C ] Element.t)
+;;
+[%%expect{|
+val f : unit -> [ `A | `C ] Element.t = <fun>
+|}];;
+
+let f () =
+  let open Element in
+  let x = if true then to_a () else to_ab () in
+  (x :> [ `A | `C ] Element.t)
+;;
+[%%expect{|
+val f : unit -> [ `A | `C ] Element.t = <fun>
+|}];;
