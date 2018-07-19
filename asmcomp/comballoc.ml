07a27d2fac064364220ef1ba4242f7a1b1d61806
Stephen Dolan <stephen.dolan@cl.cam.ac.uk>
2018-07-19 11:41:47 +0100

    Ensure that comballoc.ml does not reverse the order of allocations.

--- asmcomp/comballoc.ml
+++ asmcomp/comballoc.ml
@@ -20,9 +20,9 @@ open Mach
 type allocation_state =
     No_alloc                            (* no allocation is pending *)
   | Pending_alloc of Reg.t * int        (* an allocation is pending *)
-(* The arguments of Pending_alloc(reg, ofs) are:
-     reg  the register holding the result of the last allocation
-     ofs  the alloc position in the allocated block *)
+(* The arguments of Pending_alloc(reg, totalsz) are:
+     reg      the register holding the result of the last allocation
+     totalsz  the amount to be allocated in this block *)
 
 let allocated_size = function
     No_alloc -> 0
@@ -34,25 +34,23 @@ let rec combine i allocstate =
       (i, allocated_size allocstate)
   | Iop(Ialloc { bytes = sz; _ }) ->
       begin match allocstate with
-        No_alloc ->
-          let (newnext, newsz) =
-            combine i.next (Pending_alloc(i.res.(0), sz)) in
-          (instr_cons_debug (Iop(Ialloc {bytes = newsz; spacetime_index = 0;
-              label_after_call_gc = None; }))
-            i.arg i.res i.dbg newnext, 0)
-      | Pending_alloc(reg, ofs) ->
-          if ofs + sz < Config.max_young_wosize * Arch.size_addr then begin
-            let (newnext, newsz) =
-              combine i.next (Pending_alloc(reg, ofs + sz)) in
-            (instr_cons (Iop(Iintop_imm(Iadd, ofs))) [| reg |] i.res newnext,
+      | Pending_alloc(reg, totalsz)
+          when totalsz + sz < Config.max_young_wosize * Arch.size_addr ->
+         let (newnext, newsz) =
+              combine i.next (Pending_alloc(i.res.(0), totalsz + sz)) in
+            (instr_cons_debug (Iop(Iintop_imm(Iadd, -sz)))
+               [| reg |] i.res i.dbg newnext,
              newsz)
-          end else begin
-            let (newnext, newsz) =
-              combine i.next (Pending_alloc(i.res.(0), sz)) in
-            (instr_cons_debug (Iop(Ialloc { bytes = newsz; spacetime_index = 0;
-                label_after_call_gc = None; }))
-              i.arg i.res i.dbg newnext, ofs)
-          end
+      | _ ->
+         let (newnext, newsz) = combine i.next (Pending_alloc(i.res.(0), sz)) in
+         let newnext =
+           if newsz = sz then newnext
+           else instr_cons_debug (Iop(Iintop_imm(Iadd, newsz - sz))) i.res
+                i.res i.dbg newnext
+         in
+         (instr_cons_debug (Iop(Ialloc {bytes = newsz; spacetime_index = 0;
+                                        label_after_call_gc = None; }))
+          i.arg i.res i.dbg newnext, allocated_size allocstate)
       end
   | Iop(Icall_ind _ | Icall_imm _ | Iextcall _ |
         Itailcall_ind _ | Itailcall_imm _) ->
