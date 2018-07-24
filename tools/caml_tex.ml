4a21eecd9124abec657eff045f85f503e46f859b
Florian Angeletti <octa@polychoron.fr>
2018-07-24 15:08:16 +0200

    Update change and copyrights

--- tools/caml_tex.ml
+++ tools/caml_tex.ml
@@ -1,3 +1,20 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*             Xavier Leroy, projet Gallium, INRIA Paris                  *)
+(*             Jacques Garrigue, Nagoya University                        *)
+(*             Florian Angeletti                                          *)
+(*                                                                        *)
+(*   Copyright 2018 Institut National de Recherche en Informatique et     *)
+(*     en Automatique.                                                    *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
 [@@@warning "a-40-6"]
 open StdLabels
 open Str
