14ca927015be6281b9f817ad502a26bbbc10efb8
Stephen Dolan <stephen.dolan@cl.cam.ac.uk>
2018-07-25 09:57:31 +0100

    Add test case for RESUMETERM with extra_args != 0

---
+++ testsuite/tests/effects/test11.ml
@@ -0,0 +1,14 @@
+(* Tests RESUMETERM with extra_args != 0 in bytecode,
+   by calling a handler with a tail-continue that returns a function *)
+effect E : int
+
+let handle comp =
+  match comp () with
+  | f -> f
+  | effect E k ->
+     continue k 10
+
+let () =
+  handle (fun () ->
+      Printf.printf "%d\n" (perform E);
+      Printf.printf "%d\n") 42
