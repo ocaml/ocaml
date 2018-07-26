0961a8b92698a73cf7ce6381b30447bf27746577
Thomas Refis <thomas.refis@gmail.com>
2018-07-26 16:53:29 +0100

    testsuite: try to test all the "unclosed ..." parse error messages

---
+++ testsuite/tests/parse-errors/unclosed_paren_module_expr4.ml
@@ -0,0 +1,8 @@
+(* TEST
+* setup-ocamlc.byte-build-env
+** ocamlc.byte
+ocamlc_byte_exit_status = "2"
+*** check-ocamlc.byte-output
+*)
+
+module M = (val 3 :>
---
+++ testsuite/tests/parse-errors/unclosed_simple_expr.ml
@@ -0,0 +1,49 @@
+(* TEST
+   * toplevel
+*)
+
+(3; 2;;
+
+begin 3; 2;;
+
+List.(3; 2;;
+
+simple_expr.(3; 2;;
+
+simple_expr.[3; 2;;
+
+simple_expr.%[3;;
+
+simple_expr.%(3;;
+
+simple_expr.%{3;;
+
+foo.Bar.%[3;;
+
+foo.Bar.%(3;;
+
+foo.Bar.%{3;;
+
+simple_expr.{3, 2;;
+
+{ x = 3; y;;
+
+List.{ x = 3; y ;;
+
+[| 3; 2;;
+
+List.[|3; 2;;
+
+[3; 2;;
+
+List.[3; 2;;
+
+{< x = 3; y; ;;
+
+List.{< x = 3; y ;;
+
+(module struct end :;;
+
+List.(module struct end :;;
+
+(=;
