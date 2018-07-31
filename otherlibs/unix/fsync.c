5b884d62447f1a9b7d840130726b73688ea349db
Francois Berenger <737920+UnixJunkie@users.noreply.github.com>
2018-07-31 14:57:21 +0900

    Unix fsync (#1839)
    * added otherlibs/unix/fsync.c and otherlibs/unix/unix.ml
    
    * add Unix.fsync
    
    * added otherlibs/win32unix/fsync.c
    
    * Unix.fsync for windows
    
    * big typo
    
    * Rewrite Unix.fsync stub for Windows
    
    * belts and braces: fail in the case of named pipe
    
    * Add missing include
    
    * corrected header
    
    * better ocamldoc for fsync
    
    * rm fsync.c from the UNIX_FILES list
    
    * updated Changes for Unix.fsync
    
    * Use _commit instead of FlushFileBuffers
    
    * Include <io.h>

---
+++ otherlibs/unix/fsync.c
@@ -0,0 +1,35 @@
+/**************************************************************************/
+/*                                                                        */
+/*                                 OCaml                                  */
+/*                                                                        */
+/*             Francois Berenger, Kyushu Institute of Technology          */
+/*                                                                        */
+/*   Copyright 2018 Institut National de Recherche en Informatique et     */
+/*     en Automatique.                                                    */
+/*                                                                        */
+/*   All rights reserved.  This file is distributed under the terms of    */
+/*   the GNU Lesser General Public License version 2.1, with the          */
+/*   special exception on linking described in the file LICENSE.          */
+/*                                                                        */
+/**************************************************************************/
+
+#include <caml/mlvalues.h>
+#include <caml/signals.h>
+#include "unixsupport.h"
+
+#ifdef _WIN32
+#include <io.h>
+#define fsync(fd) _commit(win_CRT_fd_of_filedescr(fd))
+#else
+#define fsync(fd) fsync(Int_val(fd))
+#endif
+
+CAMLprim value unix_fsync(value fd)
+{
+  int ret;
+  caml_enter_blocking_section();
+  ret = fsync(fd);
+  caml_leave_blocking_section();
+  if (ret == -1) uerror("fsync", Nothing);
+  return Val_unit;
+}
