e1bf5e9accd48fd69116fb86600310641246ecc1
Stephen Dolan <stephen.dolan@cl.cam.ac.uk>
2018-08-06 12:33:58 +0100

    Add a test case for #176

---
+++ testsuite/tests/effects/partial.ml
@@ -0,0 +1,17 @@
+effect E : unit
+exception Done
+
+let handle_partial f =
+  try f () with
+  | effect E k -> assert false
+
+let f () x = perform E
+
+let () =
+  match
+    let g = handle_partial f in
+    g ()
+  with
+  | x -> assert false
+  | exception Done -> print_string "ok\n"
+  | effect E k -> discontinue k Done
