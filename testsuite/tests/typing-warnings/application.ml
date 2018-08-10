1fcb9f5b27803a9a27a33ab03227b534bad6654d
Nicolás Ojeda Bär <n.oje.bar@gmail.com>
2018-08-10 12:05:39 +0200

    Port typing-warnings/application to expect

--- testsuite/tests/typing-warnings/application.ml
+++ testsuite/tests/typing-warnings/application.ml
@@ -1,10 +1,29 @@
 (* TEST
    flags = " -w A -strict-sequence "
-   * toplevel
+   * expect
 *)
 
 (* Ignore OCAMLRUNPARAM=b to be reproducible *)
 Printexc.record_backtrace false;;
+[%%expect {|
+- : unit = ()
+|}]
 
 let _ = ignore (+);;
+[%%expect {|
+Line 1, characters 15-18:
+  let _ = ignore (+);;
+                 ^^^
+Warning 5: this function application is partial,
+maybe some arguments are missing.
+- : unit = ()
+|}]
+
 let _ = raise Exit 3;;
+[%%expect {|
+Line 1, characters 19-20:
+  let _ = raise Exit 3;;
+                     ^
+Warning 20: this argument will not be used by the function.
+Exception: Stdlib.Pervasives.Exit.
+|}]
