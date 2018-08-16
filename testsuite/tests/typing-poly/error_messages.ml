155c8d7ddba20347aec741e9dc4922d61c4d3e15
Florian Angeletti <octa@polychoron.fr>
2018-08-17 00:00:56 +0200

    expanded error message for forall failure

---
+++ testsuite/tests/typing-poly/error_messages.ml
@@ -0,0 +1,28 @@
+(* TEST
+   * expect
+*)
+
+type t = < x : 'a. int as 'a >
+[%%expect {|
+Line 1, characters 15-28:
+  type t = < x : 'a. int as 'a >
+                 ^^^^^^^^^^^^^
+Error: The universal type variable 'a cannot be generalized: it is bound to
+       int.
+|}]
+type u = < x : 'a 'b. 'a as 'b >
+[%%expect {|
+Line 1, characters 15-30:
+  type u = < x : 'a 'b. 'a as 'b >
+                 ^^^^^^^^^^^^^^^
+Error: The universal type variable 'b cannot be generalized:
+       it is already bound to another variable.
+|}]
+type v = 'b -> < x : 'a. 'b as 'a >
+[%%expect {|
+Line 1, characters 21-33:
+  type v = 'b -> < x : 'a. 'b as 'a >
+                       ^^^^^^^^^^^^
+Error: The universal type variable 'a cannot be generalized:
+       it escapes its scope.
+|}]
