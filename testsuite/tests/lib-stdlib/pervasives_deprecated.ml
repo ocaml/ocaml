7cf321a3cf51134a185198955fc6aef3a400fb3a
Jeremie Dimino <jeremie@dimino.org>
2018-08-28 16:25:45 +0100

    Update the deprecation message for Stdlib.Pervasives
    Make it point to stdlib-shims

--- testsuite/tests/lib-stdlib/pervasives_deprecated.ml
+++ testsuite/tests/lib-stdlib/pervasives_deprecated.ml
@@ -11,6 +11,9 @@ Line 3, characters 0-14:
   ^^^^^^^^^^^^^^
 Error (warning 3): deprecated: module Stdlib.Pervasives
 Use Stdlib instead.
+
+If you need to stay compatible with OCaml < 4.07, you can use the
+stdlib-shims library: https://github.com/ocaml/stdlib-shims
 |}]
 
 module X = Pervasives;;
@@ -20,6 +23,9 @@ Line 1, characters 11-21:
              ^^^^^^^^^^
 Error (warning 3): deprecated: module Stdlib.Pervasives
 Use Stdlib instead.
+
+If you need to stay compatible with OCaml < 4.07, you can use the
+stdlib-shims library: https://github.com/ocaml/stdlib-shims
 |}]
 
 open Pervasives;;
@@ -29,4 +35,7 @@ Line 1, characters 5-15:
        ^^^^^^^^^^
 Error (warning 3): deprecated: module Stdlib.Pervasives
 Use Stdlib instead.
+
+If you need to stay compatible with OCaml < 4.07, you can use the
+stdlib-shims library: https://github.com/ocaml/stdlib-shims
 |}]
