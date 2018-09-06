cc71af568ad2203df21bce1265d84c6d31a67476
Nicolás Ojeda Bär <n.oje.bar@gmail.com>
2018-09-06 10:14:06 +0200

    Add test

--- testsuite/tests/lib-unix/common/truncate.ml
+++ testsuite/tests/lib-unix/common/truncate.ml
@@ -1,15 +1,12 @@
 (* TEST
 include unix
-files = "truncate.txt"
+files = "truncate.txt ftruncate.txt"
 *)
 
 let txt = "truncate.txt"
 
-let size f =
-  let ic = open_in_bin f in
-  let n = in_channel_length ic in
-  close_in ic;
-  n
+let size s =
+  (Unix.stat s).Unix.st_size
 
 let () =
   Printf.printf "initial size: %d\n%!" (size txt);
@@ -17,3 +14,17 @@ let () =
   Printf.printf "new size: %d\n%!" (size txt);
   Unix.truncate txt 0;
   Printf.printf "final size: %d\n%!" (size txt)
+
+let ftxt = "ftruncate.txt"
+
+let size fd =
+  (Unix.fstat fd).Unix.st_size
+
+let () =
+  let fd = Unix.openfile ftxt [O_RDWR] 0 in
+  Printf.printf "initial size: %d\n%!" (size fd);
+  Unix.ftruncate fd (size fd - 3);
+  Printf.printf "new size: %d\n%!" (size fd);
+  Unix.ftruncate fd 0;
+  Printf.printf "final size: %d\n%!" (size fd);
+  Unix.close fd
