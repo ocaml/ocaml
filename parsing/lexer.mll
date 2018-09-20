ocaml/ocaml

commit 9046dc62199293bb0fb6467d8147451ec38f884a

update error

parsing/lexer.mll
@@ -170,7 +170,7 @@ let char_for_decimal_code lexbuf i =
     then 'x'
     else
       illegal_escape lexbuf
-        (Printf.sprintf "%d is outside the range of ASCII characters." c)
+        (Printf.sprintf "%d is outside the range of legal characters (0-255)." c)
   else Char.chr c
 
 let char_for_octal_code lexbuf i =
@@ -182,7 +182,7 @@ let char_for_octal_code lexbuf i =
     then 'x'
     else
       illegal_escape lexbuf
-        (Printf.sprintf "o%o (=%d) is outside the range of ASCII characters." c c)
+        (Printf.sprintf "o%o (=%d) is outside the range of legal characters (0-255)." c c)
   else Char.chr c
 
 let char_for_hexadecimal_code lexbuf i =

testsuite/tests/lexing/escape.ocaml.reference
@@ -6,11 +6,11 @@ val invalid : string = "\\99"
 Line 1, characters 15-19:
   let invalid = "\999" ;;
                  ^^^^
-Error: Illegal backslash escape in string or character (\999): 999 is outside the range of ASCII characters.
+Error: Illegal backslash escape in string or character (\999): 999 is outside the range of legal characters (0-255).
 Line 1, characters 15-20:
   let invalid = "\o777" ;;
                  ^^^^^
-Error: Illegal backslash escape in string or character (\o777): o777 (=511) is outside the range of ASCII characters.
+Error: Illegal backslash escape in string or character (\o777): o777 (=511) is outside the range of legal characters (0-255).
 Line 1, characters 15-17:
   let invalid = "\o77" ;;
                  ^^

commit 9b28ccdc160fe2f109b69c5c2f2a4d08ed8f52f1

Break long lines introduced by 9046dc6219

parsing/lexer.mll
@@ -170,7 +170,8 @@ let char_for_decimal_code lexbuf i =
     then 'x'
     else
       illegal_escape lexbuf
-        (Printf.sprintf "%d is outside the range of legal characters (0-255)." c)
+        (Printf.sprintf
+          "%d is outside the range of legal characters (0-255)." c)
   else Char.chr c
 
 let char_for_octal_code lexbuf i =
@@ -182,7 +183,8 @@ let char_for_octal_code lexbuf i =
     then 'x'
     else
       illegal_escape lexbuf
-        (Printf.sprintf "o%o (=%d) is outside the range of legal characters (0-255)." c c)
+        (Printf.sprintf
+          "o%o (=%d) is outside the range of legal characters (0-255)." c c)
   else Char.chr c
 
 let char_for_hexadecimal_code lexbuf i =