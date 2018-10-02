bd327f53f819fc50032da411cce3e87bad9debed
Mark Shinwell <mshinwell@gmail.com>
2018-10-02 10:50:45 +0100

    Enhance testsuite/tests/float-unboxing/unbox_under_switch.ml to cover boxed integer cases too

--- testsuite/tests/float-unboxing/unbox_under_switch.ml
+++ testsuite/tests/float-unboxing/unbox_under_switch.ml
@@ -15,3 +15,59 @@ let bar : type a. a t -> float -> int -> a =
 let foo (t : float t) f i =
   let r = ref 0. in
   r := bar t f i
+
+(* These boxed integer cases were also fixed by GPR#2083, although
+   current compiler code would not actually cause a failure even
+   before that fix.  The tests here are given in case register
+   typing is tightened up in future (e.g. GPR#1192). *)
+
+(* For int32 *)
+
+type _ t =
+  | IO : int option t
+  | F : int32 t
+
+let bar : type a. a t -> int32 -> int -> a =
+  fun t f i ->
+    match t with
+    | IO -> Some i
+    | F -> f
+[@@inline always]
+
+let foo (t : int32 t) f i =
+  let r = ref 0l in
+  r := bar t f i
+
+(* For int64 *)
+
+type _ t =
+  | IO : int option t
+  | F : int64 t
+
+let bar : type a. a t -> int64 -> int -> a =
+  fun t f i ->
+    match t with
+    | IO -> Some i
+    | F -> f
+[@@inline always]
+
+let foo (t : int64 t) f i =
+  let r = ref 0L in
+  r := bar t f i
+
+(* For nativeint *)
+
+type _ t =
+  | IO : int option t
+  | F : nativeint t
+
+let bar : type a. a t -> nativeint -> int -> a =
+  fun t f i ->
+    match t with
+    | IO -> Some i
+    | F -> f
+[@@inline always]
+
+let foo (t : nativeint t) f i =
+  let r = ref 0n in
+  r := bar t f i
