1b72f4b71c52294009a929ada78e04089b96d981
Mark Shinwell <mshinwell@gmail.com>
2018-10-03 11:12:42 +0100

    GPR#2085 (Add end_of_function_label to Emit.fundecl)

--- asmcomp/emit.mli
+++ asmcomp/emit.mli
@@ -15,7 +15,7 @@
 
 (* Generation of assembly code *)
 
-val fundecl: Linearize.fundecl -> unit
+val fundecl: Linearize.fundecl -> end_of_function_label:Linearize.label -> unit
 val data: Cmm.data_item list -> unit
 val begin_assembly: unit -> unit
 val end_assembly: unit -> unit
