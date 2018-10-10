d695dcaf2d06caf147df919338d231e7ab8ab3ef
Thomas Refis <thomas.refis@gmail.com>
2018-10-10 10:15:13 +0100

    shindere's review

--- bytecomp/generate_runtimedef.sh
+++ bytecomp/generate_runtimedef.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+#**************************************************************************
+#*                                                                        *
+#*                                 OCaml                                  *
+#*                                                                        *
+#*            Xavier Leroy, projet Cristal, INRIA Rocquencourt            *
+#*                                                                        *
+#*   Copyright 1999 Institut National de Recherche en Informatique et     *
+#*     en Automatique.                                                    *
+#*                                                                        *
+#*   All rights reserved.  This file is distributed under the terms of    *
+#*   the GNU Lesser General Public License version 2.1, with the          *
+#*   special exception on linking described in the file LICENSE.          *
+#*                                                                        *
+#**************************************************************************
+
 echo 'let builtin_exceptions = [|'
 cat "$1" | tr -d '\r' | \
     sed -n -e 's|.*/\* \("[A-Za-z_]*"\) \*/$|  \1;|p'
