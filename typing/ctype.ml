4a6f33d5577b5253609051406796b3aad1ededee
Thomas Refis <thomas.refis@gmail.com>
2018-10-16 17:08:18 +0100

    make check_scope_escape behave more like update_level

--- typing/ctype.ml
+++ typing/ctype.ml
@@ -759,8 +759,8 @@ let rec normalize_package_path env p =
           normalize_package_path env (Path.Pdot (p1', s, n))
       | _ -> p
 
-let check_scope_escape level ty =
-  let rec aux ty =
+let check_scope_escape env level ty =
+  let rec loop ty =
     let ty = repr ty in
     if ty.level >= lowest_level then begin
       ty.level <- pivot_level - ty.level;
@@ -768,15 +768,24 @@ let check_scope_escape level ty =
         raise(Trace.scope_escape ty);
       begin match ty.desc with
       | Tconstr (p, _, _) when level < Path.scope p ->
-        raise Trace.(Unify [escape (Constructor p)])
-      | _ -> ()
+          begin match !forward_try_expand_once env ty with
+          | ty' -> aux ty'
+          | exception Cannot_expand ->
+              raise Trace.(Unify [escape (Constructor p)])
+          end
+      | Tpackage (p, nl, tl) when level < Path.scope p ->
+          let p' = normalize_package_path env p in
+          if Path.same p p' then raise Trace.(Unify [escape (Module_type p)]);
+          aux { ty with desc = Tpackage (p', nl, tl) }
+      | _ ->
+        iter_type_expr loop ty
       end;
-      iter_type_expr aux ty
     end
-  in
-  try
-    aux ty;
+  and aux ty =
+    loop ty;
     unmark_type ty
+  in
+  try aux ty;
   with Unify [Trace.Escape x] ->
     raise Trace.(Unify[Escape { x with context = Some ty }])
 
