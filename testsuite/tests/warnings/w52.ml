3d18694fa13d9dc38a8527353de8ca72b9db48f7
Jeremy Yallop <yallop@gmail.com>
2018-11-05 09:16:54 +0000

    Generalized warn_on_literal_pattern: test cases for deep matching

--- testsuite/tests/warnings/w52.ml
+++ testsuite/tests/warnings/w52.ml
@@ -11,23 +11,38 @@ compile_only = "true"
 
 let () = try () with Invalid_argument "Any" -> ();;
 
+let () = try () with Match_failure ("Any",_,_) -> ();;
+
+let () = try () with Match_failure (_,0,_) -> ();;
+
 type t =
   | Warn of string  [@ocaml.warn_on_literal_pattern]
   | Without_warning of string
-  | Warn' of nativeint [@ocaml.warn_on_literal_pattern];;
+  | Warn' of nativeint [@ocaml.warn_on_literal_pattern]
+  | Deep of (string * int) list [@ocaml.warn_on_literal_pattern];;
 
 let f = function
 | Warn "anything" -> ()
-| Warn _ | Warn' _ | Without_warning _ -> ()
+| Warn _ | Warn' _ | Without_warning _ | Deep _ -> ()
 ;;
 
 let g = function
 | Warn' 0n -> ()
-| Warn _ | Warn' _ | Without_warning _ -> ()
+| Warn _ | Warn' _ | Without_warning _ | Deep _ -> ()
 ;;
 
 
 let h = function
 | Without_warning "outside" -> ()
-| Warn _ | Warn' _ | Without_warning _ -> ()
+| Warn _ | Warn' _ | Without_warning _ | Deep _ -> ()
+;;
+
+let i = function
+| Deep (_ :: _ :: _ :: _) -> ()
+| Warn _ | Warn' _ | Without_warning _ | Deep _ -> ()
+;;
+
+let j = function
+| Deep (_ :: _ :: ("deep",_) :: _) -> ()
+| Warn _ | Warn' _ | Without_warning _ | Deep _ -> ()
 ;;
