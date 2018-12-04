2026f4d6143a477557d9c489fe024b3bd513f014
David Allsopp <david.allsopp@metastack.com>
2018-12-04 13:29:14 +0000

    Add extra defines to s-nt.h for mingw32
    These match up with facts determined by autoconf. The following
    additional HAS_ defines are made:
    
    - HAS_UNISTD
        Behaviour of otherlibs/unix/access.c and otherlibs/win32unix/lseek.c
        is affected but the effect is correct.
        Behaviour of runtime/fix_code.c affected, but unistd.h includes
        io.h, so there's no effective change.
        In other cases, unistd.h is simply included where before it wasn't.
    - HAS_DIRENT
        Used in Unix-only closedir.c, opendir.c, readdir.c and rewinddir.c
        Used in runtime/unix.c, but that obviously won't affect Windows!
    - HAS_REWINDDIR
        Used in Unix-only rewinddir.c
    - HAS_TRUNCATE
        Used in Unix-only ftruncate.c, mmap.c and truncate.c
    - HAS_NANOSLEEP
        Used in Unix-only sleep.c
    - HAS_GETTIMEOFDAY
        Used in vmthreads and Unix-only gettimeofday.c
        Used in runtime/sys.c, but in a branch guarded by #ifndef _WIN32
    - HAS_MKSTEMP
        Changes yacc/main.c to use the MinGW implementation of mkstemp
        (part of mingwex, which is linked by flexdll)

--- config/s-nt.h
+++ config/s-nt.h
@@ -24,14 +24,29 @@
 #if defined(__MINGW32__) || _MSC_VER >= 1600
 #define HAS_STDINT_H 1
 #endif
+#ifdef __MINGW32__
+#define HAS_UNISTD 1
+#define HAS_DIRENT 1
+#define HAS_REWINDDIR 1
+#endif
 #define HAS_GETCWD 1
 #define HAS_UTIME 1
+#ifdef __MINGW32__
+#define HAS_TRUNCATE 1
+#define HAS_NANOSLEEP 1
+#endif
 #define HAS_GETHOSTNAME 1
+#ifdef __MINGW32__
+#define HAS_GETTIMEOFDAY 1
+#endif
 #define HAS_MKTIME 1
 #define HAS_PUTENV 1
 #ifndef __MINGW32__
 #define HAS_LOCALE_H 1
 #define HAS_STRTOD_L 1
+#endif
+#ifdef __MINGW32__
+#define HAS_MKSTEMP 1
 #endif
 #define HAS_NICE 1
 #define HAS_EXECVPE 1
