c3bc58efc5702e64b199b60de757262e90eb6d60
Vladimir Keleshev <vladimir@keleshev.com>
2018-12-07 15:13:02 +0100

    make promote-menhir

--- boot/menhir/menhirLib.ml
+++ boot/menhir/menhirLib.ml
@@ -3513,5 +3513,23 @@ module MakeEngineTable (T : TableFormat.TABLES) = struct
 end
 end
 module StaticVersion = struct
-let require_20181113 = ()
+(******************************************************************************)
+(*                                                                            *)
+(*                                   Menhir                                   *)
+(*                                                                            *)
+(*                       François Pottier, Inria Paris                        *)
+(*              Yann Régis-Gianas, PPS, Université Paris Diderot              *)
+(*                                                                            *)
+(*  Copyright Inria. All rights reserved. This file is distributed under the  *)
+(*  terms of the GNU Library General Public License version 2, with a         *)
+(*  special exception on linking, as described in the file LICENSE.           *)
+(*                                                                            *)
+(******************************************************************************)
+
+(* This file is overwritten when a package is created. It is supposed
+   to define a value of type [unit] whose name is [require_XXXXXXXX],
+   where [XXXXXXXX] is our 8-digit version number. *)
+
+let require_unreleased =
+  ()
 end
