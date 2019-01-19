5bfbbfa1760c769b250ed2635727c15badf43e3c
Romain Beauxis <toots@rastageeks.org>
2019-01-19 09:24:53 -0600

    Set default status in waitpid when pid = 0.

--- testsuite/tests/lib-unix/common/wait_nohang.ml
+++ testsuite/tests/lib-unix/common/wait_nohang.ml
@@ -15,3 +15,19 @@ let () =
   match Sys.remove "plop" with
   | () ->  print_endline "OK"
   | exception (Sys_error _) -> print_endline "ERROR"
+
+let () =
+  let fd = Unix.openfile "plip" [O_CREAT; O_WRONLY] 0o666 in
+  let pid =
+    Unix.create_process "sleep" [|"sleep"; "0.01"|] Unix.stdin fd Unix.stderr
+  in
+  Unix.close fd;
+  let pid, status = Unix.waitpid [WNOHANG] pid in
+  assert (pid = 0);
+  assert (status = WEXITED 0);
+  while fst (Unix.waitpid [WNOHANG] pid) = 0 do
+    Unix.sleepf 0.001
+  done;
+  match Sys.remove "plip" with
+  | () ->  print_endline "OK"
+  | exception (Sys_error _) -> print_endline "ERROR"
