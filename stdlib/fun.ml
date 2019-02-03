70cb07334a8d5a8ed65d46d92ef83691d1e9cb64
Nicolás Ojeda Bär <n.oje.bar@gmail.com>
2019-02-03 15:17:56 +0100

    Merge pull request #2118 from gadmm/finally
    protect: treat as an error the case where two exceptions are being raised
    
    (cherry-picked from commit 24867b2e10db9ee24445e7e1d114891f45578d93)

--- stdlib/fun.ml
+++ stdlib/fun.ml
@@ -17,3 +17,18 @@ external id : 'a -> 'a = "%identity"
 let const c _ = c
 let flip f x y = f y x
 let negate p v = not (p v)
+
+exception Finally_raised of exn
+
+let protect ~(finally : unit -> unit) work =
+  let finally_no_exn () =
+    try finally () with e ->
+      let bt = Printexc.get_raw_backtrace () in
+      Printexc.raise_with_backtrace (Finally_raised e) bt
+  in
+  match work () with
+  | result -> finally_no_exn () ; result
+  | exception work_exn ->
+      let work_bt = Printexc.get_raw_backtrace () in
+      finally_no_exn () ;
+      Printexc.raise_with_backtrace work_exn work_bt
