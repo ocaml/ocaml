51025d17294a1db398043be3472b005836f7c9f2
Stephen Dolan <stephen.dolan@cl.cam.ac.uk>
2019-02-12 11:13:55 +0000

    Add optimisations in simplif.ml for Obj.with_tag.

--- testsuite/tests/lib-obj/with_tag.ml
+++ testsuite/tests/lib-obj/with_tag.ml
@@ -7,5 +7,23 @@ type t =
 
 let () =
   assert (Obj.dup (Obj.repr (A ("hello", 10.))) = Obj.repr (A ("hello", 10.)));
-  assert (Obj.with_tag 1 (Obj.repr (A ("hello", 10.))) = Obj.repr (B ("hello", 10.)));
+  assert (Obj.with_tag 1 (Obj.repr (A ("hello", 10.))) = Obj.repr (B ("hello", 10.)))
+
+
+(* check optimisations *)
+let raw_allocs f =
+  let before = Gc.minor_words () in
+  f ();
+  let after = Gc.minor_words () in
+  int_of_float (after -. before)
+
+let allocs =
+  let overhead = raw_allocs (fun () -> ()) in
+  fun f -> raw_allocs f - overhead
+
+let () =
+  assert (allocs (fun () -> Obj.with_tag 1 (Obj.repr (A ("hello", 10.)))) = 0);
+  assert (allocs (fun () -> Obj.with_tag 1 (Obj.repr (ref 10))) = 2)
+
+let () =
   print_endline "ok"
