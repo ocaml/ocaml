9f392523dc9fe009a8b3e4e363f15ba82f63dec0
Jeremie Dimino <jeremie@dimino.org>
2019-02-25 14:42:07 +0000

    Delete the deprecated Bigarray.*.map_file functions
    Signed-off-by: Jeremie Dimino <jeremie@dimino.org>

--- otherlibs/bigarray/bigarray.ml
+++ otherlibs/bigarray/bigarray.ml
@@ -12,40 +12,4 @@
 (*                                                                        *)
 (**************************************************************************)
 
-module Super = Stdlib.Bigarray
-
-include (Super : module type of struct include Super end
-         with module Genarray := Super.Genarray
-         with module Array1   := Super.Array1
-         with module Array2   := Super.Array2
-         with module Array3   := Super.Array3)
-
-module Genarray = struct
-  include Super.Genarray
-  external map_internal: Unix.file_descr -> ('a, 'b) kind -> 'c layout ->
-    bool -> int array -> int64 -> ('a, 'b, 'c) t
-    = "caml_ba_map_file_bytecode" "caml_ba_map_file"
-  let map_file fd ?(pos = 0L) kind layout shared dims =
-    map_internal fd kind layout shared dims pos
-end
-
-module Array1 = struct
-  include Super.Array1
-  let map_file fd ?pos kind layout shared dim =
-    array1_of_genarray
-      (Genarray.map_file fd ?pos kind layout shared [|dim|])
-end
-
-module Array2 = struct
-  include Super.Array2
-  let map_file fd ?pos kind layout shared dim1 dim2 =
-    array2_of_genarray
-      (Genarray.map_file fd ?pos kind layout shared [|dim1;dim2|])
-end
-
-module Array3 = struct
-  include Super.Array3
-  let map_file fd ?pos kind layout shared dim1 dim2 dim3 =
-    array3_of_genarray
-      (Genarray.map_file fd ?pos kind layout shared [|dim1;dim2;dim3|])
-end
+include Stdlib.Bigarray
