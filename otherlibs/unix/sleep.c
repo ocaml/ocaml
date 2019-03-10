c69394b1df1d7ce3b7fee73e23a9ac6d7809d446
Xavier Leroy <xavier.leroy@college-de-france.fr>
2019-03-10 18:48:25 +0100

    MPR#7903: make Thread.delay interruptible again
    In OCaml 4.07, Unix.sleepf and Thread.delay were changed so that they
    would restart the sleep when interrupted by a signal (error EINTR).
    
    The unintented consequence is that Thread.delay will not run
    signal handlers until the full delay has expired.  If the effect
    of the handler is to raise an exception, as with Sys.catch_break,
    the delay is not terminated early.
    
    (This is specific to threaded programs, where asynchronous invocation
    of signal handlers is turned off and handlers are only run at the next
    leave-blocking-section.  Using Unix.sleepf in a non-threaded program
    doesn't show the issue.)
    
    This commit implements a more intuitive behavior, closer to that of 4.06:
    signals received during Thread.delay are handled immediately, and if
    the handler returns normally, the delay is restarted with the remaining time.
    
    A test is added in testsuite/tests/lib-threads/delayintr.ml

--- otherlibs/unix/sleep.c
+++ otherlibs/unix/sleep.c
@@ -35,26 +35,31 @@ CAMLprim value unix_sleep(value duration)
   {
     struct timespec t;
     int ret;
-    caml_enter_blocking_section();
     t.tv_sec = (time_t) d;
     t.tv_nsec = (d - t.tv_sec) * 1e9;
     do {
+      caml_enter_blocking_section();
       ret = nanosleep(&t, &t);
+      /* MPR#7903: if we were interrupted by a signal, and this signal
+         is handled in OCaml, we should run its handler now,
+         not at the end of the full sleep duration.  Leaving the blocking
+         section and re-entering it does the job. */
+      caml_leave_blocking_section();
     } while (ret == -1 && errno == EINTR);
-    caml_leave_blocking_section();
     if (ret == -1) uerror("sleep", Nothing);
   }
 #elif defined(HAS_SELECT)
   {
     struct timeval t;
     int ret;
-    caml_enter_blocking_section();
     t.tv_sec = (time_t) d;
     t.tv_usec = (d - t.tv_sec) * 1e6;
     do {
+      caml_enter_blocking_section();
       ret = select(0, NULL, NULL, NULL, &t);
+      /* MPR#7903: same comment as above */
+      caml_leave_blocking_section();
     } while (ret == -1 && errno == EINTR);
-    caml_leave_blocking_section();
     if (ret == -1) uerror("sleep", Nothing);
   }
 #else
