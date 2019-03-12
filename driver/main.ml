6231ad97693ae7505ea471691d2317d9907d08ab
David Allsopp <david.allsopp@metastack.com>
2019-03-12 19:45:24 +0000

    Parse entire command line first

--- driver/main.ml
+++ driver/main.ml
@@ -21,7 +21,7 @@ let usage = "Usage: ocamlc <options> <files>\nOptions are:"
 (* Error messages to standard error formatter *)
 let ppf = Format.err_formatter
 
-let vmthread_removed_message = "\
+let vmthread_removed_message = lazy "\
 The -vmthread argument of ocamlc is no longer supported\n\
 since OCaml 4.09.0.  Please switch to system threads, which have the\n\
 same API. Lightweight threads with VM-level scheduling are provided by\n\
@@ -108,7 +108,7 @@ module Options = Main_args.Make_bytecomp_options (struct
   let _strict_formats = set strict_formats
   let _no_strict_formats = unset strict_formats
   let _thread = set use_threads
-  let _vmthread = fun () -> fatal vmthread_removed_message
+  let _vmthread = fun () -> ignore (Lazy.force vmthread_removed_message)
   let _unboxed_types = set unboxed_types
   let _no_unboxed_types = unset unboxed_types
   let _unsafe = set unsafe
@@ -155,6 +155,8 @@ let main () =
     readenv ppf Before_args;
     Clflags.parse_arguments anonymous usage;
     Compmisc.read_clflags_from_env ();
+    if Lazy.is_val vmthread_removed_message then
+      fatal (Lazy.force vmthread_removed_message);
     begin try
       Compenv.process_deferred_actions
         (ppf,
