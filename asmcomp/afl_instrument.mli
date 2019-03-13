618e5dbfbd6ab7d7cb235f3800c7a20eda43ffa2
Mark Shinwell <mshinwell@gmail.com>
2019-03-13 15:40:04 +0000

    More debugging information in Cmm terms (#2308)
    Following on from GPR#851 and GPR#873, this pull request further enhances debugging information in Cmm terms. This was driven both by manually examining the debugger's behaviour and also by a report received from a user regarding substandard DWARF location information.

--- asmcomp/afl_instrument.mli
+++ asmcomp/afl_instrument.mli
@@ -1,4 +1,21 @@
-(* Instrumentation for afl-fuzz *)
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*                 Stephen Dolan, University of Cambridge                 *)
+(*                                                                        *)
+(*   Copyright 2016 Stephen Dolan.                                        *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
 
-val instrument_function : Cmm.expression -> Cmm.expression
-val instrument_initialiser : Cmm.expression -> Cmm.expression
+(** Instrumentation for afl-fuzz. *)
+
+val instrument_function : Cmm.expression -> Debuginfo.t -> Cmm.expression
+val instrument_initialiser
+   : Cmm.expression
+  -> (unit -> Debuginfo.t)
+  -> Cmm.expression
