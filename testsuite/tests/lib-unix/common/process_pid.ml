c5dac4eae04a27eacbb0a33f608c1a209486ae3b
Nicolás Ojeda Bär <n.oje.bar@gmail.com>
2019-03-18 16:28:55 +0100

    Fix lib-unix/common/process_pid test

--- testsuite/tests/lib-unix/common/process_pid.ml
+++ testsuite/tests/lib-unix/common/process_pid.ml
@@ -6,10 +6,15 @@ include unix
 *)
 
 let () =
-  let process =
+  let ic, _ as process =
     Unix.open_process "echo toto"
   in
   assert
     (Unix.process_pid process = Unix.process_pid process);
+
+  (* read everything to avoid
+     "The process tried to write to a nonexistent pipe." on Windows *)
+  ignore (input_line ic);
+
   ignore (Unix.close_process process);
   print_endline "OK"
