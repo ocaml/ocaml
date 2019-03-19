522fd6746bb494d827e9fe51dc4c833edd0ef7f8
Nicolás Ojeda Bär <n.oje.bar@gmail.com>
2019-03-19 13:48:34 +0100

    Update test results

--- testsuite/tests/generalized-open/gpr1506.ml
+++ testsuite/tests/generalized-open/gpr1506.ml
@@ -100,9 +100,9 @@ include struct open struct type t = T end let x = T end
 Line 1, characters 15-41:
 1 | include struct open struct type t = T end let x = T end
                    ^^^^^^^^^^^^^^^^^^^^^^^^^^
-Error: The type t/142 introduced by this open appears in the signature
+Error: The type t/143 introduced by this open appears in the signature
        Line 1, characters 46-47:
-         The value x has no valid type if t/142 is hidden
+         The value x has no valid type if t/143 is hidden
 |}];;
 
 module A = struct
@@ -120,9 +120,9 @@ Line 3, characters 4-56:
 4 |       type t = T
 5 |       let x = T
 6 |     end
-Error: The type t/148 introduced by this open appears in the signature
+Error: The type t/149 introduced by this open appears in the signature
        Line 7, characters 8-9:
-         The value y has no valid type if t/148 is hidden
+         The value y has no valid type if t/149 is hidden
 |}];;
 
 module A = struct
@@ -139,9 +139,9 @@ Line 3, characters 4-40:
 3 | ....open struct
 4 |       type t = T
 5 |     end
-Error: The type t/154 introduced by this open appears in the signature
+Error: The type t/155 introduced by this open appears in the signature
        Line 6, characters 8-9:
-         The value y has no valid type if t/154 is hidden
+         The value y has no valid type if t/155 is hidden
 |}]
 
 (* It was decided to not allow this anymore. *)
