d5d8faad4bd1fe12aece07951dc420ab1435766b
Andrew Litteken <andrew.litteken@gmail.com>
2019-04-09 14:20:32 -0400

    adding uncuaght excpetion tests

---
+++ testsuite/tests/tool-toplevel/uncaught_exceptions.ml
@@ -0,0 +1,39 @@
+(* TEST
+   * expect
+*)
+
+(* PR#8594 *)
+Printexc.register_printer (fun e -> 
+  match e with 
+    | Division_by_zero -> Some "A division by zero is undefined"
+    | _ -> None);;
+Printexc.register_printer (fun e -> 
+  match e with 
+    | Exit -> Some "Catching an exit"
+    | _ -> None);;
+raise Not_found;;
+[%%expect{|
+- : unit = ()
+- : unit = ()
+Exception: Not_found.
+|}];;
+
+raise Exit;;
+[%%expect{|
+Exception: Catching an exit
+|}];;
+
+exception Foo of string;;
+[%%expect {|
+exception Foo of string
+|}];;
+
+raise (Foo "bar");;
+[%%expect {|
+Exception: Foo "bar".
+|}];;
+
+raise Division_by_zero;;
+[%%expect {|
+Exception: A division by zero is undefined
+|}];;
