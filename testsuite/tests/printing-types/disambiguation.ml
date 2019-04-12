b37bf3ef0c51a02b1311fd7c39fb7807d2ec58d4
Florian Angeletti <octa@polychoron.fr>
2019-04-12 18:25:10 +0200

    type printing: private row error

--- testsuite/tests/printing-types/disambiguation.ml
+++ testsuite/tests/printing-types/disambiguation.ml
@@ -6,14 +6,10 @@ type 'a x = private [> `x] as 'a;;
 [%%expect {|
 Line 1:
 Error: Type declarations do not match:
-         type 'a x = private [> `x ] constraint 'a = 'a x/2
+         type 'a x = private [> `x ] constraint 'a = 'a x
        is not included in
          type 'a x
        Their constraints differ.
-       File "_none_", line 1:
-         Definition of type x/1
-       Line 1, characters 0-32:
-         Definition of type x/2
 |}, Principal{|
 Line 1:
 Error: Type declarations do not match:
