ed775eba0cdf4b7f33c5df327601091a28c777bf
Thomas Refis <thomas.refis@gmail.com>
2019-04-12 11:25:19 +0100

    env: refresh signature is subsumed by Subst.signature

--- typing/env.ml
+++ typing/env.ml
@@ -2070,55 +2070,8 @@ let rec add_signature sg env =
     [] -> env
   | comp :: rem -> add_signature rem (add_item comp env)
 
-let refresh_signature ~scope sg =
-  let rec refresh_bound_idents s sg =
-    let open Subst in
-    function
-      [] -> sg, s
-    | Sig_type(id, td, rs, vis) :: rest ->
-        let id' = Ident.create_scoped ~scope (Ident.name id) in
-        refresh_bound_idents
-          (add_type id (Pident id') s)
-          (Sig_type(id', td, rs, vis) :: sg)
-          rest
-    | Sig_module(id, pres, md, rs, vis) :: rest ->
-        let id' = Ident.create_scoped ~scope (Ident.name id) in
-        refresh_bound_idents
-          (add_module id (Pident id') s)
-          (Sig_module (id', pres, md, rs, vis) :: sg)
-          rest
-    | Sig_modtype(id, mtd, vis) :: rest ->
-        let id' = Ident.create_scoped ~scope (Ident.name id) in
-        refresh_bound_idents
-          (add_modtype id (Mty_ident(Pident id')) s)
-          (Sig_modtype(id', mtd, vis) :: sg)
-          rest
-    | Sig_class(id, cd, rs, vis) :: rest ->
-        (* cheat and pretend they are types cf. PR#6650 *)
-        let id' = Ident.create_scoped ~scope (Ident.name id) in
-        refresh_bound_idents
-          (add_type id (Pident id') s)
-          (Sig_class(id', cd, rs, vis) :: sg)
-          rest
-    | Sig_class_type(id, ctd, rs, vis) :: rest ->
-        (* cheat and pretend they are types cf. PR#6650 *)
-        let id' = Ident.create_scoped ~scope (Ident.name id) in
-        refresh_bound_idents
-          (add_type id (Pident id') s)
-          (Sig_class_type(id', ctd, rs, vis) :: sg)
-          rest
-    | Sig_value(id, vd, vis) :: rest ->
-        let id' = Ident.create_local (Ident.name id) in
-        refresh_bound_idents s (Sig_value(id', vd, vis) :: sg) rest
-    | Sig_typext(id, ec, es, vis) :: rest ->
-        let id' = Ident.create_scoped ~scope (Ident.name id) in
-        refresh_bound_idents s (Sig_typext(id',ec,es,vis) :: sg) rest
-  in
-  let (sg', s') = refresh_bound_idents Subst.identity [] sg in
-  List.rev_map (Subst.signature_item (Rescope scope) s') sg'
-
 let enter_signature ~scope sg env =
-  let sg = refresh_signature ~scope sg in
+  let sg = Subst.signature (Rescope scope) Subst.identity sg in
   sg, add_signature sg env
 
 (* Open a signature path *)
