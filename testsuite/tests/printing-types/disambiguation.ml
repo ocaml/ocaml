60c54624a7f2f2e9ac2223fb0a537ed61b0567ac
Florian Angeletti <octa@polychoron.fr>
2019-04-12 18:31:45 +0200

    time-travelling test

---
+++ testsuite/tests/printing-types/disambiguation.ml
@@ -0,0 +1,46 @@
+(* TEST
+   * expect
+*)
+
+type 'a x = private [> `x] as 'a;;
+[%%expect {|
+Line 1:
+Error: Type declarations do not match:
+         type 'a x = private [> `x ] constraint 'a = 'a x/2
+       is not included in
+         type 'a x
+       Their constraints differ.
+       File "_none_", line 1:
+         Definition of type x/1
+       Line 1, characters 0-32:
+         Definition of type x/2
+|}, Principal{|
+Line 1:
+Error: Type declarations do not match:
+         type 'a x = private 'a constraint 'a = [> `x ]
+       is not included in
+         type 'a x
+       Their constraints differ.
+|}];;
+
+
+type int;;
+[%%expect {|
+type int
+|}];;
+
+let x = 0;;
+[%%expect {|
+val x : int/2 = 0
+|}];;
+
+
+type float;;
+[%%expect {|
+type float
+|}];;
+
+0.;;
+[%%expect {|
+- : float = 0.
+|}];;
