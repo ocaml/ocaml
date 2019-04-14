d4ef2eecea30cbf44029e87ec3600aca87f5bf25
Andrew Litteken <andrew.litteken@gmail.com>
2019-04-14 09:38:27 -0400

    Custom Exception Handlers at toplevel (#8594)
    fixes #7156 
    
    Two new functions exposed:
    
    ```ocaml
    val to_string_default: exn -> string
    (** [Printexc.to_string_default e] returns a string representation of the
        exception [e], ignoring all registered exception printers.
        @since 4.09
    *)
    
    val use_printers: exn -> string option
    (** [Printexc.use_printers e] returns [None] if there are no registered
        printers and [Some s] with else as the resulting string otherwise.
        @since 4.09
    *)
    ```

--- typing/oprint.ml
+++ typing/oprint.ml
@@ -717,7 +717,9 @@ let print_out_exception ppf exn outv =
   | Out_of_memory -> fprintf ppf "Out of memory during evaluation.@."
   | Stack_overflow ->
       fprintf ppf "Stack overflow during evaluation (looping recursion?).@."
-  | _ -> fprintf ppf "@[Exception:@ %a.@]@." !out_value outv
+  | _ -> match Printexc.use_printers exn with
+        | None -> fprintf ppf "@[Exception:@ %a.@]@." !out_value outv
+        | Some s -> fprintf ppf "@[Exception:@ %s@]@." s
 
 let rec print_items ppf =
   function
