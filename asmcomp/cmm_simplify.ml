187146fb856a4293b6e06069e38bc21287061b4f
Stefan Muenzel <smuenzel@janestreet.com>
2019-04-26 14:39:42 +0800

    reformat

--- asmcomp/cmm_simplify.ml
+++ asmcomp/cmm_simplify.ml
@@ -15,152 +15,125 @@ open Cmm
 
 let rec reduce_cmm = function
   | Cop
-      ( Ccmpi cmp_outer,
-        [ Cop
-            ( Caddi,
-              [ Cop
-                  ( Clsl,
-                    [ Cop (Ccmpi cmp_inner, [ left; right ], dbg)
-                    ; Cconst_int (1, _)
-                    ],
-                    _ )
-              ; Cconst_int (1, _)
-              ],
-              _ )
-        ; Cconst_int (1, _)
-        ],
-        _ ) ->
+      ( Ccmpi cmp_outer
+      , [ Cop
+            ( Caddi
+            , [ Cop
+                  ( Clsl
+                  , [ Cop (Ccmpi cmp_inner, [left; right], dbg)
+                    ; Cconst_int (1, _) ]
+                  , _ )
+              ; Cconst_int (1, _) ]
+            , _ )
+        ; Cconst_int (1, _) ]
+      , _ ) ->
       (* nested-compare
          Size: (4 ops, 9 heads) in -> (2 ops, 5 heads) out *)
       Cop
-        ( Ccmpi cmp_outer,
-          [ Cop (Ccmpi cmp_inner, [ left; right ], dbg); Cconst_int (0, dbg) ],
-          dbg )
+        ( Ccmpi cmp_outer
+        , [Cop (Ccmpi cmp_inner, [left; right], dbg); Cconst_int (0, dbg)]
+        , dbg )
       |> reduce_cmm
   | Cop
-      ( Ccmpi Ceq,
-        [ Cop (Ccmpi cmp_inner, [ left; right ], dbg); Cconst_int (1, _) ],
-        _ ) ->
+      ( Ccmpi Ceq
+      , [Cop (Ccmpi cmp_inner, [left; right], dbg); Cconst_int (1, _)]
+      , _ ) ->
       (* nested-equal-one
          Size: (2 ops, 5 heads) in -> (1 ops, 3 heads) out *)
-      Cop (Ccmpi cmp_inner, [ left; right ], dbg) |> reduce_cmm
+      Cop (Ccmpi cmp_inner, [left; right], dbg) |> reduce_cmm
   | Cop
-      ( Ccmpi Ceq,
-        [ Cop
-            ( Caddi,
-              [ Cop
-                  ( Clsl,
-                    [ Cop (Ccmpi cmp_inner, [ left; right ], dbg)
-                    ; Cconst_int (1, _)
-                    ],
-                    _ )
-              ; Cconst_int (1, _)
-              ],
-              _ )
-        ; Cconst_int (3, _)
-        ],
-        _ ) ->
+      ( Ccmpi Ceq
+      , [ Cop
+            ( Caddi
+            , [ Cop
+                  ( Clsl
+                  , [ Cop (Ccmpi cmp_inner, [left; right], dbg)
+                    ; Cconst_int (1, _) ]
+                  , _ )
+              ; Cconst_int (1, _) ]
+            , _ )
+        ; Cconst_int (3, _) ]
+      , _ ) ->
       (* nested-equal-three-tagged
          Size: (4 ops, 9 heads) in -> (1 ops, 3 heads) out *)
-      Cop (Ccmpi cmp_inner, [ left; right ], dbg) |> reduce_cmm
+      Cop (Ccmpi cmp_inner, [left; right], dbg) |> reduce_cmm
   | Cop
-      ( Ccmpi Cne,
-        [ Cop (Ccmpi cmp_inner, [ left; right ], dbg); Cconst_int (0, _) ],
-        _ ) ->
+      ( Ccmpi Cne
+      , [Cop (Ccmpi cmp_inner, [left; right], dbg); Cconst_int (0, _)]
+      , _ ) ->
       (* nested-nequal-zero
          Size: (2 ops, 5 heads) in -> (1 ops, 3 heads) out *)
-      Cop (Ccmpi cmp_inner, [ left; right ], dbg) |> reduce_cmm
+      Cop (Ccmpi cmp_inner, [left; right], dbg) |> reduce_cmm
   | Cop
-      ( Cand,
-        [ Cop
-            ( Caddi,
-              [ Cop (Clsl, [ left; Cconst_int (1, _) ], _)
-              ; Cconst_int (1, _)
-              ],
-              _ )
+      ( Cand
+      , [ Cop
+            ( Caddi
+            , [Cop (Clsl, [left; Cconst_int (1, _)], _); Cconst_int (1, _)]
+            , _ )
         ; Cop
-            ( Caddi,
-              [ Cop (Clsl, [ right; Cconst_int (1, _) ], _)
-              ; Cconst_int (1, _)
-              ],
-              _ )
-        ],
-        dbg_op ) ->
+            ( Caddi
+            , [Cop (Clsl, [right; Cconst_int (1, _)], _); Cconst_int (1, _)]
+            , _ ) ]
+      , dbg_op ) ->
       (* tag-and
          Size: (5 ops, 11 heads) in -> (3 ops, 7 heads) out *)
       Cop
-        ( Caddi,
-          [ Cop
-              ( Clsl,
-                [ Cop (Cand, [ left; right ], dbg_op);
-                  Cconst_int (1, dbg_op)
-                ],
-                dbg_op );
-            Cconst_int (1, dbg_op)
-          ],
-          dbg_op )
+        ( Caddi
+        , [ Cop
+              ( Clsl
+              , [Cop (Cand, [left; right], dbg_op); Cconst_int (1, dbg_op)]
+              , dbg_op )
+          ; Cconst_int (1, dbg_op) ]
+        , dbg_op )
       |> reduce_cmm
   | Cop
-      ( Cor,
-        [ Cop
-            ( Caddi,
-              [ Cop (Clsl, [ left; Cconst_int (1, _) ], _)
-              ; Cconst_int (1, _)
-              ],
-              _ )
+      ( Cor
+      , [ Cop
+            ( Caddi
+            , [Cop (Clsl, [left; Cconst_int (1, _)], _); Cconst_int (1, _)]
+            , _ )
         ; Cop
-            ( Caddi,
-              [ Cop (Clsl, [ right; Cconst_int (1, _) ], _)
-              ; Cconst_int (1, _)
-              ],
-              _ )
-        ],
-        dbg_op ) ->
+            ( Caddi
+            , [Cop (Clsl, [right; Cconst_int (1, _)], _); Cconst_int (1, _)]
+            , _ ) ]
+      , dbg_op ) ->
       (* tag-or
          Size: (5 ops, 11 heads) in -> (3 ops, 7 heads) out *)
       Cop
-        ( Caddi,
-          [ Cop
-              ( Clsl,
-                [ Cop (Cor, [ left; right ], dbg_op); Cconst_int (1, dbg_op) ],
-                dbg_op );
-            Cconst_int (1, dbg_op)
-          ],
-          dbg_op )
+        ( Caddi
+        , [ Cop
+              ( Clsl
+              , [Cop (Cor, [left; right], dbg_op); Cconst_int (1, dbg_op)]
+              , dbg_op )
+          ; Cconst_int (1, dbg_op) ]
+        , dbg_op )
       |> reduce_cmm
   | Cop
-      ( Cor,
-        [ Cop
-            ( Cxor,
-              [ Cop
-                  ( Caddi,
-                    [ Cop (Clsl, [ left; Cconst_int (1, _) ], _)
-                    ; Cconst_int (1, _)
-                    ],
-                    _ )
+      ( Cor
+      , [ Cop
+            ( Cxor
+            , [ Cop
+                  ( Caddi
+                  , [ Cop (Clsl, [left; Cconst_int (1, _)], _)
+                    ; Cconst_int (1, _) ]
+                  , _ )
               ; Cop
-                  ( Caddi,
-                    [ Cop (Clsl, [ right; Cconst_int (1, _) ], _)
-                    ; Cconst_int (1, _)
-                    ],
-                    _ )
-              ],
-              dbg_xor )
-        ; Cconst_int (1, _)
-        ],
-        _ ) ->
+                  ( Caddi
+                  , [ Cop (Clsl, [right; Cconst_int (1, _)], _)
+                    ; Cconst_int (1, _) ]
+                  , _ ) ]
+            , dbg_xor )
+        ; Cconst_int (1, _) ]
+      , _ ) ->
       (* tag-xor
          Size: (6 ops, 13 heads) in -> (3 ops, 7 heads) out *)
       Cop
-        ( Caddi,
-          [ Cop
-              ( Clsl,
-                [ Cop (Cxor, [ left; right ], dbg_xor);
-                  Cconst_int (1, dbg_xor)
-                ],
-                dbg_xor );
-            Cconst_int (1, dbg_xor)
-          ],
-          dbg_xor )
+        ( Caddi
+        , [ Cop
+              ( Clsl
+              , [Cop (Cxor, [left; right], dbg_xor); Cconst_int (1, dbg_xor)]
+              , dbg_xor )
+          ; Cconst_int (1, dbg_xor) ]
+        , dbg_xor )
       |> reduce_cmm
   | other -> other
