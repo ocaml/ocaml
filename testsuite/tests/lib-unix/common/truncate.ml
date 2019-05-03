61a1afff93c1222039dc3868ac480d062fe2d890
David Allsopp <david.allsopp@metastack.com>
2019-05-03 10:21:11 +0100

    Refactor test

--- testsuite/tests/lib-unix/common/truncate.ml
+++ testsuite/tests/lib-unix/common/truncate.ml
@@ -1,30 +1,30 @@
 (* TEST
 include unix
-files = "truncate.txt ftruncate.txt"
 *)
 
+let str = "Hello, OCaml!"
 let txt = "truncate.txt"
 
-let size s =
-  (Unix.stat s).Unix.st_size
+let test file openfile stat truncate delta close =
+  let () =
+    let c = open_out_bin file in
+    output_string c str;
+    close_out c
+  in
+  let size file =
+    (stat file).Unix.st_size
+  in
+  let file = openfile file in
+  Printf.printf "initial size: %d\n%!" (size file);
+  truncate file (size file - delta);
+  Printf.printf "new size: %d\n%!" (size file);
+  truncate file 0;
+  Printf.printf "final size: %d\n%!" (size file);
+  close file
 
 let () =
-  Printf.printf "initial size: %d\n%!" (size txt);
-  Unix.truncate txt (size txt - 2);
-  Printf.printf "new size: %d\n%!" (size txt);
-  Unix.truncate txt 0;
-  Printf.printf "final size: %d\n%!" (size txt)
-
-let ftxt = "ftruncate.txt"
-
-let size fd =
-  (Unix.fstat fd).Unix.st_size
+  test "truncate.txt" (fun x -> x) Unix.stat Unix.truncate 2 ignore
 
 let () =
-  let fd = Unix.openfile ftxt [O_RDWR] 0 in
-  Printf.printf "initial size: %d\n%!" (size fd);
-  Unix.ftruncate fd (size fd - 3);
-  Printf.printf "new size: %d\n%!" (size fd);
-  Unix.ftruncate fd 0;
-  Printf.printf "final size: %d\n%!" (size fd);
-  Unix.close fd
+  let open_it file = Unix.openfile file [O_RDWR] 0 in
+  test "ftruncate.txt" open_it Unix.fstat Unix.ftruncate 3 Unix.close
