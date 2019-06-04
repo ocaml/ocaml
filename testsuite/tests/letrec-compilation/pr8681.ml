495434a88243a71f79e544197adafa3d207a813c
Stephen Dolan <sdolan@janestreet.com>
2019-06-04 10:27:12 +0100

    Add more tests for #8681, #8699

--- testsuite/tests/letrec-compilation/pr8681.ml
+++ testsuite/tests/letrec-compilation/pr8681.ml
@@ -6,3 +6,44 @@ let rec h =
 
 let () = Gc.minor ()
 let () = ignore (h 10)
+
+let mooo x =
+  let rec h =
+    ignore (Sys.opaque_identity x);
+    let rec g n = h n; f n
+    and f n = if n >= 0 then g (n - 1) in
+    f
+  in
+  h
+
+let h = mooo 3
+let () = Gc.minor ()
+let () = ignore (h 10)
+
+
+let rec foo =
+  let rec f = function
+    | 0 -> 100
+    | n -> foo (n-1)
+  and g = function
+    | 0 -> 200
+    | n -> f (n-1) in
+  g
+
+let () = print_int (foo 2); print_newline ()
+let () = print_int (foo 7); print_newline ()
+
+
+let with_free_vars a b c =
+  let rec foo =
+    let rec f = function
+      | 0 -> 100 + a + b + c
+      | n -> foo (n-1)
+    and g = function
+      | 0 -> 200 + a + b + c
+      | n -> f (n-1) in
+    g in
+  foo
+
+let () = print_int (with_free_vars 1 2 3 2); print_newline ()
+let () = print_int (with_free_vars 1 2 3 7); print_newline ()
