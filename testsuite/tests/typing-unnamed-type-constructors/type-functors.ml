1c0b2e160000952a9fdb3a57ba057607b2dfc5e4
mrmr1993 <mr_1993@hotmail.co.uk>
2019-06-06 02:42:07 +0100

    Update tests and expected output

--- testsuite/tests/typing-unnamed-type-constructors/type-functors.ml
+++ testsuite/tests/typing-unnamed-type-constructors/type-functors.ml
@@ -3,10 +3,17 @@
 *)
 
 (* Type declarations using type functors. *)
+
 type 'a t = 'a ('a. 'a -> 'a)
 ;;
 [%%expect {|
 type 'a t = 'a ('a0. 'a0 -> 'a0)
+|}];;
+
+type ('a, 'b) u = ('a, 'b) ('c 'd. int -> int)
+;;
+[%%expect {|
+type ('a, 'b) u = ('a, 'b) ('c 'd. int -> int)
 |}];;
 
 let f (g : 'a t) (x : int) = g x
@@ -15,11 +22,20 @@ let f (g : 'a t) (x : int) = g x
 val f : int t -> int -> int = <fun>
 |}];;
 
-(* Type declarations referencing variants. *)
-type 'a variant = A | B of 'a
+let g (f : (bool, unit) u) (x : int) = f x
 ;;
 [%%expect {|
-type 'a variant = A | B of 'a
+val g : (bool, unit) u -> int -> int = <fun>
+|}];;
+
+
+
+(* Type declarations referencing variants. *)
+
+type 'a variant = A | B of 'a ('a. 'a)
+;;
+[%%expect {|
+type 'a variant = A | B of 'a ('a0. 'a0)
 |}];;
 
 type t_variant = int ('a. 'a variant)
@@ -34,9 +50,10 @@ let f (x : t_variant) =
   | B x -> x
 ;;
 [%%expect {|
-val f : t_variant -> int = <fun>
+val f : t_variant -> int ('a. 'a) = <fun>
 |}];;
 
+(* Expected failure in return type. *)
 let f (x : t_variant) =
   match x with
   | A -> true
@@ -46,11 +63,14 @@ let f (x : t_variant) =
 Line 4, characters 11-12:
 4 |   | B x -> x
                ^
-Error: This expression has type int but an expression was expected of type
-         bool
+Error: This expression has type int ('a. 'a) = int
+       but an expression was expected of type bool
 |}];;
 
+
+
 (* Type functors in record fields. *)
+
 type record = {
     a: 'a 'b. 'a ('c. 'c -> 'c) -> 'b;
     b: int ('c. 'c -> 'c)
@@ -94,24 +114,42 @@ Error: This expression has type 'a variant
        but an expression was expected of type int
 |}];;
 
+
+
 (* Naked type functors. *)
-let x (a : int ('a. 'a)) : int = a
+
+let w (a : int ('a. 'a)) : int = a
 ;;
 [%%expect {|
-val x : int ('a. 'a) -> int = <fun>
+val w : int ('a. 'a) -> int = <fun>
 |}];;
 
-let y (f : bool ('a. 'a -> 'a)) : bool = f true
+let x (f : bool ('a. 'a -> 'a)) : bool = f true
 ;;
 [%%expect {|
-val y : bool ('a. 'a -> 'a) -> bool = <fun>
+val x : bool ('a. 'a -> 'a) -> bool = <fun>
 |}];;
 
-let z : int ('a. 'a) = 15
+let y : int ('a. 'a) = 15
 ;;
 [%%expect {|
-val z : int ('a. 'a) = 15
+val y : int ('a. 'a) = 15
 |}];;
+
+(* Expected failure in return type. *)
+let z : int ('a. 'a) = true
+;;
+[%%expect {|
+Line 1, characters 23-27:
+1 | let z : int ('a. 'a) = true
+                           ^^^^
+Error: This expression has type bool but an expression was expected of type
+         int ('a. 'a) = int
+|}];;
+
+
+
+(* Functors as type parameters. *)
 
 let a : float ('a. 'a) variant = A
 ;;
@@ -139,5 +177,114 @@ Line 1, characters 34-38:
 1 | let d : int ('a. int) variant = B 15.0
                                       ^^^^
 Error: This expression has type float but an expression was expected of type
-         int ('a. int) = int
+         int ('a. int) ('a. 'a) = int
 |}];;
+
+
+
+(* Functors using newtypes. *)
+
+let e (type t) (x : int ('a. t)) (y : t) = x = y
+;;
+[%%expect {|
+val e : int ('a. 't) -> 't -> bool = <fun>
+|}];;
+
+
+
+(* Functors as module type overrides. *)
+
+module type S = sig
+  type t
+
+  val x : t
+end
+;;
+
+module T = struct
+  type t = int
+
+  let x = 15
+end
+;;
+[%%expect {|
+module type S = sig type t val x : t end
+module T : sig type t = int val x : int end
+|}];;
+
+include (T : S with type t := (bool, bool) ('a 'b. int))
+;;
+[%%expect {|
+val x : (bool, bool) ('a 'b. int) = 15
+|}];;
+
+
+
+(* Omitted parameter error. *)
+
+type missing_param = int ('a. 'b)
+;;
+
+[%%expect{|
+Line 1, characters 30-32:
+1 | type missing_param = int ('a. 'b)
+                                  ^^
+Error: The type variable 'b is unbound in this type declaration.
+|}]
+
+
+
+
+(* Occurrence checks. *)
+
+let f (x : int ('a. 'b)) (y : 'b) = x = y
+
+[%%expect{|
+val f : int ('a. 'b) -> 'b -> bool = <fun>
+|}]
+
+let f (x : int ('a. 'a * 'b)) (y : 'b) = x = y
+;;
+
+[%%expect{|
+Line 1, characters 45-46:
+1 | let f (x : int ('a. 'a * 'b)) (y : 'b) = x = y
+                                                 ^
+Error: This expression has type 'b but an expression was expected of type
+         int ('a. 'a * 'b) = int * 'b
+       The type variable 'b occurs inside int ('a. 'a * 'b)
+|}]
+
+let f : 'a 'b. 'a ('a. 'a) -> 'b -> 'a = fun x y -> x
+;;
+
+[%%expect{|
+val f : 'a ('a0. 'a0) -> 'b -> 'a = <fun>
+|}]
+
+let f : 'a 'b. 'a ('a. 'b) -> 'b -> 'b = fun x y -> x
+;;
+
+[%%expect{|
+val f : 'a ('a0. 'b) -> 'b -> 'b = <fun>
+|}, Principal{|
+Line 1, characters 45-46:
+1 | let f : 'a 'b. 'a ('a. 'b) -> 'b -> 'b = fun x y -> x
+                                                 ^
+Error: This pattern matches values of type 'c ('a. 'b) = 'b0
+       but a pattern was expected which matches values of type 'd
+       The universal variable 'b would escape its scope
+|}]
+
+let f : 'a 'b. int ('a. 'b) -> 'b -> 'b = fun x y -> x
+;;
+[%%expect{|
+val f : int ('a. 'b) -> 'b -> 'b = <fun>
+|}, Principal{|
+Line 1, characters 46-47:
+1 | let f : 'a 'b. int ('a. 'b) -> 'b -> 'b = fun x y -> x
+                                                  ^
+Error: This pattern matches values of type 'c ('a. 'b) = 'b0
+       but a pattern was expected which matches values of type 'd
+       The universal variable 'b would escape its scope
+|}]
