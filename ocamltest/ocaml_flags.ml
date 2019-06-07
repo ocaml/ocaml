a4c81de9921087a4871b832a8c05e4014aecd1d5
Gabriel Scherer <gabriel.scherer@gmail.com>
2019-06-07 14:52:42 +0200

    Merge pull request #8722 from shindere/fix-ocamldebug-test
    ocamltest: fix the ocamldebug test

--- ocamltest/ocaml_flags.ml
+++ ocamltest/ocaml_flags.ml
@@ -55,6 +55,7 @@ let toplevel_default_flags = "-noinit -no-version -noprompt"
 
 let ocamldebug_default_flags ocamlsrcdir =
   "-no-version -no-prompt -no-time -no-breakpoint-message " ^
+  ("-I " ^ (Ocaml_directories.stdlib ocamlsrcdir) ^ " ") ^
   ("-topdirs-path " ^ (Ocaml_directories.toplevel ocamlsrcdir))
 
 let ocamlobjinfo_default_flags = "-null-crc"
