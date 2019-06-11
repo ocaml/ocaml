d4af3292d169b724e0d8c774c6847f0afab95e22
Gabriel Scherer <gabriel.scherer@gmail.com>
2019-06-11 16:34:53 +0200

    matching: default_compat: compare with a reimplementation using specialize_default

--- lambda/matching.ml
+++ lambda/matching.ml
@@ -664,20 +664,41 @@ let cons_default matrix raise_num default =
   | _ -> (matrix, raise_num) :: default
 
 let default_compat p def =
-  List.fold_right
-    (fun (pss, i) r ->
-      let qss =
-        List.fold_right
-          (fun qs r ->
-            match qs with
-            | q :: rem when may_compat p q -> rem :: r
-            | _ -> r)
-          pss []
-      in
-      match qss with
-      | [] -> r
-      | _ -> (qss, i) :: r)
-    def []
+  let specialize_row qs =
+    match qs with
+    | q :: rem when may_compat p q -> Some rem
+    | _ -> None
+  in
+  List.filter_map
+    (fun (pss, i) ->
+      match List.filter_map specialize_row pss with
+      | [] -> None
+      | qss -> Some (qss, i))
+    def
+
+let rec optim_default = function
+  | [] -> []
+  | ([] :: _, i) :: _ -> [ ([ [] ], i) ]
+  | (pss, i) :: rem -> (pss, i) :: optim_default rem
+
+let other_default_compat p def =
+  let compat_matcher q rem =
+    if may_compat p q then
+      rem
+    else
+      raise NoMatch
+  in
+  specialize_default compat_matcher def
+
+let same_def (m1, i1) (m2, i2) =
+  i1 = i2 && List.for_all2 MayCompat.compats m1 m2
+
+let default_compat p def =
+  let v1 = default_compat p def in
+  let v2 = other_default_compat p def in
+  let v1_optim = optim_default v1 in
+  assert (List.for_all2 same_def v1_optim v2);
+  v1
 
 (* Or-pattern expansion, variables are a complication w.r.t. the article *)
 
