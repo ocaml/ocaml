0ef82b2f8c93a745804905f900a10ba666444fce
Gabriel Scherer <gabriel.scherer@gmail.com>
2019-07-23 16:43:59 +0200

    Merge pull request #8822 from gasche/backport-bytecode-dynlink-fix-in-4.08
    Backport #8818 (Dynlink/packing issue in bytecode) for 4.08+rc2
    
    (cherry picked from commit 101383a4cfeddfca6fedfc45740f24dbc961b72a)

---
+++ testsuite/tests/lib-dynlink-packed/loader.ml
@@ -0,0 +1,64 @@
+(* TEST
+
+include dynlink
+libraries = ""
+files = "a.ml b.ml loader.ml"
+
+* shared-libraries
+** setup-ocamlc.byte-build-env
+*** ocamlc.byte
+flags = "-for-pack Packed"
+module = "a.ml"
+*** ocamlc.byte
+flags = "-for-pack Packed"
+module = "b.ml"
+*** ocamlc.byte
+program = "packed.cmo"
+flags = "-pack"
+all_modules = "a.cmo b.cmo"
+*** ocamlc.byte
+program = "${test_build_directory}/loader.byte"
+flags = "-linkall"
+include ocamlcommon
+libraries += "dynlink"
+all_modules = "loader.ml"
+**** run
+arguments = "packed.cmo"
+exit_status = "0"
+***** check-program-output
+reference = "${test_source_directory}/byte.reference"
+
+** native-dynlink
+*** setup-ocamlopt.byte-build-env
+**** ocamlopt.byte
+flags = "-for-pack Packed"
+module = "a.ml"
+**** ocamlopt.byte
+flags = "-for-pack Packed"
+module = "b.ml"
+**** ocamlopt.byte
+program = "packed.cmx"
+flags = "-pack"
+all_modules = "a.cmx b.cmx"
+**** ocamlopt.byte
+program = "plugin.cmxs"
+flags = "-shared"
+all_modules = "packed.cmx"
+**** ocamlopt.byte
+program = "${test_build_directory}/loader.exe"
+flags = "-linkall"
+include ocamlcommon
+libraries += "dynlink"
+all_modules = "loader.ml"
+***** run
+arguments = "plugin.cmxs"
+exit_status = "0"
+****** check-program-output
+reference = "${test_source_directory}/native.reference"
+*)
+let () =
+  try
+    Dynlink.loadfile Sys.argv.(1)
+  with
+  | Dynlink.Error error ->
+    prerr_endline (Dynlink.error_message error)
