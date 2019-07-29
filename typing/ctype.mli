df9c34f4a6c9d770d2f30146b2585eefce3b1d68
Mekhrubon Turaev <mturaev@vdc-qws-i20570a.delacy.com>
2019-07-29 18:25:42 +0100

    forgot dot in output

--- typing/ctype.mli
+++ typing/ctype.mli
@@ -21,8 +21,8 @@ open Types
 module Unification_trace: sig
   (** Unification traces are used to explain unification errors
       when printing error messages *)
-  type position = First | Second
 
+  type position = First | Second
   type desc = { t: type_expr; expanded: type_expr option }
   type 'a diff = { got: 'a; expected: 'a}
 
