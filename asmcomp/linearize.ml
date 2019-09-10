357932d9c3f8894b3966ec4f9afdef60ccb93ba7
Greta Yorsh <gyorsh@janestreet.com>
2019-09-10 12:43:30 +0100

    Fix indentation

--- asmcomp/linearize.ml
+++ asmcomp/linearize.ml
@@ -134,132 +134,132 @@ let local_exit k =
 
 (* Linearize an instruction [i]: add it in front of the continuation [n] *)
 let linear i n contains_calls =
-let rec linear i n =
-  match i.Mach.desc with
-    Iend -> n
-  | Iop(Itailcall_ind _ | Itailcall_imm _ as op) ->
-      if not Config.spacetime then
-        copy_instr (Lop op) i (discard_dead_code n)
-      else
+  let rec linear i n =
+    match i.Mach.desc with
+      Iend -> n
+    | Iop(Itailcall_ind _ | Itailcall_imm _ as op) ->
+        if not Config.spacetime then
+          copy_instr (Lop op) i (discard_dead_code n)
+        else
+          copy_instr (Lop op) i (linear i.Mach.next n)
+    | Iop(Imove | Ireload | Ispill)
+      when i.Mach.arg.(0).loc = i.Mach.res.(0).loc ->
+        linear i.Mach.next n
+    | Iop op ->
         copy_instr (Lop op) i (linear i.Mach.next n)
-  | Iop(Imove | Ireload | Ispill)
-    when i.Mach.arg.(0).loc = i.Mach.res.(0).loc ->
-      linear i.Mach.next n
-  | Iop op ->
-      copy_instr (Lop op) i (linear i.Mach.next n)
-  | Ireturn ->
-      let n1 = copy_instr Lreturn i (discard_dead_code n) in
-      if contains_calls
-      then cons_instr Lreloadretaddr n1
-      else n1
-  | Iifthenelse(test, ifso, ifnot) ->
-      let n1 = linear i.Mach.next n in
-      begin match (ifso.Mach.desc, ifnot.Mach.desc, n1.desc) with
-        Iend, _, Lbranch lbl ->
-          copy_instr (Lcondbranch(test, lbl)) i (linear ifnot n1)
-      | _, Iend, Lbranch lbl ->
-          copy_instr (Lcondbranch(invert_test test, lbl)) i (linear ifso n1)
-      | Iexit nfail1, Iexit nfail2, _
-            when is_next_catch nfail1 && local_exit nfail2 ->
-          let lbl2 = find_exit_label nfail2 in
-          copy_instr
-            (Lcondbranch (invert_test test, lbl2)) i (linear ifso n1)
-      | Iexit nfail, _, _ when local_exit nfail ->
-          let n2 = linear ifnot n1
-          and lbl = find_exit_label nfail in
-          copy_instr (Lcondbranch(test, lbl)) i n2
-      | _,  Iexit nfail, _ when local_exit nfail ->
-          let n2 = linear ifso n1 in
-          let lbl = find_exit_label nfail in
-          copy_instr (Lcondbranch(invert_test test, lbl)) i n2
-      | Iend, _, _ ->
-          let (lbl_end, n2) = get_label n1 in
-          copy_instr (Lcondbranch(test, lbl_end)) i (linear ifnot n2)
-      | _,  Iend, _ ->
-          let (lbl_end, n2) = get_label n1 in
-          copy_instr (Lcondbranch(invert_test test, lbl_end)) i
-                     (linear ifso n2)
-      | _, _, _ ->
-        (* Should attempt branch prediction here *)
-          let (lbl_end, n2) = get_label n1 in
-          let (lbl_else, nelse) = get_label (linear ifnot n2) in
-          copy_instr (Lcondbranch(invert_test test, lbl_else)) i
-            (linear ifso (add_branch lbl_end nelse))
-      end
-  | Iswitch(index, cases) ->
-      let lbl_cases = Array.make (Array.length cases) 0 in
-      let (lbl_end, n1) = get_label(linear i.Mach.next n) in
-      let n2 = ref (discard_dead_code n1) in
-      for i = Array.length cases - 1 downto 0 do
-        let (lbl_case, ncase) =
-                get_label(linear cases.(i) (add_branch lbl_end !n2)) in
-        lbl_cases.(i) <- lbl_case;
-        n2 := discard_dead_code ncase
-      done;
-      (* Switches with 1 and 2 branches have been eliminated earlier.
-         Here, we do something for switches with 3 branches. *)
-      if Array.length index = 3 then begin
-        let fallthrough_lbl = check_label !n2 in
-        let find_label n =
-          let lbl = lbl_cases.(index.(n)) in
-          if lbl = fallthrough_lbl then None else Some lbl in
-        copy_instr (Lcondbranch3(find_label 0, find_label 1, find_label 2))
-                   i !n2
-      end else
-        copy_instr (Lswitch(Array.map (fun n -> lbl_cases.(n)) index)) i !n2
-  | Icatch(_rec_flag, handlers, body) ->
-      let (lbl_end, n1) = get_label(linear i.Mach.next n) in
-      (* CR mshinwell for pchambart:
-         1. rename "io"
-         2. Make sure the test cases cover the "Iend" cases too *)
-      let labels_at_entry_to_handlers = List.map (fun (_nfail, handler) ->
-          match handler.Mach.desc with
-          | Iend -> lbl_end
-          | _ -> Cmm.new_label ())
-          handlers in
-      let exit_label_add = List.map2
-          (fun (nfail, _) lbl -> (nfail, (lbl, !try_depth)))
-          handlers labels_at_entry_to_handlers in
-      let previous_exit_label = !exit_label in
-      exit_label := exit_label_add @ !exit_label;
-      let n2 = List.fold_left2 (fun n (_nfail, handler) lbl_handler ->
-          match handler.Mach.desc with
-          | Iend -> n
-          | _ -> cons_instr (Llabel lbl_handler)
-                   (linear handler (add_branch lbl_end n)))
-          n1 handlers labels_at_entry_to_handlers
-      in
-      let n3 = linear body (add_branch lbl_end n2) in
-      exit_label := previous_exit_label;
-      n3
-  | Iexit nfail ->
-      let lbl, t = find_exit_label_try_depth nfail in
-      assert (i.Mach.next.desc = Mach.Iend);
-      let delta_traps = !try_depth - t in
-      let n1 = adjust_trap_depth delta_traps n in
-      let rec loop i tt =
-        if t = tt then i
-        else loop (cons_instr Lpoptrap i) (tt - 1)
-      in
-      loop (add_branch lbl n1) !try_depth
-  | Itrywith(body, handler) ->
-      let (lbl_join, n1) = get_label (linear i.Mach.next n) in
-      let (lbl_handler, n2) =
-        get_label (cons_instr Lentertrap (linear handler n1))
-      in
-      incr try_depth;
-      assert (i.Mach.arg = [| |] || Config.spacetime);
-      let n3 = cons_instr (Lpushtrap { lbl_handler; })
-                 (linear body
-                    (cons_instr
-                       Lpoptrap
-                       (add_branch lbl_join n2))) in
-      decr try_depth;
-      n3
+    | Ireturn ->
+        let n1 = copy_instr Lreturn i (discard_dead_code n) in
+        if contains_calls
+        then cons_instr Lreloadretaddr n1
+        else n1
+    | Iifthenelse(test, ifso, ifnot) ->
+        let n1 = linear i.Mach.next n in
+        begin match (ifso.Mach.desc, ifnot.Mach.desc, n1.desc) with
+          Iend, _, Lbranch lbl ->
+            copy_instr (Lcondbranch(test, lbl)) i (linear ifnot n1)
+        | _, Iend, Lbranch lbl ->
+            copy_instr (Lcondbranch(invert_test test, lbl)) i (linear ifso n1)
+        | Iexit nfail1, Iexit nfail2, _
+              when is_next_catch nfail1 && local_exit nfail2 ->
+            let lbl2 = find_exit_label nfail2 in
+            copy_instr
+              (Lcondbranch (invert_test test, lbl2)) i (linear ifso n1)
+        | Iexit nfail, _, _ when local_exit nfail ->
+            let n2 = linear ifnot n1
+            and lbl = find_exit_label nfail in
+            copy_instr (Lcondbranch(test, lbl)) i n2
+        | _,  Iexit nfail, _ when local_exit nfail ->
+            let n2 = linear ifso n1 in
+            let lbl = find_exit_label nfail in
+            copy_instr (Lcondbranch(invert_test test, lbl)) i n2
+        | Iend, _, _ ->
+            let (lbl_end, n2) = get_label n1 in
+            copy_instr (Lcondbranch(test, lbl_end)) i (linear ifnot n2)
+        | _,  Iend, _ ->
+            let (lbl_end, n2) = get_label n1 in
+            copy_instr (Lcondbranch(invert_test test, lbl_end)) i
+                       (linear ifso n2)
+        | _, _, _ ->
+          (* Should attempt branch prediction here *)
+            let (lbl_end, n2) = get_label n1 in
+            let (lbl_else, nelse) = get_label (linear ifnot n2) in
+            copy_instr (Lcondbranch(invert_test test, lbl_else)) i
+              (linear ifso (add_branch lbl_end nelse))
+        end
+    | Iswitch(index, cases) ->
+        let lbl_cases = Array.make (Array.length cases) 0 in
+        let (lbl_end, n1) = get_label(linear i.Mach.next n) in
+        let n2 = ref (discard_dead_code n1) in
+        for i = Array.length cases - 1 downto 0 do
+          let (lbl_case, ncase) =
+                  get_label(linear cases.(i) (add_branch lbl_end !n2)) in
+          lbl_cases.(i) <- lbl_case;
+          n2 := discard_dead_code ncase
+        done;
+        (* Switches with 1 and 2 branches have been eliminated earlier.
+           Here, we do something for switches with 3 branches. *)
+        if Array.length index = 3 then begin
+          let fallthrough_lbl = check_label !n2 in
+          let find_label n =
+            let lbl = lbl_cases.(index.(n)) in
+            if lbl = fallthrough_lbl then None else Some lbl in
+          copy_instr (Lcondbranch3(find_label 0, find_label 1, find_label 2))
+                     i !n2
+        end else
+          copy_instr (Lswitch(Array.map (fun n -> lbl_cases.(n)) index)) i !n2
+    | Icatch(_rec_flag, handlers, body) ->
+        let (lbl_end, n1) = get_label(linear i.Mach.next n) in
+        (* CR mshinwell for pchambart:
+           1. rename "io"
+           2. Make sure the test cases cover the "Iend" cases too *)
+        let labels_at_entry_to_handlers = List.map (fun (_nfail, handler) ->
+            match handler.Mach.desc with
+            | Iend -> lbl_end
+            | _ -> Cmm.new_label ())
+            handlers in
+        let exit_label_add = List.map2
+            (fun (nfail, _) lbl -> (nfail, (lbl, !try_depth)))
+            handlers labels_at_entry_to_handlers in
+        let previous_exit_label = !exit_label in
+        exit_label := exit_label_add @ !exit_label;
+        let n2 = List.fold_left2 (fun n (_nfail, handler) lbl_handler ->
+            match handler.Mach.desc with
+            | Iend -> n
+            | _ -> cons_instr (Llabel lbl_handler)
+                     (linear handler (add_branch lbl_end n)))
+            n1 handlers labels_at_entry_to_handlers
+        in
+        let n3 = linear body (add_branch lbl_end n2) in
+        exit_label := previous_exit_label;
+        n3
+    | Iexit nfail ->
+        let lbl, t = find_exit_label_try_depth nfail in
+        assert (i.Mach.next.desc = Mach.Iend);
+        let delta_traps = !try_depth - t in
+        let n1 = adjust_trap_depth delta_traps n in
+        let rec loop i tt =
+          if t = tt then i
+          else loop (cons_instr Lpoptrap i) (tt - 1)
+        in
+        loop (add_branch lbl n1) !try_depth
+    | Itrywith(body, handler) ->
+        let (lbl_join, n1) = get_label (linear i.Mach.next n) in
+        let (lbl_handler, n2) =
+          get_label (cons_instr Lentertrap (linear handler n1))
+        in
+        incr try_depth;
+        assert (i.Mach.arg = [| |] || Config.spacetime);
+        let n3 = cons_instr (Lpushtrap { lbl_handler; })
+                   (linear body
+                      (cons_instr
+                         Lpoptrap
+                         (add_branch lbl_join n2))) in
+        decr try_depth;
+        n3
 
-  | Iraise k ->
-      copy_instr (Lraise k) i (discard_dead_code n)
-in linear i n
+    | Iraise k ->
+        copy_instr (Lraise k) i (discard_dead_code n)
+  in linear i n
 
 let add_prologue first_insn prologue_required =
   (* The prologue needs to come after any [Iname_for_debugger] operations that
