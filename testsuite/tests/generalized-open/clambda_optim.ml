07946aa589290599801156585070341b33865604
Thomas Refis <thomas.refis@gmail.com>
2019-09-16 13:37:04 +0100

    testsuite: show compilation failure for open struct

---
+++ testsuite/tests/generalized-open/clambda_optim.ml
@@ -0,0 +1,16 @@
+(* TEST
+
+compile_only = "true"
+
+* no-flambda
+** setup-ocamlopt.byte-build-env
+*** ocamlopt.byte
+ocamlopt_byte_exit_status = "2"
+**** check-ocamlopt.byte-output
+
+*)
+
+module Stable = struct
+  open struct module V0 = struct module U = struct end end end
+  module V0 = V0.U
+end
