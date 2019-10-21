a6fa07e7120fb348ec03cb8d75397715b25e20d2
Jacques-Henri Jourdan <jacques-henri.jourdan@normalesup.org>
2019-10-21 13:55:40 +0200

    Fix tests/bactacktrace/callstack.ml by changing the order of the content of that file.

--- testsuite/tests/backtrace/callstack.ml
+++ testsuite/tests/backtrace/callstack.ml
@@ -4,21 +4,29 @@
    include systhreads
    compare_programs = "false"
    ** no-flambda
-   reference = "${test_source_directory}/callstack.reference"
    *** native
    *** bytecode
 *)
+
 let[@inline never] f0 () =
   Printexc.print_raw_backtrace stdout (Printexc.get_callstack 100); ()
 let[@inline never] f1 () = f0 (); ()
 let[@inline never] f2 () = f1 (); ()
 let[@inline never] f3 () = f2 (); ()
+
 let () = Printf.printf "main thread:\n"
 let () = f3 ()
-let () = Printf.printf "new thread:\n"
-let () = Thread.join (Thread.create f3 ())
 
+let () = Printf.printf "from finalizer:\n"
 let () =
   Gc.finalise (fun _ -> f0 ()) [|1|];
   Gc.full_major ();
   ()
+
+(* We run this last, because the initialization of the thread library
+   starts the "tick thread", which periodically send a signal for
+   thread preemption. If the preempion occurs exactly when the
+   finalizer above runs, then a new row for [Thread.yield] appears in
+   the callstack, which breaks the test. *)
+let () = Printf.printf "new thread:\n"
+let () = Thread.join (Thread.create f3 ())
