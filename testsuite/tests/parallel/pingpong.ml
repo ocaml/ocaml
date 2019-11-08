a8d825f93ae2a6fa2557f332f05135f004390743
Tom Kelly <ctk21@cl.cam.ac.uk>
2019-11-08 15:01:01 +0000

    Remove assert that is no longer valid with stw minor gc

--- testsuite/tests/parallel/pingpong.ml
+++ testsuite/tests/parallel/pingpong.ml
@@ -8,7 +8,6 @@ let rec even lim put =
   | Some n when n = lim -> ()
   | (Some n) when n mod 2 == 0 ->
      let next = Some (n + 1) in
-     assert (not (Obj.is_shared (Obj.repr next)));
      put next;
      even lim put
   | _ -> let _ = [!r] in even lim put
@@ -18,7 +17,6 @@ let rec odd lim put =
   | Some n when n = lim -> ()
   | (Some n) when n mod 2 == 1 ->
      let next = Some (n + 1) in
-     assert (not (Obj.is_shared (Obj.repr next)));
      put next;
      odd lim put
   | _ -> let _ = [!r] in odd lim put
