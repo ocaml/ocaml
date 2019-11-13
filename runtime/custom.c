c1aef2856c010fb8555b0658cab2cb96c3831d29
Markus Mottl <markus.mottl@gmail.com>
2019-11-13 18:08:53 -0500

    Improved readability of caml_alloc_custom_mem

--- runtime/custom.c
+++ runtime/custom.c
@@ -89,12 +89,11 @@ CAMLexport value caml_alloc_custom_mem(struct custom_operations * ops,
 {
   mlsize_t mem_minor =
     mem < caml_custom_minor_max_bsz ? mem : caml_custom_minor_max_bsz;
-  return alloc_custom_gen (ops, bsz, mem,
-                           Bsize_wsize (Caml_state->stat_heap_wsz) / 150
-                           * caml_custom_major_ratio,
-                           mem_minor,
-                           Bsize_wsize (Caml_state->minor_heap_wsz) / 100
-                           * caml_custom_minor_ratio);
+  mlsize_t max_major =
+    Bsize_wsize (Caml_state->stat_heap_wsz) / 150 * caml_custom_major_ratio;
+  mlsize_t max_minor =
+    Bsize_wsize (Caml_state->minor_heap_wsz) / 100 * caml_custom_minor_ratio;
+  return alloc_custom_gen (ops, bsz, mem, max_major, mem_minor, max_minor);
 }
 
 struct custom_operations_list {
