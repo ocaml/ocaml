60bbe8ef17b1d4002e34cdb8cd29424655cd537d
Armaël Guéneau <armael.gueneau@ens-lyon.fr>
2019-11-14 01:02:59 +0100

    Add a test for multiline errors involving tabs

--- testsuite/tests/messages/highlight_tabs.ml
+++ testsuite/tests/messages/highlight_tabs.ml
@@ -11,3 +11,19 @@ Line 1, characters 10-13:
 Error: Unbound value abc
 Hint: Did you mean abs?
 |}];;
+
+
+module M : sig type t end =
+		struct
+		  type u
+		end
+;;
+[%%expect{|
+Lines 2-4, characters 2-5:
+2 | ..struct
+3 | 		  type u
+4 | 		end
+Error: Signature mismatch:
+       Modules do not match: sig type u end is not included in sig type t end
+       The type `t' is required but not provided
+|}];;
