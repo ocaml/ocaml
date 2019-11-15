3a0385ec83484618a1b01e95d6e4521f51a28290
Gabriel Scherer <gabriel.scherer@gmail.com>
2019-11-15 13:47:19 +0100

    Merge pull request #9120 from mmottl/fix-minor-ratio-bug
    Fix GC ratio multiplier bug
    
    (cherry picked from commit fa3dfaa6a86efd9545c1126a31b66b83a2127128)

--- runtime/custom.c
+++ runtime/custom.c
@@ -90,12 +90,20 @@ CAMLexport value caml_alloc_custom_mem(struct custom_operations * ops,
 {
   mlsize_t mem_minor =
     mem < caml_custom_minor_max_bsz ? mem : caml_custom_minor_max_bsz;
-  return alloc_custom_gen (ops, bsz, mem,
-                           Bsize_wsize (caml_stat_heap_wsz) / 150
-                           * caml_custom_major_ratio,
-                           mem_minor,
-                           Bsize_wsize (caml_minor_heap_wsz) / 100
-                           * caml_custom_major_ratio);
+  mlsize_t max_major =
+    /* The major ratio is a percentage relative to the major heap size.
+       A complete GC cycle will be done every time 2/3 of that much memory
+       is allocated for blocks in the major heap.  Assuming constant
+       allocation and deallocation rates, this means there are at most
+       [M/100 * major-heap-size] bytes of floating garbage at any time.
+       The reason for a factor of 2/3 (or 1.5) is, roughly speaking, because
+       the major GC takes 1.5 cycles (previous cycle + marking phase) before
+       it starts to deallocate dead blocks allocated during the previous cycle.
+       [heap_size / 150] is really [heap_size * (2/3) / 100] (but faster). */
+    Bsize_wsize (caml_stat_heap_wsz) / 150 * caml_custom_major_ratio;
+  mlsize_t max_minor =
+    Bsize_wsize (caml_minor_heap_wsz) / 100 * caml_custom_minor_ratio;
+  return alloc_custom_gen (ops, bsz, mem, max_major, mem_minor, max_minor);
 }
 
 struct custom_operations_list {
