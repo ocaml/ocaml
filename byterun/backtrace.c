55164ab7dacb0e4a67f7b9df3db54b8d2c815719
Xavier Leroy <xavier.leroy@college-de-france.fr>
2019-12-12 16:41:17 +0100

    Avoid duplicate definitions of "common" global variables
    The variables are caml_debug_info and caml_atom_table.
    The multiple definitions look like a cut-and-paste error.
    They cause problems with C compilers that don't follow the "common" model.
    
    Fixes: #9144
    (cherry picked from commit 53327d777ddcacf829c77c0bd3569a8ade2c72b8)

--- byterun/backtrace.c
+++ byterun/backtrace.c
@@ -25,9 +25,6 @@
 #include "caml/backtrace_prim.h"
 #include "caml/fail.h"
 
-/* The table of debug information fragments */
-struct ext_table caml_debug_info;
-
 CAMLexport int caml_backtrace_active = 0;
 CAMLexport int caml_backtrace_pos = 0;
 CAMLexport backtrace_slot * caml_backtrace_buffer = NULL;
