9af44b2dd77ec26ad2dcb43256f2f34fb5c6cccd
Sébastien Hinderer <Sebastien.Hinderer@inria.fr>
2019-12-17 17:41:04 +0100

    Merge pull request #9181 from shindere/fix-objinfo
    Make ocamlobjinfo work on .cmxs files on Windows
    
    (cherry picked from commit b8e6e82063344168fc0fbf636ade591a13eee042)

--- tools/objinfo_helper.c
+++ tools/objinfo_helper.c
@@ -18,6 +18,7 @@
 #ifdef HAS_LIBBFD
 #include <stdlib.h>
 #include <string.h>
+#include <stdarg.h>
 
 // PACKAGE: protect against binutils change
 //   https://sourceware.org/bugzilla/show_bug.cgi?id=14243
@@ -27,66 +28,113 @@
 
 #define plugin_header_sym (symbol_prefix "caml_plugin_header")
 
+/* We need to refer to a few functions of the BFD library that are */
+/* actually defined as macros. We thus define equivalent */
+/* functions below */
+
+long get_static_symtab_upper_bound(bfd *fd)
+{
+  return bfd_get_symtab_upper_bound(fd);
+}
+
+long get_dynamic_symtab_upper_bound(bfd *fd)
+{
+  return bfd_get_dynamic_symtab_upper_bound(fd);
+}
+
+long canonicalize_static_symtab(bfd * fd, asymbol **symbolTable)
+{
+  return bfd_canonicalize_symtab(fd, symbolTable);
+}
+
+long canonicalize_dynamic_symtab(bfd * fd, asymbol **symbolTable)
+{
+  return bfd_canonicalize_dynamic_symtab(fd, symbolTable);
+}
+
+typedef struct {
+  long (*get_upper_bound)(bfd *);
+  long (*canonicalize)(bfd *, asymbol **);
+} symTable_ops;
+
+symTable_ops staticSymTable_ops = {
+  &get_static_symtab_upper_bound,
+  &canonicalize_static_symtab
+};
+
+symTable_ops dynamicSymTable_ops = {
+  &get_dynamic_symtab_upper_bound,
+  &canonicalize_dynamic_symtab
+};
+
+/* Print an error message and exit */
+static void error(bfd *fd, char *msg, ...)
+{
+  va_list ap;
+  va_start(ap, msg);
+  vfprintf (stderr, msg, ap);
+  va_end(ap);
+  fprintf(stderr, "\n");
+  if (fd!=NULL) bfd_close(fd);
+  exit(2);
+}
+
+/* Look for plugin_header_sym in the specified symbol table */
+/* Return its address, -1 if not found */
+long lookup(bfd* fd, symTable_ops *ops)
+{
+  long st_size;
+  asymbol ** symbol_table;
+  long sym_count, i;
+
+  st_size = ops->get_upper_bound (fd);
+  if (st_size <= 0) return -1;
+
+  symbol_table = malloc(st_size);
+  if (! symbol_table)
+    error(fd, "Error: out of memory");
+
+  sym_count = ops->canonicalize (fd, symbol_table);
+
+  for (i = 0; i < sym_count; i++) {
+    if (strcmp(symbol_table[i]->name, plugin_header_sym) == 0)
+      return symbol_table[i]->value;
+  }
+  return -1;
+}
+
 int main(int argc, char ** argv)
 {
   bfd *fd;
   asection *sec;
   file_ptr offset;
-  long st_size;
-  asymbol ** symbol_table;
-  long sym_count, i;
+  long value;
 
-  if (argc != 2) {
-    fprintf(stderr, "Usage: objinfo_helper <dynamic library>\n");
-    return 2;
-  }
+  if (argc != 2)
+    error(NULL, "Usage: objinfo_helper <dynamic library>");
 
   fd = bfd_openr(argv[1], "default");
-  if (!fd) {
-    fprintf(stderr, "Error opening file %s\n", argv[1]);
-    return 2;
-  }
-  if (! bfd_check_format (fd, bfd_object)) {
-    fprintf(stderr, "Error: wrong format\n");
-    bfd_close(fd);
-    return 2;
-  }
+  if (!fd)
+    error(NULL, "Error opening file %s", argv[1]);
+  if (! bfd_check_format (fd, bfd_object))
+    error(fd, "Error: wrong format");
 
   sec = bfd_get_section_by_name(fd, ".data");
-  if (! sec) {
-    fprintf(stderr, "Error: section .data not found\n");
-    bfd_close(fd);
-    return 2;
-  }
+  if (! sec)
+    error(fd, "Error: section .data not found");
 
   offset = sec->filepos;
-  st_size = bfd_get_dynamic_symtab_upper_bound (fd);
-  if (st_size <= 0) {
-    fprintf(stderr, "Error: size of section .data unknown\n");
-    bfd_close(fd);
-    return 2;
-  }
 
-  symbol_table = malloc(st_size);
-  if (! symbol_table) {
-    fprintf(stderr, "Error: out of memory\n");
-    bfd_close(fd);
-    return 2;
-  }
+  value = lookup(fd, &dynamicSymTable_ops);
 
-  sym_count = bfd_canonicalize_dynamic_symtab (fd, symbol_table);
-
-  for (i = 0; i < sym_count; i++) {
-    if (strcmp(symbol_table[i]->name, plugin_header_sym) == 0) {
-      printf("%ld\n", (long) (offset + symbol_table[i]->value));
-      bfd_close(fd);
-      return 0;
-    }
-  }
-
-  fprintf(stderr, "Error: missing symbol %s\n", plugin_header_sym);
+  if (value == -1)
+    value = lookup(fd, &staticSymTable_ops);
   bfd_close(fd);
-  return 2;
+
+  if (value == -1)
+    error(NULL, "Error: missing symbol %s", plugin_header_sym);
+
+  printf("%ld\n", (long) offset + value);
 }
 
 #else
