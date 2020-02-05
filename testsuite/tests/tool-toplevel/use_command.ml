4460736981c360f9b65af135106ccbae26b16950
Jeremie Dimino <jeremie@dimino.org>
2020-02-05 21:22:36 +0100

    fix windows
    Signed-off-by: Jeremie Dimino <jeremie@dimino.org>

--- testsuite/tests/tool-toplevel/use_command.ml
+++ testsuite/tests/tool-toplevel/use_command.ml
@@ -3,7 +3,7 @@
 *)
 
 (* Test a success case *)
-#use_output {|echo "let x = 42"|}
+#use_output {|echo let x = 42|}
 [%%expect {|
 val x : int = 42
 |}];;
@@ -15,7 +15,7 @@ Command exited with code 1.
 |}];;
 
 (* When the code is invalid *)
-#use_output {|echo "1 :: x"|}
+#use_output {|echo 1 :: x|}
 [%%expect {|
 File "(command-output)", line 1, characters 5-6:
 1 | 1 :: x
