9e862639c60f040dab1e036fa70507958a8252d5
Stefan Muenzel <smuenzel@janestreet.com>
2020-02-17 16:01:41 +0800

    Add a test showing that the exhaustiveness check doesn't work with the empty variant

--- testsuite/tests/typing-misc/empty_variant.ml
+++ testsuite/tests/typing-misc/empty_variant.ml
@@ -29,3 +29,31 @@ let f : t option -> int = function None -> 3
 [%%expect{|
 val f : t option -> int = <fun>
 |}]
+
+
+type nothing = |
+type ('a, 'b, 'c) t = | A of 'a | B of 'b | C of 'c
+module Runner : sig
+  val ac : f:((unit, _, unit) t -> unit) -> unit
+end = struct
+  let ac ~f =
+    f (A ());
+    f (C ());
+  ;;
+end
+
+let f () =
+  Runner.ac
+    ~f:(fun (abc : (_,nothing,_) t) ->
+      let value =
+        match abc with
+        | A _ -> 1
+      in
+      Printf.printf "%i\n" value
+    )
+[%%expect{|
+type nothing = |
+type ('a, 'b, 'c) t = A of 'a | B of 'b | C of 'c
+module Runner : sig val ac : f:((unit, 'a, unit) t -> unit) -> unit end
+val f : unit -> unit = <fun>
+|}]
