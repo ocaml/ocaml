c9e4a86bdbc9fbb96de12af1ac5d38b359db2b08
Tom Kelly <ctk21@cl.cam.ac.uk>
2020-02-28 17:20:06 +0000

    Fix caml_finish_sweeping

--- byterun/major_gc.c
+++ byterun/major_gc.c
@@ -1314,15 +1314,19 @@ void caml_finish_marking () {
 }
 
 void caml_finish_sweeping () {
-  if (!Caml_state->sweeping_done) {
-    caml_ev_begin("major_gc/finish_sweeping");
-    while (caml_sweep(Caml_state->shared_heap, 10) <= 0) {
-      caml_handle_incoming_interrupts();
+  if (Caml_state->sweeping_done) return;
+  caml_ev_begin("major_gc/finish_sweeping");
+  while (!Caml_state->sweeping_done) {
+    if (caml_sweep(Caml_state->shared_heap, 10) > 0) {
+      /* just finished sweeping */
+      CAMLassert(Caml_state->sweeping_done == 0);
+      Caml_state->sweeping_done = 1;
+      atomic_fetch_add_verify_ge0(&num_domains_to_sweep, -1);
+      break;
     }
-    Caml_state->sweeping_done = 1;
-    atomic_fetch_add_verify_ge0(&num_domains_to_sweep, -1);
-    caml_ev_end("major_gc/finish_sweeping");
+    caml_handle_incoming_interrupts();
   }
+  caml_ev_end("major_gc/finish_sweeping");
 }
 
 static struct pool* find_pool_to_rescan()
