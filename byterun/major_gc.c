634f1536ce051049e6c52c43123465981d46cd1f
Sadiq Jaffer <sadiq@toao.com>
2020-03-01 15:17:39 +0000

    put the phase change event in the actual phase change code

--- byterun/major_gc.c
+++ byterun/major_gc.c
@@ -1038,6 +1038,7 @@ static int is_complete_phase_sweep_ephe (struct domain *d)
 static void try_complete_gc_phase (struct domain* domain, void* unused,
                                    int participating_count, struct domain** participating)
 {
+  caml_ev_begin("major_gc/phase_change");
   barrier_status b;
 
   b = caml_global_barrier_begin ();
@@ -1050,6 +1051,7 @@ static void try_complete_gc_phase (struct domain* domain, void* unused,
   }
   caml_global_barrier_end(b);
   domain->state->opportunistic_work = 0;
+  caml_ev_end("major_gc/phase_change");
 }
 
 #define Chunk_size 0x4000
@@ -1215,13 +1217,11 @@ mark_again:
     /* Complete GC phase */
     if (is_complete_phase_sweep_and_mark_main(d) ||
         is_complete_phase_mark_final (d)) {
-      caml_ev_begin("major_gc/phase_change");
       if (barrier_participants) {
         try_complete_gc_phase (d, (void*)0, participant_count, barrier_participants);
       } else {
         caml_try_run_on_all_domains (&try_complete_gc_phase, 0, 0, 0);
       }
-      caml_ev_end("major_gc/phase_change");
       if (budget > 0) goto mark_again;
     }
   }
