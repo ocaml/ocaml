f435fd9a6203fc7b05b3e6652dda8181c04c9c48
KC Sivaramakrishnan <kc@kcsrk.info>
2020-04-22 14:20:22 +0530

    Added lazy tests

---
+++ testsuite/tests/lazy/lazy4.ml
@@ -0,0 +1,12 @@
+let r = ref None
+
+let f () =
+  match !r with
+  | Some l -> Lazy.force l
+  | None -> ()
+
+let l = Lazy.from_fun f
+let _ = r := Some l
+let _ =
+  try Lazy.force l
+  with Lazy.Undefined -> print_endline "Undefined"
