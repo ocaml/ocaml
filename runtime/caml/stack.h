8f3833c4d0ef656c826359f4137c1eb3d46ea0ef
Nicolás Ojeda Bär <n.oje.bar@gmail.com>
2020-04-24 16:04:50 +0200

    Add RISC-V native-code backend (#9441)
    This is a port of ocamlopt for the RISC-V processor in 64-bit mode.

--- runtime/caml/stack.h
+++ runtime/caml/stack.h
@@ -70,6 +70,11 @@
 #define Callback_link(sp) ((struct caml_context *)((sp) + 16))
 #endif
 
+#ifdef TARGET_riscv
+#define Saved_return_address(sp) *((intnat *)((sp) - 8))
+#define Callback_link(sp) ((struct caml_context *)((sp) + 16))
+#endif
+
 /* Structure of OCaml callback contexts */
 
 struct caml_context {
