e20736dda07a3c530c8c79afb3440bfc954e93e1
Nicolás Ojeda Bär <n.oje.bar@gmail.com>
2020-04-30 18:26:24 +0200

    unboxed-primitive-args: test does not require unix (#9517)

--- testsuite/tests/unboxed-primitive-args/test.ml
+++ testsuite/tests/unboxed-primitive-args/test.ml
@@ -1,22 +1,19 @@
 (* TEST
 
-* hasunix
-include unix
-
 files = "common.mli common.ml test_common.c test_common.h"
 
-** setup-ocamlopt.byte-build-env
-*** ocaml
+* setup-ocamlopt.byte-build-env
+** ocaml
 test_file = "${test_source_directory}/gen_test.ml"
 ocaml_script_as_argument = "true"
 arguments = "c"
 compiler_output = "stubs.c"
-**** ocaml
+*** ocaml
 arguments = "ml"
 compiler_output = "main.ml"
-***** ocamlopt.byte
+**** ocamlopt.byte
 all_modules = "test_common.c stubs.c common.mli common.ml main.ml"
-****** run
-******* check-program-output
+***** run
+****** check-program-output
 
 *)
