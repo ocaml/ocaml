9355b4e8f57fe19087046e67ef22113440608ad4
KC Sivaramakrishnan <kc@kcsrk.info>
2020-05-02 22:17:16 +0530

    Initialise in caml_obj_block / Obj.new_block only when necessary  (#9513)
    caml_alloc returns initialised blocks for tag < No_scan_tag. Otherwise,
    initialise the blocks as necessary.
    
    For Abtract_tag, Double_tag and Double_array_tag, the initial contents
    are irrelevant.
    
    Uninitialised Custom_tag objects are difficult to use correctly. Hence,
    reject custom block allocations through Obj.new_block.
    
    For String_tag, the last byte encodes the string length. Hence, reject
    zero-length string objects. Initialise the last byte which encodes the
    length to ensure non-negative lengths for uninitialised strings.

--- runtime/obj.c
+++ runtime/obj.c
@@ -84,15 +84,40 @@ CAMLprim value caml_obj_make_forward (value blk, value fwd)
 CAMLprim value caml_obj_block(value tag, value size)
 {
   value res;
-  mlsize_t sz, i;
+  mlsize_t sz;
   tag_t tg;
 
   sz = Long_val(size);
   tg = Long_val(tag);
-  if (sz == 0) return Atom(tg);
+
+  /* It is difficult to correctly use custom objects allocated through
+   * [Obj.new_block]. The first field of a custom object must contain a valid
+   * pointer to a block of custom operations. Without initialisation, hashing,
+   * finalising or serialising this custom object will lead to crashes. See
+   * GPR#9513 for more details.
+   */
+  if (tg == Custom_tag)
+    caml_invalid_argument ("Obj.new_block");
+
+  /* When [tg < No_scan_tag], [caml_alloc] returns an object whose fields are
+   * initialised to [Val_unit]. Otherwise, the fields are uninitialised. We aim
+   * to avoid inconsistent states in other cases.
+   *
+   * For [Abstract_tag], [Double_tag] and [Double_array_tag], the initial
+   * content is irrelevant. [Custom_tag] objects are disallowed.
+   *
+   * For [String_tag], the initial contents do no matter. However, the length
+   * of the string is encoded using the last byte of the block. For this
+   * reason, the blocks with [String_tag] cannot be of size [0]. We initialise
+   * the last byte to [0] such that the length returned by [String.length] and
+   * [Bytes.length] is non-negative number.
+   */
   res = caml_alloc(sz, tg);
-  for (i = 0; i < sz; i++)
-    Field(res, i) = Val_long(0);
+
+  if (tg == String_tag) {
+    if (sz == 0) caml_invalid_argument ("Obj.new_block");
+    Field (res, sz - 1) = 0;
+  }
 
   return res;
 }
