d51d3e86436f402d8b8d7f4f893d1614c1aff7ae
Christophe Troestler <christophe.Troestler@umons.ac.be>
2020-05-06 11:44:13 +0200

    Update stdlib/float.mli
    Co-authored-by: David Allsopp <david.allsopp@metastack.com>

--- stdlib/float.mli
+++ stdlib/float.mli
@@ -25,8 +25,7 @@
     for [0.0 /. 0.0].  These special numbers then propagate through
     floating-point computations as expected: for instance,
     [1.0 /. infinity] is [0.0], basic arithmetic operations
-    ([+.], [-.], [*.], [/.]) with [nan] as argument returns [nan] as
-    result,...
+    ([+.], [-.], [*.], [/.]) with [nan] as an argument return [nan], ...
 
     @since 4.07.0
 *)
