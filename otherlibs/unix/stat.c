233457d2711aa2c0b88398c13a19f2f4d4df5713
Xavier Leroy <xavierleroy@users.noreply.github.com>
2020-05-13 18:43:46 +0200

    Protect against bad rounding of mtime / atime / ctime stats (#9505)
    Some file systems maintain time stamps with sub-second resolution, up
    to nanosecond resolution.  When converting from a "(seconds, nanoseconds)"
    timestamp to a floating-point timestamp, rounding to nearest can produce
    "seconds + 1" as a result, i.e. the integer part of the FP timestamp
    is not equal to "seconds".  As described in #9490, this is a problem
    in some cases.
    
    This commit implements a more careful conversion of "(seconds,
    nanoseconds)" pairs to FP timestamps so that the integer part of the
    FP result is always "seconds".
    
    Both the otherlibs/unix and the otherlibs/win32unix implementations are affected
    and corrected.
    
    Closes: #9490
    (cherry picked from commit c17f6c29569b5c5b1d3bfabb36ae030ddeaf12d9)

--- otherlibs/unix/stat.c
+++ otherlibs/unix/stat.c
@@ -16,6 +16,7 @@
 #define CAML_INTERNALS
 
 #include <errno.h>
+#include <math.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <caml/mlvalues.h>
@@ -47,18 +48,36 @@ static int file_kind_table[] = {
   S_IFREG, S_IFDIR, S_IFCHR, S_IFBLK, S_IFLNK, S_IFIFO, S_IFSOCK
 };
 
+/* Transform a (seconds, nanoseconds) time stamp (in the style of
+   struct timespec) to a number of seconds in floating-point.
+   Make sure the integer part of the result is always equal to [seconds]
+   (issue #9490). */
+
+static double stat_timestamp(time_t sec, long nsec)
+{
+  /* The conversion of sec to FP is exact for the foreseeable future.
+     (It starts rounding when sec > 2^53, i.e. in 285 million years.) */
+  double s = (double) sec;
+  /* The conversion of nsec to fraction of seconds can round.
+     Still, we have 0 <= n < 1.0. */
+  double n = (double) nsec / 1e9;
+  /* The sum s + n can round up, hence s <= t + <= s + 1.0 */
+  double t = s + n;
+  /* Detect the "round up to s + 1" case and decrease t so that
+     its integer part is s. */
+  if (t == s + 1.0) t = nextafter(t, s);
+  return t;
+}
+
 static value stat_aux(int use_64, struct stat *buf)
 {
   CAMLparam0();
   CAMLlocal5(atime, mtime, ctime, offset, v);
 
   #include "nanosecond_stat.h"
-  atime = caml_copy_double((double) buf->st_atime
-                           + (NSEC(buf, a) / 1000000000.0));
-  mtime = caml_copy_double((double) buf->st_mtime
-                           + (NSEC(buf, m) / 1000000000.0));
-  ctime = caml_copy_double((double) buf->st_ctime
-                           + (NSEC(buf, c) / 1000000000.0));
+  atime = caml_copy_double(stat_timestamp(buf->st_atime, NSEC(buf, a)));
+  mtime = caml_copy_double(stat_timestamp(buf->st_mtime, NSEC(buf, m)));
+  ctime = caml_copy_double(stat_timestamp(buf->st_ctime, NSEC(buf, c)));
   #undef NSEC
   offset = use_64 ? Val_file_offset(buf->st_size) : Val_int (buf->st_size);
   v = caml_alloc_small(12, 0);
