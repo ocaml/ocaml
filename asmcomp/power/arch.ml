1260c3d3681c892cba516ba9dc1b2b08efcc41b3
David Allsopp <david.allsopp@metastack.com>
2020-05-13 10:37:48 +0100

    Make -flarge-toc the default for PowerPC
    Introduce -fsmall-toc in order to access the previous behaviour and
    document both options in the manual and ocamlopt manpage.

--- asmcomp/power/arch.ml
+++ asmcomp/power/arch.ml
@@ -34,11 +34,13 @@ let abi =
 
 (* Machine-specific command-line options *)
 
-let big_toc = ref false
+let big_toc = ref true
 
 let command_line_options = [
   "-flarge-toc", Arg.Set big_toc,
-     " Support TOC (table of contents) greater than 64 kbytes"
+     " Support TOC (table of contents) greater than 64 kbytes (default)";
+  "-fsmall-toc", Arg.Clear big_toc,
+     " TOC (table of contents) is limited to 64 kbytes"
 ]
 
 (* Specific operations *)
