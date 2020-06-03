abb8db459d8aa3440380a19d979dcb17222b7e19
Jacques Garrigue <garrigue@math.nagoya-u.ac.jp>
2020-06-03 17:47:12 +0200

    Fix #7520: Odd behaviour of refutation cases with polymorphic variants (#9547)

--- typing/ctype.mli
+++ typing/ctype.mli
@@ -258,8 +258,6 @@ val unify_gadt:
 val unify_var: Env.t -> type_expr -> type_expr -> unit
         (* Same as [unify], but allow free univars when first type
            is a variable. *)
-val with_passive_variants: ('a -> 'b) -> ('a -> 'b)
-        (* Call [f] in passive_variants mode, for exhaustiveness check. *)
 val filter_arrow: Env.t -> type_expr -> arg_label -> type_expr * type_expr
         (* A special case of unification (with l:'a -> 'b). *)
 val filter_method: Env.t -> string -> private_flag -> type_expr -> type_expr
