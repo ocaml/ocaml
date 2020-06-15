001af6876497871411e888cc4991f3cccc124979
KC Sivaramakrishnan <kc@kcsrk.info>
2020-06-15 12:54:35 +0530

    Add assertions, improve test

--- testsuite/tests/parallel/backup_thread.ml
+++ testsuite/tests/parallel/backup_thread.ml
@@ -5,7 +5,11 @@ include unix
 ** native
 *)
 
+
 let _ =
+  (* start a dummy domain and shut it down to cause a domain reuse *)
+  let d = Domain.spawn (fun _ -> ()) in
+  Domain.join d;
   let _d = Domain.spawn (fun _ ->
     Unix.sleep 10;
     print_endline "Should not reach here!") in
