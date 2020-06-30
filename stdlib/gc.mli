c4851b0ff0070bca3b5ebbb3ec51941a249031cc
Et7f3 <cadeaudeelie@gmail.com>
2020-06-30 14:06:45 +0100

    Consistently use @raise tags in Stdlib docs (#8644)

--- stdlib/gc.mli
+++ stdlib/gc.mli
@@ -308,7 +308,7 @@ external get_minor_free : unit -> int = "caml_get_minor_free"
 external get_bucket : int -> int = "caml_get_major_bucket" [@@noalloc]
 (** [get_bucket n] returns the current size of the [n]-th future bucket
     of the GC smoothing system. The unit is one millionth of a full GC.
-    Raise [Invalid_argument] if [n] is negative, return 0 if n is larger
+    @raise Invalid_argument if [n] is negative, return 0 if n is larger
     than the smoothing window.
 
     @since 4.03.0 *)
