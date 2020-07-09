e5cf983bb9823f4016a890efd32b9d5a6afa7e60
Xavier Clerc <xclerc@janestreet.com>
2020-07-09 13:19:51 +0100

    Share the strings representing scopes.

--- lambda/debuginfo.ml
+++ lambda/debuginfo.ml
@@ -64,6 +64,17 @@ module Scoped_location = struct
     | [] -> "<unknown>"
     | scopes -> String.concat "" (to_strings [] scopes)
 
+  let string_of_scopes =
+    let module StringSet = Set.Make (String) in
+    let repr = ref StringSet.empty in
+    fun scopes ->
+      let res = string_of_scopes scopes in
+      match StringSet.find_opt res !repr with
+      | Some x -> x
+      | None ->
+        repr := StringSet.add res !repr;
+        res
+
   let enter_anonymous_function ~scopes =
     Sc_anonymous_function :: scopes
   let enter_value_definition ~scopes id =
