ae3155b5c30a79d2882556cfd09a10650af82037
Sudha Parimala <sudharg247@gmail.com>
2020-07-15 22:17:23 +0530

    better test

--- testsuite/tests/parallel/domain_tls.ml
+++ testsuite/tests/parallel/domain_tls.ml
@@ -7,8 +7,14 @@ include unix
 
 let check () =
   let k1 = Domain.TLS.new_key () in
-  Domain.TLS.set k1 5;
-  let _ = Domain.TLS.get k1 in ()
+  let my_state = Random.State.make_self_init () in
+  Domain.TLS.set k1 my_state;
+  let k2 = Domain.TLS.new_key () in
+  let my_int = 500 in
+  Domain.TLS.set k2 my_int;
+  let s : Random.State.t = Obj.magic @@ Option.get @@ Domain.TLS.get k1 in
+  let v : int = Obj.magic @@ Option.get @@ Domain.TLS.get k2 in
+  ignore @@ (Random.State.int s v)
 
 let _ =
   let domains = Array.init 3 (fun _ -> Domain.spawn(fun _ -> check ())) in
