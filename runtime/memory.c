83cd8c9d5d32a52d67ea5b54dbfa2a28cddd13e7
Sadiq Jaffer <sadiq@toao.com>
2020-07-20 09:15:15 +0100

    initialise chunk pointers to start and end of chunk

--- runtime/memory.c
+++ runtime/memory.c
@@ -262,8 +262,8 @@ char *caml_alloc_for_heap (asize_t request)
     mem = (char *) block + sizeof (heap_chunk_head);
     Chunk_size (mem) = size - sizeof (heap_chunk_head);
     Chunk_block (mem) = block;
-    Chunk_redarken_start(mem) = (value*)(-1);
-    Chunk_redarken_end(mem) = 0;
+    Chunk_redarken_start(mem) = (value*)(mem + Chunk_size(mem));
+    Chunk_redarken_end(mem) = (value*)mem;
     return mem;
 #else
     return NULL;
@@ -279,8 +279,8 @@ char *caml_alloc_for_heap (asize_t request)
     mem += sizeof (heap_chunk_head);
     Chunk_size (mem) = request;
     Chunk_block (mem) = block;
-    Chunk_redarken_start(mem) = (value*)(-1);
-    Chunk_redarken_end(mem) = 0;
+    Chunk_redarken_start(mem) = (value*)(mem + Chunk_size(mem));
+    Chunk_redarken_end(mem) = (value*)mem;
     return mem;
   }
 }
