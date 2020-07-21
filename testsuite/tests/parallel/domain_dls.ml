b0e82908dfa89ae7fff9a6a420d9cf1b632d713a
Sudha Parimala <sudharg247@gmail.com>
2020-07-21 15:09:18 +0530

    use ref of object to store dls variables

---
+++ testsuite/tests/parallel/domain_dls.ml
@@ -0,0 +1,21 @@
+(* TEST
+* hasunix
+include unix
+** bytecode
+** native
+*)
+
+let check () =
+  let k1 : int Domain.DLS.key = Domain.DLS.new_key () in
+  let k2 : float Domain.DLS.key = Domain.DLS.new_key () in
+  Domain.DLS.set k1 100;
+  Domain.DLS.set k2 200.0;
+  let v1 = Option.get (Domain.DLS.get k1) in
+  let v2 = Option.get (Domain.DLS.get k2) in
+  v1 + (int_of_float v2) |> ignore
+
+let _ =
+  let domains = Array.init 3 (fun _ -> Domain.spawn(check)) in
+  check ();
+  Array.iter Domain.join domains;
+  print_endline "ok"
