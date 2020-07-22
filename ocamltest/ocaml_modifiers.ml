a28fe085e2bb906ce75ff5a8510fe5f12ff92c5d
Nicolás Ojeda Bär <n.oje.bar@gmail.com>
2020-07-22 20:34:45 +0200

    ocamltest: fix include native dynlink
    Add "-I otherlibs/dynlink/native" when doing "include dynlink".

--- ocamltest/ocaml_modifiers.ml
+++ ocamltest/ocaml_modifiers.ml
@@ -50,19 +50,19 @@ let man =
     Add (Ocaml_variables.ocamldoc_backend, "man");
   ]
 
-let wrap str = (" " ^ str ^ " ")
+let wrap strs = " " ^ String.concat " " strs ^ " "
 
-let make_library_modifier library directory =
+let make_library_modifier library directories =
 [
-  Append (Ocaml_variables.directories, (wrap directory));
-  Append (Ocaml_variables.libraries, (wrap library));
-  Append (Ocaml_variables.caml_ld_library_path, (wrap directory));
+  Append (Ocaml_variables.directories, (wrap directories));
+  Append (Ocaml_variables.libraries, (wrap [library]));
+  Append (Ocaml_variables.caml_ld_library_path, (wrap directories));
 ]
 
 let make_module_modifier unit_name directory =
 [
-  Append (Ocaml_variables.directories, (wrap directory));
-  Append (Ocaml_variables.binary_modules, (wrap unit_name));
+  Append (Ocaml_variables.directories, (wrap [directory]));
+  Append (Ocaml_variables.binary_modules, (wrap [unit_name]));
 ]
 
 let compiler_subdir subdir =
@@ -70,11 +70,11 @@ let compiler_subdir subdir =
 
 let config =
 [
-  Append (Ocaml_variables.directories, (wrap (compiler_subdir ["utils"])));
+  Append (Ocaml_variables.directories, (wrap [compiler_subdir ["utils"]]));
 ]
 
 let testing = make_library_modifier
-  "testing" (compiler_subdir ["testsuite"; "lib"])
+  "testing" [compiler_subdir ["testsuite"; "lib"]]
 
 let tool_ocaml_lib = make_module_modifier
   "lib" (compiler_subdir ["testsuite"; "lib"])
@@ -82,18 +82,20 @@ let tool_ocaml_lib = make_module_modifier
 let unixlibdir = if Sys.win32 then "win32unix" else "unix"
 
 let unix = make_library_modifier
-  "unix" (compiler_subdir ["otherlibs"; unixlibdir])
+  "unix" [compiler_subdir ["otherlibs"; unixlibdir]]
 
 let dynlink =
-  make_library_modifier "dynlink" (compiler_subdir ["otherlibs"; "dynlink"])
+  make_library_modifier "dynlink"
+    [compiler_subdir ["otherlibs"; "dynlink"];
+     compiler_subdir ["otherlibs"; "dynlink"; "native"]]
 
 let str = make_library_modifier
-  "str" (compiler_subdir ["otherlibs"; "str"])
+  "str" [compiler_subdir ["otherlibs"; "str"]]
 
 let systhreads =
   unix @
   (make_library_modifier
-    "threads" (compiler_subdir ["otherlibs"; "systhreads"]))
+    "threads" [compiler_subdir ["otherlibs"; "systhreads"]])
 
 let compilerlibs_subdirs =
 [
@@ -111,10 +113,10 @@ let compilerlibs_subdirs =
 ]
 
 let add_compiler_subdir subdir =
-  Append (Ocaml_variables.directories, (wrap (compiler_subdir [subdir])))
+  Append (Ocaml_variables.directories, (wrap [compiler_subdir [subdir]]))
 
 let compilerlibs_archive archive =
-  (Append (Ocaml_variables.libraries, wrap archive)) ::
+  (Append (Ocaml_variables.libraries, wrap [archive])) ::
   (List.map add_compiler_subdir compilerlibs_subdirs)
 
 let debugger = [add_compiler_subdir "debugger"]
