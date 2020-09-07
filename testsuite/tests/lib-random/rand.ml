a7d1af4a872e22aebd1e6cc27b2beafdea2ad2dd
Xavier Leroy <xavier.leroy@college-de-france.fr>
2020-09-07 13:55:59 +0200

    Test rand.ml: make it less likely to fail
    Use Random.bits() instead of Random.int 10000 to get a 2^-30 expected
    failure rate instead of 10^-5.

--- testsuite/tests/lib-random/rand.ml
+++ testsuite/tests/lib-random/rand.ml
@@ -4,12 +4,12 @@
 (* Test that two Random.self_init() in close succession will not result
    in the same PRNG state.
    Note that even when the code is correct this test is expected to fail
-   once in 10000 runs.
+   once in 2^30 runs.
 *)
 
 let () =
   Random.self_init ();
-  let x = Random.int 10000 in
+  let x = Random.bits () in
   Random.self_init ();
-  let y = Random.int 10000 in
+  let y = Random.bits () in
   if x = y then print_endline "FAILED" else print_endline "PASSED"
