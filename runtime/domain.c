556a16f941a6c84c9fef3d59072360fbe54dc0f6
Tom Kelly <ctk21@cl.cam.ac.uk>
2020-09-07 11:52:51 +0100

    relaxation of 389; no longer signal BT_IN_BLOCKING_SECTION -> BT_ENTER_OCAML; means the backup thread may be sleeping on the interruptor (we already broadcast interrupts) but we need to signal in domain_terminate

--- runtime/domain.c
+++ runtime/domain.c
@@ -948,9 +948,6 @@ CAMLexport void caml_leave_blocking_section() {
 
   if (self->backup_thread_running) {
     atomic_store_rel(&self->backup_thread_msg, BT_ENTERING_OCAML);
-    caml_plat_lock(&self->interruptor.lock);
-    caml_plat_signal(&self->interruptor.cond);
-    caml_plat_unlock(&self->interruptor.lock);
   }
 
   caml_plat_lock(&self->domain_lock);
@@ -1203,6 +1200,10 @@ static void domain_terminate()
       s->running = 0;
       s->unique_id += Max_domains;
 
+      /* signal the interruptor condition variable
+       * because the backup thread may be waiting on it
+       */
+      caml_plat_broadcast(&s->cond);
       Assert (domain_self->backup_thread_running);
       domain_self->backup_thread_running = 0;
     }
