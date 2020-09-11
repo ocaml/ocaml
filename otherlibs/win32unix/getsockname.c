56a2f4ff14d7b070acb705ea8d40f582da252534
David Allsopp <david.allsopp@metastack.com>
2020-09-11 10:28:23 +0100

    Merge {win32,}unix/socketaddr.h

--- otherlibs/win32unix/getsockname.c
+++ otherlibs/win32unix/getsockname.c
@@ -17,16 +17,14 @@
 #include "unixsupport.h"
 #include "socketaddr.h"
 
-CAMLprim value unix_getsockname(sock)
-     value sock;
+CAMLprim value unix_getsockname(value sock)
 {
   int retcode;
   union sock_addr_union addr;
   socklen_param_type addr_len;
 
-  addr_len = sizeof(sock_addr);
-  retcode = getsockname(Socket_val(sock),
-                        &addr.s_gen, &addr_len);
+  addr_len = sizeof(addr);
+  retcode = getsockname(Socket_val(sock), &addr.s_gen, &addr_len);
   if (retcode == -1) uerror("getsockname", Nothing);
   return alloc_sockaddr(&addr, addr_len, -1);
 }
