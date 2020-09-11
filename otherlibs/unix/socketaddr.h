0d91bef98568dc8a0a6adc99fbab1f078ea931f9
David Allsopp <david.allsopp@metastack.com>
2020-09-11 15:35:31 +0100

    Eliminate CAMLprim/CAMLexport in socketaddr.h (#9900)
    * Merge {win32,}unix/socketaddr.h so that unix/socketaddr.h can be used from win32unix/ as well.
    * Update socket-related files in win32unix/

--- otherlibs/unix/socketaddr.h
+++ otherlibs/unix/socketaddr.h
@@ -17,15 +17,19 @@
 #define CAML_SOCKETADDR_H
 
 #include "caml/misc.h"
+#ifndef _WIN32
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#endif
 
 union sock_addr_union {
   struct sockaddr s_gen;
+#ifndef _WIN32
   struct sockaddr_un s_unix;
+#endif
   struct sockaddr_in s_inet;
 #ifdef HAS_IPV6
   struct sockaddr_in6 s_inet6;
@@ -45,13 +49,13 @@ extern "C" {
 extern void get_sockaddr (value mladdr,
                           union sock_addr_union * addr /*out*/,
                           socklen_param_type * addr_len /*out*/);
-CAMLexport value alloc_sockaddr (union sock_addr_union * addr /*in*/,
+extern value alloc_sockaddr (union sock_addr_union * addr /*in*/,
                       socklen_param_type addr_len, int close_on_error);
-CAMLexport value alloc_inet_addr (struct in_addr * inaddr);
+extern value alloc_inet_addr (struct in_addr * inaddr);
 #define GET_INET_ADDR(v) (*((struct in_addr *) (v)))
 
 #ifdef HAS_IPV6
-CAMLexport value alloc_inet6_addr (struct in6_addr * inaddr);
+extern value alloc_inet6_addr (struct in6_addr * inaddr);
 #define GET_INET6_ADDR(v) (*((struct in6_addr *) (v)))
 #endif
 
