2711363dbc0e24e36766b306df5dad79bde4bb5a
Sudha Parimala <sudharg247@gmail.com>
2020-09-29 14:05:18 +0530

    Domain.DLS.new_key takes an initialiser

--- testsuite/tests/parallel/domain_dls.ml
+++ testsuite/tests/parallel/domain_dls.ml
@@ -6,12 +6,12 @@ include unix
 *)
 
 let check () =
-  let k1 : int Domain.DLS.key = Domain.DLS.new_key () in
-  let k2 : float Domain.DLS.key = Domain.DLS.new_key () in
+  let k1 = Domain.DLS.new_key (fun () -> 10) in
+  let k2 = Domain.DLS.new_key (fun () -> 1.0) in
   Domain.DLS.set k1 100;
   Domain.DLS.set k2 200.0;
-  let v1 = Option.get (Domain.DLS.get k1) in
-  let v2 = Option.get (Domain.DLS.get k2) in
+  let v1 = Domain.DLS.get k1 in
+  let v2 = Domain.DLS.get k2 in
   assert (v1 = 100);
   assert (v2 = 200.0);
   Gc.major ()
