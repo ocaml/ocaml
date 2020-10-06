18311a5aec1a7b6996420e3d37b15cb0fdecd8c9
Xavier Leroy <xavier.leroy@college-de-france.fr>
2020-10-06 10:04:05 +0200

    Semaphore: use "acquire" and "release" terminology

--- otherlibs/systhreads/semaphore.ml
+++ otherlibs/systhreads/semaphore.ml
@@ -18,48 +18,48 @@
 type t = {
   mut: Mutex.t;                         (* protects [v] *)
   mutable v: int;                       (* the current value *)
-  max: int option;                      (* the maximal value *)
+  capacity: int option;                 (* the maximal value *)
   nonzero: Condition.t                  (* signaled when [v > 0] *)
 }
 
-let make ?max v =
-  let m = Option.value max ~default:max_int in
-  if v < 0 || v > m then
+let make ?capacity v =
+  let cap = Option.value capacity ~default:max_int in
+  if v < 0 || v > cap then
     raise (Sys_error "Semaphore.init: wrong initial value");
-  if m <= 0 then
-    raise (Sys_error "Semaphore.init: wrong maximal value");
-  { mut = Mutex.create(); v; max; nonzero = Condition.create() }
+  if cap <= 0 then
+    raise (Sys_error "Semaphore.init: wrong capacity");
+  { mut = Mutex.create(); v; capacity; nonzero = Condition.create() }
 
 let make_binary b =
   { mut = Mutex.create();
     v = if b then 1 else 0;
-    max = Some 1;
+    capacity = Some 1;
     nonzero = Condition.create() }
 
-let post s =
+let release s =
   Mutex.lock s.mut;
-  begin match s.max with
+  begin match s.capacity with
     | None ->
         if s.v < max_int then
           s.v <- s.v + 1
         else begin
           Mutex.unlock s.mut;
-          raise (Sys_error "Semaphore.post: overflow")
+          raise (Sys_error "Semaphore.release: overflow")
         end
-    | Some m ->
-        if s.v < m then
+    | Some cap ->
+        if s.v < cap then
           s.v <- s.v + 1
   end;
   Condition.signal s.nonzero;
   Mutex.unlock s.mut
 
-let wait s =
+let acquire s =
   Mutex.lock s.mut;
   while s.v = 0 do Condition.wait s.nonzero s.mut done;
   s.v <- s.v - 1;
   Mutex.unlock s.mut
 
-let trywait s =
+let try_acquire s =
   Mutex.lock s.mut;
   let ret = if s.v = 0 then false else (s.v <- s.v - 1; true) in
   Mutex.unlock s.mut;
--- otherlibs/systhreads/semaphore.mli
+++ otherlibs/systhreads/semaphore.mli
@@ -21,14 +21,22 @@
   are available.
 
   The two basic operations on semaphores are:
-- "post" (also called "V", "up", "signal", and "release"), which
-  increments the value of the counter;
-- "wait" (also called "P", "down", "pend", and "acquire"), which
+- "release" (also called "V", "post", "up", and "signal"), which
+  increments the value of the counter.  This corresponds to producing
+  one more unit of the shared resource and making it available to others.
+- "acquire" (also called "P", "wait", "down", and "pend"), which
   waits until the counter is greater than zero and decrements it.
+  This corresponds to consuming one unit of the shared resource.
 
   A variant of the counting semaphore is the binary semaphore,
-  which has only two values, 0 and 1.  "post" sets the value to 1
-  and "wait" waits until the value is 1 and sets it to 0.
+  which has only two values, 0 and 1.  "release" sets the value to 1
+  and "acquire" waits until the value is 1 and sets it to 0.  A binary
+  semaphore can be used instead of a mutex (see module {!Mutex})
+  when the mutex discipline (of unlocking the mutex from the
+  thread that locked it) is too restrictive.  The "acquire" operation
+  corresponds to locking the mutex, and the "release" operation
+  to unlocking it, but "release" can be performed in a thread different
+  than the one that performed the "acquire".
 
   @since 4.12
 *)
@@ -36,31 +44,31 @@
 type t
 (** The type of semaphores. *)
 
-val make : ?max: int -> int -> t
+val make : ?capacity: int -> int -> t
 (** [make n] returns a new counted semaphore, with initial value [n].
     The initial value [n] must be nonnegative.
 
-    The [max] optional argument specifies the maximal value of
-    the semaphore: once the value of the semaphore reaches [max],
-    subsequent {!post} operations will not increase it
-    above [max].  If provided, [max] must be positive and
+    The [capacity] optional argument specifies the maximal value of
+    the semaphore: once the value of the semaphore reaches [capacity],
+    subsequent {!release} operations will not increase it
+    above [capacity].  If provided, [capacity] must be positive and
     greater than or equal to [n].
 
-    @raise Sys_error if [n < 0] or [max <= 0] or [n > max]
+    @raise Sys_error if [n < 0] or [capacity <= 0] or [n > capacity]
 *)
 
 val make_binary : bool -> t
-(** [make_binary b] is equivalent to [{!make} ~max:1 (if b then 1 else 0)].
+(** [make_binary b] is equivalent to [{!make} ~capacity:1 (if b then 1 else 0)].
     It returns a new binary semaphore, with initial value [b]
     ([false] means [0], [true] means [1]) and maximal value [1].
 *)
 
-val post : t -> unit
-(** [post s] increments the value of semaphore [s].
+val release : t -> unit
+(** [release s] increments the value of semaphore [s].
     If other threads are waiting on [s], one of them is restarted.
     If semaphore [s] was created with a maximal value, and the current
     value is equal to this maximal value, the value of the semaphore
-    is unchanged and [post s] has no effect.
+    is unchanged and [release s] has no effect.
     If semaphore [s] was created without a maximal value, and the
     current value is equal to [max_int], the value of the semaphore
     is unchanged and a [Sys_error] exception is raised to signal overflow.
@@ -68,13 +76,13 @@ val post : t -> unit
     @raise Sys_error if the value of the semaphore would overflow [max_int]
 *)
 
-val wait : t -> unit
-(** [wait s] blocks the calling thread until the value of semaphore [s]
+val acquire : t -> unit
+(** [acquire s] blocks the calling thread until the value of semaphore [s]
     is not zero, then atomically decrements the value of [s] and returns.
 *)
 
-val trywait : t -> bool
-(** [trywait s] immediately returns [false] if the value of semaphore [s]
+val try_acquire : t -> bool
+(** [try_acquire s] immediately returns [false] if the value of semaphore [s]
     is zero.  Otherwise, the value of [s] is atomically decremented
-    and [trywait s] returns [true].
+    and [try_acquire s] returns [true].
 *)
