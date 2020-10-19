9b3931a156ef1d8ed894ccb1412b72261c6091df
Stephen Dolan <sdolan@janestreet.com>
2020-10-19 10:40:25 +0100

    Ensure Mtype.nondep_sig checks use the correct environment (#9946)

--- typing/mtype.ml
+++ typing/mtype.ml
@@ -222,6 +222,8 @@ and nondep_sig_item env va ids = function
       Sig_class_type(id, Ctype.nondep_cltype_declaration env ids d, rs, vis)
 
 and nondep_sig env va ids sg =
+  let scope = Ctype.create_scope () in
+  let sg, env = Env.enter_signature ~scope sg env in
   List.map (nondep_sig_item env va ids) sg
 
 and nondep_modtype_decl env ids mtd =
