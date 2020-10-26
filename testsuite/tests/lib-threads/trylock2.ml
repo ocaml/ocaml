98fa25f3ddd10ec3722f8a1d1fad44a321e0ee79
Enguerrand Decorne <decorne.en@gmail.com>
2020-10-26 11:10:29 +0100

    testsuite: add a positive test for trylock

---
+++ testsuite/tests/lib-threads/trylock2.ml
@@ -0,0 +1,16 @@
+(* TEST
+
+* hassysthreads
+include systhreads
+** bytecode
+** native
+
+*)
+
+(* Test Mutex.try_lock *)
+
+let () =
+  let m = Mutex.create () in
+  assert (Mutex.try_lock m);
+  Mutex.unlock m;
+  print_endline "passed"
