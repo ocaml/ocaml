0f30379ad94f81360586465a840107a76a5b486b
Vincent Laviron <vincent.laviron@gmail.com>
2020-10-30 16:17:45 +0100

    Use Sys.opaque_identity instead

--- stdlib/camlinternalLazy.ml
+++ stdlib/camlinternalLazy.ml
@@ -45,12 +45,19 @@ let force_val_lazy_block (blk : 'arg lazy_t) =
   result
 
 
-(* [force] is not used except when afl instrumentation is on.
-   In this case, it must not be inlined in flambda mode as it can modify
-   its argument in some cases, and is allowed to be called
-   with immutable arguments, which will generate warning 59. *)
+(* [force] is not used, since [Lazy.force] is declared as a primitive
+   whose code inlines the tag tests of its argument, except when afl
+   instrumentation is turned on. *)
 
-let[@inline never] force (lzv : 'arg lazy_t) =
+let force (lzv : 'arg lazy_t) =
+  (* Using [Sys.opaque_identity] prevents two potential problems:
+     - If the value is known to have Forward_tag, then its tag could have
+       changed during GC, so that information must be forgotten (see GPR#713
+       and issue #7301)
+     - If the value is known to be immutable, then if the compiler
+       cannot prove that the last branch is not taken it will issue a
+       warning 59 (modification of an immutable value) *)
+  let lzv = Sys.opaque_identity lzv in
   let x = Obj.repr lzv in
   let t = Obj.tag x in
   if t = Obj.forward_tag then (Obj.obj (Obj.field x 0) : 'arg) else
