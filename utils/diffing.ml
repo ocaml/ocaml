ba329119a829c5212bbd3ad74819db70b5c9e3e6
Drup <drupyog@zoho.com>
2020-11-12 17:43:59 +0100

    More code simplification.

--- utils/diffing.ml
+++ utils/diffing.ml
@@ -41,13 +41,13 @@ type ('st,'left,'right) full_state = {
 
 module Matrix : sig
 
+  type shape = { l : int ; c : int }
+
   type ('state,'left,'right,'eq,'diff) t
 
-  val make : lines:int -> columns:int -> ('st,'l,'r,'e,'d) t
-  val reshape :
-    lines:int -> columns:int ->
-    ('st,'l,'r,'e,'d) t -> ('st,'l,'r,'e,'d) t
-
+  val make : shape -> ('st,'l,'r,'e,'d) t
+  val reshape : shape -> ('st,'l,'r,'e,'d) t -> ('st,'l,'r,'e,'d) t
+  
   val diff : (_,'l,'r,'e,'d) t -> int -> int -> ('l,'r,'e,'d) change option
   val state : ('st,'l,'r,'e,'d) t -> int -> int -> ('st, 'l, 'r) full_state option
   val weight : _ t -> int -> int -> int
@@ -57,18 +57,20 @@ module Matrix : sig
 
   val update :
     ('st,'l,'r,'e,'d) t -> int -> int ->
-    ?diff:('l,'r,'e,'d) change ->
+    diff:('l,'r,'e,'d) change option ->
     weight:int ->
     state:('st, 'l, 'r) full_state ->
     unit
 
-  val shape : _ t -> int * int
-  val[@warning "-32"] shape_at : _ t -> int -> int -> (int * int) option
-  val real_shape : _ t -> int * int
+  val shape : _ t -> shape
+  val shape_at : _ t -> int -> int -> shape option
+  val real_shape : _ t -> shape
 
   val[@warning "-32"] pp : Format.formatter -> _ t -> unit
   
 end = struct
+
+  type shape = { l : int ; c : int }
 
   type ('state,'left,'right,'eq,'diff) t =
     { states: ('state,'left,'right) full_state option array array;
@@ -84,9 +86,9 @@ end = struct
   let diff m i j = m.diff.(i).(j)
   let weight m i j = m.weight.(i).(j)
   let state m i j = m.states.(i).(j)
-  let shape m = m.lines, m.columns
-  
-  let update m i j ?diff ~weight ~state =
+  let shape m = { l = m.lines ; c = m.columns }
+
+  let update m i j ~diff ~weight ~state =
     m.weight.(i).(j) <- weight;
     m.states.(i).(j) <- Some state;
     m.diff.(i).(j) <- diff;
@@ -96,24 +98,21 @@ end = struct
     let+ st = tbl.states.(i).(j) in
     let l = Array.length st.line in
     let c = Array.length st.column in
-    l, c
+    { l ; c }
   
   let real_shape tbl =
     let lines = ref tbl.lines in
     let columns = ref tbl.columns in
-    let max_at i j =
-      let*! l, c = shape_at tbl i j in
-      if l > !lines then lines := l;
-      if c > !columns then columns := c
-    in
     for i = 0 to tbl.lines do 
       for j = 0 to tbl.columns do
-        max_at i j
+        let*! {l; c} = shape_at tbl i j in
+        if l > !lines then lines := l;
+        if c > !columns then columns := c
       done;
     done;
-    !lines, !columns
+    { l = !lines ; c = !columns }
 
-  let make ~lines ~columns =
+  let make { l = lines ; c = columns } =
     { states = Array.make_matrix (lines + 1) (columns + 1) None;
       weight = Array.make_matrix (lines + 1) (columns + 1) max_int;
       diff = Array.make_matrix (lines + 1) (columns + 1) None;
@@ -121,7 +120,7 @@ end = struct
       columns;
     }
 
-  let reshape ~lines ~columns m =
+  let reshape { l = lines ; c = columns } m =
     let copy default a =
       Array.init (1+lines) (fun i -> Array.init (1+columns) (fun j ->
           if i <= m.lines && j <= m.columns then
@@ -134,9 +133,8 @@ end = struct
       columns
     }
 
-
   let pp ppf m =
-    let l,c = shape m in
+    let { l ; c } = shape m in
     Format.eprintf "Shape : %i, %i@." l c;
     for i = 0 to l do
       for j = 0 to c do
@@ -159,7 +157,6 @@ end = struct
  
 end
 
-
 let select_best_proposition l =
   let compare_proposition curr prop =
     match curr, prop with
@@ -176,16 +173,16 @@ let compute_column0 ~weight ~update tbl i =
   Matrix.update tbl i 0
     ~weight:(weight diff + Matrix.weight tbl (i-1) 0)
     ~state:(update diff st)
-    ~diff
+    ~diff:(Some diff)
 
 let compute_line0 ~weight ~update tbl j =
   let*! st = Matrix.state tbl 0 (j-1) in
   let*! column = Matrix.column tbl 0 (j-1) in
-  let diff = Insert column  in
+  let diff = Insert column in
   Matrix.update tbl 0 j
     ~weight:(weight diff + Matrix.weight tbl 0 (j-1))
     ~state:(update diff st)
-    ~diff
+    ~diff:(Some diff)
 
 let compute_inner_cell ~weight ~test ~update tbl i j =
   let compute_proposition i j diff =
@@ -216,7 +213,7 @@ let compute_inner_cell ~weight ~test ~update tbl i j =
     select_best_proposition [diag;del;insert]
   in
   let state = update diff localstate in
-  Matrix.update tbl i j ~weight:newweight ~state ~diff
+  Matrix.update tbl i j ~weight:newweight ~state ~diff:(Some diff)
 
 let compute_cell ~weight ~test ~update m i j =
   match i, j with
@@ -227,40 +224,44 @@ let compute_cell ~weight ~test ~update m i j =
   | _ -> compute_inner_cell ~weight ~test ~update m i j
 
 let compute_matrix ~weight ~test ~update state0 =
-  let m0 = Matrix.make ~lines:0 ~columns:0 in
-  Matrix.update m0 0 0 ~weight:0 ~state:state0 ?diff:None;
-  let rec loop m =
-    let orig_lines, orig_columns = Matrix.shape m in
-    let lines, columns = Matrix.real_shape m in
-    if lines > orig_lines || columns > orig_columns then
-      let m = Matrix.reshape ~lines ~columns m in
-      for j = 0 to columns do
-        for i = 0 to lines do
+  let m0 = Matrix.make { l = 0 ; c = 0 } in
+  Matrix.update m0 0 0 ~weight:0 ~state:state0 ~diff:None;
+  let rec loop maybe_finals m =
+    let shape = Matrix.shape m in
+    let new_shape = Matrix.real_shape m in
+    if new_shape.l > shape.l || new_shape.c > shape.c then
+      let m = Matrix.reshape new_shape m in
+      for i = 0 to new_shape.l do
+        for j = 0 to new_shape.c do
           compute_cell ~update ~test ~weight m i j
         done
       done;
-      loop m
-    else m in
-  loop m0
+      let maybe_finals =
+        let i = new_shape.l and j = new_shape.c in
+        match Matrix.shape_at m i j with
+        | Some shape_here when shape_here = new_shape ->
+            (i,j) :: maybe_finals
+        | _ ->
+            maybe_finals
+      in
+      loop maybe_finals m
+    else
+      (m, maybe_finals)
+  in
+  loop [] m0
 
-let select_final_state m0 =
-  let final = ref (0,0, max_int) in
-  let l0, c0 = Matrix.shape m0 in
-  for i = 0 to l0 do
-    for j = 0 to c0 do
-      let*! l, c = Matrix.shape_at m0 i j in
-      let w = Matrix.weight m0 i j in
-      let _, _, w0 = !final in
-      if i = l && j = c && w < w0 then begin
-        final := (i, j, w)
-      end
-    done;
-  done;
-  let i_final, j_final, _ = !final in
+let select_final_state m0 maybe_finals =
+  let compare_states (i0,j0,weigth0) (i,j) =
+    let weight = Matrix.weight m0 i j in
+    if weight < weigth0 then (i,j,weight) else (i0,j0,weigth0)
+  in
+  let i_final, j_final, _ =
+    List.fold_left compare_states (0,0,max_int) maybe_finals
+  in
   assert (i_final <> 0 || j_final <> 0);
   (i_final, j_final)
 
-let construct_diff m0 =
+let construct_diff (m0, maybe_finals) =
   let rec aux acc (i, j) =
     if i = 0 && j = 0 then
       acc
@@ -275,12 +276,13 @@ let construct_diff m0 =
           in
           aux (d::acc) next
   in
-  aux [] (select_final_state m0)
+  aux [] (select_final_state m0 maybe_finals)
 
 let diff ~weight ~test ~update state line column =
   let update d fs = { fs with state = update d fs.state } in
   let fullstate = { line; column; state } in
-  construct_diff (compute_matrix ~weight ~test ~update fullstate)
+  compute_matrix ~weight ~test ~update fullstate
+  |> construct_diff
 
 type ('l, 'r, 'e, 'd, 'state) update =
   | No of (('l,'r,'e,'d) change -> 'state -> 'state)
@@ -303,4 +305,5 @@ let variadic_diff ~weight ~test ~(update:_ update) state line column =
           { fs with state ; column = Array.append fs.column a }
   in
   let fullstate = { line; column; state } in
-  construct_diff (compute_matrix ~weight ~test ~update fullstate)
+  compute_matrix ~weight ~test ~update fullstate
+  |> construct_diff
