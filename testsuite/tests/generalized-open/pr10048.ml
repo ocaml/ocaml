a5de7840392ef7d30505c3bbe236c721311fb229
Leo White <leo@lpw25.net>
2020-11-26 13:35:45 +0000

    Fix issue with local opens and existentials

--- testsuite/tests/generalized-open/pr10048.ml
+++ testsuite/tests/generalized-open/pr10048.ml
@@ -13,11 +13,5 @@ let foo (x : Ext(List).t) =
 [%%expect {|
 module Ext :
   functor (X : sig type 'a t end) -> sig type t = T : 'a X.t -> t end
-Lines 8-9, characters 4-23:
-8 | ....let open Ext(Array) in
-9 |     T (Array.of_list l)..
-Error: This expression has type t but an expression was expected of type
-         Ext(Array).t
-       The type constructor t would escape its scope
-       t is abstract because no corresponding cmi file was found in path.
+val foo : Ext(List).t -> Ext(Array).t = <fun>
 |}]
