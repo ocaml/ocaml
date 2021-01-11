d3fc1261a769ced32f7754e88d32ed9a1faadaf6
Gabriel Scherer <gabriel.scherer@gmail.com>
2021-01-11 16:50:59 +0100

    enable warning 6 [labels-omitted] by default

--- utils/warnings.ml
+++ utils/warnings.ml
@@ -662,7 +662,7 @@ let parse_options errflag s =
   alerts
 
 (* If you change these, don't forget to change them in man/ocamlc.m *)
-let defaults_w = "+a-4-6-7-9-27-29-30-32..42-44-45-48-50-60-66..70";;
+let defaults_w = "+a-4-7-9-27-29-30-32..42-44-45-48-50-60-66..70";;
 let defaults_warn_error = "-a+31";;
 
 let () = ignore @@ parse_options false defaults_w;;
