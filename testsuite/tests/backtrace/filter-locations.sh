a056b15eb8eb7ac491d1882bfae390aaebfd382f
Gabriel Scherer <gabriel.scherer@gmail.com>
2021-01-15 20:07:41 +0100

    Merge pull request #10157 from gasche/try-to-fix-appveyor-backtrace-tests
    testsuite/tests/backtrace: attempt to fix Windows \r\n issue
    
    (cherry picked from commit 1c7ca6027ca46beda7b19a2f6c67ce49708978e2)

--- testsuite/tests/backtrace/filter-locations.sh
+++ testsuite/tests/backtrace/filter-locations.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-sed -e "s/^.*in file/File/" -e 's/ (inlined)//' -e "s/, raise.*//" | grep ^File
+# This location filter is erasing information from the backtrace
+# to be robust to different inlining choices made by different compiler settings.
+# It checks that the expected locations occur (in the expected order).
+sed -e "s/^.*in file/File/" -e 's/ (inlined)//' | grep ^File
