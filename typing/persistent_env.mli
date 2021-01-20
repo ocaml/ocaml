d4df6a9dfeae9536e62acfe4af5ddb8ecc911bc0
Thomas Refis <thomas.refis@gmail.com>
2021-01-20 08:47:39 +0100

    Rename and extract EnvLazy to its own file (#10052)

--- typing/persistent_env.mli
+++ typing/persistent_env.mli
@@ -43,7 +43,7 @@ end
 
 type can_load_cmis =
   | Can_load_cmis
-  | Cannot_load_cmis of Misc.EnvLazy.log
+  | Cannot_load_cmis of Lazy_backtrack.log
 
 type 'a t
 
--- utils/misc.mli
+++ utils/misc.mli
@@ -465,29 +465,6 @@ type crcs = (modname * Digest.t option) list
 type alerts = string Stdlib.String.Map.t
 
 
-module EnvLazy: sig
-  type ('a,'b) t
-
-  type log
-
-  val force : ('a -> 'b) -> ('a,'b) t -> 'b
-  val create : 'a -> ('a,'b) t
-  val get_arg : ('a,'b) t -> 'a option
-  val create_forced : 'b -> ('a, 'b) t
-  val create_failed : exn -> ('a, 'b) t
-
-  (* [force_logged log f t] is equivalent to [force f t] but if [f]
-     returns [Error _] then [t] is recorded in [log]. [backtrack log]
-     will then reset all the recorded [t]s back to their original
-     state. *)
-  val log : unit -> log
-  val force_logged :
-    log -> ('a -> ('b, 'c) result) -> ('a,('b, 'c) result) t -> ('b, 'c) result
-  val backtrack : log -> unit
-
-end
-
-
 module Magic_number : sig
   (** a typical magic number is "Caml1999I011"; it is formed of an
       alphanumeric prefix, here Caml1990I, followed by a version,
