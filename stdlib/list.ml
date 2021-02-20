5d7836809d457e9eaa6b71f5f8f5d75f64fa8a09
Josh Berdine <josh@berdine.net>
2021-02-20 12:33:27 +0000

    Add a fast path to List.compare for physically equal lists
    Signed-off-by: Josh Berdine <josh@berdine.net>

--- stdlib/list.ml
+++ stdlib/list.ml
@@ -563,14 +563,16 @@ let rec equal eq l1 l2 =
   | a1::l1, a2::l2 -> eq a1 a2 && equal eq l1 l2
 
 let rec compare cmp l1 l2 =
-  match l1, l2 with
-  | [], [] -> 0
-  | [], _::_ -> -1
-  | _::_, [] -> 1
-  | a1::l1, a2::l2 ->
-    let c = cmp a1 a2 in
-    if c <> 0 then c
-    else compare cmp l1 l2
+  if l1 == l2 then 0
+  else
+    match l1, l2 with
+    | [], [] -> 0
+    | [], _::_ -> -1
+    | _::_, [] -> 1
+    | a1::l1, a2::l2 ->
+      let c = cmp a1 a2 in
+      if c <> 0 then c
+      else compare cmp l1 l2
 
 (** {1 Iterators} *)
 
