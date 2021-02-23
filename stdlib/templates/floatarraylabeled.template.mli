264102cd6c051f5980f34ebeeadae56e479a57cc
Nicolas Chataing <nicolas.chataing@ens.fr>
2021-02-23 12:32:28 +0100

    Add functions to manipulate floatarray

--- stdlib/templates/floatarraylabeled.template.mli
+++ stdlib/templates/floatarraylabeled.template.mli
@@ -171,6 +171,18 @@ val map2 : f:(float -> float -> float) -> t -> t -> t
     [[| f a.(0) b.(0); ...; f a.(length a - 1) b.(length b - 1)|]].
     @raise Invalid_argument if the floatarrays are not the same size. *)
 
+val map2_to_array : f:(float -> float -> 'a) -> t -> t -> 'a array
+(** [map2_to_array ~f a b] applies function [f] to all the elements
+    of the floatarrays [a] and [b], and builds an array with the
+    results returned by [f].
+   @raise Invalid_argument if the arrays are not the same size. *)
+
+val map2_with_array : f:(float -> 'a -> float) -> t -> 'a array -> t
+(** [map_with_array ~f a b] applies function [f] to all the elements
+   of the array [a] and the floatarray [b], and builds an array with
+   the results returned by [f].
+   @raise Invalid_argument if the arrays are not the same size. *)
+
 (** {1 Array scanning} *)
 
 val for_all : f:(float -> bool) -> t -> bool
@@ -294,6 +306,16 @@ val map_to_array : f:(float -> 'a) -> t -> 'a array
 val map_from_array : f:('a -> float) -> 'a array -> t
 (** [map_from_array ~f a] applies function [f] to all the elements of [a],
     and builds a floatarray with the results returned by [f]. *)
+
+val to_array : t -> float array
+(** [to_array a] builds the float array equivalent of a. *)
+
+val from_array : float array -> t
+(** [from_array a] builds the floatarray equivalent of a. *)
+
+val mapi_to_array : f:(int -> float -> 'a) -> t -> 'a array
+(** Same as {!map_to_array}, but the function is applied to the index of the
+    element as first argument, and the element itself as second argument. *)
 
 (** {1:floatarray_concurrency Arrays and concurrency safety}
 
