ffba08d8adfd3fef268a48cdc791ff562887c9fb
Alain Frisch <alain@frisch>
2021-03-01 09:55:04 +0100

    Add tests

--- testsuite/tests/prim-revapply/revapply.ml
+++ testsuite/tests/prim-revapply/revapply.ml
@@ -1,4 +1,5 @@
 (* TEST
+   flags="-w +48"
 *)
 
 external ( |> ) : 'a -> ('a -> 'b) -> 'b = "%revapply"
@@ -19,3 +20,13 @@ let _ =
       3 |> f |> g |> h; (* 37 *)
       3 |> add 2 |> add 3 |> f |> g |> add 4; (* 260 *)
     ]
+
+
+(* PR#10081 *)
+let bump ?(cap = 100) x = min cap (x + 1)
+let _f x = x |> bump (* no warning 48 *)
+
+(* PR#10081 *)
+type t = A | B
+type s = A | B
+let _f (x : t) = x |> function A -> 0 | B -> 1
