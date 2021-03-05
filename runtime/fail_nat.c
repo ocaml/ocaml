dd28ac0cf4365bd0ea1bcc374cbc5e95a6f39bea
Xavier Leroy <xavierleroy@users.noreply.github.com>
2021-03-05 19:14:07 +0100

    Dynamically allocate the alternate signal stack
    In Glibc 2.34 and later, SIGSTKSZ may not be a compile-time constant.
    It is no longer possible to statically allocate the alternate signal
    stack for the main thread, as we've been doing for the last 25 years.
    
    This commit implements dynamic allocation of the alternate signal stack
    even for the main thread.  It reuses the code already in place to allocate
    the alternate signal stack for other threads.
    
    The alternate signal stack is freed when the main OCaml code / an OCaml thread
    stops.
    
    (partial back-port of PR#10266 and PR#10726)

--- runtime/fail_nat.c
+++ runtime/fail_nat.c
@@ -32,6 +32,8 @@
 #include "caml/roots.h"
 #include "caml/callback.h"
 
+extern void caml_terminate_signals(void);
+
 /* The globals holding predefined exceptions */
 
 typedef value caml_generated_constant[1];
@@ -62,7 +64,10 @@ CAMLno_asan
 void caml_raise(value v)
 {
   Unlock_exn();
-  if (Caml_state->exception_pointer == NULL) caml_fatal_uncaught_exception(v);
+  if (Caml_state->exception_pointer == NULL) {
+    caml_terminate_signals();
+    caml_fatal_uncaught_exception(v);
+  }
 
   while (Caml_state->local_roots != NULL &&
          (char *) Caml_state->local_roots < Caml_state->exception_pointer) {
