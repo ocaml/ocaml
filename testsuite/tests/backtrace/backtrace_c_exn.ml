17fa2e17204652d6e42109513e21891cc9afd581
Tom Kelly <ctk21@cl.cam.ac.uk>
2021-03-16 19:46:34 +0000

    Link for issue 486 and add Printexc.print_backtrace for backtrace_c_exn test

--- testsuite/tests/backtrace/backtrace_c_exn.ml
+++ testsuite/tests/backtrace/backtrace_c_exn.ml
@@ -4,15 +4,17 @@
    ocamlrunparam += ",b=1"
 *)
 
-(* A test for stack backtraces *)
+(* https://github.com/ocaml-multicore/ocaml-multicore/issues/498 *)
 external stubbed_raise : unit -> unit = "caml_498_raise"
 
 let raise_exn () = failwith "exn"
+
 let () = Callback.register "test_raise_exn" raise_exn
 
 let () =
   try
     stubbed_raise ()
   with
-  | exn -> Printexc.to_string exn |> print_endline
-
+  | exn ->
+    Printexc.to_string exn |> print_endline;
+    Printexc.print_backtrace stdout
