8217553e5934feff38e90b63faea52815f2cf221
KC Sivaramakrishnan <kc@kcsrk.info>
2021-03-19 09:43:41 +0530

    Fix bug in caml_lazy_update_to_forcing.
    The function now handles the case where the object may have been
    short-circuited. We also enforce ordering constraints in the function by
    using an atomic load instruction.
    
    Also document the atomicity expectations w.r.t GC in
    camlinternalLazy.ml.

--- runtime/obj.c
+++ runtime/obj.c
@@ -261,11 +261,22 @@ CAMLprim value caml_lazy_update_to_forcing (value v)
   tag_t tag;
   value field0;
 
+  tag = obj_tag(v);
+  if (tag != Lazy_tag && tag != Forcing_tag && tag != Forward_tag) {
+    /* v is not a lazy block. It must have been forced by another domain and
+     * short-circuited by the GC at a safepoint. */
+    return Val_int(4);
+  }
+
+  /* v is a lazy block with Lazy_tag, Forcing_tag or Forward_tag. */
   if (obj_update_tag (v, Lazy_tag, Forcing_tag)) {
+    /* Successfully update the tag to Forcing_tag */
     return Val_int(0);
   } else {
     field0 = Field(v,0);
-    tag = obj_tag(v); /* obj_tag should be read after field0 */
+    /* The tag has to be read again after the field field. We use [atomic_load]
+     * in order to enforce ordering between the two reads. */
+    tag = Tag_hd (atomic_load (Hp_atomic_val(v)));
 
     if (tag == Forcing_tag) {
       if (field0 == caml_ml_domain_id(Val_unit))
@@ -275,7 +286,13 @@ CAMLprim value caml_lazy_update_to_forcing (value v)
     } else if (tag == Forward_tag) {
       return Val_int(3);
     } else {
-      return Val_int(4);
+      CAMLassert (tag == Lazy_tag);
+      /* We may reach here if the tag of [v] has been reset to [Lazy_tag] using
+       * [caml_lazy_reset_to_lazy] since our own attempt at updating the tag of
+       * [v] in [obj_update_tag] failed. The compare and swap in
+       * [obj_update_tag] failed since it must have observed [Forcing_tag].
+       * This represents a racy update of the tag of [v] by another mutator. */
+      return Val_int(2);
     }
   }
 }
