43c39e1a6e90c28a4a66a604bbe172b9808f3275
Tom Kelly <ctk21@cl.cam.ac.uk>
2021-04-07 20:27:11 +0100

    Remove unused tak. Improve termination condition of test_finaliser_gc to handle larger minor heaps and also exit quicker when success

--- testsuite/tests/callback/test_finaliser_gc.ml
+++ testsuite/tests/callback/test_finaliser_gc.ml
@@ -1,15 +1,12 @@
 (* TEST
 *)
 
-let rec tak (x, y, z as _tuple) =
-  if x > y then tak(tak (x-1, y, z), tak (y-1, z, x), tak (z-1, x, y))
-           else z
-
-let finaliser_status = ref "Finaliser NOT run"
 let z = ref (0, 1, 2, 3, 4, 5, 6, 7)
+let finaliser_pending = ref true
 
 let force_gc_fn _ =
-  finaliser_status := "Finaliser has run!";
+  print_string "Finaliser has run!"; print_newline();
+  finaliser_pending := false;
   Gc.full_major ()
 
 let trigger_finaliser () =
@@ -20,12 +17,15 @@ let trigger_finaliser () =
   let s = String.make 5 'b' in
   (* Spin on the minor heap allocating but keep [s] in a
     register and force a major cycle such that the
-    finaliser runs *)
-  for x = 0 to 10_000_000 do
-    z := (x, x, x, x, x, x, x, x);
+    finaliser runs.
+    NB: we quit after ~8B words allocated should something
+    be broken with finalisers *)
+  let x = ref 0 in
+  while (!x < 1_000_000_000) && !finaliser_pending do
+    z := (!x, !x, !x, !x, !x, !x, !x, !x);
+    incr x;
   done;
   s
 
 let _ =
   print_string (trigger_finaliser ()); print_newline();
-  print_string (!finaliser_status); print_newline()
