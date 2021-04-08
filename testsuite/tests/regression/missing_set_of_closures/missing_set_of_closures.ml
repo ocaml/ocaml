6d6929074608e75e8933b33b30c4f1d535d7cd77
Sébastien Hinderer <Sebastien.Hinderer@inria.fr>
2021-04-08 16:33:00 +0200

    Rewrite the missing_set_of_closures test to use ocamltest's subdirectories variable

--- testsuite/tests/regression/missing_set_of_closures/missing_set_of_closures.ml
+++ testsuite/tests/regression/missing_set_of_closures/missing_set_of_closures.ml
@@ -1,22 +1,19 @@
 (* TEST
 readonly_files = "a.ml b.ml b2.ml"
+subdirectories = "dir"
 * setup-ocamlopt.byte-build-env
-** script
-script = "mkdir -p dir"
-*** script
-script = "cp ${test_source_directory}/dir/c.ml dir/"
-**** ocamlopt.byte
+** ocamlopt.byte
 module = "a.ml"
-***** ocamlopt.byte
+*** ocamlopt.byte
 module = "b.ml"
-****** ocamlopt.byte
+**** ocamlopt.byte
 module = "b2.ml"
-******* script
+***** script
 script = "cp b.cmx b.cmi b2.cmx b2.cmi dir/"
-******** cd
+****** cd
 cwd = "dir"
-********* ocamlopt.byte
+******* ocamlopt.byte
 module = "c.ml"
 flags = "-w -58"
-********** check-ocamlopt.byte-output
+******** check-ocamlopt.byte-output
 *)
