5c1c1a84e49e5b6bc15d4afc5c9340ca8c4a82fb
Oghenevwogaga Ebresafe <ebresafegaga@outlook.com>
2021-04-20 15:56:54 +0100

    Added since annotation

--- stdlib/format.mli
+++ stdlib/format.mli
@@ -1124,7 +1124,9 @@ val pp_print_either :
   left:(formatter -> 'a -> unit) ->
   right:(formatter -> 'b -> unit) -> formatter -> ('a, 'b) Either.t -> unit
 (** [pp_print_either ~left ~right ppf e] prints [e] on [ppf] using
-    [left] if [e] is [Either.Left _] and [right] if [e] is [Either.Right _]. *)
+    [left] if [e] is [Either.Left _] and [right] if [e] is [Either.Right _]. 
+    
+    @since 4.13 *)
 
 (** {1:fpp Formatted pretty-printing} *)
 
