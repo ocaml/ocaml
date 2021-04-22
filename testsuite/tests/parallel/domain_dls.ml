07e15fb47b02aef4079f76ece2c455f95a49a499
Sudha Parimala <sudharg247@gmail.com>
2021-04-22 16:22:21 +0530

    coverage of domain life-cycle in domain_dls and ephetest_par tests

--- testsuite/tests/parallel/domain_dls.ml
+++ testsuite/tests/parallel/domain_dls.ml
@@ -5,7 +5,7 @@ include unix
 ** native
 *)
 
-let check () =
+let check_dls () =
   let k1 = Domain.DLS.new_key (fun () -> 10) in
   let k2 = Domain.DLS.new_key (fun () -> 1.0) in
   Domain.DLS.set k1 100;
@@ -16,8 +16,24 @@ let check () =
   assert (v2 = 200.0);
   Gc.major ()
 
-let _ =
-  let domains = Array.init 3 (fun _ -> Domain.spawn(check)) in
-  check ();
+let check_dls_domain_reuse () =
+  let k1 = Domain.DLS.new_key (fun () -> 100) in
+  let k2 = Domain.DLS.new_key (fun () -> 200) in
+  let domains = Array.init 4 (fun _ -> Domain.spawn(fun _ ->
+    Domain.DLS.set k1 1000;
+    Domain.DLS.set k2 2000;
+    assert (Domain.DLS.get k1 = 1000);
+    assert (Domain.DLS.get k2 = 2000))) in
   Array.iter Domain.join domains;
+  Gc.full_major ();
+  let domains2 = Array.init 4 (fun _ -> Domain.spawn(fun _ ->
+    assert(Domain.DLS.get k1 = 100);
+    assert(Domain.DLS.get k2 = 200))) in
+  Array.iter Domain.join domains2
+
+let _ =
+  let domains = Array.init 3 (fun _ -> Domain.spawn(check_dls)) in
+  check_dls ();
+  Array.iter Domain.join domains;
+  check_dls_domain_reuse ();
   print_endline "ok"
