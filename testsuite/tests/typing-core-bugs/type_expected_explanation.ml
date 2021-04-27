64044dd668962a3b219eca1a5c9ba22954aae47c
octachron <octa@polychoron.fr>
2021-04-27 08:29:33 +0200

    tweak error message for unknown constructors or fields
    Replace
    
      The constructor X does not belong to type t
    
    by
    
      There is no constructor X in type t
    
    to avoid suggesting that the constructor X exists when the compiler
    has neither the information nor the philosophical credentials to back
    up such ontological statement.

--- testsuite/tests/typing-core-bugs/type_expected_explanation.ml
+++ testsuite/tests/typing-core-bugs/type_expected_explanation.ml
@@ -172,7 +172,7 @@ Line 3, characters 22-26:
                           ^^^^
 Error: This variant expression is expected to have type unit
          because it is in the result of a conditional with no else branch
-       The constructor :: does not belong to type unit
+       There is no constructor :: within type unit
 |}];;
 
 (function
@@ -196,5 +196,5 @@ Line 1, characters 35-39:
                                        ^^^^
 Error: This variant expression is expected to have type unit
          because it is in the result of a conditional with no else branch
-       The constructor true does not belong to type unit
+       There is no constructor true within type unit
 |}]
