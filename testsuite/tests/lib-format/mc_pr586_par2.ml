2b2012b14641716a819ebe15ae67f5aa664cd3e0
KC Sivaramakrishnan <kc@kcsrk.info>
2021-06-24 09:39:33 +0530

    Fix test

--- testsuite/tests/lib-format/mc_pr586_par2.ml
+++ testsuite/tests/lib-format/mc_pr586_par2.ml
@@ -1,11 +1,13 @@
 (* TEST *)
 
 let () =
+  let fmt_key = Format.synchronized_formatter_of_out_channel stdout in
   let domains = Array.init 7 (fun i ->
     Domain.spawn (fun () ->
+          let fmt = Domain.DLS.get fmt_key in
           for j = 1 to 10000000 do () done;
           for j = 1 to 100 do
-            Format.printf "21. +. 21. is %f\n" (21. +. 21.);
+            Format.fprintf fmt "21. +. 21. is %f@." (21. +. 21.);
           done
       )
     ) in
