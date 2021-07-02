502ffe881f2e7b1b9164dceeccbd901de70f2b82
Mark Shinwell <mshinwell@gmail.com>
2021-07-02 10:23:07 +0100

    Alter handling of the caml_apply special case

--- asmcomp/polling.mli
+++ asmcomp/polling.mli
@@ -23,4 +23,4 @@ val instrument_fundecl : future_funcnames:Misc.Stdlib.String.Set.t
     -> Mach.fundecl -> Mach.fundecl
 
 val requires_prologue_poll : future_funcnames:Misc.Stdlib.String.Set.t
-    -> Mach.instruction -> bool
+    -> fun_name:string -> Mach.instruction -> bool
