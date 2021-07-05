db4be8bf842416166361c8fcc631f0d6a1a3d38a
Mark Shinwell <mshinwell@gmail.com>
2021-07-05 10:07:55 +0100

    Remove fun_suppress_polls

--- asmcomp/cmm_helpers.ml
+++ asmcomp/cmm_helpers.ml
@@ -1882,7 +1882,6 @@ let send_function arity =
     fun_args = List.map (fun (arg, ty) -> VP.create arg, ty) fun_args;
     fun_body = body;
     fun_codegen_options = [];
-    fun_suppress_polls = false;
     fun_dbg;
    }
 
@@ -1896,7 +1895,6 @@ let apply_function arity =
     fun_args = List.map (fun arg -> (VP.create arg, typ_val)) all_args;
     fun_body = body;
     fun_codegen_options = [];
-    fun_suppress_polls = true;
     fun_dbg;
    }
 
@@ -1925,7 +1923,6 @@ let tuplify_function arity =
           :: access_components 0 @ [Cvar clos],
           (dbg ()));
     fun_codegen_options = [];
-    fun_suppress_polls = false;
     fun_dbg;
    }
 
@@ -1995,7 +1992,6 @@ let final_curry_function arity =
     fun_args = [VP.create last_arg, typ_val; VP.create last_clos, typ_val];
     fun_body = curry_fun [] last_clos (arity-1);
     fun_codegen_options = [];
-    fun_suppress_polls = false;
     fun_dbg;
    }
 
@@ -2030,7 +2026,6 @@ let rec intermediate_curry_functions arity num =
                  Cvar arg; Cvar clos],
                 dbg ());
       fun_codegen_options = [];
-      fun_suppress_polls = false;
       fun_dbg;
      }
     ::
@@ -2070,7 +2065,6 @@ let rec intermediate_curry_functions arity num =
                fun_body = iter (num+1)
                   (List.map (fun (arg,_) -> Cvar arg) direct_args) clos;
                fun_codegen_options = [];
-               fun_suppress_polls = false;
                fun_dbg;
               }
           in
@@ -2608,7 +2602,6 @@ let entry_point namelist =
              fun_args = [];
              fun_body = body;
              fun_codegen_options = [Reduce_code_size];
-             fun_suppress_polls = false;
              fun_dbg;
             }
 
