7a529fbafaede7f39e51ec52803133961bfc5848
David Allsopp <david.allsopp@metastack.com>
2021-07-05 11:28:12 +0100

    Abstract wmain/main difference with main_os
    Introduce main_os in misc.h to allow:
    
     #ifdef _WIN32
     int wmain(int argc, wchar_t **argv)
     #else
     int main(int argc, char **argv)
     #endif
    
    to be written:
    
     int main_os(int argc, char_os **argv)

--- runtime/caml/misc.h
+++ runtime/caml/misc.h
@@ -278,6 +278,8 @@ extern double caml_log1p(double);
 
 #ifdef CAML_INTERNALS
 #define T(x) L ## x
+
+#define main_os wmain
 #endif
 
 #define access_os _waccess
@@ -316,6 +318,8 @@ extern double caml_log1p(double);
 
 #ifdef CAML_INTERNALS
 #define T(x) x
+
+#define main_os main
 #endif
 
 #define access_os access
