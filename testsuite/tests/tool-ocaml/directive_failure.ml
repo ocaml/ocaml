4819e284477a8cb544a15ad26277778690e0fd41
octachron <octa@polychoron.fr>
2021-07-07 17:02:13 +0200

    Fix test

--- testsuite/tests/tool-ocaml/directive_failure.ml
+++ testsuite/tests/tool-ocaml/directive_failure.ml
@@ -1,10 +1,9 @@
 (* TEST
-include tool-ocaml-lib
 ocaml_script_as_argument = "true"
-ocaml_exit_status = "2"
+ocaml_exit_status = "125"
 * setup-ocaml-build-env
 ** ocaml
 *)
 
-#use "";;
-let () = ()
+#use "no";;
+let () = () ;;
