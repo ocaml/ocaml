71747724c4d55f037fae94acce1a75ffe8938bb6
David Allsopp <david.allsopp@metastack.com>
2021-07-11 15:53:15 +0100

    Assume presence of mkfifo in libunix

--- otherlibs/unix/mkfifo.c
+++ otherlibs/unix/mkfifo.c
@@ -21,8 +21,6 @@
 #include <caml/signals.h>
 #include "unixsupport.h"
 
-#ifdef HAS_MKFIFO
-
 CAMLprim value caml_unix_mkfifo(value path, value mode)
 {
   CAMLparam2(path, mode);
@@ -38,36 +36,3 @@ CAMLprim value caml_unix_mkfifo(value path, value mode)
     caml_uerror("mkfifo", path);
   CAMLreturn(Val_unit);
 }
-
-#else
-
-#include <sys/types.h>
-#include <sys/stat.h>
-
-#ifdef S_IFIFO
-
-CAMLprim value caml_unix_mkfifo(value path, value mode)
-{
-  CAMLparam2(path, mode);
-  char * p;
-  int ret;
-  caml_unix_check_path(path, "mkfifo");
-  p = caml_stat_strdup(String_val(path));
-  caml_enter_blocking_section();
-  ret = mknod(p, (Int_val(mode) & 07777) | S_IFIFO, 0);
-  caml_leave_blocking_section();
-  caml_stat_free(p);
-  if (ret == -1)
-    caml_uerror("mkfifo", path);
-  CAMLreturn(Val_unit);
-}
-
-#else
-
-CAMLprim value caml_unix_mkfifo(value path, value mode)
-{
-  caml_invalid_argument("mkfifo not implemented");
-}
-
-#endif
-#endif
