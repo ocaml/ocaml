2e25ff5b01413d0133ce991c32f7d330cb4a6496
David Allsopp <david.allsopp@metastack.com>
2021-07-11 13:53:46 +0100

    Assume constants from sys/stat.h in libunix

--- otherlibs/unix/stat.c
+++ otherlibs/unix/stat.c
@@ -27,19 +27,6 @@
 #include "unixsupport.h"
 #include "cst2constr.h"
 
-#ifndef S_IFLNK
-#define S_IFLNK 0
-#endif
-#ifndef S_IFIFO
-#define S_IFIFO 0
-#endif
-#ifndef S_IFSOCK
-#define S_IFSOCK 0
-#endif
-#ifndef S_IFBLK
-#define S_IFBLK 0
-#endif
-
 static int file_kind_table[] = {
   S_IFREG, S_IFDIR, S_IFCHR, S_IFBLK, S_IFLNK, S_IFIFO, S_IFSOCK
 };
