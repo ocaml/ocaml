7e5ea949b1737b36e531cbe285ea2aff8efc270c
Jacques Garrigue <garrigue@math.nagoya-u.ac.jp>
2021-07-20 12:03:36 +0900

    promote

--- testsuite/tests/typing-modules/private.ml
+++ testsuite/tests/typing-modules/private.ml
@@ -5,14 +5,27 @@
 module M :
      sig type t = private [< `A | `B of string] end
 = struct type t = [`A|`B of string] end;;
-[%%expect]
+[%%expect{|
+module M : sig type t = private [< `A | `B of string ] end
+|}]
 
 module M = struct type header_item_tag =
     [ `CO | `HD | `Other of string | `PG | `RG | `SQ ]
 end;;
-[%%expect]
+[%%expect{|
+module M :
+  sig
+    type header_item_tag = [ `CO | `HD | `Other of string | `PG | `RG | `SQ ]
+  end
+|}]
 
 module M' : sig type header_item_tag =
     private [< `CO | `HD | `Other of string | `PG | `RG | `SQ ]
 end = M;;
-[%%expect]
+[%%expect{|
+module M' :
+  sig
+    type header_item_tag = private
+        [< `CO | `HD | `Other of string | `PG | `RG | `SQ ]
+  end
+|}]
