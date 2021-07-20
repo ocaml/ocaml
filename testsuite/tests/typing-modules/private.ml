5acd2078ada1117624c6b4d4673de86412bf4af9
Jacques Garrigue <garrigue@math.nagoya-u.ac.jp>
2021-07-20 11:35:37 +0900

    Fix polymorphic variant inclusion

---
+++ testsuite/tests/typing-modules/private.ml
@@ -0,0 +1,18 @@
+(* TEST
+  * expect
+ *)
+
+module M :
+     sig type t = private [< `A | `B of string] end
+= struct type t = [`A|`B of string] end;;
+[%%expect]
+
+module M = struct type header_item_tag =
+    [ `CO | `HD | `Other of string | `PG | `RG | `SQ ]
+end;;
+[%%expect]
+
+module M' : sig type header_item_tag =
+    private [< `CO | `HD | `Other of string | `PG | `RG | `SQ ]
+end = M;;
+[%%expect]
