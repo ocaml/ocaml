3b22f2f9122dd47da68cabcd5c7075dc3b7492d1
Nicolás Ojeda Bär <n.oje.bar@gmail.com>
2021-08-05 23:21:12 +0200

    Update manual

--- man/ocaml.m
+++ man/ocaml.m
@@ -263,6 +263,9 @@ not empty or "dumb", and that isatty(stderr) holds.
 The environment variable "OCAML_COLOR" is considered if \-color is not
 provided. Its values are auto/always/never as above.
 
+If \-color is not provided, "OCAML_COLOR" is not set and the environment
+variable "NO_COLOR" is set, then color output is disabled.
+
 .TP
 .BI \-error\-style \ mode
 Control the way error messages and warnings are printed.
