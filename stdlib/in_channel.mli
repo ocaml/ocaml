c7ca38b5af889661df4d4d72cf3b490c9853197e
Nicolás Ojeda Bär <n.oje.bar@gmail.com>
2021-08-16 19:10:37 +0200

    Add docstrings

--- stdlib/in_channel.mli
+++ stdlib/in_channel.mli
@@ -1,46 +1,144 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*             Xavier Leroy, projet Cristal, INRIA Rocquencourt           *)
+(*                                                                        *)
+(*   Copyright 2021 Institut National de Recherche en Informatique et     *)
+(*     en Automatique.                                                    *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
+(** Input channels
+
+    @since 4.14.0 *)
+
 type t = in_channel
+(** The type of input channel. *)
 
 type open_flag = Stdlib.open_flag =
-  | Open_rdonly
-  | Open_wronly
-  | Open_append
-  | Open_creat
-  | Open_trunc
-  | Open_excl
-  | Open_binary
-  | Open_text
-  | Open_nonblock
+  | Open_rdonly      (** open for reading. *)
+  | Open_wronly      (** open for writing. *)
+  | Open_append      (** open for appending: always write at end of file. *)
+  | Open_creat       (** create the file if it does not exist. *)
+  | Open_trunc       (** empty the file if it already exists. *)
+  | Open_excl        (** fail if Open_creat and the file already exists. *)
+  | Open_binary      (** open in binary mode (no conversion). *)
+  | Open_text        (** open in text mode (may perform conversions). *)
+  | Open_nonblock    (** open in non-blocking mode. *)
+(** Opening modes for {!open_gen}. *)
 
 val stdin : t
+(** The standard input for the process. *)
 
 val open' : string -> t
+(** Open the named file for reading, and return a new input channel on that
+    file, positioned at the beginning of the file. *)
 
 val open_bin : string -> t
+(** Same as {!open'}, but the file is opened in binary mode, so that no
+    translation takes place during reads. On operating systems that do not
+    distinguish between text mode and binary mode, this function behaves like
+    {!open'}. *)
 
 val open_gen : open_flag list -> t
+(** [open_gen mode perm filename] opens the named file for reading, as described
+    above. The extra arguments [mode] and [perm] specify the opening mode and
+    file permissions.  {!open'} and {!open_bin} are special cases of this
+    function. *)
 
 val seek : t -> int64 -> unit
+(** [seek chan pos] sets the current reading position to [pos] for channel
+    [chan]. This works only for regular files. On files of other kinds, the
+    behavior is unspecified. *)
 
 val pos : t -> int64
+(** Return the current reading position for the given channel.  For files opened
+    in text mode under Windows, the returned position is approximate (owing to
+    end-of-line conversion); in particular, saving the current position with
+    {!pos}, then going back to this position using {!seek} will not work.  For
+    this programming idiom to work reliably and portably, the file must be
+    opened in binary mode. *)
 
 val length : t -> int64
+(** Return the size (number of characters) of the regular file on which the
+    given channel is opened.  If the channel is opened on a file that is not a
+    regular file, the result is meaningless.  The returned size does not take
+    into account the end-of-line translations that can be performed when reading
+    from a channel opened in text mode. *)
 
 val close : t -> unit
+(** Close the given channel.  Input functions raise a [Sys_error] exception when
+    they are applied to a closed input channel, except {!close}, which does
+    nothing when applied to an already closed channel. *)
 
 val close_noerr : t -> unit
-
-val input_byte : t -> int option
+(** Same as {!close}, but ignore all errors. *)
 
 val input_char : t -> char option
+(** Read one character from the given input channel.  Returns [None] if there
+    are no more characters to read. *)
+
+val input_byte : t -> int option
+(** Same as {!input_char}, but return the 8-bit integer representing the
+    character. Returns [None] if an end of file was reached. *)
 
 val input_line : t -> string option
+(** Read characters from the given input channel, until a newline character is
+    encountered. Return the string of all characters read, without the newline
+    character at the end.  Returns [None] if the end of the file is reached at
+    the beginning of line. *)
 
 val input_binary_int : t -> int option
+(** Read an integer encoded in binary format (4 bytes, big-endian) from the
+    given input channel. See {!Out_channel.output_binary_int}.  Returns [None] if an
+    end of file was reached while reading the integer. *)
 
 val input : t -> bytes -> int -> int -> int
+(** [input ic buf pos len] reads up to [len] characters from the given channel
+    [ic], storing them in byte sequence [buf], starting at character number
+    [pos].  It returns the actual number of characters read, between 0 and [len]
+    (inclusive).
+
+    A return value of 0 means that the end of file was reached.
+
+    A return value between 0 and [len] exclusive means that not all requested
+    [len] characters were read, either because no more characters were available
+    at that time, or because the implementation found it convenient to do a
+    partial read; {!input} must be called again to read the remaining characters,
+    if desired.  (See also {!really_input} for reading exactly [len]
+    characters.)
+
+    @raise Invalid_argument if [pos] and [len] do not designate a valid range of
+    [buf]. *)
 
 val really_input : t -> bytes -> int -> int -> unit option
+(** [really_input ic buf pos len] reads [len] characters from channel [ic],
+    storing them in byte sequence [buf], starting at character number [pos].
+
+    Returns [None] if the end of file is reached before [len] characters have
+    been read.
+
+    @raise Invalid_argument if [pos] and [len] do not designate a valid range of
+    [buf]. *)
 
 val really_input_string : t -> int -> string option
+(** [really_input_string ic len] reads [len] characters from channel [ic] and
+    returns them in a new string.  Returns [None] if the end of file is reached
+    before [len] characters have been read. *)
 
 val set_binary_mode : t -> bool -> unit
+(** [set_binary_mode ic true] sets the channel [ic] to binary mode: no
+    translations take place during input.
+
+    [set_binary_mode ic false] sets the channel [ic] to text mode: depending
+    on the operating system, some translations may take place during input.  For
+    instance, under Windows, end-of-lines will be translated from [\r\n] to
+    [\n].
+
+    This function has no effect under operating systems that do not distinguish
+    between text mode and binary mode. *)
--- stdlib/out_channel.mli
+++ stdlib/out_channel.mli
@@ -1,52 +1,136 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*             Xavier Leroy, projet Cristal, INRIA Rocquencourt           *)
+(*                                                                        *)
+(*   Copyright 2021 Institut National de Recherche en Informatique et     *)
+(*     en Automatique.                                                    *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
+(** Output channels
+
+    @since 4.14.0 *)
+
 type t = out_channel
+(** The type of output channel. *)
 
 type open_flag = Stdlib.open_flag =
-  | Open_rdonly
-  | Open_wronly
-  | Open_append
-  | Open_creat
-  | Open_trunc
-  | Open_excl
-  | Open_binary
-  | Open_text
-  | Open_nonblock
+  | Open_rdonly      (** open for reading. *)
+  | Open_wronly      (** open for writing. *)
+  | Open_append      (** open for appending: always write at end of file. *)
+  | Open_creat       (** create the file if it does not exist. *)
+  | Open_trunc       (** empty the file if it already exists. *)
+  | Open_excl        (** fail if Open_creat and the file already exists. *)
+  | Open_binary      (** open in binary mode (no conversion). *)
+  | Open_text        (** open in text mode (may perform conversions). *)
+  | Open_nonblock    (** open in non-blocking mode. *)
+(** Opening modes for {!open_gen}. *)
 
 val stdout : t
+(** The standard output for the process. *)
 
 val stderr : t
+(** The standard error output for the process. *)
 
 val open' : string -> t
+(** Open the named file for writing, and return a new output channel on that
+    file, positioned at the beginning of the file. The file is truncated to zero
+    length if it already exists. It is created if it does not already exists. *)
 
 val open_bin : string -> t
+(** Same as {!open'}, but the file is opened in binary mode, so that no
+    translation takes place during writes. On operating systems that do not
+    distinguish between text mode and binary mode, this function behaves like
+    {!open'}. *)
 
 val open_gen : open_flag list -> string -> t
+(** [open_gen mode perm filename] opens the named file for writing, as described
+    above. The extra argument [mode] specifies the opening mode. The extra
+    argument [perm] specifies the file permissions, in case the file must be
+    created.  {!open'} and {!open_bin} are special cases of this function. *)
 
 val seek : t -> int64 -> unit
+(** [seek chan pos] sets the current writing position to [pos] for channel
+    [chan]. This works only for regular files. On files of other kinds (such as
+    terminals, pipes and sockets), the behavior is unspecified. *)
 
 val pos : t -> int64
+(** Return the current writing position for the given channel.  Does not work on
+    channels opened with the [Open_append] flag (returns unspecified results).
+
+    For files opened in text mode under Windows, the returned position is
+    approximate (owing to end-of-line conversion); in particular, saving the
+    current position with {!pos}, then going back to this position using {!seek}
+    will not work.  For this programming idiom to work reliably and portably,
+    the file must be opened in binary mode. *)
 
 val length : t -> int64
+(** Return the size (number of characters) of the regular file on which the
+    given channel is opened.  If the channel is opened on a file that is not a
+    regular file, the result is meaningless. *)
 
 val close : t -> unit
+(** Close the given channel, flushing all buffered write operations.  Output
+    functions raise a [Sys_error] exception when they are applied to a closed
+    output channel, except {!close} and {!flush}, which do nothing when applied
+    to an already closed channel.  Note that {!close} may raise [Sys_error] if
+    the operating system signals an error when flushing or closing. *)
 
 val close_noerr : t -> unit
+(** Same as {!close}, but ignore all errors. *)
 
 val flush : t -> unit
+(** Flush the buffer associated with the given output channel, performing all
+    pending writes on that channel.  Interactive programs must be careful about
+    flushing standard output and standard error at the right time. *)
 
 val flush_all : unit -> unit
-
-val output_byte : t -> int -> unit
+(** Flush all open output channels; ignore errors. *)
 
 val output_char : t -> char -> unit
+(** Write the character on the given output channel. *)
+
+val output_byte : t -> int -> unit
+(** Write one 8-bit integer (as the single character with that code) on the
+    given output channel. The given integer is taken modulo 256. *)
 
 val output_string : t -> string -> unit
+(** Write the string on the given output channel. *)
 
 val output_bytes : t -> bytes -> unit
+(** Write the byte sequence on the given output channel. *)
 
 val output : t -> bytes -> int -> int -> unit
+(** [output oc buf pos len] writes [len] characters from byte sequence [buf],
+    starting at offset [pos], to the given output channel [oc].
+
+    @raise Invalid_argument if [pos] and [len] do not designate a valid range of
+    [buf]. *)
 
 val output_substring : t -> string -> int -> int -> unit
+(** Same as {!output} but take a string as argument instead of a byte
+    sequence. *)
 
 val output_binary_int : t -> int -> unit
+(** Write one integer in binary format (4 bytes, big-endian) on the given output
+    channel.  The given integer is taken modulo 2{^32}.  The only reliable way
+    to read it back is through the {!In_channel.input_binary_int} function. The
+    format is compatible across all machines for a given version of OCaml. *)
 
 val set_binary_mode : t -> bool -> unit
+(** [set_binary_mode oc true] sets the channel [oc] to binary mode: no
+    translations take place during output.
+
+    [set_binary_mode oc false] sets the channel [oc] to text mode: depending on
+    the operating system, some translations may take place during output.  For
+    instance, under Windows, end-of-lines will be translated from [\n] to
+    [\r\n].
+
+    This function has no effect under operating systems that do not distinguish
+    between text mode and binary mode. *)
