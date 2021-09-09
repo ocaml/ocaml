9410b9c91ad5ef36d49d2c5afc12702103b4caca
Xavier Leroy <xavierleroy@users.noreply.github.com>
2021-09-09 09:26:45 +0200

    Limit CSE of integer constants (#10615)
    Never do CSE for constants that fit in 32 bits signed.
    
    Do it only for constants that do not fit, and only on some 64-bit targets
    where the code generated for Iconst_int can be costly (ARM64, POWER, RISC-V).
    
    Fixes: #10591

--- asmcomp/arm64/CSE.ml
+++ asmcomp/arm64/CSE.ml
@@ -31,7 +31,7 @@ method! class_of_operation op =
 
 method! is_cheap_operation op =
   match op with
-  | Iconst_int n -> n <= 65535n && n >= 0n
+  | Iconst_int n -> n <= 0x7FFF_FFFFn && n >= -0x8000_0000n
   | _ -> false
 
 end
