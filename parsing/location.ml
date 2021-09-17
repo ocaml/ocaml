680b3d2d169e295e3d7f93913584aaf9a38fd87e
Florian Angeletti <florian.angeletti@inria.fr>
2021-09-17 10:20:21 +0200

    review: factorize location interval computation

--- parsing/location.ml
+++ parsing/location.ml
@@ -173,19 +173,9 @@ let print_filename ppf file =
    Some of the information (filename, line number or characters numbers) in the
    location might be invalid; in which case we do not print it.
  *)
-let print_loc ppf loc =
-  setup_colors ();
-  let file_valid = function
-    | "_none_" ->
-        (* This is a dummy placeholder, but we print it anyway to please editors
-           that parse locations in error messages (e.g. Emacs). *)
-        true
-    | "" | "//toplevel//" -> false
-    | _ -> true
-  in
-  let line_valid line = line > 0 in
-  let chars_valid ~startchar ~endchar = startchar <> -1 && endchar <> -1 in
-
+type interval =
+  { file:string; startline: int; endline:int; startchar:int; endchar:int }
+let interval loc =
   let file =
     (* According to the comment in location.mli, if [pos_fname] is "", we must
        use [!input_name]. *)
@@ -196,6 +186,23 @@ let print_loc ppf loc =
   let endline = loc.loc_end.pos_lnum in
   let startchar = loc.loc_start.pos_cnum - loc.loc_start.pos_bol in
   let endchar = loc.loc_end.pos_cnum - loc.loc_end.pos_bol in
+  { file; startline; endline; startchar; endchar }
+
+let print_loc ppf loc =
+  setup_colors ();
+  let file_valid = function
+    | "_none_" ->
+        (* This is a dummy placeholder, but we print it anyway to please editors
+           that parse locations in error messages (e.g. Emacs). *)
+        true
+    | "" | "//toplevel//" -> false
+    | _ -> true
+  in
+  let chars_valid ~startchar ~endchar = startchar <> -1 && endchar <> -1 in
+  let {file; startline; endline; startchar; endchar} = interval loc in
+  let line_valid line = line > 0 in
+  let startline = if line_valid startline then startline else 1 in
+  let endline = if line_valid endline then endline else startline in
 
   let first = ref true in
   let capitalize s =
@@ -213,8 +220,6 @@ let print_loc ppf loc =
      existing setup of editors that parse locations in error messages (e.g.
      Emacs). *)
   comma ();
-  let startline = if line_valid startline then startline else 1 in
-  let endline = if line_valid endline then endline else startline in
   begin if startline = endline then
     Format.fprintf ppf "%s %i" (capitalize "line") startline
   else
@@ -803,10 +808,7 @@ let json_mode_printer: json_report_printer = fun json report ->
       if loc.loc_start.pos_fname = "" then !input_name
       else loc.loc_start.pos_fname
     in
-    let startchar = loc.loc_start.pos_cnum - loc.loc_start.pos_bol in
-    let endchar = loc.loc_end.pos_cnum - loc.loc_end.pos_bol in
-    let startline = loc.loc_start.pos_lnum in
-    let endline = loc.loc_end.pos_lnum in
+    let { startchar; endchar; startline; endline; _ } = interval loc in
     let start_end s e = `List [`Int s; `Int e] in
     `Assoc[
       "file", `String file;
