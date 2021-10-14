7c87c6440d095ad7ac5231c0efeb43128efa70e3
Xavier Leroy <xavier.leroy@college-de-france.fr>
2021-10-14 09:56:44 +0200

    Revised PRNG initialization and testing
    - Use binary digits of pi from Wolfram Alpha for the magic constants.
    
    - Test against Blackman and Vigna's reference C implementation
      with known initial state [| 1; 2; 4; 8 |]

--- testsuite/tests/basic-more/testrandom.ml
+++ testsuite/tests/basic-more/testrandom.ml
@@ -11,6 +11,11 @@ let _ =
   print_newline ();  print_newline ();
   for i = 0 to 20 do
     print_char ' '; print_float (float 1000.);
+  done;
+  print_newline ();  print_newline ();
+  full_init [| 1; 2; 4; 8 |];
+  for i = 0 to 49 do
+    Printf.printf "%Lx\n" (bits64 ())
   done
 
 let _ = exit 0
