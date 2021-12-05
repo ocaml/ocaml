0f784a8a8b858c420ed8a89fe4575809adfe5c88
Renato Alencar <renatoalencar.73@gmail.com>
2021-12-05 10:47:53 -0300

    Run test only on unix systems

--- testsuite/tests/output-complete-obj/test3.ml
+++ testsuite/tests/output-complete-obj/test3.ml
@@ -1,13 +1,14 @@
 (* TEST
- * setup-ocamlopt.opt-build-env
- ** ocamlopt.opt
-    program2 = "test3.${objext}"
-    flags = "-output-obj"
- *** run
-     program = "nm test3.o"
-     output = "${test_build_directory}/program-output"
-     stdout = "${output}"
- **** check-program-output
+ * unix
+ ** setup-ocamlopt.opt-build-env
+ *** ocamlopt.opt
+     program2 = "test3.${objext}"
+     flags = "-output-obj"
+ **** run
+      program = "nm test3.o"
+      output = "${test_build_directory}/program-output"
+      stdout = "${output}"
+ ***** check-program-output
 *)
 
 let () =
