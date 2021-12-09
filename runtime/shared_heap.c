e7ae89ccc0732e25d73f11c76e29f7de9bb2595b
Jeremy Yallop <yallop@gmail.com>
2021-12-09 21:30:24 +0000

    Make the global variable 'atoms' static.

--- runtime/shared_heap.c
+++ runtime/shared_heap.c
@@ -610,7 +610,7 @@ void caml_redarken_pool(struct pool* r, scanning_action f, void* fdata) {
 }
 
 
-const header_t atoms[256] = {
+static const header_t atoms[256] = {
 #define A(i) Make_header(0, i, NOT_MARKABLE)
 A(0),A(1),A(2),A(3),A(4),A(5),A(6),A(7),A(8),A(9),A(10),
 A(11),A(12),A(13),A(14),A(15),A(16),A(17),A(18),A(19),A(20),
