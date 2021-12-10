07e130494df1bff7fd7c694bf56258302f34c00e
KC Sivaramakrishnan <kc@kcsrk.info>
2021-12-10 17:48:37 +0530

    Use caml_global_heap_state instead of global

--- runtime/intern.c
+++ runtime/intern.c
@@ -388,7 +388,7 @@ static value intern_alloc_obj(struct caml_intern_state* s, caml_domain_state* d,
     if (p == NULL) {
       caml_raise_out_of_memory();
     }
-    Hd_hp(p) = Make_header (wosize, tag, global.MARKED);
+    Hd_hp(p) = Make_header (wosize, tag, caml_global_heap_state.MARKED);
   }
   return Val_hp(p);
 }
