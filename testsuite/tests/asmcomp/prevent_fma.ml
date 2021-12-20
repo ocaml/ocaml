4ffee8e18a92770e3c8d8b2d9c545a8292ddf907
Frédéric Bour <fred@tarides.com>
2021-12-20 09:01:29 +0100

    Simplify prevent_fma

--- testsuite/tests/asmcomp/prevent_fma.ml
+++ testsuite/tests/asmcomp/prevent_fma.ml
@@ -2,50 +2,21 @@
   * native
 *)
 
-let k_split = 134217729.0
-
-(* This file implements the "double double" inversion algorithm which relies on
-   floating point having exactly 64 bits of precision. Inserting
-   fused-multiply-add would exceed the precision and produce an incorrect
-   result.
-   See issue #10323. *)
-
 let ( *. ) x y = Sys.opaque_identity (x *. y)
-(** Using opaque_identity should prevent use of FMA,
-    that's the main thing tested by this file. *)
+(* Using opaque_identity should prevent use of FMA. *)
 
-type t = {hi: float; lo: float}
+let f x = (x *. x -. x *. x)
+    (* The expression above can be compiled in two ways:
+       1. First evaluating x' = x *. x, then x' -. x'
+       The result is obviously zero.
+       2. First evaluating x' = x *. x, then x *. x -. x' as a single evaluation
+       step, using fused-multiply-add (or rather sub here).
+       FMA computes with increased precision because no rounding of the
+       intermediate computation happens.
+       In this case, the result is not always exactly 0.
 
-let inv {hi; lo} =
-  (*C = 1.0/hi; *)
-  let c' = 1. /. hi in
-  (*c = SPLIT*C; *)
-  let c  = k_split *. c' in
-  (*hc =c-C;  *)
-  let hc = c -. c' in
-  (*u = SPLIT*hi;*)
-  let u  = k_split *. hi in
-  (*hc = c-hc; tc = C-hc; hy = u-hi; U = C*hi; hy = u-hy; ty = hi-hy;*)
-  let hc = c  -. hc in
-  let tc = c' -. hc in
-  let hy = u  -. hi in
-  let u' = c' *. hi in
-  let hy = u  -. hy in
-  let ty = hi -. hy in
-  (*u = (((hc*hy-U)+hc*ty)+tc*hy)+tc*ty;*)
-  let u  = (((hc *. hy -. u') +. hc *. ty) +. tc *. hy) +. tc *. ty in
-  (*c = ((((1.0-U)-u))-C*lo)/hi;*)
-  let c  = (((1.0 -. u') -. u) -. c' *. lo) /. hi in
-  let hi = c' +. c in
-  let lo = (c' -. hi) +. c in
-  {hi; lo}
+       See issue #10323. *)
+
 
 let () =
-  (*let a = {hi = 0.34; lo = 0.0} in
-  let a' = inv a in*)
-  let a' = {
-    hi = 2.9411764705882350590115947852609679102898;
-    lo = 0.0000000000000000238179334002628905858757;
-  } in
-  let a'' = inv a' in
-  assert (Int64.bits_of_float a''.lo = 0L)
+  assert (Int64.bits_of_float (f (sqrt 2.0)) = 0L)
