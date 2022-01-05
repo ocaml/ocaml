0b6f4b390d80565000de55d669839a71e9acb829
David Allsopp <david.allsopp@metastack.com>
2022-01-05 15:50:14 +0000

    Fix more display differences between ocamlnat/ocaml (#10849)
    Unites the toplevels still further. Previously, the native toplevel
    did not display output for:
    
      let _ : <type> = <expr>
    
    and related cases. The `match` for wildcard bindings is now
    shared between bytecode and native, so the native toplevel
    should always display values when the bytecode would.
    
    (cherry picked from commit c8730eef5971dc6c2de1518d1c9107eba3bd5617)

--- testsuite/tests/tool-toplevel/topeval.ml
+++ testsuite/tests/tool-toplevel/topeval.ml
@@ -3,6 +3,10 @@
    * toplevel.opt
 *)
 
+(* Various test-cases ensuring that the native and bytecode toplevels produce
+   the same output *)
+
+(* PR 10712 *)
 module A : sig
   type ('foo, 'bar) t
 
@@ -24,4 +28,20 @@ A.get_foo
 
 (* Type variables be 'a and 'b (original names lost in let-binding) *)
 let _bar = A.get_foo
+;;
+
+(* PR 10849 *)
+let _ : int = 42
+;;
+
+let (_ : bool) : bool = false
+;;
+
+let List.(_) = ""
+;;
+
+let List.(String.(_)) = 'd'
+;;
+
+let List.(_) : float = 42.0
 ;;
--- toplevel/native/topeval.ml
+++ toplevel/native/topeval.ml
@@ -181,21 +181,13 @@ let execute_phrase print_outcome ppf phr =
          bytecode and native toplevels always type-check _exactly_ the same
          expression. Adding the binding at the parsetree level (before typing)
          can create observable differences (e.g. in type variable names, see
-         tool-toplevel/pr10712.ml in the testsuite) *)
+         tool-toplevel/topeval.ml in the testsuite) *)
       let str, sg', rewritten =
-         match str.str_items with
-         | [ { str_desc = Tstr_eval (e, attrs) ; str_loc = loc } ]
-         | [ { str_desc = Tstr_value (Asttypes.Nonrecursive,
-                                       [{ vb_expr = e
-                                        ; vb_pat =
-                                            { pat_desc = Tpat_any;
-                                              pat_extra = []; _ }
-                                        ; vb_attributes = attrs }])
-             ; str_loc = loc }
-           ] ->
+         match find_eval_phrase str with
+         | Some (e, attrs, loc) ->
              let str, sg' = name_expression ~loc ~attrs e in
              str, sg', true
-         | _ -> str, sg', false
+         | None -> str, sg', false
       in
       let module_ident, res, required_globals, size =
         if Config.flambda then
