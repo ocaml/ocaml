6c0925a8a886924f2c83021422ccf065bf07e0f3
Nicolás Ojeda Bär <n.oje.bar@gmail.com>
2022-02-21 14:49:26 +0100

    st_posix.h: use caml_alloc_2

--- otherlibs/systhreads/st_posix.h
+++ otherlibs/systhreads/st_posix.h
@@ -364,10 +364,7 @@ static value st_encode_sigset(sigset_t * set)
 
   for (i = 1; i < NSIG; i++)
     if (sigismember(set, i) > 0) {
-      value newcons = caml_alloc_small(2, 0);
-      Field(newcons, 0) = Val_int(caml_rev_convert_signal_number(i));
-      Field(newcons, 1) = res;
-      res = newcons;
+      res = caml_alloc_2(2, Val_int(caml_rev_convert_signal_number(i)), res);
     }
   CAMLreturn(res);
 }
