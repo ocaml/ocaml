ocaml/ocaml

commit f022f9b9d2f85989d08308d636c93a35c612a4eb

Do not use `Begin_roots` / `End_roots` in runtime (#11002)
* Replace Begin_roots by CAMLparamX/CAMLlocalX
* st_posix.h: use caml_alloc_2
* st_stubs.c: get rid of caml_thread_descr
* select.c: use Val_emptylist
* winwait.c: use caml_alloc_boxed
* write.c: fix missing root registration

otherlibs/unix/sockopt.c
@@ -177,6 +177,9 @@ unix_getsockopt_aux(char * name,
                     enum option_type ty, int level, int option,
                     value socket)
 {
+  CAMLparam0();
+  CAMLlocal1(err);
+  value res;
   union option_value optval;
   socklen_param_type optsize;
 
@@ -200,32 +203,29 @@ unix_getsockopt_aux(char * name,
 
   switch (ty) {
   case TYPE_BOOL:
-    return Val_bool(optval.i);
+    res = Val_bool(optval.i);
   case TYPE_INT:
-    return Val_int(optval.i);
+    res = Val_int(optval.i);
   case TYPE_LINGER:
     if (optval.lg.l_onoff == 0) {
-      return Val_none;
+      res = Val_none;
     } else {
-      return caml_alloc_some(Val_int(optval.lg.l_linger));
+      res = caml_alloc_some(Val_int(optval.lg.l_linger));
     }
   case TYPE_TIMEVAL:
-    return caml_copy_double((double) optval.tv.tv_sec
-                       + (double) optval.tv.tv_usec / 1e6);
+    res = caml_copy_double((double) optval.tv.tv_sec
+                           + (double) optval.tv.tv_usec / 1e6);
   case TYPE_UNIX_ERROR:
     if (optval.i == 0) {
-      return Val_none;
+      res = Val_none;
     } else {
-      value err, res;
       err = unix_error_of_code(optval.i);
-      Begin_root(err);
-        res = caml_alloc_some(err);
-      End_roots();
-      return res;
+      res = caml_alloc_some(err);
     }
   default:
     unix_error(EINVAL, name, Nothing);
   }
+  CAMLreturn(res);
 }
 
 CAMLexport value

otherlibs/win32unix/sockopt.c
@@ -115,9 +115,11 @@ unix_getsockopt_aux(char * name,
                     enum option_type ty, int level, int option,
                     value socket)
 {
+  CAMLparam0();
+  CAMLlocal1(err);
   union option_value optval;
   socklen_param_type optsize;
-
+  value res;
 
   switch (ty) {
   case TYPE_BOOL:
@@ -141,31 +143,27 @@ unix_getsockopt_aux(char * name,
   switch (ty) {
   case TYPE_BOOL:
   case TYPE_INT:
-    return Val_int(optval.i);
+    res = Val_int(optval.i);
   case TYPE_LINGER:
     if (optval.lg.l_onoff == 0) {
-      return Val_none;
+      res = Val_none;
     } else {
-      return caml_alloc_some(Val_int(optval.lg.l_linger));
+      res = caml_alloc_some(Val_int(optval.lg.l_linger));
     }
   case TYPE_TIMEVAL:
-    return caml_copy_double((double) optval.tv.tv_sec
-                       + (double) optval.tv.tv_usec / 1e6);
+    res = caml_copy_double((double) optval.tv.tv_sec
+                           + (double) optval.tv.tv_usec / 1e6);
   case TYPE_UNIX_ERROR:
     if (optval.i == 0) {
-      return Val_none;
+      res = Val_none;
     } else {
-      value err, res;
       err = unix_error_of_code(optval.i);
-      Begin_root(err);
-        res = caml_alloc_some(err);
-      End_roots();
-      return res;
+      res = caml_alloc_some(err);
     }
   default:
     unix_error(EINVAL, name, Nothing);
-    return Val_unit; /* Avoid warning */
   }
+  CAMLreturn(res);
 }
 
 CAMLexport value

otherlibs/systhreads/st_posix.h
@@ -356,19 +356,17 @@ static void st_decode_sigset(value vset, sigset_t * set)
 
 static value st_encode_sigset(sigset_t * set)
 {
-  value res = Val_int(0);
+  CAMLparam0();
+  CAMLlocal1(res);
   int i;
 
-  Begin_root(res)
-    for (i = 1; i < NSIG; i++)
-      if (sigismember(set, i) > 0) {
-        value newcons = caml_alloc_small(2, 0);
-        Field(newcons, 0) = Val_int(caml_rev_convert_signal_number(i));
-        Field(newcons, 1) = res;
-        res = newcons;
-      }
-  End_roots();
-  return res;
+  res = Val_emptylist;
+
+  for (i = 1; i < NSIG; i++)
+    if (sigismember(set, i) > 0) {
+      res = caml_alloc_2(2, Val_int(caml_rev_convert_signal_number(i)), res);
+    }
+  CAMLreturn(res);
 }
 
 static int sigmask_cmd[3] = { SIG_SETMASK, SIG_BLOCK, SIG_UNBLOCK };

otherlibs/systhreads/st_stubs.c
@@ -49,15 +49,9 @@
 
 /* The ML value describing a thread (heap-allocated) */
 
-struct caml_thread_descr {
-  value ident;                  /* Unique integer ID */
-  value start_closure;          /* The closure to start this thread */
-  value terminated;             /* Triggered event for thread termination */
-};
-
-#define Ident(v) (((struct caml_thread_descr *)(v))->ident)
-#define Start_closure(v) (((struct caml_thread_descr *)(v))->start_closure)
-#define Terminated(v) (((struct caml_thread_descr *)(v))->terminated)
+#define Ident(v) Field(v, 0)
+#define Start_closure(v) Field(v, 1)
+#define Terminated(v) Field(v, 2)
 
 /* The infos on threads (allocated via caml_stat_alloc()) */
 
@@ -276,18 +270,15 @@ static caml_thread_t caml_thread_new_info(void)
 
 static value caml_thread_new_descriptor(value clos)
 {
-  value mu = Val_unit;
+  CAMLparam1(clos);
+  CAMLlocal1(mu);
   value descr;
-  Begin_roots2 (clos, mu)
-    /* Create and initialize the termination semaphore */
-    mu = caml_threadstatus_new();
-    /* Create a descriptor for the new thread */
-    descr = caml_alloc_small(3, 0);
-    Ident(descr) = Val_long(atomic_fetch_add(&thread_next_id, +1));
-    Start_closure(descr) = clos;
-    Terminated(descr) = mu;
-  End_roots();
-  return descr;
+  /* Create and initialize the termination semaphore */
+  mu = caml_threadstatus_new();
+  /* Create a descriptor for the new thread */
+  descr = caml_alloc_3(3, Val_long(atomic_fetch_add(&thread_next_id, +1)),
+                       clos, mu);
+  CAMLreturn(descr);
 }
 
 /* Remove a thread info block from the list of threads.
@@ -781,14 +772,13 @@ static void caml_threadstatus_terminate (value wrapper)
 
 static st_retcode caml_threadstatus_wait (value wrapper)
 {
+  CAMLparam1(wrapper); /* prevent deallocation of ts */
   st_event ts = Threadstatus_val(wrapper);
   st_retcode retcode;
 
-  Begin_roots1(wrapper)         /* prevent deallocation of ts */
-    caml_enter_blocking_section();
-    retcode = st_event_wait(ts);
-    caml_leave_blocking_section();
-  End_roots();
+  caml_enter_blocking_section();
+  retcode = st_event_wait(ts);
+  caml_leave_blocking_section();
 
-  return retcode;
+  CAMLreturnT(st_retcode, retcode);
 }

otherlibs/unix/accept.c
@@ -27,9 +27,10 @@
 
 CAMLprim value unix_accept(value cloexec, value sock)
 {
+  CAMLparam0();
+  CAMLlocal1(a);
   int retcode;
   value res;
-  value a;
   union sock_addr_union addr;
   socklen_param_type addr_len;
   int clo = unix_cloexec_p(cloexec);
@@ -48,12 +49,10 @@ CAMLprim value unix_accept(value cloexec, value sock)
   if (clo) unix_set_cloexec(retcode, "accept", Nothing);
 #endif
   a = alloc_sockaddr(&addr, addr_len, retcode);
-  Begin_root (a);
-    res = caml_alloc_small(2, 0);
-    Field(res, 0) = Val_int(retcode);
-    Field(res, 1) = a;
-  End_roots();
-  return res;
+  res = caml_alloc_small(2, 0);
+  Field(res, 0) = Val_int(retcode);
+  Field(res, 1) = a;
+  CAMLreturn(res);
 }
 
 #else

otherlibs/unix/getgr.c
@@ -24,22 +24,21 @@
 
 static value alloc_group_entry(struct group *entry)
 {
+  CAMLparam0();
+  CAMLlocal3(name, pass, mem);
   value res;
-  value name = Val_unit, pass = Val_unit, mem = Val_unit;
 
-  Begin_roots3 (name, pass, mem);
-    name = caml_copy_string(entry->gr_name);
-    /* on some platforms, namely Android, gr_passwd can be NULL,
-       hence this workaround */
-    pass = caml_copy_string(entry->gr_passwd ? entry->gr_passwd : "");
-    mem = caml_copy_string_array((const char**)entry->gr_mem);
-    res = caml_alloc_small(4, 0);
-    Field(res,0) = name;
-    Field(res,1) = pass;
-    Field(res,2) = Val_int(entry->gr_gid);
-    Field(res,3) = mem;
-  End_roots();
-  return res;
+  name = caml_copy_string(entry->gr_name);
+  /* on some platforms, namely Android, gr_passwd can be NULL,
+     hence this workaround */
+  pass = caml_copy_string(entry->gr_passwd ? entry->gr_passwd : "");
+  mem = caml_copy_string_array((const char**)entry->gr_mem);
+  res = caml_alloc_small(4, 0);
+  Field(res,0) = name;
+  Field(res,1) = pass;
+  Field(res,2) = Val_int(entry->gr_gid);
+  Field(res,3) = mem;
+  CAMLreturn(res);
 }
 
 CAMLprim value unix_getgrnam(value name)

otherlibs/unix/gethost.c
@@ -56,32 +56,30 @@ static value alloc_one_addr(char const *a)
 
 static value alloc_host_entry(struct hostent *entry)
 {
+  CAMLparam0();
+  CAMLlocal4(name, aliases, addr_list, adr);
   value res;
-  value name = Val_unit, aliases = Val_unit;
-  value addr_list = Val_unit, adr = Val_unit;
 
-  Begin_roots4 (name, aliases, addr_list, adr);
-    name = caml_copy_string((char *)(entry->h_name));
-    /* PR#4043: protect against buggy implementations of gethostbyname()
-       that return a NULL pointer in h_aliases */
-    if (entry->h_aliases)
-      aliases = caml_copy_string_array((const char**)entry->h_aliases);
-    else
-      aliases = Atom(0);
-    entry_h_length = entry->h_length;
-    addr_list =
-      caml_alloc_array(alloc_one_addr, (const char**)entry->h_addr_list);
-    res = caml_alloc_small(4, 0);
-    Field(res, 0) = name;
-    Field(res, 1) = aliases;
-    switch (entry->h_addrtype) {
-    case PF_UNIX:          Field(res, 2) = Val_int(0); break;
-    case PF_INET:          Field(res, 2) = Val_int(1); break;
-    default: /*PF_INET6 */ Field(res, 2) = Val_int(2); break;
-    }
-    Field(res, 3) = addr_list;
-  End_roots();
-  return res;
+  name = caml_copy_string((char *)(entry->h_name));
+  /* PR#4043: protect against buggy implementations of gethostbyname()
+     that return a NULL pointer in h_aliases */
+  if (entry->h_aliases)
+    aliases = caml_copy_string_array((const char**)entry->h_aliases);
+  else
+    aliases = Atom(0);
+  entry_h_length = entry->h_length;
+  addr_list =
+    caml_alloc_array(alloc_one_addr, (const char**)entry->h_addr_list);
+  res = caml_alloc_small(4, 0);
+  Field(res, 0) = name;
+  Field(res, 1) = aliases;
+  switch (entry->h_addrtype) {
+  case PF_UNIX:          Field(res, 2) = Val_int(0); break;
+  case PF_INET:          Field(res, 2) = Val_int(1); break;
+  default: /*PF_INET6 */ Field(res, 2) = Val_int(2); break;
+  }
+  Field(res, 3) = addr_list;
+  CAMLreturn(res);
 }
 
 CAMLprim value unix_gethostbyaddr(value a)

otherlibs/unix/getproto.c
@@ -27,18 +27,17 @@
 
 static value alloc_proto_entry(struct protoent *entry)
 {
+  CAMLparam0();
+  CAMLlocal2(name, aliases);
   value res;
-  value name = Val_unit, aliases = Val_unit;
 
-  Begin_roots2 (name, aliases);
-    name = caml_copy_string(entry->p_name);
-    aliases = caml_copy_string_array((const char**)entry->p_aliases);
-    res = caml_alloc_small(3, 0);
-    Field(res,0) = name;
-    Field(res,1) = aliases;
-    Field(res,2) = Val_int(entry->p_proto);
-  End_roots();
-  return res;
+  name = caml_copy_string(entry->p_name);
+  aliases = caml_copy_string_array((const char**)entry->p_aliases);
+  res = caml_alloc_small(3, 0);
+  Field(res,0) = name;
+  Field(res,1) = aliases;
+  Field(res,2) = Val_int(entry->p_proto);
+  CAMLreturn(res);
 }
 
 CAMLprim value unix_getprotobyname(value name)

commit ab0577933207323c3b1f3a914761e0f718ad6de8

Fix unix_getsockopt_aux always returning EINVAL.
Regression introduced by f022f9b9d2f8598.

otherlibs/unix/sockopt.c
@@ -203,18 +203,20 @@ unix_getsockopt_aux(char * name,
 
   switch (ty) {
   case TYPE_BOOL:
-    res = Val_bool(optval.i);
+    res = Val_bool(optval.i); break;
   case TYPE_INT:
-    res = Val_int(optval.i);
+    res = Val_int(optval.i); break;
   case TYPE_LINGER:
     if (optval.lg.l_onoff == 0) {
       res = Val_none;
     } else {
       res = caml_alloc_some(Val_int(optval.lg.l_linger));
     }
+    break;
   case TYPE_TIMEVAL:
     res = caml_copy_double((double) optval.tv.tv_sec
                            + (double) optval.tv.tv_usec / 1e6);
+    break;
   case TYPE_UNIX_ERROR:
     if (optval.i == 0) {
       res = Val_none;
@@ -222,6 +224,7 @@ unix_getsockopt_aux(char * name,
       err = unix_error_of_code(optval.i);
       res = caml_alloc_some(err);
     }
+    break;
   default:
     unix_error(EINVAL, name, Nothing);
   }

otherlibs/win32unix/sockopt.c
@@ -143,16 +143,18 @@ unix_getsockopt_aux(char * name,
   switch (ty) {
   case TYPE_BOOL:
   case TYPE_INT:
-    res = Val_int(optval.i);
+    res = Val_int(optval.i); break;
   case TYPE_LINGER:
     if (optval.lg.l_onoff == 0) {
       res = Val_none;
     } else {
       res = caml_alloc_some(Val_int(optval.lg.l_linger));
     }
+    break;
   case TYPE_TIMEVAL:
     res = caml_copy_double((double) optval.tv.tv_sec
                            + (double) optval.tv.tv_usec / 1e6);
+    break;
   case TYPE_UNIX_ERROR:
     if (optval.i == 0) {
       res = Val_none;
@@ -160,6 +162,7 @@ unix_getsockopt_aux(char * name,
       err = unix_error_of_code(optval.i);
       res = caml_alloc_some(err);
     }
+    break;
   default:
     unix_error(EINVAL, name, Nothing);
   }