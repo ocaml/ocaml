ecb30498200e0c0095df42e9a4e35c8354631e6d
Greta Yorsh <gyorsh@janestreet.com>
2022-03-04 10:19:13 +0000

    Actually, just use float_of_bits not an expression to define qNaN

--- stdlib/stdlib.ml
+++ stdlib/stdlib.ml
@@ -181,7 +181,7 @@ let infinity =
 let neg_infinity =
   float_of_bits 0xFF_F0_00_00_00_00_00_00L
 let nan =
-  abs_float (infinity -. infinity)
+  float_of_bits 0x7F_F8_00_00_00_00_00_01L
 let max_float =
   float_of_bits 0x7F_EF_FF_FF_FF_FF_FF_FFL
 let min_float =
