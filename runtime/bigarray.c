c1c449600d9a8d85d5a260dbfa4bda4eda72bd67
Roven Gabriel <roven.gabriel@lexifi.com>
2022-03-16 14:30:31 +0100

    Fix possible 32bit integer overflow of offset in bigarray C implementation.

--- runtime/bigarray.c
+++ runtime/bigarray.c
@@ -524,7 +524,7 @@ CAMLprim value caml_ba_create(value vkind, value vlayout, value vdim)
    are within the bounds and return the offset of the corresponding
    array element in the data part of the array. */
 
-static long caml_ba_offset(struct caml_ba_array * b, intnat * index)
+static intnat caml_ba_offset(struct caml_ba_array * b, intnat * index)
 {
   intnat offset;
   int i;
