fe66ef08cb11a8a7dc98a17afb0e0cf01e9a1382
David Allsopp <david.allsopp@metastack.com>
2022-04-06 16:36:08 +0100

    Use sentinels in the boot compiler's configuration

--- utils/config.fixed.ml
+++ utils/config.fixed.ml
@@ -14,31 +14,35 @@
 (**************************************************************************)
 
 (* Configuration for the boot compiler. The compiler should refuse to bootstrap
-   if configured with values which would contradict the configuration below. *)
+   if configured with values which would contradict the configuration below.
+   The values below are picked to trigger errors if accidentally used in the
+   compiler (e.g. for the C compiler). *)
 
-let bindir = "<boot compiler>"
-let standard_library_default = "<boot compiler>"
-let ccomp_type = "<boot compiler>"
-let c_compiler = "<boot compiler>"
-let c_output_obj = "<boot compiler>"
+let boot_cannot_call s = "/ The boot compiler should not call " ^ s
+
+let bindir = "/tmp"
+let standard_library_default = "/tmp"
+let ccomp_type = "n/a"
+let c_compiler = boot_cannot_call "the C compiler"
+let c_output_obj = ""
 let c_has_debug_prefix_map = false
 let as_has_debug_prefix_map = false
-let ocamlc_cflags = "<boot compiler>"
-let ocamlc_cppflags = "<boot compiler>"
-let ocamlopt_cflags = "<boot compiler>"
-let ocamlopt_cppflags = "<boot compiler>"
-let bytecomp_c_libraries = "<boot compiler>"
-let bytecomp_c_compiler = "<boot compiler>"
-let native_c_compiler = "<boot compiler>"
-let native_c_libraries = "<boot compiler>"
-let native_pack_linker = "<boot compiler>"
-let default_rpath = "<boot compiler>"
-let mksharedlibrpath = "<boot compiler>"
-let ar = "<boot compiler>"
+let ocamlc_cflags = ""
+let ocamlc_cppflags = ""
+let ocamlopt_cflags = ""
+let ocamlopt_cppflags = ""
+let bytecomp_c_libraries = ""
+let bytecomp_c_compiler = ""
+let native_c_compiler = c_compiler
+let native_c_libraries = ""
+let native_pack_linker = boot_cannot_call "the linker"
+let default_rpath = ""
+let mksharedlibrpath = ""
+let ar = boot_cannot_call "ar"
 let supports_shared_libraries = false
-let mkdll = "<boot compiler>"
-let mkexe = "<boot compiler>"
-let mkmaindll = "<boot compiler>"
+let mkdll = native_pack_linker
+let mkexe = native_pack_linker
+let mkmaindll = native_pack_linker
 let flambda = false
 let with_flambda_invariants = false
 let with_cmm_invariants = false
@@ -50,17 +54,17 @@ let afl_instrument = false
 let architecture = "none"
 let model = "default"
 let system = "unknown"
-let asm = "<boot compiler>"
+let asm = boot_cannot_call "the assembler"
 let asm_cfi_supported = false
 let with_frame_pointers = false
 let profinfo = false
 let profinfo_width = 0
-let ext_exe = "<boot compiler>"
-let ext_obj = "<boot compiler>"
-let ext_asm = "<boot compiler>"
-let ext_lib = "<boot compiler>"
-let ext_dll = "<boot compiler>"
-let host = "<boot compiler>"
-let target = "<boot compiler>"
+let ext_exe = ".ex_The boot compiler should not be using Config.ext_exe"
+let ext_obj = ".o_The boot compiler cannot process C objects"
+let ext_asm = ".s_The boot compiler should not be using Config.ext_asm"
+let ext_lib = ".a_The boot compiler cannot process C libraries"
+let ext_dll = ".so_The boot compiler cannot load DLLs"
+let host = "zinc-boot-ocaml"
+let target = host
 let systhread_supported = false
 let flexdll_dirs = []
