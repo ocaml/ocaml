fbc28431777dcf239da035ff3c74cb05058ebc29
François Pottier <francois.pottier@inria.fr>
2022-04-29 10:16:50 +0200

    Use italics for P and D.

--- stdlib/condition.mli
+++ stdlib/condition.mli
@@ -43,11 +43,11 @@
    condition variable is used for this purpose.
 
    In short, a condition variable [c] is used to convey the information
-   that a certain property [P] about a shared data structure [D],
+   that a certain property {i P} about a shared data structure {i D},
    protected by a mutex [m], may be true.
 
    Condition variables provide an efficient alternative to busy-waiting.
-   When one wishes to wait for the property [P] to be true,
+   When one wishes to wait for the property {i P} to be true,
    instead of writing a busy-waiting loop:
    {[
      Mutex.lock m;
@@ -81,24 +81,24 @@
    used to indicate that the queue is nonempty, and another condition
    variable is used to indicate that the queue is not full.
 
-   With a condition variable [c], exactly one logical property [P]
+   With a condition variable [c], exactly one logical property {i P}
    should be associated. Examples of such properties
    include "the queue is nonempty" and "the queue is not full".
    It is up to the programmer to keep track, for each condition
-   variable, of the corresponding property [P].
+   variable, of the corresponding property {i P}.
    A signal is sent on the condition variable [c]
-   as an indication that the property [P] is true, or may be true.
+   as an indication that the property {i P} is true, or may be true.
    On the receiving end, however, a thread that is woken up
-   cannot assume that [P] is true;
+   cannot assume that {i P} is true;
    after a call to {!wait} terminates,
-   one must explicitly test whether [P] is true.
+   one must explicitly test whether {i P} is true.
    There are several reasons why this is so.
    One reason is that,
    between the moment when the signal is sent
    and the moment when a waiting thread receives the signal
    and is scheduled,
-   the property [P] may be falsified by some other thread
-   that is able to acquire the mutex [m] and alter the data structure [D].
+   the property {i P} may be falsified by some other thread
+   that is able to acquire the mutex [m] and alter the data structure {i D}.
    Another reason is that {i spurious wakeups} may occur:
    a waiting thread can be woken up even if no signal was sent.
 
@@ -145,7 +145,7 @@ type t
 val create : unit -> t
 (**[create()] creates and returns a new condition variable.
    This condition variable should be associated (in the programmer's mind)
-   with a certain mutex [m] and with a certain property [P] of the data
+   with a certain mutex [m] and with a certain property {i P} of the data
    structure that is protected by the mutex [m]. *)
 
 val wait : t -> Mutex.t -> unit
@@ -156,9 +156,9 @@ val wait : t -> Mutex.t -> unit
    later be woken up after the condition variable [c] has been signaled
    via {!signal} or {!broadcast}; however, it can also be woken up for
    no reason. The mutex [m] is locked again before [wait] returns. One
-   cannot assume that the property [P] associated with the condition
+   cannot assume that the property {i P} associated with the condition
    variable [c] holds when [wait] returns; one must explicitly test
-   whether [P] holds after calling [wait]. *)
+   whether {i P} holds after calling [wait]. *)
 
 val signal : t -> unit
 (**[signal c] wakes up one of the threads waiting on the condition
