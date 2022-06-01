f1373319aeb55765f43f69b2b78446beec5dd7e9
Sébastien Hinderer <Sebastien.Hinderer@inria.fr>
2022-06-01 15:39:42 +0200

    Provide an interface for driver/main.ml

---
+++ driver/main.mli
@@ -0,0 +1,19 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*             Sebastien Hinderer, projet Cambium, INRIA Paris            *)
+(*                                                                        *)
+(*   Copyright 2022 Institut National de Recherche en Informatique et     *)
+(*     en Automatique.                                                    *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
+(* Interface to the compiler's main module *)
+
+(* This interface file is empty because the driver/main.ml only runs
+   code during initialisation and exports nothing *)
