8e632389b2b7d3d717a59894065d8d4a73baf92e
Nicolás Ojeda Bär <n.oje.bar@gmail.com>
2022-06-02 16:44:38 +0200

    Typo

--- stdlib/option.mli
+++ stdlib/option.mli
@@ -45,7 +45,7 @@ val join : 'a option option -> 'a option
 (** [join oo] is [Some v] if [oo] is [Some (Some v)] and [None] otherwise. *)
 
 val map : ('a -> 'b) -> 'a option -> 'b option
-(** [map f o] is [None] if [o] is [None] and [Some (f v)] is [o] is [Some v]. *)
+(** [map f o] is [None] if [o] is [None] and [Some (f v)] if [o] is [Some v]. *)
 
 val fold : none:'a -> some:('b -> 'a) -> 'b option -> 'a
 (** [fold ~none ~some o] is [none] if [o] is [None] and [some v] if [o] is
