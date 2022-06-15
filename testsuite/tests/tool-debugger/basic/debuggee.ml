74f5d893710faba427d3a67df29432f2486ed318
Damien Doligez <damien.doligez@inria.fr>
2022-06-15 16:06:29 +0200

    re-enable debugger tests

--- testsuite/tests/tool-debugger/basic/debuggee.ml
+++ testsuite/tests/tool-debugger/basic/debuggee.ml
@@ -2,15 +2,13 @@
 set foo = "bar"
 flags += " -g "
 ocamldebug_script = "${test_source_directory}/input_script"
-* skip
-reason = "ocamldebug is broken (#34)"
-** debugger
-*** shared-libraries
-**** setup-ocamlc.byte-build-env
-***** ocamlc.byte
-****** check-ocamlc.byte-output
-******* ocamldebug
-******** check-program-output
+* debugger
+** shared-libraries
+*** setup-ocamlc.byte-build-env
+**** ocamlc.byte
+***** check-ocamlc.byte-output
+****** ocamldebug
+******* check-program-output
 *)
 
 print_endline Sys.argv.(1);;
