ocaml/ocaml

commit eed1110e6aece8713605806bee7066ea07e2c0ca

make update_scope recursive while keeping trace_gadt_instances (#10277)

typing/ctype.ml
@@ -843,11 +843,13 @@ let check_scope_escape env level ty =
     backtrack snap;
     raise Trace.(Unify[Escape { x with context = Some ty }])
 
-let update_scope scope ty =
+let rec update_scope scope ty =
   let ty = repr ty in
-  let scope = max scope ty.scope in
-  if ty.level < scope then raise (Trace.scope_escape ty);
-  set_scope ty scope
+  if ty.scope < scope then begin
+   if ty.level < scope then raise (Trace.scope_escape ty);
+   set_scope ty scope;
+   iter_type_expr (update_scope scope) ty
+  end
 
 (* Note: the level of a type constructor must be greater than its binding
     time. That way, a type constructor cannot escape the scope of its
@@ -1630,12 +1632,10 @@ let expand_abbrev_gen kind find_type_expansion env ty =
             let ty' = subst env level kind abbrev (Some ty) params args body in
             (* For gadts, remember type as non exportable *)
             (* The ambiguous level registered for ty' should be the highest *)
-            if !trace_gadt_instances then begin
-              let scope = max lv ty.scope in
-              if level < scope then raise (Trace.scope_escape ty);
-              set_scope ty scope;
-              set_scope ty' scope
-            end;
+            (* if !trace_gadt_instances then begin *)
+            let scope = max lv ty.scope in
+            update_scope scope ty;
+            update_scope scope ty';
             ty'
       end
   | _ ->
@@ -1652,10 +1652,14 @@ let expand_head_once env ty =
 (* Check whether a type can be expanded *)
 let safe_abbrev env ty =
   let snap = Btype.snapshot () in
-  try ignore (expand_abbrev env ty); true
-  with Cannot_expand | Unify _ ->
-    Btype.backtrack snap;
-    false
+  try ignore (expand_abbrev env ty); true with
+    Cannot_expand ->
+      Btype.backtrack snap;
+      false
+  | Unify _ ->
+      Btype.backtrack snap;
+      cleanup_abbrev ();
+      false
 
 (* Expand the head of a type once.
    Raise Cannot_expand if the type cannot be expanded.
@@ -1671,7 +1675,7 @@ let try_expand_safe env ty =
   let snap = Btype.snapshot () in
   try try_expand_once env ty
   with Unify _ ->
-    Btype.backtrack snap; raise Cannot_expand
+    Btype.backtrack snap; cleanup_abbrev (); raise Cannot_expand
 
 (* Fully expand the head of a type. *)
 let rec try_expand_head try_once env ty =
@@ -1756,8 +1760,20 @@ let enforce_constraints env ty =
 
 (* Recursively expand the head of a type.
    Also expand #-types. *)
-let full_expand env ty =
-  let ty = repr (expand_head env ty) in
+let full_expand ~may_forget_scope env ty =
+  let ty =
+    if may_forget_scope then
+      let ty = repr ty in
+      try expand_head_unif env ty with Unify _ ->
+        (* #10277: forget scopes when printing trace *)
+        begin_def ();
+        init_def ty.level;
+        let ty = expand_head env (correct_levels ty) in
+        end_def ();
+        ty
+    else expand_head env ty
+  in
+  let ty = repr ty in
   match ty.desc with
     Tobject (fi, {contents = Some (_, v::_)}) when is_Tvar (repr v) ->
       newty2 ty.level (Tobject (fi, ref None))
@@ -2099,9 +2115,14 @@ let rec has_cached_expansion p abbrev =
 (* +++ Move it to some other place ? *)
 
 let expand_trace env trace =
-  let expand_desc x = match x.Trace.expanded with
-    | None -> Trace.{ t = repr x.t; expanded= Some(full_expand env x.t) }
-    | Some _ -> x in
+  let expand_desc x =
+    let open Trace in
+    match x.expanded with
+    | None ->
+        let expanded = full_expand ~may_forget_scope:true env x.t in
+        { t = repr x.t; expanded = Some expanded }
+    | Some _ -> x
+  in
   Unification_trace.map expand_desc trace
 
 (**** Unification ****)
@@ -2173,7 +2194,7 @@ let reify env t =
           end;
           iter_row iterator r
       | Tconstr (p, _, _) when is_object_type p ->
-          iter_type_expr iterator (full_expand !env ty)
+          iter_type_expr iterator (full_expand ~may_forget_scope:false !env ty)
       | _ ->
           iter_type_expr iterator ty
     end

typing/ctype.mli
@@ -248,7 +248,7 @@ val expand_head_opt: Env.t -> type_expr -> type_expr
 (** The compiler's own version of [expand_head] necessary for type-based
     optimisations. *)
 
-val full_expand: Env.t -> type_expr -> type_expr
+val full_expand: may_forget_scope:bool -> Env.t -> type_expr -> type_expr
 val extract_concrete_typedecl:
         Env.t -> type_expr -> Path.t * Path.t * type_declaration
         (* Return the original path of the types, and the first concrete

testsuite/tests/let-syntax/let_syntax.ml
@@ -720,11 +720,12 @@ Line 4, characters 11-19:
                ^^^^^^^^
 Warning 18 [not-principal]: typing this pattern requires considering GADT_ordering.point and a as equal.
 But the knowledge of these types is not principal.
-Line 5, characters 13-14:
+Line 5, characters 11-19:
 5 |       and+ { x; y } = a in
-                 ^
-Error: The record field x belongs to the type GADT_ordering.point
-       but is mixed here with fields of type a = GADT_ordering.point
+               ^^^^^^^^
+Error: This pattern matches values of type GADT_ordering.point
+       but a pattern was expected which matches values of type
+         a = GADT_ordering.point
        This instance of GADT_ordering.point is ambiguous:
        it would escape the scope of its equation
 |}];;

testsuite/tests/typing-gadts/ambivalent_apply.ml
@@ -1,0 +1,38 @@
+(* TEST
+   * expect
+*)
+
+type (_,_) eq = Refl : ('a,'a) eq;;
+[%%expect{|
+type (_, _) eq = Refl : ('a, 'a) eq
+|}]
+
+(* Both should fail *)
+let f (type a b) (w1 : (a, b -> b) eq) (w2 : (a, int -> int) eq) (g : a) =
+   let Refl = w1 in let Refl = w2 in g 3;;
+[%%expect{|
+Line 2, characters 37-40:
+2 |    let Refl = w1 in let Refl = w2 in g 3;;
+                                         ^^^
+Error: This expression has type b = int
+       but an expression was expected of type 'a
+       This instance of int is ambiguous:
+       it would escape the scope of its equation
+|}]
+let f (type a b) (w1 : (a, b -> b) eq) (w2 : (a, int -> int) eq) (g : a) =
+   let Refl = w2 in let Refl = w1 in g 3;;
+[%%expect{|
+Line 2, characters 37-40:
+2 |    let Refl = w2 in let Refl = w1 in g 3;;
+                                         ^^^
+Error: This expression has type int but an expression was expected of type 'a
+       This instance of int is ambiguous:
+       it would escape the scope of its equation
+|}]
+
+(* Ok *)
+let f (type a b) (w1 : (a, b -> b) eq) (w2 : (a, int -> int) eq) (g : a) : b =
+   let Refl = w2 in let Refl = w1 in g 3;;
+[%%expect{|
+val f : ('a, 'b -> 'b) eq -> ('a, int -> int) eq -> 'a -> 'b = <fun>
+|}]

testsuite/tests/typing-gadts/test.ml
@@ -1095,7 +1095,8 @@ Line 3, characters 2-26:
       ^^^^^^^^^^^^^^^^^^^^^^^^
 Error: This expression has type < bar : int; foo : int; .. >
        but an expression was expected of type 'a
-       The type constructor $1 would escape its scope
+       This instance of $1 is ambiguous:
+       it would escape the scope of its equation
 |}];;
 
 let g (type t) (x:t) (e : t int_foo) (e' : t int_bar) : t =
@@ -1111,7 +1112,12 @@ let g (type t) (x:t) (e : t int_foo) (e' : t int_bar) =
   x, x#foo, x#bar
 ;;
 [%%expect{|
-val g : 't -> 't int_foo -> 't int_bar -> 't * int * int = <fun>
+Line 3, characters 5-10:
+3 |   x, x#foo, x#bar
+         ^^^^^
+Error: This expression has type int but an expression was expected of type 'a
+       This instance of int is ambiguous:
+       it would escape the scope of its equation
 |}];;
 
 (* PR#5554 *)

typing/typecore.ml
@@ -3365,8 +3365,9 @@ and type_expect_
                 let ty, b = enlarge_type env ty' in
                 force ();
                 begin try Ctype.unify env arg.exp_type ty with Unify trace ->
+                  let expanded = full_expand ~may_forget_scope:true env ty' in
                   raise(Error(sarg.pexp_loc, env,
-                        Coercion_failure(ty', full_expand env ty', trace, b)))
+                              Coercion_failure(ty', expanded, trace, b)))
                 end
             end;
             (arg, ty', None, cty')
@@ -4325,9 +4326,22 @@ and type_argument ?explanation ?recarg env sarg ty_expected' ty_expected =
     let ls, tvar = list_labels env ty in
     not tvar && List.for_all ((=) Nolabel) ls
   in
-  match expand_head env ty_expected' with
-    {desc = Tarrow(Nolabel,ty_arg,ty_res,_); level = lv}
-    when is_inferred sarg ->
+  let may_coerce =
+    if not (is_inferred sarg) then None else
+    let work () =
+      match expand_head env ty_expected' with
+        {desc = Tarrow(Nolabel,_,ty_res0,_); level} ->
+          Some (no_labels ty_res0, level)
+      | _ -> None
+    in
+    (* Need to be careful not to expand local constraints here *)
+    if Env.has_local_constraints env then
+      let snap = Btype.snapshot () in
+      try_finally ~always:(fun () -> Btype.backtrack snap) work
+    else work ()
+  in
+  match may_coerce with
+    Some (safe_expect, lv) ->
       (* apply optional arguments when expected type is "" *)
       (* we must be very careful about not breaking the semantics *)
       if !Clflags.principal then begin_def ();
@@ -4346,15 +4360,20 @@ and type_argument ?explanation ?recarg env sarg ty_expected' ty_expected =
         | Tvar _ ->  List.rev args, ty_fun, false
         |  _ -> [], texp.exp_type, false
       in
-      let args, ty_fun', simple_res = make_args [] texp.exp_type in
-      let warn = !Clflags.principal &&
-        (lv <> generic_level || (repr ty_fun').level <> generic_level)
-      and texp = {texp with exp_type = instance texp.exp_type}
-      and ty_fun = instance ty_fun' in
-      if not (simple_res || no_labels ty_res) then begin
+      let args, ty_fun', simple_res = make_args [] texp.exp_type
+      and texp = {texp with exp_type = instance texp.exp_type} in
+      if not (simple_res || safe_expect) then begin
         unify_exp env texp ty_expected;
         texp
       end else begin
+      let warn = !Clflags.principal &&
+        (lv <> generic_level || (repr ty_fun').level <> generic_level)
+      and ty_fun = instance ty_fun' in
+      let ty_arg, ty_res =
+        match expand_head env ty_expected' with
+          {desc = Tarrow(Nolabel,ty_arg,ty_res,_)} -> ty_arg, ty_res
+        | _ -> assert false
+      in
       unify_exp env {texp with exp_type = ty_fun} ty_expected;
       if args = [] then texp else
       (* eta-expand to avoid side effects *)
@@ -4404,7 +4423,7 @@ and type_argument ?explanation ?recarg env sarg ty_expected' ty_expected =
                       }],
                      func let_var) }
       end
-  | _ ->
+  | None ->
       let texp = type_expect ?recarg env sarg
         (mk_expected ?explanation ty_expected') in
       unify_exp env texp ty_expected;

commit 75787982802c65bc9c6ac97305e3f2fc17ba0ef1

Fix typechecking regression in 4.13 (#10768)
This regression was introduced in
eed1110e6aece8713605806bee7066ea07e2c0ca, as part of PR #10277.

This change introduces `Ctype.wrap_no_update_gadt_scopes`, used to
disable scope checks in `Ctype.expand_abbrev_gen`. By using this wrapper
in `Includemod.signatures`, modules inside expressions can still be
constrained by signatures and packed/unpacked while containing types
with GADT equalities.

The only way that these checks can be run is by providing an explicit
module signature, either by packing a module, annotating a module with a
signature, or applying a functor. This avoids re-introducing the
'ambiguous results of applications' problem that #10277 was intended to
solve.

typing/ctype.ml
@@ -1462,6 +1462,22 @@ let () = Subst.ctype_apply_env_empty := apply Env.empty
                               (*  Abbreviation expansion  *)
                               (****************************)
 
+(**** Control GADT scope checks on abbreviation expansion*)
+
+let update_gadt_scopes = ref true
+let check_no_update_gadt_scopes env =
+  !update_gadt_scopes && Env.has_local_constraints env &&
+  (update_gadt_scopes := false; cleanup_abbrev (); true)
+
+let reset_no_update_gadt_scopes b =
+  if b then update_gadt_scopes := true
+
+let wrap_no_update_gadt_scopes env f x =
+  let b = check_no_update_gadt_scopes env in
+  let y = f x in
+  reset_no_update_gadt_scopes b;
+  y
+
 (*
    If the environment has changed, memorized expansions might not
    be correct anymore, and so we flush the cache. This is safe but
@@ -1542,10 +1558,11 @@ let expand_abbrev_gen kind find_type_expansion env ty =
             in
             (* For gadts, remember type as non exportable *)
             (* The ambiguous level registered for ty' should be the highest *)
-            (* if !trace_gadt_instances then begin *)
-            let scope = Int.max lv (get_scope ty) in
-            update_scope scope ty;
-            update_scope scope ty';
+            if !update_gadt_scopes then begin
+              let scope = Int.max lv (get_scope ty) in
+              update_scope scope ty;
+              update_scope scope ty'
+            end;
             ty'
       end
   | _ ->

typing/ctype.mli
@@ -419,6 +419,8 @@ val get_current_level: unit -> int
 val wrap_trace_gadt_instances: Env.t -> ('a -> 'b) -> 'a -> 'b
 val reset_reified_var_counter: unit -> unit
 
+val wrap_no_update_gadt_scopes: Env.t -> ('a -> 'b) -> 'a -> 'b
+
 val immediacy : Env.t -> type_expr -> Type_immediacy.t
 
 (* Stubs *)

typing/includemod.ml
@@ -601,7 +601,7 @@ and strengthened_module_decl ~loc ~aliasable env ~mark
 
 (* Inclusion between signatures *)
 
-and signatures  ~in_eq ~loc env ~mark subst sig1 sig2 mod_shape =
+and signatures'  ~in_eq ~loc env ~mark subst sig1 sig2 mod_shape =
   (* Environment used to check inclusion of components *)
   let new_env =
     Env.add_signature sig1 (Env.in_signature true env) in
@@ -711,6 +711,10 @@ and signatures  ~in_eq ~loc env ~mark subst sig1 sig2 mod_shape =
         end in
   (* Do the pairing and checking, and return the final coercion *)
   pair_components subst [] [] sig2
+
+and signatures ~in_eq ~loc env ~mark subst sig1 sig2 mod_shape =
+  Ctype.wrap_no_update_gadt_scopes env
+    (signatures' ~in_eq ~loc env ~mark subst sig1 sig2) mod_shape
 
 (* Inclusion between signature components *)
 