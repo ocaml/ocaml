67863ba31064aed95729b93edbb09d8a7235d40a
Nicolas Ojeda Bar <n.oje.bar@gmail.com>
2022-06-24 15:05:21 +0200

    Remove (.[]<-) (#11345)
    (cherry picked from commit bb03e1103d0c1d5474098c7c3da91d1faa95ac06)

--- parsing/parse.ml
+++ parsing/parse.ml
@@ -140,6 +140,12 @@ let prepare_error err =
         "broken invariant in parsetree: %s" s
   | Invalid_package_type (loc, s) ->
       Location.errorf ~loc "invalid package type: %s" s
+  | Removed_string_set loc ->
+      Location.errorf ~loc
+        "Syntax error: strings are immutable, there is no assignment \
+         syntax for them.\n\
+         Hint: Mutable sequences of bytes are available in the Bytes module.\n\
+         Hint: Did you mean to use 'Bytes.set'?"
 
 let () =
   Location.register_error_of_exn
--- parsing/syntaxerr.mli
+++ parsing/syntaxerr.mli
@@ -29,6 +29,7 @@ type error =
   | Other of Location.t
   | Ill_formed_ast of Location.t * string
   | Invalid_package_type of Location.t * string
+  | Removed_string_set of Location.t
 
 exception Error of error
 exception Escape_error
