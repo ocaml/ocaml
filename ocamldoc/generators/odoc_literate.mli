718fdb98e15947ab8e22877dd6403f93bf2e1320
Sébastien Hinderer <Sebastien.Hinderer@inria.fr>
2022-06-28 16:34:25 +0200

    Provide an interface for ocamldoc's odoc_literate module

---
+++ ocamldoc/generators/odoc_literate.mli
@@ -0,0 +1,23 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*             Sebastien Hinderer, projet Cambium, INRIA Paris            *)
+(*                                                                        *)
+(*   Copyright 2022 Institut National de Recherche en Informatique et     *)
+(*     en Automatique.                                                    *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
+module Naming = Odoc_html.Naming
+
+module Html : Odoc_html.Html_generator
+
+module Generator :
+  sig
+    class html : Odoc_html.Generator.html
+  end
