a0ecdfbfca035bb8bcaca3973581312288782d63
Christiano Haesbaert <christiano.fh@gmail.com>
2022-07-05 18:41:17 +0200

    Add a function to request a recommended max number of Domains (#11309)
    Add Domain.recommended_domain_count
    
    (cherry picked from commit e5ccc8b6ae9747abc734048eced4ef742a137029)

--- stdlib/domain.ml
+++ stdlib/domain.ml
@@ -25,6 +25,8 @@ module Raw = struct
     = "caml_ml_domain_id"
   external cpu_relax : unit -> unit
     = "caml_ml_domain_cpu_relax"
+  external get_recommended_domain_count: unit -> int
+    = "caml_recommended_domain_count" [@@noalloc]
 end
 
 let cpu_relax () = Raw.cpu_relax ()
@@ -275,3 +277,5 @@ let join { term_mutex; term_condition; term_state; _ } =
   match loop () with
   | Ok x -> x
   | Error ex -> raise ex
+
+let recommended_domain_count = Raw.get_recommended_domain_count ()
---
+++ testsuite/tests/parallel/recommended_domain_count_unix.ml
@@ -0,0 +1,28 @@
+(* TEST
+* hasunix
+include unix
+** bytecode
+** native
+*)
+
+let try_ext cmd =
+  let ic, oc, iec = Unix.open_process_full cmd (Unix.environment ()) in
+  let n = try Some (input_line ic |> int_of_string) with _ -> None in
+  match Unix.close_process_full (ic, oc, iec) with
+  | Unix.WEXITED 0 -> n
+  | _ -> None
+
+(* Try to guess the number of cpus via an external method. If we
+   can't, assume it passed since we have nothing to compare against *)
+let _ =
+  List.iter (fun cmd ->
+      match try_ext cmd with
+      | None -> ()
+      | Some n ->
+          if Domain.recommended_domain_count > n then
+            failwith
+              (Printf.sprintf
+                 "external tool %s says n=%d, recommended domain says=%d\n"
+                 cmd n Domain.recommended_domain_count))
+    ["nproc"; "sysctl -n hw.ncpu"];
+  print_string "passed\n"
