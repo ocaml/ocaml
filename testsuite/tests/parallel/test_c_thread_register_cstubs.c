54e78dcf699b40701ae15081cfb8874e96cd2050
Gabriel Scherer <gabriel.scherer@gmail.com>
2022-07-06 22:15:35 +0200

    Merge pull request #11404 from gadmm/fix_test_thread_register
    Fix rooting logic in testsuite
    
    (cherry picked from commit ff1df71bafb95494ef3d37fe12bacb94b8f8516f)

--- testsuite/tests/parallel/test_c_thread_register_cstubs.c
+++ testsuite/tests/parallel/test_c_thread_register_cstubs.c
@@ -6,10 +6,28 @@
 #include "caml/callback.h"
 #include "threads.h"
 
-void *thread_func(void *fn) {
+void *create_root(value v)
+{
+  value *root = malloc(sizeof(value));
+  *root = v;
+  caml_register_generational_global_root(root);
+  return (void*)root;
+}
+
+value consume_root(void *r)
+{
+  value *root = (value *)r;
+  value v = *root;
+  caml_remove_generational_global_root(root);
+  free(root);
+  return v;
+}
+
+void *thread_func(void *root)
+{
   caml_c_thread_register();
   caml_acquire_runtime_system();
-  caml_callback((value) fn, Val_unit);
+  caml_callback(consume_root(root), Val_unit);
   caml_release_runtime_system();
   caml_c_thread_unregister();
   return 0;
@@ -19,9 +37,9 @@ value spawn_thread(value clos)
 {
   pthread_t thr;
   pthread_attr_t attr;
-
+  void *root = create_root(clos);
   pthread_attr_init(&attr);
   pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
-  pthread_create(&thr, &attr, thread_func, (void *) clos);
+  pthread_create(&thr, &attr, thread_func, root);
   return Val_unit;
 }
