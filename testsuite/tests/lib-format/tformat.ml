354aa2544c95c07e2bc80f5b3e29a4156287f5e8
hhugo <hugo.heuzard@gmail.com>
2022-07-21 12:16:12 +0200

    Tests: use int_size instead of word_size when appropriate (#11451)
    Co-authored-by: Hugo Heuzard <hugo.heuzard@nomadic-labs.com>

--- testsuite/tests/lib-format/tformat.ml
+++ testsuite/tests/lib-format/tformat.ml
@@ -51,10 +51,10 @@ try
   test (sprintf "%*u" (-4) 42 = "42  ");
 
   say "\nu negative\n%!";
-  begin match Sys.word_size with
-  | 32 ->
+  begin match Sys.int_size with
+  | 31 ->
      test (sprintf "%u" (-1) = "2147483647");
-  | 64 ->
+  | 63 ->
      test (sprintf "%u" (-1) = "9223372036854775807");
   | _ -> test false
   end;
@@ -75,10 +75,10 @@ try
   test (sprintf "%-0+ #*x" 5 42 = "0x2a ");
 
   say "\nx negative\n%!";
-  begin match Sys.word_size with
-  | 32 ->
+  begin match Sys.int_size with
+  | 31 ->
      test (sprintf "%x" (-42) = "7fffffd6");
-  | 64 ->
+  | 63 ->
      test (sprintf "%x" (-42) = "7fffffffffffffd6");
   | _ -> test false
   end;
@@ -95,10 +95,10 @@ try
   test (sprintf "%-0+ #*X" 5 42 = "0X2A ");
 
   say "\nx negative\n%!";
-  begin match Sys.word_size with
-  | 32 ->
+  begin match Sys.int_size with
+  | 31 ->
      test (sprintf "%X" (-42) = "7FFFFFD6");
-  | 64 ->
+  | 63 ->
      test (sprintf "%X" (-42) = "7FFFFFFFFFFFFFD6");
   | _ -> test false
   end;
@@ -115,10 +115,10 @@ try
   test (sprintf "%-0+ #*o" 5 42 = "052  ");
 
   say "\no negative\n%!";
-  begin match Sys.word_size with
-  | 32 ->
+  begin match Sys.int_size with
+  | 31 ->
      test (sprintf "%o" (-42) = "17777777726");
-  | 64 ->
+  | 63 ->
      test (sprintf "%o" (-42) = "777777777777777777726");
   | _ -> test false
   end;
