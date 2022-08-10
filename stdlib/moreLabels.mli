3ad0db6c6c2ce2d9e0b4405a110cf8983317f42c
Simon Cruanes <simon.cruanes.2007@m4x.org>
2022-08-09 21:38:07 -0400

    details

--- stdlib/moreLabels.mli
+++ stdlib/moreLabels.mli
@@ -605,19 +605,23 @@ module Hashtbl : sig
           let hash = Hashtbl.hash
         end)
 
-      # let count_chars seq =
+      (*  count distinct occurrences of chars in [seq] *)
+      # let count_chars (seq:char Seq.t) : _ list =
           let counts = Char_tbl.create 16 in
           Seq.iter
             (fun c ->
               let count_c =
                 Char_tbl.find_opt counts c
-                |> Option.value ~default:0 in
+                |> Option.value ~default:0
+              in
               Char_tbl.replace counts c (count_c + 1))
             seq;
+          (* turn into a list *)
           Char_tbl.fold (fun c n l -> (c,n) :: l) counts []
             |> List.sort (fun (c1,_)(c2,_) -> Char.compare c1 c2)
       val count_chars : Char_tbl.key Seq.t -> (Char.t * int) list = <fun>
 
+      (* basic seq from a string *)
       # let seq = String.to_seq "hello world, and all the camels in it!"
       val seq : char Seq.t = <fun>
 
