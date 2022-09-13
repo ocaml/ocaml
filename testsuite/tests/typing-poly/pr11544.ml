3805f70ac640c37f41ed596151165734da699381
Leo White <leo@lpw25.net>
2022-09-13 15:11:06 +0100

    Keep generalized structure from patterns when typing let

--- testsuite/tests/typing-poly/pr11544.ml
+++ testsuite/tests/typing-poly/pr11544.ml
@@ -8,11 +8,4 @@ let poly3 : 'b. M.t -> 'b -> 'b =
 [%%expect {|
 module M : sig type t = T end
 val poly3 : M.t -> 'b -> 'b = <fun>
-|}, Principal{|
-module M : sig type t = T end
-Line 3, characters 6-7:
-3 |   fun T x -> x
-          ^
-Warning 18 [not-principal]: this type-based constructor disambiguation is not principal.
-val poly3 : M.t -> 'b -> 'b = <fun>
 |}];;
