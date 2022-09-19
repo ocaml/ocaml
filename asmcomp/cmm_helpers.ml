4a6c5058748e65595d9f647956e78c4ddcaa5691
Damien Doligez <damien.doligez@inria.fr>
2022-09-19 15:48:36 +0200

    fix #11482: random crash in large closure allocation

--- asmcomp/cmm_helpers.ml
+++ asmcomp/cmm_helpers.ml
@@ -816,7 +816,7 @@ let make_alloc_generic set_fn dbg tag wordsize args =
     | e1::el -> Csequence(set_fn (Cvar id) (Cconst_int (idx, dbg)) e1 dbg,
                           fill_fields (idx + 2) el) in
     Clet(VP.create id,
-         Cop(Cextcall("caml_alloc", typ_val, [], true),
+         Cop(Cextcall("caml_alloc_shr_check_gc", typ_val, [], true),
                  [Cconst_int (wordsize, dbg); Cconst_int (tag, dbg)], dbg),
          fill_fields 1 args)
   end
