d4b38dd1441463a3489558050c028daa1da246dc
Nicolás Ojeda Bär <n.oje.bar@gmail.com>
2022-09-26 13:52:20 +0200

    Add Stdlib.Type

---
+++ stdlib/type.mli
@@ -0,0 +1,24 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*                         The OCaml programmers                          *)
+(*                                                                        *)
+(*   Copyright 2022 Institut National de Recherche en Informatique et     *)
+(*     en Automatique.                                                    *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
+(** Type introspection.
+
+    @since 5.1 *)
+
+(** {1 Type equality witness} *)
+
+type (_, _) eq = Equal: ('a, 'a) eq
+(** A value of type [(a, b) eq] exists whenever [a] and [b] are the same
+    type. *)
