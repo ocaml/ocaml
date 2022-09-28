6bb171f9499517653427178c35ea9cb04c55c513
Simon Cruanes <simon.cruanes.2007@m4x.org>
2022-09-28 10:21:58 -0400

    fix tests for dynarray

--- testsuite/tests/lib-dynarray/test.ml
+++ testsuite/tests/lib-dynarray/test.ml
@@ -5,20 +5,20 @@ module A = Dynarray
 
 let () =
   let a = A.create() in
-  A.push a 1;
-  A.push a 2;
+  A.push_back a 1;
+  A.push_back a 2;
   assert (A.to_list a = [1;2]);;
 
 let () =
   let a = A.create() in
-  A.push a 1;
-  A.push a 2;
-  A.push a 3;
+  A.push_back a 1;
+  A.push_back a 2;
+  A.push_back a 3;
   assert (A.length a = 3);;
 
 let () =
   let a = A.make 1 5 in
-  A.push a 6;
+  A.push_back a 6;
   assert (A.to_list a = [5;6]);;
 
 let () =
@@ -38,36 +38,36 @@ let () =
 
 let () =
   let a = A.create() in
-  A.push a 0.; A.push a 1.;
+  A.push_back a 0.; A.push_back a 1.;
   A.clear a;
-  A.push a 0.; A.push a 1.; A.push a 7.; A.push a 10.; A.push a 12.;
+  A.push_back a 0.; A.push_back a 1.; A.push_back a 7.; A.push_back a 10.; A.push_back a 12.;
   A.truncate a 2;
   assert (1. = A.fold_left (+.) 0. a);
   A.clear a;
   assert (0 = A.length a);
-  A.push a 0.; A.push a 1.; A.push a 7.; A.push a 10.; A.push a 12.;
+  A.push_back a 0.; A.push_back a 1.; A.push_back a 7.; A.push_back a 10.; A.push_back a 12.;
   assert (1. +. 7. +. 10. +. 12. = A.fold_left (+.) 0. a);;
 
 let () =
   let seq = Seq.(ints 0 |> take 10_000) in
   let a = A.of_seq seq in
-  assert (Some 9999 = A.pop a);
-  assert (Some 9998 = A.pop a);
-  assert (Some 9997 = A.pop a);
+  assert (Some 9999 = A.pop_last_opt a);
+  assert (Some 9998 = A.pop_last_opt a);
+  assert (Some 9997 = A.pop_last_opt a);
   assert (9997 = A.length a);
   ();;
 
 let () =
   let a = A.of_list [1;2] in
-  assert (Some 2 = A.pop a);
-  assert (Some 1 = A.pop a);
-  assert (None = A.pop a);
-  assert (None = A.pop a);
+  assert (Some 2 = A.pop_last_opt a);
+  assert (Some 1 = A.pop_last_opt a);
+  assert (None = A.pop_last_opt a);
+  assert (None = A.pop_last_opt a);
   ();;
 
 let () =
   let a = A.of_list [1;2;3] in
-  A.push a 4;
+  A.push_back a 4;
   assert (A.to_list a = [1;2;3;4]);;
 
 let list_range start len : _ list =
@@ -106,7 +106,7 @@ let () =
 
 let () =
   let a = A.create() in
-  for i=0 to 20 do A.push a i; done;
+  for i=0 to 20 do A.push_back a i; done;
   assert (A.to_list (A.copy a) = list_range 0 21);;
 
 let () =
@@ -115,7 +115,7 @@ let () =
 
 let () =
   let a = A.create() in
-  for i=0 to 20_000 do A.push a i; done;
+  for i=0 to 20_000 do A.push_back a i; done;
   List.iter
     (fun size ->
       A.truncate a size;
@@ -125,7 +125,7 @@ let () =
 let () =
   let a = A.create() in
   for i = 0 to 200 do
-    A.push a i;
+    A.push_back a i;
   done;
   A.shrink_capacity a;
   assert (A.length a = 201);;
@@ -152,17 +152,17 @@ let () =
   let a = A.create() in
   A.ensure_capacity_with ~filler:42 a 200;
   for i=1 to 200 do
-    A.unsafe_push a i
+    A.unsafe_push_back a i
   done;
   assert (A.length a = 200);
   assert (A.to_list a = list_range 1 200);;
 
 let () =
   let a = A.create() in
-  A.push a 1;
+  A.push_back a 1;
   A.ensure_capacity_nonempty a 200;
   for i=2 to 200 do
-    A.unsafe_push a i
+    A.unsafe_push_back a i
   done;
   assert (A.length a = 200);
   assert (A.to_list a = list_range 1 200);;
