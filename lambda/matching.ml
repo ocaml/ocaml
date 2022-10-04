4d80083ff8f20239db61f9963038cbda9bd30d44
Gabriel Scherer <gabriel.scherer@gmail.com>
2022-10-04 20:05:03 +0200

    Merge pull request #11587 from lthls/poly-variant-pointer-comparison
    Stop using integer comparison on potential pointers
    
    (cherry picked from commit 457ed4e6cd8c838e829d23fb7441394a76cf634a)

--- lambda/matching.ml
+++ lambda/matching.ml
@@ -2946,9 +2946,14 @@ let combine_variant loc row arg partial ctx def (tag_lambda_list, total1, _pats)
         match (consts, nonconsts) with
         | [ (_, act1) ], [ (_, act2) ] when fail = None ->
             test_int_or_block arg act1 act2
-        | _, [] ->
-            (* One can compare integers and pointers *)
-            make_test_sequence_variant_constant fail arg consts
+        | _, [] -> (
+            let lam = make_test_sequence_variant_constant fail arg consts in
+            (* PR#11587: Switcher.test_sequence expects integer inputs, so
+               if the type allows pointers we must filter them away. *)
+            match fail with
+            | None -> lam
+            | Some fail -> test_int_or_block arg lam fail
+          )
         | [], _ -> (
             let lam = call_switcher_variant_constr loc fail arg nonconsts in
             (* One must not dereference integers *)
