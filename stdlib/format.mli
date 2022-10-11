adb19d81050ad4f6f67a2a80bac7cfc54185a1ee
amaluxx <amandinenangah1@gmail.com>
2022-10-11 16:58:55 +0100

    Documentation: changed 'good break' to 'full break' in Format module

--- stdlib/format.mli
+++ stdlib/format.mli
@@ -1285,7 +1285,7 @@ val fprintf : formatter -> ('a, formatter, unit) format -> 'a
     optionally specified with the following syntax:
     the [<] character, followed by an integer [nspaces] value,
     then an integer [offset], and a closing [>] character.
-    If no parameters are provided, the good break defaults to a
+    If no parameters are provided, the full break defaults to a
     'space' break hint.
   - [@.]: flush the pretty-printer and split the line, as with
     [print_newline ()].
