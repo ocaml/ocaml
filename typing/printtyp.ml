f1042077b1e6b17cac6f02191ba22e24b97e4ea1
Florian Angeletti <florian.angeletti@inria.fr>
2022-10-20 17:34:38 +0200

    review: reduced diff

--- typing/printtyp.ml
+++ typing/printtyp.ml
@@ -379,6 +379,7 @@ let rewrite_double_underscore_paths env p =
     rewrite_double_underscore_paths env p
 
 let rec tree_of_path ?(disambiguation=true) namespace p =
+  let tree_of_path namespace p = tree_of_path ~disambiguation namespace p in
   let namespace = if disambiguation then namespace else Other in
   match p with
   | Pident id ->
@@ -386,10 +387,10 @@ let rec tree_of_path ?(disambiguation=true) namespace p =
   | Pdot(_, s) as path when non_shadowed_stdlib namespace path ->
       Oide_ident (Out_name.create s)
   | Pdot(p, s) ->
-      Oide_dot (tree_of_path ~disambiguation Module p, s)
+      Oide_dot (tree_of_path Module p, s)
   | Papply(p1, p2) ->
-      let t1 = tree_of_path ~disambiguation Module p1 in
-      let t2 = tree_of_path ~disambiguation Module p2 in
+      let t1 = tree_of_path Module p1 in
+      let t2 = tree_of_path Module p2 in
       Oide_apply (t1, t2)
   | Pextra_ty (p, extra) -> begin
       (* inline record types are syntactically prevented from escaping their
