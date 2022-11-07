bc4ade4ed5dcebab9d33b46e70c93f590c3f50b2
Nicolás Ojeda Bär <n.oje.bar@gmail.com>
2022-11-07 13:57:03 +0100

    Comment

--- runtime/caml/platform.h
+++ runtime/caml/platform.h
@@ -40,6 +40,7 @@ Caml_inline void cpu_relax() {
 #elif defined(__aarch64__)
   __asm__ volatile ("yield" ::: "memory");
 #elif defined(__riscv)
+  /* Encoding of the pause instruction */
   __asm__ volatile (".4byte 0x100000F");
 #else
   /* Just a compiler barrier */
