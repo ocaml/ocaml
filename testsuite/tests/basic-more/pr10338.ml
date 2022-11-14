a062495af6ad70cc53f7278ab1a0c1ee5f0fa67f
Stephen Dolan <sdolan@janestreet.com>
2022-11-14 14:40:01 +0000

    Ensure push_defaults can push past module patterns

--- testsuite/tests/basic-more/pr10338.ml
+++ testsuite/tests/basic-more/pr10338.ml
@@ -19,3 +19,8 @@ let f ?(y=1) : empty -> int = function _ -> .;;
 module type E = sig exception Ex end;;
 let f ((module M) : (module E)) (M.Ex | _) = "42";;
 print_endline (f (module struct exception Ex end) Exit);;
+
+(* push_defaults should push past the module pattern *)
+let f ?(x = assert false) ((module M) : (module E)) () = 1;;
+(* so partial application of the module doesn't raise *)
+let partial = f (module struct exception Ex end);;
