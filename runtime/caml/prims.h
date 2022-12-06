1457bfc44268f3043ebf0c3fa7cbc5c7b2981aa4
Antonin Décimo <antonin@tarides.com>
2022-12-06 17:37:12 +0100

    Add typedefs for primitive types

--- runtime/caml/prims.h
+++ runtime/caml/prims.h
@@ -21,6 +21,12 @@
 #ifdef CAML_INTERNALS
 
 typedef value (*c_primitive)(void);
+typedef value (*c_primitive1)(value);
+typedef value (*c_primitive2)(value, value);
+typedef value (*c_primitive3)(value, value, value);
+typedef value (*c_primitive4)(value, value, value, value);
+typedef value (*c_primitive5)(value, value, value, value, value);
+typedef value (*c_primitiveN)(value *, int);
 
 extern c_primitive caml_builtin_cprim[];
 extern char * caml_names_of_builtin_cprim[];
@@ -30,15 +36,12 @@ extern struct ext_table caml_prim_table;
 extern struct ext_table caml_prim_name_table;
 #endif
 
-#define Primitive1(n) ((value (*)(value))caml_prim_table.contents[n])
-#define Primitive2(n) ((value (*)(value,value))caml_prim_table.contents[n])
-#define Primitive3(n) \
-    ((value (*)(value,value,value))caml_prim_table.contents[n])
-#define Primitive4(n) \
-    ((value (*)(value,value,value,value))caml_prim_table.contents[n])
-#define Primitive5(n) \
-    ((value (*)(value,value,value,value,value))caml_prim_table.contents[n])
-#define PrimitiveN(n) ((value (*)(value*,int))caml_prim_table.contents[n])
+#define Primitive1(n) ((c_primitive1)caml_prim_table.contents[n])
+#define Primitive2(n) ((c_primitive2)caml_prim_table.contents[n])
+#define Primitive3(n) ((c_primitive3)caml_prim_table.contents[n])
+#define Primitive4(n) ((c_primitive4)caml_prim_table.contents[n])
+#define Primitive5(n) ((c_primitive5)caml_prim_table.contents[n])
+#define PrimitiveN(n) ((c_primitiveN)caml_prim_table.contents[n])
 
 extern char * caml_section_table;
 extern asize_t caml_section_table_size;
