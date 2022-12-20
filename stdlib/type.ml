5b8a6c698d6ba66206980c70a35b45ae511c8807
Daniel Bünzli <daniel.buenzli@erratique.ch>
2022-12-20 23:13:00 +0100

    Add Type.Id

--- stdlib/type.ml
+++ stdlib/type.ml
@@ -13,4 +13,31 @@
 (*                                                                        *)
 (**************************************************************************)
 
+(* Type equality witness *)
+
 type (_, _) eq = Equal: ('a, 'a) eq
+
+(* Type identifiers *)
+
+module Id = struct
+  type _ id = ..
+  module type ID = sig
+    type t
+    type _ id += Id : t id
+  end
+
+  type 'a t = (module ID with type t = 'a)
+
+  let make () (type s) =
+    let module T = struct
+      type t = s
+      type _ id += Id : t id end
+    in
+    (module T : ID with type t = s)
+
+  let equal (type t0) (type t1) (t0 : t0 t) (t1 : t1 t) : (t0, t1) eq option
+    =
+    let module T0 = (val t0 : ID with type t = t0) in
+    let module T1 = (val t1 : ID with type t = t1) in
+    match T0.Id with T1.Id -> Some Equal | _ -> None
+end
--- stdlib/type.mli
+++ stdlib/type.mli
@@ -35,3 +35,78 @@ type (_, _) eq = Equal: ('a, 'a) eq (** *)
 
     At runtime, this function simply returns its second argument unchanged.
 *)
+
+(** {1:identifiers Type identifiers} *)
+
+(** Type identifiers.
+
+    Type identifiers are values that can be tested for
+    {{!Id.equal}equality} to prove they represent the same type. Note
+    that unequal identifiers does not imply unequal types: the same
+    type can be represented by multiple identifiers.
+
+    See an {{!Id.example}example} of use. *)
+module Id : sig
+
+  (** {1:ids Type identifiers} *)
+
+  type 'a t
+  (** The type for type identifiers. ['a] is the represented type. *)
+
+  val make : unit -> 'a t
+  (** [make ()] is a new type identifier. *)
+
+  val equal : 'a t -> 'b t -> ('a, 'b) eq option
+  (** [equal id0 id1] is [Some Equal] if identifier [id0] is equal to [id1]
+      and [None] otherwise. *)
+
+  (** {1:example Example}
+
+      The following shows how type identifiers can be used to implement
+      an heterogeneous key-value dictionary.
+{[
+(** Heterogeneous dictionaries. *)
+module Dict : sig
+  type t
+  (** The type for dictionaries. *)
+
+  type 'a key
+  (** The type for keys binding values of type ['a]. *)
+
+  val key : unit -> 'a key
+  (** [key ()] is a new dictionary key. *)
+
+  val empty : t
+  (** [empty] is the empty dictionary. *)
+
+  val add : 'a key -> 'a -> t -> t
+  (** [add k v d] is [d] with [k] bound to [v]. *)
+
+  val remove : 'a key -> t -> t
+  (** [remove k d] is [d] with the last binding of [k] removed. *)
+
+  val find : 'a key -> t -> 'a option
+  (** [find k d] is the binding of [k] in [d], if any. *)
+end = struct
+  type 'a key = { tid : 'a Type.Id.t; exist : exist_key }
+  and exist_key = K : 'a key -> exist_key
+
+  let key () = let rec k = { tid = Type.Id.make (); exist = K k } in k
+
+  type binding = B : 'a key * 'a -> binding
+  type t = (exist_key * binding) list
+
+  let empty = []
+  let add k v d = (k.exist, B (k, v)) :: d
+  let remove k d = List.remove_assq k.exist d
+  let find : type a. a key -> t -> a option = fun k d ->
+    match List.assq_opt k.exist d with
+    | None -> None
+    | Some (B (k', v)) ->
+        match Type.Id.equal k.tid k'.tid with
+        | None -> None
+        | Some Type.Equal -> Some v
+end
+]}
+*)
+end
