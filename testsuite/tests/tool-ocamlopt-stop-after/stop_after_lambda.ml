c3a96d294b831d5b8e65e600963568d6ba5a8087
Gabriel Scherer <gabriel.scherer@gmail.com>
2023-01-12 10:44:38 +0100

    disable the ocamlopt -stop-after-lambda test when flambda (different output)

--- testsuite/tests/tool-ocamlopt-stop-after/stop_after_lambda.ml
+++ testsuite/tests/tool-ocamlopt-stop-after/stop_after_lambda.ml
@@ -1,10 +1,15 @@
 (* TEST
-* setup-ocamlopt.byte-build-env
-** ocamlopt.byte
+* no-flambda
+** setup-ocamlopt.byte-build-env
+*** ocamlopt.byte
     flags = "-dlambda -stop-after lambda -nopervasives "
     ocamlopt_byte_exit_status = "0"
-*** check-ocamlopt.byte-output
+**** check-ocamlopt.byte-output
 *)
+
+(* no-flambda: the -lambda output differs with flambda, and
+   maintaining two outputs is inconvenient for a test that is not
+   really related to code generation. *)
 
 external p : int -> unit = ""
 let () = p 1
