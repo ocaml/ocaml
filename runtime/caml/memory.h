7911db5acb89ad4885ce258a94349677a7376475
kirisky <youandox@gmail.com>
2023-01-15 23:42:11 +0200

    restore the description in memory.h

--- runtime/caml/memory.h
+++ runtime/caml/memory.h
@@ -267,7 +267,7 @@ struct caml__roots_block {
    Your function may raise an exception or return a [value] with the
    [CAMLreturn] macro.  Its argument is simply the [value] returned by
    your function.  Do NOT directly return a [value] with the [return]
-   keyword.  If your function returns void, use [CAMLreturn0].
+   keyword. If your function returns void, use [CAMLreturn0].
    If you un-register the local roots (i.e. undo the effects of the [CAMLparam*]
    and [CAMLlocal] macros) without returning immediately, use [CAMLdrop].
 
