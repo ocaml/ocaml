58356c7928e98f394fb265fb870f7f921e397907
Bartosz Modelski <modelski.bartosz@gmail.com>
2023-01-30 19:16:18 +0000

    Add cache-aligned atomic
    Cache-aligned atomic to avoid false-sharing in lock-free algorithms.
    This patch relies on the slab allocator in shared heap, which is
    cache-line-aligned by design.

--- stdlib/atomic.ml
+++ stdlib/atomic.ml
@@ -15,6 +15,7 @@
 type !'a t
 
 external make : 'a -> 'a t = "%makemutable"
+external make_contended : 'a -> 'a t = "caml_atomic_make_contended"
 external get : 'a t -> 'a = "%atomic_load"
 external exchange : 'a t -> 'a -> 'a = "%atomic_exchange"
 external compare_and_set : 'a t -> 'a -> 'a -> bool = "%atomic_cas"
