81168f8e38abbc35f0fad8105356fa0f3e9afc58
Nicolás Ojeda Bär <n.oje.bar@gmail.com>
2023-02-24 09:40:39 +0100

    check-typo

--- otherlibs/unix/times_unix.c
+++ otherlibs/unix/times_unix.c
@@ -35,11 +35,15 @@ CAMLprim value caml_unix_times(value unit)
   res = caml_alloc_small(4 * Double_wosize, Double_array_tag);
 
   getrusage (RUSAGE_SELF, &ru);
-  Store_double_flat_field (res, 0, ru.ru_utime.tv_sec + ru.ru_utime.tv_usec / 1e6);
-  Store_double_flat_field (res, 1, ru.ru_stime.tv_sec + ru.ru_stime.tv_usec / 1e6);
+  Store_double_flat_field(res, 0,
+                          ru.ru_utime.tv_sec + ru.ru_utime.tv_usec / 1e6);
+  Store_double_flat_field(res, 1,
+                          ru.ru_stime.tv_sec + ru.ru_stime.tv_usec / 1e6);
   getrusage (RUSAGE_CHILDREN, &ru);
-  Store_double_flat_field (res, 2, ru.ru_utime.tv_sec + ru.ru_utime.tv_usec / 1e6);
-  Store_double_flat_field (res, 3, ru.ru_stime.tv_sec + ru.ru_stime.tv_usec / 1e6);
+  Store_double_flat_field(res, 2,
+                          ru.ru_utime.tv_sec + ru.ru_utime.tv_usec / 1e6);
+  Store_double_flat_field(res, 3,
+                          ru.ru_stime.tv_sec + ru.ru_stime.tv_usec / 1e6);
   return res;
 
 #else
