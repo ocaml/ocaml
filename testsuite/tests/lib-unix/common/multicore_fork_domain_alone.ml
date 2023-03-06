ac3367178e964306f119ab736735dfe4714ae1ac
Guillaume Munch-Maccagnoni <Guillaume.Munch-Maccagnoni@inria.fr>
2023-03-06 18:39:51 +0100

    Do not depend on exact error string in testsuite

--- testsuite/tests/lib-unix/common/multicore_fork_domain_alone.ml
+++ testsuite/tests/lib-unix/common/multicore_fork_domain_alone.ml
@@ -12,16 +12,11 @@
 (* on Multicore, fork is not allowed is another domain is, and was running. *)
 (* this test checks that we can't fork if a domain is currently running. *)
 
-let expect_exn ="Unix.fork may not be called while other domains were created"
-
 let () =
   let d = Domain.spawn (fun () -> Unix.sleep 1) in
-  begin match Unix.fork () with
-  | exception Failure msg ->
-     if String.equal msg expect_exn then
-       print_endline "OK"
-     else
-       Printf.printf "failed: expected Failure: %s, got %s\n" expect_exn msg
-  | _ -> print_endline "NOK"
-  end;
-  Domain.join d
+  let res = match Unix.fork () with
+    | exception Failure _ -> 0
+    | _ -> 1
+  in
+  Domain.join d;
+  exit res
