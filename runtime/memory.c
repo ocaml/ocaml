b9f68eaef332e5b3cd74f735cb50739ab696e418
Damien Doligez <damien.doligez@inria.fr>
2023-03-09 16:36:03 +0100

    more GC slices when allocating directly in major heap / extra heap resources

--- runtime/memory.c
+++ runtime/memory.c
@@ -192,9 +192,8 @@ CAMLexport void caml_adjust_gc_speed (mlsize_t res, mlsize_t max)
   if (max == 0) max = 1;
   if (res > max) res = max;
   Caml_state->extra_heap_resources += (double) res / (double) max;
-  if (Caml_state->extra_heap_resources > 1.0){
+  if (Caml_state->extra_heap_resources > 0.5){
     CAML_EV_COUNTER (EV_C_REQUEST_MAJOR_ADJUST_GC_SPEED, 1);
-    Caml_state->extra_heap_resources = 1.0;
     caml_request_major_slice_global ();
   }
 }
@@ -342,7 +341,7 @@ Caml_inline value alloc_shr(mlsize_t wosize, tag_t tag, reserved_t reserved,
   }
 
   dom_st->allocated_words += Whsize_wosize(wosize);
-  if (dom_st->allocated_words > dom_st->minor_heap_wsz) {
+  if (dom_st->allocated_words > dom_st->minor_heap_wsz / 2) {
     CAML_EV_COUNTER (EV_C_REQUEST_MAJOR_ALLOC_SHR, 1);
     caml_request_major_slice_global();
   }
