d509725fa2b0e7c9ed62c28bb02a54eee2a9593f
Florian Angeletti <florian.angeletti@inria.fr>
2023-03-14 11:19:14 +0100

    testsuite: recursive polyvariant and weak row variables

--- testsuite/tests/typing-misc/polyvars.ml
+++ testsuite/tests/typing-misc/polyvars.ml
@@ -220,3 +220,15 @@ type a = int
 type t = [ `A of a ]
 val inspect : [< `A of a & int ] -> unit = <fun>
 |}]
+
+(** Error messages with weakly polymorphic row variables *)
+let x = Fun.id (function `X -> () | _ -> ())
+[%%expect {|
+val x : ([> `X ] as '_weak1) -> unit = <fun>
+|}]
+
+let x = let rec x = `X (`Y (fun y -> x = y)) in Fun.id x
+[%%expect {|
+val x : [> `X of [> `Y of '_weak2 -> bool ] as '_weak3 ] as '_weak2 =
+  `X (`Y <fun>)
+|}]
