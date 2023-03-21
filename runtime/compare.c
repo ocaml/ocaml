78be3f27d9e834d5379729be788b3fbda407ae3d
Gabriel Scherer <gabriel.scherer@gmail.com>
2023-03-21 23:26:57 +0100

    Poll for interrupts in structural equality loop
    Co-authored-by: eutro <git@eutro.dev>
    Co-authored-by: Xavier Leroy <xavier.leroy@college-de-france.fr>

--- runtime/compare.c
+++ runtime/compare.c
@@ -37,6 +37,8 @@ struct compare_stack {
   struct compare_item* limit;
 };
 
+#define COMPARE_POLL_PERIOD 1024
+
 /* Free the compare stack if needed */
 static void compare_free_stack(struct compare_stack* stk)
 {
@@ -84,7 +86,6 @@ static struct compare_item * compare_resize_stack(struct compare_stack* stk,
   return newstack + sp_offset;
 }
 
-
 static intnat do_compare_val(struct compare_stack* stk,
                              value v1, value v2, int total);
 
@@ -97,6 +98,24 @@ static intnat compare_val(value v1, value v2, int total)
   res = do_compare_val(&stk, v1, v2, total);
   compare_free_stack(&stk);
   return res;
+}
+
+static void run_pending_actions(struct compare_stack* stk,
+                                struct compare_item* sp)
+{
+  value exn;
+  value* roots_start = (value*)(stk->stack);
+  size_t roots_length =
+    (sp - stk->stack)
+    * sizeof(struct compare_item) / sizeof(value);
+  Begin_roots_block(roots_start, roots_length);
+  exn = caml_do_pending_actions_exn();
+  End_roots();
+  if (Is_exception_result(exn)) {
+    exn = Extract_exception(exn);
+    compare_free_stack(stk);
+    caml_raise(exn);
+  }
 }
 
 /* Structural comparison */
@@ -118,188 +137,206 @@ static intnat do_compare_val(struct compare_stack* stk,
 {
   struct compare_item * sp;
   tag_t t1, t2;
+  int poll_timer;
 
   sp = stk->stack;
   while (1) {
-    if (v1 == v2 && total) goto next_item;
-    if (Is_long(v1)) {
-      if (v1 == v2) goto next_item;
-      if (Is_long(v2))
-        return Long_val(v1) - Long_val(v2);
-      /* Subtraction above cannot overflow and cannot result in UNORDERED */
-      switch (Tag_val(v2)) {
-        case Forward_tag:
-          v2 = Forward_val(v2);
-          continue;
-        case Custom_tag: {
-          int res;
-          int (*compare)(value v1, value v2) = Custom_ops_val(v2)->compare_ext;
-          if (compare == NULL) break;  /* for backward compatibility */
-          Caml_state->compare_unordered = 0;
-          res = compare(v1, v2);
-          if (Caml_state->compare_unordered && !total) return UNORDERED;
-          if (res != 0) return res;
-          goto next_item;
-        }
-        default: /*fallthrough*/;
-        }
+    poll_timer = COMPARE_POLL_PERIOD;
+    while (--poll_timer > 0) {
+      if (v1 == v2 && total) goto next_item;
+      if (Is_long(v1)) {
+        if (v1 == v2) goto next_item;
+        if (Is_long(v2))
+          return Long_val(v1) - Long_val(v2);
+        /* Subtraction above cannot overflow and cannot result in UNORDERED */
+        switch (Tag_val(v2)) {
+          case Forward_tag:
+            v2 = Forward_val(v2);
+            continue;
+          case Custom_tag: {
+            int res;
+            int (*compare)(value v1, value v2) =
+              Custom_ops_val(v2)->compare_ext;
+            if (compare == NULL) break;  /* for backward compatibility */
+            Caml_state->compare_unordered = 0;
+            res = compare(v1, v2);
+            if (Caml_state->compare_unordered && !total) return UNORDERED;
+            if (res != 0) return res;
+            goto next_item;
+          }
+          default: /*fallthrough*/;
+          }
 
-      return LESS;                /* v1 long < v2 block */
-    }
-    if (Is_long(v2)) {
-      switch (Tag_val(v1)) {
-        case Forward_tag:
-          v1 = Forward_val(v1);
-          continue;
-        case Custom_tag: {
-          int res;
-          int (*compare)(value v1, value v2) = Custom_ops_val(v1)->compare_ext;
-          if (compare == NULL) break;  /* for backward compatibility */
-          Caml_state->compare_unordered = 0;
-          res = compare(v1, v2);
-          if (Caml_state->compare_unordered && !total) return UNORDERED;
-          if (res != 0) return res;
-          goto next_item;
-        }
-        default: /*fallthrough*/;
-        }
-      return GREATER;            /* v1 block > v2 long */
-    }
-    t1 = Tag_val(v1);
-    t2 = Tag_val(v2);
-    if (t1 != t2) {
-        /* Besides long/block comparisons, the only forms of
-           heterogeneous comparisons we support are:
-           - Forward_tag pointers, which may point to values of any type, and
-           - comparing Infix_tag and Closure_tag functions (#9521).
-
-           Other heterogeneous cases may still happen due to
-           existential types, and we just compare the tags.
-        */
-        if (t1 == Forward_tag) { v1 = Forward_val (v1); continue; }
-        if (t2 == Forward_tag) { v2 = Forward_val (v2); continue; }
-        if (t1 == Infix_tag) t1 = Closure_tag;
-        if (t2 == Infix_tag) t2 = Closure_tag;
-        if (t1 != t2)
-            return (intnat)t1 - (intnat)t2;
-    }
-    switch(t1) {
-    case Forward_tag: {
-        v1 = Forward_val (v1);
-        v2 = Forward_val (v2);
-        continue;
-    }
-    case String_tag: {
-      mlsize_t len1, len2;
-      int res;
-      if (v1 == v2) break;
-      len1 = caml_string_length(v1);
-      len2 = caml_string_length(v2);
-      res = memcmp(String_val(v1), String_val(v2), len1 <= len2 ? len1 : len2);
-      if (res < 0) return LESS;
-      if (res > 0) return GREATER;
-      if (len1 != len2) return len1 - len2;
-      break;
-    }
-    case Double_tag: {
-      double d1 = Double_val(v1);
-      double d2 = Double_val(v2);
-      if (d1 < d2) return LESS;
-      if (d1 > d2) return GREATER;
-      if (d1 != d2) {
-        if (! total) return UNORDERED;
-        /* One or both of d1 and d2 is NaN.  Order according to the
-           convention NaN = NaN and NaN < f for all other floats f. */
-        if (d1 == d1) return GREATER; /* d1 is not NaN, d2 is NaN */
-        if (d2 == d2) return LESS;    /* d2 is not NaN, d1 is NaN */
-        /* d1 and d2 are both NaN, thus equal: continue comparison */
+        return LESS;                /* v1 long < v2 block */
       }
-      break;
-    }
-    case Double_array_tag: {
-      mlsize_t sz1 = Wosize_val(v1) / Double_wosize;
-      mlsize_t sz2 = Wosize_val(v2) / Double_wosize;
-      mlsize_t i;
-      if (sz1 != sz2) return sz1 - sz2;
-      for (i = 0; i < sz1; i++) {
-        double d1 = Double_flat_field(v1, i);
-        double d2 = Double_flat_field(v2, i);
+      if (Is_long(v2)) {
+        switch (Tag_val(v1)) {
+          case Forward_tag:
+            v1 = Forward_val(v1);
+            continue;
+          case Custom_tag: {
+            int res;
+            int (*compare)(value v1, value v2) =
+              Custom_ops_val(v1)->compare_ext;
+            if (compare == NULL) break;  /* for backward compatibility */
+            Caml_state->compare_unordered = 0;
+            res = compare(v1, v2);
+            if (Caml_state->compare_unordered && !total) return UNORDERED;
+            if (res != 0) return res;
+            goto next_item;
+          }
+          default: /*fallthrough*/;
+          }
+        return GREATER;            /* v1 block > v2 long */
+      }
+      t1 = Tag_val(v1);
+      t2 = Tag_val(v2);
+      if (t1 != t2) {
+          /* Besides long/block comparisons, the only forms of
+             heterogeneous comparisons we support are:
+             - Forward_tag pointers, which may point to values of any type, and
+             - comparing Infix_tag and Closure_tag functions (#9521).
+
+             Other heterogeneous cases may still happen due to
+             existential types, and we just compare the tags.
+          */
+          if (t1 == Forward_tag) { v1 = Forward_val (v1); continue; }
+          if (t2 == Forward_tag) { v2 = Forward_val (v2); continue; }
+          if (t1 == Infix_tag) t1 = Closure_tag;
+          if (t2 == Infix_tag) t2 = Closure_tag;
+          if (t1 != t2)
+              return (intnat)t1 - (intnat)t2;
+      }
+      switch(t1) {
+      case Forward_tag: {
+          v1 = Forward_val (v1);
+          v2 = Forward_val (v2);
+          continue;
+      }
+      case String_tag: {
+        mlsize_t len1, len2;
+        int res;
+        if (v1 == v2) break;
+        len1 = caml_string_length(v1);
+        len2 = caml_string_length(v2);
+        res = memcmp(String_val(v1), String_val(v2),
+                     len1 <= len2 ? len1 : len2);
+        if (res < 0) return LESS;
+        if (res > 0) return GREATER;
+        if (len1 != len2) return len1 - len2;
+        break;
+      }
+      case Double_tag: {
+        double d1 = Double_val(v1);
+        double d2 = Double_val(v2);
         if (d1 < d2) return LESS;
         if (d1 > d2) return GREATER;
         if (d1 != d2) {
           if (! total) return UNORDERED;
-          /* See comment for Double_tag case */
-          if (d1 == d1) return GREATER;
-          if (d2 == d2) return LESS;
+          /* One or both of d1 and d2 is NaN.  Order according to the
+             convention NaN = NaN and NaN < f for all other floats f. */
+          if (d1 == d1) return GREATER; /* d1 is not NaN, d2 is NaN */
+          if (d2 == d2) return LESS;    /* d2 is not NaN, d1 is NaN */
+          /* d1 and d2 are both NaN, thus equal: continue comparison */
         }
+        break;
       }
-      break;
-    }
-    case Abstract_tag:
-      compare_free_stack(stk);
-      caml_invalid_argument("compare: abstract value");
-    case Closure_tag:
-    case Infix_tag:
-      compare_free_stack(stk);
-      caml_invalid_argument("compare: functional value");
-    case Cont_tag:
-      compare_free_stack(stk);
-      caml_invalid_argument("compare: continuation value");
-    case Object_tag: {
-      intnat oid1 = Oid_val(v1);
-      intnat oid2 = Oid_val(v2);
-      if (oid1 != oid2) return oid1 - oid2;
-      break;
-    }
-    case Custom_tag: {
-      int res;
-      int (*compare)(value v1, value v2) = Custom_ops_val(v1)->compare;
-      /* Hardening against comparisons between different types */
-      if (compare != Custom_ops_val(v2)->compare) {
-        return strcmp(Custom_ops_val(v1)->identifier,
-                      Custom_ops_val(v2)->identifier) < 0
-               ? LESS : GREATER;
+      case Double_array_tag: {
+        mlsize_t sz1 = Wosize_val(v1) / Double_wosize;
+        mlsize_t sz2 = Wosize_val(v2) / Double_wosize;
+        mlsize_t i;
+        if (sz1 != sz2) return sz1 - sz2;
+        for (i = 0; i < sz1; i++) {
+          double d1 = Double_flat_field(v1, i);
+          double d2 = Double_flat_field(v2, i);
+          if (d1 < d2) return LESS;
+          if (d1 > d2) return GREATER;
+          if (d1 != d2) {
+            if (! total) return UNORDERED;
+            /* See comment for Double_tag case */
+            if (d1 == d1) return GREATER;
+            if (d2 == d2) return LESS;
+          }
+        }
+        break;
       }
-      if (compare == NULL) {
+      case Abstract_tag:
         compare_free_stack(stk);
         caml_invalid_argument("compare: abstract value");
+      case Closure_tag:
+      case Infix_tag:
+        compare_free_stack(stk);
+        caml_invalid_argument("compare: functional value");
+      case Cont_tag:
+        compare_free_stack(stk);
+        caml_invalid_argument("compare: continuation value");
+      case Object_tag: {
+        intnat oid1 = Oid_val(v1);
+        intnat oid2 = Oid_val(v2);
+        if (oid1 != oid2) return oid1 - oid2;
+        break;
       }
-      Caml_state->compare_unordered = 0;
-      res = compare(v1, v2);
-      if (Caml_state->compare_unordered && !total) return UNORDERED;
-      if (res != 0) return res;
-      break;
-    }
-    default: {
-      mlsize_t sz1 = Wosize_val(v1);
-      mlsize_t sz2 = Wosize_val(v2);
-      /* Compare sizes first for speed */
-      if (sz1 != sz2) return sz1 - sz2;
-      if (sz1 == 0) break;
-      /* Remember that we still have to compare fields 1 ... sz - 1. */
-      if (sz1 > 1) {
-        if (sp >= stk->limit) sp = compare_resize_stack(stk, sp);
-        struct compare_item* next = sp++;
-        next->v1 = v1;
-        next->v2 = v2;
-        next->size = Val_long(sz1);
-        next->offset = Val_long(1);
+      case Custom_tag: {
+        int res;
+        int (*compare)(value v1, value v2) = Custom_ops_val(v1)->compare;
+        /* Hardening against comparisons between different types */
+        if (compare != Custom_ops_val(v2)->compare) {
+          return strcmp(Custom_ops_val(v1)->identifier,
+                        Custom_ops_val(v2)->identifier) < 0
+                 ? LESS : GREATER;
+        }
+        if (compare == NULL) {
+          compare_free_stack(stk);
+          caml_invalid_argument("compare: abstract value");
+        }
+        Caml_state->compare_unordered = 0;
+        res = compare(v1, v2);
+        if (Caml_state->compare_unordered && !total) return UNORDERED;
+        if (res != 0) return res;
+        break;
       }
-      /* Continue comparison with first field */
-      v1 = Field(v1, 0);
-      v2 = Field(v2, 0);
-      continue;
-    }
-    }
-  next_item:
-    /* Pop one more item to compare, if any */
-    if (sp == stk->stack) return EQUAL; /* we're done */
+      default: {
+        mlsize_t sz1 = Wosize_val(v1);
+        mlsize_t sz2 = Wosize_val(v2);
+        /* Compare sizes first for speed */
+        if (sz1 != sz2) return sz1 - sz2;
+        if (sz1 == 0) break;
+        /* Remember that we still have to compare fields 1 ... sz - 1. */
+        if (sz1 > 1) {
+          if (sp >= stk->limit) sp = compare_resize_stack(stk, sp);
+          struct compare_item* next = sp++;
+          next->v1 = v1;
+          next->v2 = v2;
+          next->size = Val_long(sz1);
+          next->offset = Val_long(1);
+        }
+        /* Continue comparison with first field */
+        v1 = Field(v1, 0);
+        v2 = Field(v2, 0);
+        continue;
+      }
+      }
+    next_item:
+      /* Pop one more item to compare, if any */
+      if (sp == stk->stack) return EQUAL; /* we're done */
 
-    struct compare_item* last = sp-1;
-    v1 = Field(last->v1, Long_val(last->offset));
-    v2 = Field(last->v2, Long_val(last->offset));
-    last->offset += 2;/* Long_val(last->offset) += 1 */
-    if (last->offset == last->size) sp--;
+      struct compare_item* last = sp-1;
+      v1 = Field(last->v1, Long_val(last->offset));
+      v2 = Field(last->v2, Long_val(last->offset));
+      last->offset += 2;/* Long_val(last->offset) += 1 */
+      if (last->offset == last->size) sp--;
+    }
+    /* Poll for actions */
+    if (caml_check_pending_actions()) {
+      /* Preserve v1, v2 if a GC occurs.  Registering v1 and v2 directly
+         as roots would prevent them from being allocated to registers. */
+      value root_v1 = v1, root_v2 = v2;
+      Begin_roots2(root_v1, root_v2);
+      run_pending_actions(stk, sp);
+      v1 = root_v1; v2 = root_v2;
+      End_roots();
+    }
+    /* Resume comparison after resetting poll_timer */
   }
 }
 
