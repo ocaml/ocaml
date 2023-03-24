a42f608c96d82a66f9d9faaa5fb372e6b24527f1
Florian Angeletti <florian.angeletti@inria.fr>
2023-03-24 17:15:43 +0100

    Fix no-float-array test after #12024

--- testsuite/tests/typing-misc/pr6939-no-flat-float-array.ml
+++ testsuite/tests/typing-misc/pr6939-no-flat-float-array.ml
@@ -9,6 +9,7 @@ Line 1, characters 12-19:
 1 | let rec x = [| x |]; 1.;;
                 ^^^^^^^
 Warning 10 [non-unit-statement]: this expression should have type unit.
+
 val x : float = 1.
 |}];;
 
@@ -18,6 +19,7 @@ Line 1, characters 16-17:
 1 | let rec x = let u = [|y|] in 10. and y = 1.;;
                     ^
 Warning 26 [unused-var]: unused variable u.
+
 val x : float = 10.
 val y : float = 1.
 |}];;
