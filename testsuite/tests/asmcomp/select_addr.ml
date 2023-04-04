904f50f0d21962edf9629e913d33c39d63a739b4
Vincent Laviron <vincent.laviron@gmail.com>
2023-04-04 18:56:30 +0200

    Fix test on 32-bit architextures

--- testsuite/tests/asmcomp/select_addr.ml
+++ testsuite/tests/asmcomp/select_addr.ml
@@ -5,4 +5,10 @@ let[@inline never][@local never] f n =
   let open Int64 in
   to_int (add n (of_int Int.min_int))
 
-let _ = Printf.printf "0x%x\n%!" (f 1)
+let _ =
+  (* The test only works on architectures where Sys.int_size is 63,
+     as it depends on the exact value of Int.min_int. *)
+  if Sys.int_size <> 63 then
+    Printf.printf "0x4000000000000001\n"
+  else
+    Printf.printf "0x%x\n%!" (f 1)
