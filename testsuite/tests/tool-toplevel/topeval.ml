a3a777f75143964a9b6daef151a308e2448d3e89
Fabian <copy@copy.sh>
2023-05-24 23:15:47 +0800

    add test for #12257

--- testsuite/tests/tool-toplevel/topeval.ml
+++ testsuite/tests/tool-toplevel/topeval.ml
@@ -48,3 +48,10 @@ let List.(String.(_)) = 'd'
 
 let List.(_) : float = 42.0
 ;;
+
+(* issue #12257: external or module alias followed by regular value triggers
+   an exception in ocamlnat *)
+external foo : int -> int -> int = "%addint"
+module S = String
+let x = 42
+;;
