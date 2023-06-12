d74e68ebf7fcdda6b2a0d5a642fb5b6b2c957228
Xavier Leroy <xavier.leroy@college-de-france.fr>
2023-06-12 11:28:34 +0200

    Digest: add generic interface Digest.S and specific implementation Digest.MD5
    Also: use In_channel.with_open_bin.

--- stdlib/digest.ml
+++ stdlib/digest.ml
@@ -13,9 +13,57 @@
 (*                                                                        *)
 (**************************************************************************)
 
-(* Message digest (MD5) *)
+(* Utility functions *)
+
+let hex_of_string d =
+  let char_hex n =
+    Char.chr (if n < 10 then Char.code '0' + n
+                        else Char.code 'a' + n - 10) in
+  let len = String.length d in
+  let result = Bytes.create (len * 2) in
+  for i = 0 to len - 1 do
+    let x = Char.code d.[i] in
+    Bytes.unsafe_set result (i*2) (char_hex (x lsr 4));
+    Bytes.unsafe_set result (i*2+1) (char_hex (x land 0x0f));
+  done;
+  Bytes.unsafe_to_string result
+
+let string_of_hex s =
+  let digit c =
+    match c with
+    | '0'..'9' -> Char.code c - Char.code '0'
+    | 'A'..'F' -> Char.code c - Char.code 'A' + 10
+    | 'a'..'f' -> Char.code c - Char.code 'a' + 10
+    | _ -> invalid_arg "Digest.from_hex" in
+  let byte i = digit s.[i] lsl 4 + digit s.[i+1] in
+  String.init (String.length s / 2) (fun i -> Char.chr (byte (2 * i)))
+
+(* Generic interface *)
+
+module type S = sig
+  type t = string
+  val hash_size : int
+  val compare : t -> t -> int
+  val equal : t -> t -> bool
+  val string : string -> t
+  val bytes : bytes -> t
+  val substring : string -> int -> int -> t
+  val subbytes : bytes -> int -> int -> t
+  val channel : in_channel -> int -> t
+  val file : string -> t
+  val output : out_channel -> t -> unit
+  val input : in_channel -> t
+  val to_hex : t -> string
+  val from_hex : string -> t
+end
+
+(* MD5 hashing *)
+
+module MD5 = struct
 
 type t = string
+
+let hash_size = 16
 
 let compare = String.compare
 let equal = String.equal
@@ -36,41 +84,23 @@ let substring str ofs len =
 let subbytes b ofs len = substring (Bytes.unsafe_to_string b) ofs len
 
 let file filename =
-  let ic = open_in_bin filename in
-  match channel ic (-1) with
-    | d -> close_in ic; d
-    | exception e -> close_in ic; raise e
+  In_channel.with_open_bin filename (fun ic -> channel ic (-1))
 
-let output chan digest =
-  output_string chan digest
+let output chan digest = output_string chan digest
 
 let input chan = really_input_string chan 16
 
-let char_hex n =
-  Char.unsafe_chr (n + if n < 10 then Char.code '0' else (Char.code 'a' - 10))
-
 let to_hex d =
   if String.length d <> 16 then invalid_arg "Digest.to_hex";
-  let result = Bytes.create 32 in
-  for i = 0 to 15 do
-    let x = Char.code d.[i] in
-    Bytes.unsafe_set result (i*2) (char_hex (x lsr 4));
-    Bytes.unsafe_set result (i*2+1) (char_hex (x land 0x0f));
-  done;
-  Bytes.unsafe_to_string result
+  hex_of_string d
 
 let from_hex s =
   if String.length s <> 32 then invalid_arg "Digest.from_hex";
-  let digit c =
-    match c with
-    | '0'..'9' -> Char.code c - Char.code '0'
-    | 'A'..'F' -> Char.code c - Char.code 'A' + 10
-    | 'a'..'f' -> Char.code c - Char.code 'a' + 10
-    | _ -> raise (Invalid_argument "Digest.from_hex")
-  in
-  let byte i = digit s.[i] lsl 4 + digit s.[i+1] in
-  let result = Bytes.create 16 in
-  for i = 0 to 15 do
-    Bytes.set result i (Char.chr (byte (2 * i)));
-  done;
-  Bytes.unsafe_to_string result
+  string_of_hex s
+
+end
+
+(* Default exported implementation is MD5 *)
+
+include MD5
+
--- stdlib/digest.mli
+++ stdlib/digest.mli
@@ -13,16 +13,18 @@
 (*                                                                        *)
 (**************************************************************************)
 
-(** MD5 message digest.
+(** Message digest.
 
-   This module provides functions to compute 128-bit 'digests' of
-   arbitrary-length strings or files. The algorithm used is MD5.
+   This module provides functions to compute 'digests', also known as
+   'hashes', of arbitrary-length strings or files.
+   The supported hashing algorithm is MD5. *)
 
-   The MD5 hash function is not cryptographically secure.
-   Hence, this module should not be used for security-sensitive
-   applications.  More recent, stronger cryptographic primitives
-   should be used instead.
-*)
+(** {1 Basic functions} *)
+
+(** The functions in this section use the MD5 hash function to produce
+   128-bit digests (16 bytes).  MD5 is not cryptographically secure.
+   Hence, these functions should not be used for security-sensitive
+   applications.  *)
 
 type t = string
 (** The type of digests: 16-character strings. *)
@@ -82,3 +84,69 @@ val from_hex : string -> t
     @raise Invalid_argument if the argument is not exactly 32 hexadecimal
    characters.
    @since 4.00 *)
+
+(** {1 Generic interface} *)
+
+module type S = sig
+
+  type t = string
+    (** The type of digests. *)
+
+  val hash_size : int
+    (** The length of digests, in bytes. *)
+
+  val compare : t -> t -> int
+    (** Compare two digests, , with the same specification as
+        {!Stdlib.compare}. *)
+        
+  val equal : t -> t -> bool
+    (** Test two digests for equality. *)
+
+  val string : string -> t
+    (** Return the digest of the given string. *)
+
+  val bytes : bytes -> t
+    (** Return the digest of the given byte sequence. *)
+
+  val substring : string -> int -> int -> t
+    (** [substring s ofs len] returns the digest of the substring
+        of [s] starting at index [ofs] and containing [len] characters. *)
+
+  val subbytes : bytes -> int -> int -> t
+    (** [subbytes s ofs len] returns the digest of the subsequence
+        of [s] starting at index [ofs] and containing [len] bytes. *)
+
+  val channel : in_channel -> int -> t
+    (** Read characters from the channel and return their digest.
+        See {!Digest.channel} for the full specification. *)
+
+  val file : string -> t
+    (** Return the digest of the file whose name is given. *)
+
+  val output : out_channel -> t -> unit
+    (** Write a digest on the given output channel. *)
+
+  val input : in_channel -> t
+    (** Read a digest from the given input channel. *)
+
+  val to_hex : t -> string
+    (** Return the printable hexadecimal representation of the given digest.
+        @raise Invalid_argument if the length of the argument
+        is not [hash_size], *)
+
+  val from_hex : string -> t
+    (** Convert a hexadecimal representation back into the corresponding digest.
+        @raise Invalid_argument if the length of the argument
+        is not [2 * hash_size], or if the arguments contains non-hexadecimal
+        characters. *)
+end
+   (** The signature for a hash function that produces digests of length
+       [hash_size] from character strings, byte arrays, and files.
+       @since 5.2 *)
+
+(** {1 Specific hash functions} *)
+
+module MD5 : S
+  (** [MD5] is the MD5 hash function.  It produces 128-bit (16-byte) digests
+      and is not cryptographically secure at all.
+      @since 5.2 *)
