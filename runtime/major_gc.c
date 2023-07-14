ocaml-multicore/ocaml-tsan

Disable TSan reporting on parts of the runtime
TSan warns about data races on these functions. We reported those
warnings in #11040, and silence them to avoid facing users of
TSan with data race reports that are not related to their code.
This is done in two ways: either un-instrumenting those functions, or
adding their name in __tsan_default_suppressions in tsan.c.

Co-authored-by: Fabrice Buoro <fabrice@tarides.com>

runtime/major_gc.c
@@ -276,6 +276,7 @@ void caml_final_domain_terminate (caml_domain_state *domain_state)
   }
 }
 
+CAMLno_user_tsan /* Disable TSan reports from this function (see #11040) */
 static int no_orphaned_work (void)
 {
   return
@@ -323,6 +324,7 @@ static void orph_ephe_list_verify_status (int status)
 
 static intnat ephe_mark (intnat budget, uintnat for_cycle, int force_alive);
 
+CAMLno_user_tsan /* Disable TSan reports from this function (see #11040) */
 void caml_add_to_orphaned_ephe_list(struct caml_ephe_info* ephe_info)
 {
   caml_plat_lock(&orphaned_lock);
@@ -352,6 +354,7 @@ void caml_add_to_orphaned_ephe_list(struct caml_ephe_info* ephe_info)
   }
 }
 
+CAMLno_user_tsan /* Disable TSan reports from this function (see #11040) */
 void caml_adopt_orphaned_work (void)
 {
   caml_domain_state* domain_state = Caml_state;
@@ -689,6 +692,7 @@ static void realloc_mark_stack (struct mark_stack* stk)
   mark_stack_prune(stk);
 }
 
+CAMLno_user_tsan /* Disable TSan reports from this function (see #11040) */
 Caml_inline void mark_stack_push_range(struct mark_stack* stk,
                                        value* start, value* end)
 {
@@ -702,6 +706,7 @@ Caml_inline void mark_stack_push_range(struct mark_stack* stk,
   me->end = end;
 }
 
+CAMLno_user_tsan /* Disable TSan reports from this function (see #11040) */
 /* returns the work done by skipping unmarkable objects */
 static intnat mark_stack_push_block(struct mark_stack* stk, value block)
 {
@@ -772,6 +777,7 @@ void caml_shrink_mark_stack (void)
 
 void caml_darken_cont(value cont);
 
+CAMLno_user_tsan /* Disable TSan reports from this function (see #11040) */
 static void mark_slice_darken(struct mark_stack* stk, value child,
                               intnat* work)
 {
@@ -816,6 +822,7 @@ static void mark_slice_darken(struct mark_stack* stk, value child,
   }
 }
 
+CAMLno_user_tsan /* Disable TSan reports from this function (see #11040) */
 Caml_noinline static intnat do_some_marking(struct mark_stack* stk,
                                             intnat budget) {
   prefetch_buffer_t pb = { .enqueued = 0, .dequeued = 0,

runtime/minor_gc.c
@@ -185,6 +185,7 @@ header_t caml_get_header_val(value v) {
 }
 
 
+CAMLno_user_tsan /* Disable TSan reports from this function (see #11040) */
 static int try_update_object_header(value v, volatile value *p, value result,
                                     mlsize_t infix_offset) {
   int success = 0;
@@ -232,6 +233,7 @@ static int try_update_object_header(value v, volatile value *p, value result,
 static scanning_action_flags oldify_scanning_flags =
   SCANNING_ONLY_YOUNG_VALUES;
 
+CAMLno_user_tsan /* Disable TSan reports from this function (see #11040) */
 /* Note that the tests on the tag depend on the fact that Infix_tag,
    Forward_tag, and No_scan_tag are contiguous. */
 static void oldify_one (void* st_v, value v, volatile value *p)

runtime/shared_heap.c
@@ -263,6 +263,7 @@ static intnat pool_sweep(struct caml_heap_state* local,
 
 /* Adopt pool from the pool_freelist avail and full pools
    to satisfy an allocation */
+CAMLno_user_tsan /* Disable TSan reports from this function (see #11040) */
 static pool* pool_global_adopt(struct caml_heap_state* local, sizeclass sz)
 {
   pool* r = NULL;
@@ -512,6 +513,7 @@ static intnat pool_sweep(struct caml_heap_state* local, pool** plist,
   return work;
 }
 
+CAMLno_user_tsan /* Disable TSan reports from this function (see #11040) */
 static intnat large_alloc_sweep(struct caml_heap_state* local) {
   value* p;
   header_t hd;

ocaml/ocaml

runtime/major_gc.c
@@ -322,6 +322,7 @@ void caml_final_domain_terminate (caml_domain_state *domain_state)
   }
 }
 
+CAMLno_tsan /* Disable TSan reports from this function (see #11040) */
 static int no_orphaned_work (void)
 {
   return
@@ -363,6 +364,7 @@ static void orph_ephe_list_verify_status (int status)
 
 static intnat ephe_mark (intnat budget, uintnat for_cycle, int force_alive);
 
+CAMLno_tsan /* Disable TSan reports from this function (see #11040) */
 void caml_add_to_orphaned_ephe_list(struct caml_ephe_info* ephe_info)
 {
   caml_plat_lock(&orphaned_lock);
@@ -392,6 +394,7 @@ void caml_add_to_orphaned_ephe_list(struct caml_ephe_info* ephe_info)
   }
 }
 
+CAMLno_tsan /* Disable TSan reports from this function (see #11040) */
 void caml_adopt_orphaned_work (void)
 {
   caml_domain_state* domain_state = Caml_state;
@@ -758,6 +761,7 @@ static void realloc_mark_stack (struct mark_stack* stk)
   mark_stack_prune(stk);
 }
 
+CAMLno_tsan /* Disable TSan reports from this function (see #11040) */
 Caml_inline void mark_stack_push_range(struct mark_stack* stk,
                                        value_ptr start, value_ptr end)
 {
@@ -771,6 +775,7 @@ Caml_inline void mark_stack_push_range(struct mark_stack* stk,
   me->end = end;
 }
 
+CAMLno_tsan /* Disable TSan reports from this function (see #11040) */
 /* returns the work done by skipping unmarkable objects */
 static intnat mark_stack_push_block(struct mark_stack* stk, value block)
 {
@@ -841,6 +846,7 @@ void caml_shrink_mark_stack (void)
 
 void caml_darken_cont(value cont);
 
+CAMLno_tsan /* Disable TSan reports from this function (see #11040) */
 static void mark_slice_darken(struct mark_stack* stk, value child,
                               intnat* work)
 {
@@ -885,6 +891,7 @@ static void mark_slice_darken(struct mark_stack* stk, value child,
   }
 }
 
+CAMLno_tsan /* Disable TSan reports from this function (see #11040) */
 Caml_noinline static intnat do_some_marking(struct mark_stack* stk,
                                             intnat budget) {
   prefetch_buffer_t pb = { .enqueued = 0, .dequeued = 0,

runtime/minor_gc.c
@@ -187,6 +187,7 @@ header_t caml_get_header_val(value v) {
 }
 
 
+CAMLno_tsan /* Disable TSan reports from this function (see #11040) */
 static int try_update_object_header(value v, volatile value *p, value result,
                                     mlsize_t infix_offset) {
   int success = 0;
@@ -234,6 +235,7 @@ static int try_update_object_header(value v, volatile value *p, value result,
 static scanning_action_flags oldify_scanning_flags =
   SCANNING_ONLY_YOUNG_VALUES;
 
+CAMLno_tsan /* Disable TSan reports from this function (see #11040) */
 /* Note that the tests on the tag depend on the fact that Infix_tag,
    Forward_tag, and No_scan_tag are contiguous. */
 static void oldify_one (void* st_v, value v, volatile value *p)

runtime/shared_heap.c
@@ -264,6 +264,7 @@ static intnat pool_sweep(struct caml_heap_state* local,
 
 /* Adopt pool from the pool_freelist avail and full pools
    to satisfy an allocation */
+CAMLno_tsan /* Disable TSan reports from this function (see #11040) */
 static pool* pool_global_adopt(struct caml_heap_state* local, sizeclass sz)
 {
   pool* r = NULL;
@@ -513,6 +514,7 @@ static intnat pool_sweep(struct caml_heap_state* local, pool** plist,
   return work;
 }
 
+CAMLno_tsan /* Disable TSan reports from this function (see #11040) */
 static intnat large_alloc_sweep(struct caml_heap_state* local) {
   value* p;
   header_t hd;