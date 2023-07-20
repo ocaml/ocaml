8cca9abe5a4ac42da1bbd2e2ffc5cee2c97f54a4
Guillaume Munch-Maccagnoni <Guillaume.Munch-Maccagnoni@inria.fr>
2023-07-20 22:41:34 +0200

    [minor] Clean-up header inclusion & use caml_check_error in domain.c

--- runtime/caml/fail.h
+++ runtime/caml/fail.h
@@ -73,6 +73,9 @@ struct caml_exception_context {
 
 int caml_is_special_exception(value exn);
 
+/* from runtime/sync.c */
+CAMLextern void caml_check_error(int err, char const * msg);
+
 #endif /* CAML_INTERNALS */
 
 #ifdef __cplusplus
