f050568f352dae4987180d3f0db2e4ef26c2d423
Glen Mével <glen.mevel@crans.org>
2023-08-09 21:54:32 +0200

    Random/test: use default state in compat_32_64.ml

--- testsuite/tests/lib-random/compat_32_64.ml
+++ testsuite/tests/lib-random/compat_32_64.ml
@@ -10,27 +10,23 @@ let max_int31 = 0x3FFF_FFFF (* = 2^30-1 *)
 
 let _ =
 
-  let a = Array1.of_array Int64 C_layout [| 1L; 2L; 3L; 4L |] in
-  (* Violate abstraction of type Random.State.t to manipulate state directly *)
-  let r = (Obj.magic a : Random.State.t) in
-
   (* [full_int], range that fits in 30 bits: *)
   for i = 0 to 49 do
-    Printf.printf "%i\n" (Random.State.full_int r max_int31)
+    Printf.printf "%i\n" (Random.full_int max_int31)
   done;
   print_newline ();
 
   (* [int_in_range], all-negative range whose length fits in 30 bits: *)
   for i = 0 to 49 do
     Printf.printf "%i\n"
-      (Random.State.int_in_range r ~min:min_int31 ~max:(min_int31 + 996))
+      (Random.int_in_range ~min:min_int31 ~max:(min_int31 + 996))
   done;
   print_newline ();
 
   (* [int_in_range], full 31-bit range: *)
   for i = 0 to 49 do
     Printf.printf "%i\n"
-      (Random.State.int_in_range r ~min:min_int31 ~max:max_int31)
+      (Random.int_in_range ~min:min_int31 ~max:max_int31)
   done
 
 let _ = exit 0
