2e4c472ff3d796a566c86b245bcb1f0cc1b4725b
Kakadu <Kakadu@pm.me>
2023-08-20 13:50:42 +0300

    Pprintast.ml: Fix types' attributes and classes' parameters
    Signed-off-by: Dmitrii Kosarev <Dmitrii.Kosarev@pm.me>

--- parsing/pprintast.ml
+++ parsing/pprintast.ml
@@ -1102,7 +1102,7 @@ and class_expr ctxt f x =
         pp f "%a%a"
           (fun f l-> if l <>[] then
               pp f "[%a]@ "
-                (list (core_type ctxt) ~sep:",") l) l
+                (list (core_type ctxt) ~sep:",@ ") l) l
           longident_loc li
     | Pcl_constraint (ce, ct) ->
         pp f "(%a@ :@ %a)"
@@ -1535,7 +1535,7 @@ and type_def_list ctxt f (rf, exported, l) =
       else if exported then " ="
       else " :="
     in
-    pp f "@[<2>%s %a%a%a%s%a@]%a" kwd
+    pp f "@[<hov>@[<2>%s %a%a%a%s%a@]@ %a@]" kwd
       nonrec_flag rf
       (type_params ctxt) x.ptype_params
       ident_of_name x.ptype_name.txt
