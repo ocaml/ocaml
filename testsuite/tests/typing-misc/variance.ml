d58c7b899c7f2ae5d139bb912d36af09f36979b1
Florian Angeletti <florian.angeletti@inria.fr>
2023-10-03 11:26:06 +0200

    Fix variance composition
    Possible positive occurence under possible negative occurence were
    forgotten, making the typechecker accepts the absurd statement
    
        type -'a n
        type +'a p
        type +'a ko = 'a p n

--- testsuite/tests/typing-misc/variance.ml
+++ testsuite/tests/typing-misc/variance.ml
@@ -36,3 +36,90 @@ type !'a t = (module s with type t = 'a);;
 module type s = sig type t end
 type 'a t = (module s with type t = 'a)
 |}]
+
+(* Composition *)
+type -'a n
+type +'a p
+type !'a i
+
+type +'a error_np = 'a n p;;
+[%%expect{|
+type -'a n
+type +'a p
+type !'a i
+Line 5, characters 0-26:
+5 | type +'a error_np = 'a n p;;
+    ^^^^^^^^^^^^^^^^^^^^^^^^^^
+Error: In this definition, expected parameter variances are not satisfied.
+       The 1st type parameter was expected to be covariant,
+       but it is contravariant.
+|}]
+
+
+type +'a error_pn = 'a p n;;
+[%%expect{|
+Line 1, characters 0-26:
+1 | type +'a error_pn = 'a p n;;
+    ^^^^^^^^^^^^^^^^^^^^^^^^^^
+Error: In this definition, expected parameter variances are not satisfied.
+       The 1st type parameter was expected to be covariant,
+       but it is contravariant.
+|}]
+
+type -'a error_pp = 'a p p;;
+[%%expect{|
+Line 1, characters 0-26:
+1 | type -'a error_pp = 'a p p;;
+    ^^^^^^^^^^^^^^^^^^^^^^^^^^
+Error: In this definition, expected parameter variances are not satisfied.
+       The 1st type parameter was expected to be contravariant,
+       but it is covariant.
+|}]
+
+type -'a error_nn = 'a n n;;
+[%%expect{|
+Line 1, characters 0-26:
+1 | type -'a error_nn = 'a n n;;
+    ^^^^^^^^^^^^^^^^^^^^^^^^^^
+Error: In this definition, expected parameter variances are not satisfied.
+       The 1st type parameter was expected to be contravariant,
+       but it is covariant.
+|}]
+
+type !'a inj_in = 'a i n
+[%%expect{|
+Line 1, characters 0-24:
+1 | type !'a inj_in = 'a i n
+    ^^^^^^^^^^^^^^^^^^^^^^^^
+Error: In this definition, expected parameter variances are not satisfied.
+       The 1st type parameter was expected to be injective invariant,
+       but it is invariant.
+|}]
+
+type !'a inj_in = 'a n i
+[%%expect{|
+Line 1, characters 0-24:
+1 | type !'a inj_in = 'a n i
+    ^^^^^^^^^^^^^^^^^^^^^^^^
+Error: In this definition, expected parameter variances are not satisfied.
+       The 1st type parameter was expected to be injective invariant,
+       but it is invariant.
+|}]
+
+module Make_covariant(M: sig type 'a t end): sig
+  type 'a i = 'a
+  type +'a t = 'a i M.t
+end = struct
+  type 'a i = 'a
+  type +'a t = 'a i M.t
+end
+
+module Positive_ref = Make_covariant(struct type 'a t = 'a ref end)
+[%%expect {|
+Line 6, characters 2-23:
+6 |   type +'a t = 'a i M.t
+      ^^^^^^^^^^^^^^^^^^^^^
+Error: In this definition, expected parameter variances are not satisfied.
+       The 1st type parameter was expected to be covariant,
+       but it is invariant.
+|}]
