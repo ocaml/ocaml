aae3ad5757c4661d806f0a7d5a8350039216f485
Xavier Leroy <xavier.leroy@college-de-france.fr>
2023-10-09 15:51:27 +0200

    Move the functions that use ZSTD compression to a separate zstd.c file
    These functions used to be in extern.c and intern.c.
    Now, they are in zstd.c and connect themselves to extern.c and intern.c
    via hooks activated by a new primitive `caml_zstd_initialize`.

--- runtime/gen_primitives.sh
+++ runtime/gen_primitives.sh
@@ -26,7 +26,7 @@ export LC_ALL=C
       lexing md5 meta memprof obj parsing signals str sys callback weak \
       finalise domain platform fiber memory startup_aux runtime_events sync \
       dynlink backtrace_byt backtrace afl \
-      bigarray prng
+      bigarray prng zstd
   do
       sed -n -e 's/^CAMLprim value \([a-z0-9_][a-z0-9_]*\).*/\1/p' \
         "runtime/$prim.c"
