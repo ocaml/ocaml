dee097259d60cb75b5f16a80ceafd5fa547edf35
Olivier Nicole <olivier@chnik.fr>
2023-10-20 14:04:56 +0200

    Remove benign data race in parallel test (#12680)
    The data race shows up in TSan-enabled runs, and can be removed for a
    small cost.

--- testsuite/tests/parallel/domain_parallel_spawn_burn.ml
+++ testsuite/tests/parallel/domain_parallel_spawn_burn.ml
@@ -29,9 +29,9 @@ let test_parallel_spawn () =
   done
 
 let () =
-  let running = ref true in
+  let running = Atomic.make true in
   let rec run_until_stop fn () =
-    while !running do
+    while Atomic.get running do
       fn ();
     done
   in
@@ -41,7 +41,7 @@ let () =
 
   test_parallel_spawn ();
 
-  running := false;
+  Atomic.set running false;
   join domain_minor_gc;
   join domain_major_gc;
 
