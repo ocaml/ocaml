414199f96044a099569776db9412af7bf76f0f50
David Allsopp <david.allsopp@metastack.com>
2023-10-22 11:04:53 +0200

    Remove unnecessary libunix
    This appears to have been a copy+paste issue in the ocaml-multicore
    history. These tests don't use the Unix library, which disables them for
    the minimal build but, more importantly, also on Windows.

--- testsuite/tests/callback/nested_fiber.ml
+++ testsuite/tests/callback/nested_fiber.ml
@@ -1,12 +1,5 @@
 (* TEST
- include unix;
  modules = "nested_fiber_.c";
- libunix;
- {
-   bytecode;
- }{
-   native;
- }
 *)
 
 external caml_to_c : (unit -> 'a) -> 'a = "caml_to_c"
--- testsuite/tests/callback/test3.ml
+++ testsuite/tests/callback/test3.ml
@@ -1,8 +1,6 @@
 (* TEST
- include unix;
  modules = "test3_.c";
  no-tsan; (* TSan does not support call stacks bigger than 64k frames *)
- libunix;
  {
    bytecode;
  }{
