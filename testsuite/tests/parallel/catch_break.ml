f7d02698f7cfb27ad10116ff2ef6d3dd6a42e6e1
Guillaume Munch-Maccagnoni <Guillaume.Munch-Maccagnoni@Inria.fr>
2023-10-23 16:32:22 +0100

    Make tests/parallel/catch_break.ml more robust (#12618)

--- testsuite/tests/parallel/catch_break.ml
+++ testsuite/tests/parallel/catch_break.ml
@@ -10,6 +10,27 @@ no-tsan;
 }
 *)
 
+(* PR #11307. The following program deadlocks when input in the
+   toplevel and interrupted by the user with Ctrl-C, by busy-waiting
+   on signals to be processed.
+
+   {[
+let break_trap s =
+  (try while true do () done
+   with Sys.Break -> print_endline "[Sys.Break caught]" ) ;
+  print_endline s
+
+let () =
+  Sys.catch_break true ;
+  let d = Domain.spawn (fun () -> break_trap "Domain 1") in
+  break_trap "Domain 0 - 1" ;
+  Domain.join d ;
+  break_trap  "Domain 0 - 2";
+  print_endline "Success."
+   ]}
+
+*)
+
 let verbose = false
 
 (* Expected when verbose (depending on scheduling and platform):
@@ -24,43 +45,70 @@ Success.
 
 *)
 
+let delay = 0.001 (* 1 ms *)
+let fuel = Atomic.make 1000 (* = 1s max retry duration *)
+
 let print = if verbose then print_endline else fun _ -> ()
 
+(* start sending interrupts when reaches 1 or 2 *)
+let ready_count = Atomic.make 0
+
+(* Does not poll *)
+
+let sleep () =
+  if Atomic.get fuel <= 0 then (
+    print "[Reached max attempts without succeeding]";
+    Unix._exit 1
+  );
+  Atomic.decr fuel;
+  Unix.sleepf delay
+
+let rec wait n =
+  if Atomic.get ready_count <> n then (
+    sleep ();
+    wait n
+  )
+
+(* We busy-wait because other synchronisation mechanisms involve
+   blocking calls, which may exercise other parts of the async
+   callback implementation than we want.*)
 let break_trap s =
   begin
-    try while true do () done
-    with Sys.Break -> print "[Sys.Break caught]";
+    try Atomic.incr ready_count; while true do () done
+    with Sys.Break -> print "[Sys.Break caught]"
   end;
-  print s
+  print s;
+  Atomic.decr ready_count
+
+(* Simulate repeated Ctrl-C from a parallel thread *)
+let interruptor_domain () =
+  Domain.spawn @@ fun () ->
+  ignore (Thread.sigmask Unix.SIG_BLOCK [Sys.sigint]);
+  let kill () = sleep () ; Unix.kill (Unix.getpid ()) Sys.sigint in
+  wait 2;
+  kill (); (* interrupt Domain 1 or Domain 0-1 *)
+  wait 1;
+  kill (); (* interrupt the other one of Domain 1 or Domain 0-1 *)
+  wait 2;
+  kill ()  (* interrupt Domain 0-2 *)
 
 let run () =
-  (* Goal: joining the domain [d] must be achievable by Ctrl-C *)
-  let d = Domain.spawn (fun () -> break_trap "Domain 1")
-  in
-  let finished = Atomic.make false in
-  (* Simulate repeated Ctrl-C *)
-  let d2 = Domain.spawn (fun () ->
-    ignore (Thread.sigmask Unix.SIG_BLOCK [Sys.sigint]);
-    let pid = Unix.getpid () in
-    let rec kill n =
-      if n = 0 then (
-        print "[Kill thread reached max attempts without succeeding]";
-        Unix._exit 1
-      );
-      Unix.sleepf 0.05;
-      Unix.kill pid Sys.sigint;
-      if not (Atomic.get finished) then kill (n - 1)
-    in
-    kill 10)
-  in
+  (* We simulate the user pressing Ctrl-C repeatedly. Goal: joining
+     the domain [d] must be achievable by Ctrl-C. This tests proper
+     reception of SIGINT. *)
+  let d = Domain.spawn (fun () -> break_trap "Domain 1") in
+  let d2 = interruptor_domain () in
   break_trap "Domain 0 - 1";
   Domain.join d;
+  assert (Atomic.get ready_count = 0);
+  Atomic.incr ready_count; (* Make sure it reaches 2 *)
   break_trap "Domain 0 - 2";
-  Atomic.set finished true;
   Domain.join d2
 
 let () =
   Sys.catch_break true;
-  (try run () with Sys.Break -> ());
-  (try print "Success." with Sys.Break -> ());
+  (try run () with Sys.Break ->
+     print ("Test could not complete due to scheduling hazard"
+            ^ " (possible false positive)."));
+  print "Success.";
   exit 0
