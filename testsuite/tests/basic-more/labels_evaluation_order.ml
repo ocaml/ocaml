8cd682a9e4af67bee144d0c0ea46ccee7ac88b2d
Jacques Garrigue <garrigue@math.nagoya-u.ac.jp>
2023-11-16 18:52:15 +0900

    add more examples

--- testsuite/tests/basic-more/labels_evaluation_order.ml
+++ testsuite/tests/basic-more/labels_evaluation_order.ml
@@ -15,7 +15,7 @@ let _ = print_endline "f defined"
 
 let _ = f ~b:(print_endline "b argument")
 
-
+(* Example with many arguments *)
 let many ?(arg1=print_endline"arg1") ?(arg2=print_endline"arg2")
   ?(arg3=print_endline"arg3") ?(arg4=print_endline"arg4")
   ?(arg5=print_endline"arg5") ?(arg6=print_endline"arg6")
@@ -31,3 +31,32 @@ let many ?(arg1=print_endline"arg1") ?(arg2=print_endline"arg2")
 let f = many ~arg10:() ~arg20:()
 let g = f ~arg19:()
 let () = g ()
+
+(* Example of delayed side-effect after labelled non-optional argument *)
+[@@@warning "-unerasable-optional-argument"]
+let foo2 = prerr_endline "foo2";
+  fun ?(a=()) -> print_endline "a";
+  fun ~b:() -> print_endline "b";
+  fun ?(c=()) ?(d=()) () -> print_endline "all"
+
+let f = print_endline "f"; foo2 ~b:()
+let g = print_endline "g"; f ~d:()
+let h = print_endline "h"; g ~a:()
+let i = print_endline "i"; h ()
+
+(* Cannot happen if there is an unlabelled argument before the remaining
+   optional ones *)
+let foo3 = prerr_endline "foo3";
+  fun ?(a=()) -> print_endline "a";
+  fun ~b:() -> print_endline "b";
+  fun () ?(c=()) ?(d=()) () -> print_endline "all"
+
+let f = print_endline "f"; foo3 ~b:()
+let g = print_endline "g"; f ~d:()
+let h = print_endline "h"; g ()
+let i = print_endline "i"; h ()
+
+let e = print_endline "e"; foo3 ()
+let f = print_endline "f"; e ~b:()
+let g = print_endline "g"; f ~d:()
+let h = print_endline "h"; g ()
