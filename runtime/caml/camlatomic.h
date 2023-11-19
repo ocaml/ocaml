9219b4f572db30ac8cc7d89e3e567ef6e9231411
Frédéric Recoules <frederic.recoules@cea.fr>
2023-11-19 15:49:42 +0100

    Add some atomic primitives (pointer type, sub)

--- runtime/caml/camlatomic.h
+++ runtime/caml/camlatomic.h
@@ -29,8 +29,10 @@
 extern "C++" {
 #include <atomic>
 #define ATOMIC_UINTNAT_INIT(x) (x)
+#define ATOMIC_PTR_INIT(p) (p)
 typedef std::atomic<uintnat> atomic_uintnat;
 typedef std::atomic<intnat> atomic_intnat;
+#define ATOMIC_PTR_TYPEDEF(t, n) typedef std::atomic<t*> n;
 using std::memory_order_relaxed;
 using std::memory_order_acquire;
 using std::memory_order_release;
@@ -42,8 +44,10 @@ using std::memory_order_seq_cst;
 
 #include <stdatomic.h>
 #define ATOMIC_UINTNAT_INIT(x) (x)
+#define ATOMIC_PTR_INIT(p) (p)
 typedef _Atomic uintnat atomic_uintnat;
 typedef _Atomic intnat atomic_intnat;
+#define ATOMIC_PTR_TYPEDEF(t, n) typedef t * _Atomic n;
 
 #elif defined(__GNUC__)
 
@@ -58,8 +62,10 @@ typedef enum memory_order {
 } memory_order;
 
 #define ATOMIC_UINTNAT_INIT(x) { (x) }
+#define ATOMIC_PTR_INIT(p) { (p) }
 typedef struct { uintnat repr; } atomic_uintnat;
 typedef struct { intnat repr; } atomic_intnat;
+#define ATOMIC_PTR_TYPEDEF(t, n) typedef struct { t *repr; } n;
 
 #define atomic_load_explicit(x, m) __atomic_load_n(&(x)->repr, (m))
 #define atomic_load(x) atomic_load_explicit((x), memory_order_seq_cst)
@@ -74,6 +80,8 @@ typedef struct { intnat repr; } atomic_intnat;
   __atomic_exchange_n(&(x)->repr, (newv), memory_order_seq_cst)
 #define atomic_fetch_add(x, n) \
   __atomic_fetch_add(&(x)->repr, (n), memory_order_seq_cst)
+#define atomic_fetch_sub(x, n) \
+  __atomic_fetch_sub(&(x)->repr, (n), memory_order_seq_cst)
 #define atomic_fetch_or(x, n) \
   __atomic_fetch_or(&(x)->repr, (n), memory_order_seq_cst)
 #define atomic_thread_fence __atomic_thread_fence
