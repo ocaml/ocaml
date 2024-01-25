3a7d227c14bf53a9fe25935620a3006763edd9b2
Nick Roberts <nroberts@janestreet.com>
2024-01-25 15:58:45 -0500

    Add regression test

---
+++ testsuite/tests/typing-modules/pr12936.ml
@@ -0,0 +1,41 @@
+(* TEST
+ expect;
+*)
+
+(* This test ensures that the error messages for missing bindings
+   are printed in a logical order, with "In module N:" mentioned
+   in sensible places.
+ *)
+
+module M : sig
+  module N : sig
+    type t
+    val x : t
+    val y : t
+  end
+end = struct
+  module N = struct
+  end
+end
+
+[%%expect {|
+Lines 7-10, characters 6-3:
+ 7 | ......struct
+ 8 |   module N = struct
+ 9 |   end
+10 | end
+Error: Signature mismatch:
+       Modules do not match:
+         sig module N : sig end end
+       is not included in
+         sig module N : sig type t val x : t val y : t end end
+       In module "N":
+       Modules do not match:
+         sig end
+       is not included in
+         sig type t val x : t val y : t end
+       The type "t" is required but not provided
+       The value "x" is required but not provided
+       In module "N":
+       The value "y" is required but not provided
+|}];;
