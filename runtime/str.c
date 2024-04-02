1c36d854571a31ebca21e30add0cd4f5fb558bbf
Arthur Carcano <arthur.carcano@ocamlpro.com>
2024-04-02 16:39:53 +0200

    Make memcmp delegation to libc conditional

--- runtime/str.c
+++ runtime/str.c
@@ -273,16 +273,40 @@ CAMLno_tsan_for_perf /* This attribute needs to stay on its own line for this
                         system. */
 CAMLprim value caml_string_equal(value s1, value s2)
 {
-  mlsize_t sz1, sz2;
-  int res;
+  // musl's memcmp is not optimized and works on bytes
+  // so use our own word-sized implementation.
+  // To check if musl updated their implementation:
+  // http://git.musl-libc.org/cgit/musl/tree/src/string/memcmp.c
+  #ifdef LIBC_IS_MUSL
+      mlsize_t sz1, sz2;
+      value * p1, * p2;
 
-  if (s1 == s2) return Val_true;
-  sz1 = Bosize_val(s1);
-  sz2 = Bosize_val(s2);
-  if (sz1 != sz2) return Val_false;
-  res = memcmp(Op_val(s1), Op_val(s2), sz1);
-  if (res == 0) return Val_true;
-  return Val_false;
+      if (s1 == s2) return Val_true;
+      sz1 = Wosize_val(s1);
+      sz2 = Wosize_val(s2);
+      if (sz1 != sz2) return Val_false;
+      for(p1 = Op_val(s1), p2 = Op_val(s2); sz1 > 0; sz1--, p1++, p2++)
+        if (*p1 != *p2) return Val_false;
+      return Val_true;
+  #else
+      mlsize_t sz1, sz2;
+      int res;
+
+      if (s1 == s2) return Val_true;
+      sz1 = Bosize_val(s1);
+      sz2 = Bosize_val(s2);
+      if (sz1 != sz2) return Val_false;
+      // __memcmpeq has the same semantics as memcmp
+      // when only its return value truthfullness is
+      // considered, but is more efficient.
+      #ifdef HAS_MEMCMPEQ
+        res = __memcmpeq(Op_val(s1), Op_val(s2), sz1);
+      #else
+        res = memcmp(Op_val(s1), Op_val(s2), sz1);
+      #endif
+      if (res == 0) return Val_true;
+      return Val_false;
+  #endif
 }
 
 CAMLprim value caml_bytes_equal(value s1, value s2)
