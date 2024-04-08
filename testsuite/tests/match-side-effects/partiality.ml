209de5f8dc4480a990d2644792b1dcacec6cd822
Gabriel Scherer <gabriel.scherer@gmail.com>
2024-04-08 23:05:17 +0200

    matching: refine the test match-side-effects/partiality.ml

--- testsuite/tests/match-side-effects/partiality.ml
+++ testsuite/tests/match-side-effects/partiality.ml
@@ -44,24 +44,45 @@ val f : t -> int = <fun>
    inside a mutable position. *)
 type t = {a: bool; mutable b: int option}
 
-let f x =
+let simple x =
   match x with
-  | {a = false; b = _} -> 0
-  | {a = _;     b = None} -> 1
-  | {a = true;  b = Some y} -> y
+  | {b = None} -> 1
+  | {b = Some y} -> y
 ;;
 (* Performance expectation: there should not be a Match_failure case. *)
 [%%expect {|
 0
 type t = { a : bool; mutable b : int option; }
 (let
-  (f/291 =
-     (function x/292 : int
-       (if (field_int 0 x/292)
-         (let (*match*/296 =o (field_mut 1 x/292))
-           (if *match*/296 (field_imm 0 *match*/296) 1))
+  (simple/291 =
+     (function x/293 : int
+       (let (*match*/296 =o (field_mut 1 x/293))
+         (if *match*/296 (field_imm 0 *match*/296) 1))))
+  (apply (field_mut 1 (global Toploop!)) "simple" simple/291))
+val simple : t -> int = <fun>
+|}]
+
+(* This more complex case has the switch on [b] split across two cases
+   on [a], so it may need a [Match_failure] for soundness -- it does
+   if the two accesses to [b] are done on different reads of the same
+   mutable field.
+
+   PASS: a single read of [field_mut 1 x], no Match_failure case. *)
+let f x =
+  match x with
+  | {a = false; b = _} -> 0
+  | {a = _;     b = None} -> 1
+  | {a = true;  b = Some y} -> y
+;;
+[%%expect {|
+(let
+  (f/297 =
+     (function x/298 : int
+       (if (field_int 0 x/298)
+         (let (*match*/302 =o (field_mut 1 x/298))
+           (if *match*/302 (field_imm 0 *match*/302) 1))
          0)))
-  (apply (field_mut 1 (global Toploop!)) "f" f/291))
+  (apply (field_mut 1 (global Toploop!)) "f" f/297))
 val f : t -> int = <fun>
 |}]
 
@@ -76,7 +97,7 @@ let f r =
   | None -> 3
 ;;
 (* Correctness condition: there should either be a single
-   (field_mut 1) access, or the second access should include
+   (field_mut 0) access, or the second access should include
    a Match_failure case.
 
    FAIL: the second occurrence of (field_mut 0) is used with a direct
@@ -84,21 +105,21 @@ let f r =
    unsound here. *)
 [%%expect {|
 (let
-  (f/298 =
-     (function r/299 : int
-       (let (*match*/301 = (makeblock 0 r/299))
+  (f/304 =
+     (function r/305 : int
+       (let (*match*/307 = (makeblock 0 r/305))
          (catch
-           (if *match*/301
-             (let (*match*/303 =o (field_mut 0 (field_imm 0 *match*/301)))
-               (if *match*/303 (exit 11) 0))
-             (exit 11))
-          with (11)
-           (if (seq (setfield_ptr 0 r/299 0) 0) 1
-             (if *match*/301
-               (let (*match*/305 =o (field_mut 0 (field_imm 0 *match*/301)))
-                 (field_imm 0 *match*/305))
+           (if *match*/307
+             (let (*match*/309 =o (field_mut 0 (field_imm 0 *match*/307)))
+               (if *match*/309 (exit 13) 0))
+             (exit 13))
+          with (13)
+           (if (seq (setfield_ptr 0 r/305 0) 0) 1
+             (if *match*/307
+               (let (*match*/311 =o (field_mut 0 (field_imm 0 *match*/307)))
+                 (field_imm 0 *match*/311))
                3))))))
-  (apply (field_mut 1 (global Toploop!)) "f" f/298))
+  (apply (field_mut 1 (global Toploop!)) "f" f/304))
 val f : int option ref -> int = <fun>
 |}]
 
@@ -118,10 +139,10 @@ let test = function
 0
 type _ t = Int : int -> int t | Bool : bool -> bool t
 (let
-  (test/309 =
-     (function param/312 : int
-       (if param/312 (field_imm 0 (field_imm 0 param/312)) 0)))
-  (apply (field_mut 1 (global Toploop!)) "test" test/309))
+  (test/315 =
+     (function param/318 : int
+       (if param/318 (field_imm 0 (field_imm 0 param/318)) 0)))
+  (apply (field_mut 1 (global Toploop!)) "test" test/315))
 val test : int t option -> int = <fun>
 |}]
 
@@ -139,11 +160,11 @@ let test = function
 0
 type _ t = Int : int -> int t | Bool : bool -> bool t
 (let
-  (test/317 =
-     (function param/319 : int
-       (let (*match*/320 =o (field_mut 0 param/319))
-         (if *match*/320 (field_imm 0 (field_imm 0 *match*/320)) 0))))
-  (apply (field_mut 1 (global Toploop!)) "test" test/317))
+  (test/323 =
+     (function param/325 : int
+       (let (*match*/326 =o (field_mut 0 param/325))
+         (if *match*/326 (field_imm 0 (field_imm 0 *match*/326)) 0))))
+  (apply (field_mut 1 (global Toploop!)) "test" test/323))
 val test : int t option ref -> int = <fun>
 |}]
 
@@ -164,18 +185,18 @@ let test n =
 0
 type _ t = Int : int -> int t | Bool : bool -> bool t
 (let
-  (test/325 =
-     (function n/326 : int
+  (test/331 =
+     (function n/332 : int
        (let
-         (*match*/329 =
+         (*match*/335 =
             (makeblock 0 (makeblock 0 (makemutable 0 (int) 1) [0: 42])))
-         (if *match*/329
+         (if *match*/335
            (let
-             (*match*/330 =a (field_imm 0 *match*/329)
-              *match*/332 =o (field_mut 0 (field_imm 0 *match*/330)))
-             (if *match*/332 (field_imm 0 (field_imm 1 *match*/330))
-               (~ (field_imm 0 (field_imm 1 *match*/330)))))
+             (*match*/336 =a (field_imm 0 *match*/335)
+              *match*/338 =o (field_mut 0 (field_imm 0 *match*/336)))
+             (if *match*/338 (field_imm 0 (field_imm 1 *match*/336))
+               (~ (field_imm 0 (field_imm 1 *match*/336)))))
            3))))
-  (apply (field_mut 1 (global Toploop!)) "test" test/325))
+  (apply (field_mut 1 (global Toploop!)) "test" test/331))
 val test : 'a -> int = <fun>
 |}]
