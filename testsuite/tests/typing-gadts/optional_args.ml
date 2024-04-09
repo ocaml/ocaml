35a8ccd0b0fcc86615aa1ce8eada9152ab37c54e
Richard Eisenberg <reisenberg@janestreet.com>
2024-04-09 16:56:40 -0400

    Add failing test

---
+++ testsuite/tests/typing-gadts/optional_args.ml
@@ -0,0 +1,66 @@
+(* TEST
+ expect;
+*)
+
+type (_, _) refl = Refl : ('a, 'a) refl
+
+[%%expect{|
+type (_, _) refl = Refl : ('a, 'a) refl
+|}]
+
+let apply (_ : unit -> 'a) : 'a = assert false
+let go (type a) (Refl : (unit, a) refl) = apply (fun () : a -> ())
+
+[%%expect{|
+val apply : (unit -> 'a) -> 'a = <fun>
+val go : (unit, 'a) refl -> 'a = <fun>
+|}]
+
+let apply (_ : x:unit -> unit -> 'a) : 'a = assert false
+let go (type a) (Refl : (unit, a) refl) = apply (fun ~x:_ () : a -> ())
+
+[%%expect{|
+val apply : (x:unit -> unit -> 'a) -> 'a = <fun>
+val go : (unit, 'a) refl -> 'a = <fun>
+|}]
+
+let apply (_ : ?x:unit -> unit -> 'a) : 'a = assert false
+let go (type a) (Refl : (unit, a) refl) = apply (fun ?x:_ () : a -> ())
+
+[%%expect{|
+val apply : (?x:unit -> unit -> 'a) -> 'a = <fun>
+Line 2, characters 42-71:
+2 | let go (type a) (Refl : (unit, a) refl) = apply (fun ?x:_ () : a -> ())
+                                              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+Error: This expression has type "a" = "unit"
+       but an expression was expected of type "'a"
+       This instance of "unit" is ambiguous:
+       it would escape the scope of its equation
+|}]
+
+let apply (_ : unit -> x:unit -> 'a) : 'a = assert false
+let go (type a) (Refl : (unit, a) refl) = apply (fun () ~x:_ : a -> ())
+
+[%%expect{|
+val apply : (unit -> x:unit -> 'a) -> 'a = <fun>
+val go : (unit, 'a) refl -> 'a = <fun>
+|}]
+
+let apply (_ : unit -> ?x:unit -> 'a) : 'a = assert false
+let go (type a) (Refl : (unit, a) refl) = apply (fun () ?x:_ : a -> ())
+
+[%%expect{|
+val apply : (unit -> ?x:unit -> 'a) -> 'a = <fun>
+Line 2, characters 59-60:
+2 | let go (type a) (Refl : (unit, a) refl) = apply (fun () ?x:_ : a -> ())
+                                                               ^
+Warning 16 [unerasable-optional-argument]: this optional argument cannot be erased.
+
+Line 2, characters 42-71:
+2 | let go (type a) (Refl : (unit, a) refl) = apply (fun () ?x:_ : a -> ())
+                                              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+Error: This expression has type "a" = "unit"
+       but an expression was expected of type "'a"
+       This instance of "unit" is ambiguous:
+       it would escape the scope of its equation
+|}]
