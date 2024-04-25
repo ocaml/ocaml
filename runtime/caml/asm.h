1b8edc582285da023144d7a004383a01c0d18361
Miod Vallat <miod@tarides.com>
2024-04-25 15:04:18 +0000

    Copyright update.

--- runtime/caml/asm.h
+++ runtime/caml/asm.h
@@ -2,7 +2,12 @@
 /*                                                                        */
 /*                                 OCaml                                  */
 /*                                                                        */
-/*  TODO: proper attribution and copyright notice here                    */
+/*             Xavier Leroy, projet Gallium, INRIA Rocquencourt           */
+/*             Bart Jacobs, KU Leuven                                     */
+/*             Tom Kelly, OCaml Labs Consultancy, UK                      */
+/*                                                                        */
+/*   Copyright 2012 Institut National de Recherche en Informatique et     */
+/*     en Automatique.                                                    */
 /*                                                                        */
 /*   All rights reserved.  This file is distributed under the terms of    */
 /*   the GNU Lesser General Public License version 2.1, with the          */
