a656315b12d1cd666281fe4960c3e417f70af6bf
Damien Doligez <damien.doligez@inria.fr>
2024-04-26 11:16:23 +0200

    Fix some parallel tests that assume fairness.

--- testsuite/tests/parallel/domain_parallel_spawn_burn_gc_set.ml
+++ testsuite/tests/parallel/domain_parallel_spawn_burn_gc_set.ml
@@ -1,4 +1,8 @@
-(* TEST *)
+(* TEST
+  include unix;
+  hasunix;
+  { bytecode; } { native; }
+ *)
 
 open Domain
 
@@ -16,15 +20,10 @@ let rec set_gc l =
 
 let test_size =
   try int_of_string (Sys.getenv "OCAML_TEST_SIZE")
-  with Not_found | Failure _ -> 0
+  with Not_found | Failure _ -> 2
 
-let (list_size, num_domains, niters) =
-  if test_size >= 2 then (14, 8, 20) else (13, 4, 5)
-
-(* Don't run the test if we have only 2 cores available, it times out often. *)
-
-let _  =
-  if test_size <= 1 then begin print_endline "ok"; exit 0 end
+let (list_size, num_domains) =
+  if test_size >= 2 then (14, 8) else (13, 4)
 
 let rec burn l =
   if List.hd l > list_size then ()
@@ -32,10 +31,8 @@ let rec burn l =
     burn (l @ l |> List.map (fun x -> x + 1))
 
 let test_parallel_spawn () =
-  for i = 1 to niters do
     Array.init num_domains (fun _ -> Domain.spawn (fun () -> burn [0]))
     |> Array.iter join
-  done
 
 let () =
   let running = Atomic.make true in
@@ -51,13 +48,15 @@ let () =
     Domain.spawn (run_until_stop (fun () -> burn [8]; Gc.major ())) in
   let domain_set_gc = Domain.spawn (run_until_stop (fun () -> set_gc 1; )) in
   let domain_set_gc2 = Domain.spawn (run_until_stop (fun () -> set_gc 3; )) in
+  let domain_parallel_spawn = Domain.spawn test_parallel_spawn in
 
-  test_parallel_spawn ();
+  Unix.sleep 3;
 
   Atomic.set running false;
   join domain_minor_gc;
   join domain_set_gc;
   join domain_major_gc;
   join domain_set_gc2;
+  join domain_parallel_spawn;
 
   print_endline "ok"
