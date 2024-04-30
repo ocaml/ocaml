c9b39184cbd5d9b7b1ca061e2862274a281389cf
Olivier Nicole <olivier@chnik.fr>
2024-04-30 18:32:27 +0200

    Avoid synchronising by sleep in a systhreads test
    Achieved by using an Event.channel.
    
    Co-authored-by: Olivier Nicole <olivier@chnik.fr>

--- testsuite/tests/lib-threads/sockets.ml
+++ testsuite/tests/lib-threads/sockets.ml
@@ -16,7 +16,6 @@ open Printf
 let serve_connection s =
   let buf = Bytes.make 1024 '>' in
   let n = Unix.read s buf 2 (Bytes.length buf - 2) in
-  Thread.delay 1.0;
   ignore (Unix.write s buf 0 (n + 2));
   Unix.close s
 
@@ -26,14 +25,27 @@ let server sock =
     ignore(Thread.create serve_connection s)
   done
 
-let client (addr, msg) =
+let client1_done = Event.new_channel ()
+
+let wait_for_turn id =
+  if id = 2 then
+    Event.receive client1_done |> Event.sync |> ignore
+
+let signal_turn id =
+  if id = 1 then
+    Event.send client1_done 2 |> Event.sync
+
+let client (id, addr) =
+  let msg = "Client #" ^ Int.to_string id ^ "\n" in
   let sock =
     Unix.socket (Unix.domain_of_sockaddr addr) Unix.SOCK_STREAM 0 in
   Unix.connect sock addr;
   let buf = Bytes.make 1024 ' ' in
-  ignore(Unix.write_substring sock msg 0 (String.length msg));
+  ignore (Unix.write_substring sock msg 0 (String.length msg));
   let n = Unix.read sock buf 0 (Bytes.length buf) in
-  print_bytes (Bytes.sub buf 0 n); flush stdout
+  wait_for_turn id;
+  print_bytes (Bytes.sub buf 0 n); flush stdout;
+  signal_turn id
 
 let _ =
   let addr = Unix.ADDR_INET(Unix.inet_addr_loopback, 0) in
@@ -44,6 +56,6 @@ let _ =
   let addr = Unix.getsockname sock in
   Unix.listen sock 5;
   ignore (Thread.create server sock);
-  ignore (Thread.create client (addr, "Client #1\n"));
-  Thread.delay 0.5;
-  client (addr, "Client #2\n")
+  let c = Thread.create client (2, addr) in
+  client (1, addr);
+  Thread.join c
