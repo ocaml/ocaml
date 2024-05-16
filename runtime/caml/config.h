2d99baf75a0655b4c7b831a6a300b497932ba473
Antonin Décimo <antonin@tarides.com>
2024-05-16 08:27:08 +0200

    Include Autoconf related headers first in config.h

--- runtime/caml/config.h
+++ runtime/caml/config.h
@@ -16,6 +16,10 @@
 #ifndef CAML_CONFIG_H
 #define CAML_CONFIG_H
 
+#include "m.h"
+#include "s.h"
+#include "compatibility.h"
+
 /* CAML_NAME_SPACE was introduced in OCaml 3.08 to declare compatibility with
    the newly caml_-prefixed names of C runtime functions and to disable the
    definition of compatibility macros for the un-prefixed names. The
@@ -24,8 +28,6 @@
 #ifndef CAML_NAME_SPACE
 #define CAML_NAME_SPACE
 #endif
-
-#include "m.h"
 
 /* If supported, tell gcc that we can use 32-bit code addresses for
  * threaded code, unless we are compiled for a shared library (-fPIC option) */
@@ -43,9 +45,6 @@
 #else
 #define Caml_inline static inline
 #endif
-
-#include "s.h"
-#include "compatibility.h"
 
 #ifndef CAML_CONFIG_H_NO_TYPEDEFS
 
