a8862dd66dfeb1021075558d560b62ea9a72a359
Boris D. <boris.dob@gmail.com>
2024-05-31 16:43:32 -0700

    Backport 'configure: check whether ar supports response files #12075'

--- utils/ccomp.mli
+++ utils/ccomp.mli
@@ -25,7 +25,7 @@ val run_command: string -> unit
 val compile_file:
   ?output:string -> ?opt:string -> ?stable_name:string -> string -> int
 val create_archive: string -> string list -> int
-val quote_files: string list -> string
+val quote_files: response_files:bool -> string list -> string
 val quote_optfile: string option -> string
 (*val make_link_options: string list -> string*)
 
