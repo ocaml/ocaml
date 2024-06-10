fddcc50d5cf55b979c765a406f445b2cc2d608d9
Antonin Décimo <antonin@tarides.com>
2024-06-10 16:25:45 +0200

    Annotate alloc/free open/close functions with compiler attributes
    This helps the compiler optimize code, and static analysis by
    detecting potential mismatches in alloc/free pairs.
    
    - malloc
    
    > The malloc attribute indicates that the function acts like a system
    > memory allocation function, returning a pointer to allocated storage
    > disjoint from the storage for any other object accessible to the
    > caller.
    
    https://clang.llvm.org/docs/AttributeReference.html#malloc
    
    > Associating a function with a deallocator helps detect calls to
    > mismatched allocation and deallocation functions and diagnose them
    > under the control of options such as -Wmismatched-dealloc. It also
    > makes it possible to diagnose attempts to deallocate objects that
    > were not allocated dynamically, by -Wfree-nonheap-object. To
    > indicate that an allocation function both satisifies the nonaliasing
    > property and has a deallocator associated with it, both the plain
    > form of the attribute and the one with the deallocator argument must
    > be used.
    
    > The warnings guarded by -fanalyzer respect allocation and
    > deallocation pairs marked with the malloc.
    
    https://gcc.gnu.org/onlinedocs/gcc/Common-Function-Attributes.html#index-malloc-function-attribute
    
    Note that the malloc attribute can only be applied to functions
    returning pointers. The OCaml value type is a typedef to an integer
    type, and the C compiler will refuse applying the attribute to a
    function returning an OCaml value.
    
    - nodiscard / warn_unused_result
    
    Prevent memory leaks by warning if the result of an allocation is
    ignored.
    
    https://clang.llvm.org/docs/AttributeReference.html#nodiscard-warn-unused-result
    
    - alloc_align
    
    > GCC uses this information to improve pointer alignment analysis.
    
    https://clang.llvm.org/docs/AttributeReference.html#alloc-align
    https://gcc.gnu.org/onlinedocs/gcc/Common-Function-Attributes.html#index-alloc_005falign-function-attribute
    
    - alloc_size
    
    > GCC uses this information to improve the results of
    > __builtin_object_size.
    
    https://clang.llvm.org/docs/AttributeReference.html#alloc-size
    https://gcc.gnu.org/onlinedocs/gcc/Common-Function-Attributes.html#index-alloc_005fsize-function-attribute
    
    - returns_nonnull
    
    > lets the compiler optimize callers based on the knowledge that the
    > return value will never be null.
    
    > The analyzer considers the possibility that an allocation function
    > could fail and return null. […] If the allocator always returns
    > non-null, use __attribute__ ((returns_nonnull)) to suppress these
    > warnings.
    
    https://gcc.gnu.org/onlinedocs/gcc/Common-Function-Attributes.html#index-returns_005fnonnull-function-attribute

--- runtime/caml/misc.h
+++ runtime/caml/misc.h
@@ -220,6 +220,37 @@ CAMLdeprecated_typedef(addr, char *);
 #endif
 #endif /* CAML_INTERNALS */
 
+#if __has_attribute(malloc) && __has_attribute(warn_unused_result) &&   \
+  __has_attribute(alloc_size) && __has_attribute(alloc_align) &&        \
+  __has_attribute(returns_nonnull)
+#define CAMLreturns_nonnull() __attribute__ ((returns_nonnull))
+#define CAMLrealloc(alloc_size_N,...)                           \
+  __attribute__ ((warn_unused_result,alloc_size(alloc_size_N)))
+#if defined(__GNUC__) && !defined(__llvm__)
+#define CAMLalloc(deallocator,ptr_index,...)                            \
+  __attribute__ ((malloc,malloc(deallocator,ptr_index),warn_unused_result))
+#else
+#define CAMLalloc(deallocator,ptr_index,...)    \
+  __attribute__ ((malloc,warn_unused_result))
+#endif
+#define CAMLmalloc(deallocator,ptr_index,alloc_size_N,...)      \
+  CAMLalloc(deallocator,ptr_index)                              \
+    __attribute__ ((alloc_size(alloc_size_N)))
+#define CAMLcalloc(deallocator,ptr_index,alloc_size_N,alloc_size_M,...) \
+  CAMLalloc(deallocator,ptr_index)                                      \
+    __attribute__ ((alloc_size(alloc_size_N,alloc_size_M)))
+#define CAMLaligned_alloc(deallocator,ptr_index,alloc_size_N,alloc_align_,...) \
+  CAMLmalloc(deallocator,ptr_index,alloc_size_N)                        \
+    __attribute__ ((alloc_align(alloc_align_)))
+#else
+#define CAMLreturns_nonnull()
+#define CAMLrealloc(...)
+#define CAMLalloc(...)
+#define CAMLmalloc(...)
+#define CAMLcalloc(...)
+#define CAMLaligned_alloc(...)
+#endif
+
 /* GC timing hooks. These can be assigned by the user. These hooks
    must not allocate, change any heap value, nor call OCaml code. They
    can obtain the domain id with Caml_state->id. These functions must
