78c8e86d2370354e49bbb99fc482aeef9663be1d
Gabriel Scherer <gabriel.scherer@gmail.com>
2024-07-18 16:36:19 +0200

    testsuite: tests for ocamltest negation

--- testsuite/tests/tool-ocamltest/semantics.ml
+++ testsuite/tests/tool-ocamltest/semantics.ml
@@ -8,4 +8,14 @@
   reference="conditions.reference";
   check-program-output;
 }
+
+{
+  readonly_files="negation.test negation.reference";
+  program="negation.test";
+  output="negation.result";
+  flags="-e";
+  ocamltest;
+  reference="negation.reference";
+  check-program-output;
+}
 *)
