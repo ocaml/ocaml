2e22237f064ae1a01cff4c969e5dad6d94eab033
Gabriel Scherer <gabriel.scherer@gmail.com>
2024-07-18 15:17:47 +0200

    ocamltest: a test case for negation

---
+++ testsuite/tests/tool-ocamltest/negation.ml
@@ -0,0 +1,10 @@
+(* TEST
+readonly_files="negation.test negation.reference";
+program="negation.test";
+output="negation.result";
+flags="-e";
+ocamltest;
+reference="negation.reference";
+check-program-output;
+*)
+
