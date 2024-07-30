c91f5ee1ae67293a1030a9ef01e74db90bc8ad1e
David Allsopp <david.allsopp@metastack.com>
2024-07-30 12:29:25 +0100

    Fix tests/lib-threads/beat.ml on macOS ARM
    Posix specifically notes that nanosleep may delay the calling thread for
    _longer_ than the requested timeout while also stating that it may only
    return early on error or owing to delivery of a signal.
    
    macOS (especially on Apple Silicon) appears to take a very liberal view
    of sub-second nano-sleep calls, such as the the 0.33333 and 0.5 delays
    in the beat.ml test.
    
    The test is tweaked to time each Thread.delay (using Unix.gettimeofday)
    and allow any extra delay to be credited against the next delay. An
    unfair OS scheduler (and a malfunctionaling OCaml runtime) thus will
    both still trigger a failure, but a slightly lazy OS scheduler which
    inflates some of the nanosleep calls by 30% or so will not cause the
    test to fail.

--- testsuite/tests/lib-threads/beat.ml
+++ testsuite/tests/lib-threads/beat.ml
@@ -1,15 +1,10 @@
 (* TEST
+ include systhreads;
+ hassysthreads;
  {
-   include systhreads;
-   hassysthreads;
+   bytecode;
  }{
-   reason = "off-by-one error on MacOS+Clang (https://github.com/ocaml-multicore/ocaml-multicore/issues/408)";
-   skip;
-   {
-     bytecode;
-   }{
-     native;
-   }
+   native;
  }
 *)
 
@@ -18,10 +13,30 @@
 open Printf
 
 let tick (delay, count) =
-  while true do
-    Thread.delay delay;
-    incr count
-  done
+  (* nanosleep is always permitted to suspend execution for _longer_ than the
+     specified timeout. In particular, macOS seems to do this quite readily on
+     Apple silicon (seen very readily in CI). We make some allowance for this by
+     deducting additional sleep time from the next delay.
+     The alternative is to time the C part of Thread.delay _precisely_ (in C),
+     and then test whether both threads spend ~3 seconds in nanosleep, but this
+     is a much more verbose test (and requires a C stub, so it doesn't strictly
+     test Thread.delay at that point). *)
+  let threshold = 1.05 *. delay in
+  let rec loop delta =
+    let start = Unix.gettimeofday () in
+    Thread.delay (delay -. delta);
+    let wallclock_delay = (Unix.gettimeofday () -. start) +. delta in
+    incr count;
+    let delta =
+      if wallclock_delay >= threshold then
+        (* Always call Thread.delay for at least 5% of the specified delay *)
+        min (0.95 *. delay) (wallclock_delay -. delay)
+      else
+        delta
+    in
+    loop delta
+  in
+  loop 0.0
 
 let _ =
   let c1 = ref 0 and c2 = ref 0 in
