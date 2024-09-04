92b7ef3f78111d827b7a135b35de231ed5163592
Nicolás Ojeda Bär <n.oje.bar@gmail.com>
2024-09-04 13:58:23 +0200

    check-typo

--- stdlib/filename.ml
+++ stdlib/filename.ml
@@ -221,7 +221,11 @@ Quoting commands for execution by cmd.exe is difficult.
   let quote_cmd_filename f =
     (* In cmd.exe, forward slashes in the program path (argument 0) are
        interpreted as introducing a flag. *)
-    let f = if String.contains f '/' then String.map (function '/' -> '\\' | c -> c) f else f in
+    let f =
+      if String.contains f '/' then
+        String.map (function '/' -> '\\' | c -> c) f
+      else f
+    in
     if String.exists (function '\"' | '%' -> true | _ -> false) f then
       failwith ("Filename.quote_command: bad file name " ^ f)
     else if String.contains f ' ' then
