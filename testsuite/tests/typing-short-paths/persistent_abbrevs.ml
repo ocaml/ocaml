16897c58d7b255346130c99d8847dce6314864c8
Florian Angeletti <florian.angeletti@inria.fr>
2024-09-12 10:41:56 +0200

    better interaction between short-paths and ident disambiguation

--- testsuite/tests/typing-short-paths/persistent_abbrevs.ml
+++ testsuite/tests/typing-short-paths/persistent_abbrevs.ml
@@ -14,12 +14,27 @@ let x = [[1]; 2.0]
 Line 2, characters 14-17:
 2 | let x = [[1]; 2.0]
                   ^^^
-Error: The constant "2.0" has type "float/2"
-       but an expression was expected of type "int/2 list/2"
-       File "_none_", line 1:
-         Definition of type "float/2"
-       File "_none_", line 1:
-         Definition of type "int/2"
-       File "_none_", line 1:
-         Definition of type "list/2"
+Error: The constant "2.0" has type "float" but an expression was expected of type
+         "int list"
+|}]
+
+type a = A
+module M=struct
+  type a = B
+  let f A B = ()
+   let g f = f B A
+  let () = g f
+end
+[%%expect{|
+type a = A
+Line 6, characters 13-14:
+6 |   let () = g f
+                 ^
+Error: The value "f" has type "a/2 -> a -> unit"
+       but an expression was expected of type "a -> a/2 -> 'a"
+       Type "a/2" is not compatible with type "a"
+       Line 3, characters 2-12:
+         Definition of type "a"
+       Line 1, characters 0-10:
+         Definition of type "a/2"
 |}]
