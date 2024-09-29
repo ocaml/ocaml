7759a4ea7e8a132149ae37921bb5cfde2c8079c1
David Allsopp <david.allsopp@metastack.com>
2024-09-29 11:44:12 +0100

    Correct OOM semantics of caml_stat_strdup_to_os
    The Windows implementation caml_stat_strdup_to_utf16 returned NULL on
    OOM, where caml_stat_strdup raises Out_of_memory. Windows implementation
    fixed to raise Out_of_memory.
    
    caml_stat_strdup_noexc_to_os to be used in the one place in the runtime
    where NULL should in fact be returned.

--- runtime/caml/misc.h
+++ runtime/caml/misc.h
@@ -523,6 +523,7 @@ extern double caml_log1p(double);
 #define caml_stat_strconcat_os caml_stat_wcsconcat
 
 #define caml_stat_strdup_to_os caml_stat_strdup_to_utf16
+#define caml_stat_strdup_noexc_to_os caml_stat_strdup_noexc_to_utf16
 #define caml_stat_strdup_of_os caml_stat_strdup_of_utf16
 #define caml_stat_strdup_noexc_of_os caml_stat_strdup_noexc_of_utf16
 #define caml_copy_string_of_os caml_copy_string_of_utf16
@@ -565,6 +566,7 @@ extern double caml_log1p(double);
 #define caml_stat_strconcat_os caml_stat_strconcat
 
 #define caml_stat_strdup_to_os caml_stat_strdup
+#define caml_stat_strdup_noexc_to_os caml_stat_strdup_noexc
 #define caml_stat_strdup_of_os caml_stat_strdup
 #define caml_stat_strdup_noexc_of_os caml_stat_strdup_noexc
 #define caml_copy_string_of_os caml_copy_string
--- runtime/caml/osdeps.h
+++ runtime/caml/osdeps.h
@@ -162,13 +162,27 @@ extern void caml_init_os_params(void);
 
 #ifdef _WIN32
 
+/* [caml_stat_strdup_noexc_to_utf16(s)] returns a null-terminated copy of [s],
+   re-encoded in UTF-16.  The encoding of [s] is assumed to be UTF-8 if
+   [caml_windows_unicode_runtime_enabled] is non-zero **and** [s] is valid
+   UTF-8, or the current Windows code page otherwise.
+
+   The returned string is allocated with [caml_stat_alloc_noexc], so it should
+   be freed using [caml_stat_free].
+
+   If allocation fails, this returns NULL.
+*/
+CAMLextern wchar_t* caml_stat_strdup_noexc_to_utf16(const char *s);
+
 /* [caml_stat_strdup_to_utf16(s)] returns a null-terminated copy of [s],
    re-encoded in UTF-16.  The encoding of [s] is assumed to be UTF-8 if
    [caml_windows_unicode_runtime_enabled] is non-zero **and** [s] is valid
    UTF-8, or the current Windows code page otherwise.
 
-   The returned string is allocated with [caml_stat_alloc], so it should be free
-   using [caml_stat_free].
+   The returned string is allocated with [caml_stat_alloc], so it should be
+   freed using [caml_stat_free].
+
+   If allocation fails, this raises Out_of_memory.
 */
 CAMLextern wchar_t* caml_stat_strdup_to_utf16(const char *s);
 
@@ -176,8 +190,8 @@ CAMLextern wchar_t* caml_stat_strdup_to_utf16(const char *s);
    re-encoded in UTF-8 if [caml_windows_unicode_runtime_enabled] is non-zero or
    the current Windows code page otherwise.
 
-   The returned string is allocated with [caml_stat_alloc_noexc], so
-   it should be freed using [caml_stat_free].
+   The returned string is allocated with [caml_stat_alloc_noexc], so it should
+   be freed using [caml_stat_free].
 
    If allocation fails, this returns NULL.
 */
@@ -187,8 +201,8 @@ CAMLextern char* caml_stat_strdup_noexc_of_utf16(const wchar_t *s);
    re-encoded in UTF-8 if [caml_windows_unicode_runtime_enabled] is non-zero or
    the current Windows code page otherwise.
 
-   The returned string is allocated with [caml_stat_alloc_noexc], so
-   it should be freed using [caml_stat_free].
+   The returned string is allocated with [caml_stat_alloc], so it should be
+   freed using [caml_stat_free].
 
    If allocation fails, this raises Out_of_memory.
 */
