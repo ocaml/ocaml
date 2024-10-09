717a7198555e777d063c37b0dbb8683abbe33272
Gabriel Scherer <gabriel.scherer@gmail.com>
2024-10-09 11:54:01 +0200

    a testsuite for toplevel printing of constructors

---
+++ testsuite/tests/tool-toplevel/constructor_printing.ml
@@ -0,0 +1,53 @@
+(* TEST
+ expect;
+*)
+
+module M = struct
+  module N = struct
+    type t = A | B | C
+  end
+end
+[%%expect {|
+module M : sig module N : sig type t = A | B | C end end
+|}]
+
+let it = (M.N.A, M.N.B, M.N.C)
+[%%expect {|
+val it : M.N.t * M.N.t * M.N.t = (M.N.A, M.N.B, M.N.C)
+|}]
+
+open M
+let it = (N.A, N.B, N.C)
+(* The behavior in this case is known to be suboptimal: #12642. *)
+[%%expect {|
+val it : M.N.t * M.N.t * M.N.t = (M.N.A, M.N.B, M.N.C)
+|}]
+
+open N
+
+let it = (A, B, C)
+[%%expect {|
+val it : M.N.t * M.N.t * M.N.t = (A, B, C)
+|}]
+
+
+module O = struct
+  module P = struct
+    type t = D | E | F
+  end
+end
+[%%expect {|
+module O : sig module P : sig type t = D | E | F end end
+|}]
+
+let it = (O.P.D, O.P.E, O.P.F)
+[%%expect {|
+val it : O.P.t * O.P.t * O.P.t = (O.P.D, O.P.E, O.P.F)
+|}]
+
+open O.P
+
+let it = (D, E, F)
+[%%expect {|
+val it : O.P.t * O.P.t * O.P.t = (D, E, F)
+|}]
