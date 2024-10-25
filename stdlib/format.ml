e35b307a63269354f68a7514bbe0d07ed58961b9
Christophe Raffalli <christophe@raffalli.eu>
2024-10-25 13:40:16 -1000

    Update format.ml
    typo

--- stdlib/format.ml
+++ stdlib/format.ml
@@ -442,7 +442,7 @@ let rec advance_left state =
   | Some { size; token; length } ->
     let pending_count = state.pp_right_total - state.pp_left_total in
     if Size.is_known size || pending_count >= state.pp_space_left then begin
-      Queue.take_opt state.pp_queue |> ignore (* Not empty: we peek into it *)
+      Queue.take_opt state.pp_queue |> ignore; (* Not empty: we peek into it *)
       let size = if Size.is_known size then Size.to_int size else pp_infinity in
       format_pp_token state size token;
       state.pp_left_total <- length + state.pp_left_total;
