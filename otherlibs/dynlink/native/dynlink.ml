0efcd64e6344a285c89aeb4ef33df2e417595385
David Allsopp <david.allsopp@metastack.com>
2025-01-17 10:01:25 +0000

    Move all shared Dynlink code to Dynlink_common
    Eliminates the Dynlink_types and Dynlink_platform_intf modules.

--- otherlibs/dynlink/native/dynlink.ml
+++ otherlibs/dynlink/native/dynlink.ml
@@ -22,7 +22,6 @@ module Config = Dynlink_config
 open Dynlink_cmxs_format
 
 module DC = Dynlink_common
-module DT = Dynlink_types
 
 type global_map = {
   name : string;
@@ -70,7 +69,7 @@ module Native = struct
         let implementation =
           match crc_impl with
           | None -> None
-          | Some _ as crco -> Some (crco, DT.Check_inited !rank)
+          | Some _ as crco -> Some (crco, DC.Check_inited !rank)
         in
         f acc ~compunit:name ~interface:crc_intf
             ~implementation ~defined_symbols:syms)
@@ -85,17 +84,17 @@ module Native = struct
         try ndl_run handle cu
         with exn ->
           Printexc.raise_with_backtrace
-            (DT.Error (Library's_module_initializers_failed exn))
+            (DC.Error (Library's_module_initializers_failed exn))
             (Printexc.get_raw_backtrace ()))
       (Unit_header.defined_symbols unit_header)
 
   let load ~filename ~priv =
     let handle, header =
       try ndl_open filename (not priv)
-      with exn -> raise (DT.Error (Cannot_open_dynamic_library exn))
+      with exn -> raise (DC.Error (Cannot_open_dynamic_library exn))
     in
     if header.dynu_magic <> Config.cmxs_magic_number then begin
-      raise (DT.Error (Not_a_bytecode_file filename))
+      raise (DC.Error (Not_a_bytecode_file filename))
     end;
     let syms =
       "_shared_startup" ::
@@ -104,7 +103,7 @@ module Native = struct
     try
       ndl_register handle (Array.of_list syms);
       handle, header.dynu_units
-    with exn -> raise (DT.Error (Cannot_open_dynamic_library exn))
+    with exn -> raise (DC.Error (Cannot_open_dynamic_library exn))
 
   let unsafe_get_global_value ~bytecode_or_asm_symbol =
     match ndl_loadsym bytecode_or_asm_symbol with
@@ -116,12 +115,12 @@ end
 
 include DC.Make (Native)
 
-type linking_error = DT.linking_error =
+type linking_error = DC.linking_error =
   | Undefined_global of string
   | Unavailable_primitive of string
   | Uninitialized_global of string
 
-type error = DT.error =
+type error = DC.error =
   | Not_a_bytecode_file of string
   | Inconsistent_import of string
   | Unavailable_unit of string
@@ -134,5 +133,5 @@ type error = DT.error =
   | Module_already_loaded of string
   | Private_library_cannot_implement_interface of string
 
-exception Error = DT.Error
-let error_message = DT.error_message
+exception Error = DC.Error
+let error_message = DC.error_message
