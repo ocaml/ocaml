7afb3ea67c10885c496d1ca51c9926874e3b5e72
Gabriel Scherer <gabriel.scherer@gmail.com>
2025-02-02 03:39:57 +0100

    testsuite update

--- testsuite/tests/typing-gadts/packed-module-recasting.ml
+++ testsuite/tests/typing-gadts/packed-module-recasting.ml
@@ -407,27 +407,12 @@ let f (type a b) (w1 : (a, b -> b) eq) (w2 : (a, int -> int) eq) (g : a) =
   let Refl = w1 in let Refl = w2 in M.g 3;;
 [%%expect {|
 val f : ('a, 'b -> 'b) eq -> ('a, int -> int) eq -> 'a -> 'b = <fun>
-|}, Principal{|
-Line 3, characters 36-41:
-3 |   let Refl = w1 in let Refl = w2 in M.g 3;;
-                                        ^^^^^
-Error: This expression has type "b" = "int"
-       but an expression was expected of type "'a"
-       This instance of "int" is ambiguous:
-       it would escape the scope of its equation
 |}]
 let f (type a b) (w1 : (a, b -> b) eq) (w2 : (a, int -> int) eq) (g : a) =
    let module M = struct let g = g end in
    let Refl = w2 in let Refl = w1 in M.g 3;;
 [%%expect{|
 val f : ('a, 'b -> 'b) eq -> ('a, int -> int) eq -> 'a -> int = <fun>
-|}, Principal{|
-Line 3, characters 37-42:
-3 |    let Refl = w2 in let Refl = w1 in M.g 3;;
-                                         ^^^^^
-Error: This expression has type "int" but an expression was expected of type "'a"
-       This instance of "int" is ambiguous:
-       it would escape the scope of its equation
 |}]
 
 (* Ambivalance via packed module *)
@@ -444,15 +429,6 @@ module type S = sig type a val g : a end
 val f :
   ('a, 'b -> 'b) eq ->
   ('a, int -> int) eq -> (module S with type a = 'a) -> 'b = <fun>
-|}, Principal{|
-module type S = sig type a val g : a end
-Line 7, characters 36-41:
-7 |   let Refl = w1 in let Refl = w2 in M.g 3
-                                        ^^^^^
-Error: This expression has type "b" = "int"
-       but an expression was expected of type "'a"
-       This instance of "int" is ambiguous:
-       it would escape the scope of its equation
 |}]
 let f (type a b) (w1 : (a, b -> b) eq) (w2 : (a, int -> int) eq)
     (module M : S with type a = a) =
@@ -461,13 +437,6 @@ let f (type a b) (w1 : (a, b -> b) eq) (w2 : (a, int -> int) eq)
 val f :
   ('a, 'b -> 'b) eq ->
   ('a, int -> int) eq -> (module S with type a = 'a) -> int = <fun>
-|}, Principal{|
-Line 3, characters 36-41:
-3 |   let Refl = w2 in let Refl = w1 in M.g 3
-                                        ^^^^^
-Error: This expression has type "int" but an expression was expected of type "'a"
-       This instance of "int" is ambiguous:
-       it would escape the scope of its equation
 |}]
 
 (* Ambivalance in module expression *)
@@ -478,14 +447,6 @@ let f (type a b) (w1 : (a, b -> b) eq) (w2 : (a, int -> int) eq) (g : a) =
   M.res;;
 [%%expect {|
 val f : ('a, 'b -> 'b) eq -> ('a, int -> int) eq -> 'a -> 'b = <fun>
-|}, Principal{|
-Line 4, characters 2-7:
-4 |   M.res;;
-      ^^^^^
-Error: The value "M.res" has type "b" = "int"
-       but an expression was expected of type "'a"
-       This instance of "int" is ambiguous:
-       it would escape the scope of its equation
 |}]
 let f (type a b) (w1 : (a, b -> b) eq) (w2 : (a, int -> int) eq) (g : a) =
    let Refl = w2 in let Refl = w1 in
@@ -493,11 +454,4 @@ let f (type a b) (w1 : (a, b -> b) eq) (w2 : (a, int -> int) eq) (g : a) =
    M.res;;
 [%%expect{|
 val f : ('a, 'b -> 'b) eq -> ('a, int -> int) eq -> 'a -> int = <fun>
-|}, Principal{|
-Line 4, characters 3-8:
-4 |    M.res;;
-       ^^^^^
-Error: The value "M.res" has type "int" but an expression was expected of type "'a"
-       This instance of "int" is ambiguous:
-       it would escape the scope of its equation
 |}]
--- testsuite/tests/typing-gadts/test.ml
+++ testsuite/tests/typing-gadts/test.ml
@@ -1099,14 +1099,6 @@ Line 3, characters 2-26:
 Error: This expression has type "< bar : int; foo : int; .. >"
        but an expression was expected of type "'a"
        The type constructor "$1" would escape its scope
-|}, Principal{|
-Line 3, characters 2-26:
-3 |   (x:<foo:int;bar:int;..>)
-      ^^^^^^^^^^^^^^^^^^^^^^^^
-Error: This expression has type "< bar : int; foo : int; .. >"
-       but an expression was expected of type "'a"
-       This instance of "$1" is ambiguous:
-       it would escape the scope of its equation
 |}];;
 
 let g (type t) (x:t) (e : t int_foo) (e' : t int_bar) : t =
@@ -1123,14 +1115,6 @@ let g (type t) (x:t) (e : t int_foo) (e' : t int_bar) =
 ;;
 [%%expect{|
 val g : 't -> 't int_foo -> 't int_bar -> 't * int * int = <fun>
-|}, Principal{|
-Line 3, characters 5-10:
-3 |   x, x#foo, x#bar
-         ^^^^^
-Error: The method call "x#foo" has type "int"
-       but an expression was expected of type "'a"
-       This instance of "int" is ambiguous:
-       it would escape the scope of its equation
 |}];;
 
 (* PR#5554 *)
