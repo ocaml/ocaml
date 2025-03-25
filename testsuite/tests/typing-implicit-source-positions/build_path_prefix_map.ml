dbb2204a2e68236b50ecc673e10766888a90323b
Olivier Nicole <olivier@chnik.fr>
2025-03-25 18:09:52 +0100

    Support special chars in prefix path in test

--- testsuite/tests/typing-implicit-source-positions/build_path_prefix_map.ml
+++ testsuite/tests/typing-implicit-source-positions/build_path_prefix_map.ml
@@ -1,7 +1,14 @@
 (* TEST
    native-compiler;
    setup-ocamlopt.byte-build-env;
-   set BUILD_PATH_PREFIX_MAP="app/foo=${test_build_directory}";
+   set build_path_prefix = "${test_build_directory}";
+   (* Escape special characters in the prefix path as per
+      BUILD_PATH_PREFIX_MAP's specification *)
+   script =
+     "sh ${test_source_directory}/encode_build_path_prefix_map.sh \
+      ${build_path_prefix}";
+   script;
+   set BUILD_PATH_PREFIX_MAP="app/foo=${build_path_prefix}";
    (* Make the program's relative path absolute: *)
    all_modules = "${test_build_directory}/build_path_prefix_map.ml";
    ocamlopt.byte;
