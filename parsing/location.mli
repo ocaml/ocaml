858f94cd4fb62de17de6f4ed351d72c0444c7317
Olivier Nicole <olivier@chnik.fr>
2025-03-25 14:19:18 +0100

    CR: remove dead code, unrelated change and bad test

--- parsing/location.mli
+++ parsing/location.mli
@@ -26,22 +26,6 @@ type t = Warnings.loc = {
   loc_start: Lexing.position;
   loc_end: Lexing.position;
   loc_ghost: bool;
-    (** Ghost expressions and patterns:
-        expressions and patterns that do not appear explicitly in the
-        source file they have the [loc_ghost] flag set to true.
-        Then the profiler will not try to instrument them and the
-        [-annot] option will not try to display their type.
-
-        Every grammar rule that generates an element with a location must
-        make at most one non-ghost element, the topmost one.
-
-        How to tell whether your location must be ghost:
-        A location corresponds to a range of characters in the source file.
-        If the location contains a piece of code that is syntactically
-        valid (according to the documentation), and corresponds to the
-        AST node, then the location must be real; in all other cases,
-        it must be ghost.
-    *)
 }
 
 (** Note on the use of Lexing.position in this module.
