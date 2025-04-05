0b20afe0f0facfd5eb1c6dcaf729b3ea2ff83575
David Allsopp <david.allsopp@metastack.com>
2025-04-05 20:47:46 +0100

    Simplify - and correct - check-configure.sh
    The script contained an unused feature to be able to check PRs as one -
    this is never wanted, as we should always be checking that every commit
    has a valid committed configure script.
    
    The mechanism for checking whether commits should be tested is shifted
    back to the actual trawl of the log. This is both simpler (because it
    can be done with a simple file list) but also fixes a bug, since commits
    which _didn't_ update configure could reset a failure from an earlier
    commit.

--- tools/ci/actions/check-configure.sh
+++ tools/ci/actions/check-configure.sh
@@ -50,46 +50,37 @@ else
   COLOR='33'
 fi
 
-CI_SCRIPT='tools/ci/actions/check-configure.sh'
-PATHS=\
-'configure\|configure\.ac\|VERSION\|aclocal\.m4\|build-aux/.*'\
-'\|tools/autogen\|tools/git-dev-options\.sh'
-
 # $1 - commit to checkout files from
-# $2 - range of commits to diff
-# When testing a single commit, $1 and $2 will be the same; when validating the
-# tip of a branch, $1 will be HEAD and $2 will be the range of commits in the
-# branch.
 CheckTree () {
   RET=0
   COMMIT="$1"
-  COMMITS_TO_SEARCH="$2"
-  if git diff-tree --diff-filter=d --no-commit-id --name-only -r \
-       "$COMMITS_TO_SEARCH" | grep -qx "$PATHS\|$CI_SCRIPT"; then
-    git checkout -qB return
-    git checkout -q "$COMMIT"
-    mv configure configure.ref
-    tools/autogen
-    if diff -q configure configure.ref >/dev/null ; then
-      echo -e "$COMMIT: \e[32mconfigure.ac generates configure\e[0m"
-    else
-      RET=1
-      echo -e \
-        "$COMMIT: \e[${COLOR}mconfigure.ac doesn't generate configure\e[0m"
-    fi
-    mv configure.ref configure
-    git checkout -q return
+  git checkout -qB return
+  git checkout -q "$COMMIT"
+  mv configure configure.ref
+  tools/autogen
+  if diff -q configure configure.ref >/dev/null ; then
+    echo -e "$COMMIT: \e[32mconfigure.ac generates configure\e[0m"
+  else
+    RET=1
+    echo -e \
+      "$COMMIT: \e[${COLOR}mconfigure.ac doesn't generate configure\e[0m"
   fi
+  mv configure.ref configure
+  git checkout -q return
   return $RET
 }
+
+PATHS=(tools/ci/actions/check-configure.sh configure configure.ac VERSION \
+       aclocal.m4 build-aux tools/autogen tools/git-dev-options.sh)
 
 # $RESULT is 1 for success and 0 for error
 RESULT=1
 # We traverse the commits in commit order; if $ALL_COMMITS_MUST_PASS=0, the
 # success of the most recent commit of the branch (traversed last) will
 # override any previous failure.
-for commit in $(git rev-list "$MERGE_BASE..$PR_HEAD" --reverse); do
-  if CheckTree "$commit" "$commit"; then
+for commit in $(git rev-list "$MERGE_BASE..$PR_HEAD" --reverse \
+                      -- "${PATHS[@]}"); do
+  if CheckTree "$commit"; then
     if ((!ALL_COMMITS_MUST_PASS)); then
       # Commit passed, so reset any previous failure
       RESULT=1
