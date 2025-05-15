2af137b83450555a7667409bdfa1be07923d705b
Antonin Décimo <antonin@tarides.com>
2025-05-15 15:26:59 +0100

    Prevent a GCC 11.1+ note on _Atomic long long int alignment in i686
        runtime/caml/runtime_events.h:217:1: note: the alignment of '_Atomic long long unsigned int' fields changed in GCC 11.1
        217 | };
            | ^

--- runtime/caml/runtime_events.h
+++ runtime/caml/runtime_events.h
@@ -208,9 +208,10 @@ CAMLextern int caml_runtime_events_are_active(void);
 
 #ifdef CAML_INTERNALS
 
+/* Force alignment to prevent GCC note on i686. */
 struct runtime_events_buffer_header {
-  atomic_uint_fast64_t ring_head;
-  atomic_uint_fast64_t ring_tail;
+  CAMLalign(8) atomic_uint_fast64_t ring_head;
+  CAMLalign(8) atomic_uint_fast64_t ring_tail;
   uint64_t padding[8]; /* Padding so headers don't share cache lines. Eight
                           words guarantees that buffer headers don't share
                           cache lines, even for non-aligned allocations. */
