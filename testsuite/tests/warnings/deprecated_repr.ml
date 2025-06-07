8c79dec2412bd15e0dbff636c45468a52c8b55d2
Valentin Gatien-Baron <valentin.gatienbaron@gmail.com>
2025-06-07 11:02:48 +0200

    test

--- testsuite/tests/warnings/deprecated_repr.ml
+++ testsuite/tests/warnings/deprecated_repr.ml
@@ -22,11 +22,13 @@ module M :
     val print : t -> unit
     val prints : t list -> unit
   end
-|}]
+|}] (* ideally, deprecated_repr would be printed, but the issue exists with
+       [@@deprecated] *)
 
-(* ############## *)
-(* Converting between int and t should trigger warnings, but simply using
-   values of type t shouldn't. *)
+(****
+  Converting between int and t should trigger warnings, but simply using
+  values of type t shouldn't.
+ ****)
 
 let () = M.print M.zero
 [%%expect {|
@@ -106,10 +108,11 @@ let _ = M.zero (* This expands the type definition to warn if it's a function. I
 - : M.t = 0
 |}]
 
-(* ############## *)
-(* Converting between M.t and u shouldn't trigger warnings, since this is correct
-   regardless of t's representation. This implies that the typer shoudn't merely
-   expands all head type constructors during unification and other other operations. *)
+(****
+  Converting between M.t and u shouldn't trigger warnings, since this is correct
+  regardless of t's representation. This implies that the typer shoudn't merely
+  expands all head type constructors during unification and other other operations.
+ ****)
 
 type u = M.t
 module F(U : sig
@@ -131,9 +134,33 @@ type u = M.t
 module F : (U : sig val print : u -> unit val zero : u end) -> sig end
 |}]
 
-(* ############## *)
+(**** in expressions ****)
 
-(* More cases where a type constructor may get expanded, in types. *)
+module F : sig
+  type t = int -> int [@@deprecated_repr]
+  val f : t
+end = struct
+  type t = int -> int
+  let f = Fun.id
+end
+let _ = F.f 1
+[%%expect {|
+module F : sig type t = int -> int val f : t end
+- : int = 1
+|}] (* bug: should warn *)
+
+module C : sig
+  type t = < a : int > [@@deprecated_repr]
+end = struct
+  type t = < a : int >
+end
+let __ (x : C.t) = x#a
+[%%expect {|
+module C : sig type t = < a : int > end
+val __ : C.t -> int = <fun>
+|}] (* bug: should warn *)
+
+(**** in types ****)
 
 type _t = [ `A of M.t | `A of int ]
 type _u = [ `A of u | `A of M.t ]
@@ -161,7 +188,7 @@ Alert deprecated: implicitly converting between a type and its deprecated repres
 - : unit = ()
 |}]
 
-(* More cases where a type constructor may get expanded, in modules. *)
+(**** in modules ****)
 
 module Z1 : sig
   val x : int
@@ -201,7 +228,30 @@ end (* bug: missing warning *)
 module Z3 : sig type z = int end
 |}]
 
-(* More cases where a type constructor may get expanded, in pattern. *)
+let f () =
+  (* nondep expanding local type constructors away. It's hard to imagine why anyone
+     would care about this, but perhaps there are examples involving functors that make
+     sense. *)
+  let module M : sig
+        type t = int [@@deprecated_repr]
+        val x : t
+      end = struct
+        type t = int
+        let x = 0
+      end
+  in
+  M.x
+[%%expect {|
+Line 13, characters 2-5:
+13 |   M.x
+       ^^^
+Alert deprecated: implicitly converting between a type and its deprecated representation
+
+val f : unit -> int = <fun>
+|}]
+
+(**** in patterns ****)
+
 let _ = fun ((_ : M.t) : int) -> ()
 let _ = fun ((_ : int) : M.t) -> ()
 [%%expect {|
@@ -219,8 +269,50 @@ Alert deprecated: implicitly converting between a type and its deprecated repres
 - : M.t -> unit = <fun>
 |}]
 
-(* More cases where a type constructor may get expanded, filling in of omitted
-   optional parameter. *)
+(**** type-based disambiguation ****)
+
+type record = { a : int }
+type record_alias = record [@@deprecated_repr]
+let __ (x : record_alias) = x.a
+let __ ({ a } : record_alias) = a
+[%%expect {|
+type record = { a : int; }
+type record_alias = record
+val __ : record_alias -> int = <fun>
+val __ : record_alias -> int = <fun>
+|}, Principal{|
+type record = { a : int; }
+type record_alias = record
+val __ : record_alias -> int = <fun>
+Line 4, characters 8-13:
+4 | let __ ({ a } : record_alias) = a
+            ^^^^^
+Alert deprecated: implicitly converting between a type and its deprecated representation
+
+val __ : record_alias -> int = <fun>
+|}]
+(* bug: should warn regardless of -principal, and both functions should warn *)
+
+type sum = A of int
+type sum_alias = sum [@@deprecated_repr]
+let __ (A a : sum_alias) = a
+[%%expect {|
+type sum = A of int
+type sum_alias = sum
+val __ : sum_alias -> int = <fun>
+|}, Principal{|
+type sum = A of int
+type sum_alias = sum
+Line 3, characters 8-11:
+3 | let __ (A a : sum_alias) = a
+            ^^^
+Alert deprecated: implicitly converting between a type and its deprecated representation
+
+val __ : sum_alias -> int = <fun>
+|}]
+(* bug: should warn regardless of -principal *)
+
+(**** when optional parameters are filled in ****)
 
 open (struct
        type fu = unit -> unit
@@ -265,3 +357,122 @@ Error: The value "f" has type "?machin:'a -> fu"
        but an unlabeled argument was expected
 |}] (* bug? Not clear if warning 16 should trigger here or not. Also not clear if the last
        function should fail to type or not. *)
+
+(**** parametric type ****)
+
+module P : sig
+  type 'a t = 'a * int [@@deprecated_repr]
+  val z : 'a list t
+end = struct
+  type 'a t = 'a * int
+  let z = [], 1
+end
+
+let x = P.z
+let __ (x : int P.t) = ()
+let __ () = (assert false : float P.t)
+
+[%%expect{|
+module P : sig type 'a t = 'a * int val z : 'a list t end
+val x : 'a list P.t = ([], 1)
+val __ : int P.t -> unit = <fun>
+val __ : unit -> float P.t = <fun>
+|}]
+
+(**** parametric but phantom type ****)
+
+module Phantom : sig
+  type 'a t = int [@@deprecated_repr]
+  val z : 'a t
+end = struct
+  type 'a t = int
+  let z = 1
+end
+
+let x = Phantom.z
+[%%expect{|
+module Phantom : sig type 'a t = int val z : 'a t end
+val x : 'a Phantom.t = 1
+|}]
+let __ (x : int Phantom.t) = ()
+[%%expect{|
+Line 1, characters 7-26:
+1 | let __ (x : int Phantom.t) = ()
+           ^^^^^^^^^^^^^^^^^^^
+Alert deprecated: implicitly converting between a type and its deprecated representation
+
+val __ : int -> unit = <fun>
+|}] (* bug: this shouldn't warn *)
+let __ () = (assert false : float Phantom.t)
+[%%expect{|
+Line 1, characters 12-44:
+1 | let __ () = (assert false : float Phantom.t)
+                ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+Alert deprecated: implicitly converting between a type and its deprecated representation
+
+val __ : unit -> int = <fun>
+|}] (* bug: this shouldn't warn *)
+let __ (x : int Phantom.t) = (x : float Phantom.t)
+[%%expect{|
+Line 1, characters 7-26:
+1 | let __ (x : int Phantom.t) = (x : float Phantom.t)
+           ^^^^^^^^^^^^^^^^^^^
+Alert deprecated: implicitly converting between a type and its deprecated representation
+
+Line 1, characters 30-31:
+1 | let __ (x : int Phantom.t) = (x : float Phantom.t)
+                                  ^
+Alert deprecated: implicitly converting between a type and its deprecated representation
+
+Line 1, characters 30-31:
+1 | let __ (x : int Phantom.t) = (x : float Phantom.t)
+                                  ^
+Alert deprecated: implicitly converting between a type and its deprecated representation
+
+Line 1, characters 30-31:
+1 | let __ (x : int Phantom.t) = (x : float Phantom.t)
+                                  ^
+Error: The value "x" has type "int Phantom.t" = "int"
+       but an expression was expected of type "float Phantom.t" = "int"
+       Type "int" is not compatible with type "float"
+|}, Principal{|
+Line 1, characters 7-26:
+1 | let __ (x : int Phantom.t) = (x : float Phantom.t)
+           ^^^^^^^^^^^^^^^^^^^
+Alert deprecated: implicitly converting between a type and its deprecated representation
+
+Line 1, characters 30-31:
+1 | let __ (x : int Phantom.t) = (x : float Phantom.t)
+                                  ^
+Alert deprecated: implicitly converting between a type and its deprecated representation
+
+Line 1, characters 29-50:
+1 | let __ (x : int Phantom.t) = (x : float Phantom.t)
+                                 ^^^^^^^^^^^^^^^^^^^^^
+Alert deprecated: implicitly converting between a type and its deprecated representation
+
+val __ : int -> int = <fun>
+|}] (* bug: apart from the noise from the previous bugs, the divergence between principal
+       and non-principal is weird. *)
+
+(* When user code has type errors, we probably don't want to emit random deprecation
+   warnings. *)
+
+let f (_ : M.t) (_ : _ Queue.t) = ()
+let () = f (Queue.create ()) M.zero
+
+[%%expect{|
+val f : M.t -> 'a Queue.t -> unit = <fun>
+Line 2, characters 11-28:
+2 | let () = f (Queue.create ()) M.zero
+               ^^^^^^^^^^^^^^^^^
+Alert deprecated: implicitly converting between a type and its deprecated representation
+
+Line 2, characters 11-28:
+2 | let () = f (Queue.create ()) M.zero
+               ^^^^^^^^^^^^^^^^^
+Error: This expression has type "'a Queue.t"
+       but an expression was expected of type "M.t" = "int"
+|}]
+(* bug: we should probably delay emitting the warnings until the surrounding structure
+   item or compilation unit types, or something. *)
