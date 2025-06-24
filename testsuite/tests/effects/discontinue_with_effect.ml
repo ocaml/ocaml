682269fb83a6eb7ccc1e5bc90564bea4958e85e4
michaelleejl <michaelleejl@gmail.com>
2025-06-24 18:31:06 +0800

    Add more tests

--- testsuite/tests/effects/discontinue_with_effect.ml
+++ testsuite/tests/effects/discontinue_with_effect.ml
@@ -11,3 +11,19 @@ let x = try perform (E ()) with
                     | effect F x, k -> x + 2;;
 
 assert (x = 3)
+
+let y = try 
+          try 
+            perform (E ()) 
+          with effect F x, k -> x + 2
+        with effect E x, k -> discontinue_with_effect k (F 1);;
+        
+assert (y = 3)
+
+let z = try 
+          try 
+            perform (E ()) 
+          with effect E x, k -> discontinue_with_effect k (F 1)
+        with effect F x, k -> x + 2;;
+        
+assert (z = 3)
