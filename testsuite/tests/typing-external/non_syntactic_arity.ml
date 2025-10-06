2471f817acb8930e455c2fc9d80f4474c92d6000
Fourchaux <jprodi04@gmail.com>
2025-10-07 01:23:59 +0200

    Update non_syntactic_arity.ml

--- testsuite/tests/typing-external/non_syntactic_arity.ml
+++ testsuite/tests/typing-external/non_syntactic_arity.ml
@@ -8,5 +8,5 @@ Line 1, characters 37-39:
 1 | external fail: (int -> int as 'a) -> 'a = "%identity"
                                          ^^
 Error: This external declaration has a non-syntactic arity,
-       its arity is greater than its syntatic arity.
+       its arity is greater than its syntactic arity.
 |}]
