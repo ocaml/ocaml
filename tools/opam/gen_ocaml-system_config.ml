8f6e3e1c8a38d78c43b9f54496cddc8dbedefd86
David Allsopp <david.allsopp@metastack.com>
2025-11-12 15:34:05 +0000

    Towards support for trunk system compilers in opam
    Switch gen_ocaml-system_config.ml to receive arguments by the command
    line instead of through opam's substs mechanism.

---
+++ tools/opam/gen_ocaml-system_config.ml
@@ -0,0 +1,46 @@
+let () =
+  let exe = ".exe" in
+  let ocamlc =
+    let (base, suffix) =
+      let s = Sys.executable_name in
+      if Filename.check_suffix s exe then
+        (Filename.chop_suffix s exe, exe)
+      else
+        (s, "") in
+    base ^ "c" ^ suffix in
+  let required_version = Sys.argv.(1) in
+  let package_name = Sys.argv.(2) in
+  if Sys.ocaml_version <> required_version then begin
+    Printf.eprintf
+      "ERROR: The compiler found at %s has version %s,\n\
+       and this package requires %s.\n\
+       You should use e.g. 'opam switch create %s.%s' \
+       instead."
+      ocamlc Sys.ocaml_version required_version package_name Sys.ocaml_version;
+    exit 1
+  end else
+    let ocamlc_digest = Digest.to_hex (Digest.file ocamlc) in
+    let config = package_name ^ ".config" in
+    let libdir =
+      if Sys.command (ocamlc ^ " -where > " ^ config) = 0 then
+        let ic = open_in config in
+        let r = input_line ic in
+        close_in ic;
+        Sys.remove config;
+        r
+      else
+        failwith "Bad return from 'ocamlc -where'"
+    in
+    let graphics = Filename.concat libdir "graphics.cmi" in
+    let graphics_digest =
+      if Sys.file_exists graphics then
+        Digest.to_hex (Digest.file graphics)
+      else
+        String.make 32 '0'
+    in
+    let oc = open_out config in
+    Printf.fprintf oc "opam-version: \"2.0\"\n\
+                       file-depends: [ [ %S %S ] [ %S %S ] ]\n\
+                       variables { path: %S }\n"
+      ocamlc ocamlc_digest graphics graphics_digest (Filename.dirname ocamlc);
+    close_out oc
