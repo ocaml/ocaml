065cf367171db850eb64cd052278929ae434e927
Martin Jambon <martin@mjambon.com>
2025-12-30 20:51:35 -0800

    Add missing copyright header

--- lex/exhaustiveness.mli
+++ lex/exhaustiveness.mli
@@ -1,3 +1,16 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*                             Martin Jambon                              *)
+(*                                                                        *)
+(*   Copyright 2025 Martin Jambon                                         *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
 (**
    Check whether a rule may fail to match on some input
 *)
