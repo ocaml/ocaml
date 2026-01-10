7bbe7b65c3b4b89fbbe89197a47866114dff6cf0
Xavier Leroy <xavier.leroy@college-de-france.fr>
2026-01-10 10:15:07 +0100

    Stdlib.Int32: add fdiv, cdiv, ediv, erem
    Tests will be added later.

--- stdlib/int32.mli
+++ stdlib/int32.mli
@@ -79,6 +79,45 @@ val unsigned_rem : int32 -> int32 -> int32
 
     @since 4.08 *)
 
+val fdiv : int32 -> int32 -> int32
+(** Floor division.
+    [fdiv x y] is the real quotient [x / y] rounded down to an integer.
+    We have [fdiv x y <= div x y <= cdiv x y] and [cdiv x y - fdiv x y <= 1].
+
+    @raise Division_by_zero if the second argument is 0.
+    @since 5.5
+*)
+
+val cdiv : int32 -> int32 -> int32
+(** Ceil division.
+    [cdiv x y] is the real quotient [x / y] rounded up to an integer.
+    We have [fdiv x y <= div x y <= cdiv x y] and [cdiv x y - fdiv x y <= 1].
+
+    @raise Division_by_zero if the second argument is 0.
+    @since 5.5
+*)
+
+val ediv : int32 -> int32 -> int32
+(** Euclidean division.
+    [ediv x y] is the real quotient [x / y] rounded down to an integer
+    if [y > 0] and rounded up to an integer if [y < 0].
+    The remainder [erem x y = x - ediv x y * y] is always non-negative.
+    Moreover, [ediv x (-y)] = [- ediv x y].
+
+    @raise Division_by_zero if the second argument is 0.
+    @since 5.5
+*)
+
+val erem : int32 -> int32 -> int32
+(** Euclidean remainder.  If [y] is not zero, we have
+    [x = ediv x y * y + erem x y] and [0 <= erem x y <= abs y - 1].
+    The result of [erem x y] is always non-negative,
+    unlike the result of [rem x y], which has the sign of [x].
+
+    @raise Division_by_zero if the second argument is 0.
+    @since 5.5
+*)
+
 val succ : int32 -> int32
 (** Successor.  [Int32.succ x] is [Int32.add x Int32.one]. *)
 
