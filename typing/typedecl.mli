85be3f6f8886bbe537f99b8c0d5cd936cea5dcde
Stefan Muenzel <source@s.muenzel.net>
2026-03-02 13:17:03 +0100

    Fix PR5914 regression

--- typing/typedecl.mli
+++ typing/typedecl.mli
@@ -80,6 +80,7 @@ and reaching_type_step =
   | Expands_to of type_expr * type_expr
   | Contains of type_expr * type_expr
   | Parameter of Path.t * int * type_expr
+  | Considered_abstract of Path.t
 
 type error =
     Repeated_parameter
