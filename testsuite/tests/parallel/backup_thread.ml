67df568c0ff92ea8070415083f61ec9601121e8e
Gabriel Scherer <gabriel.scherer@gmail.com>
2026-03-04 17:53:11 +0100

    testsuite: fix parallel/backup_thread.ml in cleanup-on-exit mode

--- testsuite/tests/parallel/backup_thread.ml
+++ testsuite/tests/parallel/backup_thread.ml
@@ -13,8 +13,12 @@ let _ =
   (* start a dummy domain and shut it down to cause a domain reuse *)
   let d = Domain.spawn (fun _ -> ()) in
   Domain.join d;
-  let _d = Domain.spawn (fun _ ->
-    Unix.sleep 10;
-    print_endline "Should not reach here!") in
+  let finished = Atomic.make false in
+  let d = Domain.spawn (fun _ ->
+    Unix.sleep 1;
+    if not (Atomic.get finished) then
+      print_endline "Should not reach here!") in
   Gc.full_major ();
-  print_endline "OK"
+  print_endline "OK";
+  Atomic.set finished true;
+  Domain.join d
