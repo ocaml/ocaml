ocaml/ocaml

commit 6125c291d0c63c16ef8a5cff8960a14c915cf380

Do not change GC pace when creating sub-arrays of bigarrays (#12754)
This is achieved by adding a CAML_BA_SUBARRAY flag that is honored by caml_ba_alloc.

Fixes: #12491
Closes: #12500

runtime/bigarray.c
@@ -225,7 +225,7 @@ CAMLexport value
 caml_ba_alloc(int flags, int num_dims, void * data, intnat * dim)
 {
   uintnat num_elts, asize, size;
-  int i, is_managed;
+  int i, uses_resources;
   value res;
   struct caml_ba_array * b;
   intnat dimcopy[CAML_BA_MAX_NUM_DIMS];
@@ -248,8 +248,10 @@ caml_ba_alloc(int flags, int num_dims, void * data, intnat * dim)
     flags |= CAML_BA_MANAGED;
   }
   asize = SIZEOF_BA_ARRAY + num_dims * sizeof(intnat);
-  is_managed = ((flags & CAML_BA_MANAGED_MASK) == CAML_BA_MANAGED);
-  res = caml_alloc_custom_mem(&caml_ba_ops, asize, is_managed ? size : 0);
+  uses_resources =
+    ((flags & CAML_BA_MANAGED_MASK) == CAML_BA_MANAGED)
+    && !(flags & CAML_BA_SUBARRAY);
+  res = caml_alloc_custom_mem(&caml_ba_ops, asize, uses_resources ? size : 0);
   b = Caml_ba_array_val(res);
   b->data = data;
   b->num_dims = num_dims;
@@ -1133,7 +1135,8 @@ CAMLprim value caml_ba_slice(value vb, value vind)
     (char *) b->data +
     offset * caml_ba_element_size[b->flags & CAML_BA_KIND_MASK];
   /* Allocate an OCaml bigarray to hold the result */
-  res = caml_ba_alloc(b->flags, b->num_dims - num_inds, sub_data, sub_dims);
+  res = caml_ba_alloc(b->flags | CAML_BA_SUBARRAY,
+                      b->num_dims - num_inds, sub_data, sub_dims);
   /* Copy the finalization function from the original array (PR#8568) */
   Custom_ops_val(res) = Custom_ops_val(vb);
   /* Create or update proxy in case of managed bigarray */
@@ -1160,7 +1163,8 @@ CAMLprim value caml_ba_change_layout(value vb, value vlayout)
     intnat new_dim[CAML_BA_MAX_NUM_DIMS];
     unsigned int i;
     for(i = 0; i < b->num_dims; i++) new_dim[i] = b->dim[b->num_dims - i - 1];
-    res = caml_ba_alloc(flags, b->num_dims, b->data, new_dim);
+    res = caml_ba_alloc(flags | CAML_BA_SUBARRAY,
+                        b->num_dims, b->data, new_dim);
     /* Copy the finalization function from the original array (PR#8568) */
     Custom_ops_val(res) = Custom_ops_val(vb);
     caml_ba_update_proxy(b, Caml_ba_array_val(res));
@@ -1205,7 +1209,8 @@ CAMLprim value caml_ba_sub(value vb, value vofs, value vlen)
     (char *) b->data +
     ofs * mul * caml_ba_element_size[b->flags & CAML_BA_KIND_MASK];
   /* Allocate an OCaml bigarray to hold the result */
-  res = caml_ba_alloc(b->flags, b->num_dims, sub_data, b->dim);
+  res = caml_ba_alloc(b->flags | CAML_BA_SUBARRAY,
+                      b->num_dims, sub_data, b->dim);
   /* Copy the finalization function from the original array (PR#8568) */
   Custom_ops_val(res) = Custom_ops_val(vb);
   /* Doctor the changed dimension */
@@ -1388,7 +1393,7 @@ CAMLprim value caml_ba_reshape(value vb, value vdim)
   if (num_elts != caml_ba_num_elts(b))
     caml_invalid_argument("Bigarray.reshape: size mismatch");
   /* Create bigarray with same data and new dimensions */
-  res = caml_ba_alloc(b->flags, num_dims, b->data, dim);
+  res = caml_ba_alloc(b->flags | CAML_BA_SUBARRAY, num_dims, b->data, dim);
   /* Copy the finalization function from the original array (PR#8568) */
   Custom_ops_val(res) = Custom_ops_val(vb);
   /* Create or update proxy in case of managed bigarray */

runtime/caml/bigarray.h
@@ -74,6 +74,10 @@ enum caml_ba_managed {
   CAML_BA_MANAGED_MASK = 0x600 /* Mask for "managed" bits in flags field */
 };
 
+enum caml_ba_subarray {
+  CAML_BA_SUBARRAY = 0x800     /* Data is shared with another bigarray */
+};
+
 struct caml_ba_proxy {
   atomic_uintnat refcount;      /* Reference count */
   void * data;                  /* Pointer to base of actual data */

commit 70695ca15464287ea8fba7d00c97811e9564eba3

caml_ba_alloc: flags&CAML_BA_SUBARRAY with data = NULL must not result in OOM crash (backward compat fix for old C bindings)
This flag didn't exist to OCaml 5.2, and there are C bindings that pass
through `flags` unchanged when creating a bigarray in the shape of
another bigarray.

For example NX contained this call:
```
caml_ba_alloc(flags, 1, NULL, dims);
```

If `flags` contained `CAML_BA_SUBARRAY` (because the argument to this NX
function was already a bigarray subarray), then `caml_ba_alloc` would
allocate memory, but tell the GC it allocated 0 bytes:

```
Breakpoint 7, caml_ba_alloc (flags=2560, num_dims=num_dims@entry=1, data=data@entry=0x0, dim=dim@entry=0x7fffffffd068)
    at runtime/bigarray.c:242
242         data = malloc(size);
(gdb) c
Continuing.

Breakpoint 5, caml_alloc_custom_mem (ops=ops@entry=0x8c9e60 <caml_ba_ops>, bsz=bsz@entry=40, mem_bytes=mem_bytes@entry=0)
    at runtime/custom.c:76
76        size_t mem_words = (mem_bytes + sizeof(value) - 1) / sizeof(value);
```

Eventually this could result in an out of memory crash, because the GC
won't do enough work to keep up.

Alternatively `uses_resources = !data` could be set prior to
`if (!data)`, but it seems better to fix the flag.

Fixes: 6125c291d0 ("Do not change GC pace when creating sub-arrays of bigarrays (#12754)")

Signed-off-by: Edwin Török <edwin@tarides.com>

runtime/bigarray.c
@@ -242,6 +242,8 @@ caml_ba_alloc(int flags, int num_dims, void * data, intnat * dim)
     data = malloc(size);
     if (data == NULL && size != 0) caml_raise_out_of_memory();
     flags |= CAML_BA_MANAGED;
+    /* backwards compatibility with C bindings that pass through flags */
+    flags &= ~CAML_BA_SUBARRAY;
   }
   asize = SIZEOF_BA_ARRAY + num_dims * sizeof(intnat);
   uses_resources =