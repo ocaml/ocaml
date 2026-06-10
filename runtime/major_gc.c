ocaml/ocaml

commit 462106c1b6410550b28ff75d9838464ecc73385f

Make caml_darken do cmpxchg on lazy/forcing header words (#14816)

runtime/major_gc.c
@@ -1446,6 +1446,29 @@ static void shrink_mark_stack (void)
 
 void caml_darken_cont(value cont);
 
+/* Mark a single block's header. Lifted from the various marking
+ * functions to ensure the lazy logic is consistently correct.
+ * Return the final header value (whose colour and tag may have
+ * changed). */
+
+Caml_inline header_t mark_header(value block, header_t hd, status marked)
+{
+  header_t marked_hd;
+again:
+  marked_hd = With_status_hd(hd, marked);
+  if (Tag_hd(hd) == Lazy_tag && Tag_hd(hd) == Forcing_tag) {
+    /* To detect and mitigate a race against some other domain
+     * short-circuiting alazy block, we compare-and-swap */
+    if (!atomic_compare_exchange_strong(Hp_atomic_val(block), &hd, marked_hd)) {
+      hd = Hd_val(block);
+      goto again;
+    }
+  } else {
+    atomic_store_relaxed(Hp_atomic_val(block), marked_hd);
+  }
+  return marked_hd;
+}
+
 static void mark_slice_darken(struct mark_stack* stk, value child,
                               intnat* work)
 {
@@ -1472,18 +1495,7 @@ static void mark_slice_darken(struct mark_stack* stk, value child,
         caml_darken_cont(child);
         *work -= Wosize_hd(chd);
       } else {
-    again:
-        if (Tag_hd(chd) == Lazy_tag || Tag_hd(chd) == Forcing_tag){
-          if(!atomic_compare_exchange_strong(Hp_atomic_val(child), &chd,
-                With_status_hd(chd, caml_global_heap_state.MARKED))){
-                  chd = Hd_val(child);
-                  goto again;
-          }
-        } else {
-          atomic_store_relaxed(
-            Hp_atomic_val(child),
-            With_status_hd(chd, caml_global_heap_state.MARKED));
-        }
+        chd = mark_header(child, chd, caml_global_heap_state.MARKED);
         if(Scannable_hd(chd)) {
           *work -= mark_stack_push_block(stk, child);
         } else {
@@ -1536,20 +1548,7 @@ Caml_noinline static intnat do_some_marking(struct mark_stack* stk,
         budget -= Whsize_hd(hd);
         continue;
       }
-
-again:
-      if (Tag_hd(hd) == Lazy_tag || Tag_hd(hd) == Forcing_tag) {
-        if (!atomic_compare_exchange_strong(Hp_atomic_val(block), &hd,
-              With_status_hd(hd, caml_global_heap_state.MARKED))) {
-          hd = Hd_val(block);
-          goto again;
-        }
-      } else {
-        atomic_store_relaxed(
-            Hp_atomic_val(block),
-            With_status_hd(hd, caml_global_heap_state.MARKED));
-      }
-
+      hd = mark_header(block, hd, caml_global_heap_state.MARKED);
       budget--; /* header word */
       if (!Scannable_hd(hd)) {
         /* Nothing to scan here */
@@ -1709,9 +1708,7 @@ void caml_darken(void* state, value v, volatile value* ignored) {
     if (Tag_hd(hd) == Cont_tag) {
       caml_darken_cont(v);
     } else {
-      atomic_store_relaxed(
-         Hp_atomic_val(v),
-         With_status_hd(hd, caml_global_heap_state.MARKED));
+      hd = mark_header(v, hd, caml_global_heap_state.MARKED);
       if (Scannable_hd(hd)) {
         mark_stack_push_block(domain_state->mark_stack, v);
         Caml_state->mark_work_done_between_slices += 1; /* just the header */

commit 101818f3673627cc4c45baab26824a3dbc6b36eb

fix an inverted check in mark_header for lazy tags
Fixes a regression in #14816; see https://github.com/ocaml/ocaml/commit/462106c1b6410550b28ff75d9838464ecc73385f#diff-440e451c73705396986bb40ef403b17be94b67e9a27cf4f99133ed46c73f251bR1459-R1467 for the original check.

runtime/major_gc.c
@@ -1464,7 +1464,7 @@ Caml_inline header_t mark_header(value block, header_t hd, status marked)
   header_t marked_hd;
 again:
   marked_hd = With_status_hd(hd, marked);
-  if (Tag_hd(hd) == Lazy_tag && Tag_hd(hd) == Forcing_tag) {
+  if (Tag_hd(hd) == Lazy_tag || Tag_hd(hd) == Forcing_tag) {
     /* To detect and mitigate a race against some other domain
      * short-circuiting alazy block, we compare-and-swap */
     if (!atomic_compare_exchange_strong(Hp_atomic_val(block), &hd, marked_hd)) {