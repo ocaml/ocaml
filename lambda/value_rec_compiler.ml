dec0957b2ec882ac059413d489bee9fc3c658585
Vincent Laviron <vincent.laviron@gmail.com>
2026-06-26 10:15:51 +0200

    Simpler version

--- lambda/value_rec_compiler.ml
+++ lambda/value_rec_compiler.ml
@@ -497,15 +497,8 @@ let rec split_static_function block_var local_idents renamings lam :
     Reachable (lifted,
                Lprim (Pmakeblock (0, lifted_block_mut, None), [Lvar v], no_loc))
   | Lfunction lfun ->
-    let free_vars = Lambda.free_variables lfun.body in
-    (* Translate the free variables through renamings *)
-    let free_vars =
-      Ident.Map.fold (fun left right free_vars ->
-          if Ident.Set.mem left free_vars
-          then Ident.Set.add right (Ident.Set.remove left free_vars)
-          else free_vars)
-        renamings free_vars
-    in
+    let body = Lambda.rename renamings lfun.body in
+    let free_vars = Lambda.free_variables body in
     let local_free_vars = Ident.Set.inter free_vars local_idents in
     let free_vars_block_size, subst, block_fields_rev =
       Ident.Set.fold (fun var (i, subst, fields) ->
@@ -517,13 +510,6 @@ let rec split_static_function block_var local_idents renamings lam :
           (succ i, Ident.Map.add var access subst, Lvar var :: fields))
         local_free_vars (0, Ident.Map.empty, [])
     in
-    let subst =
-      Ident.Map.fold (fun var_left var_right subst ->
-          Ident.Map.add var_left
-            (Lambda.subst (fun _ _ env -> env) subst (Lvar var_right))
-            subst)
-        renamings subst
-    in
     (* Note: When there are no local free variables, we don't need the
        substitution and we don't need to generate code for pre-allocating
        and backpatching a block of size 0.
@@ -531,7 +517,7 @@ let rec split_static_function block_var local_idents renamings lam :
        noticeably worse, so we use it for simplicity. *)
     let new_fun =
       lfunction_with_body lfun
-        (Lambda.subst (fun _ _ env -> env) subst lfun.body)
+        (Lambda.subst (fun _ _ env -> env) subst body)
     in
     let lifted = { lfun = new_fun; free_vars_block_size } in
     let block =
