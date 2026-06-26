6da5644b7db2cff5713f1906e80a7bc835594943
octachron <octa@polychoron.fr>
2026-06-26 16:14:20 +0200

    pattern matching overflow: example

--- testsuite/tests/basic-more/morematch.ml
+++ testsuite/tests/basic-more/morematch.ml
@@ -10,10 +10,10 @@
 
 let test msg f arg r =
   if f arg <> r then begin
-    prerr_endline msg ;
-    failwith "Malaise"
+    Format.eprintf "Error:%s@." msg
   end
 ;;
+
 
 type t = A | B | C | D | E | F
   ;;
@@ -1021,6 +1021,36 @@ test "maf" maf (`TTuple []) 4 ;
 test "maf" maf (`TConstr []) 5 ;
 test "maf" maf (`TVariant []) 6
 ;;
+
+(* Extended version  *)
+let f = function
+  | -4611686018427387904 -> 0
+  | -4611686018427387903 -> 1
+  | -4611686018427387902 -> 2
+
+  | -4611686018427387609 -> 11
+  | -4611686018427387608 -> 12
+
+  | -4611686018427387509 -> 21
+  | -4611686018427387508 -> 22
+
+
+  | -4611686018427387009 -> 31
+  | -4611686018427387008 -> 32
+
+
+  | 65529 -> 101
+  | 65530 -> 102
+  | _ -> -1;;
+
+test "min_int_overflow" f (-4611686018427387904) 0;
+test "min_int_overflow" f (-4611686018427387903) 1;
+test "min_int_overflow" f (-4611686018427387609) 11;
+test "min_int_overflow" f (-4611686018427387508) 22;
+test "min_int_overflow" f (-4611686018427387008) 32;
+test "min_int_overflow" f 0 (-1);
+test "min_int_overflow" f 65529 101;;
+
 
 (* PR#3517
   Using ``get_args'' in place or an ad-hoc ``matcher'' function for tuples.
