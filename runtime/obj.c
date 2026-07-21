89999960411b5f5221a1b4c4571cb469b8675b56
Frédéric Bour <frederic.bour@lakaban.net>
2026-07-21 18:49:08 +0900

    tag representation + global for storing freshly compiled tags

--- runtime/obj.c
+++ runtime/obj.c
@@ -446,3 +446,26 @@ CAMLprim value caml_obj_set_reserved(value obj, value tag)
 }
 
 #endif
+
+CAMLprim value caml_compiler_tags(value unit)
+{
+  (void)unit;
+  static value tag_ref = 0;
+  static caml_plat_mutex tag_lock = CAML_PLAT_MUTEX_INITIALIZER;
+
+  if (tag_ref != 0)
+    return tag_ref;
+
+  caml_plat_lock_non_blocking(&tag_lock);
+
+  if (tag_ref == 0)
+  {
+    tag_ref = caml_alloc(1, 0);
+    Store_field(tag_ref, 0, Val_unit);
+    caml_register_generational_global_root(&tag_ref);
+  }
+
+  caml_plat_unlock(&tag_lock);
+
+  return tag_ref;
+}
