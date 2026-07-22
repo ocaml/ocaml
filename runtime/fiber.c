tkoukpari/oxcaml

Address fiber cache leak (#5017)
* Test sending fibers from one domain to another

This is designed to expose leak from fiber caches growing unboundedly.

* Configurably limit number of cached stacks per class

runtime/fiber.c
@@ -996,6 +996,14 @@ static void free_stack_memory(struct stack_info* stack)
 #endif
 }
 
+uintnat caml_cache_stacks_per_class =
+#if defined(USE_MMAP_MAP_STACK) || defined(STACK_GUARD_PAGES)
+  1
+#else
+  128
+#endif
+  ;
+
 void caml_free_stack (struct stack_info* stack)
 {
   CAMLnoalloc;
@@ -1008,13 +1016,19 @@ void caml_free_stack (struct stack_info* stack)
   caml_free_local_arenas(stack->local_arenas);
 
   if (stack->cache_bucket != -1) {
-    stack->exception_ptr =
-      (void*)(cache[stack->cache_bucket]);
-    cache[stack->cache_bucket] = stack;
+    struct stack_info* top = (struct stack_info*)cache[stack->cache_bucket];
+    int64_t count = top ? top->id : 0;
+    if (count < caml_cache_stacks_per_class) {
+      stack->exception_ptr = (void *)top;
+      stack->id = count + 1;
+      cache[stack->cache_bucket] = stack;
 #if defined(DEBUG) && defined(STACK_CHECKS_ENABLED)
-    memset(Stack_base(stack), 0x42,
-           (Stack_high(stack)-Stack_base(stack))*sizeof(value));
+      memset(Stack_base(stack), 0x42,
+             (Stack_high(stack)-Stack_base(stack))*sizeof(value));
 #endif
+    } else {
+      free_stack_memory(stack);
+    }
   } else {
     free_stack_memory(stack);
   }

ocaml/ocaml

runtime/fiber.c
@@ -588,6 +588,27 @@ struct stack_info* caml_alloc_main_stack (uintnat init_wsize)
   return stk;
 }
 
+atomic_uintnat caml_cache_stacks_per_class =
+#if defined(USE_MMAP_MAP_STACK)
+  1
+#else
+  128
+#endif
+  ;
+
+void caml_free_stack_memory(struct stack_info* stack) {
+  atomic_fetch_sub(&live_stack_counter,
+                   (value*)(stack->handler+1) - (value*)stack);
+#ifdef DEBUG
+  memset(stack, 0x42, (char*)stack->handler - (char*)stack);
+#endif
+#ifdef USE_MMAP_MAP_STACK
+  munmap(stack, stack->size);
+#else
+  caml_stat_free(stack);
+#endif
+}
+
 void caml_free_stack (struct stack_info* stack)
 {
   CAMLnoalloc;
@@ -596,24 +617,21 @@ void caml_free_stack (struct stack_info* stack)
   CAMLassert(stack->magic == 42);
   CAMLassert(cache != NULL);
   if (stack->cache_bucket != -1) {
-    stack->exception_ptr =
-      (void*)(cache[stack->cache_bucket]);
-    cache[stack->cache_bucket] = stack;
+    struct stack_info* top = (struct stack_info*)cache[stack->cache_bucket];
+    int64_t count = top ? top->id : 0;
+    if (count < caml_cache_stacks_per_class) {
+      stack->exception_ptr = (void *)top;
+      stack->id = count + 1;
+      cache[stack->cache_bucket] = stack;
 #ifdef DEBUG
     memset(Stack_base(stack), 0x42,
            (Stack_high(stack)-Stack_base(stack))*sizeof(value));
 #endif
+    } else {
+      caml_free_stack_memory(stack);
+    }
   } else {
-    atomic_fetch_sub(&live_stack_counter,
-                     (value*)(stack->handler+1) - (value*)stack);
-#ifdef DEBUG
-    memset(stack, 0x42, (char*)stack->handler - (char*)stack);
-#endif
-#ifdef USE_MMAP_MAP_STACK
-    munmap(stack, stack->size);
-#else
-    caml_stat_free(stack);
-#endif
+    caml_free_stack_memory(stack);
   }
 }
 