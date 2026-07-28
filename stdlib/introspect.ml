e46a285214e4a15ecb928c11a18f3979aba9b2d3
Frédéric Bour <frederic.bour@lakaban.net>
2026-07-28 16:11:54 +0900

    Introspect: tweak pretty-printing style

--- stdlib/introspect.ml
+++ stdlib/introspect.ml
@@ -409,16 +409,18 @@ module Print = struct
     | Dyn.Tuple { name ="::"; fields } when Dyn.field_count fields = 2 ->
         fprintf ppf "[@[<hv>%a@]]" (pp_list true) fields
     | Dyn.Tuple { name; fields } ->
-        fprintf ppf "%s(@[<hv>%a@])"
+        fprintf ppf "%s@ (@[<hv>%a@])"
           name (pp_fields pp_tuple_field ",") fields
     | Dyn.Record {name; fields} ->
-        fprintf ppf "%s{@[<hv>%a@]}" name
+        fprintf ppf "%s@ {@[<hv>%a@]}" name
           (pp_fields pp_record_field ";") fields
+    | Dyn.Extension (name, uid, fields) when Dyn.field_count fields = 0 ->
+        fprintf ppf "%s/%d" name uid
     | Dyn.Extension (name, uid, fields) ->
-        fprintf ppf "%s/%d(@[<hv>%a@])"
+        fprintf ppf "%s/%d (@[<hv>%a@])"
           name uid (pp_fields pp_dynobj ",") fields
     | Dyn.Polymorphic_variant (name, payload) ->
-        fprintf ppf "`%s(@[<hv>%a@])" name pp_dynobj payload
+        fprintf ppf "`%s (@[<hv>%a@])" name pp_dynobj payload
     | Dyn.Closure  -> fprintf ppf "<Closure>"
     | Dyn.Lazy     -> fprintf ppf "<Lazy>"
     | Dyn.Abstract -> fprintf ppf "<Abstract>"
