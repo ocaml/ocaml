f8db9503abe6a8cc1ea473d3da0676e7b91474b7
Frédéric Bour <frederic.bour@lakaban.net>
2026-07-29 21:45:28 +0900

    add cycle test

--- testsuite/tests/introspect/printval.ml
+++ testsuite/tests/introspect/printval.ml
@@ -5,6 +5,7 @@ open Introspect.Print
 let () = print_any_endline 1
 let () = print_any_endline "Hello"
 let () = print_any_endline [1;2;3]
+let () = print_any_endline (let rec l = 1 :: l in l)
 
 module M = Map.Make(Int)
 let () = print_any_endline (M.of_list [1, 1; 2, 2; 3, 3])
--- testsuite/tests/introspect/printval_toplevel.ml
+++ testsuite/tests/introspect/printval_toplevel.ml
@@ -11,6 +11,7 @@ open Introspect.Print;;
 print_any_endline 1;;
 print_any_endline "Hello";;
 print_any_endline [1;2;3];;
+print_any_endline (let rec l = 1 :: l in l);;
 
 module M = Map.Make(Int);;
 
