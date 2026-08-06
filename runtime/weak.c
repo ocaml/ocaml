cf9369536133334a8fbbf9700f258d742879fe04
Stefan Muenzel <source@s.muenzel.net>
2026-08-06 21:18:24 +0100

    Update comment in ephe_get_field_copy

--- runtime/weak.c
+++ runtime/weak.c
@@ -382,7 +382,8 @@ static value ephe_get_field_copy (value e, mlsize_t offset)
       goto some;
     }
 
-    /* Don't copy, but do darken, custom blocks #7279 */
+    /* Don't copy, but do darken, custom blocks #7279
+       or continuations #14988 */
     if (Tag_val(val) == Custom_tag || Tag_val(val) == Cont_tag) {
       if (caml_marking_started())
         caml_darken (Caml_state, val, 0);
