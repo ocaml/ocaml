ecf3ee6dd5e71510c18f65b5b12ebffaf0ac9e13
Nicolás Ojeda Bär <n.oje.bar@gmail.com>
2026-08-28 08:04:24 +0200

    Restore test that catches preemption issue on Windows.

--- testsuite/tests/lib-systhreads/testpreempt.ml
+++ testsuite/tests/lib-systhreads/testpreempt.ml
@@ -1,12 +1,6 @@
 (* TEST
  include systhreads;
  hassysthreads;
-   (*
-     On Windows, we use Sleep(0) for triggering preemption of threads.
-     However, this does not seem very reliable, so that this test fails
-     on some Windows configurations. See GPR #1533.
-   *)
- not-target-windows;
  {
    bytecode;
  }{
