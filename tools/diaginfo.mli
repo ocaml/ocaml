2a0a1de6b9a38aceb399987a4d2ccf2fcf0bca0f
Florian Angeletti <florian.angeletti@inria.fr>
2026-09-08 16:11:39 +0200

    review: add toplevel comment to diaginfo.mli

--- tools/diaginfo.mli
+++ tools/diaginfo.mli
@@ -12,3 +12,15 @@
 (*   special exception on linking described in the file LICENSE.          *)
 (*                                                                        *)
 (**************************************************************************)
+
+
+(** [ocamldiaginfo] provides a way to print metadata information about all
+    diagnostics printed by the compiler and REPL
+
+  - [ocamldiaginfo -history] prints the full history of diagnostics across all
+  versions
+  - [ocamldiaginfo -schema <name>] prints the schema of a diagnostic, by default
+  in an annotated ADT format or as a json schema with the [-schema-format json]
+  flag
+  - [ocamldiaginfo -list] prints all known schema
+*)
